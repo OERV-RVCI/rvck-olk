64f55f50566ae9a05ffb73661ea6c3259b6eaeef
Borislav Petkov (AMD) <bp@alien8.de>
2024-11-18 17:17:24 +0100

    x86/microcode/AMD: Have __apply_microcode_amd() return bool
    mainline inclusion
    from mainline-v6.14-rc1
    commit 78e0aadbd4c6807a06a9d25bc190fe515d3f3c42
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICUIGI
    CVE: NA
    
    Reference: https://github.com/torvalds/linux/commit/78e0aadbd4c6807a06a9d25bc190fe515d3f3c42
    
    --------------------------------
    
    commit 78e0aadbd4c6807a06a9d25bc190fe515d3f3c42 upstream
    
    This is the natural thing to do anyway.
    
    No functional changes.
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>

--- arch/x86/kernel/cpu/microcode/amd.c
+++ arch/x86/kernel/cpu/microcode/amd.c
@@ -416,7 +416,7 @@ static void scan_containers(u8 *ucode, size_t size, struct cont_desc *desc)
 	}
 }
 
-static int __apply_microcode_amd(struct microcode_amd *mc, unsigned int psize)
+static bool __apply_microcode_amd(struct microcode_amd *mc, unsigned int psize)
 {
 	unsigned long p_addr = (unsigned long)&mc->hdr.data_code;
 	u32 rev, dummy;
@@ -439,9 +439,9 @@ static int __apply_microcode_amd(struct microcode_amd *mc, unsigned int psize)
 	/* verify patch application was successful */
 	native_rdmsr(MSR_AMD64_PATCH_LEVEL, rev, dummy);
 	if (rev != mc->hdr.patch_id)
-		return -1;
+		return false;
 
-	return 0;
+	return true;
 }
 
 /*
@@ -477,7 +477,7 @@ static bool early_apply_microcode(u32 cpuid_1_eax, u32 old_rev, void *ucode, siz
 	if (old_rev > mc->hdr.patch_id)
 		return ret;
 
-	return !__apply_microcode_amd(mc, desc.psize);
+	return __apply_microcode_amd(mc, desc.psize);
 }
 
 static bool get_builtin_microcode(struct cpio_data *cp, unsigned int family)
@@ -639,7 +639,7 @@ void reload_ucode_amd(unsigned int cpu)
 	rdmsr(MSR_AMD64_PATCH_LEVEL, rev, dummy);
 
 	if (rev < mc->hdr.patch_id) {
-		if (!__apply_microcode_amd(mc, p->size))
+		if (__apply_microcode_amd(mc, p->size))
 			pr_info_once("reload revision: 0x%08x\n", mc->hdr.patch_id);
 	}
 }
@@ -692,7 +692,7 @@ static enum ucode_state apply_microcode_amd(int cpu)
 		goto out;
 	}
 
-	if (__apply_microcode_amd(mc_amd, p->size)) {
+	if (!__apply_microcode_amd(mc_amd, p->size)) {
 		pr_err("CPU%d: update failed for patch_level=0x%08x\n",
 			cpu, mc_amd->hdr.patch_id);
 		return UCODE_ERROR;
