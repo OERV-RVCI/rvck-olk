openeuler-mirror/kernel

commit 582fab522ccec5d0d750a10dbf60888d5367433e

crypto: ccp: Implement CSV_DOWNLOAD_FIRMWARE ioctl command
hygon inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I98VXP
CVE: NA

---------------------------

The CSV_DOWNLOAD_FIRMWARE command can be used by the platform owner to
updating CSV firmware.

Signed-off-by: hanliyang <hanliyang@hygon.cn>

drivers/crypto/ccp/hygon/csv-dev.c
@@ -16,6 +16,7 @@
 #include <uapi/linux/psp-hygon.h>
 
 #include "psp-dev.h"
+#include "csv-dev.h"
 
 /*
  * Hygon CSV build info:
@@ -90,6 +91,74 @@ e_free:
 	return ret;
 }
 
+static int csv_ioctl_do_download_firmware(struct sev_issue_cmd *argp)
+{
+	struct sev_data_download_firmware *data = NULL;
+	struct csv_user_data_download_firmware input;
+	int ret, order;
+	struct page *p;
+	u64 data_size;
+
+	/* Only support DOWNLOAD_FIRMWARE if build greater or equal 1667 */
+	if (!csv_version_greater_or_equal(1667)) {
+		pr_err("DOWNLOAD_FIRMWARE not supported\n");
+		return -EIO;
+	}
+
+	if (copy_from_user(&input, (void __user *)argp->data, sizeof(input)))
+		return -EFAULT;
+
+	if (!input.address) {
+		argp->error = SEV_RET_INVALID_ADDRESS;
+		return -EINVAL;
+	}
+
+	if (!input.length || input.length > CSV_FW_MAX_SIZE) {
+		argp->error = SEV_RET_INVALID_LEN;
+		return -EINVAL;
+	}
+
+	/*
+	 * CSV FW expects the physical address given to it to be 32
+	 * byte aligned. Memory allocated has structure placed at the
+	 * beginning followed by the firmware being passed to the CSV
+	 * FW. Allocate enough memory for data structure + alignment
+	 * padding + CSV FW.
+	 */
+	data_size = ALIGN(sizeof(struct sev_data_download_firmware), 32);
+
+	order = get_order(input.length + data_size);
+	p = alloc_pages(GFP_KERNEL, order);
+	if (!p)
+		return -ENOMEM;
+
+	/*
+	 * Copy firmware data to a kernel allocated contiguous
+	 * memory region.
+	 */
+	data = page_address(p);
+	if (copy_from_user((void *)(page_address(p) + data_size),
+			   (void *)input.address, input.length)) {
+		ret = -EFAULT;
+		goto err_free_page;
+	}
+
+	data->address = __psp_pa(page_address(p) + data_size);
+	data->len = input.length;
+
+	ret = hygon_psp_hooks.__sev_do_cmd_locked(SEV_CMD_DOWNLOAD_FIRMWARE,
+						  data, &argp->error);
+	if (ret)
+		pr_err("Failed to update CSV firmware: %#x\n", argp->error);
+	else
+		pr_info("CSV firmware update successful\n");
+
+err_free_page:
+	__free_pages(p, order);
+
+	return ret;
+}
+
 static long csv_ioctl(struct file *file, unsigned int ioctl, unsigned long arg)
 {
 	void __user *argp = (void __user *)arg;
@@ -122,6 +191,9 @@ static long csv_ioctl(struct file *file, unsigned int ioctl, unsigned long arg)
 		break;
 	case CSV_PLATFORM_SHUTDOWN:
 		ret = hygon_psp_hooks.__sev_platform_shutdown_locked(&input.error);
+		break;
+	case CSV_DOWNLOAD_FIRMWARE:
+		ret = csv_ioctl_do_download_firmware(&input);
 		break;
 	default:
 		/*

include/linux/psp-hygon.h
@@ -16,6 +16,8 @@
 /***************************** CSV interface *********************************/
 /*****************************************************************************/
 
+#define CSV_FW_MAX_SIZE		0x80000	/* 512KB */
+
 /**
  * Guest/platform management commands for CSV
  */

include/uapi/linux/psp-hygon.h
@@ -23,6 +23,7 @@
 enum {
 	CSV_PLATFORM_INIT = 101,
 	CSV_PLATFORM_SHUTDOWN = 102,
+	CSV_DOWNLOAD_FIRMWARE = 128,
 	CSV_HGSC_CERT_IMPORT = 201,
 
 	CSV_MAX,
@@ -41,6 +42,17 @@ struct csv_user_data_hgsc_cert_import {
 	__u32 hgscsk_cert_len;                  /* In */
 	__u64 hgsc_cert_address;                /* In */
 	__u32 hgsc_cert_len;                    /* In */
+} __packed;
+
+/**
+ * struct csv_user_data_download_firmware - DOWNLOAD_FIRMWARE command parameters
+ *
+ * @address: physical address of CSV firmware image
+ * @length: length of the CSV firmware image
+ */
+struct csv_user_data_download_firmware {
+	__u64 address;				/* In */
+	__u32 length;				/* In */
 } __packed;
 
 #endif	/* __PSP_HYGON_USER_H__ */

OERV-RVCI/rvck-olk

commit 0fd35a0205376c68c02cd29302e665e9daa0b6f8

crypto: ccp: Get api version again when update Hygon CSV firmware at runtime
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IBGDLQ
CVE: NA

---------------------------

The commit 582fab522cce ("crypto: ccp: Implement CSV_DOWNLOAD_FIRMWARE
ioctl command") support update Hygon CSV firmware at runtime, but it
don't update API version info in the driver after issues the
DOWNLOAD_FIRMWARE command. When we want use the new features in the
updated firmware, the version check in this driver will fail. To address
this problem, we should regain the api version when DOWNLOAD_FIRMWARE
command returns.

Fixes: 582fab522cce ("crypto: ccp: Implement CSV_DOWNLOAD_FIRMWARE ioctl command")
Signed-off-by: hanliyang <hanliyang@hygon.cn>

drivers/crypto/ccp/hygon/csv-dev.c
@@ -128,6 +128,45 @@ e_free:
 	return ret;
 }
 
+static int csv_get_api_version(void)
+{
+	struct sev_device *sev;
+	struct sev_user_data_status *status;
+	int error = 0, ret;
+
+	if (!hygon_psp_hooks.sev_dev_hooks_installed)
+		return -ENODEV;
+
+	if (!psp_master || !psp_master->sev_data)
+		return -ENODEV;
+
+	sev = psp_master->sev_data;
+
+	status = kzalloc(sizeof(*status), GFP_KERNEL);
+	if (!status)
+		return -ENOMEM;
+
+	ret = hygon_psp_hooks.__sev_do_cmd_locked(SEV_CMD_PLATFORM_STATUS,
+						  status, &error);
+	if (ret) {
+		dev_err(sev->dev,
+			"CSV: failed to get status. Error: %#x\n", error);
+		goto e_free_status;
+	}
+
+	sev->api_major = status->api_major;
+	sev->api_minor = status->api_minor;
+	sev->build = status->build;
+	sev->state = status->state;
+
+	csv_update_api_version(status);
+
+	ret = 0;
+e_free_status:
+	kfree(status);
+	return ret;
+}
+
 static int csv_ioctl_do_download_firmware(struct sev_issue_cmd *argp)
 {
 	struct sev_data_download_firmware *data = NULL;
@@ -185,10 +224,20 @@ static int csv_ioctl_do_download_firmware(struct sev_issue_cmd *argp)
 
 	ret = hygon_psp_hooks.__sev_do_cmd_locked(SEV_CMD_DOWNLOAD_FIRMWARE,
 						  data, &argp->error);
-	if (ret)
+	if (ret) {
 		pr_err("Failed to update CSV firmware: %#x\n", argp->error);
-	else
+		goto err_free_page;
+	} else {
 		pr_info("CSV firmware update successful\n");
+	}
+
+	/*
+	 * Synchronize API version status. The return value of csv_get_api_version
+	 * will inform the user of any error encountered when attempting to
+	 * communicate with the Hygon PSP after the DOWNLOAD_FIRMWARE API completes
+	 * successfully.
+	 */
+	ret = csv_get_api_version();
 
 err_free_page:
 	__free_pages(p, order);