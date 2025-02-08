openeuler-mirror/kernel

commit a9843ce606efa1727038da23e07dfccf4eaac8d7

perf/x86/amd/core: Update and fix stalled-cycles-* events for Zen 2 and later
mainline inclusion
from mainline-v6.9-rc2
commit c7b2edd8377be983442c1344cb940cd2ac21b601
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I9HG74
CVE: NA

--------------------------------

AMD processors based on Zen 2 and later microarchitectures do not
support PMCx087 (instruction pipe stalls) which is used as the backing
event for "stalled-cycles-frontend" and "stalled-cycles-backend".

Use PMCx0A9 (cycles where micro-op queue is empty) instead to count
frontend stalls and remove the entry for backend stalls since there
is no direct replacement.

Signed-off-by: Sandipan Das <sandipan.das@amd.com>
Signed-off-by: Ingo Molnar <mingo@kernel.org>
Reviewed-by: Ian Rogers <irogers@google.com>
Fixes: 3fe3331bb285 ("perf/x86/amd: Add event map for AMD Family 17h")
Link: https://lore.kernel.org/r/03d7fc8fa2a28f9be732116009025bdec1b3ec97.1711352180.git.sandipan.das@amd.com
Signed-off-by: Wenkuan Wang <Wenkuan.Wang@amd.com>

arch/x86/events/amd/core.c
@@ -250,7 +250,7 @@ static const u64 amd_perfmon_event_map[PERF_COUNT_HW_MAX] =
 /*
  * AMD Performance Monitor Family 17h and later:
  */
-static const u64 amd_f17h_perfmon_event_map[PERF_COUNT_HW_MAX] =
+static const u64 amd_zen1_perfmon_event_map[PERF_COUNT_HW_MAX] =
 {
 	[PERF_COUNT_HW_CPU_CYCLES]		= 0x0076,
 	[PERF_COUNT_HW_INSTRUCTIONS]		= 0x00c0,
@@ -262,10 +262,24 @@ static const u64 amd_f17h_perfmon_event_map[PERF_COUNT_HW_MAX] =
 	[PERF_COUNT_HW_STALLED_CYCLES_BACKEND]	= 0x0187,
 };
 
+static const u64 amd_zen2_perfmon_event_map[PERF_COUNT_HW_MAX] =
+{
+	[PERF_COUNT_HW_CPU_CYCLES]		= 0x0076,
+	[PERF_COUNT_HW_INSTRUCTIONS]		= 0x00c0,
+	[PERF_COUNT_HW_CACHE_REFERENCES]	= 0xff60,
+	[PERF_COUNT_HW_CACHE_MISSES]		= 0x0964,
+	[PERF_COUNT_HW_BRANCH_INSTRUCTIONS]	= 0x00c2,
+	[PERF_COUNT_HW_BRANCH_MISSES]		= 0x00c3,
+	[PERF_COUNT_HW_STALLED_CYCLES_FRONTEND]	= 0x00a9,
+};
+
 static u64 amd_pmu_event_map(int hw_event)
 {
-	if (boot_cpu_data.x86 >= 0x17)
-		return amd_f17h_perfmon_event_map[hw_event];
+	if (cpu_feature_enabled(X86_FEATURE_ZEN2) || boot_cpu_data.x86 >= 0x19)
+		return amd_zen2_perfmon_event_map[hw_event];
+
+	if (cpu_feature_enabled(X86_FEATURE_ZEN1))
+		return amd_zen1_perfmon_event_map[hw_event];
 
 	return amd_perfmon_event_map[hw_event];
 }

OERV-RVCI/rvck-olk

commit 3a33ea90b0470372c371c42d25fe3e631c683209

perf/x86/amd/core: Fix performance monitor for Hygon family 18h processor
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IBJ5Z1
CVE: NA

---------------------------

The commit a9843ce606ef ("perf/x86/amd/core: Update and fix
stalled-cycles-* events for Zen 2 and later") changed
amd_f17h_perfmon_event_map to amd_zen1_perfmon_event_map
which is limited to processors only with X86_FEATURE_ZEN1.

This makes some event counters such as cache-misses and
cache-references can not be tracked on Hygon family 18h processors.
When using perf tool to monitor performance, the result will be
listed as follows:
  0    cache-miesses  #  0.000 % of all cache refs
  0    cache-references

So add perfmon_event_map support for Hygon family 18h processors
to fix the incorrect display.

Fixes: a9843ce606ef ("perf/x86/amd/core: Update and fix stalled-cycles-* events for Zen 2 and later")

Signed-off-by: Wenhui Fan <fanwh@hygon.cn>

arch/x86/events/amd/core.c
@@ -296,6 +296,9 @@ static u64 amd_pmu_event_map(int hw_event)
 	if (cpu_feature_enabled(X86_FEATURE_ZEN1))
 		return amd_zen1_perfmon_event_map[hw_event];
 
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
+		return amd_zen1_perfmon_event_map[hw_event];
+
 	return amd_perfmon_event_map[hw_event];
 }
 