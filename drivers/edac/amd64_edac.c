openeuler-mirror/kernel

commit 7722509e5c1f219db46298c43ca4f9b9d85ce40a

EDAC/amd64: Adjust UMC channel for Hygon family 18h model 6h
hygon inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8U3K8
CVE: NA

---------------------------

Hygon family 18h model 6h has 2 cs mapped to 1 umc, so adjust for it.

Signed-off-by: Pu Wen <puwen@hygon.cn>

drivers/edac/amd64_edac.c
@@ -3115,6 +3115,7 @@ static void decode_umc_error(int node_id, struct mce *m)
 	struct amd64_pvt *pvt;
 	struct err_info err;
 	u64 sys_addr;
+	u8 umc;
 
 	node_id = fixup_node_id(node_id, m);
 
@@ -3145,7 +3146,12 @@ static void decode_umc_error(int node_id, struct mce *m)
 
 	pvt->ops->get_err_info(m, &err);
 
-	if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, err.channel, &sys_addr)) {
+	if (hygon_f18h_m4h() && boot_cpu_data.x86_model == 0x6)
+		umc = err.channel << 1;
+	else
+		umc = err.channel;
+
+	if (umc_normaddr_to_sysaddr(m->addr, pvt->mc_node_id, umc, &sys_addr)) {
 		err.err_code = ERR_NORM_ADDR;
 		goto log_error;
 	}

OERV-RVCI/rvck-olk

commit f937023664ce77091d50d07642b05f2b70c23bac

EDAC/amd64: Fix the calculation of instance id for Hygon family 18h model 6h
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IBN075
CVE: NA

---------------------------

On Hygon family 18h model 6h platform, each DDR has two subchannels,
and each subchannel has a mca bank with a cs corresponded. In the
process of address translation, it needs to know the instance id
of cs to access df register correctly. The instance id of cs can be
calculated from IPID[23:20] and IPID[13]: IPID[23:20] represents
which umc controller it belongs to, and IPID[13] represents which
subchannel it is.

Fixes: 7722509e5c1f ("EDAC/amd64: Adjust UMC channel for Hygon family 18h model 6h")
Signed-off-by: Wenhui Fan <fanwh@hygon.cn>

drivers/edac/amd64_edac.c
@@ -3152,7 +3152,7 @@ static void decode_umc_error(int node_id, struct mce *m)
 
 	if (hygon_f18h_m4h() || boot_cpu_data.x86_model == 0x10) {
 		if (boot_cpu_data.x86_model == 0x6)
-			umc = err.channel << 1;
+			umc = (err.channel << 1) + ((m->ipid & BIT(13)) >> 13);
 		else
 			umc = err.channel;
 