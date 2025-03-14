847b8974b76230ac4621856d6d248eda5d70e881
Josh Poimboeuf <jpoimboe@kernel.org>
2025-03-14 12:29:00 -0700

    objtool: Fix error handling inconsistencies in check()
    stable inclusion
    from stable-v6.6.93
    commit 4beb1e55f6b08aaa9376ed6926586e966bc4d319
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8365
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=4beb1e55f6b08aaa9376ed6926586e966bc4d319
    
    --------------------------------
    
    [ Upstream commit b745962cb97569aad026806bb0740663cf813147 ]
    
    Make sure all fatal errors are funneled through the 'out' label with a
    negative ret.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Brendan Jackman <jackmanb@google.com>
    Link: https://lore.kernel.org/r/0f49d6a27a080b4012e84e6df1e23097f44cc082.1741975349.git.jpoimboe@kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 4beb1e55f6b08aaa9376ed6926586e966bc4d319)
    Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -4726,8 +4726,10 @@ int check(struct objtool_file *file)
 	init_cfi_state(&force_undefined_cfi);
 	force_undefined_cfi.force_undefined = true;
 
-	if (!cfi_hash_alloc(1UL << (file->elf->symbol_bits - 3)))
+	if (!cfi_hash_alloc(1UL << (file->elf->symbol_bits - 3))) {
+		ret = -1;
 		goto out;
+	}
 
 	cfi_hash_add(&init_cfi);
 	cfi_hash_add(&func_cfi);
@@ -4744,7 +4746,7 @@ int check(struct objtool_file *file)
 	if (opts.retpoline) {
 		ret = validate_retpoline(file);
 		if (ret < 0)
-			return ret;
+			goto out;
 		warnings += ret;
 	}
 
@@ -4780,7 +4782,7 @@ int check(struct objtool_file *file)
 		 */
 		ret = validate_unrets(file);
 		if (ret < 0)
-			return ret;
+			goto out;
 		warnings += ret;
 	}
 
@@ -4843,7 +4845,7 @@ int check(struct objtool_file *file)
 	if (opts.prefix) {
 		ret = add_prefix_symbols(file);
 		if (ret < 0)
-			return ret;
+			goto out;
 		warnings += ret;
 	}
 
