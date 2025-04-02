1d334a25caf930d12a66e7a346cdbacba4900910
eillon <yezhenyu2@huawei.com>
2025-04-02 15:56:27 +0800

    arm64/kabi: use KABI_EXTEND to skip KABI check
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IB103D
    CVE: NA
    
    ----------------------------------------------------------------------
    
    Use KABI_EXTEND to skip KABI check of HDBSS feature.
    
    Signed-off-by: eillon <yezhenyu2@huawei.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -644,10 +644,10 @@ struct kvm_vcpu_arch {
 
 #ifdef CONFIG_ARM64_HDBSS
 	/* HDBSS registers info */
-	struct {
+	KABI_EXTEND(struct {
 		u64 br_el2;
 		u64 prod_el2;
-	} hdbss;
+	} hdbss)
 #endif
 };
 
