351c46dd8b00876226c3aa72aaa554446c8ed636
Baihan Li <libaihan@huawei.com>
2025-05-14 10:10:46 +0800

    Revert "drm/hisilicon/hibmc: Getting connector info and EDID by using AUX channel"
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/IC4PGN
    CVE: NA
    
    ----------------------------------------------------------------------
    
    This reverts commit d101c46d63c30d8c147a8b8235fdd66da0ea657c.
    
    Revert this patch to fix the problem in this commit.
    
    Fixes: d101c46d63c3 ("drm/hisilicon/hibmc: Getting connector info and EDID by using AUX channel")
    Signed-off-by: Baihan Li <libaihan@huawei.com>

--- drivers/gpu/drm/hisilicon/hibmc/dp/dp_aux.c
+++ drivers/gpu/drm/hisilicon/hibmc/dp/dp_aux.c
@@ -161,8 +161,7 @@ void hibmc_dp_aux_init(struct hibmc_dp *dp)
 				 HIBMC_DP_MIN_PULSE_NUM);
 
 	dp->aux.transfer = hibmc_dp_aux_xfer;
-	dp->aux.name = "HIBMC DRM dp aux";
-	dp->aux.drm_dev = dp->drm_dev;
+	dp->aux.is_remote = 0;
 	drm_dp_aux_init(&dp->aux);
 	dp->dp_dev->aux = &dp->aux;
 }
