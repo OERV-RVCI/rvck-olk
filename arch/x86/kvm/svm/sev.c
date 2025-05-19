openeuler-mirror/kernel

commit f52f5e1c0d84d6bf76541c6e4a035194da031c5e

KVM: SVM: Print Hygon CSV support info if support is detected
hygon inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I98NP1
CVE: NA

---------------------------

The KVM will print 'SEV supported' instead of 'CSV supported' on Hygon
CPU if CSV is supported. Fix these confused messages here.

Fix other 'SEV' messages in arch/x86/kvm/svm/svm.c.

Signed-off-by: hanliyang <hanliyang@hygon.cn>

arch/x86/kvm/svm/sev.c
@@ -24,6 +24,7 @@
 #include <asm/trapnr.h>
 #include <asm/fpu/xcr.h>
 #include <asm/debugreg.h>
+#include <asm/processor-hygon.h>
 
 #include "mmu.h"
 #include "x86.h"
@@ -2274,13 +2275,15 @@ void __init sev_hardware_setup(void)
 
 out:
 	if (boot_cpu_has(X86_FEATURE_SEV))
-		pr_info("SEV %s (ASIDs %u - %u)\n",
+		pr_info("%s %s (ASIDs %u - %u)\n",
+			is_x86_vendor_hygon() ? "CSV" : "SEV",
 			sev_supported ? min_sev_asid <= max_sev_asid ? "enabled" :
 								       "unusable" :
 								       "disabled",
 			min_sev_asid, max_sev_asid);
 	if (boot_cpu_has(X86_FEATURE_SEV_ES))
-		pr_info("SEV-ES %s (ASIDs %u - %u)\n",
+		pr_info("%s %s (ASIDs %u - %u)\n",
+			is_x86_vendor_hygon() ? "CSV2" : "SEV-ES",
 			sev_es_supported ? "enabled" : "disabled",
 			min_sev_asid > 1 ? 1 : 0, min_sev_asid - 1);
 

arch/x86/kvm/svm/svm.c
@@ -41,6 +41,7 @@
 #include <asm/traps.h>
 #include <asm/reboot.h>
 #include <asm/fpu/api.h>
+#include <asm/processor-hygon.h>
 
 #include <trace/events/ipi.h>
 
@@ -545,7 +546,8 @@ static bool __kvm_is_svm_supported(void)
 	}
 
 	if (cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT)) {
-		pr_info("KVM is unsupported when running as an SEV guest\n");
+		pr_info("KVM is unsupported when running as an %s guest\n",
+			is_x86_vendor_hygon() ? "CSV" : "SEV");
 		return false;
 	}
 

OERV-RVCI/rvck-olk

commit 0d0f1559924a008edb77d9f4929f587db5d7f471

KVM: SVM: CSV is unsupported if both the firmware with a build ID < 1878 and SME is not in use
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IC8VUC
CVE: NA

---------------------------

For firmware with a build ID < 1878, the CSV guest cannot run if the host
kernel is not using SME.

Fixes: f52f5e1c0d84 ("KVM: SVM: Print Hygon CSV support info if support is detected")
Signed-off-by: hanliyang <hanliyang@hygon.cn>

arch/x86/kvm/svm/sev.c
@@ -2335,6 +2335,9 @@ void __init sev_hardware_setup(void)
 	bool sev_es_supported = false;
 	bool sev_supported = false;
 
+	if (is_x86_vendor_hygon() && hygon_csv_build < 1878 && !sme_me_mask)
+		goto out;
+
 	if (!sev_enabled || !npt_enabled || !nrips)
 		goto out;
 