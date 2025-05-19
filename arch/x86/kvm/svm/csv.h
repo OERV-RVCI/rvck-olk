openeuler-mirror/kernel

commit 39bc4f1b8de8752405bb4e54d3be35c6cb0f8887

KVM: SVM: Fix the available ASID range for CSV2 guest
hygon inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I98WPG
CVE: NA

---------------------------

All the ASIDs in range [1, max_sev_asid] are available for CSV2 guest,
regardless of the value of min_sev_asid.

Signed-off-by: hanliyang <hanliyang@hygon.cn>

arch/x86/kvm/svm/sev.c
@@ -159,6 +159,13 @@ static int sev_asid_new(struct kvm_sev_info *sev)
 	bool retry = true;
 	int ret;
 
+	/*
+	 * No matter what the min_sev_asid is, all asids in range
+	 * [1, max_sev_asid] can be used for CSV2 guest on Hygon CPUs.
+	 */
+	if (is_x86_vendor_hygon())
+		max_asid = max_sev_asid;
+
 	if (min_asid > max_asid)
 		return -ENOTTY;
 
@@ -2295,11 +2302,19 @@ void __init sev_hardware_setup(void)
 	if (!boot_cpu_has(X86_FEATURE_SEV_ES))
 		goto out;
 
-	/* Has the system been allocated ASIDs for SEV-ES? */
-	if (min_sev_asid == 1)
-		goto out;
+	if (is_x86_vendor_hygon()) {
+		/*
+		 * Ths ASIDs from 1 to max_sev_asid are available for hygon
+		 * CSV2 guest.
+		 */
+		sev_es_asid_count = max_sev_asid;
+	} else {
+		/* Has the system been allocated ASIDs for SEV-ES? */
+		if (min_sev_asid == 1)
+			goto out;
 
-	sev_es_asid_count = min_sev_asid - 1;
+		sev_es_asid_count = min_sev_asid - 1;
+	}
 	WARN_ON_ONCE(misc_cg_set_capacity(MISC_CG_RES_SEV_ES, sev_es_asid_count));
 	sev_es_supported = true;
 
@@ -2315,7 +2330,8 @@ out:
 		pr_info("%s %s (ASIDs %u - %u)\n",
 			is_x86_vendor_hygon() ? "CSV2" : "SEV-ES",
 			sev_es_supported ? "enabled" : "disabled",
-			min_sev_asid > 1 ? 1 : 0, min_sev_asid - 1);
+			is_x86_vendor_hygon() ? 1 : (min_sev_asid > 1 ? 1 : 0),
+			is_x86_vendor_hygon() ? max_sev_asid : min_sev_asid - 1);
 
 	sev_enabled = sev_supported;
 	sev_es_enabled = sev_es_supported;

OERV-RVCI/rvck-olk

commit f4aa328196ca98e178af6493cd15a2070782a44f

KVM: SVM: The ASID range available for CSV2 guests vary across different firmware
hygon inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IC8VUC
CVE: NA

---------------------------

For firmware with a build ID < 1810, if CPUID_0x8000_001F_EDX.bits0-31 is
1, the ASID range available for CSV2 guests is [0, 0) (i.e., no ASIDs are
available). Otherwise, the range is [1, CPUID_0x8000_001F_EDX.bits0-31).

For firmware with a build ID >= 1810, the ASID range available for CSV2
guests is [1, CPUID_0x8000_001F_ECX.bits0-31].

Fixes: 39bc4f1b8de8 ("KVM: SVM: Fix the available ASID range for CSV2 guest")
Signed-off-by: hanliyang <hanliyang@hygon.cn>

arch/x86/kvm/svm/csv.h
@@ -32,6 +32,8 @@ struct csv_ringbuf_infos {
 	int num;
 };
 
+extern u32 hygon_csv_build;
+
 #ifdef CONFIG_KVM_SUPPORTS_CSV_REUSE_ASID
 
 #define ASID_USERID_LENGTH 20

arch/x86/kvm/svm/sev.c
@@ -164,10 +164,10 @@ static int sev_asid_new(struct kvm_sev_info *sev)
 	int ret;
 
 	/*
-	 * No matter what the min_sev_asid is, all asids in range
+	 * When the firmware is with build ID >= 1810, all asids in range
 	 * [1, max_sev_asid] can be used for CSV2 guest on Hygon CPUs.
 	 */
-	if (is_x86_vendor_hygon())
+	if (is_x86_vendor_hygon() && hygon_csv_build >= 1810)
 		max_asid = max_sev_asid;
 
 	if (min_asid > max_asid)
@@ -2402,7 +2402,7 @@ void __init sev_hardware_setup(void)
 	if (!boot_cpu_has(X86_FEATURE_SEV_ES))
 		goto out;
 
-	if (is_x86_vendor_hygon()) {
+	if (is_x86_vendor_hygon() && hygon_csv_build >= 1810) {
 		/*
 		 * Ths ASIDs from 1 to max_sev_asid are available for hygon
 		 * CSV2 guest.
@@ -2430,8 +2430,10 @@ out:
 		pr_info("%s %s (ASIDs %u - %u)\n",
 			is_x86_vendor_hygon() ? "CSV2" : "SEV-ES",
 			sev_es_supported ? "enabled" : "disabled",
-			is_x86_vendor_hygon() ? 1 : (min_sev_asid > 1 ? 1 : 0),
-			is_x86_vendor_hygon() ? max_sev_asid : min_sev_asid - 1);
+			(is_x86_vendor_hygon() && hygon_csv_build >= 1810) ?
+			1 : (min_sev_asid > 1 ? 1 : 0),
+			(is_x86_vendor_hygon() && hygon_csv_build >= 1810) ?
+			max_sev_asid : min_sev_asid - 1);
 
 	sev_enabled = sev_supported;
 	sev_es_enabled = sev_es_supported;

drivers/crypto/ccp/hygon/csv-dev.c
@@ -29,6 +29,7 @@
  *    in AMD SEV.
  */
 u32 hygon_csv_build;
+EXPORT_SYMBOL_GPL(hygon_csv_build);
 
 int csv_comm_mode = CSV_COMM_MAILBOX_ON;
 