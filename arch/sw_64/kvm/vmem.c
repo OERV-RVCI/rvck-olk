openeuler-mirror/kernel

commit 09a48172fb73b5d43fa16ba6c52d8f715546a88e

sw64: add KVM support
Sunway inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I56WV8

--------------------------------

Add KVM support for SW64.

Signed-off-by: Mao Minkai <maominkai@wxiat.com>
Reviewed-by: He Sheng <hesheng@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>

arch/sw_64/kvm/vmem.c
@@ -1,0 +1,183 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/mm.h>
+#include <linux/slab.h>
+#include <linux/genalloc.h>
+#include <linux/miscdevice.h>
+#include <linux/kvm_host.h>
+#include <asm/kvm_host.h>
+
+static bool addr_in_pool(struct gen_pool *pool,
+		unsigned long start, size_t size)
+{
+	bool found = false;
+	unsigned long end = start + size - 1;
+	struct gen_pool_chunk *chunk;
+
+	rcu_read_lock();
+	list_for_each_entry_rcu(chunk, &(pool)->chunks, next_chunk) {
+		if (start >= chunk->start_addr && start <= chunk->end_addr) {
+			if (end <= chunk->end_addr) {
+				found = true;
+				break;
+			}
+		}
+	}
+	rcu_read_unlock();
+	return found;
+}
+
+static int vmem_vm_insert_page(struct vm_area_struct *vma)
+{
+	unsigned long addr, uaddr;
+	struct page *vmem_page;
+	struct vmem_info *info;
+	size_t size;
+	int ret;
+
+	info = vma->vm_private_data;
+	addr = info->start;
+	size = info->size;
+	uaddr = vma->vm_start;
+
+	vm_flags_init(vma, VM_DONTEXPAND | VM_DONTDUMP | VM_MIXEDMAP);
+	vmem_page = pfn_to_page(addr >> PAGE_SHIFT);
+	do {
+		ret = vm_insert_page(vma, uaddr, vmem_page);
+		if (ret < 0) {
+			pr_info("vm_insert_page failed: %d\n", ret);
+			return ret;
+		}
+		vmem_page++;
+		uaddr += PAGE_SIZE;
+		size -= PAGE_SIZE;
+	} while (size > 0);
+
+	return 0;
+}
+
+static void vmem_vm_open(struct vm_area_struct *vma)
+{
+	struct vmem_info *info = vma->vm_private_data;
+
+	atomic_inc(&info->refcnt);
+}
+
+static void vmem_vm_close(struct vm_area_struct *vma)
+{
+	unsigned long addr;
+	size_t size;
+	struct vmem_info *info;
+
+	info = vma->vm_private_data;
+	addr = info->start;
+	size = round_up(info->size, 8 << 20);
+
+	if (atomic_dec_and_test(&info->refcnt)) {
+		if (sw64_kvm_pool && addr_in_pool(sw64_kvm_pool, addr, size)) {
+			pr_info("gen pool free addr: %#lx, size: %#lx\n",
+					addr, size);
+			gen_pool_free(sw64_kvm_pool, addr, size);
+		}
+		kfree(info);
+	}
+}
+
+const struct vm_operations_struct vmem_vm_ops = {
+	.open = vmem_vm_open,
+	.close = vmem_vm_close,
+};
+EXPORT_SYMBOL_GPL(vmem_vm_ops);
+
+static int vmem_open(struct inode *inode, struct file *flip)
+{
+	flip->private_data = NULL;
+	return 0;
+}
+
+static loff_t vmem_llseek(struct file *filp, loff_t offset, int whence)
+{
+	loff_t newpos = 256UL << 30;
+	return newpos;
+}
+
+static int vmem_release(struct inode *inode, struct file *flip)
+{
+	return 0;
+}
+
+static int vmem_mmap(struct file *flip, struct vm_area_struct *vma)
+{
+	unsigned long addr;
+	static struct vmem_info *info;
+	size_t size = vma->vm_end - vma->vm_start;
+	int ret;
+
+	if (!(vma->vm_flags & VM_SHARED)) {
+		pr_err("%s: mapping must be shared\n", __func__);
+		return -EINVAL;
+	}
+
+	if (!sw64_kvm_pool)
+		return -ENOMEM;
+
+	if (flip->private_data == NULL) {
+		addr = gen_pool_alloc(sw64_kvm_pool, round_up(size, 8 << 20));
+		if (!addr)
+			return -ENOMEM;
+
+		info = kzalloc(sizeof(struct vmem_info), GFP_KERNEL);
+		pr_info("guest phys addr=%#lx, size=%#lx\n", addr, size);
+		info->start = addr;
+		info->size = size;
+		flip->private_data = (void *)info;
+	} else {
+		info = flip->private_data;
+		addr = info->start;
+	}
+
+	vma->vm_private_data = (void *)info;
+	vma->vm_ops = &vmem_vm_ops;
+	vma->vm_ops->open(vma);
+
+	/*to do if size bigger than vm_mem_size*/
+	pr_info("sw64_vmem: vm_start=%#lx, size= %#lx\n", vma->vm_start, size);
+
+	vmem_vm_insert_page(vma);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static const struct file_operations vmem_fops = {
+	.owner = THIS_MODULE,
+	.open = vmem_open,
+	.llseek = vmem_llseek,
+	.release = vmem_release,
+	.mmap = vmem_mmap,
+};
+
+static struct miscdevice vmem_dev = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name  = "sw64_vmem",
+	.fops  = &vmem_fops,
+};
+
+int __init vmem_init(void)
+{
+	int err;
+
+	err = misc_register(&vmem_dev);
+	if (err != 0) {
+		pr_err("Could not register sw64_vmem device\n");
+		return err;
+	}
+	return 0;
+}
+
+void vmem_exit(void)
+{
+	misc_deregister(&vmem_dev);
+}

arch/sw_64/include/asm/hcall.h
@@ -1,0 +1,41 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_SW64_HCALL_H
+#define _ASM_SW64_HCALL_H
+
+#define HMC_hcall	0x32
+/* HCALL must > 0 */
+enum HCALL_TYPE {
+	HCALL_HALT		= 10,
+	HCALL_NOTIFY		= 11,
+	HCALL_SHUTDOWN		= 12,
+	HCALL_SET_CLOCKEVENT	= 13,
+	HCALL_IVI		= 14,   /* interrupt between virtual cpu */
+	HCALL_TBI		= 15,   /* tlb flush for virtual cpu */
+	HCALL_STOP		= 16,   /* indicate virtual cpu stopped */
+	HCALL_RESTART		= 17,	/* indicate virtual cpu restarted */
+	HCALL_MSI		= 18,   /* guest request msi intr */
+	HCALL_MSIX		= 19,	/* guest request msix intr */
+	HCALL_SWNET		= 20,   /* guest request swnet service */
+	HCALL_SWNET_IRQ		= 21,   /* guest request swnet intr */
+	HCALL_FATAL_ERROR	= 22,   /* guest fatal error, issued by hmcode */
+	HCALL_MEMHOTPLUG	= 23,   /* guest memory hotplug event */
+	NR_HCALL
+};
+
+static inline unsigned long hcall(unsigned long hcall, unsigned long arg0,
+				  unsigned long arg1, unsigned long arg2)
+{
+	register unsigned long __r0 __asm__("$0");
+	register unsigned long __r16 __asm__("$16") = hcall;
+	register unsigned long __r17 __asm__("$17") = arg0;
+	register unsigned long __r18 __asm__("$18") = arg1;
+	register unsigned long __r19 __asm__("$19") = arg2;
+	__asm__ __volatile__(
+		"sys_call %5 "
+		: "=r"(__r16), "=r"(__r17), "=r"(__r18), "=r"(__r19), "=r"(__r0)
+		: "i"(HMC_hcall), "0"(__r16), "1"(__r17), "2"(__r18), "3"(__r19)
+		: "$1", "$22", "$23", "$24", "$25");
+	return __r0;
+}
+
+#endif /* _ASM_SW64_HCALL_H */

arch/sw_64/include/asm/kvm_asm.h
@@ -1,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_SW64_KVM_ASM_H
+#define _ASM_SW64_KVM_ASM_H
+
+#define SW64_KVM_EXIT_HOST_INTR		0
+#define SW64_KVM_EXIT_IO		1
+#define SW64_KVM_MIGRATION_SET_DIRTY    2
+#define SW64_KVM_MIGRATION_SET_DIRTY_HM 3
+#define SW64_KVM_EXIT_HALT		10
+#define SW64_KVM_EXIT_SHUTDOWN		12
+#define SW64_KVM_EXIT_TIMER		13
+#define SW64_KVM_EXIT_IPI		14
+#define SW64_KVM_EXIT_STOP		16
+#define SW64_KVM_EXIT_RESTART		17
+#define SW64_KVM_EXIT_APT_FAULT		18
+#define SW64_KVM_EXIT_FATAL_ERROR	22
+#define SW64_KVM_EXIT_MEMHOTPLUG	23
+#define SW64_KVM_EXIT_DEBUG		24
+
+
+#define kvm_sw64_exception_type	\
+	{0, "HOST_INTR" },	\
+	{1, "IO" },		\
+	{10, "HALT" },		\
+	{12, "SHUTDOWN" },	\
+	{13, "TIMER" },		\
+	{14, "IPI" },		\
+	{16, "STOP" },		\
+	{17, "RESTART" },	\
+	{18, "APT_FAULT" },	\
+	{22, "FATAL_ERROR" },	\
+	{23, "MEMHOTPLUG" },	\
+	{24, "DEBUG" }
+
+
+#include <asm/csr.h>
+
+#endif /* _ASM_SW64_KVM_ASM_H */

arch/sw_64/include/asm/kvm_cma.h
@@ -1,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_SW64_KVM_CMA_H
+#define _ASM_SW64_KVM_CMA_H
+
+#include <linux/cma.h>
+
+extern int __init kvm_cma_declare_contiguous(phys_addr_t base,
+			phys_addr_t size, phys_addr_t limit,
+			phys_addr_t alignment, unsigned int order_per_bit,
+			const char *name, struct cma **res_cma);
+#endif /* _ASM_SW64_KVM_CMA_H */

arch/sw_64/include/asm/kvm_emulate.h
@@ -1,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_SW64_KVM_EMULATE_H
+#define _ASM_SW64_KVM_EMULATE_H
+
+#include <linux/kvm_host.h>
+#include <asm/kvm_asm.h>
+
+#define R(x)	((size_t) &((struct kvm_regs *)0)->x)
+
+static int reg_offsets[32] = {
+	R(r0), R(r1), R(r2), R(r3), R(r4), R(r5), R(r6), R(r7), R(r8),
+	R(r9), R(r10), R(r11), R(r12), R(r13), R(r14), R(r15),
+	R(r16), R(r17), R(r18),
+	R(r19), R(r20), R(r21), R(r22), R(r23), R(r24), R(r25), R(r26),
+	R(r27), R(r28), R(gp),
+	0, 0,
+};
+
+
+static inline void vcpu_set_reg(struct kvm_vcpu *vcpu, u8 reg_num,
+				unsigned long val)
+{
+	void *regs_ptr = (void *)&vcpu->arch.regs;
+
+	regs_ptr += reg_offsets[reg_num];
+	*(unsigned long *)regs_ptr = val;
+}
+
+static inline unsigned long vcpu_get_reg(struct kvm_vcpu *vcpu, u8 reg_num)
+{
+	void *regs_ptr = (void *)&vcpu->arch.regs;
+
+	if (reg_num == 31)
+		return 0;
+	regs_ptr += reg_offsets[reg_num];
+	return *(unsigned long *)regs_ptr;
+}
+
+void sw64_decode(struct kvm_vcpu *vcpu, unsigned int insn,
+		 struct kvm_run *run);
+
+unsigned int interrupt_pending(struct kvm_vcpu *vcpu, bool *more);
+void clear_vcpu_irq(struct kvm_vcpu *vcpu);
+void inject_vcpu_irq(struct kvm_vcpu *vcpu, unsigned int irq);
+void try_deliver_interrupt(struct kvm_vcpu *vcpu, unsigned int irq, bool more);
+#endif /* _ASM_SW64_KVM_EMULATE_H */

arch/sw_64/include/asm/kvm_host.h
@@ -1,0 +1,225 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_SW64_KVM_HOST_H
+#define _ASM_SW64_KVM_HOST_H
+
+#include <linux/types.h>
+#include <linux/hardirq.h>
+#include <linux/list.h>
+#include <linux/mutex.h>
+#include <linux/spinlock.h>
+#include <linux/signal.h>
+#include <linux/sched.h>
+#include <linux/bug.h>
+#include <linux/mm.h>
+#include <linux/mmu_notifier.h>
+#include <linux/preempt.h>
+#include <linux/msi.h>
+#include <linux/slab.h>
+#include <linux/rcupdate.h>
+#include <linux/ratelimit.h>
+#include <linux/err.h>
+#include <linux/bitmap.h>
+#include <linux/compiler.h>
+#include <asm/signal.h>
+#include <asm/vcpu.h>
+
+#include <generated/autoconf.h>
+#include <asm/ptrace.h>
+
+#include <asm/kvm_mmio.h>
+
+#define last_vpn(cpu)	(cpu_data[cpu].last_vpn)
+
+#ifdef CONFIG_SUBARCH_C3B
+#define VPN_BITS	8
+#define GUEST_RESET_PC          0xffffffff80011100
+#endif
+
+#ifdef CONFIG_SUBARCH_C4
+#define VPN_BITS	10
+#define GUEST_RESET_PC          0xfff0000000011002
+#endif
+
+#define VPN_FIRST_VERSION	(1UL << VPN_BITS)
+#define VPN_MASK		((1UL << VPN_BITS) - 1)
+#define VPN_SHIFT		(64 - VPN_BITS)
+
+#define KVM_MAX_VCPUS 64
+#define KVM_INTERNAL_MEM_SLOTS	(KVM_MEM_SLOTS_NUM - 512)
+
+#define KVM_HALT_POLL_NS_DEFAULT 0
+#define KVM_IRQCHIP_NUM_PINS     256
+/* KVM Hugepage definitions for sw64 */
+#define KVM_NR_PAGE_SIZES   3
+#define KVM_HPAGE_GFN_SHIFT(x)  (((x) - 1) * 9)
+#define KVM_HPAGE_SHIFT(x)  (PAGE_SHIFT + KVM_HPAGE_GFN_SHIFT(x))
+#define KVM_HPAGE_SIZE(x)   (1UL << KVM_HPAGE_SHIFT(x))
+#define KVM_HPAGE_MASK(x)   (~(KVM_HPAGE_SIZE(x) - 1))
+#define KVM_PAGES_PER_HPAGE(x)  (KVM_HPAGE_SIZE(x) / PAGE_SIZE)
+
+/*
+ * The architecture supports 48-bit GPA as input to the addtional stage translations.
+ */
+#define KVM_PHYS_SHIFT	(48)
+#define KVM_PHYS_SIZE	(_AC(1, ULL) << KVM_PHYS_SHIFT)
+#define KVM_PHYS_MASK	(KVM_PHYS_SIZE - _AC(1, ULL))
+
+struct kvm_arch_memory_slot {
+	unsigned long host_phys_addr;
+	bool valid;
+};
+
+struct kvm_arch {
+	unsigned long host_phys_addr;
+	unsigned long size;
+
+	/* segment table */
+	unsigned long *seg_pgd;
+
+	struct swvm_mem mem;
+	/* Addtional stage page table*/
+	pgd_t *pgd;
+};
+
+#define KVM_NR_MEM_OBJS		40
+
+/*
+ * We don't want allocation failures within the mmu code, so we preallocate
+ * enough memory for a single page fault in a cache.
+ */
+struct kvm_mmu_memory_cache {
+	int nobjs;
+	void *objects[KVM_NR_MEM_OBJS];
+};
+
+struct kvm_vcpu_arch {
+	struct kvm_regs regs __aligned(32);
+	struct vcpucb vcb;
+	struct task_struct *tsk;
+	unsigned int pcpu_id; /* current running pcpu id */
+
+	/* Virtual clock device */
+	struct hrtimer hrt;
+	unsigned long timer_next_event;
+	unsigned long vtimer_freq;
+
+	int first_run;
+	int halted;
+	int stopped;
+	int restart;
+
+	/* Pending virtual interrupts */
+	DECLARE_BITMAP(irqs_pending, SWVM_IRQS);
+	unsigned long vpnc[NR_CPUS];
+
+	/* Detect first run of a vcpu */
+	bool has_run_once;
+
+	/* WAIT executed */
+	int wait;
+
+	/* vcpu power-off state */
+	bool power_off;
+
+	/* Don't run the guest (internal implementation need) */
+	bool pause;
+
+	struct kvm_decode mmio_decode;
+
+	/* Cache some mmu pages needed inside spinlock regions */
+	struct kvm_mmu_memory_cache mmu_page_cache;
+
+	/* guest live migration */
+	unsigned long migration_mark;
+	unsigned long shtclock;
+};
+
+struct vmem_info {
+	unsigned long start;
+	size_t size;
+	atomic_t refcnt;
+};
+
+struct kvm_vm_stat {
+	struct kvm_vm_stat_generic generic;
+};
+
+struct kvm_vcpu_stat {
+	struct kvm_vcpu_stat_generic generic;
+	u64 pid;
+	u64 exits;
+	u64 io_exits;
+	u64 mmio_exits;
+	u64 migration_set_dirty;
+	u64 shutdown_exits;
+	u64 restart_exits;
+	u64 stop_exits;
+	u64 ipi_exits;
+	u64 timer_exits;
+	u64 debug_exits;
+#ifdef CONFIG_KVM_MEMHOTPLUG
+	u64 memhotplug_exits;
+#endif
+	u64 fatal_error_exits;
+	u64 halt_exits;
+	u64 halt_successful_poll;
+	u64 halt_attempted_poll;
+	u64 halt_wakeup;
+	u64 halt_poll_success_ns;
+	u64 halt_poll_fail_ns;
+	u64 halt_poll_invalid;
+	u64 signal_exits;
+	u64 steal;
+	u64 st_max;
+	u64 utime;
+	u64 stime;
+	u64 gtime;
+};
+
+#ifdef CONFIG_KVM_MEMHOTPLUG
+void vcpu_mem_hotplug(struct kvm_vcpu *vcpu, unsigned long start_addr);
+#endif
+#ifdef CONFIG_SUBARCH_C4
+#define KVM_ARCH_WANT_MMU_NOTIFIER
+#endif
+int kvm_set_spte_hva(struct kvm *kvm, unsigned long hva, pte_t pte);
+int kvm_unmap_hva_range(struct kvm *kvm, unsigned long start, unsigned long end, bool blockable);
+int kvm_age_hva(struct kvm *kvm, unsigned long start, unsigned long end);
+int kvm_test_age_hva(struct kvm *kvm, unsigned long hva);
+
+void update_vcpu_stat_time(struct kvm_vcpu_stat *vcpu_stat);
+void check_vcpu_requests(struct kvm_vcpu *vcpu);
+void sw64_kvm_switch_vpn(struct kvm_vcpu *vcpu);
+int vmem_init(void);
+void vmem_exit(void);
+int __sw64_vcpu_run(unsigned long vcb_pa, struct kvm_regs *regs,
+		struct hcall_args *args);
+int handle_exit(struct kvm_vcpu *vcpu, struct kvm_run *run,
+		int exception_index, struct hcall_args *hargs);
+void vcpu_send_ipi(struct kvm_vcpu *vcpu, int target_vcpuid, int type);
+static inline void kvm_arch_hardware_disable(void) {}
+static inline void kvm_arch_sync_events(struct kvm *kvm) {}
+static inline void kvm_arch_vcpu_uninit(struct kvm_vcpu *vcpu) {}
+static inline void kvm_arch_sched_in(struct kvm_vcpu *vcpu, int cpu) {}
+static inline void kvm_arch_free_memslot(struct kvm *kvm,
+		struct kvm_memory_slot *slot) {}
+static inline void kvm_arch_memslots_updated(struct kvm *kvm, u64 gen) {}
+static inline void kvm_arch_vcpu_blocking(struct kvm_vcpu *vcpu) {}
+static inline void kvm_arch_vcpu_unblocking(struct kvm_vcpu *vcpu) {}
+static inline void kvm_arch_vcpu_block_finish(struct kvm_vcpu *vcpu) {}
+
+void kvm_arch_vcpu_free(struct kvm_vcpu *vcpu);
+
+int kvm_sw64_perf_init(void);
+int kvm_sw64_perf_teardown(void);
+void kvm_flush_tlb_all(void);
+void kvm_sw64_update_vpn(struct kvm_vcpu *vcpu, unsigned long vpn);
+int kvm_sw64_init_vm(struct kvm *kvm);
+void kvm_sw64_destroy_vm(struct kvm *kvm);
+int kvm_sw64_vcpu_reset(struct kvm_vcpu *vcpu);
+long kvm_sw64_set_vcb(struct file *filp, unsigned long arg);
+long kvm_sw64_get_vcb(struct file *filp, unsigned long arg);
+
+void update_aptp(unsigned long pgd);
+void vcpu_set_numa_affinity(struct kvm_vcpu *vcpu);
+#endif /* _ASM_SW64_KVM_HOST_H */

arch/sw_64/include/asm/kvm_mmio.h
@@ -1,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_SW64_KVM_MMIO_H
+#define _ASM_SW64_KVM_MMIO_H
+
+#include <linux/kvm_host.h>
+#include <asm/kvm_asm.h>
+
+struct kvm_decode {
+	unsigned long rt;
+	bool sign_extend;
+};
+
+int kvm_handle_mmio_return(struct kvm_vcpu *vcpu, struct kvm_run *run);
+int io_mem_abort(struct kvm_vcpu *vcpu, struct kvm_run *run,
+		 struct hcall_args *hargs);
+
+#endif /* _ASM_SW64_KVM_MMIO_H */

arch/sw_64/include/asm/kvm_mmu.h
@@ -1,0 +1,131 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_SW64_KVM_MMU_H
+#define _ASM_SW64_KVM_MMU_H
+
+#define AF_ACCESS_TYPE_SHIFT		55
+#define AF_INV_LEVEL_SHIFT		53
+#define AF_FAULT_STATUS_SHIFT		48
+
+#define AF_ACCESS_TYPE_MASK		0x3
+#define AF_INV_LEVEL_MASK		0x3
+#define AF_FAULT_STATUS_MASK		0x1f
+#define AF_ENTRY_ADDR_MASK		((0x1UL << AF_FAULT_STATUS_SHIFT) - 1)
+
+/* access type defination */
+#define AF_READ_ACCESS_TYPE		0x1
+#define AF_WRITE_ACCESS_TYPE		0x2
+#define AF_EXEC_ACCESS_TYPE		0x3
+
+/* invalid page level */
+#define AF_INV_LEVEL_1			0
+#define AF_INV_LEVEL_2			1
+#define AF_INV_LEVEL_3			2
+#define AF_INV_LEVEL_4			3
+
+/* fault status */
+#define AF_STATUS_MISCONFIG		0x1
+#define AF_STATUS_FOR			0x2
+#define AF_STATUS_FOW			0x4
+#define AF_STATUS_FOE			0x8
+#define AF_STATUS_INV			0x10
+
+#define KVM_MMU_CACHE_MIN_PAGES		2
+
+static inline void kvm_set_aptpte_readonly(pte_t *pte)
+{
+	pte_val(*pte) |= _PAGE_FOW;
+}
+
+static inline bool kvm_aptpte_readonly(pte_t *pte)
+{
+	return (pte_val(*pte) & _PAGE_FOW) == _PAGE_FOW;
+}
+
+static inline void kvm_set_aptpmd_readonly(pmd_t *pmd)
+{
+	pmd_val(*pmd) |= _PAGE_FOW;
+}
+
+static inline bool kvm_aptpmd_readonly(pmd_t *pmd)
+{
+	return (pmd_val(*pmd) & _PAGE_FOW) == _PAGE_FOW;
+}
+
+static inline void kvm_set_aptpud_readonly(pud_t *pud)
+{
+	pud_val(*pud) |= _PAGE_FOW;
+}
+
+static inline bool kvm_aptpud_readonly(pud_t *pud)
+{
+	return (pud_val(*pud) & _PAGE_FOW) == _PAGE_FOW;
+}
+
+static inline pte_t kvm_pte_mkwrite(pte_t pte)
+{
+	pte_val(pte) &= ~_PAGE_FOW;
+	return pte;
+}
+
+static inline pte_t kvm_pte_mkexec(pte_t pte)
+{
+	pte_val(pte) &= ~_PAGE_FOE;
+	return pte;
+}
+
+static inline bool kvm_pte_exec(pte_t *pte)
+{
+	return !(pte_val(*pte) & _PAGE_FOE);
+}
+
+static inline pmd_t kvm_pmd_mkwrite(pmd_t pmd)
+{
+	pmd_val(pmd) &= ~_PAGE_FOW;
+	return pmd;
+}
+
+static inline pmd_t kvm_pmd_mkexec(pmd_t pmd)
+{
+	pmd_val(pmd) &= ~_PAGE_FOE;
+	return pmd;
+}
+
+static inline bool kvm_pmd_exec(pmd_t *pmd)
+{
+	return !(pmd_val(*pmd) & _PAGE_FOE);
+}
+
+static inline pud_t kvm_pud_mkwrite(pud_t pud)
+{
+	pud_val(pud) &= ~_PAGE_FOW;
+	return pud;
+}
+
+static inline pud_t kvm_pud_mkexec(pud_t pud)
+{
+	pud_val(pud) &= ~_PAGE_FOE;
+	return pud;
+}
+
+static inline bool kvm_pud_exec(pud_t *pud)
+{
+	return !(pud_val(*pud) & _PAGE_FOE);
+}
+
+void kvm_core4_commit_memory_region(struct kvm *kvm,
+		const struct kvm_userspace_memory_region *mem,
+		const struct kvm_memory_slot *old,
+		const struct kvm_memory_slot *new,
+		enum kvm_mr_change change);
+void kvm_core4_flush_shadow_memslot(struct kvm *kvm,
+		struct kvm_memory_slot *slot);
+void kvm_core4_flush_shadow_all(struct kvm *kvm);
+void kvm_mmu_free_memory_caches(struct kvm_vcpu *vcpu);
+int kvm_age_hva(struct kvm *kvm, unsigned long start, unsigned long end);
+void kvm_handle_apt_fault(struct kvm_vcpu *vcpu);
+int kvm_alloc_addtional_stage_pgd(struct kvm *kvm);
+void kvm_arch_flush_shadow_memslot(struct kvm *kvm,
+		struct kvm_memory_slot *slot);
+int kvm_handle_guest_abort(struct kvm_vcpu *vcpu, struct kvm_run *run);
+void apt_unmap_vm(struct kvm *kvm);
+#endif /* _ASM_SW64_KVM_MMU_H */

OERV-RVCI/rvck-olk

commit 9d26edf4ce88ecff0f56acdd41c53abadbccb0fe

sw64: kvm: fix incorrect vm_flags in vmem_mmap()
Sunway inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IC9DUA

--------------------------------

When adding KVM support for CORE3B, vm_flags_init() was mistakenly
called, causing the vm_flags of related shared memory mapping to
be corrupted, and the write permission for this mapping is cleared.

In commit 5923165afc62 ("sw64: fix mmap protection_map"), FOW bit is
set for shared writable page to timely mark dirty pages. Subsequently,
a write fault is triggered while writing to the abovementioned shared
writable memory. Due to the incorrect vm_flags, it can't be handled
properly, and segment fault is thrown eventually.

This patch fixes vm_flags with vm_flags_set() which performs an OR
operation.

Fixes: 09a48172fb73 ("sw64: add KVM support")
Signed-off-by: Chen Wang <chenwang@wxiat.com>
Reviewed-by: He Sheng <hesheng@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>

arch/sw_64/kvm/vmem.c
@@ -41,7 +41,7 @@ static int vmem_vm_insert_page(struct vm_area_struct *vma)
 	size = info->size;
 	uaddr = vma->vm_start;
 
-	vm_flags_init(vma, VM_DONTEXPAND | VM_DONTDUMP | VM_MIXEDMAP);
+	vm_flags_set(vma, VM_DONTEXPAND | VM_DONTDUMP | VM_MIXEDMAP);
 	vmem_page = pfn_to_page(addr >> PAGE_SHIFT);
 	do {
 		ret = vm_insert_page(vma, uaddr, vmem_page);