OERV-RVCI/rvck-olk

commit 645a1ba256efa1b6dd765d20b7b407b501ed3ba6

sched: topology: Build soft domain for LLC
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/IC8X6H

--------------------------------

On Kunpeng server, each LLC domain contains multiple clusters. When
multiple services are deployed within the same LLC domain, their tasks
become distributed across all clusters. This results in:

1. High cache synchronization overheadbetween different tasks of the
 same service.
2. Severe cache contention among tasks from different services.

The Soft Domain architecture partitions resources by clusters. Under
low-load conditions, each service operates exclusively within its
dedicated domain to prevent cross-service interference, thereby
enhancing both CPU isolation and improving cache locality.

Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>

kernel/sched/core.c
@@ -9972,6 +9972,7 @@ void __init sched_init_smp(void)
 	sched_smp_initialized = true;
 
 	sched_grid_zone_init();
+	build_soft_domain();
 
 #ifdef CONFIG_QOS_SCHED_SMART_GRID
 	init_auto_affinity(&root_task_group);

kernel/sched/soft_domain.c
@@ -1,0 +1,113 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Common code for Soft Domain Scheduling
+ *
+ * Copyright (C) 2025-2025 Huawei Technologies Co., Ltd
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+
+static DEFINE_PER_CPU(struct soft_domain *, g_sf_d);
+
+static void free_sub_soft_domain(struct soft_domain *sf_d);
+
+static int build_soft_sub_domain(struct sched_domain *sd, struct cpumask *cpus)
+{
+	struct cpumask *span = sched_domain_span(sd);
+	int nid = cpu_to_node(cpumask_first(span));
+	struct soft_domain *sf_d = NULL;
+	int i;
+
+	sf_d = kzalloc_node(sizeof(struct soft_domain) + cpumask_size(),
+			    GFP_KERNEL, nid);
+	if (!sf_d)
+		return -ENOMEM;
+
+	INIT_LIST_HEAD(&sf_d->child_domain);
+	sf_d->nr_available_cpus = cpumask_weight(span);
+	cpumask_copy(to_cpumask(sf_d->span), span);
+
+	for_each_cpu_and(i, sched_domain_span(sd), cpus) {
+		struct soft_subdomain *sub_d = NULL;
+
+		sub_d = kzalloc_node(sizeof(struct soft_subdomain) + cpumask_size(),
+				     GFP_KERNEL, nid);
+		if (!sub_d) {
+			free_sub_soft_domain(sf_d);
+			return -ENOMEM;
+		}
+
+		list_add_tail(&sub_d->node, &sf_d->child_domain);
+		cpumask_copy(soft_domain_span(sub_d->span), cpu_clustergroup_mask(i));
+		cpumask_andnot(cpus, cpus, cpu_clustergroup_mask(i));
+	}
+
+	for_each_cpu(i, sched_domain_span(sd)) {
+		rcu_assign_pointer(per_cpu(g_sf_d, i), sf_d);
+	}
+
+	return 0;
+}
+
+static void free_sub_soft_domain(struct soft_domain *sf_d)
+{
+	struct list_head *children = &sf_d->child_domain;
+	struct soft_subdomain *entry = NULL, *next = NULL;
+	int i;
+
+	list_for_each_entry_safe(entry, next, children, node) {
+		list_del(&entry->node);
+		kfree(entry);
+	}
+
+	for_each_cpu(i, to_cpumask(sf_d->span)) {
+		rcu_assign_pointer(per_cpu(g_sf_d, i), NULL);
+	}
+
+	kfree(sf_d);
+}
+
+static void free_soft_domain(void)
+{
+	struct soft_domain *sf_d = NULL;
+	int i;
+
+	for_each_cpu(i, cpu_active_mask) {
+		sf_d = rcu_dereference(per_cpu(g_sf_d, i));
+		if (sf_d)
+			free_sub_soft_domain(sf_d);
+	}
+}
+
+void build_soft_domain(void)
+{
+	struct sched_domain *sd;
+	static struct cpumask cpus;
+	int i, ret;
+
+	cpumask_copy(&cpus, cpu_active_mask);
+	rcu_read_lock();
+	for_each_cpu(i, &cpus) {
+		/* build soft domain for each llc domain. */
+		sd = rcu_dereference(per_cpu(sd_llc, i));
+		if (sd) {
+			ret = build_soft_sub_domain(sd, &cpus);
+			if (ret) {
+				free_soft_domain();
+				goto out;
+			}
+		}
+	}
+
+out:
+	rcu_read_unlock();
+}

include/linux/sched/topology.h
@@ -77,6 +77,27 @@ extern int sched_domain_level_max;
 
 struct sched_group;
 
+#ifdef CONFIG_SCHED_SOFT_DOMAIN
+
+struct soft_subdomain {
+	/* the count of task group attached this sub domain. */
+	int			attached;
+	struct list_head	node;
+	unsigned long		span[];
+};
+
+/*
+ * Each LLC builds a soft domain:
+ * A soft scheduling domain is divided into multiple subdomains,
+ * typically based on the physical structure of CPU clusters.
+ */
+struct soft_domain {
+	struct list_head	child_domain;
+	int			nr_available_cpus;
+	unsigned long		span[];
+};
+#endif
+
 struct sched_domain_shared {
 	atomic_t	ref;
 	atomic_t	nr_busy_cpus;

init/Kconfig
@@ -1148,6 +1148,18 @@ config QOS_SCHED_DYNAMIC_AFFINITY
 	 of taskgroup is below threshold setted, otherwise make taskgroup to use
 	 cpus allowed.
 
+config SCHED_SOFT_DOMAIN
+	bool "Soft domain scheduler"
+	depends on FAIR_GROUP_SCHED
+	depends on SCHED_CLUSTER
+	default n
+	help
+	  This feature builds a CPU soft domain for each task group. Tasks are
+	  prioritized and aggregated to execute within soft domains, which optimizes
+	  resource allocation and enhances cache locality.
+
+	  If in doubt, say N.
+
 config SCHED_MM_CID
 	def_bool n
 	depends on SMP && RSEQ

kernel/sched/build_policy.c
@@ -52,3 +52,6 @@
 #include "cputime.c"
 #include "deadline.c"
 
+#ifdef CONFIG_SCHED_SOFT_DOMAIN
+#include "soft_domain.c"
+#endif

kernel/sched/sched.h
@@ -3743,4 +3743,16 @@ extern int entity_eligible(struct cfs_rq *cfs_rq, struct sched_entity *se);
 bool bpf_sched_is_cpu_allowed(struct task_struct *p, int cpu);
 #endif
 
+#ifdef CONFIG_SCHED_SOFT_DOMAIN
+void build_soft_domain(void);
+static inline struct cpumask *soft_domain_span(unsigned long span[])
+{
+	return to_cpumask(span);
+}
+#else
+
+static inline void build_soft_domain(void) { }
+
+#endif
+
 #endif /* _KERNEL_SCHED_SCHED_H */

commit 3fbcef632472fd566c42df235e62ec5ab844c02e

sched: Add cmdline sched_soft_domain switch for soft domain feature
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICB7K1

--------------------------------

Add a command-line "sched_soft_domain" switch for the soft domain
feature; this switch is enabled by default.

Fixes: 645a1ba256ef ("sched: topology: Build soft domain for LLC")
Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>

kernel/sched/core.c
@@ -11753,6 +11753,9 @@ static s64 cpu_soft_domain_read_s64(struct cgroup_subsys_state *css,
 {
 	struct task_group *tg = css_tg(css);
 
+	if (!tg->sf_ctx)
+		return 0;
+
 	return (s64)tg->sf_ctx->policy;
 }
 
@@ -11777,12 +11780,18 @@ static u64 cpu_soft_domain_quota_read_u64(struct cgroup_subsys_state *css,
 {
 	struct task_group *tg = css_tg(css);
 
+	if (!tg->sf_ctx)
+		return 0;
+
 	return (u64)tg->sf_ctx->nr_cpus;
 }
 
 static int soft_domain_cpu_list_seq_show(struct seq_file *sf, void *v)
 {
 	struct task_group *tg = css_tg(seq_css(sf));
+
+	if (!tg->sf_ctx)
+		return 0;
 
 	seq_printf(sf, "%*pbl\n", cpumask_pr_args(to_cpumask(tg->sf_ctx->span)));
 

kernel/sched/soft_domain.c
@@ -17,6 +17,30 @@
 
 #include <linux/sort.h>
 
+static DEFINE_STATIC_KEY_TRUE(__soft_domain_switch);
+
+static int __init soft_domain_switch_setup(char *str)
+{
+	int val = 0;
+
+	if (kstrtoint(str, 0, &val))
+		pr_warn("sched_soft_domain parameter is error: %s\n", str);
+	else {
+		if (val == 1)
+			static_branch_enable(&__soft_domain_switch);
+		else if (val == 0)
+			static_branch_disable(&__soft_domain_switch);
+	}
+
+	return 1;
+}
+__setup("sched_soft_domain=", soft_domain_switch_setup);
+
+static bool soft_domain_enabled(void)
+{
+	return static_branch_likely(&__soft_domain_switch);
+}
+
 static DEFINE_PER_CPU(struct soft_domain *, g_sf_d);
 
 static void free_sub_soft_domain(struct soft_domain *sf_d);
@@ -87,6 +111,8 @@ static void free_soft_domain(void)
 		if (sf_d)
 			free_sub_soft_domain(sf_d);
 	}
+
+	static_branch_disable(&__soft_domain_switch);
 }
 
 void build_soft_domain(void)
@@ -94,6 +120,9 @@ void build_soft_domain(void)
 	struct sched_domain *sd;
 	static struct cpumask cpus;
 	int i, ret;
+
+	if (!soft_domain_enabled())
+		return;
 
 	cpumask_copy(&cpus, cpu_active_mask);
 	rcu_read_lock();
@@ -348,6 +377,9 @@ static int __sched_group_unset_soft_domain(struct task_group *tg)
 int sched_group_set_soft_domain(struct task_group *tg, long val)
 {
 	int ret = 0;
+
+	if (!soft_domain_enabled())
+		return -EPERM;
 
 	if (val < -1 || val > nr_node_ids)
 		return -EINVAL;