OERV-RVCI/rvck-olk

commit 2bd01fbe4c473148bb3ecb1fbceeaf61500a795d

sched: fair: Select idle cpu in soft domain
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/IC8X6H

--------------------------------

Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>

kernel/sched/fair.c
@@ -8226,6 +8226,40 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, bool 
 		}
 	}
 
+#ifdef CONFIG_SCHED_SOFT_DOMAIN
+	if (sched_feat(SOFT_DOMAIN)) {
+		struct task_group *tg = task_group(p);
+
+		if (tg->sf_ctx && tg->sf_ctx->policy != 0) {
+			struct cpumask *tmpmask = to_cpumask(tg->sf_ctx->span);
+
+			for_each_cpu_wrap(cpu, tmpmask, target + 1) {
+				if (!cpumask_test_cpu(cpu, tmpmask))
+					continue;
+
+				if (has_idle_core) {
+					i = select_idle_core(p, cpu, cpus, &idle_cpu);
+					if ((unsigned int)i < nr_cpumask_bits)
+						return i;
+
+				} else {
+					if (--nr <= 0)
+						return -1;
+					idle_cpu = __select_idle_cpu(cpu, p);
+					if ((unsigned int)idle_cpu < nr_cpumask_bits)
+						return idle_cpu;
+				}
+			}
+
+			if (idle_cpu != -1)
+				return idle_cpu;
+
+			cpumask_andnot(cpus, cpus, tmpmask);
+		}
+
+	}
+#endif
+
 	if (static_branch_unlikely(&sched_cluster_active)) {
 		struct sched_group *sg = sd->groups;
 
@@ -9136,6 +9170,30 @@ static void set_task_select_cpus(struct task_struct *p, int *idlest_cpu,
 }
 #endif
 
+#ifdef CONFIG_SCHED_SOFT_DOMAIN
+static int wake_soft_domain(struct task_struct *p, int target)
+{
+	struct cpumask *mask = NULL;
+	struct soft_domain_ctx *ctx = NULL;
+
+	rcu_read_lock();
+	ctx = task_group(p)->sf_ctx;
+	if (!ctx || ctx->policy == 0)
+		goto unlock;
+
+	mask = to_cpumask(ctx->span);
+	if (cpumask_test_cpu(target, mask))
+		goto unlock;
+	else
+		target = cpumask_any_distribute(mask);
+
+unlock:
+	rcu_read_unlock();
+
+	return target;
+}
+#endif
+
 /*
  * select_task_rq_fair: Select target runqueue for the waking task in domains
  * that have the relevant SD flag set. In practice, this is SD_BALANCE_WAKE,
@@ -9189,6 +9247,11 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int wake_flags)
 				return new_cpu;
 			new_cpu = prev_cpu;
 		}
+
+#ifdef CONFIG_SCHED_SOFT_DOMAIN
+		if (sched_feat(SOFT_DOMAIN))
+			new_cpu = prev_cpu = wake_soft_domain(p, prev_cpu);
+#endif
 
 #ifdef CONFIG_QOS_SCHED_DYNAMIC_AFFINITY
 		want_affine = !wake_wide(p) && cpumask_test_cpu(cpu, p->select_cpus);

kernel/sched/features.h
@@ -105,3 +105,7 @@ SCHED_FEAT(HZ_BW, true)
  */
 SCHED_FEAT(DA_UTIL_TASKGROUP, true)
 #endif
+
+#ifdef CONFIG_SCHED_SOFT_DOMAIN
+SCHED_FEAT(SOFT_DOMAIN, false)
+#endif

commit 80bb22b8b06f737a32b19f0ff7eebaf57bb00991

sched: Consider task affinity in wake_soft_domain()
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICB7K1

--------------------------------

For a soft_domain task, the CPU selection should be the intersection of
the soft domain, cpu_active_mask, and task affinity.

Fixes: 2bd01fbe4c47 ("sched: fair: Select idle cpu in soft domain")
Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>

kernel/sched/fair.c
@@ -9176,22 +9176,25 @@ static void set_task_select_cpus(struct task_struct *p, int *idlest_cpu,
 #ifdef CONFIG_SCHED_SOFT_DOMAIN
 static int wake_soft_domain(struct task_struct *p, int target)
 {
-	struct cpumask *mask = NULL;
+	struct cpumask *mask = this_cpu_cpumask_var_ptr(select_rq_mask);
 	struct soft_domain_ctx *ctx = NULL;
 
-	rcu_read_lock();
 	ctx = task_group(p)->sf_ctx;
 	if (!ctx || ctx->policy == 0)
-		goto unlock;
+		goto out;
 
-	mask = to_cpumask(ctx->span);
-	if (cpumask_test_cpu(target, mask))
-		goto unlock;
+#ifdef CONFIG_QOS_SCHED_DYNAMIC_AFFINITY
+	cpumask_and(mask, to_cpumask(ctx->span), p->select_cpus);
+#else
+	cpumask_and(mask, to_cpumask(ctx->span), p->cpus_ptr);
+#endif
+	cpumask_and(mask, mask, cpu_active_mask);
+	if (cpumask_empty(mask) || cpumask_test_cpu(target, mask))
+		goto out;
 	else
 		target = cpumask_any_distribute(mask);
 
-unlock:
-	rcu_read_unlock();
+out:
 
 	return target;
 }
@@ -9251,11 +9254,6 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int wake_flags)
 			new_cpu = prev_cpu;
 		}
 
-#ifdef CONFIG_SCHED_SOFT_DOMAIN
-		if (sched_feat(SOFT_DOMAIN))
-			new_cpu = prev_cpu = wake_soft_domain(p, prev_cpu);
-#endif
-
 #ifdef CONFIG_QOS_SCHED_DYNAMIC_AFFINITY
 		want_affine = !wake_wide(p) && cpumask_test_cpu(cpu, p->select_cpus);
 #else
@@ -9264,6 +9262,11 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int wake_flags)
 	}
 
 	rcu_read_lock();
+
+#ifdef CONFIG_SCHED_SOFT_DOMAIN
+	if (sched_feat(SOFT_DOMAIN))
+		new_cpu = prev_cpu = wake_soft_domain(p, prev_cpu);
+#endif
 #ifdef CONFIG_BPF_SCHED
 	if (bpf_sched_enabled()) {
 		ctx.task = p;