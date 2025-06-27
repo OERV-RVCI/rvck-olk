ae72360a5e6b462d14e5ea7befcbedce629ff2c3
闫海涛 <yanhaitao2@huawei.com>
2025-06-27 10:14:11 +0800

    fix review issue
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICH2DG
    
    --------------------------------
    
    fix review issue:
    delete unused code
    
    Signed-off-by: 闫海涛 <yanhaitao2@huawei.com>

--- fs/nfs/enfs/enfs_multipath.c
+++ fs/nfs/enfs/enfs_multipath.c
@@ -1003,22 +1003,11 @@ static void enfs_create_xprt_ctx(struct rpc_xprt *xprt)
 
 static void enfs_set_transport(struct rpc_task *task, struct rpc_clnt *clnt)
 {
-#ifdef ENFS_SUPPORT_NFSV4
-	if (enfs_is_singularr_route(clnt) && clnt->cl_vers == 4) {
-		failover_reselect_transport(task, clnt);
-		return;
-	}
-
-	if (enfs_is_rr_route(clnt) && clnt->cl_vers == 3) {
-		shard_set_transport(task, clnt);
-	}
-#else
 	if (clnt->cl_vers == 4)
 		return;
 
 	if (enfs_is_rr_route(clnt))
 		shard_set_transport(task, clnt);
-#endif
 }
 
 static void enfs_inc_queuelen(struct rpc_xprt *xprt)
