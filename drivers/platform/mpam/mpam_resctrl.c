OERV-RVCI/rvck-olk

commit 556688623b2b867f4e766229a1593f1d0820be34

fs/resctrl: Create l2 cache monitors
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IC30P1

--------------------------------

Support MPAM to create *mon_L2_${cache_id}/l2c_occupancy* cache occupancy
monitors under mon_data directory. In the other hand, RDT doesn't support
L2 monitor feature.

In addition, let MPAM L2 cache msc support memory bandwidth monitor
feature. L2 cache msc not only has the MSMON_CSU monitor, but also has the
MSMON_MBWU monitor. Now provide *mon_L2_${cache_id}/mbm_core_bytes*
interface to display the value of l2c MSMON_MBWU counter.

Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -36,6 +36,7 @@ static bool exposed_alloc_capable;
 static bool exposed_mon_capable;
 static struct mpam_class *mbm_local_class;
 static struct mpam_class *mbm_total_class;
+static struct mpam_class *mbm_core_class;
 
 /*
  * MPAM emulates CDP by setting different PARTID in the I/D fields of MPAM1_EL1.
@@ -77,6 +78,11 @@ bool resctrl_arch_is_mbm_local_enabled(void)
 bool resctrl_arch_is_mbm_total_enabled(void)
 {
 	return mbm_total_class;
+}
+
+bool resctrl_arch_is_mbm_core_enabled(void)
+{
+	return mbm_core_class;
 }
 
 bool resctrl_arch_get_cdp_enabled(enum resctrl_res_level rid)
@@ -307,6 +313,8 @@ static void *resctrl_arch_mon_ctx_alloc_no_wait(struct rdt_resource *r,
 	case QOS_L3_OCCUP_EVENT_ID:
 	case QOS_L3_MBM_LOCAL_EVENT_ID:
 	case QOS_L3_MBM_TOTAL_EVENT_ID:
+	case QOS_L2_OCCUP_EVENT_ID:
+	case QOS_L2_MBM_CORE_EVENT_ID:
 		*ret = __mon_is_rmid_idx;
 		return ret;
 
@@ -372,10 +380,12 @@ int resctrl_arch_rmid_read(struct rdt_resource	*r, struct rdt_domain *d,
 
 	switch (eventid) {
 	case QOS_L3_OCCUP_EVENT_ID:
+	case QOS_L2_OCCUP_EVENT_ID:
 		type = mpam_feat_msmon_csu;
 		break;
 	case QOS_L3_MBM_LOCAL_EVENT_ID:
 	case QOS_L3_MBM_TOTAL_EVENT_ID:
+	case QOS_L2_MBM_CORE_EVENT_ID:
 		type = mpam_feat_msmon_mbwu;
 		break;
 	default:
@@ -522,6 +532,11 @@ static bool cache_has_usable_csu(struct mpam_class *class)
 bool resctrl_arch_is_llc_occupancy_enabled(void)
 {
 	return cache_has_usable_csu(mpam_resctrl_exports[RDT_RESOURCE_L3].class);
+}
+
+bool resctrl_arch_is_l2c_occupancy_enabled(void)
+{
+	return cache_has_usable_csu(mpam_resctrl_exports[RDT_RESOURCE_L2].class);
 }
 
 static bool class_has_usable_mbwu(struct mpam_class *class)
@@ -918,19 +933,23 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		 * be local. If it's on the memory controller, its assumed to
 		 * be global.
 		 */
-		if (has_mbwu && class->level >= 3) {
-			mbm_local_class = class;
-			r->mon_capable = true;
+		if (has_mbwu) {
+			if (class->level == 3) {
+				mbm_local_class = class;
+				r->mon_capable = true;
+
+			} else if (class->level == 2) {
+				mbm_core_class = class;
+				r->mon_capable = true;
+			}
 		}
 
 		/*
 		 * CSU counters only make sense on a cache. The file is called
 		 * llc_occupancy, but its expected to the on the L3.
 		 */
-		if (has_csu && class->type == MPAM_CLASS_CACHE &&
-		    class->level == 3) {
+		if (has_csu && class->type == MPAM_CLASS_CACHE)
 			r->mon_capable = true;
-		}
 		break;
 
 	case RDT_RESOURCE_MBA:
@@ -1502,6 +1521,11 @@ static struct mon_evt llc_occupancy_event = {
 	.evtid		= QOS_L3_OCCUP_EVENT_ID,
 };
 
+static struct mon_evt l2c_occupancy_event = {
+	.name		= "l2c_occupancy",
+	.evtid		= QOS_L2_OCCUP_EVENT_ID,
+};
+
 static struct mon_evt mbm_total_event = {
 	.name		= "mbm_total_bytes",
 	.evtid		= QOS_L3_MBM_TOTAL_EVENT_ID,
@@ -1510,6 +1534,11 @@ static struct mon_evt mbm_total_event = {
 static struct mon_evt mbm_local_event = {
 	.name		= "mbm_local_bytes",
 	.evtid		= QOS_L3_MBM_LOCAL_EVENT_ID,
+};
+
+static struct mon_evt mbm_core_event = {
+	.name		= "mbm_core_bytes",
+	.evtid		= QOS_L2_MBM_CORE_EVENT_ID,
 };
 
 /*
@@ -1534,6 +1563,14 @@ static void l3_mon_evt_init(struct rdt_resource *r)
 			list_add_tail(&mbm_local_event.list, &r->evt_list);
 	}
 
+	if (r->rid == RDT_RESOURCE_L2) {
+		if (resctrl_arch_is_l2c_occupancy_enabled())
+			list_add_tail(&l2c_occupancy_event.list, &r->evt_list);
+
+		if (resctrl_arch_is_mbm_core_enabled())
+			list_add_tail(&mbm_core_event.list, &r->evt_list);
+	}
+
 	if ((r->rid == RDT_RESOURCE_MBA) &&
 	     resctrl_arch_is_mbm_total_enabled())
 		list_add_tail(&mbm_total_event.list, &r->evt_list);
@@ -1542,6 +1579,7 @@ static void l3_mon_evt_init(struct rdt_resource *r)
 int resctrl_arch_mon_resource_init(void)
 {
 	l3_mon_evt_init(resctrl_arch_get_resource(RDT_RESOURCE_L3));
+	l3_mon_evt_init(resctrl_arch_get_resource(RDT_RESOURCE_L2));
 	l3_mon_evt_init(resctrl_arch_get_resource(RDT_RESOURCE_MBA));
 
 	if (resctrl_arch_is_evt_configurable(QOS_L3_MBM_TOTAL_EVENT_ID)) {

arch/x86/include/asm/resctrl.h
@@ -101,6 +101,8 @@ static inline bool resctrl_arch_is_mbm_local_enabled(void)
 	return (rdt_mon_features & (1 << QOS_L3_MBM_LOCAL_EVENT_ID));
 }
 
+static inline bool resctrl_arch_is_mbm_core_enabled(void) { return false; }
+
 /*
  * __resctrl_sched_in() - Writes the task's CLOSid/RMID to IA32_PQR_MSR
  *

drivers/platform/mpam/mpam_devices.c
@@ -926,6 +926,7 @@ static u64 mpam_msmon_overflow_val(struct mpam_msc_ris *ris)
 static const struct midr_range mbwu_flowrate_list[] = {
 	MIDR_ALL_VERSIONS(MIDR_HISI_TSV110),
 	MIDR_ALL_VERSIONS(MIDR_HISI_LINXICORE9100),
+	MIDR_ALL_VERSIONS(MIDR_HISI_HIP12),
 	{ /* sentinel */ }
 };
 

fs/resctrl/monitor.c
@@ -597,6 +597,20 @@ static void mbm_update(struct rdt_resource *r, struct rdt_domain *d,
 
 		resctrl_arch_mon_ctx_free(rr.r, rr.evtid, rr.arch_mon_ctx);
 	}
+	if (resctrl_arch_is_mbm_core_enabled()) {
+		rr.evtid = QOS_L2_MBM_CORE_EVENT_ID;
+		rr.val = 0;
+		rr.arch_mon_ctx = resctrl_arch_mon_ctx_alloc(rr.r, rr.evtid);
+		if (IS_ERR(rr.arch_mon_ctx)) {
+			pr_warn_ratelimited("Failed to allocate monitor context: %ld",
+					    PTR_ERR(rr.arch_mon_ctx));
+			return;
+		}
+
+		__mon_event_count(closid, rmid, &rr);
+
+		resctrl_arch_mon_ctx_free(rr.r, rr.evtid, rr.arch_mon_ctx);
+	}
 }
 
 /*

fs/resctrl/rdtgroup.c
@@ -114,13 +114,15 @@ void rdt_staged_configs_clear(void)
 static bool resctrl_is_mbm_enabled(void)
 {
 	return (resctrl_arch_is_mbm_total_enabled() ||
-		resctrl_arch_is_mbm_local_enabled());
+		resctrl_arch_is_mbm_local_enabled() ||
+		resctrl_arch_is_mbm_core_enabled());
 }
 
 static bool resctrl_is_mbm_event(int e)
 {
-	return (e >= QOS_L3_MBM_TOTAL_EVENT_ID &&
-		e <= QOS_L3_MBM_LOCAL_EVENT_ID);
+	return (e == QOS_L3_MBM_TOTAL_EVENT_ID ||
+		e == QOS_L3_MBM_LOCAL_EVENT_ID ||
+		e == QOS_L2_MBM_CORE_EVENT_ID);
 }
 
 /*
@@ -3998,6 +4000,15 @@ static int domain_setup_mon_state(struct rdt_resource *r, struct rdt_domain *d)
 		if (!d->mbm_local) {
 			bitmap_free(d->rmid_busy_llc);
 			kfree(d->mbm_total);
+			return -ENOMEM;
+		}
+	}
+	if (resctrl_arch_is_mbm_core_enabled()) {
+		tsize = sizeof(*d->mbm_core);
+		d->mbm_core = kcalloc(idx_limit, tsize, GFP_KERNEL);
+		if (!d->mbm_core) {
+			bitmap_free(d->rmid_busy_llc);
+			kfree(d->mbm_core);
 			return -ENOMEM;
 		}
 	}

include/linux/arm_mpam.h
@@ -64,6 +64,8 @@ static inline bool resctrl_arch_event_is_free_running(enum resctrl_event_id evt)
 bool resctrl_arch_alloc_capable(void);
 bool resctrl_arch_mon_capable(void);
 bool resctrl_arch_is_llc_occupancy_enabled(void);
+bool resctrl_arch_is_l2c_occupancy_enabled(void);
+bool resctrl_arch_is_mbm_core_enabled(void);
 bool resctrl_arch_is_mbm_local_enabled(void);
 bool resctrl_arch_is_mbm_total_enabled(void);
 bool resctrl_arch_would_mbm_overflow(void);

include/linux/resctrl.h
@@ -113,6 +113,7 @@ struct rdt_domain {
 	unsigned long			*rmid_busy_llc;
 	struct mbm_state		*mbm_total;
 	struct mbm_state		*mbm_local;
+	struct mbm_state		*mbm_core;
 	struct delayed_work		mbm_over;
 	struct delayed_work		cqm_limbo;
 	int				mbm_work_cpu;

include/linux/resctrl_types.h
@@ -99,11 +99,21 @@ enum resctrl_res_level {
 /*
  * Event IDs, the values match those used to program IA32_QM_EVTSEL before
  * reading IA32_QM_CTR on RDT systems.
+ *
+ * Monitor Event IDs, representative a variety of monitoring events:
+ * QOS_L3_OCCUP_EVENT_ID:     L3 Cache Occupancy statistics event
+ * QOS_L3_MBM_TOTAL_EVENT_ID: Global Memory Bandwidth statistics event
+ * QOS_L3_MBM_LOCAL_EVENT_ID: L3 Cache Bandwidth statistics event
+ * QOS_L2_OCCUP_EVENT_ID:     L2 Cache Occupancy statistics event
+ * QOS_L2_MBM_CORE_EVENT_ID:  L2 Cache Bandwidth statistics event
  */
 enum resctrl_event_id {
 	QOS_L3_OCCUP_EVENT_ID		= 0x01,
 	QOS_L3_MBM_TOTAL_EVENT_ID	= 0x02,
 	QOS_L3_MBM_LOCAL_EVENT_ID	= 0x03,
+
+	QOS_L2_OCCUP_EVENT_ID,
+	QOS_L2_MBM_CORE_EVENT_ID,
 };
 
 #endif /* __LINUX_RESCTRL_TYPES_H */

commit 33f6871f62e0e77038373b5b02085a8bfba514d9

arm64/mpam: Fix L2 monitor issue under CDP mode
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IC30P1

--------------------------------

The L2 monitor counters always reset to 0 under CDP working mode. This
occurs because the L2 monitor employs cumulative statistics, and
monitors a single partid within each statistical cycle. After completing
the configuration for the new partid monitoring, the monitor instance
starts a new monitoring cycle and resets the counter to zero.

Since MPAM implements the CDP feature by allocating two partids, switching
the monitor's target partid during a monitoring cycle falsely triggers a
"new control group" assumption, leading to erroneous counter resets.

To resolve this, use the real partid % num_mon to ensure distinct
monitor instances track different partids under the CDP control group,
preventing unintended resets.

Fixes: 556688623b2b ("fs/resctrl: Create l2 cache monitors")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -309,19 +309,8 @@ static void *resctrl_arch_mon_ctx_alloc_no_wait(struct rdt_resource *r,
 	if (!ret)
 		return ERR_PTR(-ENOMEM);
 
-	switch (evtid) {
-	case QOS_L3_OCCUP_EVENT_ID:
-	case QOS_L3_MBM_LOCAL_EVENT_ID:
-	case QOS_L3_MBM_TOTAL_EVENT_ID:
-	case QOS_L2_OCCUP_EVENT_ID:
-	case QOS_L2_MBM_CORE_EVENT_ID:
-		*ret = __mon_is_rmid_idx;
-		return ret;
-
-	default:
-		kfree(ret);
-		return ERR_PTR(-EOPNOTSUPP);
-	}
+	*ret = __mon_is_rmid_idx;
+	return ret;
 }
 
 void *resctrl_arch_mon_ctx_alloc(struct rdt_resource *r, int evtid)
@@ -371,7 +360,6 @@ int resctrl_arch_rmid_read(struct rdt_resource	*r, struct rdt_domain *d,
 	struct mon_cfg cfg;
 	struct mpam_resctrl_dom *dom;
 	struct mpam_resctrl_res *res;
-	u32 mon = *(u32 *)arch_mon_ctx;
 	enum mpam_device_features type;
 
 	resctrl_arch_rmid_read_context_check();
@@ -392,20 +380,15 @@ int resctrl_arch_rmid_read(struct rdt_resource	*r, struct rdt_domain *d,
 		return -EINVAL;
 	}
 
-	cfg.mon = mon;
-	if (cfg.mon == USE_RMID_IDX) {
-		/*
-		 * The number of mbwu monitors can't support free run mode,
-		 * adapt the remainder of rmid to the num_mon as compromise.
-		 */
-		res = container_of(r, struct mpam_resctrl_res, resctrl_res);
-		if (type == mpam_feat_msmon_mbwu)
-			num_mon = res->class->props.num_mbwu_mon;
-		else
-			num_mon = res->class->props.num_csu_mon;
-
-		cfg.mon = closid % num_mon;
-	}
+	/*
+	 * The number of mbwu monitors can't support free run mode,
+	 * adapt the remainder of rmid to the num_mon as compromise.
+	 */
+	res = container_of(r, struct mpam_resctrl_res, resctrl_res);
+	if (type == mpam_feat_msmon_mbwu)
+		num_mon = res->class->props.num_mbwu_mon;
+	else
+		num_mon = res->class->props.num_csu_mon;
 
 	cfg.match_pmg = true;
 	cfg.pmg = rmid;
@@ -413,11 +396,13 @@ int resctrl_arch_rmid_read(struct rdt_resource	*r, struct rdt_domain *d,
 
 	if (cdp_enabled) {
 		cfg.partid = resctrl_get_config_index(closid, CDP_DATA);
+		cfg.mon = cfg.partid % num_mon;
 		err = mpam_msmon_read(dom->comp, &cfg, type, val);
 		if (err)
 			return err;
 
 		cfg.partid = resctrl_get_config_index(closid, CDP_CODE);
+		cfg.mon = cfg.partid % num_mon;
 		err = mpam_msmon_read(dom->comp, &cfg, type, &cdp_val);
 		if (!err) {
 			pr_debug("read monitor rmid %u %s:%u CODE/DATA: %lld/%lld\n",
@@ -427,6 +412,7 @@ int resctrl_arch_rmid_read(struct rdt_resource	*r, struct rdt_domain *d,
 		}
 	} else {
 		cfg.partid = closid;
+		cfg.mon = cfg.partid % num_mon;
 		err = mpam_msmon_read(dom->comp, &cfg, type, val);
 	}
 