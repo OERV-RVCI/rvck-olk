OERV-RVCI/rvck-olk

commit 805b20db8a880a7842bf1d9556abcaea12ef261d

interference: Add cgroup-based inteference statistics framework
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

--------------------------------

Add cgroup-based interference statistics framework.

Signed-off-by: Xu Kuohai <xukuohai@huawei.com>
Signed-off-by: Pu Lehui <pulehui@huawei.com>

kernel/cgroup/ifs.c
@@ -1,0 +1,163 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Interference statistics for cgroup
+ *
+ * Copyright (C) 2025-2025 Huawei Technologies Co., Ltd
+ */
+
+#include "cgroup-internal.h"
+
+static DEFINE_PER_CPU(struct cgroup_ifs_cpu, cgrp_root_ifs_cpu);
+struct cgroup_ifs cgroup_root_ifs = {
+	.pcpu = &cgrp_root_ifs_cpu,
+};
+
+DEFINE_STATIC_KEY_FALSE(cgrp_ifs_enabled);
+
+#ifdef CONFIG_CGROUP_IFS_DEFAULT_ENABLED
+static bool ifs_enable = true;
+#else
+static bool ifs_enable;
+#endif
+
+static int __init setup_ifs(char *str)
+{
+	return kstrtobool(str, &ifs_enable) == 0;
+}
+__setup("cgroup_ifs=", setup_ifs);
+
+int cgroup_ifs_alloc(struct cgroup *cgrp)
+{
+	cgrp->ifs = kzalloc(sizeof(struct cgroup_ifs), GFP_KERNEL);
+	if (!cgrp->ifs)
+		return -ENOMEM;
+
+	cgrp->ifs->pcpu = alloc_percpu(struct cgroup_ifs_cpu);
+	if (!cgrp->ifs->pcpu) {
+		kfree(cgrp->ifs);
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+void cgroup_ifs_free(struct cgroup *cgrp)
+{
+	free_percpu(cgrp->ifs->pcpu);
+	kfree(cgrp->ifs);
+}
+
+static const char *ifs_type_name(int type)
+{
+	char *name = NULL;
+
+	switch (type) {
+	default:
+		break;
+	}
+
+	return name;
+}
+
+static int print_sum_time(struct cgroup_ifs *ifs, struct seq_file *seq)
+{
+	u64 time[NR_IFS_TYPES] = { 0 };
+	int cpu;
+	int i;
+
+	for_each_possible_cpu(cpu) {
+		for (i = 0; i < NR_IFS_TYPES; i++)
+			time[i] += per_cpu_ptr(ifs->pcpu, cpu)->time[i];
+	}
+
+	seq_printf(seq, "%-18s%s\n", "Interference", "Total Time (ns)");
+
+	for (i = 0; i < NR_IFS_TYPES; i++)
+		seq_printf(seq, "%-18s%llu\n", ifs_type_name(i), time[i]);
+
+	return 0;
+}
+
+static int cgroup_ifs_show(struct seq_file *seq, void *v)
+{
+	struct cgroup __maybe_unused *cgrp = seq_css(seq)->cgroup;
+	struct cgroup_ifs __maybe_unused *ifs = cgroup_ifs(cgrp);
+	int ret;
+
+	if (!ifs) {
+		pr_info("cgroup_ino(cgrp) = %ld\n", cgroup_ino(cgrp));
+		return -EINVAL;
+	}
+
+	ret = print_sum_time(ifs, seq);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+static ssize_t cgroup_ifs_write(struct kernfs_open_file *of, char *buf,
+				size_t nbytes, loff_t off)
+{
+	struct cgroup *cgrp = seq_css(of->seq_file)->cgroup;
+	struct cgroup_ifs *ifs = cgroup_ifs(cgrp);
+	struct cgroup_ifs_cpu *ifsc;
+	bool clear;
+	int cpu;
+
+	if (!ifs)
+		return -EOPNOTSUPP;
+
+	if (kstrtobool(strstrip(buf), &clear) < 0)
+		return -EINVAL;
+
+	if (!clear) {
+		for_each_possible_cpu(cpu) {
+			ifsc = per_cpu_ptr(ifs->pcpu, cpu);
+			memset(ifsc->time, 0, sizeof(ifsc->time));
+		}
+	}
+
+	return nbytes;
+}
+
+static struct cftype cgroup_ifs_files[] = {
+	{
+		.name = "interference.stat",
+		.seq_show = cgroup_ifs_show,
+		.write = cgroup_ifs_write,
+	},
+	{ }     /* terminate */
+};
+
+void cgroup_ifs_init(void)
+{
+	if (!ifs_enable)
+		return;
+
+	BUG_ON(cgroup_init_cftypes(NULL, cgroup_ifs_files));
+
+	static_branch_enable(&cgrp_ifs_enabled);
+}
+
+int cgroup_ifs_add_files(struct cgroup_subsys_state *css, struct cgroup *cgrp)
+{
+	int ret = 0;
+
+	if (!cgroup_ifs_enabled())
+		return 0;
+
+	ret = cgroup_addrm_files(css, cgrp, cgroup_ifs_files, true);
+	if (ret < 0) {
+		cgroup_addrm_files(css, cgrp, cgroup_ifs_files, false);
+		return ret;
+	}
+
+	return 0;
+}
+
+void cgroup_ifs_rm_files(struct cgroup_subsys_state *css, struct cgroup *cgrp)
+{
+	if (cgroup_ifs_enabled())
+		cgroup_addrm_files(css, cgrp, cgroup_ifs_files, false);
+}

include/linux/cgroup-defs.h
@@ -552,7 +552,11 @@ struct cgroup {
 	struct bpf_local_storage __rcu  *bpf_cgrp_storage;
 #endif
 
+#ifdef CONFIG_CGROUP_IFS
+	KABI_USE(1, struct cgroup_ifs *ifs)
+#else
 	KABI_RESERVE(1)
+#endif
 	KABI_RESERVE(2)
 	KABI_RESERVE(3)
 	KABI_RESERVE(4)

include/linux/cgroup.h
@@ -860,4 +860,59 @@ static inline void cgroup_bpf_put(struct cgroup *cgrp) {}
 void cgroup_move_task_to_root(struct task_struct *tsk);
 #endif
 
+#ifdef CONFIG_CGROUP_IFS
+
+enum ifs_types {
+	IFS_STUB,
+	NR_IFS_TYPES,
+};
+
+struct cgroup_ifs_cpu {
+	/* total time for each interference, in ns */
+	u64 time[NR_IFS_TYPES];
+};
+
+/*
+ * cgroup interference statistics
+ */
+struct cgroup_ifs {
+	/* per-cpu interference statistics tracking */
+	struct cgroup_ifs_cpu __percpu *pcpu;
+};
+
+extern struct cgroup_ifs cgroup_root_ifs;
+
+DECLARE_STATIC_KEY_FALSE(cgrp_ifs_enabled);
+static inline bool cgroup_ifs_enabled(void)
+{
+	return static_branch_unlikely(&cgrp_ifs_enabled);
+}
+
+static inline struct cgroup_ifs *cgroup_ifs(struct cgroup *cgrp)
+{
+	return cgroup_ino(cgrp) == 1 ? &cgroup_root_ifs : cgrp->ifs;
+}
+
+static inline struct cgroup_ifs *task_ifs(struct task_struct *task)
+{
+	return cgroup_ifs(task_dfl_cgroup(task));
+}
+
+static inline struct cgroup_ifs *current_ifs(void)
+{
+	return task_ifs(current);
+}
+
+static inline void cgroup_ifs_account_delta(struct cgroup_ifs_cpu *ifsc,
+					    int type, u64 delta)
+{
+	if (!cgroup_ifs_enabled())
+		return;
+
+	if (delta > 0)
+		ifsc->time[type] += delta;
+}
+
+#endif /* CONFIG_CGROUP_IFS */
+
 #endif /* _LINUX_CGROUP_H */

kernel/cgroup/Makefile
@@ -7,3 +7,4 @@ obj-$(CONFIG_CGROUP_RDMA) += rdma.o
 obj-$(CONFIG_CPUSETS) += cpuset.o
 obj-$(CONFIG_CGROUP_MISC) += misc.o
 obj-$(CONFIG_CGROUP_DEBUG) += debug.o
+obj-$(CONFIG_CGROUP_IFS) += ifs.o

kernel/cgroup/cgroup-internal.h
@@ -302,4 +302,20 @@ int cgroup_addrm_files(struct cgroup_subsys_state *css,
 		       struct cgroup *cgrp, struct cftype cfts[],
 		       bool is_add);
 
+#ifdef CONFIG_CGROUP_IFS
+int cgroup_ifs_alloc(struct cgroup *cgrp);
+void cgroup_ifs_free(struct cgroup *cgrp);
+void cgroup_ifs_init(void);
+int cgroup_ifs_add_files(struct cgroup_subsys_state *css, struct cgroup *cgrp);
+void cgroup_ifs_rm_files(struct cgroup_subsys_state *css, struct cgroup *cgrp);
+#else /* !CONFIG_CGROUP_IFS */
+static inline int cgroup_ifs_alloc(struct cgroup *cgrp) { return 0; }
+static inline void cgroup_ifs_free(struct cgroup *cgrp) {}
+static inline void cgroup_ifs_init(void) {}
+static inline int cgroup_ifs_add_files(struct cgroup_subsys_state *css,
+				       struct cgroup *cgrp) { return 0; }
+static inline void cgroup_ifs_rm_files(struct cgroup_subsys_state *css,
+				       struct cgroup *cgrp) {}
+#endif /* CONFIG_CGROUP_IFS */
+
 #endif /* __CGROUP_INTERNAL_H */

kernel/cgroup/cgroup.c
@@ -1700,6 +1700,7 @@ static void css_clear_dir(struct cgroup_subsys_state *css)
 			if (cgroup_psi_enabled())
 				cgroup_addrm_files(css, cgrp,
 						   cgroup_psi_files, false);
+			cgroup_ifs_rm_files(css, cgrp);
 		} else {
 			cgroup_addrm_files(css, cgrp,
 					   cgroup1_base_files, false);
@@ -1738,6 +1739,10 @@ static int css_populate_dir(struct cgroup_subsys_state *css)
 				if (ret < 0)
 					return ret;
 			}
+
+			ret = cgroup_ifs_add_files(css, cgrp);
+			if (ret < 0)
+				return ret;
 		} else {
 			ret = cgroup_addrm_files(css, cgrp,
 						 cgroup1_base_files, true);
@@ -5477,6 +5482,7 @@ static void css_free_rwork_fn(struct work_struct *work)
 			cgroup_put(cgroup_parent(cgrp));
 			kernfs_put(cgrp->kn);
 			psi_cgroup_free(cgrp);
+			cgroup_ifs_free(cgrp);
 			cgroup_rstat_exit(cgrp);
 			kfree(cgrp);
 		} else {
@@ -5724,10 +5730,14 @@ static struct cgroup *cgroup_create(struct cgroup *parent, const char *name,
 	if (ret)
 		goto out_kernfs_remove;
 
+	ret = cgroup_ifs_alloc(cgrp);
+	if (ret)
+		goto out_psi_free;
+
 	if (cgrp->root == &cgrp_dfl_root) {
 		ret = cgroup_bpf_inherit(cgrp);
 		if (ret)
-			goto out_psi_free;
+			goto out_ifs_free;
 	}
 
 	/*
@@ -5788,6 +5798,8 @@ static struct cgroup *cgroup_create(struct cgroup *parent, const char *name,
 
 	return cgrp;
 
+out_ifs_free:
+	cgroup_ifs_free(cgrp);
 out_psi_free:
 	psi_cgroup_free(cgrp);
 out_kernfs_remove:
@@ -6194,6 +6206,8 @@ int __init cgroup_init(void)
 	BUG_ON(cgroup_init_cftypes(NULL, cgroup_base_files));
 	BUG_ON(cgroup_init_cftypes(NULL, cgroup_psi_files));
 	BUG_ON(cgroup_init_cftypes(NULL, cgroup1_base_files));
+
+	cgroup_ifs_init();
 
 	cgroup_rstat_boot();
 

commit c27ddc47dc1b14f3e0fc7313de5a51110cf81245

ifs: Defer cgroup ifs enable
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

--------------------------------

Start cgroup ifs after the kernel core startup process is
completed, otherwise the intermediate state may be accessed.

Fixes: 805b20db8a88 ("interference: Add cgroup-based inteference statistics framework")
Signed-off-by: Pu Lehui <pulehui@huawei.com>

kernel/cgroup/ifs.c
@@ -537,15 +537,13 @@ void cgroup_ifs_init(void)
 
 	BUG_ON(cgroup_init_cftypes(NULL, cgroup_ifs_files));
 	cgroup_ifs_tdesc_init();
-
-	static_branch_enable(&cgrp_ifs_enabled);
 }
 
 int cgroup_ifs_add_files(struct cgroup_subsys_state *css, struct cgroup *cgrp)
 {
 	int ret = 0;
 
-	if (!cgroup_ifs_enabled())
+	if (!ifs_enable)
 		return 0;
 
 	ret = cgroup_addrm_files(css, cgrp, cgroup_ifs_files, true);
@@ -559,7 +557,7 @@ int cgroup_ifs_add_files(struct cgroup_subsys_state *css, struct cgroup *cgrp)
 
 void cgroup_ifs_rm_files(struct cgroup_subsys_state *css, struct cgroup *cgrp)
 {
-	if (cgroup_ifs_enabled())
+	if (ifs_enable)
 		cgroup_addrm_files(css, cgrp, cgroup_ifs_files, false);
 }
 
@@ -576,3 +574,16 @@ void cgroup_ifs_enable_irq_account(bool enable)
 	ifs_irq_enable = enable;
 }
 #endif
+
+static __init int cgroup_ifs_enable(void)
+{
+	if (!ifs_enable)
+		return 0;
+
+	if (!this_cpu_read(ifs_tsc_freq))
+		return 0;
+
+	static_branch_enable(&cgrp_ifs_enabled);
+	return 0;
+}
+late_initcall_sync(cgroup_ifs_enable);