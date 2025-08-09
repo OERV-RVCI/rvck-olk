OERV-RVCI/rvck-olk

commit 390982f28c5796a1e590381044630b768e6b9696

mm/mem_sampling: Add sysctl control for NUMA balancing integration
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IC8KS8
CVE: NA

--------------------------------

Extend the /proc/sys/kernel/mem_sampling_enable sysctl interface to
support fine-grained control over mem_sampling and its integration with
NUMA balancing.

Supported values:
  0 - Disable mem_sampling entirely
  1 - Enable mem_sampling (used by perf or other subsystems)
  2 - Enable mem_sampling and allow NUMA balancing to consume access hints

Although mem_sampling and perf both rely on the same hardware sampling
resources (e.g. ARM SPE), perf is allowed to preempt mem_sampling:
if perf starts sampling, mem_sampling will be automatically stopped to
avoid resource conflicts.

This ensures that tools using perf have priority, while still allowing
NUMA and other kernel subsystems to use sampling data when perf is idle.

The sysctl interface allows dynamic switching at runtime without reboot.

Signed-off-by: Ze Zuo <zuoze1@huawei.com>
Signed-off-by: Tong Tiangen <tongtiangen@huawei.com>
Signed-off-by: Shuang Yan <yanshuang7@huawei.com>

mm/mem_sampling.c
@@ -25,10 +25,15 @@
 
 #define MEM_SAMPLING_DISABLED		0x0
 #define MEM_SAMPLING_NORMAL		0x1
+#define MEM_SAMPLING_MIN_VALUE		0
+#define MEM_SAMPLING_MAX_VALUE		3
 
 struct mem_sampling_ops_struct mem_sampling_ops;
 static int mem_sampling_override __initdata;
 static int sysctl_mem_sampling_mode;
+
+static const int mem_sampling_min_value = MEM_SAMPLING_MIN_VALUE;
+static const int mem_sampling_max_value = MEM_SAMPLING_MAX_VALUE;
 
 /* keep track of who use the SPE */
 DEFINE_PER_CPU(enum arm_spe_user_e, arm_spe_user);
@@ -269,10 +274,13 @@ static void numa_balancing_mem_sampling_cb_unregister(void)
 }
 static void set_numabalancing_mem_sampling_state(bool enabled)
 {
-	if (enabled)
+	if (enabled) {
 		numa_balancing_mem_sampling_cb_register();
-	else
+		static_branch_enable(&sched_numabalancing_mem_sampling);
+	} else {
 		numa_balancing_mem_sampling_cb_unregister();
+		static_branch_disable(&sched_numabalancing_mem_sampling);
+	}
 }
 #else
 static inline void set_numabalancing_mem_sampling_state(bool enabled) { }
@@ -396,18 +404,40 @@ static int proc_mem_sampling_enable(struct ctl_table *table, int write,
 {
 	struct ctl_table t;
 	int err;
-	int state = sysctl_mem_sampling_mode;
+	int state = 0;
+
+	if (static_branch_likely(&mem_sampling_access_hints))
+		state = 1;
+	if (static_branch_likely(&sched_numabalancing_mem_sampling))
+		state = 2;
 
 	if (write && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
 
 	t = *table;
 	t.data = &state;
+	t.extra1 = (int *)&mem_sampling_min_value;
+	t.extra2 = (int *)&mem_sampling_max_value;
 	err = proc_dointvec_minmax(&t, write, buffer, lenp, ppos);
 	if (err < 0)
 		return err;
-	if (write)
-		set_mem_sampling_state(state);
+	if (write) {
+		switch (state) {
+		case 0:
+			set_mem_sampling_state(false);
+			break;
+		case 1:
+			set_mem_sampling_state(false);
+			set_mem_sampling_state(true);
+			break;
+		case 2:
+			set_mem_sampling_state(true);
+			set_numabalancing_mem_sampling_state(true);
+			break;
+		default:
+			return -EINVAL;
+		}
+	}
 	return err;
 }
 
@@ -419,7 +449,7 @@ static struct ctl_table mem_sampling_sysctls[] = {
 		.mode		= 0644,
 		.proc_handler   = proc_mem_sampling_enable,
 		.extra1		= SYSCTL_ZERO,
-		.extra2		= SYSCTL_ONE,
+		.extra2		= (int *)&mem_sampling_max_value,
 	},
 	{}
 };

commit 44ed49d5a096c4d6ad62d142d00a929a3007372b

mm/mem_sampling: preserve kernel subsystem SPE state across perf sampling
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICRXXI
CVE: NA

--------------------------------

Ensure proper save/restore of SPE state for kernel subsystems (NUMA
balancing, DAMON etc.) when perf SPE sampling is enabled/disabled:
- Suspend subsystem memory sampling during perf SPE operation
- Restore original configurations when perf sampling stops

Fixes: 390982f28c57 ("mm/mem_sampling: Add sysctl control for NUMA balancing integration")
Signed-off-by: Ze Zuo <zuoze1@huawei.com>

mm/mem_sampling.c
@@ -41,8 +41,11 @@ DEFINE_PER_CPU(enum arm_spe_user_e, arm_spe_user);
 EXPORT_PER_CPU_SYMBOL_GPL(arm_spe_user);
 
 enum mem_sampling_saved_state_e {
-	MEM_SAMPLING_STATE_ENABLE,
 	MEM_SAMPLING_STATE_DISABLE,
+	MEM_SAMPLING_STATE_ENABLE,
+	MEM_SAMPLING_STATE_NUMA_ENABLE,
+	MEM_SAMPLING_STATE_DAMON_ENABLE,
+	MEM_SAMPLING_STATE_NUM_DAMON_ENABLE,
 	MEM_SAMPLING_STATE_EMPTY,
 };
 enum mem_sampling_saved_state_e mem_sampling_saved_state = MEM_SAMPLING_STATE_EMPTY;
@@ -416,16 +419,25 @@ static void __set_mem_sampling_state(bool enabled)
 	}
 }
 
+static enum mem_sampling_saved_state_e get_mem_sampling_saved_state(void)
+{
+	if (static_branch_likely(&mm_damon_mem_sampling) &&
+		static_branch_likely(&sched_numabalancing_mem_sampling))
+		return MEM_SAMPLING_STATE_NUM_DAMON_ENABLE;
+	if (static_branch_likely(&mm_damon_mem_sampling))
+		return MEM_SAMPLING_STATE_DAMON_ENABLE;
+	if (static_branch_likely(&sched_numabalancing_mem_sampling))
+		return MEM_SAMPLING_STATE_NUMA_ENABLE;
+	if (static_branch_likely(&mem_sampling_access_hints))
+		return MEM_SAMPLING_STATE_ENABLE;
+
+	return MEM_SAMPLING_STATE_DISABLE;
+}
+
 void set_mem_sampling_state(bool enabled)
 {
 	if (!mem_sampling_ops.sampling_start || !mm_spe_enabled())
 		return;
-
-	if (mem_sampling_saved_state != MEM_SAMPLING_STATE_EMPTY) {
-		mem_sampling_saved_state = enabled ? MEM_SAMPLING_STATE_ENABLE :
-					    MEM_SAMPLING_STATE_DISABLE;
-		return;
-	}
 
 	if (enabled)
 		sysctl_mem_sampling_mode = MEM_SAMPLING_NORMAL;
@@ -434,10 +446,45 @@ void set_mem_sampling_state(bool enabled)
 	__set_mem_sampling_state(enabled);
 }
 
+static int set_state(int state)
+{
+	if (mem_sampling_saved_state != MEM_SAMPLING_STATE_EMPTY) {
+		mem_sampling_saved_state = state;
+		return -EINVAL;
+	}
+	switch (state) {
+	case 0:
+		set_mem_sampling_state(false);
+		break;
+	case 1:
+		set_mem_sampling_state(false);
+		set_mem_sampling_state(true);
+		break;
+	case 2:
+		set_mem_sampling_state(false);
+		set_mem_sampling_state(true);
+		set_numabalancing_mem_sampling_state(true);
+		break;
+	case 3:
+		set_mem_sampling_state(false);
+		set_mem_sampling_state(true);
+		set_damon_mem_sampling_state(true);
+		break;
+	case 4:
+		set_mem_sampling_state(true);
+		set_numabalancing_mem_sampling_state(true);
+		set_damon_mem_sampling_state(true);
+		break;
+	default:
+		return -EINVAL;
+	}
+	return 0;
+}
+
 void mem_sampling_user_switch_process(enum user_switch_type type)
 {
-	bool state, mm_spe_is_perf_user = false;
-	int cpu;
+	bool mm_spe_is_perf_user = false;
+	int cpu, hints;
 
 	if (type >= USER_SWITCH_TYPE_MAX) {
 		pr_err("user switch type error.\n");
@@ -456,26 +503,27 @@ void mem_sampling_user_switch_process(enum user_switch_type type)
 		if (mem_sampling_saved_state != MEM_SAMPLING_STATE_EMPTY)
 			return;
 
-		if (static_branch_unlikely(&mem_sampling_access_hints))
-			mem_sampling_saved_state = MEM_SAMPLING_STATE_ENABLE;
+		hints = get_mem_sampling_saved_state();
+		set_state(0);
+
+		if (hints)
+			mem_sampling_saved_state = hints;
 		else
 			mem_sampling_saved_state = MEM_SAMPLING_STATE_DISABLE;
 
-		pr_debug("user switch away from mem_sampling, %s is saved, set to disable.\n",
-				mem_sampling_saved_state ? "disabled" : "enabled");
+		pr_debug("user switch away from mem_sampling, %d is saved, set to disable.\n",
+				mem_sampling_saved_state);
 
-		set_mem_sampling_state(false);
 	} else {
 		/* If the state is not backed up, do not restore it */
 		if (mem_sampling_saved_state == MEM_SAMPLING_STATE_EMPTY || mm_spe_is_perf_user)
 			return;
 
-		state = (mem_sampling_saved_state == MEM_SAMPLING_STATE_ENABLE) ? true : false;
-		set_mem_sampling_state(state);
+		hints = mem_sampling_saved_state;
 		mem_sampling_saved_state = MEM_SAMPLING_STATE_EMPTY;
+		set_state(hints);
 
-		pr_debug("user switch back to mem_sampling, set to saved %s.\n",
-				state ? "enalbe" : "disable");
+		pr_debug("user switch back to mem_sampling, set to saved %d.\n", hints);
 	}
 }
 EXPORT_SYMBOL_GPL(mem_sampling_user_switch_process);
@@ -488,15 +536,7 @@ static int proc_mem_sampling_enable(struct ctl_table *table, int write,
 	int err;
 	int state = 0;
 
-	if (static_branch_likely(&mem_sampling_access_hints))
-		state = 1;
-	if (static_branch_likely(&sched_numabalancing_mem_sampling))
-		state = 2;
-	if (static_branch_likely(&mm_damon_mem_sampling))
-		state = 3;
-	if (static_branch_likely(&mm_damon_mem_sampling) &&
-		static_branch_likely(&sched_numabalancing_mem_sampling))
-		state = 4;
+	state = get_mem_sampling_saved_state();
 
 	if (write && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
@@ -508,34 +548,8 @@ static int proc_mem_sampling_enable(struct ctl_table *table, int write,
 	err = proc_dointvec_minmax(&t, write, buffer, lenp, ppos);
 	if (err < 0)
 		return err;
-	if (write) {
-		switch (state) {
-		case 0:
-			set_mem_sampling_state(false);
-			break;
-		case 1:
-			set_mem_sampling_state(false);
-			set_mem_sampling_state(true);
-			break;
-		case 2:
-			set_mem_sampling_state(false);
-			set_mem_sampling_state(true);
-			set_numabalancing_mem_sampling_state(true);
-			break;
-		case 3:
-			set_mem_sampling_state(false);
-			set_mem_sampling_state(true);
-			set_damon_mem_sampling_state(true);
-			break;
-		case 4:
-			set_mem_sampling_state(true);
-			set_numabalancing_mem_sampling_state(true);
-			set_damon_mem_sampling_state(true);
-			break;
-		default:
-			return -EINVAL;
-		}
-	}
+	if (write)
+		err = set_state(state);
 	return err;
 }
 