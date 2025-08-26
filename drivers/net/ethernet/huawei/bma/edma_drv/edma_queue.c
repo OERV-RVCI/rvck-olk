OERV-RVCI/rvck-olk

commit 85502a982830072aff2efa8b65f75c240b4d6181

Huawei iBMA: Added support for Hi1712 Chip
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICB3EN
CVE: NA

-----------------------------------------

To meet the competitive evolution requirements ("5+1+1") for the
new-generation Kunpeng ARM platform, Tianchi architecture, and
BMC management system. The products, BMC, and HiSilicon collaboratively
planned the next-generation BMC evolution chip Hi1712.

Building upon Hi1711, the Hi1712 chip enhances interfaces, computing power,
and security. The Huawei iBMA driver requires adaptation to support Hi1712
for in-band and out-of-band communication.

Signed-off-by: Huangjunhua <huangjunhua14@huawei.com>

drivers/net/ethernet/huawei/bma/edma_drv/edma_queue.c
@@ -1,0 +1,470 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Huawei iBMA driver.
+ * Copyright (c) 2025, Huawei Technologies Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/errno.h>
+#include <linux/kthread.h>
+#include <linux/mm.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+#include "bma_pci.h"
+#include "edma_host.h"
+#include "edma_queue.h"
+
+static u32 pcie_dma_read(u32 offset)
+{
+	u32 reg_val;
+
+	reg_val = readl(get_bma_dev()->bma_pci_dev->bma_base_addr + offset);
+	BMA_LOG(DLOG_DEBUG, "readl, offset 0x%x val 0x%x\n", offset, reg_val);
+	return reg_val;
+}
+
+static void pcie_dma_write(u32 offset, u32 reg_val)
+{
+	u32 read_val;
+
+	(void)writel(reg_val, get_bma_dev()->bma_pci_dev->bma_base_addr + offset);
+	read_val = readl(get_bma_dev()->bma_pci_dev->bma_base_addr + offset);
+	if (read_val != reg_val) {
+		BMA_LOG(DLOG_DEBUG,
+			"writel fail, read_value: 0x%x, set_value: 0x%x, offset: 0x%x\n",
+			read_val, reg_val, offset);
+		return;
+	}
+	BMA_LOG(DLOG_DEBUG, "writel, offset 0x%x val 0x%x\n", offset, reg_val);
+}
+
+static void set_dma_queue_int_msk(u32 val)
+{
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_INT_MSK_0_REG, val);
+}
+
+static void set_dma_queue_err_int_msk(u32 val)
+{
+	union U_DMA_QUEUE_INT_MSK reg_val;
+
+	// The least significant bit (bit 0) of this register is reserved and must be cleared,
+	// while the remaining bits should retain their original values.
+	reg_val.u32 = val & 0xFFFFFFFE;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_ERR_INT_MSK_0_REG, reg_val.u32);
+}
+
+static void set_dma_queue_int_sts(u32 val)
+{
+	union U_DMA_QUEUE_INT_STS reg_val;
+
+	reg_val.u32 = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_INT_STS_0_REG, reg_val.u32);
+}
+
+static void get_dma_queue_int_sts(u32 *val)
+{
+	union U_DMA_QUEUE_INT_STS reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_INT_STS_0_REG);
+	*val = reg_val.u32;
+}
+
+static void get_dma_queue_fsm_sts(u32 *val)
+{
+	union U_DMA_QUEUE_FSM_STS reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_FSM_STS_0_REG);
+	*val = reg_val.bits.dma_queue_sts;
+}
+
+static void pause_dma_queue(u32 val)
+{
+	union U_DMA_QUEUE_CTRL0 reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_CTRL0_0_REG);
+	reg_val.bits.dma_queue_pause = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CTRL0_0_REG, reg_val.u32);
+}
+
+static void enable_dma_queue(u32 val)
+{
+	union U_DMA_QUEUE_CTRL0 reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_CTRL0_0_REG);
+	reg_val.bits.dma_queue_en = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CTRL0_0_REG, reg_val.u32);
+}
+
+static void reset_dma_queue(u32 val)
+{
+	union U_DMA_QUEUE_CTRL1 reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_CTRL1_0_REG);
+	reg_val.bits.dma_queue_reset = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CTRL1_0_REG, reg_val.u32);
+}
+
+static void set_dma_queue_sq_tail(u32 val)
+{
+	union U_DMA_QUEUE_SQ_TAIL_PTR reg_val;
+
+	reg_val.bits.dma_queue_sq_tail_ptr = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_SQ_TAIL_PTR_0_REG, reg_val.u32);
+}
+
+static void set_dma_queue_cq_head(u32 val)
+{
+	union U_DMA_QUEUE_CQ_HEAD_PTR reg_val;
+
+	reg_val.bits.dma_queue_cq_head_ptr = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CQ_HEAD_PTR_0_REG, reg_val.u32);
+}
+
+void set_dma_queue_sq_base_l(u32 val)
+{
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_SQ_BASE_L_0_REG, val);
+}
+
+void set_dma_queue_sq_base_h(u32 val)
+{
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_SQ_BASE_H_0_REG, val);
+}
+
+void set_dma_queue_cq_base_l(u32 val)
+{
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CQ_BASE_L_0_REG, val);
+}
+
+void set_dma_queue_cq_base_h(u32 val)
+{
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CQ_BASE_H_0_REG, val);
+}
+
+static void set_dma_queue_sq_depth(u32 val)
+{
+	union U_DMA_QUEUE_SQ_DEPTH reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_SQ_DEPTH_0_REG);
+	reg_val.bits.dma_queue_sq_depth = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_SQ_DEPTH_0_REG, reg_val.u32);
+}
+
+static void set_dma_queue_cq_depth(u32 val)
+{
+	union U_DMA_QUEUE_CQ_DEPTH reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_CQ_DEPTH_0_REG);
+	reg_val.bits.dma_queue_cq_depth = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CQ_DEPTH_0_REG, reg_val.u32);
+}
+
+static void set_dma_queue_arb_weight(u32 val)
+{
+	union U_DMA_QUEUE_CTRL0 reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_CTRL0_0_REG);
+	reg_val.bits.dma_queue_arb_weight = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CTRL0_0_REG, reg_val.u32);
+}
+
+static void set_dma_queue_drct_sel(u32 val)
+{
+	union U_DMA_QUEUE_CTRL0 reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_CTRL0_0_REG);
+	reg_val.bits.dma_queue_cq_drct_sel = val;
+	reg_val.bits.dma_queue_sq_drct_sel = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CTRL0_0_REG, reg_val.u32);
+}
+
+static void get_dma_queue_sq_tail(u32 *val)
+{
+	union U_DMA_QUEUE_SQ_TAIL_PTR reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_SQ_TAIL_PTR_0_REG);
+	*val = reg_val.bits.dma_queue_sq_tail_ptr;
+}
+
+static void get_dma_queue_cq_tail(u32 *val)
+{
+	union U_DMA_QUEUE_CQ_TAIL_PTR reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_CQ_TAIL_PTR_0_REG);
+	*val = reg_val.bits.dma_queue_cq_tail_ptr;
+}
+
+static void get_dma_queue_sq_head(u32 *val)
+{
+	u32 reg_val;
+
+	reg_val = pcie_dma_read(PCIE_DMA_QUEUE_SQ_STS_0_REG);
+	/* dma_queue_sq_head_ptr bit[15:0] */
+	*val = reg_val & 0xFFFF;
+}
+
+static void set_dma_queue_err_abort(u32 val)
+{
+	union U_DMA_QUEUE_CTRL0 reg_val;
+
+	reg_val.u32 = pcie_dma_read(PCIE_DMA_QUEUE_CTRL0_0_REG);
+	reg_val.bits.dma_queue_sq_pa_lkp_err_abort_en = val;
+	reg_val.bits.dma_queue_sq_proc_err_abort_en = val;
+	reg_val.bits.dma_queue_sq_drop_err_abort_en = val;
+	reg_val.bits.dma_queue_sq_cfg_err_abort_en = val;
+	(void)pcie_dma_write(PCIE_DMA_QUEUE_CTRL0_0_REG, reg_val.u32);
+}
+
+static void set_dma_queue_flr_disable(u32 val)
+{
+	(void)pcie_dma_write(PCIE_DMA_FLR_DISABLE_REG, val);
+}
+
+static void clear_dma_queue_int_chk(u32 mask)
+{
+	u32 int_sts;
+
+	(void)get_dma_queue_int_sts(&int_sts);
+	if (int_sts & mask)
+		(void)set_dma_queue_int_sts(mask);
+}
+
+s32 check_dma_queue_state(u32 state, u32 flag)
+{
+	u32 dma_state = 0;
+	unsigned long timeout;
+
+	BMA_LOG(DLOG_DEBUG, "state:%u, flag:%u\n", state, flag);
+
+	timeout = jiffies + TIMER_INTERVAL_CHECK;
+
+	while (1) {
+		get_dma_queue_fsm_sts(&dma_state);
+		BMA_LOG(DLOG_DEBUG, "DMA stats[%u]\n", dma_state);
+		// Flag is 0 and state does not equal to target value
+		// OR Flag is 1 and state is equal to target value
+		if ((!flag && dma_state != state) || (flag && dma_state == state))
+			break;
+
+		if (time_after(jiffies, timeout)) {
+			BMA_LOG(DLOG_DEBUG, "Wait stats[%u] fail\n", state);
+			return -ETIMEDOUT;
+		}
+		udelay(1);
+	}
+	return 0;
+}
+
+static s32 reset_dma(void)
+{
+	u32 dma_state = 0;
+
+	/* get dma channel fsm */
+	check_dma_queue_state(WAIT_STATE, FALSE);
+	get_dma_queue_fsm_sts(&dma_state);
+	BMA_LOG(DLOG_DEBUG, "dma_state:%u\n", dma_state);
+	switch (dma_state) {
+	/* idle status, dma channel need no reset */
+	case IDLE_STATE:
+		return 0;
+	case RUN_STATE:
+		pause_dma_queue(ENABLE);
+		fallthrough;
+	case ABORT_STATE:
+	case CPL_STATE:
+		enable_dma_queue(DISABLE);
+		if (check_dma_queue_state(RUN_STATE, FALSE))
+			return -ETIMEDOUT;
+		fallthrough;
+	case PAUSE_STATE:
+	case HALT_STATE:
+		set_dma_queue_sq_tail(0);
+		set_dma_queue_cq_head(0);
+		reset_dma_queue(ENABLE);
+		pause_dma_queue(DISABLE);
+		if (check_dma_queue_state(IDLE_STATE, TRUE))
+			return -ETIMEDOUT;
+		fallthrough;
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static void init_dma(void)
+{
+	/* set dma channel sq tail */
+	set_dma_queue_sq_tail(0);
+	/* set dma channel cq head */
+	set_dma_queue_cq_head(0);
+	/* set dma queue drct sel */
+	set_dma_queue_drct_sel(DRC_LOCAL);
+	/* set dma channel sq depth */
+	set_dma_queue_sq_depth(SQ_DEPTH - 1);
+	/* set dma channel cq depth */
+	set_dma_queue_cq_depth(CQ_DEPTH - 1);
+	/* dma not process FLR , only cpu process FLR */
+	set_dma_queue_flr_disable(0x1);
+	/* set dma queue arb weight */
+	set_dma_queue_arb_weight(0x1F);
+	/* clear dma queue int status */
+	set_dma_queue_int_sts(0x1FFF);
+	/* set dma queue int mask */
+	set_dma_queue_err_int_msk(0x0);
+	set_dma_queue_int_msk(0x0);
+	/* set dma queue abort err en */
+	set_dma_queue_err_abort(ENABLE);
+	/* enable dma channel en */
+	enable_dma_queue(ENABLE);
+}
+
+s32 wait_done_dma_queue(unsigned long timeout)
+{
+	struct dma_ch_cq_s *p_cur_last_cq;
+	struct dma_ch_cq_s *p_dma_cq;
+	unsigned long end;
+	u32 sq_tail;
+	u32 sq_valid;
+	u32 cq_tail;
+	u32 cq_valid;
+
+	p_dma_cq = (struct dma_ch_cq_s *)((&get_bma_dev()->edma_host)->edma_cq_addr);
+	end = jiffies + timeout;
+
+	while (time_before(jiffies, end)) {
+		(void)get_dma_queue_sq_tail(&sq_tail);
+		(void)get_dma_queue_cq_tail(&cq_tail);
+
+		cq_valid = (cq_tail + CQ_DEPTH - 1) % (CQ_DEPTH);
+		p_cur_last_cq = &p_dma_cq[cq_valid];
+		sq_valid = (sq_tail + SQ_DEPTH - 1) % (SQ_DEPTH);
+		BMA_LOG(DLOG_DEBUG,
+			"sq_tail %d, cq_tail %d, cq_valid %d, sq_valid %d, p_cur_last_cq->sqhd %d\n",
+			sq_tail, cq_tail, cq_valid, sq_valid, p_cur_last_cq->sqhd);
+		if (p_cur_last_cq->sqhd == sq_valid) {
+			set_dma_queue_cq_head(cq_valid);
+			return 0;
+		}
+	}
+
+	return -ETIMEDOUT;
+}
+
+static s32 submit_dma_queue_sq(u32 dir, struct bspveth_dmal pdmalbase_v, u32 pf)
+{
+	u32 sq_tail;
+	u32 sq_head;
+	u32 sq_availble;
+	struct dma_ch_sq_s sq_submit;
+	struct dma_ch_sq_s *p_dma_sq;
+
+	p_dma_sq = (struct dma_ch_sq_s *)((&get_bma_dev()->edma_host)->edma_sq_addr);
+	(void)get_dma_queue_sq_tail(&sq_tail);
+	(void)get_dma_queue_sq_head(&sq_head);
+	sq_availble = SQ_DEPTH - 1 - (((sq_tail - sq_head) + SQ_DEPTH) % SQ_DEPTH);
+	if (sq_availble < 1) {
+		BMA_LOG(DLOG_DEBUG, "cannot process %u descriptors, try again later\n", 1);
+		return -1;
+	}
+
+	BMA_LOG(DLOG_DEBUG, "submit dma queue sq, sq_tail get %d, sq_head %d, sq_availble %d\n",
+		sq_tail, sq_head, sq_availble);
+
+	(void)memset(&sq_submit, 0, sizeof(sq_submit));
+	if (dir == DIR_H2B)
+		sq_submit.opcode = DMA_READ;
+	else
+		sq_submit.opcode = DMA_WRITE;
+
+	BMA_LOG(DLOG_DEBUG, "PF: %u\n", pf);
+	sq_submit.ldie = ENABLE;
+	sq_submit.rdie = ENABLE;
+	sq_submit.attr &= (~0x2); /* SO(Strong Ordering) */
+	sq_submit.pf = pf & 0x7;  /* 0x7 */
+	sq_submit.p3p4 = (pf >> 3) & 0x3; /* 0x3 */
+	sq_submit.length = pdmalbase_v.len;
+	sq_submit.src_addr_l = pdmalbase_v.slow;
+	sq_submit.src_addr_h = pdmalbase_v.shi;
+	sq_submit.dst_addr_l = pdmalbase_v.dlow;
+	sq_submit.dst_addr_h = pdmalbase_v.dhi;
+
+	BMA_LOG(DLOG_DEBUG, "submit dma queue sq, dir %d, op %d, length %d\n", dir,
+		sq_submit.opcode, sq_submit.length);
+
+	memcpy(p_dma_sq + sq_tail, &sq_submit, sizeof(sq_submit));
+	sq_tail = (sq_tail + 1) % SQ_DEPTH;
+
+	BMA_LOG(DLOG_DEBUG, "submit dma queue sq, sq_tail change %d,\n", sq_tail);
+	wmb(); /* memory barriers. */
+
+	(void)set_dma_queue_sq_tail(sq_tail);
+
+	return 0;
+}
+
+s32 transfer_dma_queue(struct bma_dma_transfer_s *dma_transfer)
+{
+	struct bspveth_dmal *pdmalbase_v;
+	u32 dmal_cnt;
+	s32 ret;
+	int i;
+
+	if (!dma_transfer) {
+		BMA_LOG(DLOG_DEBUG, "dma_transfer is NULL.\n");
+		return -EFAULT;
+	}
+
+	BMA_LOG(DLOG_DEBUG, "transfer dma queue.\n");
+
+	/* clear local done int */
+	clear_dma_queue_int_chk(DMA_DONE_MASK);
+
+	pdmalbase_v = dma_transfer->pdmalbase_v;
+	dmal_cnt = dma_transfer->dmal_cnt;
+	for (i = 0; i < dmal_cnt; i++)
+		submit_dma_queue_sq(dma_transfer->dir, pdmalbase_v[i],
+				    get_bma_dev()->bma_pci_dev->pdev->devfn);
+
+	(void)set_dma_queue_int_msk(DMA_DONE_UNMASK);
+	(void)set_dma_queue_err_int_msk(DMA_ERR_UNMASK);
+	(void)enable_dma_queue(ENABLE);
+
+	ret = wait_done_dma_queue(DMA_TMOUT);
+	if (ret)
+		BMA_LOG(DLOG_DEBUG, "EP DMA: dma wait timeout");
+
+	return ret;
+}
+
+void reset_edma_host(struct edma_host_s *edma_host)
+{
+	unsigned long flags = 0;
+	int count = 0;
+
+	if (!edma_host)
+		return;
+
+	spin_lock_irqsave(&edma_host->reg_lock, flags);
+
+	while (count++ < MAX_RESET_DMA_TIMES) {
+		if (reset_dma() == 0) {
+			BMA_LOG(DLOG_DEBUG, "reset dma successfully\n");
+			init_dma();
+			break;
+		}
+
+		mdelay(DELAY_BETWEEN_RESET_DMA);
+	}
+
+	spin_unlock_irqrestore(&edma_host->reg_lock, flags);
+	BMA_LOG(DLOG_DEBUG, "reset dma count=%d\n", count);
+}

MAINTAINERS
@@ -9707,6 +9707,12 @@ F:	drivers/net/ethernet/huawei/hinic3/cqm/
 F:	drivers/net/ethernet/huawei/hinic3/hw/
 F:	drivers/net/ethernet/huawei/hinic3/include/
 
+HUAWEI ETHERNET DRIVER
+M:	Huangjunhua <huangjunhua14@huawei.com>
+L:	netdev@vger.kernel.org
+S:	Maintained
+F:	drivers/net/ethernet/huawei/bma/
+
 HUAWEI BIFUR DRIVER
 M:	Xiaoping zheng <zhengxiaoping5@huawei.com>
 L:	netdev@vger.kernel.org

drivers/net/ethernet/huawei/bma/cdev_drv/bma_cdev.c
@@ -28,7 +28,7 @@
 #ifdef DRV_VERSION
 #define CDEV_VERSION		MICRO_TO_STR(DRV_VERSION)
 #else
-#define CDEV_VERSION		"0.3.10"
+#define CDEV_VERSION		"0.4.0"
 #endif
 
 #define CDEV_DEFAULT_NUM	4

drivers/net/ethernet/huawei/bma/cdev_veth_drv/virtual_cdev_eth_net.c
@@ -151,6 +151,12 @@ int edma_veth_setup_all_tx_resources(struct edma_eth_dev_s *edma_eth)
 	u8 *shmq_head = NULL;
 	u8 *shmq_head_p = NULL;
 	struct edma_rxtx_q_s *tx_queue = NULL;
+	int ret = 0;
+	phys_addr_t veth_address = 0;
+
+	ret = bma_intf_get_map_address(TYPE_VETH_ADDR, &veth_address);
+	if (ret != 0)
+		return -EFAULT;
 
 	tx_queue = (struct edma_rxtx_q_s *)
 		   kmalloc(sizeof(struct edma_rxtx_q_s), GFP_KERNEL);
@@ -173,7 +179,7 @@ int edma_veth_setup_all_tx_resources(struct edma_eth_dev_s *edma_eth)
 
 	tx_queue->pdmalbase_v = (struct edma_dmal_s *)
 				(shmq_head + SHMDMAL_OFFSET);
-	tx_queue->pdmalbase_p = (u8 *)(VETH_SHAREPOOL_BASE_INBMC +
+	tx_queue->pdmalbase_p = (u8 *)(veth_address +
 				(MAX_SHAREQUEUE_SIZE * 0) + SHMDMAL_OFFSET);
 
 	memset(tx_queue->pdmalbase_v, 0, MAX_SHMDMAL_SIZE);
@@ -219,6 +225,12 @@ int edma_veth_setup_all_rx_resources(struct edma_eth_dev_s *edma_eth)
 	u8 *shmq_head = NULL;
 	u8 *shmq_head_p = NULL;
 	struct edma_rxtx_q_s *rx_queue = NULL;
+	int ret = 0;
+	phys_addr_t veth_address = 0;
+
+	ret = bma_intf_get_map_address(TYPE_VETH_ADDR, &veth_address);
+	if (ret != 0)
+		return -EFAULT;
 
 	rx_queue = (struct edma_rxtx_q_s *)
 		   kmalloc(sizeof(struct edma_rxtx_q_s), GFP_KERNEL);
@@ -241,7 +253,7 @@ int edma_veth_setup_all_rx_resources(struct edma_eth_dev_s *edma_eth)
 	/* DMA address list (only used in host). */
 	rx_queue->pdmalbase_v = (struct edma_dmal_s *)
 				(shmq_head + SHMDMAL_OFFSET);
-	rx_queue->pdmalbase_p = (u8 *)(VETH_SHAREPOOL_BASE_INBMC +
+	rx_queue->pdmalbase_p = (u8 *)(veth_address +
 				MAX_SHAREQUEUE_SIZE + SHMDMAL_OFFSET);
 	memset(rx_queue->pdmalbase_v, 0, MAX_SHMDMAL_SIZE);
 
@@ -1304,6 +1316,8 @@ int __start_dmalist_H_2(struct edma_rxtx_q_s *prxtx_queue, u32 type, u32 cnt)
 	dma_transfer.type = DMA_LIST;
 	dma_transfer.transfer.list.dma_addr =
 	    (dma_addr_t)prxtx_queue->pdmalbase_p;
+	dma_transfer.pdmalbase_v = (struct bspveth_dmal *)prxtx_queue->pdmalbase_v;
+	dma_transfer.dmal_cnt = prxtx_queue->dmal_cnt;
 
 	ret = bma_intf_start_dma(g_eth_edmaprivate.edma_priv, &dma_transfer);
 	LOG(DLOG_DEBUG, "after -> %u/%u/%u/%u, ret: %d",

drivers/net/ethernet/huawei/bma/cdev_veth_drv/virtual_cdev_eth_net.h
@@ -56,7 +56,6 @@
 #define BSP_ERR_AGAIN                  (BSP_ETH_ERR_BASE + 18)
 #define BSP_ERR_NOT_TO_HANDLE          (BSP_ETH_ERR_BASE + 19)
 
-#define VETH_SHAREPOOL_BASE_INBMC  (0x84820000)
 #define VETH_SHAREPOOL_SIZE        (0xdf000)
 #define VETH_SHAREPOOL_OFFSET      (0x10000)
 #define MAX_SHAREQUEUE_SIZE        (0x20000)

drivers/net/ethernet/huawei/bma/edma_drv/Makefile
@@ -1,2 +1,2 @@
 obj-$(CONFIG_BMA) += host_edma_drv.o
-host_edma_drv-y := bma_pci.o bma_devintf.o edma_host.o
+host_edma_drv-y := bma_pci.o bma_devintf.o edma_host.o edma_queue.o

drivers/net/ethernet/huawei/bma/edma_drv/bma_devintf.c
@@ -31,6 +31,18 @@ static struct bma_dev_s *g_bma_dev;
 
 static ATOMIC_NOTIFIER_HEAD(bma_int_notify_list);
 
+static enum pci_type_e g_pci_type = PCI_TYPE_UNKNOWN;
+
+enum pci_type_e get_pci_type(void)
+{
+	return g_pci_type;
+}
+
+void set_pci_type(enum pci_type_e type)
+{
+	g_pci_type = type;
+}
+
 static int bma_priv_insert_priv_list(struct bma_priv_data_s *priv, u32 type,
 				     u32 sub_type)
 {
@@ -342,6 +354,82 @@ int bma_intf_unregister_type(void **handle)
 }
 EXPORT_SYMBOL(bma_intf_unregister_type);
 
+int bma_intf_get_host_number(unsigned int *host_number)
+{
+	unsigned int devfn = 0;
+
+	if (!host_number)
+		return -EFAULT;
+
+	if (!g_bma_dev) {
+		BMA_LOG(DLOG_ERROR, "g_bma_dev is NULL\n");
+		return -ENXIO;
+	}
+
+	devfn = g_bma_dev->bma_pci_dev->pdev->devfn;
+	BMA_LOG(DLOG_DEBUG, "devfn is %u\n", devfn);
+	if (devfn == PF7 || devfn == PF10) {
+		*host_number = HOST_NUMBER_0;
+	} else if (devfn == PF4) {
+		*host_number = HOST_NUMBER_1;
+	} else {
+		BMA_LOG(DLOG_DEBUG, "Treat as host0 because of unknown PF %u\n", devfn);
+		*host_number = HOST_NUMBER_0;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(bma_intf_get_host_number);
+
+int bma_intf_get_map_address(enum addr_type type, phys_addr_t *addr)
+{
+	u32 host_number = 0;
+	u32 devfn = 0;
+	u32 i = 0;
+	enum pci_type_e pci_type = get_pci_type();
+	struct bma_pci_dev_s *bma_pci_dev = get_bma_pci_dev();
+
+	static struct bma_map_addr_s addr_info[] = {
+		{PCI_TYPE_UNKNOWN,  HOST_NUMBER_0, TYPE_EDMA_ADDR, EDMA_1711_HOST0_ADDR},
+		{PCI_TYPE_UNKNOWN,  HOST_NUMBER_0, TYPE_VETH_ADDR, VETH_1711_HOST0_ADDR},
+		{PCI_TYPE_171x,	 HOST_NUMBER_0, TYPE_EDMA_ADDR, EDMA_1711_HOST0_ADDR},
+		{PCI_TYPE_171x,	 HOST_NUMBER_0, TYPE_VETH_ADDR, VETH_1711_HOST0_ADDR},
+		{PCI_TYPE_1712,	 HOST_NUMBER_0, TYPE_EDMA_ADDR, EDMA_1712_HOST0_ADDR},
+		{PCI_TYPE_1712,	 HOST_NUMBER_0, TYPE_VETH_ADDR, VETH_1712_HOST0_ADDR},
+		{PCI_TYPE_1712,	 HOST_NUMBER_1, TYPE_EDMA_ADDR, EDMA_1712_HOST1_ADDR},
+		{PCI_TYPE_1712,	 HOST_NUMBER_1, TYPE_VETH_ADDR, VETH_1712_HOST1_ADDR},
+	};
+
+	if (!bma_pci_dev) {
+		BMA_LOG(DLOG_ERROR, "bma_pci_dev is null\n");
+		return -EFAULT;
+	}
+
+	devfn = bma_pci_dev->pdev->devfn;
+	if (devfn == PF7 || devfn == PF10) {
+		host_number = HOST_NUMBER_0;
+	} else if (devfn == PF4) {
+		host_number = HOST_NUMBER_1;
+	} else {
+		BMA_LOG(DLOG_DEBUG, "Treat as host0 because of unknown PF %u\n", devfn);
+		host_number = HOST_NUMBER_0;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(addr_info); i++) {
+		if (pci_type == addr_info[i].pci_type &&
+		    host_number == addr_info[i].host_number && type == addr_info[i].addr_type) {
+			*addr = addr_info[i].addr;
+			return 0;
+		}
+	}
+
+	BMA_LOG(DLOG_DEBUG,
+		"Cannot find proper map address! pci_type: %u, host_number: %u, addr_type: %u\n",
+		pci_type, host_number, type);
+	return -EFAULT;
+}
+EXPORT_SYMBOL(bma_intf_get_map_address);
+
 int bma_intf_check_edma_supported(void)
 {
 	return !(!g_bma_dev);
@@ -350,13 +438,30 @@ EXPORT_SYMBOL(bma_intf_check_edma_supported);
 
 int bma_intf_check_dma_status(enum dma_direction_e dir)
 {
-	return edma_host_check_dma_status(dir);
+	enum pci_type_e pci_type = get_pci_type();
+
+	if (pci_type == PCI_TYPE_UNKNOWN) {
+		BMA_LOG(DLOG_ERROR, "pci type is UNKNOWN.\n");
+		return -EFAULT;
+	}
+
+	return get_bma_pci_dev_handler_s()[pci_type].check_dma(dir);
 }
 EXPORT_SYMBOL(bma_intf_check_dma_status);
 
 void bma_intf_reset_dma(enum dma_direction_e dir)
 {
-	edma_host_reset_dma(&g_bma_dev->edma_host, dir);
+	enum pci_type_e pci_type = get_pci_type();
+
+	if (!g_bma_dev)
+		return;
+
+	if (pci_type == PCI_TYPE_UNKNOWN) {
+		BMA_LOG(DLOG_ERROR, "pci type is UNKNOWN.\n");
+		return;
+	}
+
+	get_bma_pci_dev_handler_s()[pci_type].reset_dma(&g_bma_dev->edma_host, dir);
 }
 EXPORT_SYMBOL(bma_intf_reset_dma);
 
@@ -375,9 +480,15 @@ int bma_intf_start_dma(void *handle, struct bma_dma_transfer_s *dma_transfer)
 {
 	int ret = 0;
 	struct bma_priv_data_s *priv = (struct bma_priv_data_s *)handle;
+	enum pci_type_e pci_type = get_pci_type();
 
 	if (!handle || !dma_transfer)
 		return -EFAULT;
+
+	if (pci_type == PCI_TYPE_UNKNOWN) {
+		BMA_LOG(DLOG_ERROR, "pci type is UNKNOWN.\n");
+		return -EFAULT;
+	}
 
 	ret = edma_host_dma_start(&g_bma_dev->edma_host, priv);
 	if (ret) {
@@ -386,7 +497,8 @@ int bma_intf_start_dma(void *handle, struct bma_dma_transfer_s *dma_transfer)
 		return ret;
 	}
 
-	ret = edma_host_dma_transfer(&g_bma_dev->edma_host, priv, dma_transfer);
+	ret = get_bma_pci_dev_handler_s()[pci_type].transfer_edma_host(&g_bma_dev->edma_host, priv,
+								       dma_transfer);
 	if (ret)
 		BMA_LOG(DLOG_ERROR,
 			"edma_host_dma_transfer failed! ret = %d\n", ret);

commit 56e1767fa109546cd789662fbdc75d06f7c6b2c0

iBMA: Fix veth soft lockup by adding DMA-pre-readl sync
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICUK85
CVE: NA

-----------------------------------------

Fix soft lockup during large file transfers over veth interfaces in
Taishan 500(Model 2280) environments by adding a readl operation prior to
DMA initiation. This guarantees proper synchronization of Send Queue buffer
descriptors, preventing descriptor corruption that caused system hangs.

Fixes: 85502a982830 ("Huawei iBMA: Added support for Hi1712 Chip")
Signed-off-by: Huangjunhua <huangjunhua14@huawei.com>

drivers/net/ethernet/huawei/bma/edma_drv/edma_queue.c
@@ -406,6 +406,12 @@ static s32 submit_dma_queue_sq(u32 dir, struct bspveth_dmal pdmalbase_v, u32 pf)
 	BMA_LOG(DLOG_DEBUG, "submit dma queue sq, sq_tail change %d,\n", sq_tail);
 	wmb(); /* memory barriers. */
 
+	/* readl last u32 of sq buffer descriptor to confirm the sq buffer descriptor
+	 * write to local sq
+	 */
+	(void)readl((void __iomem *)(p_dma_sq + sq_tail + sizeof(struct dma_ch_sq_s)
+				     - sizeof(u32)));
+
 	(void)set_dma_queue_sq_tail(sq_tail);
 
 	return 0;