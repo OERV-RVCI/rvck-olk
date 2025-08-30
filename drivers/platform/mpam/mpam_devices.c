OERV-RVCI/rvck-olk

commit ef5e3cfd0c6a0f88c722a8ac6fcea6872c5542cb

arm64/mpam: Support partial-core boot for MPAM
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICRZPF

--------------------------------

L2 shares the same power domain with its associated CPUs. Therefore, in
scenarios where only partial cores power up, the L2 MSCs belonging to the
un-powered cores should not access, otherwise bus-access fault would occur.

During initialization of class->props, skip any L2 MSC that is not powered,
so that ensure the class->props member unaffected from uninitialized
ris->props in mpam_enable_merge_features().

Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_devices.c
@@ -1529,8 +1529,11 @@ static int mpam_discovery_cpu_online(unsigned int cpu)
 
 	mutex_lock(&mpam_list_lock);
 	list_for_each_entry(msc, &mpam_all_msc, glbl_list) {
-		if (!cpumask_test_cpu(cpu, &msc->accessibility))
+		if (!cpumask_test_cpu(cpu, &msc->accessibility)) {
+			msc->probed = true;
+			msc->skipped = true;
 			continue;
+		}
 
 		mutex_lock(&msc->lock);
 		if (!msc->probed)
@@ -1946,14 +1949,14 @@ static void mpam_enable_init_class_features(struct mpam_class *class)
 	struct mpam_msc_ris *ris;
 	struct mpam_component *comp;
 
-	comp = list_first_entry_or_null(&class->components,
-					struct mpam_component, class_list);
-	if (WARN_ON(!comp))
-		return;
+	list_for_each_entry(comp, &class->components, class_list) {
+		list_for_each_entry(ris, &comp->ris, comp_list) {
+			if (!ris->msc->skipped)
+				break;
+		}
+	}
 
-	ris = list_first_entry_or_null(&comp->ris,
-				       struct mpam_msc_ris, comp_list);
-	if (WARN_ON(!ris))
+	if (WARN_ON(!comp) || WARN_ON(!ris))
 		return;
 
 	class->props = ris->props;
@@ -1973,6 +1976,9 @@ static void mpam_enable_merge_features(void)
 
 		list_for_each_entry(comp, &class->components, class_list) {
 			list_for_each_entry(ris, &comp->ris, comp_list) {
+				if (ris->msc->skipped)
+					continue;
+
 				__resource_props_mismatch(ris, class);
 
 				class->nrdy_usec = max(class->nrdy_usec,

drivers/platform/mpam/mpam_internal.h
@@ -55,6 +55,7 @@ struct mpam_msc
 
 	struct mutex		lock;
 	bool			probed;
+	bool			skipped;
 	bool			error_irq_requested;
 	bool			error_irq_hw_enabled;
 	u16			partid_max;

commit 2b7f2328bfbb02eeec51e1d7523b45ff7fe36a0d

arm64/mpam: Fix the logic that determines whether an MPAM MSC should initialize
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICRZPF

--------------------------------

In non-full core boot scenarios, L2 MSCs corresponding to offline CPUs
should skip. The former logic used a boolean skipped flag to mark an MSC
skipped, but this caused mpam_enable() to wrongly ignore MSCs that
actually required initialization.

This fix patch removes the skipped flag and instead relies on cpu_mask:
if the MSC's accessibility mask contains no online CPUs, the MSC is left
uninitialized.

Fixes: ef5e3cfd0c6a ("arm64/mpam: Support partial-core boot for MPAM")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_devices.c
@@ -1529,11 +1529,8 @@ static int mpam_discovery_cpu_online(unsigned int cpu)
 
 	mutex_lock(&mpam_list_lock);
 	list_for_each_entry(msc, &mpam_all_msc, glbl_list) {
-		if (!cpumask_test_cpu(cpu, &msc->accessibility)) {
-			msc->probed = true;
-			msc->skipped = true;
+		if (!cpumask_test_cpu(cpu, &msc->accessibility))
 			continue;
-		}
 
 		mutex_lock(&msc->lock);
 		if (!msc->probed)
@@ -1951,7 +1948,7 @@ static void mpam_enable_init_class_features(struct mpam_class *class)
 
 	list_for_each_entry(comp, &class->components, class_list) {
 		list_for_each_entry(ris, &comp->ris, comp_list) {
-			if (!ris->msc->skipped)
+			if (ris->msc->probed)
 				break;
 		}
 	}
@@ -1976,7 +1973,7 @@ static void mpam_enable_merge_features(void)
 
 		list_for_each_entry(comp, &class->components, class_list) {
 			list_for_each_entry(ris, &comp->ris, comp_list) {
-				if (ris->msc->skipped)
+				if (!ris->msc->probed)
 					continue;
 
 				__resource_props_mismatch(ris, class);
@@ -2352,6 +2349,7 @@ void mpam_disable(struct work_struct *ignored)
  */
 void mpam_enable(struct work_struct *work)
 {
+	cpumask_t mask;
 	static atomic_t once;
 	struct mpam_msc *msc;
 	bool all_devices_probed = true;
@@ -2359,8 +2357,11 @@ void mpam_enable(struct work_struct *work)
 	mutex_lock(&mpam_list_lock);
 	list_for_each_entry(msc, &mpam_all_msc, glbl_list) {
 		mutex_lock(&msc->lock);
-		if (!msc->probed)
-			all_devices_probed = false;
+		if (!msc->probed) {
+			cpumask_and(&mask, &msc->accessibility, cpu_online_mask);
+			if (!cpumask_empty(&mask))
+				all_devices_probed = false;
+		}
 		mutex_unlock(&msc->lock);
 
 		if (!all_devices_probed)

drivers/platform/mpam/mpam_internal.h
@@ -55,7 +55,6 @@ struct mpam_msc
 
 	struct mutex		lock;
 	bool			probed;
-	bool			skipped;
 	bool			error_irq_requested;
 	bool			error_irq_hw_enabled;
 	u16			partid_max;