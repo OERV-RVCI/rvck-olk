OERV-RVCI/rvck-olk

commit da5038956e6561ef9d61d2f461a97b60699694f8

fs/resctrl: L2_MON does not support the limbo mechanism
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IC30P1

--------------------------------

Currently, only L3_MON supports the limbo mechanism, while L2_MON does not.
Therefore, during initialization, avoid incorrect initialization of the
resctrl_rmid_realloc_limit and resctrl_rmid_realloc_threshold variables by
L2_MON.

Similarly, since L2_MON does not support read/write operations on the
max_threshold_occupancy interface, and directly returns.

Fixes: 556688623b2b ("fs/resctrl: Create l2 cache monitors")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

fs/resctrl/rdtgroup.c
@@ -1304,6 +1304,11 @@ static int rdt_delay_linear_show(struct kernfs_open_file *of,
 static int max_threshold_occ_show(struct kernfs_open_file *of,
 				  struct seq_file *seq, void *v)
 {
+	struct rdt_resource *r = of->kn->parent->priv;
+
+	if (r->cache_level != 3)
+		return 0;
+
 	seq_printf(seq, "%u\n", resctrl_rmid_realloc_threshold);
 
 	return 0;
@@ -1326,8 +1331,12 @@ static int rdt_thread_throttle_mode_show(struct kernfs_open_file *of,
 static ssize_t max_threshold_occ_write(struct kernfs_open_file *of,
 				       char *buf, size_t nbytes, loff_t off)
 {
+	struct rdt_resource *r = of->kn->parent->priv;
 	unsigned int bytes;
 	int ret;
+
+	if (r->cache_level != 3)
+		return 0;
 
 	ret = kstrtouint(buf, 0, &bytes);
 	if (ret)

drivers/platform/mpam/mpam_resctrl.c
@@ -738,8 +738,10 @@ static void mpam_resctrl_pick_caches(void)
 			continue;
 		}
 
-		if (mpam_has_feature(mpam_feat_msmon_csu, cprops))
-			update_rmid_limits(cache_size);
+		if (mpam_has_feature(mpam_feat_msmon_csu, cprops)) {
+			if (class->level == 3)
+				update_rmid_limits(cache_size);
+		}
 
 		if (has_cpor) {
 			if (class->level == 2) {

commit 854b802bf3e636c0f7012c835d5e12b3325173f7

fs/resctrl: Fix max_threshold_occupancy write operation
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICVT69

--------------------------------

Since L2_MON does not support the limbo mechanism, the
max_threshold_occupancy interface does not allow updates. If a write
operation is attempted, skip it simply and return nbytes.

Fixes: da5038956e65 ("fs/resctrl: L2_MON does not support the limbo mechanism")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

fs/resctrl/rdtgroup.c
@@ -1336,7 +1336,7 @@ static ssize_t max_threshold_occ_write(struct kernfs_open_file *of,
 	int ret;
 
 	if (r->cache_level != 3)
-		return 0;
+		goto out;
 
 	ret = kstrtouint(buf, 0, &bytes);
 	if (ret)
@@ -1347,6 +1347,7 @@ static ssize_t max_threshold_occ_write(struct kernfs_open_file *of,
 
 	resctrl_rmid_realloc_threshold = resctrl_arch_round_mon_val(bytes);
 
+out:
 	return nbytes;
 }
 