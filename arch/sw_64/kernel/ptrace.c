OERV-RVCI/rvck-olk

commit 6361501a23c3f000596e2ba2bdadd58285926c87

sw64: ptrace: return -EIO in invalid PEEKUSR/POKEUSR requests
Sunway inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICR460

--------------------------------

When PTRACE_PEEKUSR or PTRACE_POKEUSR operations are called with an
out-of-bounds or unaligned address, the system should return -EIO
instead of 0. This commit fixes the issue.

Signed-off-by: Gu Yuchen <guyuchen@wxiat.com>
Reviewed-by: He Sheng <hesheng@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>

arch/sw_64/kernel/ptrace.c
@@ -127,7 +127,9 @@ get_reg_addr(struct task_struct *task, unsigned long regno)
 unsigned long
 get_reg(struct task_struct *task, unsigned long regno)
 {
-	return *get_reg_addr(task, regno);
+	unsigned long addr = *get_reg_addr(task, regno);
+
+	return (addr) ? addr : -EIO;
 }
 
 /*
@@ -136,8 +138,9 @@ get_reg(struct task_struct *task, unsigned long regno)
 static int
 put_reg(struct task_struct *task, unsigned long regno, unsigned long data)
 {
-	*get_reg_addr(task, regno) = data;
-	return 0;
+	unsigned long addr = *get_reg_addr(task, regno);
+
+	return (addr) ? 0 : -EIO;
 }
 
 static inline int

commit e0e53b9684631ff507446efa74a1cb520e3961f7

sw64: ptrace: fix a bug in return value handling in PEEKUSR AND POKEUSR
Sunway inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICVT8Z

--------------------------------

According to commit 6361501a23c3 ("sw64: ptrace: return -EIO in invalid
PEEKUSR/POKEUSR requests"), it incorrectly checked the value stored in
the register returned by get_reg_addr() to determine whether the
register address is valid, instead of checking for error codes like
-EIO. Additionally, put_reg and get_reg have the same implementations.
These two issues lead to problems in the usage of get_reg and put_reg.

Fixes: 6361501a23c3 ("sw64: ptrace: return -EIO in invalid PEEKUSR/POKEUSR requests")

Signed-off-by: Gu Yuchen <guyuchen@wxiat.com>
Reviewed-by: He Sheng <hesheng@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>

arch/sw_64/kernel/ptrace.c
@@ -62,8 +62,6 @@ static int pcboff[] = {
 	[PT_IDA_MASK] = PCB_OFF(ida_mask)
 };
 
-static unsigned long zero;
-
 /*
  * Get address of register REGNO in task TASK.
  */
@@ -96,14 +94,6 @@ get_reg_addr(struct task_struct *task, unsigned long regno)
 		addr = &task->thread.fpstate.fp[fno].v[0];
 		break;
 	case PT_VECREG_BASE ... PT_VECREG_END:
-		/*
-		 * return addr for zero value if we catch vectors of f31
-		 * v0 and v3 of f31 are not in this range so ignore them
-		 */
-		if (regno == PT_F31_V1 || regno == PT_F31_V2) {
-			addr = &zero;
-			break;
-		}
 		fno = (regno - PT_VECREG_BASE) & 0x1f;
 		vno = 1 + ((regno - PT_VECREG_BASE) >> 5);
 		addr = &task->thread.fpstate.fp[fno].v[vno];
@@ -115,7 +105,7 @@ get_reg_addr(struct task_struct *task, unsigned long regno)
 		addr = (void *)task_pt_regs(task) + PT_REGS_PC;
 		break;
 	default:
-		addr = &zero;
+		return ERR_PTR(-EIO);
 	}
 
 	return addr;
@@ -127,9 +117,17 @@ get_reg_addr(struct task_struct *task, unsigned long regno)
 unsigned long
 get_reg(struct task_struct *task, unsigned long regno)
 {
-	unsigned long addr = *get_reg_addr(task, regno);
+	unsigned long *addr;
 
-	return (addr) ? addr : -EIO;
+	/*
+	 * return zero value if we catch vectors of f31
+	 * v0 and v3 of f31 are not in this range so ignore them
+	 */
+	if (regno == PT_F31_V1 || regno == PT_F31_V2)
+		return 0;
+
+	addr = get_reg_addr(task, regno);
+	return (unlikely(IS_ERR(addr))) ? PTR_ERR(addr) : *addr;
 }
 
 /*
@@ -138,9 +136,18 @@ get_reg(struct task_struct *task, unsigned long regno)
 static int
 put_reg(struct task_struct *task, unsigned long regno, unsigned long data)
 {
-	unsigned long addr = *get_reg_addr(task, regno);
+	unsigned long *addr;
 
-	return (addr) ? 0 : -EIO;
+	if (regno == PT_F31_V1 || regno == PT_F31_V2)
+		return 0;
+
+	addr = get_reg_addr(task, regno);
+	if (unlikely(IS_ERR(addr)))
+		return PTR_ERR(addr);
+	else {
+		*addr = data;
+		return 0;
+	}
 }
 
 static inline int