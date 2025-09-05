OERV-RVCI/rvck-olk

commit e6faa11b29056bfdd959b913c1da731f3f8f5770

nfs: use dfprintk() to debug enfs
kylin inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICOGXU

--------------------------------

Add NFSDBG_ENFS to support debugging enfs via dfprintk().
Use the following command to debug enfs module:

  echo 0x10000 >  /proc/sys/sunrpc/nfs_debug

Signed-off-by: ChenXiaoSong <chenxiaosong@kylinos.cn>

fs/nfs/enfs/enfs_log.h
@@ -6,20 +6,15 @@
 #ifndef ENFS_LOG_H
 #define ENFS_LOG_H
 
-#include <linux/printk.h>
-
-extern unsigned int enfs_debug;
+#include <linux/nfs_fs.h>
 
 #define enfs_log_info(fmt, ...) \
 	pr_info("enfs:[%s]" pr_fmt(fmt), __func__, ##__VA_ARGS__)
 #define enfs_log_error(fmt, ...) \
 	pr_err("enfs:[%s]" pr_fmt(fmt), __func__, ##__VA_ARGS__)
-#define enfs_log_debug(fmt, ...)                                   \
-	do {                                                       \
-		if (enfs_debug != 0) {                             \
-			pr_info("enfs:[%s]" pr_fmt(fmt), __func__, \
-				##__VA_ARGS__);                    \
-		}                                                  \
-	} while (0)
+#define enfs_log_debug(fmt, ...)						\
+do {										\
+	dfprintk(ENFS, "enfs:[%s] " pr_fmt(fmt), __func__, ##__VA_ARGS__);	\
+} while (0)									\
 
 #endif // ENFS_ERRCODE_H

fs/nfs/enfs/shard_route.c
@@ -3,6 +3,7 @@
  * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
  */
 #include <linux/kthread.h>
+#include <linux/lockd/lockd.h>
 #include <linux/nfs3.h>
 #include <linux/nfs_fs.h>
 #include <linux/nfs_xdr.h>
@@ -11,7 +12,6 @@
 #include <linux/sunrpc/types.h>
 #include <linux/sunrpc/xprt.h>
 #include <linux/moduleparam.h>
-#include <linux/lockd/lockd.h>
 #include "../../../net/sunrpc/netns.h"
 #include "../../../fs/nfs/nfs4_fs.h"
 #include "../../../fs/nfs/netns.h"

fs/nfs/enfs/enfs_init.c
@@ -23,10 +23,6 @@
 #include "dns_internal.h"
 #include "shard.h"
 
-unsigned int enfs_debug;
-module_param_named(enfs_debug, enfs_debug, uint, 0600);
-MODULE_PARM_DESC(enfs_debug, "enfs debugging mask");
-
 static struct enfs_adapter_ops enfs_adapter = {
 	.name = "enfs",
 	.owner = THIS_MODULE,

fs/nfs/enfs/enfs_proc.c
@@ -107,7 +107,7 @@ static int debug_show_clnt(struct rpc_clnt *clnt, void *data)
 
 static void debug_print_all_xprt(void)
 {
-	if (enfs_debug != 0)
+	ifdebug(ENFS)
 		enfs_iter_rpc_clnt(debug_show_clnt, NULL);
 }
 

include/uapi/linux/nfs_fs.h
@@ -57,7 +57,8 @@
 #define NFSDBG_PNFS_LD		0x2000
 #define NFSDBG_STATE		0x4000
 #define NFSDBG_XATTRCACHE	0x8000
-#define NFSDBG_ALL		0xFFFF
+#define NFSDBG_ENFS		0x10000
+#define NFSDBG_ALL		0xFFFFF
 
 
 #endif /* _UAPI_LINUX_NFS_FS_H */

commit 0fbb6e383e6818e7f24f90388d061f020f9640b0

nfs/enfs: introduce is_enfs_debug()
kylin inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICWD0V

--------------------------------

ifdebug(ENFS) is true when enfs debug if enabled.

Fixes: e6faa11b2905 ("nfs: use dfprintk() to debug enfs")
Signed-off-by: ChenXiaoSong <chenxiaosong@kylinos.cn>

fs/nfs/enfs/enfs_log.h
@@ -8,6 +8,13 @@
 
 #include <linux/nfs_fs.h>
 
+static inline bool is_enfs_debug(void)
+{
+	ifdebug(ENFS)
+		return true;
+	return false;
+}
+
 #define enfs_log_info(fmt, ...) \
 	pr_info("enfs:[%s]" pr_fmt(fmt), __func__, ##__VA_ARGS__)
 #define enfs_log_error(fmt, ...) \

fs/nfs/enfs/shard_route.c
@@ -681,7 +681,7 @@ void enfs_print_uuid(struct enfs_file_uuid *file_uuid)
 	char buf[80];		/* 80 uuid buf */
 	uint8_t *uuid = file_uuid->data;
 
-	ifdebug(ENFS)
+	if (!is_enfs_debug())
 		return;
 
 	enfs_log_debug("dev:%llu fs:%u dtree:%u snap:%u pfid:%llu fid:%llu\n",