OERV-RVCI/rvck-olk

commit ae6bcabac7158992c9f0723df8bdb11ba7f2d579

arm64/mpam: Fix num_rmids information
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I8T2RT

--------------------------------

Select the maximum number of RMIDs supported by the system as the RMIDs
number.

Fixes: cc622cf0fb21 ("arm_mpam: resctrl: Pick a value for num_rmid")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -1095,7 +1095,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		 * For mpam, each control group has its own pmg/rmid
 		 * space.
 		 */
-		r->num_rmid = mpam_partid_max * mpam_pmg_max;
+		r->num_rmid = resctrl_arch_system_num_rmid_idx();
 	}
 
 	return 0;

commit d58b0f4a5cfe2f3d03b11a8cd5b8d00930ca2840

arm64/mpam: Half the number of RMIDs for all resources under CDP mode
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICVT5V

--------------------------------

Under the ARM64 MPAM architecture, once CDP mode is enabled, the number
of RMIDs for all MSC resources is halved indeed, not just those belonging
to L2 and L3 MSCs.

Fixes: ae6bcabac715 ("arm64/mpam: Fix num_rmids information")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -101,16 +101,20 @@ bool resctrl_arch_get_cdp_enabled(enum resctrl_res_level rid)
 	}
 }
 
-int resctrl_arch_set_cdp_enabled(enum resctrl_res_level rid, bool enable)
+int resctrl_arch_set_cdp_enabled(enum resctrl_res_level ignored, bool enable)
 {
 	u64 regval;
 	struct rdt_resource *r;
-	u32 partid, partid_i, partid_d;
+	u32 i, partid, partid_i, partid_d;
 
-	r = resctrl_arch_get_resource(rid);
-	r->num_rmid = resctrl_arch_system_num_rmid_idx();
-	if (enable)
-		r->num_rmid >>= 1;
+	for (i = 0; i < RDT_NUM_RESOURCES; i++) {
+		r = resctrl_arch_get_resource(i);
+		if (r->mon_capable) {
+			r->num_rmid = resctrl_arch_system_num_rmid_idx();
+			if (enable)
+				r->num_rmid >>= 1;
+		}
+	}
 
 	cdp_enabled = enable;
 

include/linux/arm_mpam.h
@@ -74,7 +74,7 @@ bool resctrl_arch_would_mbm_overflow(void);
 void resctrl_arch_reset_resources(void);
 
 bool resctrl_arch_get_cdp_enabled(enum resctrl_res_level ignored);
-int resctrl_arch_set_cdp_enabled(enum resctrl_res_level rid, bool enable);
+int resctrl_arch_set_cdp_enabled(enum resctrl_res_level ignored, bool enable);
 bool resctrl_arch_hide_cdp(enum resctrl_res_level rid);
 bool resctrl_arch_match_closid(struct task_struct *tsk, u32 closid);
 bool resctrl_arch_match_rmid(struct task_struct *tsk, u32 closid, u32 rmid);