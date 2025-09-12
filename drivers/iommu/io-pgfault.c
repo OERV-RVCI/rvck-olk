openeuler-mirror/raspberrypi-kernel

commit 6b74104e5aa19a548b997fc70ee595bdca5c608f

iommu: Fix kabi breakage due to "iommufd: Add iommufd fault object"
virt inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IB4WDJ
CVE: NA

--------------------------------

Fix kabi breakage due to "iommufd: Add iommufd fault object".

Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>

drivers/iommu/io-pgfault.c
@@ -54,8 +54,11 @@ static void __iopf_free_group(struct iopf_group *group)
 
 void iopf_free_group(struct iopf_group *group)
 {
+	struct iopf_group_extend *group_extend;
+
 	__iopf_free_group(group);
-	kfree(group);
+	group_extend = container_of(group, struct iopf_group_extend, iopf_group);
+	kfree(group_extend);
 }
 EXPORT_SYMBOL_GPL(iopf_free_group);
 
@@ -84,15 +87,17 @@ static struct iopf_group *iopf_group_alloc(struct iommu_fault_param *iopf_param,
 {
 	struct iopf_fault *iopf, *next;
 	struct iopf_group *group;
+	struct iopf_group_extend *group_extend;
 
-	group = kzalloc(sizeof(*group), GFP_KERNEL);
-	if (!group) {
+	group_extend = kzalloc(sizeof(*group_extend), GFP_KERNEL);
+	if (!group_extend) {
 		/*
 		 * We always need to construct the group as we need it to abort
 		 * the request at the driver if it can't be handled.
 		 */
 		group = abort_group;
 	}
+	group = &group_extend->iopf_group;
 
 	group->fault_param = iopf_param;
 	group->last_fault.fault = evt->fault;

drivers/iommu/iommufd/fault.c
@@ -18,7 +18,7 @@
 void iommufd_fault_destroy(struct iommufd_object *obj)
 {
 	struct iommufd_fault *fault = container_of(obj, struct iommufd_fault, obj);
-	struct iopf_group *group, *next;
+	struct iopf_group_extend *group_extend, *next;
 
 	/*
 	 * The iommufd object's reference count is zero at this point.
@@ -26,10 +26,10 @@ void iommufd_fault_destroy(struct iommufd_object *obj)
 	 * accessing this pointer. Therefore, acquiring the mutex here
 	 * is unnecessary.
 	 */
-	list_for_each_entry_safe(group, next, &fault->deliver, node) {
-		list_del(&group->node);
-		iopf_group_response(group, IOMMU_PAGE_RESP_INVALID);
-		iopf_free_group(group);
+	list_for_each_entry_safe(group_extend, next, &fault->deliver, node) {
+		list_del(&group_extend->node);
+		iopf_group_response(&group_extend->iopf_group, IOMMU_PAGE_RESP_INVALID);
+		iopf_free_group(&group_extend->iopf_group);
 	}
 }
 
@@ -56,6 +56,7 @@ static ssize_t iommufd_fault_fops_read(struct file *filep, char __user *buf,
 	struct iommu_hwpt_pgfault data;
 	struct iommufd_device *idev;
 	struct iopf_group *group;
+	struct iopf_group_extend *group_extend;
 	struct iopf_fault *iopf;
 	size_t done = 0;
 	int rc = 0;
@@ -65,8 +66,9 @@ static ssize_t iommufd_fault_fops_read(struct file *filep, char __user *buf,
 
 	mutex_lock(&fault->mutex);
 	while (!list_empty(&fault->deliver) && count > done) {
-		group = list_first_entry(&fault->deliver,
-					 struct iopf_group, node);
+		group_extend = list_first_entry(&fault->deliver,
+						struct iopf_group_extend, node);
+		group = &group_extend->iopf_group;
 
 		if (group->fault_count * fault_size > count - done)
 			break;
@@ -89,7 +91,7 @@ static ssize_t iommufd_fault_fops_read(struct file *filep, char __user *buf,
 			done += fault_size;
 		}
 
-		list_del(&group->node);
+		list_del(&group_extend->node);
 	}
 	mutex_unlock(&fault->mutex);
 

include/linux/iommu.h
@@ -141,7 +141,6 @@ struct iopf_fault {
 struct iopf_group {
 	struct iopf_fault last_fault;
 	struct list_head faults;
-	size_t fault_count;
 	/* list node for iommu_fault_param::faults */
 	struct list_head pending_node;
 	struct work_struct work;
@@ -149,9 +148,17 @@ struct iopf_group {
 		     struct iommu_attach_handle *attach_handle)
 	/* The device's fault data parameter. */
 	struct iommu_fault_param *fault_param;
+
+	KABI_USE(1, size_t fault_count)
+	KABI_USE(2, u32 cookie)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
+};
+
+struct iopf_group_extend {
+	struct iopf_group iopf_group;
 	/* Used by handler provider to hook the group on its own lists. */
 	struct list_head node;
-	u32 cookie;
 
 	KABI_RESERVE(1)
 	KABI_RESERVE(2)

OERV-RVCI/rvck-olk

commit d8cadc87f50de3106f9462439ebb05d4b0499916

iommu: Fix the incorrect exception process of the failed memory allocation
virt inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICXPUC?from=project-issue
CVE: NA

--------------------------------

Fix the incorrect exception process of the failed memory allocation.

Fixes: 6b74104e5aa1 ("iommu: Fix kabi breakage due to "iommufd: Add iommufd fault object"")
Signed-off-by: Yihua Jin <jinyihua@huawei.com>

drivers/iommu/io-pgfault.c
@@ -96,8 +96,9 @@ static struct iopf_group *iopf_group_alloc(struct iommu_fault_param *iopf_param,
 		 * the request at the driver if it can't be handled.
 		 */
 		group = abort_group;
+	} else {
+		group = &group_extend->iopf_group;
 	}
-	group = &group_extend->iopf_group;
 
 	group->fault_param = iopf_param;
 	group->last_fault.fault = evt->fault;