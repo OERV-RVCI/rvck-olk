OERV-RVCI/rvck-olk

commit befbde867291b5bb068a97b17086ad20484b6068

VirtCCA: Adapt virtcca operations by cca_base.
virtcca inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IBY08N

--------------------------------

Refactor the code of virtCCA to utilize the code from cca_base as much
as possible. Let virtCCA and CCA share the common components vput_is_rec
and kvm_is_realm, while retaining vcpu_is_tec as a virtCCA-specific
identifier to distinguish scenarios where code sharing is
not feasible.

Signed-off-by: He Jingxian <hejingxian@huawei.com>
Signed-off-by: Yang Xiangkai <yangxiangkai@huawei.com>
Signed-off-by: Xu Raoqing <xuraoqing@huawei.com>

arch/arm64/kvm/guest.c
@@ -26,6 +26,7 @@
 #include <asm/kvm_emulate.h>
 #include <asm/kvm_nested.h>
 #include <asm/sigcontext.h>
+#include <asm/kvm_tmi.h>
 
 #include "trace.h"
 
@@ -931,7 +932,7 @@ int __kvm_arm_vcpu_set_events(struct kvm_vcpu *vcpu,
 	bool has_esr = events->exception.serror_has_esr;
 	bool ext_dabt_pending = events->exception.ext_dabt_pending;
 
-	if (vcpu_is_rec(vcpu)) {
+	if (_vcpu_is_rec(vcpu)) {
 		/* Cannot inject SError into a Realm. */
 		if (serror_pending)
 			return -EINVAL;
@@ -954,6 +955,10 @@ int __kvm_arm_vcpu_set_events(struct kvm_vcpu *vcpu,
 
 		return 0;
 	}
+#ifdef CONFIG_HISI_VIRTCCA_HOST
+	if (vcpu_is_tec(vcpu))
+		return kvm_cvm_vcpu_set_events(vcpu, serror_pending, ext_dabt_pending);
+#endif
 
 	if (serror_pending && has_esr) {
 		if (!cpus_have_const_cap(ARM64_HAS_RAS_EXTN))

arch/arm64/kvm/mmu.c
@@ -328,7 +328,7 @@ static void __unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 
 	lockdep_assert_held_write(&kvm->mmu_lock);
 	WARN_ON(size & ~PAGE_MASK);
 
-	if (kvm_is_realm(kvm))
+	if (_kvm_is_realm(kvm))
 		kvm_realm_unmap_range(kvm, start, size, !only_shared);
 	else
 		WARN_ON(stage2_apply_range(mmu, start, end,
@@ -1042,7 +1042,7 @@ void kvm_free_stage2_pgd(struct kvm_s2_mmu *mmu)
 
 	write_lock(&kvm->mmu_lock);
 	pgt = mmu->pgt;
-	if (kvm_is_realm(kvm) &&
+	if (_kvm_is_realm(kvm) &&
 	    (kvm_realm_state(kvm) != REALM_STATE_DEAD &&
 	     kvm_realm_state(kvm) != REALM_STATE_NONE)) {
 		unmap_stage2_range(mmu, 0, (~0ULL) & PAGE_MASK, false);
@@ -1488,6 +1488,8 @@ static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 	 */
 	if (vcpu_is_rec(vcpu))
 		write_fault = true;
+	if (vcpu_is_tec(vcpu))
+		prot = KVM_PGTABLE_PROT_R | KVM_PGTABLE_PROT_W;
 
 	exec_fault = kvm_vcpu_trap_is_exec_fault(vcpu);
 	VM_BUG_ON(write_fault && exec_fault);
@@ -1622,7 +1624,7 @@ static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 	 * that depends on the higher version feature guest memfd.
 	 * Here we should handle protected addresses fault expect protected devices.
 	 */
-	if (device && vcpu_is_rec(vcpu) &&
+	if (device && _vcpu_is_rec(vcpu) &&
 	    kvm_gpa_from_fault(kvm, fault_ipa) == fault_ipa)
 		return -EINVAL;
 

arch/arm64/kvm/sys_regs.c
@@ -1598,8 +1598,9 @@ static int set_id_aa64dfr0_el1(struct kvm_vcpu *vcpu,
 	 * ID_AA64DFR0_EL1.DebugVer, BRPs and WRPs all have to be greater than
 	 * zero. CTX_CMPs is never greater than BRPs.
 	 */
-	if (debugver < ID_AA64DFR0_EL1_DebugVer_IMP || !bps || !wps ||
-	    ctx_cmps > bps)
+	if ((kvm_get_cvm_type() != VIRTCCA_CVM) &&
+	    (debugver < ID_AA64DFR0_EL1_DebugVer_IMP || !bps || !wps ||
+	    ctx_cmps > bps))
 		return -EINVAL;
 
 	return set_id_reg(vcpu, rd, val);

arch/arm64/include/asm/kvm_emulate.h
@@ -693,4 +693,16 @@ static inline bool kvm_arm_rec_finalized(struct kvm_vcpu *vcpu)
 	return vcpu->arch.rec.mpidr != INVALID_HWID;
 }
 
+#ifdef CONFIG_HISI_VIRTCCA_HOST
+
+static inline enum virtcca_cvm_state virtcca_cvm_state(struct kvm *kvm)
+{
+	struct virtcca_cvm *virtcca_cvm = kvm->arch.virtcca_cvm;
+
+	if (!virtcca_cvm)
+		return 0;
+	return READ_ONCE(virtcca_cvm->state);
+}
+#endif
+
 #endif /* __ARM64_KVM_EMULATE_H__ */

arch/arm64/include/asm/kvm_tmi.h
@@ -146,8 +146,8 @@ struct tmi_tec_exit {
 };
 
 struct tmi_tec_run {
-	struct tmi_tec_entry tec_entry;
-	struct tmi_tec_exit tec_exit;
+	struct tmi_tec_entry enter;
+	struct tmi_tec_exit exit;
 };
 
 #define TMI_FNUM_MIN_VALUE	U(0x150)

arch/arm64/include/asm/kvm_tmm.h
@@ -6,6 +6,7 @@
 #define __ASM_KVM_TMM_H
 
 #include <uapi/linux/kvm.h>
+#include <asm/rmi_smc.h>
 
 /*
  * There is a conflict with the internal iova of CVM,
@@ -14,10 +15,10 @@
  * iova is not being used, so it is used as the iova range for msi
  * mapping.
  */
-#define CVM_MSI_ORIG_IOVA	0x8000000
-#define CVM_MSI_MIN_IOVA	0x0a001000
-#define CVM_MSI_MAX_IOVA	0x0b000000
-#define CVM_MSI_IOVA_OFFSET	0x1000
+#define CVM_MSI_ORIG_IOVA      0x8000000
+#define CVM_MSI_MIN_IOVA       0x0a001000
+#define CVM_MSI_MAX_IOVA       0x0b000000
+#define CVM_MSI_IOVA_OFFSET    0x1000
 
 #define CVM_RW_8_BIT	0x8
 #define CVM_RW_16_BIT	0x10
@@ -101,7 +102,7 @@ struct virtcca_cvm {
 struct virtcca_cvm_tec {
 	u64 tec;
 	bool tec_created;
-	void *tec_run;
+	struct tmi_tec_run *run;
 };
 
 struct cvm_ttt_addr {
@@ -109,7 +110,7 @@ struct cvm_ttt_addr {
 	u64 addr;
 };
 
-int kvm_init_tmm(void);
+void kvm_init_tmm(void);
 int kvm_cvm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap);
 void kvm_destroy_cvm(struct kvm *kvm);
 int kvm_finalize_vcpu_tec(struct kvm_vcpu *vcpu);
@@ -118,7 +119,7 @@ int kvm_tec_enter(struct kvm_vcpu *vcpu);
 int handle_cvm_exit(struct kvm_vcpu *vcpu, int rec_run_status);
 int kvm_arm_create_cvm(struct kvm *kvm);
 void kvm_free_rd(struct kvm *kvm);
-int cvm_psci_complete(struct kvm_vcpu *calling, struct kvm_vcpu *target);
+int cvm_psci_complete(struct kvm_vcpu *calling, struct kvm_vcpu *target, unsigned long status);
 
 void kvm_cvm_unmap_destroy_range(struct kvm *kvm);
 int kvm_cvm_map_range(struct kvm *kvm);

arch/arm64/include/uapi/asm/kvm.h
@@ -421,6 +421,9 @@ enum {
 #define   KVM_DEV_ARM_VGIC_SAVE_PENDING_TABLES	3
 #define   KVM_DEV_ARM_ITS_CTRL_RESET		4
 
+#define KVM_CAP_ARM_RME_MEASUREMENT_ALGO_SHA256		0
+#define KVM_CAP_ARM_RME_MEASUREMENT_ALGO_SHA512		1
+
 /* KVM_CAP_ARM_RME on VM fd */
 #define KVM_CAP_ARM_RME_CONFIG_REALM		0
 #define KVM_CAP_ARM_RME_CREATE_REALM		1

arch/arm64/kvm/Makefile
@@ -25,6 +25,9 @@ kvm-y += arm.o mmu.o mmio.o psci.o hypercalls.o pvtime.o pvsched.o \
 
 kvm-$(CONFIG_VIRT_PLAT_DEV)  += vgic/shadow_dev.o
 kvm-$(CONFIG_HW_PERF_EVENTS)  += pmu-emul.o pmu.o
+kvm-$(CONFIG_HISI_VIRTCCA_HOST) += tmi.o
+kvm-$(CONFIG_HISI_VIRTCCA_HOST) += virtcca_cvm.o
+kvm-$(CONFIG_HISI_VIRTCCA_HOST) += virtcca_cvm_exit.o
 obj-$(CONFIG_KVM_HISI_VIRT) += hisilicon/
 
 always-y := hyp_constants.h hyp-constants.s

commit e75432dba1fc2bcf39297ee2b361c790980209da

arm64 kvm: On the Adaptation of CCA and virtCCA.
community inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICYRLF?from=project-issue

------------------------

Enforce isolation between CCA and virtCCA use _kvm_is_realm to avoid kernel
panic in virtCCA scenarios.

Fixes: befbde867291 ("VirtCCA: Adapt virtcca operations by cca_base.")
Signed-off-by: yxk <yangxiangkai@huawei.com>

arch/arm64/kvm/guest.c
@@ -716,7 +716,7 @@ static unsigned long num_sve_regs(const struct kvm_vcpu *vcpu)
 	if (!vcpu_has_sve(vcpu) || !kvm_arm_vcpu_sve_finalized(vcpu))
 		return 0;
 
-	if (kvm_is_realm(vcpu->kvm))
+	if (_kvm_is_realm(vcpu->kvm))
 		return 1; /* KVM_REG_ARM64_SVE_VLS */
 
 	return slices * (SVE_NUM_PREGS + SVE_NUM_ZREGS + 1 /* FFR */)
@@ -746,7 +746,7 @@ static int copy_sve_reg_indices(const struct kvm_vcpu *vcpu,
 		return -EFAULT;
 	++num_regs;
 
-	if (kvm_is_realm(vcpu->kvm))
+	if (_kvm_is_realm(vcpu->kvm))
 		return num_regs;
 
 	for (i = 0; i < slices; i++) {

arch/arm64/kvm/hypercalls.c
@@ -444,7 +444,7 @@ void kvm_arm_teardown_hypercalls(struct kvm *kvm)
 
 int kvm_arm_get_fw_num_regs(struct kvm_vcpu *vcpu)
 {
-	return kvm_is_realm(vcpu->kvm) ? 0 : ARRAY_SIZE(kvm_arm_fw_reg_ids);
+	return _kvm_is_realm(vcpu->kvm) ? 0 : ARRAY_SIZE(kvm_arm_fw_reg_ids);
 }
 
 int kvm_arm_copy_fw_reg_indices(struct kvm_vcpu *vcpu, u64 __user *uindices)

arch/arm64/kvm/mmu.c
@@ -348,7 +348,7 @@ static void stage2_flush_memslot(struct kvm *kvm,
 	phys_addr_t addr = memslot->base_gfn << PAGE_SHIFT;
 	phys_addr_t end = addr + PAGE_SIZE * memslot->npages;
 
-	if (kvm_is_realm(kvm))
+	if (_kvm_is_realm(kvm))
 		kvm_realm_unmap_range(kvm, addr, end - addr, false);
 	else
 		stage2_apply_range_resched(&kvm->arch.mmu, addr, end,
@@ -1019,7 +1019,7 @@ void stage2_unmap_vm(struct kvm *kvm)
 	int idx, bkt;
 
 	/* For realms this is handled by the RMM so nothing to do here */
-	if (kvm_is_realm(kvm))
+	if (_kvm_is_realm(kvm))
 		return;
 
 	idx = srcu_read_lock(&kvm->srcu);
@@ -1120,7 +1120,7 @@ int kvm_phys_addr_ioremap(struct kvm *kvm, phys_addr_t guest_ipa,
 		return -EPERM;
 
 	/* We don't support mapping special pages into a Realm */
-	if (kvm_is_realm(kvm))
+	if (_kvm_is_realm(kvm))
 		return -EPERM;
 
 	size += offset_in_page(guest_ipa);
@@ -1943,7 +1943,7 @@ bool kvm_age_gfn(struct kvm *kvm, struct kvm_gfn_range *range)
 		return false;
 
 	/* We don't support aging for Realms */
-	if (kvm_is_realm(kvm))
+	if (_kvm_is_realm(kvm))
 		return true;
 
 	return kvm_pgtable_stage2_test_clear_young(kvm->arch.mmu.pgt,
@@ -1959,7 +1959,7 @@ bool kvm_test_age_gfn(struct kvm *kvm, struct kvm_gfn_range *range)
 		return false;
 
 	/* We don't support aging for Realms */
-	if (kvm_is_realm(kvm))
+	if (_kvm_is_realm(kvm))
 		return true;
 
 	return kvm_pgtable_stage2_test_clear_young(kvm->arch.mmu.pgt,

arch/arm64/kvm/reset.c
@@ -75,7 +75,7 @@ int __init kvm_arm_init_sve(void)
 
 unsigned int kvm_sve_get_max_vl(struct kvm *kvm)
 {
-	if (kvm_is_realm(kvm))
+	if (_kvm_is_realm(kvm))
 		return kvm_realm_sve_max_vl();
 	else
 		return kvm_sve_max_vl;

arch/arm64/kvm/sys_regs.c
@@ -3678,7 +3678,7 @@ int kvm_arm_sys_reg_set_reg(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg
 
 static unsigned int num_demux_regs(struct kvm_vcpu *vcpu)
 {
-	return kvm_is_realm(vcpu->kvm) ? 0 : CSSELR_MAX;
+	return _kvm_is_realm(vcpu->kvm) ? 0 : CSSELR_MAX;
 }
 
 static int write_demux_regids(struct kvm_vcpu *vcpu, u64 __user *uindices)
@@ -3697,7 +3697,7 @@ static int write_demux_regids(struct kvm_vcpu *vcpu, u64 __user *uindices)
 
 static unsigned int num_invariant_regs(struct kvm_vcpu *vcpu)
 {
-	return kvm_is_realm(vcpu->kvm) ? 0 : ARRAY_SIZE(invariant_sys_regs);
+	return _kvm_is_realm(vcpu->kvm) ? 0 : ARRAY_SIZE(invariant_sys_regs);
 }
 
 static int write_invariant_regids(struct kvm_vcpu *vcpu, u64 __user *uindices)
@@ -3738,7 +3738,7 @@ static bool copy_reg_to_user(const struct sys_reg_desc *reg, u64 __user **uind)
 
 static bool kvm_realm_sys_reg_hidden_user(const struct kvm_vcpu *vcpu, u64 reg)
 {
-	if (!kvm_is_realm(vcpu->kvm))
+	if (!_kvm_is_realm(vcpu->kvm))
 		return false;
 
 	switch (reg) {