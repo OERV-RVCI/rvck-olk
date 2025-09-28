be906931e5e1a548775a1d456743926d41a2594e
Peng Tao <tao.peng@linux.alibaba.com>
2025-09-28 14:43:03 +0800

    fuse: Introduce sysfs API for resend pending requests
    anolis inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICFOA8
    CVE: NA
    
    --------------------------------
    
    commit 017eaddf79d79778d3ba4af5ed1076a1e9558151 OpenAnolis
    
    When a FUSE daemon panics and fails over, we want to reuse the existing FUSE
    connection and avoid affecting applications as little as possible. During
    FUSE daemon failover, the FUSE processing queue requests are waiting for
    replies from user space daemon that never come back and applications would
    stuck forever.
    
    Besides flushing the processing queue requests like being done in fuse_flush_pq(),
    we can also resend these requests to user space daemon so that they can be
    processed properly again. Such strategy can only be done for idempotent requests
    or if the user space daemon takes good care to record and avoid processing
    duplicated non-idempotent requests, otherwise there can be consistency issues.
    We trust users to know what they are doing by calling writing to this interface.
    
    Signed-off-by: Peng Tao <tao.peng@linux.alibaba.com>
    Reviewed-by: Liu Bo <bo.liu@linux.alibaba.com>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Jeffle Xu <jefflexu@linux.alibaba.com>
    Acked-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Zhao Chen <winters.zc@antgroup.com>
    Signed-off-by: Long Li <leo.lilong@huawei.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -46,7 +46,7 @@
 #define FUSE_NAME_MAX 1024
 
 /** Number of dentries for each connection in the control filesystem */
-#define FUSE_CTL_NUM_DENTRIES 6
+#define FUSE_CTL_NUM_DENTRIES 7
 
 /** Maximum of max_pages received in init_out */
 extern unsigned int fuse_max_pages_limit;
@@ -1202,6 +1202,9 @@ void fuse_wait_aborted(struct fuse_conn *fc);
 
 /* Flush all requests in processing queue */
 void fuse_flush_pq(struct fuse_conn *fc);
+
+/* Resend all requests in processing queue so they can represent to userspace */
+void fuse_resend_pqueue(struct fuse_conn *fc);
 
 /**
  * Invalidate inode attributes
