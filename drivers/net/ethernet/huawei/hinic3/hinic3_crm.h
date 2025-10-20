OERV-RVCI/rvck-olk

commit 0bf0c942a09ba92e1d22e6960464b628dd4408fa

net/hinic3: Synchronize new NIC features and bug fixes
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IC5K0L?from=project-issue
CVE: NA

--------------------------------

Add optical module type adaptation.
Add ethtool fec function.
Add NIC XDP PASS function.
Add QoS persistence.
Add NIC out-of-band VLAN configuration function.
Fix NIC bifur bugs.
Fix performance test bugs.
Fix NIC RX bugs.
Fix NIC VF bugs.
Fix the bug of exiting when probing NIC.
Fix bond bugs.
Fix the bug of MAC address configuration failure in OS 6.6.
Fix the bug of reading NIC status incorrectly.
Fix and add some statistics.

Signed-off-by: fuguiming <fuguiming@huawei.com>

drivers/net/ethernet/huawei/hinic3/hinic3_crm.h
@@ -8,7 +8,7 @@
 
 #include "mpu_cmd_base_defs.h"
 
-#define HINIC3_DRV_VERSION "15.17.1.2"
+#define HINIC3_DRV_VERSION "17.7.8.1"
 #define HINIC3_DRV_DESC "Intelligent Network Interface Card Driver"
 #define HIUDK_DRV_DESC "Intelligent Network Unified Driver"
 
@@ -45,6 +45,7 @@ enum hinic3_service_type {
 	SERVICE_T_CUSTOM,
 	SERVICE_T_VROCE,
 	SERVICE_T_CRYPT,
+	SERVICE_T_VSOCK,
 	SERVICE_T_BIFUR,
 	SERVICE_T_MAX,
 
@@ -64,6 +65,9 @@ struct nic_service_cap {
 	u16 max_sqs;
 	u16 max_rqs;
 	u16 default_num_queues;
+	u16 outband_vlan_cfg_en;
+	u8 lro_enable;
+	u8 rsvd1[3];
 };
 
 struct ppa_service_cap {
@@ -910,6 +914,13 @@ int hinic3_get_fw_version(void *hwdev, struct hinic3_fw_version *fw_ver,
 			  u16 channel);
 
 /* *
+ * @brief hinic3_get_bond_create_mode - get bond create mode
+ * @param hwdev: device pointer to hwdev
+ * @retval global function id
+ */
+u8 hinic3_get_bond_create_mode(void *udkdev);
+
+/* *
  * @brief hinic3_global_func_id - get global function id
  * @param hwdev: device pointer to hwdev
  * @retval global function id
@@ -1262,5 +1273,8 @@ int hinic3_mbox_to_host_sync(void *hwdev, enum hinic3_mod_type mod,
 			     void *buf_out, u16 *out_size, u32 timeout, u16 channel);
 
 int hinic3_get_func_vroce_enable(void *hwdev, u16 glb_func_idx, u8 *en);
+
+void hinic3_module_get(void *hwdev, enum hinic3_service_type type);
+void hinic3_module_put(void *hwdev, enum hinic3_service_type type);
 
 #endif

drivers/net/ethernet/huawei/hinic3/hinic3_nic_dev.h
@@ -15,9 +15,10 @@
 #include "hinic3_tx.h"
 #include "hinic3_rx.h"
 #include "hinic3_dcb.h"
+#include "vram_common.h"
 
 #define HINIC3_NIC_DRV_NAME	"hinic3"
-#define HINIC3_NIC_DRV_VERSION	HINIC3_DRV_VERSION
+#define HINIC3_NIC_DRV_VERSION	"17.7.8.1"
 
 #define HINIC3_FUNC_IS_VF(hwdev)	(hinic3_func_type(hwdev) == TYPE_VF)
 
@@ -25,6 +26,10 @@
 #define HINIC3_MODERATONE_DELAY   HZ
 
 #define LP_PKT_CNT 64
+#define LP_PKT_LEN 60
+
+#define NAPI_IS_REGIN 1
+#define NAPI_NOT_REGIN 0
 
 enum hinic3_flags {
 	HINIC3_INTF_UP,
@@ -91,6 +96,8 @@ struct hinic3_irq {
 	u16			rsvd1;
 	u32			irq_id;         /* The IRQ number from OS */
 
+	u32			napi_reign;
+
 	char			irq_name[IFNAMSIZ + 16];
 	struct napi_struct	napi;
 	cpumask_t		affinity_mask;
@@ -123,6 +130,41 @@ struct hinic3_dyna_txrxq_params {
 	struct hinic3_dyna_txq_res	*txqs_res;
 	struct hinic3_dyna_rxq_res	*rxqs_res;
 	struct hinic3_irq		*irq_cfg;
+	char irq_cfg_vram_name[VRAM_NAME_MAX_LEN];
+};
+
+struct hinic3_flush_rq {
+	union {
+		struct {
+#if defined(BYTE_ORDER) && (BYTE_ORDER == BIG_ENDIAN)
+			u32 lb_proc : 1;
+			u32 rsvd : 10;
+			u32 rq_id : 8;
+			u32 func_id : 13;
+#else
+			u32 func_id : 13;
+			u32 rq_id : 8;
+			u32 rsvd : 10;
+			u32 lb_proc : 1;
+#endif
+		} bs;
+		u32 value;
+	} dw;
+
+	union {
+		struct {
+#if defined(BYTE_ORDER) && (BYTE_ORDER == BIG_ENDIAN)
+			u32 rsvd2 : 2;
+			u32 src_chnl : 12;
+			u32 pkt_len : 18;
+#else
+			u32 pkt_len : 18;
+			u32 src_chnl : 12;
+			u32 rsvd2 : 2;
+#endif
+		} bs;
+		u32 value;
+	} lb_info0; /* loop back information, used by uCode */
 };
 
 #define HINIC3_NIC_STATS_INC(nic_dev, field)			\
@@ -145,6 +187,10 @@ struct hinic3_nic_stats {
 #else
 	struct u64_stats_sync_empty syncp;
 #endif
+};
+
+struct hinic3_nic_vport_stats {
+	u64 rx_discard_vport;
 };
 
 #define HINIC3_TCAM_DYNAMIC_BLOCK_SIZE 16
@@ -185,6 +231,28 @@ struct hinic3_tcam_info {
 	struct hinic3_tcam_dynamic_block_info tcam_dynamic_info;
 };
 
+struct hinic3_dcb {
+	u8			 cos_config_num_max;
+	u8			 func_dft_cos_bitmap;
+	/* used to tool validity check */
+	u16			 port_dft_cos_bitmap;
+
+	struct hinic3_dcb_config hw_dcb_cfg;
+	struct hinic3_dcb_config wanted_dcb_cfg;
+	unsigned long		 dcb_flags;
+};
+
+struct hinic3_vram {
+	u32 vram_mtu;
+	u16 vram_num_qps;
+	unsigned long flags;
+};
+
+struct hinic3_outband_cfg {
+	u16 outband_default_vid;
+	u16 rsvd;
+};
+
 struct hinic3_nic_dev {
 	struct pci_dev		*pdev;
 	struct net_device	*netdev;
@@ -204,6 +272,7 @@ struct hinic3_nic_dev {
 	u32			dma_rx_buff_size;
 	u16			rx_buff_len;
 	u32			page_order;
+	bool			page_pool_enabled;
 
 	/* Rss related varibles */
 	u8			rss_hash_engine;
@@ -213,17 +282,13 @@ struct hinic3_nic_dev {
 	u32			*rss_hkey_be;
 	u32			*rss_indir;
 
-	u8			cos_config_num_max;
-	u8			func_dft_cos_bitmap;
-	u16			port_dft_cos_bitmap; /* used to tool validity check */
+	struct hinic3_dcb	*dcb;
+	char			dcb_name[VRAM_NAME_MAX_LEN];
 
-	struct hinic3_dcb_config hw_dcb_cfg;
-	struct hinic3_dcb_config wanted_dcb_cfg;
-	struct hinic3_dcb_config dcb_cfg;
-	unsigned long		dcb_flags;
+	struct hinic3_vram	*nic_vram;
+	char			nic_vram_name[VRAM_NAME_MAX_LEN];
+
 	int			disable_port_cnt;
-	/* lock for disable or enable traffic flow */
-	struct semaphore	dcb_sem;
 
 	struct hinic3_intr_coal_info *intr_coalesce;
 	unsigned long		last_moder_jiffies;
@@ -235,11 +300,10 @@ struct hinic3_nic_dev {
 #endif
 
 	struct hinic3_nic_stats	stats;
+	struct hinic3_nic_vport_stats	vport_stats;
 
 	/* lock for nic resource */
 	struct mutex		nic_mutex;
-	bool			force_port_disable;
-	struct semaphore	port_state_sem;
 	u8			link_status;
 
 	struct nic_service_cap	nic_cap;
@@ -281,9 +345,10 @@ struct hinic3_nic_dev {
 	struct hinic3_nic_prof_attr *prof_attr;
 	struct hinic3_prof_adapter *prof_adap;
 	u64			rsvd8[7];
-	u32			rsvd9;
+	struct hinic3_outband_cfg	outband_cfg;
 	u32			rxq_get_err_times;
 	struct delayed_work	rxq_check_work;
+	struct delayed_work		vport_stats_work;
 
 	struct hinic3_tx_rx_ops	tx_rx_ops;
 };
@@ -312,7 +377,7 @@ struct hinic3_uld_info *get_nic_uld_info(void);
 
 u32 hinic3_get_io_stats_size(const struct hinic3_nic_dev *nic_dev);
 
-void hinic3_get_io_stats(const struct hinic3_nic_dev *nic_dev, void *stats);
+int hinic3_get_io_stats(const struct hinic3_nic_dev *nic_dev, void *stats);
 
 int hinic3_open(struct net_device *netdev);
 
@@ -331,6 +396,8 @@ int hinic3_qps_irq_init(struct hinic3_nic_dev *nic_dev);
 
 void hinic3_qps_irq_deinit(struct hinic3_nic_dev *nic_dev);
 
+void qp_del_napi(struct hinic3_irq *irq_cfg);
+
 void hinic3_set_netdev_ops(struct hinic3_nic_dev *nic_dev);
 
 bool hinic3_is_netdev_ops_match(const struct net_device *netdev);
@@ -347,10 +414,6 @@ void hinic3_get_ethtool_stats(struct net_device *netdev,
 			      struct ethtool_stats *stats, u64 *data);
 
 int hinic3_get_sset_count(struct net_device *netdev, int sset);
-
-int hinic3_force_port_disable(struct hinic3_nic_dev *nic_dev);
-
-int hinic3_force_set_port_state(struct hinic3_nic_dev *nic_dev, bool enable);
 
 int hinic3_maybe_set_port_state(struct hinic3_nic_dev *nic_dev, bool enable);
 
@@ -384,6 +447,19 @@ void hinic3_link_status_change(struct hinic3_nic_dev *nic_dev, bool status);
 #ifdef HAVE_XDP_SUPPORT
 bool hinic3_is_xdp_enable(struct hinic3_nic_dev *nic_dev);
 int hinic3_xdp_max_mtu(struct hinic3_nic_dev *nic_dev);
+#endif
+
+#ifdef HAVE_UDP_TUNNEL_NIC_INFO
+int hinic3_udp_tunnel_set_port(struct net_device *netdev, unsigned int table,
+			       unsigned int entry, struct udp_tunnel_info *ti);
+int hinic3_udp_tunnel_unset_port(struct net_device *netdev, unsigned int table,
+				 unsigned int entry,
+				 struct udp_tunnel_info *ti);
+#endif /* HAVE_UDP_TUNNEL_NIC_INFO */
+
+#if defined(ETHTOOL_GFECPARAM) && defined(ETHTOOL_SFECPARAM)
+int set_fecparam(void *hwdev, u8 fecparam);
+int get_fecparam(void *hwdev, u8 *advertised_fec, u8 *supported_fec);
 #endif
 
 #endif

commit 6f4aaaeb81b422bdfa00f8ad1b8986b3118d4b7a

net/hinic3: Modify driver version
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICGDO6?from=project-issue
CVE: NA

--------------------------------

Modify driver version.

Fixes: 0bf0c942a09b ("net/hinic3: Synchronize new NIC features and bug fixes")
Signed-off-by: zhuyikai <zhuyikai1@h-partners.com>

drivers/net/ethernet/huawei/hinic3/hinic3_crm.h
@@ -8,7 +8,7 @@
 
 #include "mpu_cmd_base_defs.h"
 
-#define HINIC3_DRV_VERSION "17.7.8.1"
+#define HINIC3_DRV_VERSION "17.7.8.101"
 #define HINIC3_DRV_DESC "Intelligent Network Interface Card Driver"
 #define HIUDK_DRV_DESC "Intelligent Network Unified Driver"
 

drivers/net/ethernet/huawei/hinic3/hinic3_nic_dev.h
@@ -18,7 +18,7 @@
 #include "vram_common.h"
 
 #define HINIC3_NIC_DRV_NAME	"hinic3"
-#define HINIC3_NIC_DRV_VERSION	"17.7.8.1"
+#define HINIC3_NIC_DRV_VERSION	"17.7.8.101"
 
 #define HINIC3_FUNC_IS_VF(hwdev)	(hinic3_func_type(hwdev) == TYPE_VF)
 