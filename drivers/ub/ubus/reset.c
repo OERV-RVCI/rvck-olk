chenglongshe/openEuler

ub:ubus: Support device level and port level reset
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID34DG
CVE: NA

-----------------------------------------------------------

Support device level and port level reset.

Signed-off-by: Yahui Liu <liuyahui6@huawei.com>

drivers/ub/ubus/reset.c
@@ -1,0 +1,326 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) HiSilicon Technologies Co., Ltd. 2025. All rights reserved.
+ */
+
+#define pr_fmt(fmt)	"ubus reset: " fmt
+
+#include <linux/delay.h>
+
+#include "ubus.h"
+#include "port.h"
+#include "route.h"
+#include "ubus_controller.h"
+#include "ubus_config.h"
+
+enum elr_type {
+	ELR_PREPARE = 0,
+	ELR_DONE = 1,
+};
+
+enum SAVED_CFG_SPACE_NUM {
+	DEV_TOKEN_ID = 0,
+};
+
+static u32 saved_cfg_offset[] = {
+	[DEV_TOKEN_ID] = UB_CFG1_BASIC + 0xb4,
+};
+
+/**
+ * ub_elr - Initiate an UB entity level reset
+ * @dev: UB entity to reset
+ */
+int ub_elr(struct ub_entity *dev)
+{
+	u8 command;
+	u8 val = 0;
+	int ret;
+
+	/* enable ELR */
+	command = 0x01;
+	ret = ub_cfg_write_byte(dev, UB_ELR, command);
+	if (ret) {
+		ub_err(dev, "dev elr failed, write byte error, ret=%d", ret);
+		return ret;
+	}
+
+	/*
+	 * ub entity must complete an ELR within 100ms,
+	 * but may silently discard requests while the ELR is in
+	 * progress. Wait 100ms before trying to access the device.
+	 */
+	msleep(100);
+
+	ret = ub_cfg_read_byte(dev, UB_ELR_DONE, &val);
+	if (ret || !val) {
+		ub_err(dev, "dev elr failed, ret=%d, ELR_DONE=%u\n", ret, val);
+		return -EINVAL;
+	}
+
+	ub_info(dev, "dev elr success\n");
+
+	return 0;
+}
+
+/**
+ * ub_device_reset - Initiate a UB entity reset
+ * @ent: UB entity.
+ */
+int ub_device_reset(struct ub_entity *ent)
+{
+	u16 command;
+	int ret;
+
+	if (!ent) {
+		pr_err("device which will be reset is NULL\n");
+		return -EINVAL;
+	}
+
+	if (is_ibus_controller(ent)) {
+		ub_warn(ent, "ub bus controller do not support reset.\n");
+		return -EINVAL;
+	}
+
+	/* device reset only support Entity0 */
+	if (ent->entity_idx) {
+		ub_err(ent, "device is not entity0, entity_idx=%u\n", ent->entity_idx);
+		return -EINVAL;
+	}
+
+	device_lock(&ent->dev);
+
+	/* enable device reset */
+	command = 0x01;
+	ret = ub_send_cfg(ent, (u8)sizeof(u16), UB_ENTITY_RST, (u32 *)&command);
+	if (ret) {
+		ub_err(ent, "device reset fail, ret=%d\n", ret);
+		device_unlock(&ent->dev);
+		return -EIO;
+	}
+
+	device_unlock(&ent->dev);
+	ub_info(ent, "device reset success\n");
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(ub_device_reset);
+
+static void ub_save_token_state(struct ub_entity *dev)
+{
+	int ret;
+	int i;
+
+	for (i = DEV_TOKEN_ID; i <= DEV_TOKEN_ID; i++) {
+		ret = ub_cfg_read_dword(dev, saved_cfg_offset[i],
+					&dev->saved_config_space[i]);
+		if (ret) {
+			ub_err(dev, "ub cfg read dword failed, save cfg offset: %#x.\n",
+				saved_cfg_offset[i]);
+			continue;
+		}
+		ub_info(dev, "saving config space at address %#x (reading %#x)\n",
+			saved_cfg_offset[i], dev->saved_config_space[i]);
+	}
+}
+
+static int ub_save_state(struct ub_entity *dev)
+{
+	const struct ub_error_handlers *err_handler =
+			dev->driver ? dev->driver->err_handler : NULL;
+
+	if (err_handler && err_handler->reset_prepare)
+		err_handler->reset_prepare(dev);
+
+	ub_save_token_state(dev);
+
+	dev->state_saved = true;
+
+	return 0;
+}
+
+static void ub_restore_config_dword(struct ub_entity *dev, u32 pos, u32 saved_val)
+{
+	int retry = 10;
+	u32 val;
+	int ret;
+
+	ret = ub_cfg_read_dword(dev, pos, &val);
+	if (ret || val == saved_val)
+		return;
+	while (retry-- > 0) {
+		ub_info(dev, "restoring config space at address %#x (was %#x, writing %#x)\n",
+			pos, val, saved_val);
+		ub_cfg_write_dword(dev, pos, saved_val);
+
+		if (ub_cfg_read_dword(dev, pos, &val))
+			continue;
+
+		if (val == saved_val)
+			return;
+
+#define RESTORE_RETRY_SLEEP_MS 1
+		msleep(RESTORE_RETRY_SLEEP_MS);
+	}
+}
+
+static void ub_restore_state(struct ub_entity *dev)
+{
+	const struct ub_error_handlers *err_handler =
+			dev->driver ? dev->driver->err_handler : NULL;
+	int i;
+
+	if (!dev->state_saved)
+		return;
+
+	for (i = DEV_TOKEN_ID; i <= DEV_TOKEN_ID; i++) {
+		ub_restore_config_dword(dev, saved_cfg_offset[i],
+					dev->saved_config_space[i]);
+	}
+
+	dev->state_saved = false;
+
+	if (err_handler && err_handler->reset_done)
+		err_handler->reset_done(dev);
+}
+
+static int ub_reset_check(struct ub_entity *dev)
+{
+	if (is_ibus_controller(dev)) {
+		ub_err(dev, "UB Bus Controller does not support ELR!\n");
+		return -EINVAL;
+	}
+
+	if (!dev->reset_fn)
+		return -ENOTTY;
+
+	return 0;
+}
+
+int ub_reset_entity(struct ub_entity *dev)
+{
+	int ret, rc;
+
+	if (!dev) {
+		pr_err("device is NULL\n");
+		return -EINVAL;
+	}
+
+	rc = ub_reset_check(dev);
+	if (rc)
+		return rc;
+
+	if (!device_trylock(&dev->dev))
+		return -EBUSY;
+
+	ret = ub_save_state(dev);
+	if (ret) {
+		device_unlock(&dev->dev);
+		return ret;
+	}
+
+	rc = ub_elr(dev);
+
+	ub_restore_state(dev);
+
+	device_unlock(&dev->dev);
+
+	return rc;
+}
+EXPORT_SYMBOL_GPL(ub_reset_entity);
+
+int ub_port_reset_check(struct ub_entity *dev, int port_id)
+{
+	struct ub_port *port = NULL;
+
+	if (!dev)
+		return -EINVAL;
+
+	if (is_idev(dev)) {
+		ub_err(dev, "IDEV does not support port reset!\n");
+		return -EINVAL;
+	}
+
+	if (port_id >= dev->port_nums) {
+		ub_err(dev, "Can't reset port because port(%d) is over port_nums(%u)!\n",
+			port_id, dev->port_nums);
+		return -EINVAL;
+	}
+
+	port = dev->ports + port_id;
+
+	if (port->type == VIRTUAL) {
+		ub_err(dev, "vport reset is not supported now!\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static bool ub_wait_port_complete(struct ub_port *port)
+{
+	u64 timeout = 0;
+
+#define TOTAL_WAIT_CNT 150 /* wait 15s for port reset */
+#define WAIT_TIME 100
+
+	do {
+		if (port->link_state == LINK_STATE_DONE)
+			return true;
+
+		msleep(WAIT_TIME);
+		timeout++;
+	} while (timeout < TOTAL_WAIT_CNT);
+
+	return false;
+}
+
+int ub_port_reset(struct ub_entity *dev, int port_id)
+{
+	struct ub_port *port = NULL;
+	int ret;
+
+	if (ub_port_reset_check(dev, port_id))
+		return -EINVAL;
+
+	device_lock(&dev->dev);
+	port = dev->ports + port_id;
+	if (port->link_state != LINK_STATE_NORMAL) {
+		ub_err(dev, "port reset is not complete last time!\n");
+		device_unlock(&dev->dev);
+		return -EINVAL;
+	}
+
+	if (port->shareable)
+		ub_notify_share_port(port, RESET_PREPARE);
+
+	/* enable port reset */
+	ret = ub_port_write_dword(port, UB_PORT_RST, 0x01);
+	if (ret) {
+		ub_err(port->uent, "port reset fail, dev_eid=%u, port_id=%d, ret=%d\n",
+				dev->eid, port_id, ret);
+		device_unlock(&dev->dev);
+		return -EIO;
+	}
+
+	port->link_state = LINK_STATE_RESETING;
+
+	device_unlock(&dev->dev);
+
+	if (ub_wait_port_complete(port)) {
+		if (port->shareable)
+			ub_notify_share_port(port, RESET_DONE);
+		port->link_state = LINK_STATE_NORMAL;
+		ub_info(dev, "port(%d) reset success!\n", port_id);
+		return ret;
+	}
+
+	port->link_state = LINK_STATE_NORMAL;
+	ub_err(dev, "port(%d) reset timeout!\n", port_id);
+	return -ETIME;
+}
+EXPORT_SYMBOL_GPL(ub_port_reset);
+
+int ub_port_reset_function(struct ub_port *port)
+{
+	return ub_port_reset(port->uent, port->index);
+}

include/ub/ubus/ubus.h
@@ -105,6 +105,12 @@ enum ub_port_type {
 #define UB_MAX_CNA_NUM SZ_64K
 #define UB_PORT_CAP_NUM SZ_256
 
+enum ub_link_state {
+	LINK_STATE_NORMAL = 0,
+	LINK_STATE_RESETING = 1,
+	LINK_STATE_DONE = 2,
+};
+
 struct ub_port {
 	struct ub_entity *uent;
 	u16 index;
@@ -119,6 +125,8 @@ struct ub_port {
 	DECLARE_BITMAP(cna_maps, UB_MAX_CNA_NUM);
 	/* cap cache */
 	DECLARE_BITMAP(cap_map, UB_PORT_CAP_NUM);
+
+	enum ub_link_state link_state;
 };
 
 struct ue_map {
@@ -167,9 +175,14 @@ struct ub_entity {
 	u16 port_nums;
 	struct ub_port *ports;
 
+	unsigned int state_saved : 1;
+
 	/* entity DMA info */
 	u64 dma_mask;
 	struct device_dma_parameters dma_parms;
+
+	/* UB reset info */
+	unsigned int reset_fn : 1;
 
 	/* entity route info */
 	struct list_head cna_list; /* store distance for cna in route table */
@@ -179,9 +192,19 @@ struct ub_entity {
 	/* UB entity TID */
 	u32 tid;
 
+	/* UB saved config space */
+	u32 saved_config_space[24]; /* Config space saved at reset time */
+
 	u32 support_feature;
 
 	u16 upi;
+};
+
+/* UB bus error event callbacks */
+struct ub_error_handlers {
+	/* UB function reset prepare or completed */
+	void (*reset_prepare)(struct ub_entity *uent);
+	void (*reset_done)(struct ub_entity *uent);
 };
 
 struct ub_dynids {
@@ -218,6 +241,7 @@ struct ub_dynids {
  *		context, so it can sleep.
  * @shutdown:	Hook into reboot_notifier_list (kernel/sys.c).
  *		Intended to stop any idling operations.
+ * @err_handler: Error handling callbacks.
  * @groups:	Sysfs attribute groups.
  * @dev_groups: Attributes attached to the device that will be
  *		created once it is bound to the driver.
@@ -240,6 +264,7 @@ struct ub_driver {
 	/* entity removed (NULL if not a hot-plug capable driver) */
 	void (*remove)(struct ub_entity *uent);
 	void (*shutdown)(struct ub_entity *uent);
+	const struct ub_error_handlers *err_handler;
 	const struct attribute_group **groups;
 	const struct attribute_group **dev_groups;
 	struct device_driver driver;
@@ -400,6 +425,33 @@ int ub_register_share_port(struct ub_entity *uent, u16 port_id,
  */
 void ub_unregister_share_port(struct ub_entity *uent, u16 port_id,
 			      struct ub_share_port_ops *ops);
+
+/**
+ * ub_reset_entity() - Function entity level reset.
+ * @ent: UB entity.
+ *
+ * Reset a single entity without affecting other entities, If you want to reuse
+ * the entity after reset, you need to re-initialize it.
+ *
+ * Context: Any context
+ * Return: 0 if success, or %-EINVAL if entity not support elr,
+ * or %-ENOTTY if entity can't be reset safely,
+ * or -EBUSY if can't get device_trylock(), or other failed negative values.
+ */
+int ub_reset_entity(struct ub_entity *ent);
+
+/**
+ * ub_device_reset() - Device level reset.
+ * @ent: UB entity.
+ *
+ * Reset Device, include all entities under the device, If you want to reuse
+ * the device after reset, you need to re-initialize it.
+ *
+ * Context: Any context
+ * Return: 0 if success, or %-EINVAL if parameters invalid,
+ * or %-EIO if device can't reset now, can try later.
+ */
+int ub_device_reset(struct ub_entity *ent);
 
 /**
  * ub_cfg_read_byte() - 1 byte configuration access read.
@@ -584,6 +636,10 @@ static inline int ub_register_share_port(struct ub_entity *uent, u16 port_id,
 { return -ENODEV; }
 static inline void ub_unregister_share_port(struct ub_entity *uent, u16 port_id,
 					    struct ub_share_port_ops *ops) {}
+static inline int ub_reset_entity(struct ub_entity *uent)
+{ return -ENODEV; }
+static inline int ub_device_reset(struct ub_entity *uent)
+{ return -ENODEV; }
 static inline int
 __ub_register_driver(struct ub_driver *drv, struct module *owner,
 		     const char *mod_name)

OERV-RVCI/rvck-olk

drivers/ub/ubus/reset.c
@@ -1,0 +1,326 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) HiSilicon Technologies Co., Ltd. 2025. All rights reserved.
+ */
+
+#define pr_fmt(fmt)	"ubus reset: " fmt
+
+#include <linux/delay.h>
+
+#include "ubus.h"
+#include "port.h"
+#include "route.h"
+#include "ubus_controller.h"
+#include "ubus_config.h"
+
+enum elr_type {
+	ELR_PREPARE = 0,
+	ELR_DONE = 1,
+};
+
+enum SAVED_CFG_SPACE_NUM {
+	DEV_TOKEN_ID = 0,
+};
+
+static u32 saved_cfg_offset[] = {
+	[DEV_TOKEN_ID] = UB_CFG1_BASIC + 0xb4,
+};
+
+/**
+ * ub_elr - Initiate an UB entity level reset
+ * @dev: UB entity to reset
+ */
+int ub_elr(struct ub_entity *dev)
+{
+	u8 command;
+	u8 val = 0;
+	int ret;
+
+	/* enable ELR */
+	command = 0x01;
+	ret = ub_cfg_write_byte(dev, UB_ELR, command);
+	if (ret) {
+		ub_err(dev, "dev elr failed, write byte error, ret=%d", ret);
+		return ret;
+	}
+
+	/*
+	 * ub entity must complete an ELR within 100ms,
+	 * but may silently discard requests while the ELR is in
+	 * progress. Wait 100ms before trying to access the device.
+	 */
+	msleep(100);
+
+	ret = ub_cfg_read_byte(dev, UB_ELR_DONE, &val);
+	if (ret || !val) {
+		ub_err(dev, "dev elr failed, ret=%d, ELR_DONE=%u\n", ret, val);
+		return -EINVAL;
+	}
+
+	ub_info(dev, "dev elr success\n");
+
+	return 0;
+}
+
+/**
+ * ub_device_reset - Initiate a UB entity reset
+ * @ent: UB entity.
+ */
+int ub_device_reset(struct ub_entity *ent)
+{
+	u16 command;
+	int ret;
+
+	if (!ent) {
+		pr_err("device which will be reset is NULL\n");
+		return -EINVAL;
+	}
+
+	if (is_ibus_controller(ent)) {
+		ub_warn(ent, "ub bus controller do not support reset.\n");
+		return -EINVAL;
+	}
+
+	/* device reset only support Entity0 */
+	if (ent->entity_idx) {
+		ub_err(ent, "device is not entity0, entity_idx=%u\n", ent->entity_idx);
+		return -EINVAL;
+	}
+
+	device_lock(&ent->dev);
+
+	/* enable device reset */
+	command = 0x01;
+	ret = ub_send_cfg(ent, (u8)sizeof(u16), UB_ENTITY_RST, (u32 *)&command);
+	if (ret) {
+		ub_err(ent, "device reset failed, ret=%d\n", ret);
+		device_unlock(&ent->dev);
+		return -EIO;
+	}
+
+	device_unlock(&ent->dev);
+	ub_info(ent, "device reset success\n");
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(ub_device_reset);
+
+static void ub_save_token_state(struct ub_entity *dev)
+{
+	int ret;
+	int i;
+
+	for (i = DEV_TOKEN_ID; i <= DEV_TOKEN_ID; i++) {
+		ret = ub_cfg_read_dword(dev, saved_cfg_offset[i],
+					&dev->saved_config_space[i]);
+		if (ret) {
+			ub_err(dev, "ub cfg read dword failed, save cfg offset: %#x.\n",
+				saved_cfg_offset[i]);
+			continue;
+		}
+		ub_info(dev, "saving config space at address %#x (reading %#x)\n",
+			saved_cfg_offset[i], dev->saved_config_space[i]);
+	}
+}
+
+static int ub_save_state(struct ub_entity *dev)
+{
+	const struct ub_error_handlers *err_handler =
+			dev->driver ? dev->driver->err_handler : NULL;
+
+	if (err_handler && err_handler->ub_reset_prepare)
+		err_handler->ub_reset_prepare(dev);
+
+	ub_save_token_state(dev);
+
+	dev->state_saved = true;
+
+	return 0;
+}
+
+static void ub_restore_config_dword(struct ub_entity *dev, u32 pos, u32 saved_val)
+{
+	int retry = 10;
+	u32 val;
+	int ret;
+
+	ret = ub_cfg_read_dword(dev, pos, &val);
+	if (ret || val == saved_val)
+		return;
+	while (retry-- > 0) {
+		ub_info(dev, "restoring config space at address %#x (was %#x, writing %#x)\n",
+			pos, val, saved_val);
+		ub_cfg_write_dword(dev, pos, saved_val);
+
+		if (ub_cfg_read_dword(dev, pos, &val))
+			continue;
+
+		if (val == saved_val)
+			return;
+
+#define RESTORE_RETRY_SLEEP_MS 1
+		msleep(RESTORE_RETRY_SLEEP_MS);
+	}
+}
+
+static void ub_restore_state(struct ub_entity *dev)
+{
+	const struct ub_error_handlers *err_handler =
+			dev->driver ? dev->driver->err_handler : NULL;
+	int i;
+
+	if (!dev->state_saved)
+		return;
+
+	for (i = DEV_TOKEN_ID; i <= DEV_TOKEN_ID; i++) {
+		ub_restore_config_dword(dev, saved_cfg_offset[i],
+					dev->saved_config_space[i]);
+	}
+
+	dev->state_saved = false;
+
+	if (err_handler && err_handler->ub_reset_done)
+		err_handler->ub_reset_done(dev);
+}
+
+static int ub_reset_check(struct ub_entity *dev)
+{
+	if (is_ibus_controller(dev)) {
+		ub_err(dev, "UB Bus Controller does not support ELR!\n");
+		return -EINVAL;
+	}
+
+	if (!dev->reset_fn)
+		return -ENOTTY;
+
+	return 0;
+}
+
+int ub_reset_entity(struct ub_entity *dev)
+{
+	int ret, rc;
+
+	if (!dev) {
+		pr_err("device is NULL\n");
+		return -EINVAL;
+	}
+
+	rc = ub_reset_check(dev);
+	if (rc)
+		return rc;
+
+	if (!device_trylock(&dev->dev))
+		return -EBUSY;
+
+	ret = ub_save_state(dev);
+	if (ret) {
+		device_unlock(&dev->dev);
+		return ret;
+	}
+
+	rc = ub_elr(dev);
+
+	ub_restore_state(dev);
+
+	device_unlock(&dev->dev);
+
+	return rc;
+}
+EXPORT_SYMBOL_GPL(ub_reset_entity);
+
+int ub_port_reset_check(struct ub_entity *dev, int port_id)
+{
+	struct ub_port *port = NULL;
+
+	if (!dev)
+		return -EINVAL;
+
+	if (is_idev(dev)) {
+		ub_err(dev, "IDEV does not support port reset!\n");
+		return -EINVAL;
+	}
+
+	if (port_id >= dev->port_nums) {
+		ub_err(dev, "Can't reset port because port(%d) is over port_nums(%u)!\n",
+			port_id, dev->port_nums);
+		return -EINVAL;
+	}
+
+	port = dev->ports + port_id;
+
+	if (port->type == VIRTUAL) {
+		ub_err(dev, "vport reset is not supported now!\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static bool ub_wait_port_complete(struct ub_port *port)
+{
+	u64 timeout = 0;
+
+#define TOTAL_WAIT_CNT 150 /* wait 15s for port reset */
+#define WAIT_TIME 100
+
+	do {
+		if (port->link_state == LINK_STATE_DONE)
+			return true;
+
+		msleep(WAIT_TIME);
+		timeout++;
+	} while (timeout < TOTAL_WAIT_CNT);
+
+	return false;
+}
+
+int ub_port_reset(struct ub_entity *dev, int port_id)
+{
+	struct ub_port *port = NULL;
+	int ret;
+
+	if (ub_port_reset_check(dev, port_id))
+		return -EINVAL;
+
+	device_lock(&dev->dev);
+	port = dev->ports + port_id;
+	if (port->link_state != LINK_STATE_NORMAL) {
+		ub_err(dev, "port reset is not complete last time!\n");
+		device_unlock(&dev->dev);
+		return -EINVAL;
+	}
+
+	if (port->shareable)
+		ub_notify_share_port(port, RESET_PREPARE);
+
+	/* enable port reset */
+	ret = ub_port_write_dword(port, UB_PORT_RST, 0x01);
+	if (ret) {
+		ub_err(port->uent, "port reset failed, dev_eid=%u, port_id=%d, ret=%d\n",
+				dev->eid, port_id, ret);
+		device_unlock(&dev->dev);
+		return -EIO;
+	}
+
+	port->link_state = LINK_STATE_RESETING;
+
+	device_unlock(&dev->dev);
+
+	if (ub_wait_port_complete(port)) {
+		if (port->shareable)
+			ub_notify_share_port(port, RESET_DONE);
+		port->link_state = LINK_STATE_NORMAL;
+		ub_info(dev, "port(%d) reset success!\n", port_id);
+		return ret;
+	}
+
+	port->link_state = LINK_STATE_NORMAL;
+	ub_err(dev, "port(%d) reset timeout!\n", port_id);
+	return -ETIME;
+}
+EXPORT_SYMBOL_GPL(ub_port_reset);
+
+int ub_port_reset_function(struct ub_port *port)
+{
+	return ub_port_reset(port->uent, port->index);
+}

include/ub/ubus/ubus.h
@@ -105,6 +105,12 @@ enum ub_port_type {
 #define UB_MAX_CNA_NUM SZ_64K
 #define UB_PORT_CAP_NUM SZ_256
 
+enum ub_link_state {
+	LINK_STATE_NORMAL = 0,
+	LINK_STATE_RESETING = 1,
+	LINK_STATE_DONE = 2,
+};
+
 struct ub_port {
 	struct ub_entity *uent;
 	u16 index;
@@ -119,6 +125,8 @@ struct ub_port {
 	DECLARE_BITMAP(cna_maps, UB_MAX_CNA_NUM);
 	/* cap cache */
 	DECLARE_BITMAP(cap_map, UB_PORT_CAP_NUM);
+
+	enum ub_link_state link_state;
 };
 
 struct ue_map {
@@ -167,9 +175,14 @@ struct ub_entity {
 	u16 port_nums;
 	struct ub_port *ports;
 
+	unsigned int state_saved : 1;
+
 	/* entity DMA info */
 	u64 dma_mask;
 	struct device_dma_parameters dma_parms;
+
+	/* UB reset info */
+	unsigned int reset_fn : 1;
 
 	/* entity route info */
 	struct list_head cna_list; /* store distance for cna in route table */
@@ -179,9 +192,19 @@ struct ub_entity {
 	/* UB entity TID */
 	u32 tid;
 
+	/* UB saved config space */
+	u32 saved_config_space[24]; /* Config space saved at reset time */
+
 	u32 support_feature;
 
 	u16 upi;
+};
+
+/* UB bus error event callbacks */
+struct ub_error_handlers {
+	/* UB function reset prepare or completed */
+	void (*ub_reset_prepare)(struct ub_entity *uent);
+	void (*ub_reset_done)(struct ub_entity *uent);
 };
 
 struct ub_dynids {
@@ -218,6 +241,7 @@ struct ub_dynids {
  *		context, so it can sleep.
  * @shutdown:	Hook into reboot_notifier_list (kernel/sys.c).
  *		Intended to stop any idling operations.
+ * @err_handler: Error handling callbacks.
  * @groups:	Sysfs attribute groups.
  * @dev_groups: Attributes attached to the device that will be
  *		created once it is bound to the driver.
@@ -240,6 +264,7 @@ struct ub_driver {
 	/* entity removed (NULL if not a hot-plug capable driver) */
 	void (*remove)(struct ub_entity *uent);
 	void (*shutdown)(struct ub_entity *uent);
+	const struct ub_error_handlers *err_handler;
 	const struct attribute_group **groups;
 	const struct attribute_group **dev_groups;
 	struct device_driver driver;
@@ -400,6 +425,33 @@ int ub_register_share_port(struct ub_entity *uent, u16 port_id,
  */
 void ub_unregister_share_port(struct ub_entity *uent, u16 port_id,
 			      struct ub_share_port_ops *ops);
+
+/**
+ * ub_reset_entity() - Function entity level reset.
+ * @ent: UB entity.
+ *
+ * Reset a single entity without affecting other entities, If you want to reuse
+ * the entity after reset, you need to re-initialize it.
+ *
+ * Context: Any context
+ * Return: 0 if success, or %-EINVAL if entity not support elr,
+ * or %-ENOTTY if entity can't be reset safely,
+ * or -EBUSY if can't get device_trylock(), or other failed negative values.
+ */
+int ub_reset_entity(struct ub_entity *ent);
+
+/**
+ * ub_device_reset() - Device level reset.
+ * @ent: UB entity.
+ *
+ * Reset Device, include all entities under the device, If you want to reuse
+ * the device after reset, you need to re-initialize it.
+ *
+ * Context: Any context
+ * Return: 0 if success, or %-EINVAL if parameters invalid,
+ * or %-EIO if device can't reset now, can try later.
+ */
+int ub_device_reset(struct ub_entity *ent);
 
 /**
  * ub_cfg_read_byte() - 1 byte configuration access read.
@@ -584,6 +636,10 @@ static inline int ub_register_share_port(struct ub_entity *uent, u16 port_id,
 { return -ENODEV; }
 static inline void ub_unregister_share_port(struct ub_entity *uent, u16 port_id,
 					    struct ub_share_port_ops *ops) {}
+static inline int ub_reset_entity(struct ub_entity *uent)
+{ return -ENODEV; }
+static inline int ub_device_reset(struct ub_entity *uent)
+{ return -ENODEV; }
 static inline int
 __ub_register_driver(struct ub_driver *drv, struct module *owner,
 		     const char *mod_name)