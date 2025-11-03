chenglongshe/openEuler

net: Add a new protocol family AF_UB
openEuler inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID29LT
CVE: NA

--------------------------------

The implementation includes:
1. Add a new protocol family AF_UB to support
UB Memory based Socket(UMS)
2. Introduce a negotiation mechanism during
the TCP three-way handshake to enable both
communication ends to determine mutual support for the
AF_UB protocol family

Signed-off-by: tianzhensong <tianzhensong@huawei.com>

include/net/tcp.h
@@ -201,6 +201,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCPOPT_FASTOPEN_MAGIC	0xF989
 #define TCPOPT_SMC_MAGIC	0xE2D4C3D9
 #define TCPOPT_COMP_MAGIC	0x7954
+#define TCPOPT_UMS_MAGIC	0xE4D4E2
 
 /*
  *     TCP option lengths
@@ -215,6 +216,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCPOLEN_EXP_FASTOPEN_BASE  4
 #define TCPOLEN_EXP_SMC_BASE   6
 #define TCPOLEN_EXP_COMP_BASE  4
+#define TCPOLEN_EXP_UMS_BASE   6
 
 /* But this is what stacks really send out. */
 #define TCPOLEN_TSTAMP_ALIGNED		12
@@ -226,6 +228,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCPOLEN_MD5SIG_ALIGNED		20
 #define TCPOLEN_MSS_ALIGNED		4
 #define TCPOLEN_EXP_SMC_BASE_ALIGNED	8
+#define TCPOLEN_EXP_UMS_BASE_ALIGNED	8
 
 /* Flags in tp->nonagle */
 #define TCP_NAGLE_OFF		1	/* Nagle's algo is disabled */
@@ -2553,6 +2556,10 @@ static inline void tcp_bpf_rtt(struct sock *sk)
 
 #if IS_ENABLED(CONFIG_SMC)
 extern struct static_key_false tcp_have_smc;
+#endif
+
+#if IS_ENABLED(CONFIG_UB_UMS)
+extern struct static_key_false tcp_have_ums;
 #endif
 
 #if IS_ENABLED(CONFIG_TLS_DEVICE)

OERV-RVCI/rvck-olk

include/net/tcp.h
@@ -201,6 +201,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCPOPT_FASTOPEN_MAGIC	0xF989
 #define TCPOPT_SMC_MAGIC	0xE2D4C3D9
 #define TCPOPT_COMP_MAGIC	0x7954
+#define TCPOPT_UMS_MAGIC	0xE4C2D4E2
 
 /*
  *     TCP option lengths
@@ -215,6 +216,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCPOLEN_EXP_FASTOPEN_BASE  4
 #define TCPOLEN_EXP_SMC_BASE   6
 #define TCPOLEN_EXP_COMP_BASE  4
+#define TCPOLEN_EXP_UMS_BASE   6
 
 /* But this is what stacks really send out. */
 #define TCPOLEN_TSTAMP_ALIGNED		12
@@ -226,6 +228,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCPOLEN_MD5SIG_ALIGNED		20
 #define TCPOLEN_MSS_ALIGNED		4
 #define TCPOLEN_EXP_SMC_BASE_ALIGNED	8
+#define TCPOLEN_EXP_UMS_BASE_ALIGNED	8
 
 /* Flags in tp->nonagle */
 #define TCP_NAGLE_OFF		1	/* Nagle's algo is disabled */
@@ -2553,6 +2556,10 @@ static inline void tcp_bpf_rtt(struct sock *sk)
 
 #if IS_ENABLED(CONFIG_SMC)
 extern struct static_key_false tcp_have_smc;
+#endif
+
+#if IS_ENABLED(CONFIG_UB_UMS)
+extern struct static_key_false tcp_have_ums;
 #endif
 
 #if IS_ENABLED(CONFIG_TLS_DEVICE)