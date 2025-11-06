OERV-RVCI/rvck-olk

commit 9e0a8eb88dc76428c8ccad9afcce6177ec7ad0c4

arm64/mpam: Add quirk for cmax and cmin
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICXA3G

--------------------------------

According to the ARM MPAM specification, the legal range of CMAX depends on
the MPAMF_CCAP_IDR.CMAX_WD field. However, on certain specialized SoCs,
the valid CMAX range additionally relies on the length of the cache
partition bit mask.

To accommodate these chips, the conversion algorithm between ca_max and
percent must be adapted so that the values delivered match the intended
targets exactly.

Fixes: 83c4315c75ba ("arm64/mpam: Add CMAX feature")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -683,6 +683,39 @@ static u16 percent_to_mbw_max(u32 pc, u8 wd)
 	return value;
 }
 
+static u16 percent_to_ca_max(u32 pc, u8 wd)
+{
+	struct rdt_resource *l3 = resctrl_arch_get_resource(RDT_RESOURCE_L3);
+	u32 valid_max;
+
+	if (read_cpuid_implementor() != ARM_CPU_IMP_HISI)
+		return percent_to_mbw_max(pc, wd);
+
+	valid_max = l3->cache.cbm_len;
+
+	if (pc >= MAX_MBA_BW)
+		return valid_max << (16 - wd);
+
+	return ((pc * valid_max + 50) / 100) << (16 - wd);
+}
+
+static u16 ca_max_to_percent(u16 ca_max, u8 wd)
+{
+	struct rdt_resource *l3 = resctrl_arch_get_resource(RDT_RESOURCE_L3);
+	u32 valid_max;
+
+	if (read_cpuid_implementor() != ARM_CPU_IMP_HISI)
+		return mbw_max_to_percent(ca_max, wd);
+
+	valid_max = l3->cache.cbm_len;
+
+	ca_max = ca_max >> (16 - wd);
+	if (ca_max >= valid_max)
+		return MAX_MBA_BW;
+
+	return (ca_max * 100 + valid_max / 2) / valid_max;
+}
+
 /* Test whether we can export MPAM_CLASS_CACHE:{2,3}? */
 static void mpam_resctrl_pick_caches(void)
 {
@@ -885,6 +918,20 @@ void resctrl_arch_reset_rmid_all(struct rdt_resource *r, struct rdt_domain *d)
 	mpam_msmon_reset_all_mbwu(dom->comp);
 }
 
+static void mpam_llc_gran_hisi_workaround(struct rdt_resource *r)
+{
+	unsigned int cbm_len;
+
+	if (read_cpuid_implementor() != ARM_CPU_IMP_HISI)
+		return;
+
+	if (r->fflags != RFTYPE_RES_CACHE || r->cache_level != 3)
+		return;
+
+	cbm_len = resctrl_arch_get_resource(RDT_RESOURCE_L3)->cache.cbm_len;
+	r->membw.bw_gran = max(100 / cbm_len, 1);
+}
+
 static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 {
 	struct mpam_class *class = res->class;
@@ -1002,6 +1049,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 
 		r->membw.min_bw = 1;
 		r->membw.bw_gran = max(100 / (1 << cprops->cmax_wd), 1);
+		mpam_llc_gran_hisi_workaround(r);
 		break;
 
 	case RDT_RESOURCE_L3_MIN:
@@ -1019,6 +1067,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 
 		r->membw.min_bw = 0;
 		r->membw.bw_gran = max(100 / (1 << cprops->cmax_wd), 1);
+		mpam_llc_gran_hisi_workaround(r);
 		break;
 
 	case RDT_RESOURCE_MB_MIN:
@@ -1249,9 +1298,9 @@ u32 resctrl_arch_get_config(struct rdt_resource *r, struct rdt_domain *d,
 		/* TODO: Scaling is not yet supported */
 		return cfg->cpbm;
 	case mpam_feat_ccap_part:
-		return mbw_max_to_percent(cfg->cmax, cprops->cmax_wd);
+		return ca_max_to_percent(cfg->cmax, cprops->cmax_wd);
 	case mpam_feat_cmin:
-		return mbw_max_to_percent(cfg->cmin, cprops->cmax_wd);
+		return ca_max_to_percent(cfg->cmin, cprops->cmax_wd);
 	case mpam_feat_intpri_part:
 		return cfg->intpri;
 	case mpam_feat_mbw_part:
@@ -1301,12 +1350,12 @@ int resctrl_arch_update_one(struct rdt_resource *r, struct rdt_domain *d,
 		break;
 	case RDT_RESOURCE_L2_MAX:
 	case RDT_RESOURCE_L3_MAX:
-		cfg.cmax = percent_to_mbw_max(cfg_val, cprops->cmax_wd);
+		cfg.cmax = percent_to_ca_max(cfg_val, cprops->cmax_wd);
 		mpam_set_feature(mpam_feat_ccap_part, &cfg);
 		break;
 	case RDT_RESOURCE_L2_MIN:
 	case RDT_RESOURCE_L3_MIN:
-		cfg.cmin = percent_to_mbw_max(cfg_val, cprops->cmax_wd);
+		cfg.cmin = percent_to_ca_max(cfg_val, cprops->cmax_wd);
 		mpam_set_feature(mpam_feat_cmin, &cfg);
 		break;
 	case RDT_RESOURCE_L2_PRI:

commit 8dfbb8460f25f9026e690c767bd70ba22ec899b0

fs/resctrl: Fix the L3 CMAX conversion mechanism
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICXA3G

--------------------------------

RDT formats the precision through bw_validate() during the validity check.
But MPAM does not rely on such conversion, the CMAX interface converts
percentages through percent_to_ca_max() and ca_max_to_percent(). So skip
the conversion in bw_validate() to avoid loss of precision.

Finally, percent_to_ca_max() and ca_max_to_percent() are restored to round
up the user configuration to the required precision, avoiding to pass
values smaller than the minimum threshold as parameters.

Fixes: 9e0a8eb88dc7 ("arm64/mpam: Add quirk for cmax and cmin")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -686,7 +686,7 @@ static u16 percent_to_mbw_max(u32 pc, u8 wd)
 static u16 percent_to_ca_max(u32 pc, u8 wd)
 {
 	struct rdt_resource *l3 = resctrl_arch_get_resource(RDT_RESOURCE_L3);
-	u32 valid_max;
+	u32 valid_max, ca_max;
 
 	if (read_cpuid_implementor() != ARM_CPU_IMP_HISI)
 		return percent_to_mbw_max(pc, wd);
@@ -698,7 +698,8 @@ static u16 percent_to_ca_max(u32 pc, u8 wd)
 	if (pc >= MAX_MBA_BW)
 		return valid_max << (16 - wd);
 
-	return ((pc * valid_max + 50) / 100) << (16 - wd);
+	ca_max = DIV_ROUND_UP(pc * valid_max, 100);
+	return ca_max << (16 - wd);
 }
 
 static u16 ca_max_to_percent(u16 ca_max, u8 wd)
@@ -717,7 +718,7 @@ static u16 ca_max_to_percent(u16 ca_max, u8 wd)
 	if (ca_max >= valid_max)
 		return MAX_MBA_BW;
 
-	return (ca_max * 100 + valid_max / 2) / valid_max;
+	return (ca_max * 100) / valid_max;
 }
 
 /* Test whether we can export MPAM_CLASS_CACHE:{2,3}? */

fs/resctrl/ctrlmondata.c
@@ -65,7 +65,11 @@ static bool bw_validate(char *buf, unsigned long *data, struct rdt_resource *r)
 		return false;
 	}
 
-	*data = roundup(bw, (unsigned long)r->membw.bw_gran);
+	if (!IS_ENABLED(CONFIG_ARM64_MPAM))
+		*data = roundup(bw, (unsigned long)r->membw.bw_gran);
+	else
+		*data = bw;
+
 	return true;
 }
 