OERV-RVCI/rvck-olk

commit 468b0cd7b1e1c131b3ee7d2ea8b96521d3faad8b

net:nebula-matrix:Add S1000 SNIC driver support
nbl_core-driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID0342

--------------------------------

support full-scenario functions

Signed-off-by: nebula_matrix_driver <open@nebula-matrix.com>

drivers/net/ethernet/nebula-matrix/nbl/nbl_core/nbl_dev.h
@@ -8,16 +8,22 @@
 #define _NBL_DEV_H_
 
 #include "nbl_core.h"
+#include "nbl_export_rdma.h"
 #include "nbl_dev_user.h"
+#include "nbl_sysfs.h"
 
 #define NBL_DEV_MGT_TO_COMMON(dev_mgt)		((dev_mgt)->common)
 #define NBL_DEV_MGT_TO_DEV(dev_mgt)		NBL_COMMON_TO_DEV(NBL_DEV_MGT_TO_COMMON(dev_mgt))
 #define NBL_DEV_MGT_TO_COMMON_DEV(dev_mgt)	((dev_mgt)->common_dev)
 #define NBL_DEV_MGT_TO_CTRL_DEV(dev_mgt)	((dev_mgt)->ctrl_dev)
 #define NBL_DEV_MGT_TO_NET_DEV(dev_mgt)		((dev_mgt)->net_dev)
+#define NBL_DEV_MGT_TO_RDMA_DEV(dev_mgt)	((dev_mgt)->rdma_dev)
 #define NBL_DEV_MGT_TO_USER_DEV(dev_mgt)	((dev_mgt)->user_dev)
+#define NBL_DEV_MGT_TO_REP_DEV(dev_mgt)		((dev_mgt)->rep_dev)
 #define NBL_DEV_COMMON_TO_MSIX_INFO(dev_common)	(&(dev_common)->msix_info)
 #define NBL_DEV_CTRL_TO_TASK_INFO(dev_ctrl)	(&(dev_ctrl)->task_info)
+#define NBL_DEV_FACTORY_TO_TASK_INFO(dev_factory)	(&(dev_factory)->task_info)
+#define NBL_DEV_MGT_TO_EMP_CONSOLE(dev_mgt)	((dev_mgt)->emp_console)
 #define NBL_DEV_MGT_TO_NETDEV_OPS(dev_mgt)	((dev_mgt)->net_dev->ops)
 
 #define NBL_DEV_MGT_TO_SERV_OPS_TBL(dev_mgt)	((dev_mgt)->serv_ops_tbl)
@@ -38,26 +44,47 @@
 
 #define NBL_KEEPALIVE_TIME_CYCLE		(10 * HZ)
 
-enum nbl_dev_mode_switch_op {
-	NBL_DEV_KERNEL_TO_USER,
-	NBL_DEV_USER_TO_KERNEL,
+#define NBL_DEV_BATCH_RESET_FUNC_NUM		(32)
+#define NBL_DEV_BATCH_RESET_USEC		(1000000)
+
+#define NBL_TIME_LEN				(32)
+#define NBL_SAVED_TRACES_NUM			(16)
+
+#define NBL_DEV_FW_RESET_WAIT_TIME		(3500)
+
+enum nbl_reset_status {
+	NBL_RESET_INIT,
+	NBL_RESET_SEND,
+	NBL_RESET_DONE,
+	NBL_RESET_STATUS_MAX
 };
 
 struct nbl_task_info {
 	struct nbl_adapter *adapter;
 	struct nbl_dev_mgt *dev_mgt;
+	struct work_struct offload_network_task;
 	struct work_struct fw_hb_task;
 	struct delayed_work fw_reset_task;
 	struct work_struct clean_adminq_task;
+	struct work_struct ipsec_task;
 	struct work_struct adapt_desc_gother_task;
 	struct work_struct clean_abnormal_irq_task;
 	struct work_struct recovery_abnormal_task;
-
+	struct work_struct report_temp_task;
+	struct work_struct report_reboot_task;
+	struct work_struct reset_task;
+	enum nbl_reset_event reset_event;
+	enum nbl_reset_status reset_status[NBL_MAX_FUNC];
 	struct timer_list serv_timer;
 	unsigned long serv_timer_period;
 
 	bool fw_resetting;
 	bool timer_setup;
+};
+
+struct nbl_reset_task_info {
+	struct work_struct task;
+	enum nbl_reset_event event;
 };
 
 enum nbl_msix_serv_type {
@@ -73,6 +100,7 @@ enum nbl_msix_serv_type {
 };
 
 struct nbl_msix_serv_info {
+	char irq_name[NBL_STRING_NAME_LEN];
 	u16 num;
 	u16 base_vector_id;
 	/* true: hw report msix, hw need to mask actively */
@@ -94,10 +122,84 @@ struct nbl_dev_common {
 
 	struct devlink_ops *devlink_ops;
 	struct devlink *devlink;
+	struct nbl_reset_task_info reset_task;
+};
+
+struct nbl_dev_factory {
+	struct nbl_task_info task_info;
+};
+
+enum nbl_dev_temp_status {
+	NBL_TEMP_STATUS_NORMAL = 0,
+	NBL_TEMP_STATUS_WARNING,
+	NBL_TEMP_STATUS_CRIT,
+	NBL_TEMP_STATUS_EMERG,
+	NBL_TEMP_STATUS_MAX
+};
+
+enum nbl_emp_log_level {
+	NBL_EMP_ALERT_LOG_FATAL = 0,
+	NBL_EMP_ALERT_LOG_ERROR = 1,
+	NBL_EMP_ALERT_LOG_WARNING = 2,
+	NBL_EMP_ALERT_LOG_INFO = 3,
+};
+
+struct nbl_fw_reporter_ctx {
+	u64 timestamp;
+	u32 temp_num;
+	char reboot_report_time[NBL_TIME_LEN];
+};
+
+struct nbl_fw_temp_trace_data {
+	u64 timestamp;
+	u32 temp_num;
+};
+
+struct nbl_fw_reboot_trace_data {
+	char local_time[NBL_TIME_LEN];
+};
+
+struct nbl_health_reporters {
+	struct {
+		struct nbl_fw_temp_trace_data trace_data[NBL_SAVED_TRACES_NUM];
+		u8 saved_traces_index;
+		struct mutex lock; /* protect reading data of temp_trace_data*/
+	} temp_st_arr;
+
+	struct {
+		struct nbl_fw_reboot_trace_data trace_data[NBL_SAVED_TRACES_NUM];
+		u8 saved_traces_index;
+		struct mutex lock; /* protect reading data of reboot_trace_data*/
+	} reboot_st_arr;
+
+	struct nbl_fw_reporter_ctx reporter_ctx;
+	struct devlink_health_reporter *fw_temp_reporter;
+	struct devlink_health_reporter *fw_reboot_reporter;
 };
 
 struct nbl_dev_ctrl {
 	struct nbl_task_info task_info;
+	enum nbl_dev_temp_status temp_status;
+	struct nbl_health_reporters health_reporters;
+};
+
+enum nbl_dev_emp_alert_event {
+	NBL_EMP_EVENT_TEMP_ALERT = 1,
+	NBL_EMP_EVENT_LOG_ALERT = 2,
+	NBL_EMP_EVENT_MAX
+};
+
+enum nbl_dev_temp_threshold {
+	NBL_TEMP_NOMAL_THRESHOLD = 85,
+	NBL_TEMP_WARNING_THRESHOLD = 105,
+	NBL_TEMP_CRIT_THRESHOLD = 115,
+	NBL_TEMP_EMERG_THRESHOLD = 120,
+};
+
+struct nbl_dev_temp_alarm_info {
+	int logvel;
+#define NBL_TEMP_ALARM_STR_LEN		128
+	char alarm_info[NBL_TEMP_ALARM_STR_LEN];
 };
 
 struct nbl_dev_vsi_controller {
@@ -111,17 +213,81 @@ struct nbl_dev_net_ops {
 				struct nbl_init_param *param);
 	int (*setup_ethtool_ops)(void *priv, struct net_device *netdev,
 				 struct nbl_init_param *param);
+	int (*setup_dcbnl_ops)(void *priv, struct net_device *netdev,
+			       struct nbl_init_param *param);
+};
+
+struct nbl_dev_attr_info {
+	struct nbl_netdev_name_attr dev_name_attr;
 };
 
 struct nbl_dev_net {
 	struct net_device *netdev;
+	struct nbl_dev_attr_info dev_attr;
+	struct nbl_lag_member *lag_mem;
 	struct nbl_dev_net_ops *ops;
+	u8 lag_inited;
+	u8 eth_id;
 	struct nbl_dev_vsi_controller vsi_ctrl;
 	u16 total_queue_num;
 	u16 kernel_queue_num;
 	u16 user_queue_num;
-	u8 eth_id;
-	u8 resv;
+	u16 total_vfs;
+	struct nbl_net_qos qos_config;
+	struct nbl_net_mirror mirror_config;
+};
+
+struct nbl_dev_virtio {
+	u8 device_msix;
+};
+
+struct nbl_dev_rdma_event_data {
+	struct list_head node;
+	/* Lag event will be processed async, so we need to fully store the param in case it is
+	 * released by caller.
+	 *
+	 * callback_data will always be dev_mgt, which will not be released, so don't bother.
+	 */
+	struct nbl_event_param event_data;
+	void *callback_data;
+	u16 type;
+};
+
+struct nbl_dev_rdma {
+	struct auxiliary_device *adev;
+	struct auxiliary_device *grc_adev;
+	struct auxiliary_device *bond_adev;
+
+	struct work_struct abnormal_event_task;
+
+	struct work_struct event_task;
+	struct list_head event_param_list;
+	struct mutex event_lock;		/* Protect event_param_list */
+
+	int adev_index;
+	u32 mem_type;
+	bool has_rdma;
+	bool has_grc;
+	u16 func_id;
+	u16 lag_id;
+	bool bond_registered;
+	bool bond_shaping_configed;
+
+	bool is_halting;
+	bool event_ready;
+	bool mirror_enable;
+	bool has_abnormal_event_task;
+	atomic_t adev_busy;
+};
+
+struct nbl_dev_emp_console {
+	struct nbl_dev_mgt *dev_mgt;
+	unsigned int id;
+	atomic_t opened;
+	wait_queue_head_t wait;
+	struct cdev cdev;
+	struct kfifo rx_fifo;
+	struct ktermios termios;
 };
 
 struct nbl_dev_user_iommu_group {
@@ -131,22 +297,39 @@ struct nbl_dev_user_iommu_group {
 	struct rb_root dma_tree;
 	struct iommu_group *iommu_group;
 	struct device *dev;
+	struct device *mdev;
 	struct vfio_device *vdev;
 };
 
 struct nbl_dev_user {
-	struct vfio_device vdev;
-	struct device *mdev;
+	struct vfio_device *vdev;
+	struct device mdev;
 	struct notifier_block iommu_notifier;
 	struct device *dev;
 	struct nbl_adapter *adapter;
 	struct nbl_dev_user_iommu_group *group;
 	void *shm_msg_ring;
+	u64 dma_limit;
+	atomic_t open_cnt;
 	int minor;
+	int network_type;
 	bool iommu_status;
 	bool remap_status;
-	int network_type;
-	atomic_t open_cnt;
+	bool user_promisc_mode;
+	bool user_mcast_mode;
+	u16 user_vsi;
+};
+
+struct nbl_vfio_device {
+	struct vfio_device vdev;
+	struct nbl_dev_user *user;
+};
+
+#define NBL_USERDEV_TO_VFIO_DEV(user)	((user)->vdev)
+#define NBL_VFIO_DEV_TO_USERDEV(vdev)	(*(struct nbl_dev_user **)((vdev) + 1))
+struct nbl_dev_rep {
+	struct nbl_rep_data *rep;
+	int num_vfs;
 };
 
 struct nbl_dev_mgt {
@@ -156,6 +339,9 @@ struct nbl_dev_mgt {
 	struct nbl_dev_common *common_dev;
 	struct nbl_dev_ctrl *ctrl_dev;
 	struct nbl_dev_net *net_dev;
+	struct nbl_dev_rdma *rdma_dev;
+	struct nbl_dev_emp_console *emp_console;
+	struct nbl_dev_rep *rep_dev;
 	struct nbl_dev_user *user_dev;
 };
 
@@ -171,8 +357,8 @@ struct nbl_dev_vsi_ops {
 	int (*setup)(struct nbl_dev_mgt *dev_mgt, struct nbl_init_param *param,
 		     void *vsi_data);
 	void (*remove)(struct nbl_dev_mgt *dev_mgt, void *vsi_data);
-	int (*start)(struct nbl_dev_mgt *dev_mgt, struct net_device *netdev, void *vsi_data);
-	void (*stop)(struct nbl_dev_mgt *dev_mgt, void *vsi_data);
+	int (*start)(void *dev_priv, struct net_device *netdev, void *vsi_data);
+	void (*stop)(void *dev_priv, void *vsi_data);
 	int (*netdev_build)(struct nbl_dev_mgt *dev_mgt, struct nbl_init_param *param,
 			    struct net_device *netdev, void *vsi_data);
 	void (*netdev_destroy)(struct nbl_dev_mgt *dev_mgt, void *vsi_data);
@@ -191,6 +377,8 @@ struct nbl_dev_vsi {
 	u16 in_kernel;
 	u8 index;
 	bool enable;
+	bool use_independ_irq;
+	bool static_queue;
 };
 
 struct nbl_dev_vsi_tbl {
@@ -198,11 +386,13 @@ struct nbl_dev_vsi_tbl {
 	bool vf_support;
 	bool only_nic_support;
 	u16 in_kernel;
+	bool use_independ_irq;
+	bool static_queue;
 };
 
 #define NBL_DEV_BOARD_ID_MAX			NBL_DRIVER_DEV_MAX
 struct nbl_dev_board_id_entry {
-	u16 bus;
+	u32 board_key; /* domain << 16 | bus_id */
 	u8 refcount;
 	bool valid;
 };
@@ -211,7 +401,29 @@ struct nbl_dev_board_id_table {
 	struct nbl_dev_board_id_entry entry[NBL_DEV_BOARD_ID_MAX];
 };
 
+int nbl_dev_setup_rdma_dev(struct nbl_adapter *adapter, struct nbl_init_param *param);
+void nbl_dev_remove_rdma_dev(struct nbl_adapter *adapter);
+int nbl_dev_start_rdma_dev(struct nbl_adapter *adapter);
+void nbl_dev_stop_rdma_dev(struct nbl_adapter *adapter);
+int nbl_dev_resume_rdma_dev(struct nbl_adapter *adapter);
+int nbl_dev_suspend_rdma_dev(struct nbl_adapter *adapter);
+void nbl_dev_rdma_process_abnormal_event(struct nbl_dev_rdma *rdma_dev);
+void nbl_dev_rdma_process_flr_event(struct nbl_dev_rdma *rdma_dev, u16 vsi_id);
+size_t nbl_dev_rdma_qos_cfg_store(struct nbl_dev_mgt *dev_mgt, int offset,
+				  const char *buf, size_t count);
+size_t nbl_dev_rdma_qos_cfg_show(struct nbl_dev_mgt *dev_mgt, int offset, char *buf);
+
+int nbl_dev_init_emp_console(struct nbl_adapter *adapter);
+void nbl_dev_destroy_emp_console(struct nbl_adapter *adapter);
 int nbl_dev_setup_hwmon(struct nbl_adapter *adapter);
 void nbl_dev_remove_hwmon(struct nbl_adapter *adapter);
+struct nbl_dev_vsi *nbl_dev_vsi_select(struct nbl_dev_mgt *dev_mgt, u8 vsi_index);
 
+int nbl_netdev_add_sysfs(struct net_device *netdev, struct nbl_dev_net *net_dev);
+int nbl_netdev_add_mirror_sysfs(struct net_device *netdev, struct nbl_dev_net *net_dev);
+void nbl_netdev_remove_sysfs(struct nbl_dev_net *net_dev);
+void nbl_netdev_remove_mirror_sysfs(struct nbl_dev_net *net_dev);
+void nbl_net_add_name_attr(struct nbl_netdev_name_attr *dev_name_attr, char *rep_name);
+void nbl_net_remove_dev_attr(struct nbl_dev_net *net_dev);
 #endif
+

commit c83cda743e4b3b6d71e36276bf3dc2b35dc64515

net:nebula-matrix:fix compile err
nbl_core-driver inclusion
category: bugfix
bugzilla: https://gitee.com/src-openeuler/kernel/issues/ID5BWD

------------------------------------------------------------

fix compile err

Fixes: 468b0cd7b1e1 ("net:nebula-matrix:Add S1000 SNIC driver support")

Signed-off-by: nebula_matrix_driver <open@nebula-matrix.com>

drivers/net/ethernet/nebula-matrix/nbl/nbl_core/nbl_dev.c
@@ -679,6 +679,7 @@ static void nbl_dev_clear_interrupt_scheme(struct nbl_dev_mgt *dev_mgt)
 	nbl_dev_free_msix_intr(dev_mgt);
 }
 
+#ifdef CONFIG_NET_DEVLINK
 static void nbl_fw_tracer_clean_saved_traces_array(struct nbl_health_reporters *reps)
 {
 	mutex_destroy(&reps->temp_st_arr.lock);
@@ -735,9 +736,11 @@ static void nbl_fw_reboot_save_trace(struct nbl_health_reporters *reps)
 		(reps->reboot_st_arr.saved_traces_index + 1) & (NBL_SAVED_TRACES_NUM - 1);
 	mutex_unlock(&reps->reboot_st_arr.lock);
 }
+#endif
 
 static void nbl_dev_health_report_temp_task(struct work_struct *work)
 {
+#ifdef CONFIG_NET_DEVLINK
 	struct nbl_fw_reporter_ctx fw_reporter_cxt;
 	struct nbl_task_info *task_info = container_of(work, struct nbl_task_info,
 						       report_temp_task);
@@ -754,10 +757,12 @@ static void nbl_dev_health_report_temp_task(struct work_struct *work)
 	err = devlink_health_report(reps->fw_temp_reporter, "nbl_fw_temp", &fw_reporter_cxt);
 	if (err)
 		dev_err(dev, "failed to report nbl_fw_temp health\n");
+#endif
 }
 
 static void nbl_dev_health_report_reboot_task(struct work_struct *work)
 {
+#ifdef CONFIG_NET_DEVLINK
 	struct nbl_task_info *task_info = container_of(work, struct nbl_task_info,
 						       report_reboot_task);
 	struct nbl_dev_mgt *dev_mgt = task_info->dev_mgt;
@@ -772,6 +777,7 @@ static void nbl_dev_health_report_reboot_task(struct work_struct *work)
 	if (err) {
 		dev_err(dev, "failed to report nbl_fw_reboot health\n");
 	}
+#endif
 }
 
 /* ----------  Channel config  ---------- */
@@ -972,7 +978,9 @@ static void nbl_dev_fw_reset_task(struct work_struct *work)
 	struct nbl_channel_ops *chan_ops = NBL_DEV_MGT_TO_CHAN_OPS(dev_mgt);
 	struct nbl_service_ops *serv_ops = NBL_DEV_MGT_TO_SERV_OPS(dev_mgt);
 	struct nbl_common_info *common = NBL_DEV_MGT_TO_COMMON(dev_mgt);
+#ifdef CONFIG_NET_DEVLINK
 	struct nbl_dev_ctrl *ctrl_dev = NBL_DEV_MGT_TO_CTRL_DEV(dev_mgt);
+#endif
 
 	if (serv_ops->check_fw_reset(NBL_DEV_MGT_TO_SERV_PRIV(dev_mgt))) {
 		dev_notice(NBL_COMMON_TO_DEV(common), "FW recovered");
@@ -994,7 +1002,9 @@ static void nbl_dev_fw_reset_task(struct work_struct *work)
 			nbl_dev_enable_port(dev_mgt, true);
 		}
 		task_info->fw_resetting = false;
+#ifdef CONFIG_NET_DEVLINK
 		nbl_fw_reboot_save_trace(&ctrl_dev->health_reporters);
+#endif
 		nbl_common_queue_work(&task_info->report_reboot_task, true, false);
 		return;
 	}
@@ -1174,7 +1184,9 @@ static void nbl_dev_handle_temp_ext(struct nbl_dev_mgt *dev_mgt, u8 *data, u16 d
 		uptime = *(u64 *)(data + sizeof(u16));
 	if (new_temp_status != NBL_TEMP_STATUS_NORMAL) {
 		ctrl_dev->health_reporters.reporter_ctx.temp_num = temp;
+#ifdef CONFIG_NET_DEVLINK
 		nbl_fw_temp_save_trace(&ctrl_dev->health_reporters, temp, uptime);
+#endif
 		nbl_common_queue_work(&ctrl_dev->task_info.report_temp_task, false, false);
 	}
 	nbl_log(common, temp_alarm_info[new_temp_status].logvel,
@@ -1469,6 +1481,7 @@ static void nbl_dev_remove_common_dev(struct nbl_adapter *adapter)
 	NBL_DEV_MGT_TO_COMMON_DEV(dev_mgt) = NULL;
 }
 
+#ifdef CONFIG_NET_DEVLINK
 static void nbl_devlink_fmsg_fill_temp_trace(struct devlink_fmsg *fmsg,
 					     struct nbl_fw_temp_trace_data *trace_data)
 {
@@ -1653,6 +1666,7 @@ static int nbl_dev_health_init(struct nbl_dev_mgt *dev)
 	nbl_setup_devlink_reporter(dev);
 	return 0;
 }
+#endif
 
 static int nbl_dev_setup_ctrl_dev(struct nbl_adapter *adapter, struct nbl_init_param *param)
 {
@@ -2960,9 +2974,9 @@ static void nbl_dev_remove_ktls_ops(struct net_device *netdev)
 }
 
 #else
-static ivoidnt nbl_dev_setup_ktls_ops(struct nbl_dev_mgt *dev_mgt, struct net_device *netdev
-				      struct nbl_init_param *param)
+static int nbl_dev_setup_ktls_ops(struct nbl_dev_mgt *dev_mgt, struct net_device *netdev)
 {
+	return 0;
 }
 
 static void nbl_dev_remove_ktls_ops(struct net_device *netdev) {}
@@ -3081,6 +3095,7 @@ static void nbl_dev_remove_xfrm_ops(struct net_device *netdev)
 }
 #endif
 
+#ifdef CONFIG_DCB
 static int nbl_dev_ieee_setets(struct net_device *netdev, struct ieee_ets *ets)
 {
 	struct nbl_adapter *adapter = NBL_NETDEV_TO_ADAPTER(netdev);
@@ -3248,6 +3263,7 @@ static void nbl_dev_remove_dcbnl_ops(struct net_device *netdev)
 {
 	netdev->dcbnl_ops = NULL;
 }
+#endif
 
 static void nbl_dev_set_eth_mac_addr(struct nbl_dev_mgt *dev_mgt, struct net_device *netdev)
 {
@@ -3307,10 +3323,11 @@ static int nbl_dev_cfg_netdev(struct net_device *netdev, struct nbl_dev_mgt *dev
 	ret = net_dev_ops->setup_ethtool_ops(dev_mgt, netdev, param);
 	if (ret)
 		goto set_ethtool_fail;
-
+#ifdef CONFIG_DCB
 	ret = net_dev_ops->setup_dcbnl_ops(dev_mgt, netdev, param);
 	if (ret)
 		goto set_dcbnl_fail;
+#endif
 
 	if (!param->is_rep) {
 		ret = nbl_dev_setup_ktls_ops(dev_mgt, netdev);
@@ -3328,8 +3345,10 @@ static int nbl_dev_cfg_netdev(struct net_device *netdev, struct nbl_dev_mgt *dev
 set_xfrm_fail:
 	nbl_dev_remove_ktls_ops(netdev);
 set_ktls_fail:
+#ifdef CONFIG_DCB
 	nbl_dev_remove_dcbnl_ops(netdev);
 set_dcbnl_fail:
+#endif
 	nbl_dev_remove_ethtool_ops(netdev);
 set_ethtool_fail:
 	nbl_dev_remove_netops(netdev);
@@ -3341,7 +3360,9 @@ static void nbl_dev_reset_netdev(struct net_device *netdev)
 {
 	nbl_dev_remove_ktls_ops(netdev);
 	nbl_dev_remove_xfrm_ops(netdev);
+#ifdef CONFIG_DCB
 	nbl_dev_remove_dcbnl_ops(netdev);
+#endif
 	nbl_dev_remove_ethtool_ops(netdev);
 	nbl_dev_remove_netops(netdev);
 }
@@ -4028,7 +4049,9 @@ static struct nbl_dev_net_ops netdev_ops[NBL_PRODUCT_MAX] = {
 	{
 		.setup_netdev_ops	= nbl_dev_setup_netops_leonis,
 		.setup_ethtool_ops	= nbl_dev_setup_ethtool_ops_leonis,
+		#ifdef CONFIG_DCB
 		.setup_dcbnl_ops	= nbl_dev_setup_dcbnl_ops_leonis,
+		#endif
 	},
 };
 
@@ -4382,9 +4405,9 @@ static int nbl_dev_start_ctrl_dev(struct nbl_adapter *adapter, struct nbl_init_p
 	err = nbl_dev_enable_adminq_irq(dev_mgt);
 	if (err)
 		goto enable_adminq_irq_err;
-
+#ifdef CONFIG_NET_DEVLINK
 	nbl_dev_health_init(dev_mgt);
-
+#endif
 	nbl_dev_get_port_attributes(dev_mgt);
 	nbl_dev_init_port(dev_mgt);
 	nbl_dev_enable_port(dev_mgt, true);
@@ -4412,7 +4435,9 @@ static void nbl_dev_stop_ctrl_dev(struct nbl_adapter *adapter)
 	nbl_dev_ctrl_task_stop(dev_mgt);
 	nbl_dev_enable_port(dev_mgt, false);
 	nbl_dev_disable_adminq_irq(dev_mgt);
+#ifdef CONFIG_NET_DEVLINK
 	nbl_dev_destroy_health(dev_mgt);
+#endif
 	nbl_dev_free_adminq_irq(dev_mgt, &NBL_DEV_MGT_TO_CTRL_DEV(dev_mgt)->task_info);
 	nbl_dev_disable_abnormal_irq(dev_mgt);
 	nbl_dev_free_abnormal_irq(dev_mgt);
@@ -4624,6 +4649,7 @@ static int nbl_dev_eswitch_unload_rep(struct nbl_dev_mgt *dev_mgt)
 	return 0;
 }
 
+#ifdef CONFIG_NET_DEVLINK
 static int nbl_dev_eswitch_mode_to_devlink(u16 cur_eswitch_mode, u16 *devlink_eswitch_mode)
 {
 	switch (cur_eswitch_mode) {
@@ -4653,6 +4679,7 @@ static int nbl_dev_eswitch_mode_from_devlink(u16 devlink_eswitch_mode, u16 *cfg_
 	}
 	return 0;
 }
+#endif
 
 int nbl_dev_destroy_rep(void *p)
 {
@@ -4739,6 +4766,7 @@ void nbl_dev_remove_vf_config(void *p)
 	return serv_ops->remove_vf_config(NBL_DEV_MGT_TO_SERV_PRIV(dev_mgt));
 }
 
+#ifdef CONFIG_NET_DEVLINK
 static int nbl_dev_init_offload_mode(struct nbl_dev_mgt *dev_mgt, u16 vsi_id)
 {
 	struct nbl_service_ops *serv_ops = NBL_DEV_MGT_TO_SERV_OPS(dev_mgt);
@@ -4801,6 +4829,13 @@ fail_unset_upcall_rule:
 	serv_ops->disable_phy_flow(NBL_DEV_MGT_TO_SERV_PRIV(dev_mgt), common->eth_id);
 	return ret;
 }
+#else
+
+static int nbl_dev_uninit_offload_mode(struct nbl_dev_mgt *dev_mgt)
+{
+	return 0;
+}
+#endif
 
 static void nbl_dev_destroy_flow_res(struct nbl_dev_mgt *dev_mgt)
 {
@@ -5133,6 +5168,8 @@ static void nbl_dev_suspend_net_dev(struct nbl_adapter *adapter)
 	nbl_dev_free_net_irq(dev_mgt);
 }
 
+/* ----------  Devlink config  ---------- */
+#ifdef CONFIG_NET_DEVLINK
 static int nbl_dev_get_devlink_eswitch_mode(struct devlink *devlink, u16 *mode)
 {
 	struct nbl_devlink_priv *priv = devlink_priv(devlink);
@@ -5241,7 +5278,7 @@ fail_cfg_rep:
 	}
 	return -EBUSY;
 }
-/* ----------  Devlink config  ---------- */
+
 static void nbl_dev_devlink_free(void *devlink_ptr)
 {
 	devlink_free((struct devlink *)devlink_ptr);
@@ -5304,6 +5341,16 @@ static void nbl_dev_remove_devlink(struct nbl_dev_mgt *dev_mgt)
 	}
 }
 
+#else
+static int nbl_dev_setup_devlink(struct nbl_dev_mgt *dev_mgt, struct nbl_init_param *param)
+{
+	return 0;
+}
+
+static void nbl_dev_remove_devlink(struct nbl_dev_mgt *dev_mgt)
+{
+}
+#endif
 static int nbl_dev_start_common_dev(struct nbl_adapter *adapter, struct nbl_init_param *param)
 {
 	struct nbl_dev_mgt *dev_mgt = (struct nbl_dev_mgt *)NBL_ADAPTER_TO_DEV_MGT(adapter);
@@ -5325,11 +5372,9 @@ static int nbl_dev_start_common_dev(struct nbl_adapter *adapter, struct nbl_init
 	ret = nbl_dev_enable_mailbox_irq(dev_mgt);
 	if (ret)
 		goto enable_mailbox_irq_err;
-
 	ret = nbl_dev_setup_devlink(dev_mgt, param);
 	if (ret)
 		goto setup_devlink_err;
-
 	if (!param->caps.is_vf &&
 	    serv_ops->get_product_fix_cap(NBL_DEV_MGT_TO_SERV_PRIV(dev_mgt),
 	    NBL_HWMON_TEMP_CAP)) {

drivers/net/ethernet/nebula-matrix/nbl/nbl_core/nbl_dev.h
@@ -213,8 +213,10 @@ struct nbl_dev_net_ops {
 				struct nbl_init_param *param);
 	int (*setup_ethtool_ops)(void *priv, struct net_device *netdev,
 				 struct nbl_init_param *param);
+#ifdef CONFIG_DCB
 	int (*setup_dcbnl_ops)(void *priv, struct net_device *netdev,
 			       struct nbl_init_param *param);
+#endif
 };
 
 struct nbl_dev_attr_info {

drivers/net/ethernet/nebula-matrix/nbl/nbl_core/nbl_dev_user.c
@@ -1387,13 +1387,11 @@ static int nbl_bus_probe(struct device *dev)
 		return PTR_ERR(vdev);
 	user->vdev = &vdev->vdev;
 	vdev->user = user;
-
 	ret = vfio_register_emulated_iommu_dev(NBL_USERDEV_TO_VFIO_DEV(user));
 	if (ret) {
 		dev_err(dev, "vfio register iommu failed, ret %d\n", ret);
 		vfio_put_device(NBL_USERDEV_TO_VFIO_DEV(user));
 	}
-
 	return ret;
 }
 

drivers/net/ethernet/nebula-matrix/nbl/nbl_core/nbl_service.c
@@ -4628,7 +4628,9 @@ static void nbl_init_qos_config(struct nbl_serv_net_resource_mgt *net_resource_m
 	qos_info->rdma_bw = NBL_MAX_BW >> 1;
 	qos_info->rdma_rate = NBL_COMMON_TO_ETH_MAX_SPEED(common);
 	qos_info->net_rate = NBL_COMMON_TO_ETH_MAX_SPEED(common);
+#ifdef CONFIG_DCB
 	qos_info->dcbx_mode = DCB_CAP_DCBX_HOST | DCB_CAP_DCBX_VER_IEEE | DCB_CAP_DCBX_VER_CEE;
+#endif
 	for (i = 0; i < NBL_DSCP_MAX; i++)
 		qos_info->dscp2prio_map[i] = i / NBL_MAX_PFC_PRIORITIES;
 
@@ -4886,6 +4888,7 @@ static void nbl_serv_get_real_bdf(void *priv, u16 vsi_id, u8 *bus, u8 *dev, u8 *
 				      bus, dev, function);
 }
 
+#ifdef CONFIG_NET_DEVLINK
 static int nbl_serv_get_devlink_info(struct devlink *devlink, struct devlink_info_req *req,
 				     struct netlink_ext_ack *extack)
 {
@@ -4906,6 +4909,7 @@ static int nbl_serv_get_devlink_info(struct devlink *devlink, struct devlink_inf
 
 	return ret;
 }
+#endif
 
 /* Why do we need this?
  * Because the original function in kernel cannot handle when we set subvendor and subdevice
@@ -5063,6 +5067,7 @@ static const struct pldmfw_ops nbl_update_fw_ops = {
 int nbl_serv_update_firmware(struct nbl_service_mgt *serv_mgt, const struct firmware *fw,
 			     struct netlink_ext_ack *extack)
 {
+#ifdef CONFIG_FW_PLDM
 	struct nbl_serv_update_fw_priv priv = {{0}};
 	int ret = 0;
 
@@ -5074,8 +5079,12 @@ int nbl_serv_update_firmware(struct nbl_service_mgt *serv_mgt, const struct firm
 	ret = pldmfw_flash_image(&priv.context, fw);
 
 	return ret;
+#else
+	return 0;
+#endif
 }
 
+#ifdef CONFIG_NET_DEVLINK
 static int nbl_serv_update_devlink_flash(struct devlink *devlink,
 					 struct devlink_flash_update_params *params,
 					 struct netlink_ext_ack *extack)
@@ -5096,6 +5105,7 @@ static int nbl_serv_update_devlink_flash(struct devlink *devlink,
 						   NULL, 0, 0);
 	return ret;
 }
+#endif
 
 static u32 nbl_serv_get_adminq_tx_buf_size(void *priv)
 {
@@ -6964,6 +6974,7 @@ static ssize_t nbl_serv_pfc_buffer_size_show(void *priv, u8 eth_id, char *buf)
 	return count;
 }
 
+#ifdef CONFIG_DCB
 static u8 nbl_serv_dcb_get_num_tc(struct net_device *netdev, struct ieee_ets *ets)
 {
 	bool tc_unused = false;
@@ -7310,6 +7321,7 @@ static u8 nbl_serv_dcnbl_getpfcstate(struct net_device *netdev)
 
 	return 0;
 }
+#endif
 
 static void nbl_serv_get_board_info(void *priv, struct nbl_board_port_info *board_info)
 {
@@ -7591,8 +7603,10 @@ static struct nbl_service_ops serv_ops = {
 	.configure_mirror = nbl_serv_configure_mirror,
 	.configure_mirror_table = nbl_serv_configure_mirror_table,
 	.clear_mirror_cfg = nbl_serv_clear_mirror_cfg,
+#ifdef CONFIG_NET_DEVLINK
 	.get_devlink_info = nbl_serv_get_devlink_info,
 	.update_devlink_flash = nbl_serv_update_devlink_flash,
+#endif
 	.get_adminq_tx_buf_size = nbl_serv_get_adminq_tx_buf_size,
 	.emp_console_write = nbl_serv_emp_console_write,
 
@@ -7631,7 +7645,7 @@ static struct nbl_service_ops serv_ops = {
 	.pfc_buffer_size_show = nbl_serv_pfc_buffer_size_show,
 	.set_pfc_buffer_size = nbl_serv_set_pfc_buffer_size,
 	.set_rate_limit = nbl_serv_set_rate_limit,
-
+	#ifdef CONFIG_DCB
 	.ieee_setets = nbl_serv_ieee_setets,
 	.ieee_getets = nbl_serv_ieee_getets,
 	.ieee_setpfc = nbl_serv_ieee_setpfc,
@@ -7647,6 +7661,7 @@ static struct nbl_service_ops serv_ops = {
 	.dcbnl_getstate = nbl_serv_dcnbl_getstate,
 	.dcbnl_setstate = nbl_serv_dcnbl_setstate,
 	.dcbnl_getpfcstate = nbl_serv_dcnbl_getpfcstate,
+	#endif
 	.get_vf_function_id = nbl_serv_get_vf_function_id,
 	.cfg_mirror_outputport_event = nbl_serv_cfg_mirror_outputport_event,
 };

drivers/net/ethernet/nebula-matrix/nbl/nbl_core/nbl_service.h
@@ -282,9 +282,11 @@ struct nbl_serv_qos_info {
 	u32 rdma_rate;
 	u32 net_rate;
 	DECLARE_BITMAP(dscp_mapped, NBL_DSCP_MAX);
+	#ifdef CONFIG_DCB
 	struct dcb_app app[NBL_DSCP_MAX];
-	int buffer_sizes[NBL_MAX_PFC_PRIORITIES][2];
 	struct ieee_ets ets;
+	#endif
+	int buffer_sizes[NBL_MAX_PFC_PRIORITIES][2];
 };
 
 struct nbl_serv_net_resource_mgt {

drivers/net/ethernet/nebula-matrix/nbl/nbl_core/nbl_tc_tun.c
@@ -94,7 +94,7 @@ static int nbl_route_lookup_ipv4(const struct nbl_common_info *common,
 
 	out_dev = rt->dst.dev;
 	if (is_vlan_dev(out_dev)) {
-		parent_dev = vlan_dev_priv(out_dev)->real_dev;
+		parent_dev = vlan_dev_real_dev(out_dev);
 		if (is_vlan_dev(parent_dev)) {
 			nbl_debug(common, NBL_DEBUG_FLOW, "encap o_dev is %s p_dev:%s\n",
 				  out_dev->name, parent_dev ? parent_dev->name : "NULL");
@@ -318,7 +318,7 @@ static int nbl_route_lookup_ipv6(const struct nbl_common_info *common,
 
 	out_dev = dst->dev;
 	if (is_vlan_dev(out_dev)) {
-		parent_dev = vlan_dev_priv(out_dev)->real_dev;
+		parent_dev = vlan_dev_real_dev(out_dev);
 		real_out_dev = vlan_dev_real_dev(out_dev);
 		if (is_vlan_dev(parent_dev)) {
 			nbl_debug(common, NBL_DEBUG_FLOW, "ipv6 encap o_dev is %s, p_dev:%s\n",

drivers/net/ethernet/nebula-matrix/nbl/nbl_include/nbl_def_service.h
@@ -271,13 +271,13 @@ struct nbl_service_ops {
 	u16 (*covert_vfid_to_vsi_id)(void *priv, u16 vfid);
 	void (*recovery_abnormal)(void *priv);
 	void (*keep_alive)(void *priv);
-
+#ifdef CONFIG_NET_DEVLINK
 	int (*get_devlink_info)(struct devlink *devlink, struct devlink_info_req *req,
 				struct netlink_ext_ack *extack);
 	int (*update_devlink_flash)(struct devlink *devlink,
 				    struct devlink_flash_update_params *params,
 				    struct netlink_ext_ack *extack);
-
+#endif
 	u32 (*get_adminq_tx_buf_size)(void *priv);
 	int (*emp_console_write)(void *priv, char *buf, size_t count);
 	bool (*check_fw_heartbeat)(void *priv);
@@ -345,6 +345,7 @@ struct nbl_service_ops {
 	ssize_t (*pfc_buffer_size_show)(void *priv, u8 eth_id, char *buf);
 
 	/* dcb nl ops */
+	#ifdef CONFIG_DCB
 	int (*ieee_setets)(struct net_device *netdev, struct ieee_ets *ets);
 	int (*ieee_getets)(struct net_device *netdev, struct ieee_ets *ets);
 	int (*ieee_setpfc)(struct net_device *netdev, struct ieee_pfc *pfc);
@@ -360,6 +361,7 @@ struct nbl_service_ops {
 	u8 (*dcbnl_setstate)(struct net_device *netdev, u8 state);
 	u8 (*dcbnl_getpfcstate)(struct net_device *netdev);
 	u8 (*dcbnl_getcap)(struct net_device *netdev, int capid, u8 *cap);
+	#endif
 	u16 (*get_vf_function_id)(void *priv, int vf_id);
 	void (*cfg_mirror_outputport_event)(void *priv, bool enable);
 };