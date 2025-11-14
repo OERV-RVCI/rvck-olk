openeuler-mirror/raspberrypi-kernel

commit 642aa73254943d1eca875f6c2b759b99fdf710d8

uacce: add stop_queue to replace put_queue in ioctl
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IB9H0P
CVE: NA

----------------------------------------------------------------------

Since the resources of uacce_queue cannot be ensured to be fully
released before calling put_queue, directly putting the queue might
pose a slight risk.

However, the release interface can guarantee that the resources have
already been released. Therefore, putting the queue through the
release interface is a reliable action.

Signed-off-by: Yang Shen <shenyang39@huawei.com>
Signed-off-by: JiangShui Yang <yangjiangshui@h-partners.com>

drivers/misc/uacce/uacce.c
@@ -47,20 +47,26 @@ static int uacce_start_queue(struct uacce_queue *q)
 	return 0;
 }
 
-static int uacce_put_queue(struct uacce_queue *q)
+static int uacce_stop_queue(struct uacce_queue *q)
 {
 	struct uacce_device *uacce = q->uacce;
 
 	if ((q->state == UACCE_Q_STARTED) && uacce->ops->stop_queue)
 		uacce->ops->stop_queue(q);
 
-	if ((q->state == UACCE_Q_INIT || q->state == UACCE_Q_STARTED) &&
-	     uacce->ops->put_queue)
-		uacce->ops->put_queue(q);
-
 	q->state = UACCE_Q_ZOMBIE;
 
 	return 0;
+}
+
+static void uacce_put_queue(struct uacce_queue *q)
+{
+	struct uacce_device *uacce = q->uacce;
+
+	uacce_stop_queue(q);
+
+	if (uacce->ops->put_queue)
+		uacce->ops->put_queue(q);
 }
 
 static long uacce_cmd_share_qfr(struct uacce_queue *src, int fd)
@@ -222,7 +228,7 @@ static long uacce_fops_unl_ioctl(struct file *filep,
 		ret = uacce_start_queue(q);
 		break;
 	case UACCE_CMD_PUT_Q:
-		ret = uacce_put_queue(q);
+		ret = uacce_stop_queue(q);
 		break;
 	case UACCE_CMD_SHARE_SVAS:
 		ret = uacce_cmd_share_qfr(q, (int)arg);

OERV-RVCI/rvck-olk

commit 166b2a3797ed26232a50049dce1fb0a27eaf47cb

misc: uacce - fix a null pointer access issue when poweroff
driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID6IAP
CVE: NA

----------------------------------------------------------------------

When "poweroff -f" is call in virt, if there are accelerator
services that have not yet completed, a situation may arise where
uacce_fops_release and uacce_remove are executed concurrently.
This can lead to the uacce_put_queue operation in
uacce_fops_release accessing an empty ops pointer.

Fixes: 642aa7325494 ("uacce: add stop_queue to replace put_queue in ioctl")
Signed-off-by: Chenghai Huang <huangchenghai2@huawei.com>
Signed-off-by: JiangShui Yang <yangjiangshui@h-partners.com>

drivers/misc/uacce/uacce.c
@@ -47,10 +47,13 @@ static int uacce_stop_queue(struct uacce_queue *q)
 {
 	struct uacce_device *uacce = q->uacce;
 
-	if ((q->state == UACCE_Q_STARTED) && uacce->ops->stop_queue)
+	if (q->state != UACCE_Q_STARTED)
+		return 0;
+
+	if (uacce->ops->stop_queue)
 		uacce->ops->stop_queue(q);
 
-	q->state = UACCE_Q_ZOMBIE;
+	q->state = UACCE_Q_INIT;
 
 	return 0;
 }
@@ -61,8 +64,13 @@ static void uacce_put_queue(struct uacce_queue *q)
 
 	uacce_stop_queue(q);
 
+	if (q->state != UACCE_Q_INIT)
+		return;
+
 	if (uacce->ops->put_queue)
 		uacce->ops->put_queue(q);
+
+	q->state = UACCE_Q_ZOMBIE;
 }
 
 static long uacce_get_ss_dma(struct uacce_queue *q, void __user *arg)