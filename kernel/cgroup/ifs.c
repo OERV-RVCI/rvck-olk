OERV-RVCI/rvck-olk

commit 9c735dcd118dfb5e867c56d125f5ff1ea249f0e8

interference: Use hardware timer counter
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

--------------------------------

Use hardware timer counter

Signed-off-by: Pu Lehui <pulehui@huawei.com>

kernel/cgroup/ifs.c
@@ -21,6 +21,7 @@ struct smt_info {
 	bool is_noidle;
 };
 
+static DEFINE_PER_CPU(u64, ifs_tsc_freq);
 static DEFINE_PER_CPU(struct smt_itf, smt_itf);
 static DEFINE_PER_CPU(struct smt_info, smt_info);
 static DEFINE_PER_CPU_READ_MOSTLY(int, smt_sibling) = -1;
@@ -236,6 +237,37 @@ static const char *ifs_type_name(int type)
 	return name;
 }
 
+static u64 tsc_cycles_to_nsec(u64 tsc_cycles)
+{
+#if defined(__aarch64__) || defined(__x86_64__)
+	return (((u64)NSEC_PER_SEC << 5) / this_cpu_read(ifs_tsc_freq) * tsc_cycles) >> 5;
+#else
+	return tsc_cycles;
+#endif
+}
+
+static int cgroup_ifs_tsc_init(void)
+{
+	u64 freq = 0;
+	int cpu;
+
+#if defined(__aarch64__)
+	asm volatile ("MRS %0, CNTFRQ_EL0" : "=r" (freq));
+#elif defined(__x86_64__)
+	if (likely(boot_cpu_has(X86_FEATURE_CONSTANT_TSC)))
+		freq = tsc_khz * 1000;
+#endif
+	if (!freq) {
+		pr_warn("Disable CGROUP IFS: no constant tsc\n");
+		return -1;
+	}
+
+	for_each_possible_cpu(cpu)
+		per_cpu(ifs_tsc_freq, cpu) = freq;
+
+	return 0;
+}
+
 static bool should_print(int type)
 {
 #ifdef CONFIG_SCHEDSTATS
@@ -268,6 +300,8 @@ static int print_sum_time(struct cgroup_ifs *ifs, struct seq_file *seq)
 	for (i = 0; i < NR_IFS_TYPES; i++) {
 		if (!should_print(i))
 			continue;
+		if (i == IFS_SPINLOCK || i == IFS_MUTEX)
+			time[i] = tsc_cycles_to_nsec(time[i]);
 		seq_printf(seq, "%-18s%llu\n", ifs_type_name(i), time[i]);
 	}
 
@@ -339,6 +373,9 @@ struct cftype cgroup_v1_ifs_files[] = {
 void cgroup_ifs_init(void)
 {
 	if (!ifs_enable)
+		return;
+
+	if (cgroup_ifs_tsc_init() < 0)
 		return;
 
 	BUG_ON(cgroup_init_cftypes(NULL, cgroup_ifs_files));

include/linux/cgroup.h
@@ -932,7 +932,19 @@ static inline void cgroup_ifs_account_delta(struct cgroup_ifs_cpu *ifsc,
 
 static inline u64 cgroup_ifs_time_counter(void)
 {
+#if defined(__aarch64__)
+	u64 counter;
+
+	asm volatile("mrs %0, cntvct_el0" : "=r" (counter) :: "memory");
+	return counter;
+#elif defined(__x86_64__)
+	unsigned int lo, hi;
+
+	asm volatile("rdtsc" : "=a"(lo), "=d"(hi) :: "memory");
+	return ((u64)hi << 32) | lo;
+#else
 	return sched_clock();
+#endif
 }
 
 static inline void cgroup_ifs_enter_lock(u64 *clock)

commit ea7d6f2c3a100b1474166d222f955c8805c063b3

interference: Avoid null pointer dereference in cgroup_ifs_add_files
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

-----------------------------------------

IFS subsystem disables itself in cgroup_ifs_tsc_init() when TSC is
not detected, skipping the initialization of cgroup_ifs_files.
However, ifs_enable may remain true in this case, leading to a
subsequent call to cgroup_ifs_add_files that uses the uninitialized
cgroup_ifs_files and causes a null pointer dereference.

Fix this by explicitly setting ifs_enable to false when IFS is
disabled.

Fixes: 9c735dcd118d ("interference: Use hardware timer counter")
Signed-off-by: Tengda Wu <wutengda2@huawei.com>

kernel/cgroup/ifs.c
@@ -340,6 +340,7 @@ static int cgroup_ifs_tsc_init(void)
 #endif
 	if (!freq) {
 		pr_warn("Disable CGROUP IFS: no constant tsc\n");
+		ifs_enable = false;
 		return -1;
 	}
 