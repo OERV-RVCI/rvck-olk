OERV-RVCI/rvck-olk

commit 1dc959aea0fd61040acf0967d867715efdbaccc0

iommu/ummu: Support UMMU attribute show and store operations
ubmmu driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID40QK

-----------------------------------------------------------

This patch implements information interaction between kernel space
and user space based on sysfs and kobject.
It can show and store some attributes related to UMMU.

Signed-off-by: Jiashun Wang <wangjiashun@huawei.com>
Signed-off-by: Yanlong Zhu <zhuyanlong3@huawei.com>

drivers/iommu/hisilicon/attribute.c
@@ -1,0 +1,254 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ */
+#define pr_fmt(fmt) "UMMU: " fmt
+
+#include <linux/device.h>
+#include <linux/errno.h>
+#include <linux/iommu.h>
+#include <linux/ummu_core.h>
+
+#include "ummu.h"
+#include "cfg_table.h"
+#include "attribute.h"
+
+#define ATTR_SHOW(dev, capability, buf, ret)                                          \
+	do {                                                                          \
+		struct ummu_device *ummu =                                            \
+			core_to_ummu_device(to_ummu_core(dev_to_iommu_device(dev)));  \
+		*(ret) = sysfs_emit(buf, "0x%x\n", ummu->cap.capability);             \
+	} while (0)
+
+#define ATTR_SHOW_64(dev, capability, buf, ret)                                       \
+	do {                                                                          \
+		struct ummu_device *ummu =                                            \
+			core_to_ummu_device(to_ummu_core(dev_to_iommu_device(dev)));  \
+		*(ret) = sysfs_emit(buf, "0x%llx\n", ummu->cap.capability);           \
+	} while (0)
+
+static ssize_t features_show(struct device *dev, struct device_attribute *attr,
+			     char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, features, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(features);
+
+static ssize_t tid_bits_show(struct device *dev, struct device_attribute *attr,
+			     char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, tid_bits, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(tid_bits);
+
+static ssize_t pgsize_bitmap_show(struct device *dev,
+				  struct device_attribute *attr, char *buf)
+{
+	int ret;
+
+	ATTR_SHOW_64(dev, pgsize_bitmap, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(pgsize_bitmap);
+
+static ssize_t ias_show(struct device *dev, struct device_attribute *attr,
+			char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, ias, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(ias);
+
+static ssize_t oas_show(struct device *dev, struct device_attribute *attr,
+			char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, oas, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(oas);
+
+static ssize_t ptsize_bitmap_show(struct device *dev,
+				  struct device_attribute *attr, char *buf)
+{
+	int ret;
+
+	ATTR_SHOW_64(dev, ptsize_bitmap, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(ptsize_bitmap);
+
+static ssize_t options_show(struct device *dev, struct device_attribute *attr,
+			    char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, options, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(options);
+
+static ssize_t mcmdq_log2num_show(struct device *dev,
+				  struct device_attribute *attr, char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, mcmdq_log2num, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(mcmdq_log2num);
+
+static ssize_t mcmdq_log2size_show(struct device *dev,
+				   struct device_attribute *attr, char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, mcmdq_log2size, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(mcmdq_log2size);
+
+static ssize_t evtq_log2num_show(struct device *dev,
+				 struct device_attribute *attr, char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, evtq_log2num, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(evtq_log2num);
+
+static ssize_t evtq_log2size_show(struct device *dev,
+				  struct device_attribute *attr, char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, evtq_log2size, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(evtq_log2size);
+
+static ssize_t permq_num_show(struct device *dev, struct device_attribute *attr,
+			      char *buf)
+{
+	int ret;
+
+	ATTR_SHOW(dev, permq_num, buf, &ret);
+	return ret;
+}
+static DEVICE_ATTR_RO(permq_num);
+
+static ssize_t permq_ent_num_show(struct device *dev,
+				  struct device_attribute *attr, char *buf)
+{
+	struct ummu_device *ummu =
+		core_to_ummu_device(to_ummu_core(dev_to_iommu_device(dev)));
+	u32 pcplq_ent_num, pcmdq_ent_num;
+
+	pcplq_ent_num = ummu->cap.permq_ent_num.cplq_num;
+	pcmdq_ent_num = ummu->cap.permq_ent_num.cmdq_num;
+	return sysfs_emit(buf, "pcmdq 0x%x pcplq 0x%x\n", pcmdq_ent_num,
+		       pcplq_ent_num);
+}
+static DEVICE_ATTR_RO(permq_ent_num);
+
+static ssize_t eid_list_show(struct device *dev, struct device_attribute *attr,
+			     char *buf)
+{
+	char *str = ummu_get_eid_list();
+	size_t len;
+
+	if (!str)
+		return -EINVAL;
+
+	len = strlen(str);
+	if (len >= PAGE_SIZE)
+		len = PAGE_SIZE - 1;
+
+	memcpy(buf, str, len);
+	buf[len] = '\0';
+	kfree(str);
+	return len;
+}
+static DEVICE_ATTR_RO(eid_list);
+
+static const char *get_domain_type_str(u32 domain_type)
+{
+	switch (domain_type) {
+	case IOMMU_DOMAIN_DMA:
+		return "IOMMU_DOMAIN_DMA";
+	case IOMMU_DOMAIN_SVA:
+		return "IOMMU_DOMAIN_SVA";
+	default:
+		return "UNKNOWN DOMAIN TYPE";
+	}
+}
+
+static ssize_t tid_type_store(struct device *dev,
+			      struct device_attribute *attr,
+			      const char *buf, size_t count)
+{
+	struct ummu_core_device *ummu_core;
+	u32 tid = 0, tid_type;
+	int ret;
+
+	ret = kstrtouint(buf, 0, &tid);
+	if (ret < 0 || tid >= UMMU_INVALID_TID)
+		return -EINVAL;
+
+	ummu_core = to_ummu_core(dev_to_iommu_device(dev));
+	ret = ummu_core_get_tid_type(ummu_core, tid, &tid_type);
+	if (ret) {
+		pr_err("Invalid tid = 0x%x, ret = %d.\n", tid, ret);
+		return ret;
+	}
+
+	pr_info("tid = 0x%x, domain_type = %s.\n", tid,
+		get_domain_type_str(tid_type));
+
+	return (ssize_t)count;
+}
+static DEVICE_ATTR_WO(tid_type);
+
+static struct attribute *ummu_iommu_attrs[] = {
+	&dev_attr_features.attr,
+	&dev_attr_tid_bits.attr,
+	&dev_attr_pgsize_bitmap.attr,
+	&dev_attr_ias.attr,
+	&dev_attr_oas.attr,
+	&dev_attr_ptsize_bitmap.attr,
+	&dev_attr_options.attr,
+	&dev_attr_mcmdq_log2num.attr,
+	&dev_attr_mcmdq_log2size.attr,
+	&dev_attr_evtq_log2num.attr,
+	&dev_attr_evtq_log2size.attr,
+	&dev_attr_permq_num.attr,
+	&dev_attr_permq_ent_num.attr,
+	&dev_attr_eid_list.attr,
+	&dev_attr_tid_type.attr,
+	NULL,
+};
+
+static struct attribute_group ummu_iommu_group = {
+	.name = "ummu-iommu",
+	.attrs = ummu_iommu_attrs,
+};
+
+static const struct attribute_group *ummu_iommu_groups[] = {
+	&ummu_iommu_group,
+	NULL,
+};
+
+const struct attribute_group **get_attribute_group(void)
+{
+	return ummu_iommu_groups;
+};

drivers/iommu/hisilicon/Makefile
@@ -18,4 +18,5 @@ ummu-y := ummu_main.o \
 	  perm_table.o \
 	  perm_table.o \
 	  perm_queue.o \
+	  attribute.o \
 	  logic_ummu/logic_ummu.o

drivers/iommu/hisilicon/attribute.h
@@ -1,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ */
+
+#ifndef __UMMU_ATTRIBUTE_H__
+#define __UMMU_ATTRIBUTE_H__
+
+#include <linux/sysfs.h>
+
+const struct attribute_group **get_attribute_group(void);
+
+#endif  /* __UMMU_ATTRIBUTE_H__ */

drivers/iommu/hisilicon/ummu-core/core_tid.c
@@ -15,6 +15,7 @@ struct ummu_tid_data {
 	struct device *device;
 	enum ummu_mapt_mode mode;
 	enum tid_alloc_mode alloc_mode;
+	u32 domain_type;
 };
 
 static int default_tid_alloc(struct ummu_tid_manager *manager,
@@ -116,6 +117,7 @@ static int ummu_global_pasid_alloc(struct ummu_tid_manager *manager,
 	tid_data->device = param->device;
 	tid_data->mode = param->mode;
 	tid_data->alloc_mode = param->alloc_mode;
+	tid_data->domain_type = param->domain_type;
 
 	switch (param->alloc_mode) {
 	case TID_ALLOC_TRANSPARENT:
@@ -268,6 +270,31 @@ void ummu_core_put_device(struct device *dev)
 	put_device(dev);
 }
 EXPORT_SYMBOL_GPL(ummu_core_put_device);
+
+int ummu_core_get_tid_type(struct ummu_core_device *ummu_core, u32 tid,
+			   u32 *tid_type)
+{
+	struct ummu_tid_data *tid_data;
+	int ret;
+
+	if (!ummu_core || !ummu_core->tid_manager || tid >= UMMU_INVALID_TID ||
+	    !tid_type)
+		return -EINVAL;
+
+	xa_lock(&ummu_core->tid_manager->token_ids);
+	tid_data = xa_load(&ummu_core->tid_manager->token_ids, tid);
+	if (tid_data) {
+		*tid_type = tid_data->domain_type;
+		ret = 0;
+	} else {
+		pr_err("cannot get tid_data.\n");
+		ret = -ENOENT;
+	}
+	xa_unlock(&ummu_core->tid_manager->token_ids);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(ummu_core_get_tid_type);
 
 const struct tid_ops *ummu_core_tid_ops[] = {
 	[PASID_OPS] = &ummu_global_pasid_ops,

drivers/iommu/hisilicon/ummu_main.c
@@ -18,6 +18,7 @@
 #include "regs.h"
 #include "flush.h"
 #include "ummu.h"
+#include "attribute.h"
 #include "cfg_table.h"
 
 #define UMMU_DRV_NAME "ummu"
@@ -68,14 +69,18 @@ static int ummu_ioremap(struct ummu_device *ummu, resource_size_t start,
 
 static int ummu_device_register(struct ummu_device *ummu)
 {
+	const struct attribute_group **groups;
 	int ret;
 
-	ret = iommu_device_sysfs_add(&ummu->core_dev.iommu, ummu->dev, NULL,
+	groups = get_attribute_group();
+	ret = iommu_device_sysfs_add(&ummu->core_dev.iommu, ummu->dev, groups,
 				     "%s", dev_name(ummu->dev));
-	if (ret)
+	if (ret) {
 		dev_err(ummu->dev, "add iommu sysfs failed, ret = %d.\n", ret);
+		return ret;
+	}
 
-	return ret;
+	return 0;
 }
 
 static void ummu_device_unregister(struct ummu_device *ummu)

include/linux/ummu_core.h
@@ -134,6 +134,7 @@ struct ummu_tid_param {
 	enum ummu_mapt_mode mode;
 	enum tid_alloc_mode alloc_mode;
 	u32 assign_tid;
+	u32 domain_type;
 
 	KABI_RESERVE(1)
 	KABI_RESERVE(2)
@@ -630,6 +631,18 @@ struct device *ummu_core_alloc_tdev(struct tdev_attr *attr, u32 *ptid);
  * Return: 0 on success or an error.
  */
 int ummu_core_free_tdev(struct device *dev);
+
+/**
+ * Get ummu_tid_type related to the tid.
+ * @dev: the ummu_core device tid belongs to.
+ * @tid: token id.
+ * @tid_type: out param, ummu_tid_type
+ *
+ * Return: 0 on success , others for an error.
+ */
+int ummu_core_get_tid_type(struct ummu_core_device *dev, u32 tid,
+			   u32 *tid_type);
+
 #else
 static inline int ummu_sva_grant_range(struct iommu_sva *sva, void *va,
 				       size_t size, int perm, void *cookie)
@@ -740,6 +753,12 @@ static inline struct device *ummu_core_alloc_tdev(struct tdev_attr *attr, u32 *p
 }
 
 static inline int ummu_core_free_tdev(struct device *dev)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int ummu_core_get_tid_type(struct ummu_core_device *dev, u32 tid,
+					 u32 *tid_type)
 {
 	return -EOPNOTSUPP;
 }

commit 11e53fb0223606e325a54134bc3bdfc491d61bf3

iommu/ummu: Move tid_type attr to logic ummu
ubmmu driver inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID798W

-----------------------------------------------------------

1. Move tid_type attr to logic ummu.
2. Modify the device IDs of the UMMU and PMU.

Fixes: 1dc959aea0fd ("iommu/ummu: Support UMMU attribute show and store operations")

Signed-off-by: Liming An <anliming1@h-partners.com>

drivers/iommu/hisilicon/attribute.c
@@ -187,44 +187,6 @@ static ssize_t eid_list_show(struct device *dev, struct device_attribute *attr,
 }
 static DEVICE_ATTR_RO(eid_list);
 
-static const char *get_domain_type_str(u32 domain_type)
-{
-	switch (domain_type) {
-	case IOMMU_DOMAIN_DMA:
-		return "IOMMU_DOMAIN_DMA";
-	case IOMMU_DOMAIN_SVA:
-		return "IOMMU_DOMAIN_SVA";
-	default:
-		return "UNKNOWN DOMAIN TYPE";
-	}
-}
-
-static ssize_t tid_type_store(struct device *dev,
-			      struct device_attribute *attr,
-			      const char *buf, size_t count)
-{
-	struct ummu_core_device *ummu_core;
-	u32 tid = 0, tid_type;
-	int ret;
-
-	ret = kstrtouint(buf, 0, &tid);
-	if (ret < 0 || tid >= UMMU_INVALID_TID)
-		return -EINVAL;
-
-	ummu_core = to_ummu_core(dev_to_iommu_device(dev));
-	ret = ummu_core_get_tid_type(ummu_core, tid, &tid_type);
-	if (ret) {
-		pr_err("Invalid tid = 0x%x, ret = %d.\n", tid, ret);
-		return ret;
-	}
-
-	pr_info("tid = 0x%x, domain_type = %s.\n", tid,
-		get_domain_type_str(tid_type));
-
-	return (ssize_t)count;
-}
-static DEVICE_ATTR_WO(tid_type);
-
 static struct attribute *ummu_iommu_attrs[] = {
 	&dev_attr_features.attr,
 	&dev_attr_tid_bits.attr,
@@ -240,7 +202,6 @@ static struct attribute *ummu_iommu_attrs[] = {
 	&dev_attr_permq_num.attr,
 	&dev_attr_permq_ent_num.attr,
 	&dev_attr_eid_list.attr,
-	&dev_attr_tid_type.attr,
 	NULL,
 };
 

drivers/iommu/hisilicon/logic_ummu/logic_ummu.c
@@ -2061,6 +2061,61 @@ static inline struct fwnode_handle *logic_ummu_alloc_fwnode_static(void)
 	return handle;
 }
 
+static const char *get_domain_type_str(u32 domain_type)
+{
+	switch (domain_type) {
+	case IOMMU_DOMAIN_DMA:
+		return "IOMMU_DOMAIN_DMA";
+	case IOMMU_DOMAIN_IDENTITY:
+		return "IOMMU_DOMAIN_IDENTITY";
+	case IOMMU_DOMAIN_SVA:
+		return "IOMMU_DOMAIN_SVA";
+	default:
+		return "UNKNOWN DOMAIN TYPE";
+	}
+}
+
+static ssize_t tid_type_store(struct device *dev,
+			      struct device_attribute *attr,
+			      const char *buf, size_t count)
+{
+	struct ummu_core_device *ummu_core;
+	u32 tid = 0, tid_type;
+	int ret;
+
+	ret = kstrtouint(buf, 0, &tid);
+	if (ret < 0 || tid >= UMMU_INVALID_TID)
+		return -EINVAL;
+
+	ummu_core = to_ummu_core(dev_to_iommu_device(dev));
+	ret = ummu_core_get_tid_type(ummu_core, tid, &tid_type);
+	if (ret) {
+		pr_err("Invalid tid = 0x%x, ret = %d.\n", tid, ret);
+		return ret;
+	}
+
+	pr_info("tid = 0x%x, domain_type = %s.\n", tid,
+		get_domain_type_str(tid_type));
+
+	return (ssize_t)count;
+}
+static DEVICE_ATTR_WO(tid_type);
+
+static struct attribute *logic_ummu_attrs[] = {
+	&dev_attr_tid_type.attr,
+	NULL,
+};
+
+static struct attribute_group logic_ummu_group = {
+	.name = NULL,
+	.attrs = logic_ummu_attrs,
+};
+
+const struct attribute_group *logic_ummu_groups[] = {
+	&logic_ummu_group,
+	NULL,
+};
+
 int logic_ummu_device_init(void)
 {
 	int ret;
@@ -2085,7 +2140,7 @@ int logic_ummu_device_init(void)
 		pr_err("add logic ummu device failed\n");
 		goto out_free_fwnode;
 	}
-	ret = iommu_device_sysfs_add(&logic_ummu.core_dev.iommu, NULL, NULL,
+	ret = iommu_device_sysfs_add(&logic_ummu.core_dev.iommu, NULL, logic_ummu_groups,
 				     "%s", "logic_ummu");
 	if (ret) {
 		pr_err("register logic ummu to sysfs failed.\n");

drivers/ub/ubfi/ummu.c
@@ -109,14 +109,15 @@ static int __init ummu_add_resources(struct platform_device *pdev,
 
 static int ummu_rename_device(struct platform_device *pdev, enum ubrt_node_type type)
 {
-	static int device_count;
+	static int device_ummu_count;
+	static int device_pmu_count;
 	char new_name[32];
 	int ret;
 
 	if (type == UBRT_UMMU)
-		ret = snprintf(new_name, sizeof(new_name), "ummu.%d", device_count);
+		ret = snprintf(new_name, sizeof(new_name), "ummu.%d", device_ummu_count++);
 	else
-		ret = snprintf(new_name, sizeof(new_name), "ummu_pmu.%d", device_count);
+		ret = snprintf(new_name, sizeof(new_name), "ummu_pmu.%d", device_pmu_count++);
 
 	if (ret < 0 || ret >= sizeof(new_name)) {
 		dev_err(&pdev->dev, "failed to generate new device name\n");
@@ -129,8 +130,6 @@ static int ummu_rename_device(struct platform_device *pdev, enum ubrt_node_type 
 		return ret;
 	}
 	pdev->name = pdev->dev.kobj.name;
-
-	device_count++;
 
 	return 0;
 }

Documentation/ABI/testing/sysfs-class-iommu-ummu-iommu
@@ -104,7 +104,7 @@ Contact:	Jingbin Wu <wujingbin2@huawei.com>
 Description:
 		Maximum TokenID bit width supported in non-secure state.
 
-What:		/sys/class/iommu/ummu.<x>/ummu-iommu/tid_type
+What:		/sys/class/iommu/logic_ummu/tid_type
 Date:		Oct 2025
 KernelVersion:	6.6
 Contact:	Jingbin Wu <wujingbin2@huawei.com>