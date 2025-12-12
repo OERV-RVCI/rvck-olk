OERV-RVCI/rvck-olk

commit da1164d1b52edbba7cf637423e626b0647388b23

Add IPI direct inject method for virtCCA CVM.
virtcca inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/ICV6VM

--------------------------------

Implement direct IPI injection for CVM to optimize
inter-processor interrupts handling in virtCCA scenarios.

Signed-off-by: hjx_gitff <hejingxian@huawei.com>
Signed-off-by: yxk <yangxiangkai@huawei.com>

arch/arm64/include/asm/virtcca_cvm_guest.h
@@ -8,6 +8,9 @@
 #ifdef CONFIG_HISI_VIRTCCA_GUEST
 struct device;
 
+DECLARE_PER_CPU(unsigned int, virtcca_unpark_idle_notify);
+DECLARE_PER_CPU(unsigned int, virtcca_park_idle_state);
+
 extern int set_cvm_memory_encrypted(unsigned long addr, int numpages);
 
 extern int set_cvm_memory_decrypted(unsigned long addr, int numpages);

arch/arm64/kernel/smp.c
@@ -34,6 +34,9 @@
 #include <linux/kexec.h>
 #include <linux/crash_dump.h>
 #include <linux/kvm_host.h>
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
+#include <linux/virtcca_cvm_sched.h>
+#endif
 
 #include <asm/alternative.h>
 #include <asm/atomic.h>
@@ -830,8 +833,24 @@ void arch_send_call_function_ipi_mask(const struct cpumask *mask)
 	smp_cross_call(mask, IPI_CALL_FUNC);
 }
 
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
+static void cvm_send_call_function_single_ipi(int cpu, int ipi_type)
+{
+	if (virtcca_spin_cpumask_test_cpu(cpu) && is_cvm_in_park_idle_state(cpu))
+		virtcca_set_unpark_idle_notify(cpu);
+	else
+		smp_cross_call(cpumask_of(cpu), ipi_type);
+}
+#endif
+
 void arch_send_call_function_single_ipi(int cpu)
 {
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
+	if (virtcca_cvm_domain()) {
+		cvm_send_call_function_single_ipi(cpu, IPI_CALL_FUNC);
+		return;
+	}
+#endif
 	smp_cross_call(cpumask_of(cpu), IPI_CALL_FUNC);
 }
 
@@ -1027,6 +1046,12 @@ void __init set_smp_ipi_range(int ipi_base, int n)
 
 void arch_smp_send_reschedule(int cpu)
 {
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
+	if (virtcca_cvm_domain()) {
+		cvm_send_call_function_single_ipi(cpu, IPI_RESCHEDULE);
+		return;
+	}
+#endif
 	smp_cross_call(cpumask_of(cpu), IPI_RESCHEDULE);
 }
 

arch/arm64/kernel/virtcca_cvm_guest.c
@@ -8,6 +8,8 @@
 #include <linux/sched.h>
 #include <linux/vmalloc.h>
 #include <linux/swiotlb.h>
+#include <linux/pci.h>
+#include <linux/virtcca_cvm_domain.h>
 
 #include <asm/cacheflush.h>
 #include <asm/set_memory.h>
@@ -71,6 +73,55 @@ bool is_virtcca_cvm_world(void)
 	return cvm_guest_enable && static_branch_likely(&cvm_tsi_present);
 }
 EXPORT_SYMBOL_GPL(is_virtcca_cvm_world);
+
+struct cpumask cvm_spin_cpumask;
+static DEFINE_SPINLOCK(ipi_passthrough_lock);
+DEFINE_PER_CPU(unsigned int, virtcca_unpark_idle_notify);
+DEFINE_PER_CPU(unsigned int, virtcca_park_idle_state);
+
+bool virtcca_spin_cpumask_test_cpu(int cpu)
+{
+	return cpumask_test_cpu(cpu, &cvm_spin_cpumask);
+}
+
+static ssize_t soft_ipi_passthrough_store(struct kobject *kobj,
+						struct kobj_attribute *attr,
+						const char *buf, size_t count)
+{
+	spin_lock(&ipi_passthrough_lock);
+	if (cpumask_parse(buf, &cvm_spin_cpumask) < 0)
+		return -EINVAL;
+
+	spin_unlock(&ipi_passthrough_lock);
+
+	return count;
+}
+
+static ssize_t soft_ipi_passthrough_show(struct kobject *kobj,
+					struct kobj_attribute *attr, char *buf)
+{
+	int ret;
+
+	ret = scnprintf(buf, PAGE_SIZE, "%*pb\n", cpumask_pr_args(&cvm_spin_cpumask));
+
+	return ret;
+}
+
+static struct kobj_attribute soft_ipi_passthrough_attr = __ATTR_RW(soft_ipi_passthrough);
+
+static int __init soft_ipi_passthrough_init(void)
+{
+	unsigned int cpu;
+	unsigned int max_nr_cpus = num_possible_cpus();
+
+	cpumask_clear(&cvm_spin_cpumask);
+	for (cpu = 0; cpu < max_nr_cpus; cpu++)
+		virtcca_clear_unpark_idle_notify(cpu);
+
+
+	return sysfs_create_file(kernel_kobj, &soft_ipi_passthrough_attr.attr);
+}
+late_initcall(soft_ipi_passthrough_init);
 
 static int change_page_range_cvm(pte_t *ptep, unsigned long addr, void *data)
 {

include/linux/virtcca_cvm_domain.h
@@ -6,21 +6,13 @@
 #ifndef __VIRTCCA_CVM_DOMAIN_H
 #define __VIRTCCA_CVM_DOMAIN_H
 #include <linux/device.h>
-#ifdef CONFIG_HISI_VIRTCCA_GUEST
+#include <linux/virtcca_cvm_sched.h>
 
-#include <asm/virtcca_cvm_guest.h>
-static inline bool virtcca_cvm_domain(void)
-{
-	return is_virtcca_cvm_world();
-}
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
 
 extern void enable_swiotlb_for_cvm_dev(struct device *dev, bool enable);
 
 #else
-static inline bool virtcca_cvm_domain(void)
-{
-	return false;
-}
 
 static inline void enable_swiotlb_for_cvm_dev(struct device *dev, bool enable) {}
 

include/linux/virtcca_cvm_sched.h
@@ -1,0 +1,80 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2024. Huawei Technologies Co., Ltd. All rights reserved.
+ */
+
+#ifndef __VIRTCCA_CVM_SCHED_H
+#define __VIRTCCA_CVM_SCHED_H
+
+enum cvm_park_idle_state {
+	CVM_PARK_RUNNING,
+	CVM_PARK_IDLE
+};
+
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
+#include <asm/virtcca_cvm_guest.h>
+
+static inline bool is_cvm_in_park_idle_state(int cpu)
+{
+	return (per_cpu(virtcca_park_idle_state, cpu) == CVM_PARK_IDLE);
+}
+
+static inline bool virtcca_cvm_domain(void)
+{
+	return is_virtcca_cvm_world();
+}
+
+
+extern bool virtcca_spin_cpumask_test_cpu(int cpu);
+
+static inline void virtcca_set_park_idle_state(int cpu,
+					enum cvm_park_idle_state state)
+{
+	per_cpu(virtcca_park_idle_state, cpu) = state;
+}
+
+static inline bool is_virtcca_unpark_idle_notify_set(int cpu)
+{
+	return (per_cpu(virtcca_unpark_idle_notify, cpu) == 1);
+}
+
+static inline void virtcca_set_unpark_idle_notify(int cpu)
+{
+	per_cpu(virtcca_unpark_idle_notify, cpu) = 1;
+}
+
+static inline void virtcca_clear_unpark_idle_notify(int cpu)
+{
+	per_cpu(virtcca_unpark_idle_notify, cpu) = 0;
+}
+
+#else
+static inline bool is_cvm_in_park_idle_state(int cpu)
+{
+	return false;
+}
+
+static inline bool virtcca_cvm_domain(void)
+{
+	return false;
+}
+
+static inline bool virtcca_spin_cpumask_test_cpu(int cpu)
+{
+	return false;
+}
+
+static inline void virtcca_set_park_idle_state(int cpu,
+					enum cvm_park_idle_state state) {}
+
+static inline bool is_virtcca_unpark_idle_notify_set(int cpu)
+{
+	return false;
+}
+
+static inline void virtcca_set_unpark_idle_notify(int cpu) {}
+
+static inline void virtcca_clear_unpark_idle_notify(int cpu) {}
+
+#endif
+#endif /* __VIRTCCA_CVM_SCHED_H */

kernel/sched/idle.c
@@ -7,6 +7,10 @@
  *        tasks which are handled in sched/fair.c )
  */
 
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
+#include <linux/virtcca_cvm_sched.h>
+#endif
+
 /* Linker adds these: start and end of __cpuidle functions */
 extern char __cpuidle_text_start[], __cpuidle_text_end[];
 
@@ -229,6 +233,36 @@ exit_idle:
 		local_irq_enable();
 }
 
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
+static noinline int virtcca_cvm_cpu_idle_poll(void)
+{
+	unsigned int cpu = smp_processor_id();
+
+	if (!virtcca_cvm_domain() || !virtcca_spin_cpumask_test_cpu(cpu))
+		return 0;
+
+	trace_cpu_idle(0, smp_processor_id());
+	stop_critical_timings();
+	ct_cpuidle_enter();
+
+	virtcca_set_park_idle_state(cpu, CVM_PARK_IDLE);
+	raw_local_irq_enable();
+	while (!tif_need_resched() && !is_virtcca_unpark_idle_notify_set(cpu))
+		cpu_relax();
+
+	raw_local_irq_disable();
+	virtcca_clear_unpark_idle_notify(cpu);
+	virtcca_set_park_idle_state(cpu, CVM_PARK_RUNNING);
+
+	ct_cpuidle_exit();
+	start_critical_timings();
+	trace_cpu_idle(PWR_EVENT_EXIT, smp_processor_id());
+	local_irq_enable();
+
+	return 1;
+}
+#endif
+
 /*
  * Generic idle loop implementation
  *
@@ -269,6 +303,12 @@ static void do_idle(void)
 		arch_cpu_idle_enter();
 		rcu_nocb_flush_deferred_wakeup();
 
+#ifdef CONFIG_HISI_VIRTCCA_GUEST
+		if (virtcca_cvm_cpu_idle_poll()) {
+			arch_cpu_idle_exit();
+			break;
+		}
+#endif
 		/*
 		 * In poll mode we reenable interrupts and spin. Also if we
 		 * detected in the wakeup from idle path that the tick

commit c982a2461f281db6403121efee54a9638af4b274

virtcca: Add missing includes for DECLARE_PER_CPU
virtcca inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/oec-hardware/issues/ID9DGN

--------------------------------

The following commit is missing percpu.h.
 It causes the NVIDIA GPU driver to compile failed.
https://gitee.com/openeuler/kernel/commit/da1164d1b52edbba7cf637423e626b0647388b23

Fixes: da1164d1b52e ("Add IPI direct inject method for virtCCA CVM")
Signed-off-by: siyu zhang <siyu.zhang@windriver.com>
Signed-off-by: baogen shang <baogen.shang@windriver.com>

arch/arm64/include/asm/virtcca_cvm_guest.h
@@ -6,6 +6,8 @@
 #define __VIRTCCA_CVM_GUEST_H
 
 #ifdef CONFIG_HISI_VIRTCCA_GUEST
+#include <linux/percpu.h>
+
 struct device;
 
 DECLARE_PER_CPU(unsigned int, virtcca_unpark_idle_notify);