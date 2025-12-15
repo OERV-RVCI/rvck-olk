OERV-RVCI/rvck-olk

commit 64c6ae6a06e840c6c26f510acfbb8de3943a611d

obmm: Add memory region export functionality
euleros inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/ID45O2
CVE: NA

------------------------------------------------------------------

Implement the core export functionality for OBMM memory regions,
enabling memory sharing between different processes and hosts.

Key features include:
1. Added user-space interfaces (OBMM_CMD_EXPORT, OBMM_CMD_UNEXPORT) to
export or reclaim memory regions. Exported memory becomes inaccessible
from the local kernel space and allows access from remote servers within
the same supernode.

2. Defined the core data structure 'obmm_export_region' to manage
   exported
memory regions with associated metadata and permissions.

3. Implemented essential functions for export region operations:
   - Cache maintenance operations
   - Kernel page table attribute modifications
   - Memory mapping support
   - Address translation functions

4. Added necessary flag definitions (OBMM_EXPORT_FLAG_ALLOW_MMAP,
   OBMM_EXPORT_FLAG_FAST) to control export behavior and permissions.

5. Created new header file (obmm_export_region_ops.h) to declare public
   functions

This enables the primary use case of OBMM: secure and efficient
memory sharing across system boundaries.

Signed-off-by: Li Ruilin <liruilin4@huawei.com>
Signed-off-by: Gao Chao <gaochao24@huawei.com>

drivers/ub/obmm/obmm_core.c
@@ -21,7 +21,9 @@
 #include <ub/ubus/ub-mem-decoder.h>
 
 #include "obmm_cache.h"
+#include "obmm_export_region_ops.h"
 #include "ubmempool_allocator.h"
+#include "obmm_export.h"
 #include "obmm_core.h"
 
 size_t __obmm_memseg_size;
@@ -144,12 +146,45 @@ struct obmm_region *search_deactivate_obmm_region(int regionid)
 int obmm_query_by_offset(struct obmm_region *reg, unsigned long offset,
 			 struct obmm_ext_addr *ext_addr)
 {
-	return -ENOTTY;
+	int ret;
+	struct obmm_export_region *e_reg;
+
+	if (reg->type == OBMM_EXPORT_REGION) {
+		e_reg = container_of(reg, struct obmm_export_region, region);
+		ret = get_offset_detail_export_region(e_reg,
+			offset, ext_addr);
+	}
+
+	return ret;
 }
 
 int obmm_query_by_pa(unsigned long pa, struct obmm_ext_addr *ext_addr)
 {
-	return -ENOTTY;
+	int ret = -ENOENT;
+	struct obmm_region *region;
+	unsigned long flags;
+	spinlock_t *lock;
+
+	lock = &g_obmm_ctx_info.lock;
+
+	spin_lock_irqsave(lock, flags);
+	list_for_each_entry(region, &g_obmm_ctx_info.regions, node) {
+		if (region->type == OBMM_EXPORT_REGION) {
+			struct obmm_export_region *e_reg;
+
+			e_reg = container_of(region, struct obmm_export_region,
+				region);
+			ret = get_pa_detail_export_region(e_reg, pa, ext_addr);
+		}
+
+		if (ret == 0)
+			break;
+	}
+	spin_unlock_irqrestore(lock, flags);
+
+	if (ret)
+		return -ENOENT;
+	return 0;
 }
 
 static int nid_to_package_id(int nid)
@@ -356,10 +391,41 @@ static long obmm_dev_ioctl(struct file *file __always_unused, unsigned int cmd, 
 {
 	int ret;
 	union {
+		struct obmm_cmd_export create;
+		struct obmm_cmd_unexport unexport;
 		struct obmm_cmd_addr_query query;
 	} cmd_param;
 
 	switch (cmd) {
+	case OBMM_CMD_EXPORT: {
+		ret = (int)copy_from_user(&cmd_param.create, (void __user *)arg,
+					sizeof(struct obmm_cmd_export));
+		if (ret) {
+			pr_err("failed to load export argument");
+			return -EFAULT;
+		}
+
+		ret = obmm_export_from_pool(&cmd_param.create);
+		if (ret)
+			return ret;
+
+		ret = (int)copy_to_user((void __user *)arg, &cmd_param.create,
+				   sizeof(struct obmm_cmd_export));
+		if (ret) {
+			pr_err("failed to write export result");
+			return -EFAULT;
+		}
+	} break;
+	case OBMM_CMD_UNEXPORT: {
+		ret = (int)copy_from_user(&cmd_param.unexport, (void __user *)arg,
+					sizeof(struct obmm_cmd_unexport));
+		if (ret) {
+			pr_err("failed to load unexport argument");
+			return -EFAULT;
+		}
+
+		ret = obmm_unexport(&cmd_param.unexport);
+	} break;
 	case OBMM_CMD_ADDR_QUERY: {
 		ret = (int)copy_from_user(&cmd_param.query, (void __user *)arg,
 					  sizeof(struct obmm_cmd_addr_query));

drivers/ub/obmm/Makefile
@@ -4,6 +4,7 @@
 obmm-y := obmm_core.o \
 	obmm_cache.o \
 	conti_mem_allocator.o \
-	ubmempool_allocator.o
+	obmm_export.o obmm_export_from_pool.o \
+	ubmempool_allocator.o obmm_export_region_ops.o
 
 obj-$(CONFIG_OBMM) += obmm.o

drivers/ub/obmm/obmm_cache.c
@@ -13,6 +13,7 @@
 #include <ub/ubus/ub-mem-decoder.h>
 
 #include "obmm_core.h"
+#include "obmm_export_region_ops.h"
 #include "obmm_cache.h"
 
 static bool skip_cache_maintain;
@@ -120,7 +121,35 @@ int flush_cache_by_pa(phys_addr_t addr, size_t size, unsigned long cache_ops)
 int obmm_region_flush_range(struct obmm_region *reg, unsigned long offset, unsigned long length,
 			    uint8_t cache_ops)
 {
-	return -ENOTTY;
+	int ret;
+	struct obmm_export_region *e_reg;
+
+	/* validation */
+	if (!is_valid_cache_ops(cache_ops)) {
+		pr_err("invalid cache operation %u\n", cache_ops);
+		return -EINVAL;
+	}
+	if (offset >= reg->mem_size || length > reg->mem_size - offset ||
+		!IS_ALIGNED(offset, PAGE_SIZE) || !IS_ALIGNED(length, PAGE_SIZE)) {
+		pr_err("invalid flush range for region=%d: offset=0x%lx, flush_length=0x%lx, region_length=0x%llx\n",
+			reg->regionid, offset, length, reg->mem_size);
+		return -EINVAL;
+	}
+
+	if (cache_ops == OBMM_SHM_CACHE_NONE)
+		return 0;
+	pr_debug("flush cache: region=%d, offset=0x%lx, length=0x%lx, cache_ops=%u\n",
+		 reg->regionid, offset, length, cache_ops);
+	/* clear cache and ubus queue */
+	e_reg = container_of(reg, struct obmm_export_region, region);
+	ret = flush_export_region(e_reg, offset, length, cache_ops);
+
+	if (ret)
+		pr_err("flush failed: region=%d, offset=0x%lx, length=0x%lx, cache_ops=%u\n",
+			reg->regionid, offset, length, cache_ops);
+	else
+		pr_debug("cache successfully flushed.\n");
+	return ret;
 }
 
 /* flush the entire process address space */

drivers/ub/obmm/obmm_core.h
@@ -44,6 +44,12 @@ enum obmm_region_type {
 	OBMM_EXPORT_REGION,
 };
 
+enum obmm_mmap_granu {
+	OBMM_MMAP_GRANU_NONE,
+	OBMM_MMAP_GRANU_PAGE,
+	OBMM_MMAP_GRANU_PMD
+};
+
 #define OBMM_REGION_FLAG_NUMA_REMOTE		0x1
 #define OBMM_REGION_FLAG_ALLOW_MMAP		0x2
 #define OBMM_REGION_FLAG_MEMORY_FROM_USER	0x4
@@ -53,6 +59,9 @@ enum obmm_region_type {
 #define OBMM_MIN_VALID_REGIONID 1
 #define OBMM_MAX_VALID_REGIONID MINORMASK
 #define OBMM_REGIONID_MAX_COUNT (OBMM_MAX_VALID_REGIONID - OBMM_MIN_VALID_REGIONID + 1)
+
+#define OBMM_MEM_ALLOW_CACHEABLE_MMAP 0x1
+#define OBMM_MEM_ALLOW_NONCACHEABLE_MMAP 0x2
 
 /* invalidate cache **on start-up** */
 /* region models a set of memory to share across hosts: a unit of sharing. */
@@ -68,7 +77,18 @@ struct obmm_region {
 
 	/* the total size of all memory segments included in meminfo */
 	u64 mem_size;
-
+	/*
+	 * the granularity of memory mapping, initially OBMM_MMAP_GRANU_NONE.
+	 * When users call mmap, the mmap granularity is determined based on
+	 * the mmap flags and OBMM_REGION_FLAG_ALLOW_MMAP.
+	 */
+	enum obmm_mmap_granu mmap_granu;
+	/*
+	 * Determines what mode the memory can be mapped with.
+	 * OBMM_MEM_ALLOW_CACHEABLE_MMAP: Supports cacheable mapping
+	 * OBMM_MEM_ALLOW_NONCACHEABLE_MMAP: Supports non-cacheable mapping
+	 */
+	unsigned long mem_cap;
 	/* regions are chained into a list for management */
 	struct list_head node;
 
@@ -76,8 +96,45 @@ struct obmm_region {
 	unsigned char priv[OBMM_MAX_PRIV_LEN];
 };
 
+static inline bool region_allow_mmap(const struct obmm_region *reg)
+{
+	return reg->flags & OBMM_REGION_FLAG_ALLOW_MMAP;
+}
+static inline bool region_fast_alloc(const struct obmm_region *reg)
+{
+	return reg->flags & OBMM_REGION_FLAG_FAST_ALLOC;
+}
+
 struct mem_description_pool {
 	struct list_head head[OBMM_MAX_LOCAL_NUMA_NODES];
+};
+
+struct obmm_export_region {
+	struct obmm_region region;
+
+	/* export region may use physical memory from NUMA node[0] to node[node_count-1] */
+	unsigned int node_count;
+	uint64_t node_mem_size[OBMM_MAX_LOCAL_NUMA_NODES];
+
+	/* physical pages */
+	union {
+		struct mem_description_pool mem_desc;
+	};
+
+	/* DMA mapping */
+	struct sg_table sgt;
+
+	/* UMMU device for the tokenid */
+	struct device *ummu_dev;
+	/* UMMU RAS event notifier */
+	struct ummu_event_block *ummu_event_block;
+
+	unsigned int tokenid;
+	u64 uba;
+	unsigned int vendor_len;
+	void *vendor_info;
+	int affinity;
+	u8 deid[16];
 };
 
 struct obmm_ctx_info {

drivers/ub/obmm/obmm_export.c
@@ -1,0 +1,267 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Description：OBMM Framework's implementations.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/bitmap.h>
+#include <linux/mm.h>
+#include <linux/memory.h>
+#include <linux/kthread.h>
+#include <linux/dma-mapping.h>
+#include <linux/io.h>
+#include <linux/wait.h>
+#include <linux/mmzone.h>
+
+#include <uapi/linux/sched/types.h>
+
+#include <uapi/ub/obmm.h>
+#include <linux/ummu_core.h>
+
+#include "conti_mem_allocator.h"
+#include "ubmempool_allocator.h"
+#include "obmm_core.h"
+#include "obmm_cache.h"
+#include "obmm_export.h"
+
+int export_flags_to_region_flags(unsigned long *region_flags, unsigned long user_flags)
+{
+	*region_flags = 0;
+
+	if (user_flags & (~OBMM_EXPORT_FLAG_MASK))
+		return -EINVAL;
+	if (user_flags & OBMM_EXPORT_FLAG_ALLOW_MMAP)
+		*region_flags |= OBMM_REGION_FLAG_ALLOW_MMAP;
+	if (user_flags & OBMM_EXPORT_FLAG_FAST)
+		*region_flags |= OBMM_REGION_FLAG_FAST_ALLOC;
+
+	return 0;
+}
+static int fill_ummu_info(struct tdev_attr *attr, struct obmm_export_region *e_reg)
+{
+	tdev_attr_init(attr);
+	attr->name = (char *)"OBMM_TDEV";
+	if (e_reg->vendor_len > 0) {
+		attr->priv = kmemdup(e_reg->vendor_info, e_reg->vendor_len, GFP_KERNEL);
+		if (!attr->priv)
+			return -ENOMEM;
+	}
+	attr->priv_len = e_reg->vendor_len;
+	return 0;
+}
+
+static void drain_ummu_info(struct tdev_attr *attr)
+{
+	kfree(attr->priv);
+}
+
+static int setup_ummu(struct obmm_export_region *e_reg)
+{
+	struct tdev_attr attr;
+	uint32_t tokenid = UMMU_INVALID_TID;
+	int retval;
+
+	retval = fill_ummu_info(&attr, e_reg);
+	if (retval)
+		return retval;
+
+	/* register the memory region through UMMU */
+	pr_info("call ummu_core_alloc_tdev(), priv_len=%u, tid=%u\n", attr.priv_len, tokenid);
+	e_reg->ummu_dev = ummu_core_alloc_tdev(&attr, &tokenid);
+	if (e_reg->ummu_dev == NULL) {
+		pr_err("Failed to create UMMU device\n");
+		retval = -EPERM;
+		goto out_drain_info;
+	}
+	e_reg->tokenid = tokenid;
+	pr_debug("ummu_core_alloc_tdev() returned ummu_dev: tid=%u, name=%s\n", tokenid,
+		 dev_name(e_reg->ummu_dev));
+
+	/* DMA mapping */
+	pr_info("call dma_map_sgtable(..., dir=DMA_BIDIRECTIONAL, attrs=0)\n");
+	retval = dma_map_sgtable(e_reg->ummu_dev, &e_reg->sgt, DMA_BIDIRECTIONAL, 0);
+	if (retval) {
+		pr_err("Failed to map sgtable on UMMU. ret=%pe\n", ERR_PTR(retval));
+		goto out_free_device;
+	}
+	pr_debug("dma_map_sgtable returned 0\n");
+
+	e_reg->uba = sg_dma_address(e_reg->sgt.sgl);
+	drain_ummu_info(&attr);
+	return 0;
+
+out_free_device:
+	if (ummu_core_free_tdev(e_reg->ummu_dev))
+		pr_warn("Failed to create memory region but unable to cleanup allocated UMMU device\n");
+out_drain_info:
+	drain_ummu_info(&attr);
+	return retval;
+}
+
+static int teardown_ummu(struct obmm_export_region *e_reg)
+{
+	int ret, rollback_ret;
+
+	pr_debug("call external: dma_unmap_sgtable\n");
+	dma_unmap_sgtable(e_reg->ummu_dev, &e_reg->sgt, DMA_BIDIRECTIONAL, 0);
+
+	pr_debug("call external: ummu_core_free_tdev()\n");
+	ret = ummu_core_free_tdev(e_reg->ummu_dev);
+	if (ret) {
+		pr_err("Failed to free UMMU tdev, ret=%pe.\n", ERR_PTR(ret));
+		goto err_free_tdev;
+	}
+
+	return 0;
+
+err_free_tdev:
+
+	rollback_ret = dma_map_sgtable(e_reg->ummu_dev, &e_reg->sgt, DMA_BIDIRECTIONAL, 0);
+	if (rollback_ret) {
+		pr_err("Failed to map sgtable on UMMU. ret=%pe\n", ERR_PTR(rollback_ret));
+		ret = -ENOTRECOVERABLE;
+	}
+	if (e_reg->uba != sg_dma_address(e_reg->sgt.sgl)) {
+		pr_err("Tried remapping in UMMU on rollback but UBA changed.\n");
+		ret = -ENOTRECOVERABLE;
+		pr_debug("call external: dma_unmap_sgtable\n");
+		dma_unmap_sgtable(e_reg->ummu_dev, &e_reg->sgt, DMA_BIDIRECTIONAL, 0);
+	}
+	return ret;
+}
+
+/* Make sure the memory to be exported is in properly allocated and ready to be mapped by UMMU.
+ * The detailed information of the memory should be put in place in e_reg->sgt
+ */
+static int alloc_export_memory(struct obmm_export_region *e_reg)
+{
+	return alloc_export_memory_pool(e_reg);
+}
+
+static void free_export_memory_pool(struct obmm_export_region *e_reg)
+{
+	sg_free_table(&e_reg->sgt);
+	free_memory_contiguous(&e_reg->mem_desc);
+}
+
+static void free_export_memory(struct obmm_export_region *e_reg)
+{
+	free_export_memory_pool(e_reg);
+}
+
+/* Ensure all user inputs are properly converted and filled into the region. */
+int obmm_export_common(struct obmm_export_region *e_reg)
+{
+	int ret;
+
+	ret = alloc_export_memory(e_reg);
+	if (ret)
+		return ret;
+
+	ret = setup_ummu(e_reg);
+	if (ret)
+		goto free_memory;
+
+	return 0;
+
+free_memory:
+	free_export_memory(e_reg);
+
+	return ret;
+}
+
+int obmm_unexport_common(struct obmm_export_region *e_reg)
+{
+	int ret;
+
+	ret = teardown_ummu(e_reg);
+	if (ret)
+		return ret;
+	free_export_memory(e_reg);
+
+	return 0;
+}
+
+/* NOTE: the operation order is not precisely the reverse order of initialization for the ease of
+ * error rollback. Please make careful evaluation on modifications.
+ */
+int obmm_unexport(const struct obmm_cmd_unexport *cmd_unexport)
+{
+	int ret;
+	struct obmm_region *reg;
+	struct obmm_export_region *e_reg;
+
+	pr_info("%s: mem_id=%llu, flags=%#llx.\n", __func__, cmd_unexport->mem_id,
+		cmd_unexport->flags);
+	if (!validate_obmm_mem_id(cmd_unexport->mem_id))
+		return -ENOENT;
+	if (cmd_unexport->flags & (~OBMM_UNEXPORT_FLAG_MASK)) {
+		pr_err("%s: invalid flags %#llx.\n", __func__, cmd_unexport->flags);
+		return -EINVAL;
+	}
+
+	reg = search_deactivate_obmm_region(cmd_unexport->mem_id);
+	if (IS_ERR(reg))
+		return PTR_ERR(reg);
+
+	if (reg->type != OBMM_EXPORT_REGION) {
+		pr_err("%s: mem_id=%llu region type mismatched.\n", __func__, cmd_unexport->mem_id);
+		ret = -EINVAL;
+		goto err_unexport_common;
+	}
+
+	e_reg = container_of(reg, struct obmm_export_region, region);
+	ret = obmm_unexport_common(e_reg);
+	if (ret)
+		goto err_unexport_common;
+
+	deregister_obmm_region(reg);
+	uninit_obmm_region(reg);
+	free_export_region(e_reg);
+
+	pr_info("%s: mem_id=%llu completed.\n", __func__, cmd_unexport->mem_id);
+	return 0;
+
+err_unexport_common:
+	activate_obmm_region(reg);
+	pr_err("%s: mem_id=%llu failed, %pe.\n", __func__, cmd_unexport->mem_id, ERR_PTR(ret));
+
+	return ret;
+}
+
+int set_export_vendor(struct obmm_export_region *e_reg, const void __user *vendor_info,
+		      unsigned int vendor_len)
+{
+	if (vendor_len == 0) {
+		e_reg->vendor_info = NULL;
+		e_reg->vendor_len = vendor_len;
+		return 0;
+	}
+	if (vendor_len > OBMM_MAX_VENDOR_LEN) {
+		pr_err("invalid vendor_len = 0x%x, should less than 0x%x", vendor_len,
+		       OBMM_MAX_VENDOR_LEN);
+		return -EINVAL;
+	}
+	e_reg->vendor_info = kmalloc(vendor_len, GFP_KERNEL);
+	if (!e_reg->vendor_info)
+		return -ENOMEM;
+
+	if (copy_from_user(e_reg->vendor_info, vendor_info, vendor_len)) {
+		kfree(e_reg->vendor_info);
+		e_reg->vendor_info = NULL;
+		pr_err("failed to save vendor data.\n");
+		return -EFAULT;
+	}
+	e_reg->vendor_len = vendor_len;
+	return 0;
+}
+
+void free_export_region(struct obmm_export_region *e_reg)
+{
+	if (e_reg->vendor_len)
+		kfree(e_reg->vendor_info);
+
+	kfree(e_reg);
+}

drivers/ub/obmm/obmm_export.h
@@ -1,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Description：OBMM Framework's implementations.
+ */
+
+#ifndef OBMM_EXPORT_C_H
+#define OBMM_EXPORT_C_H
+int obmm_export_common(struct obmm_export_region *e_reg);
+
+int export_flags_to_region_flags(unsigned long *region_flags, unsigned long user_flags);
+
+int alloc_export_memory_pool(struct obmm_export_region *e_reg);
+int obmm_unexport_common(struct obmm_export_region *e_reg);
+int obmm_export_from_pool(struct obmm_cmd_export *cmd_export);
+int obmm_unexport(const struct obmm_cmd_unexport *cmd_unexport);
+
+int set_export_vendor(struct obmm_export_region *e_reg, const void __user *vendor_info,
+		      unsigned int vendor_len);
+void free_export_region(struct obmm_export_region *e_reg);
+#endif

drivers/ub/obmm/obmm_export_from_pool.c
@@ -1,0 +1,349 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Description：OBMM Framework's implementations.
+ */
+
+#include <linux/list.h>
+#include <linux/scatterlist.h>
+#include <linux/slab.h>
+#include <linux/printk.h>
+#include <linux/nodemask.h>
+#include <linux/mmzone.h>
+#include <linux/pagemap.h>
+#include <linux/numa.h>
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+
+#include "ubmempool_allocator.h"
+#include "conti_mem_allocator.h"
+#include "obmm_export.h"
+
+/* SGL size is specified as an unsigned int. It's best to limit the size of single SGL
+ * no larger than (1 << MAX_CHUNK_SHIFT)
+ */
+#define MAX_CHUNK_SHIFT (31)
+#define MAX_CHUNK_SIZE (1U << MAX_CHUNK_SHIFT)
+#define MAX_CHUNK_MASK (MAX_CHUNK_SIZE - 1)
+
+static unsigned long size_to_chunk_count(size_t size)
+{
+	return (size >> MAX_CHUNK_SHIFT) + (unsigned long)((size & MAX_CHUNK_MASK) != 0);
+}
+
+static unsigned long memseg_list_to_chunk_count(struct list_head *head)
+{
+	struct memseg_node *node;
+	phys_addr_t start = 0, end = 0;
+	unsigned long chunk_count = 0;
+
+	list_for_each_entry(node, head, list) {
+		/* whether the new node follows previous ones */
+		if (end == node->addr) {
+			end += OBMM_MEMSEG_SIZE;
+			continue;
+		}
+		chunk_count += size_to_chunk_count(end - start);
+
+		start = node->addr;
+		end = node->addr + OBMM_MEMSEG_SIZE;
+	}
+	chunk_count += size_to_chunk_count(end - start);
+	return chunk_count;
+}
+
+static struct scatterlist *fill_sg_chunks(struct scatterlist *s, phys_addr_t start, size_t size,
+					  unsigned long *filled_chunks)
+{
+	size_t chunk_size;
+	unsigned long num_chunks_to_fill;
+
+	*filled_chunks = 0;
+	num_chunks_to_fill = size_to_chunk_count(size);
+	while (num_chunks_to_fill--) {
+		if (s == NULL) {
+			/* this error is not expected to show up in release version, thus proper
+			 * error handling is not included
+			 */
+			pr_warn_once("bug: scatterlist is not big enough.\n");
+			return s;
+		}
+		chunk_size = size > MAX_CHUNK_SIZE ? MAX_CHUNK_SIZE : size;
+		sg_set_page(s, pfn_to_page(start >> PAGE_SHIFT), chunk_size, 0);
+		s = sg_next(s);
+
+		start += chunk_size;
+		size -= chunk_size;
+		*filled_chunks += 1;
+	}
+	return s;
+}
+/* Return the number of chunks to fill in the scatterlist. If @sg is NULL, the
+ * function performs a dry run.
+ */
+static struct scatterlist *fill_sg_list(struct scatterlist *s, struct list_head *head,
+					unsigned long *filled_chunks)
+{
+	struct memseg_node *node;
+	phys_addr_t start = 0, end = 0;
+	unsigned long chunk_count;
+
+	*filled_chunks = 0;
+	list_for_each_entry(node, head, list) {
+		/* whether the new node follows previous ones */
+		if (end == node->addr) {
+			end += OBMM_MEMSEG_SIZE;
+			continue;
+		}
+
+		if (end != 0) {
+			s = fill_sg_chunks(s, start, end - start, &chunk_count);
+			*filled_chunks += chunk_count;
+		}
+
+		/* track the first piece of new chunk */
+		start = node->addr;
+		end = node->addr + OBMM_MEMSEG_SIZE;
+	}
+
+	if (end != 0) {
+		s = fill_sg_chunks(s, start, end - start, &chunk_count);
+		*filled_chunks += chunk_count;
+	}
+
+	return s;
+}
+
+static int sg_alloc_table_from_memdesc(struct sg_table *sgt, struct mem_description_pool *desc,
+				       gfp_t gfp_mask)
+{
+	unsigned long chunk_count, total_chunks, filled_chunks;
+	struct scatterlist *s;
+	int ret, i;
+
+	total_chunks = 0;
+	for (i = 0; i < OBMM_MAX_LOCAL_NUMA_NODES; i++)
+		total_chunks += memseg_list_to_chunk_count(&desc->head[i]);
+	if (total_chunks == 0) {
+		pr_err("%s: no memory.\n", __func__);
+		return -EINVAL;
+	}
+
+	ret = sg_alloc_table(sgt, total_chunks, gfp_mask);
+	if (ret) {
+		pr_err("alloc sgt failed.\n");
+		return ret;
+	}
+
+	s = sgt->sgl;
+	filled_chunks = 0;
+	for (i = 0; i < OBMM_MAX_LOCAL_NUMA_NODES; i++) {
+		s = fill_sg_list(s, &desc->head[i], &chunk_count);
+		filled_chunks += chunk_count;
+	}
+
+	if (filled_chunks != total_chunks || s != NULL) {
+		pr_err("%s: internal error.\n", __func__);
+		ret = -ENOTRECOVERABLE;
+		goto sg_err;
+	}
+	return 0;
+
+sg_err:
+	sg_free_table(sgt);
+
+	return ret;
+}
+
+int alloc_export_memory_pool(struct obmm_export_region *e_reg)
+{
+	int ret;
+	unsigned int i;
+	struct mem_description_pool *desc;
+	bool allow_slow = !region_fast_alloc(&e_reg->region);
+
+	for (i = 0; i < e_reg->node_count; i++) {
+		if (e_reg->node_mem_size[i] == 0)
+			continue;
+		if (e_reg->node_mem_size[i] % OBMM_MEMSEG_SIZE) {
+			pr_err("invalid size 0x%llx on node %d: not aligned to mempool granu %#lx\n",
+			       e_reg->node_mem_size[i], i, OBMM_MEMSEG_SIZE);
+			return -EINVAL;
+		}
+	}
+
+	pr_debug("export_from_pool: allocation started.\n");
+	desc = &e_reg->mem_desc;
+	ret = allocate_memory_contiguous(e_reg->node_mem_size, e_reg->node_count, desc, true,
+					 allow_slow);
+	if (ret)
+		return ret;
+	pr_debug("export_from_pool: allocation completed. sgtable preparation started.\n");
+
+	ret = sg_alloc_table_from_memdesc(&e_reg->sgt, desc, GFP_KERNEL);
+	if (ret) {
+		free_memory_contiguous(desc);
+		return ret;
+	}
+	pr_debug("export_from_pool: sgtable preparation completed.\n");
+
+	return 0;
+}
+
+static int calculate_export_region_size(unsigned long *total_size,
+					struct obmm_cmd_export *cmd_export)
+{
+	uint64_t i;
+	nodemask_t nodes = NODE_MASK_NONE;
+
+	if (cmd_export->length > OBMM_MAX_LOCAL_NUMA_NODES) {
+		pr_err("Size list is too long: max=%d, actual_length=%lld\n",
+		       OBMM_MAX_LOCAL_NUMA_NODES, cmd_export->length);
+		return -E2BIG;
+	}
+	if (cmd_export->pxm_numa > OBMM_MAX_LOCAL_NUMA_NODES) {
+		pr_err("Invalid pxm_numa %d\n", cmd_export->pxm_numa);
+		return -EINVAL;
+	}
+
+	*total_size = 0;
+	for (i = 0; i < cmd_export->length; i++) {
+		if (!IS_ALIGNED(cmd_export->size[i], OBMM_MEMSEG_SIZE)) {
+			pr_err("The size of new OBMM region 0x%llx on node %d is not aligned to OBMM memseg size %#lx.\n",
+			       cmd_export->size[i], (int)i, OBMM_MEMSEG_SIZE);
+			return -EINVAL;
+		}
+		if (cmd_export->size[i] != 0 && !is_online_local_node(i)) {
+			pr_err("Cannot export memory from offlined or remote numa node %d\n",
+			       (int)i);
+			return -ENODEV;
+		}
+		if (cmd_export->size[i] != 0) {
+			if (*total_size > *total_size + cmd_export->size[i]) {
+				pr_err("Memory size overflowed!\n");
+				return -EOVERFLOW;
+			}
+			*total_size += cmd_export->size[i];
+			node_set(i, nodes);
+		}
+	}
+	if (*total_size == 0) {
+		pr_err("The size of new OBMM region is 0. Non-zero value expected\n");
+		return -EINVAL;
+	}
+	node_set(cmd_export->pxm_numa, nodes);
+	if (!nodes_on_same_package(&nodes)) {
+		pr_err("Cannot use memory from multiple sockets.\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static struct obmm_export_region *alloc_region_from_cmd(struct obmm_cmd_export *cmd_export)
+{
+	struct obmm_export_region *e_reg;
+	unsigned long total_size;
+	int ret;
+
+	ret = calculate_export_region_size(&total_size, cmd_export);
+	if (ret)
+		return ERR_PTR(ret);
+
+	e_reg = kzalloc(sizeof(struct obmm_export_region), GFP_KERNEL);
+	if (e_reg == NULL)
+		return ERR_PTR(-ENOMEM);
+
+	e_reg->region.type = OBMM_EXPORT_REGION;
+	e_reg->region.mem_size = total_size;
+	e_reg->region.mem_cap = OBMM_MEM_ALLOW_CACHEABLE_MMAP | OBMM_MEM_ALLOW_NONCACHEABLE_MMAP;
+	e_reg->affinity = cmd_export->pxm_numa;
+	memcpy(e_reg->deid, cmd_export->deid, sizeof(e_reg->deid));
+	ret = export_flags_to_region_flags(&e_reg->region.flags, cmd_export->flags);
+	if (ret) {
+		kfree(e_reg);
+		return ERR_PTR(ret);
+	}
+	e_reg->node_count = cmd_export->length;
+	memcpy(e_reg->node_mem_size, cmd_export->size, sizeof(uint64_t) * e_reg->node_count);
+	/* compaction */
+	while (e_reg->node_count - 1 > 0 && e_reg->node_mem_size[e_reg->node_count - 1] == 0)
+		e_reg->node_count--;
+	ret = set_obmm_region_priv(&e_reg->region, cmd_export->priv_len, cmd_export->priv);
+	if (ret) {
+		kfree(e_reg);
+		return ERR_PTR(ret);
+	}
+	ret = set_export_vendor(e_reg, cmd_export->vendor_info, cmd_export->vendor_len);
+	if (ret) {
+		kfree(e_reg);
+		return ERR_PTR(ret);
+	}
+	return e_reg;
+}
+
+static void print_export_param(const struct obmm_cmd_export *cmd_export)
+{
+	unsigned int i;
+
+	pr_info("obmm_export: len(sizes)=%#llx sizes={", cmd_export->length);
+	for (i = 0; i < cmd_export->length && i < OBMM_MAX_LOCAL_NUMA_NODES; i++)
+		if (cmd_export->size[i])
+			pr_cont(" [%u]:%#llx", i, cmd_export->size[i]);
+	if (i < cmd_export->length)
+		pr_cont(" ...");
+
+	pr_cont(" } flags=%#llx deid=" EID_FMT64 " priv_len=%u\n", cmd_export->flags,
+		EID_ARGS64_H(cmd_export->deid), EID_ARGS64_L(cmd_export->deid),
+		cmd_export->priv_len);
+}
+
+/* obmm_export_from_pool: create an OBMM-exported memory region. The region is
+ * physically located on this host and can be accessed from remote host.
+ * In OBMM's terminology, it is an export region.
+ */
+int obmm_export_from_pool(struct obmm_cmd_export *cmd_export)
+{
+	struct obmm_export_region *e_reg;
+	uint64_t uba, mem_id;
+	uint32_t token_id;
+	int ret;
+
+	print_export_param(cmd_export);
+	e_reg = alloc_region_from_cmd(cmd_export);
+	if (IS_ERR(e_reg))
+		return PTR_ERR(e_reg);
+
+	ret = init_obmm_region(&e_reg->region);
+	if (ret)
+		goto out_free_reg;
+
+	ret = obmm_export_common(e_reg);
+	if (ret)
+		goto out_unit_reg;
+
+	token_id = e_reg->tokenid;
+	uba = e_reg->uba;
+	mem_id = (uint64_t)e_reg->region.regionid;
+
+	ret = register_obmm_region(&e_reg->region);
+	if (ret)
+		goto out_unexport;
+	activate_obmm_region(&e_reg->region);
+
+	cmd_export->tokenid = token_id;
+	cmd_export->uba = uba;
+	cmd_export->mem_id = mem_id;
+
+	pr_info("obmm_export: mem_id=%llu online.\n", mem_id);
+	return 0;
+
+out_unexport:
+	obmm_unexport_common(e_reg);
+out_unit_reg:
+	uninit_obmm_region(&e_reg->region);
+out_free_reg:
+	free_export_region(e_reg);
+	return ret;
+}

commit b4c1dac0f739d5cfc9e145cb9640a18e12e3e531

obmm: Fix race condition between unexport and addr_query_by_pa
euleros inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IDBKX6
CVE: NA

-------------------------------------

When obmm_query_by_pa was iterating through regions, it could access
a region that was being unexported and thus in an inconsistent state.
This may cause sgt structure being accessed after it was freed.
Fix this by trying to get region refcount.

Fixes: 64c6ae6a06e8 ("obmm: Add memory region export functionality")

Signed-off-by: Li Ruilin <liruilin4@huawei.com>

drivers/ub/obmm/obmm_core.c
@@ -178,6 +178,8 @@ int obmm_query_by_pa(unsigned long pa, struct obmm_ext_addr *ext_addr)
 
 	spin_lock_irqsave(lock, flags);
 	list_for_each_entry(region, &g_obmm_ctx_info.regions, node) {
+		if (!try_get_obmm_region(region))
+			continue;
 		if (region->type == OBMM_IMPORT_REGION) {
 			struct obmm_import_region *i_reg;
 
@@ -190,7 +192,7 @@ int obmm_query_by_pa(unsigned long pa, struct obmm_ext_addr *ext_addr)
 			e_reg = container_of(region, struct obmm_export_region, region);
 			ret = get_pa_detail_export_region(e_reg, pa, ext_addr);
 		}
-
+		put_obmm_region(region);
 		if (ret == 0)
 			break;
 	}