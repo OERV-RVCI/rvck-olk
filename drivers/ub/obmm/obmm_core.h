OERV-RVCI/rvck-olk

commit b100600e5c187238984b334e2fce60bedb545b24

obmm: Add shared memory device interface
euleros inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/ID45O2
CVE: NA

------------------------------------------------------------------

This patch adds obmm_shmdev character devices to manage OBMM remote
memory.
For an OBMM memory region with memid ${id}, the corresponding device
path is /dev/obmm_shmdev${id}.

The obmm_shmdev devices provide ioctl interfaces for user-space
libraries (libobmm) to configure memory devices, and mmap interfaces
for general user-space applications to map the OBMM memory.

This implementation ensures both import and export regions have
corresponding memory devices, providing a standardized interface for
user-space applications to interact with OBMM memory regions.

Signed-off-by: Li Ruilin <liruilin4@huawei.com>
Signed-off-by: Gao Chao <gaochao24@huawei.com>

drivers/ub/obmm/obmm_core.h
@@ -77,6 +77,9 @@ struct obmm_region {
 
 	unsigned long flags;
 
+	struct cdev cdevice;
+	struct device device;
+
 	refcount_t refcnt;
 
 	/* the total size of all memory segments included in meminfo */

drivers/ub/obmm/obmm_shm_dev.c
@@ -1,0 +1,144 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+ * Description：OBMM Framework's implementations.
+ */
+
+#include <linux/cacheflush.h>
+#include <asm/tlbflush.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+
+#include "obmm_cache.h"
+#include "obmm_export_region_ops.h"
+#include "obmm_import.h"
+#include "obmm_shm_dev.h"
+
+static dev_t obmm_devt;
+
+static const char *obmm_shm_region_name = "OBMM_SHMDEV";
+static const char *obmm_shm_rootdev_name = "obmm";
+static struct device *obmm_shm_rootdev;
+
+static int obmm_shm_fops_open(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static int obmm_shm_fops_flush(struct file *file __always_unused, fl_owner_t owner __always_unused)
+{
+	return 0;
+}
+
+static int obmm_shm_fops_release(struct inode *inode __always_unused, struct file *file)
+{
+	return 0;
+}
+
+static int obmm_shm_fops_mmap(struct file *file, struct vm_area_struct *vma)
+{
+	return -ENOTTY;
+}
+
+static long obmm_shm_fops_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	return -ENOTTY;
+}
+
+const struct file_operations obmm_shm_fops = { .owner = THIS_MODULE,
+					       .unlocked_ioctl = obmm_shm_fops_ioctl,
+					       .mmap = obmm_shm_fops_mmap,
+					       .open = obmm_shm_fops_open,
+					       .flush = obmm_shm_fops_flush,
+					       .release = obmm_shm_fops_release };
+
+static void obmm_shm_dev_release(struct device *dev)
+{
+	struct obmm_region *reg;
+
+	reg = container_of(dev, struct obmm_region, device);
+	module_put(THIS_MODULE);
+}
+
+int obmm_shm_dev_add(struct obmm_region *reg)
+{
+	int ret;
+	dev_t devt;
+
+	if (!try_module_get(THIS_MODULE)) {
+		pr_err("Module is dying. Reject all memory requests\n");
+		return -EPERM;
+	}
+
+	devt = MKDEV(MAJOR(obmm_devt), reg->regionid);
+	cdev_init(&reg->cdevice, &obmm_shm_fops);
+	reg->cdevice.owner = THIS_MODULE;
+	reg->device.devt = devt;
+	reg->device.release = obmm_shm_dev_release;
+	reg->device.parent = obmm_shm_rootdev;
+	device_initialize(&reg->device);
+
+	ret = dev_set_name(&reg->device, "obmm_shmdev%d", reg->regionid);
+	if (ret) {
+		pr_err("Failed to set name for shmdev %d. ret=%pe\n", reg->regionid, ERR_PTR(ret));
+		goto err_put_dev;
+	}
+
+	ret = cdev_device_add(&reg->cdevice, &reg->device);
+	if (ret) {
+		pr_err("Failed to add shm device %d. ret=%pe\n", reg->regionid, ERR_PTR(ret));
+		goto err_put_dev;
+	}
+
+	return 0;
+
+	/* NOTE: If the device is properly initialized, the refcount of module
+	 * should be maintained by device kobject (and the associated
+	 * obmm_shm_dev_release function). The refcount of region is always
+	 * recovered by kobject-triggered release function.
+	 */
+err_put_dev:
+	put_device(&reg->device);
+	return ret;
+}
+
+void obmm_shm_dev_del(struct obmm_region *reg)
+{
+	cdev_device_del(&reg->cdevice, &reg->device);
+	put_device(&reg->device);
+}
+
+int obmm_shm_dev_init(void)
+{
+	int ret;
+
+	pr_info("shmdev: root device initialization started\n");
+	ret = alloc_chrdev_region(&obmm_devt, OBMM_MIN_VALID_REGIONID, OBMM_REGIONID_MAX_COUNT,
+				  obmm_shm_region_name);
+	if (ret) {
+		pr_err("Failed to allocate char device ID. ret=%pe\n", ERR_PTR(ret));
+		goto err_reg_alloc;
+	}
+
+	obmm_shm_rootdev = root_device_register(obmm_shm_rootdev_name);
+	if (IS_ERR_OR_NULL(obmm_shm_rootdev)) {
+		pr_err("error register obmm root device\n");
+		ret = -ENOMEM;
+		goto err_rootdev;
+	}
+
+	pr_info("shmdev: root device initialization completed\n");
+	return 0;
+err_rootdev:
+	unregister_chrdev_region(obmm_devt, OBMM_REGIONID_MAX_COUNT);
+err_reg_alloc:
+	return ret;
+}
+
+void obmm_shm_dev_exit(void)
+{
+	pr_info("shmdev: root device starts shutting down\n");
+	root_device_unregister(obmm_shm_rootdev);
+	unregister_chrdev_region(obmm_devt, OBMM_REGIONID_MAX_COUNT);
+	pr_info("shmdev: root device shut down completed\n");
+}

drivers/ub/obmm/obmm_shm_dev.h
@@ -1,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+ */
+
+#ifndef OBMM_SHM_DEV_H
+#define OBMM_SHM_DEV_H
+
+#include "obmm_core.h"
+
+int obmm_shm_dev_init(void);
+void obmm_shm_dev_exit(void);
+int obmm_shm_dev_add(struct obmm_region *reg);
+void obmm_shm_dev_del(struct obmm_region *reg);
+
+#endif

drivers/ub/obmm/Makefile
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-2.0+
 
 obmm-y := obmm_core.o \
-	obmm_cache.o obmm_import.o \
+	obmm_shm_dev.o obmm_cache.o obmm_import.o \
 	obmm_export_from_user.o conti_mem_allocator.o \
 	obmm_export.o obmm_export_from_pool.o obmm_preimport.o \
 	ubmempool_allocator.o obmm_export_region_ops.o \

drivers/ub/obmm/obmm_core.c
@@ -20,6 +20,7 @@
 
 #include <ub/ubus/ub-mem-decoder.h>
 
+#include "obmm_shm_dev.h"
 #include "obmm_cache.h"
 #include "obmm_export_region_ops.h"
 #include "ubmempool_allocator.h"
@@ -321,11 +322,19 @@ int register_obmm_region(struct obmm_region *region)
 {
 	int retval;
 
+	/* create device */
+	retval = obmm_shm_dev_add(region);
+	if (retval) {
+		pr_err("Failed to create device %d. ret=%pe\n", region->regionid, ERR_PTR(retval));
+		return retval;
+	}
+
 	/* insert OBMM_region */
 	retval = insert_obmm_region(region);
 	if (retval < 0) {
 		pr_err("Failed to insert obmm region %d on creation. ret=%pe\n", region->regionid,
 		       ERR_PTR(retval));
+		obmm_shm_dev_del(region);
 		return retval;
 	}
 
@@ -335,6 +344,7 @@ int register_obmm_region(struct obmm_region *region)
 void deregister_obmm_region(struct obmm_region *region)
 {
 	remove_obmm_region(region);
+	obmm_shm_dev_del(region);
 }
 
 int set_obmm_region_priv(struct obmm_region *region, unsigned int priv_len, const void __user *priv)
@@ -573,6 +583,12 @@ static int __init obmm_init(void)
 	spin_lock_init(&g_obmm_ctx_info.lock);
 	INIT_LIST_HEAD(&g_obmm_ctx_info.regions);
 
+	ret = obmm_shm_dev_init();
+	if (ret) {
+		pr_err("failed to initialize obmm_shm_dev. ret=%pe\n", ERR_PTR(ret));
+		goto out_misc_deregister;
+	}
+
 	module_addr_check_init();
 
 	ret = module_preimport_init();
@@ -586,6 +602,8 @@ static int __init obmm_init(void)
 
 out_addr_check_exit:
 	module_addr_check_exit();
+	obmm_shm_dev_exit();
+out_misc_deregister:
 	misc_deregister(&obmm_dev_handle);
 out_allocator_exit:
 	ubmempool_allocator_exit();
@@ -598,6 +616,7 @@ static void __exit obmm_exit(void)
 
 	module_preimport_exit();
 	module_addr_check_exit();
+	obmm_shm_dev_exit();
 	misc_deregister(&obmm_dev_handle);
 	ubmempool_allocator_exit();
 

commit 0923efc7df209ddc774e3dedb51461ca57d8d91e

obmm: Fix race condition of region release and device release
euleros inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IDBKX6
CVE: NA

-------------------------------------

Due to the Linux inode cache mechanism, the release callback
of the device embedded in an OBMM region might execute after
the region itself has been freed with kfree(region) . This
creates a potential use-after-free vulnerability, as the
callback would be operating on already deallocated memory.
To address this, we need to implement a proper synchronization
mechanism to ensure the region memory is only freed after the
device release callback has completed execution.

Fixes: b100600e5c18 ("obmm: Add shared memory device interface")

Signed-off-by: Li Ruilin <liruilin4@huawei.com>

drivers/ub/obmm/obmm_core.h
@@ -87,6 +87,7 @@ struct obmm_region {
 
 	struct cdev cdevice;
 	struct device device;
+	atomic_t device_released;
 
 	refcount_t refcnt;
 

drivers/ub/obmm/obmm_export.c
@@ -25,6 +25,7 @@
 #include "obmm_core.h"
 #include "obmm_cache.h"
 #include "obmm_export.h"
+#include "obmm_shm_dev.h"
 
 int export_flags_to_region_flags(unsigned long *region_flags, unsigned long user_flags)
 {
@@ -266,6 +267,7 @@ int set_export_vendor(struct obmm_export_region *e_reg, const void __user *vendo
 
 void free_export_region(struct obmm_export_region *e_reg)
 {
+	wait_until_dev_released(&e_reg->region);
 	if (e_reg->vendor_len)
 		kfree(e_reg->vendor_info);
 

drivers/ub/obmm/obmm_export_from_pool.c
@@ -255,6 +255,8 @@ static struct obmm_export_region *alloc_region_from_cmd(struct obmm_cmd_export *
 	if (e_reg == NULL)
 		return ERR_PTR(-ENOMEM);
 
+	atomic_set(&e_reg->region.device_released, 1);
+
 	e_reg->region.type = OBMM_EXPORT_REGION;
 	e_reg->region.mem_size = total_size;
 	e_reg->region.mem_cap = OBMM_MEM_ALLOW_CACHEABLE_MMAP | OBMM_MEM_ALLOW_NONCACHEABLE_MMAP;

drivers/ub/obmm/obmm_export_from_user.c
@@ -279,6 +279,8 @@ alloc_export_region_from_obmm_cmd_export_pid(const struct obmm_cmd_export_pid *e
 	if (e_reg == NULL)
 		return ERR_PTR(-ENOMEM);
 
+	atomic_set(&e_reg->region.device_released, 1);
+
 	e_reg->mem_desc_pid.pid = export_pid->pid;
 	e_reg->mem_desc_pid.user_va = export_pid->va;
 	e_reg->region.mem_size = export_pid->length;

drivers/ub/obmm/obmm_import.c
@@ -16,6 +16,7 @@
 #include "obmm_preimport.h"
 #include "obmm_resource.h"
 #include "obmm_addr_check.h"
+#include "obmm_shm_dev.h"
 
 static void set_import_region_datapath(const struct obmm_import_region *i_reg,
 				       struct obmm_datapath *datapath)
@@ -415,6 +416,8 @@ int obmm_import(struct obmm_cmd_import *cmd_import)
 	if (i_reg == NULL)
 		return -ENOMEM;
 
+	atomic_set(&i_reg->region.device_released, 1);
+
 	/* arguments to region (logs produced by callee) */
 	retval = init_import_region_from_cmd(cmd_import, i_reg);
 	if (retval)
@@ -455,6 +458,7 @@ out_release_memory:
 out_region_uninit:
 	uninit_obmm_region(&i_reg->region);
 out_free_ireg:
+	wait_until_dev_released(&i_reg->region);
 	kfree(i_reg);
 	return retval;
 }
@@ -493,6 +497,7 @@ int obmm_unimport(const struct obmm_cmd_unimport *cmd_unimport)
 
 	deregister_obmm_region(reg);
 	uninit_obmm_region(reg);
+	wait_until_dev_released(&i_reg->region);
 	kfree(i_reg);
 
 	pr_info("%s: mem_id=%llu completed.\n", __func__, cmd_unimport->mem_id);

drivers/ub/obmm/obmm_shm_dev.c
@@ -881,7 +881,16 @@ const struct file_operations obmm_shm_fops = { .owner = THIS_MODULE,
 
 static void obmm_shm_dev_release(struct device *dev)
 {
+	struct obmm_region *reg = container_of(dev, struct obmm_region, device);
+
+	atomic_set(&reg->device_released, 1);
 	module_put(THIS_MODULE);
+}
+
+void wait_until_dev_released(struct obmm_region *reg)
+{
+	while (atomic_read(&reg->device_released) == 0)
+		cpu_relax();
 }
 
 int obmm_shm_dev_add(struct obmm_region *reg)
@@ -917,6 +926,8 @@ int obmm_shm_dev_add(struct obmm_region *reg)
 		pr_err("Failed to add shm device %d. ret=%pe\n", reg->regionid, ERR_PTR(ret));
 		goto err_put_dev;
 	}
+
+	atomic_set(&reg->device_released, 0);
 
 	return 0;
 

drivers/ub/obmm/obmm_shm_dev.h
@@ -12,5 +12,7 @@ int obmm_shm_dev_init(void);
 void obmm_shm_dev_exit(void);
 int obmm_shm_dev_add(struct obmm_region *reg);
 void obmm_shm_dev_del(struct obmm_region *reg);
+void wait_until_dev_released(struct obmm_region *reg);
+
 
 #endif