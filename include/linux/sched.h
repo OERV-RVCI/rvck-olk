OERV-RVCI/rvck-olk

commit 832cfa264d7c521e75b1b9ecb763c1f67993ad0e

xsched: add xsched_{set,get}attr syscall
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID9IFI

-----------------------------------------

If CONFIG_CGROUP_XCU is not enabled, the xsched defaults to the RT
scheduling class, and there is no interface available to configure the
RT priority. Therefore, two new syscalls are added to set and get the
configuration of the RT scheduling class.

The default priority for RT is 4 (the lowest priority) in the kernel. In
contrast, user-configured values work the opposity way: the smaller the
value, the lower the priority. The child process inherits the priority
configuration of the parent process during fork. If pid=0, it indicates
that the configuration is applied to the current process. Additionally,
an interface is reserved for configuring the xsched scheduling class,
which will be used to support CFS in the future.

Currently, these system calls are only available for ARM64 and x86_64
architectures.

Fixes: 832ec54e11a0 ("xsched: Add xsched RT class")
Signed-off-by: Liu Kai <liukai284@huawei.com>

include/linux/sched.h
@@ -41,6 +41,10 @@
 #include <linux/thread_bits.h>
 #include <linux/kabi.h>
 
+#ifdef CONFIG_XCU_SCHEDULER
+#include <linux/xsched_types.h>
+#endif
+
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
 struct bio_list;
@@ -776,6 +780,10 @@ struct kmap_ctrl {
 struct task_struct_resvd {
 	/* pointer back to the main task_struct */
 	struct task_struct	*task;
+
+#ifdef CONFIG_XCU_SCHEDULER
+	struct xsched_attr xse_attr;
+#endif
 };
 
 struct task_struct {

include/linux/xsched.h
@@ -6,6 +6,7 @@
 #include <linux/kref.h>
 #include <linux/vstream.h>
 #include <linux/xcu_group.h>
+#include <linux/xsched_types.h>
 
 #ifndef pr_fmt
 #define pr_fmt(fmt) fmt
@@ -52,20 +53,6 @@
 #define XSCHED_CFS_KICK_SLICE 10
 
 extern struct xsched_cu *xsched_cu_mgr[XSCHED_NR_CUS];
-
-enum xcu_sched_type {
-	XSCHED_TYPE_RT = 0,
-	XSCHED_TYPE_CFS = 1,
-	XSCHED_TYPE_NUM,
-	XSCHED_TYPE_DFLT = XSCHED_TYPE_RT
-};
-
-enum xse_prio {
-	XSE_PRIO_HIGH = 0,
-	XSE_PRIO_LOW = 4,
-	NR_XSE_PRIO,
-	XSE_PRIO_DFLT = XSE_PRIO_LOW
-};
 
 extern struct xsched_class rt_xsched_class;
 extern struct xsched_class fair_xsched_class;
@@ -406,7 +393,7 @@ static inline u64 gcd(u64 a, u64 b)
 }
 
 struct xsched_class {
-	enum xcu_sched_type class_id;
+	enum xcu_sched_class class_id;
 	size_t kick_slice;
 	struct list_head node;
 
@@ -456,7 +443,7 @@ int xsched_init_entity(struct xsched_context *ctx, struct vstream_info *vs);
 int ctx_bind_to_xcu(vstream_info_t *vstream_info, struct xsched_context *ctx);
 int xsched_vsm_add_tail(struct vstream_info *vs, vstream_args_t *arg);
 struct vstream_metadata *xsched_vsm_fetch_first(struct vstream_info *vs);
-int xsched_rt_prio_set(pid_t tgid, unsigned int prio);
+void xsched_rt_prio_set(pid_t tgid, unsigned int prio);
 void enqueue_ctx(struct xsched_entity *xse, struct xsched_cu *xcu);
 void dequeue_ctx(struct xsched_entity *xse, struct xsched_cu *xcu);
 int delete_ctx(struct xsched_context *ctx);

init/init_task.c
@@ -14,6 +14,9 @@
 #include <linux/scs.h>
 
 #include <linux/uaccess.h>
+#ifdef CONFIG_XCU_SCHEDULER
+#include <linux/xsched_types.h>
+#endif
 
 static struct signal_struct init_signals = {
 	.nr_threads	= 1,
@@ -59,6 +62,9 @@ unsigned long init_shadow_call_stack[SCS_SIZE / sizeof(long)]
 
 static struct task_struct_resvd init_task_struct_resvd = {
 	.task = &init_task,
+#ifdef CONFIG_XCU_SCHEDULER
+	.xse_attr = { .xsched_priority = XSE_PRIO_DFLT },
+#endif
 };
 
 /*

arch/arm/tools/syscall.tbl
@@ -480,5 +480,5 @@
 464	common	kabi_reserved464		sys_ni_syscall
 465	common	kabi_reserved465		sys_ni_syscall
 466	common	kabi_reserved466		sys_ni_syscall
-467	common	kabi_reserved467		sys_ni_syscall
-468	common	kabi_reserved468		sys_ni_syscall
+467	common	xsched_setattr		sys_ni_syscall
+468	common	xsched_getattr		sys_ni_syscall

arch/powerpc/kernel/syscalls/syscall.tbl
@@ -557,5 +557,5 @@
 464	common	kabi_reserved464		sys_ni_syscall
 465	common	kabi_reserved465		sys_ni_syscall
 466	common	kabi_reserved466		sys_ni_syscall
-467	common	kabi_reserved467		sys_ni_syscall
-468	common	kabi_reserved468		sys_ni_syscall
+467	common	xsched_setattr		sys_ni_syscall
+468	common	xsched_getattr		sys_ni_syscall

arch/x86/entry/syscalls/syscall_32.tbl
@@ -471,5 +471,5 @@
 464	i386	kabi_reserved464	sys_ni_syscall
 465	i386	kabi_reserved465	sys_ni_syscall
 466	i386	kabi_reserved466	sys_ni_syscall
-467	i386	kabi_reserved467	sys_ni_syscall
-468	i386	kabi_reserved468	sys_ni_syscall
+467	i386	xsched_setattr	sys_ni_syscall
+468	i386	xsched_getattr	sys_ni_syscall

arch/x86/entry/syscalls/syscall_64.tbl
@@ -388,8 +388,8 @@
 464	common	kabi_reserved464	sys_ni_syscall
 465	common	kabi_reserved465	sys_ni_syscall
 466	common	kabi_reserved466	sys_ni_syscall
-467	common	kabi_reserved467	sys_ni_syscall
-468	common	kabi_reserved468	sys_ni_syscall
+467	common	xsched_setattr	    sys_xsched_setattr
+468	common	xsched_getattr	    sys_xsched_getattr
 
 #
 # Due to a historical design error, certain syscalls are numbered differently

include/linux/syscalls.h
@@ -950,6 +950,9 @@ asmlinkage long sys_cachestat(unsigned int fd,
 asmlinkage long sys_map_shadow_stack(unsigned long addr, unsigned long size, unsigned int flags);
 
 asmlinkage long sys_vstream_manage(struct vstream_args __user *arg, int cmd);
+
+asmlinkage long sys_xsched_setattr(pid_t pid, struct xsched_attr __user *arg);
+asmlinkage long sys_xsched_getattr(pid_t pid, struct xsched_attr __user *arg);
 /*
  * Architecture-specific system calls
  */

commit 09d8c86c56b27f90588057f45d342ff479559f48

xsched: move xsched types to user-accessible headers
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/ID9IFI

-----------------------------------------

Fix xsched syscall compilation by resolving declaration conflicts and
ensuring user-space access to parameter types.

- Move include/linux/xsched_types.h -> include/uapi/linux/xsched for
  user-space visibility
- Move include/uapi/linux/xcu_vstream.h -> include/uapi/linux/xsched
- Fixes errors: "conflicting types" and "struct not visible"

Fixes: 832cfa264d7c ("xsched: add xsched_{set,get}attr syscall")
Signed-off-by: Liu Kai <liukai284@huawei.com>

include/linux/sched.h
@@ -42,7 +42,7 @@
 #include <linux/kabi.h>
 
 #ifdef CONFIG_XCU_SCHEDULER
-#include <linux/xsched_types.h>
+#include <uapi/linux/xsched/types.h>
 #endif
 
 /* task_struct member predeclarations (sorted alphabetically): */

include/linux/vstream.h
@@ -2,7 +2,7 @@
 #ifndef _LINUX_VSTREAM_H
 #define _LINUX_VSTREAM_H
 
-#include <uapi/linux/xcu_vstream.h>
+#include <uapi/linux/xsched/xcu_vstream.h>
 #include <linux/ktime.h>
 
 #define MAX_VSTREAM_SIZE 2048

include/linux/xcu_group.h
@@ -3,7 +3,7 @@
 #define __XSCHED_XCU_GROUP_H__
 
 #include <linux/idr.h>
-#include <uapi/linux/xcu_vstream.h>
+#include <uapi/linux/xsched/xcu_vstream.h>
 
 #ifndef CONFIG_XSCHED_NR_CUS
 #define CONFIG_XSCHED_NR_CUS 128

include/linux/xsched.h
@@ -4,10 +4,10 @@
 
 #include <linux/cgroup.h>
 #include <linux/kref.h>
+#include <linux/math64.h>
 #include <linux/vstream.h>
 #include <linux/xcu_group.h>
-#include <linux/xsched_types.h>
-#include <linux/math64.h>
+#include <uapi/linux/xsched/types.h>
 
 #define XSCHED_LOG_PREFIX "XSched"
 #define XSCHED_INFO(fmt, ...)                                                  \

include/uapi/linux/xsched/types.h
@@ -1,13 +1,15 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _XSCHED_TYPE_H
-#define _XSCHED_TYPE_H
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI_LINUX_XSCHED_TYPES_H
+#define _UAPI_LINUX_XSCHED_TYPES_H
+
+#include <linux/types.h>
 
 struct xsched_attr {
 	/* Scheduling class type, from enum xcu_sched_class */
-	unsigned int xsched_class;
+	__u32 xsched_class;
 
 	/* RT scheduling priority, from enum xse_prio */
-	unsigned int xsched_priority;
+	__u32 xsched_priority;
 };
 
 enum xcu_sched_class {
@@ -24,4 +26,4 @@ enum xse_prio {
 	XSE_PRIO_DFLT = XSE_PRIO_LOW
 };
 
-#endif /* ! _XSCHED_TYPE_H */
+#endif /* _UAPI_LINUX_XSCHED_TYPES_H */

include/uapi/linux/xsched/xcu_vstream.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _UAPI_XCU_VSTREAM_H
-#define _UAPI_XCU_VSTREAM_H
+#ifndef _UAPI_LINUX_XSCHED_XCU_VSTREAM_H
+#define _UAPI_LINUX_XSCHED_XCU_VSTREAM_H
 
 #include <linux/types.h>
 
@@ -74,4 +74,4 @@ typedef struct vstream_args {
 	KABI_RESERVE_BYTES(2, 8);
 } vstream_args_t;
 
-#endif /* _UAPI_LINUX_SCHED_H */
+#endif /* _UAPI_LINUX_XSCHED_XCU_VSTREAM_H */

init/init_task.c
@@ -15,7 +15,7 @@
 
 #include <linux/uaccess.h>
 #ifdef CONFIG_XCU_SCHEDULER
-#include <linux/xsched_types.h>
+#include <uapi/linux/xsched/types.h>
 #endif
 
 static struct signal_struct init_signals = {