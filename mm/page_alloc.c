openeuler-mirror/kernel

commit e0fb8bd67d0adc65b595f01762f2e8f1981eed67

mm: mem_reliable: Introduce memory reliable
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8USBA
CVE: NA

--------------------------------

Introduction

============

Memory reliable feature is a memory tiering mechanism. It is based on
kernel mirror feature, which splits memory into two separate regions,
mirrored(reliable) region and non-mirrored (non-reliable) region.

for kernel mirror feature:

- allocate kernel memory from mirrored region by default
- allocate user memory from non-mirrored region by default

non-mirrored region will be arranged into ZONE_MOVABLE.

for kernel reliable feature, it has additional features below:

- normal user tasks never alloc memory from mirrored region with userspace
  apis(malloc, mmap, etc.)
- special user tasks will allocate memory from mirrored region by default
- shmem/pagecache allocate memory from mirrored region by default
- upper limit of mirrored region allocated for user tasks, shmem and
  page cache

Support Reliable fallback mechanism which allows special user tasks, shmem
and page cache can fallback to alloc non-mirrored region, it's the default
setting.

In order to fulfill the goal

- GFP_KERNEL flag added for task to alloc memory from mirrored region.
- the high_zoneidx for special user tasks/shmem/pagecache is set to
  ZONE_NORMAL to alloc memory from mirrored region.
- normal user tasks can only alloc memory from ZONE_MOVABLE.

This patch is just the main framework, memory reliable support for special
user tasks, page cache and shmem has their own patches.

To enable this function, mirrored(reliable) memory is needed and
"kernelcore=reliable" should be added to kernel parameters.

Signed-off-by: Ma Wupeng <mawupeng1@huawei.com>

mm/page_alloc.c
@@ -3189,6 +3189,10 @@ retry:
 		struct page *page;
 		unsigned long mark;
 
+		/* skip non-mirrored zone for normal user tasks */
+		if (skip_non_mirrored_zone(gfp_mask, z))
+			continue;
+
 		if (cpusets_enabled() &&
 			(alloc_flags & ALLOC_CPUSET) &&
 			!__cpuset_zone_allowed(zone, gfp_mask))

include/linux/gfp.h
@@ -134,6 +134,12 @@ static inline enum zone_type gfp_zone(gfp_t flags)
 	z = (GFP_ZONE_TABLE >> (bit * GFP_ZONES_SHIFT)) &
 					 ((1 << GFP_ZONES_SHIFT) - 1);
 	VM_BUG_ON((GFP_ZONE_BAD >> bit) & 1);
+
+#ifdef CONFIG_MEMORY_RELIABLE
+	if (z == ZONE_MOVABLE && (flags & GFP_RELIABLE))
+		return ZONE_NORMAL;
+#endif
+
 	return z;
 }
 

include/linux/gfp_types.h
@@ -31,7 +31,11 @@ typedef unsigned int __bitwise gfp_t;
 #define ___GFP_IO		0x40u
 #define ___GFP_FS		0x80u
 #define ___GFP_ZERO		0x100u
-/* 0x200u unused */
+#ifdef CONFIG_MEMORY_RELIABLE
+#define ___GFP_RELIABLE		0x200u
+#else
+#define ___GFP_RELIABLE		0
+#endif
 #define ___GFP_DIRECT_RECLAIM	0x400u
 #define ___GFP_KSWAPD_RECLAIM	0x800u
 #define ___GFP_WRITE		0x1000u
@@ -248,6 +252,9 @@ typedef unsigned int __bitwise gfp_t;
 /* Disable lockdep for GFP context tracking */
 #define __GFP_NOLOCKDEP ((__force gfp_t)___GFP_NOLOCKDEP)
 
+/* Alloc memory from mirrored region */
+#define __GFP_RELIABLE ((__force gfp_t)___GFP_RELIABLE)
+
 /* Room for N __GFP_FOO bits */
 #define __GFP_BITS_SHIFT (26 + IS_ENABLED(CONFIG_LOCKDEP))
 #define __GFP_BITS_MASK ((__force gfp_t)((1 << __GFP_BITS_SHIFT) - 1))
@@ -336,5 +343,6 @@ typedef unsigned int __bitwise gfp_t;
 #define GFP_TRANSHUGE_LIGHT	((GFP_HIGHUSER_MOVABLE | __GFP_COMP | \
 			 __GFP_NOMEMALLOC | __GFP_NOWARN) & ~__GFP_RECLAIM)
 #define GFP_TRANSHUGE	(GFP_TRANSHUGE_LIGHT | __GFP_DIRECT_RECLAIM)
+#define GFP_RELIABLE __GFP_RELIABLE
 
 #endif /* __LINUX_GFP_TYPES_H */

include/linux/mem_reliable.h
@@ -1,0 +1,79 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __MM_MEM_RELIABLE__
+#define __MM_MEM_RELIABLE__
+
+#ifdef CONFIG_MEMORY_RELIABLE
+
+#include <linux/stddef.h>
+#include <linux/gfp.h>
+#include <linux/mmzone.h>
+#include <linux/mm_types.h>
+#include <linux/sched.h>
+
+DECLARE_STATIC_KEY_FALSE(mem_reliable);
+
+extern bool reliable_enabled;
+
+void mem_reliable_init(bool has_unmirrored_mem, unsigned long mirrored_sz);
+bool mem_reliable_status(void);
+
+static inline bool mem_reliable_is_enabled(void)
+{
+	return static_branch_likely(&mem_reliable);
+}
+
+static inline bool page_reliable(struct page *page)
+{
+	if (!mem_reliable_is_enabled())
+		return false;
+
+	if (!page)
+		return false;
+
+	return page_zonenum(page) < ZONE_MOVABLE;
+}
+
+static inline bool folio_reliable(struct folio *folio)
+{
+	if (!mem_reliable_is_enabled())
+		return false;
+
+	if (!folio)
+		return false;
+
+	return folio_zonenum(folio) < ZONE_MOVABLE;
+}
+
+static inline bool skip_non_mirrored_zone(gfp_t gfp, struct zoneref *z)
+{
+	if (!mem_reliable_is_enabled())
+		return false;
+
+	if (!current->mm || (current->flags & PF_KTHREAD))
+		return false;
+
+	/* user tasks can only alloc memory from non-mirrored region */
+	if (!(gfp & GFP_RELIABLE) && (gfp & __GFP_HIGHMEM) &&
+	    (gfp & __GFP_MOVABLE)) {
+		if (zonelist_zone_idx(z) < ZONE_MOVABLE)
+			return true;
+	}
+
+	return false;
+}
+#else
+#define reliable_enabled 0
+
+static inline bool mem_reliable_is_enabled(void) { return false; }
+static inline void mem_reliable_init(bool has_unmirrored_mem,
+				     unsigned long mirrored_sz) {}
+static inline bool page_reliable(struct page *page) { return false; }
+static inline bool folio_reliable(struct folio *folio) { return false; }
+static inline bool skip_non_mirrored_zone(gfp_t gfp, struct zoneref *z)
+{
+	return false;
+}
+static inline bool mem_reliable_status(void) { return false; }
+#endif
+
+#endif

include/linux/mm.h
@@ -4093,4 +4093,7 @@ static inline void accept_memory(phys_addr_t start, phys_addr_t end)
 
 #endif
 
+/* added to mm.h to avoid every caller adding new header file */
+#include <linux/mem_reliable.h>
+
 #endif /* _LINUX_MM_H */

include/trace/events/mmflags.h
@@ -50,7 +50,8 @@
 	gfpflag_string(__GFP_RECLAIM),		\
 	gfpflag_string(__GFP_DIRECT_RECLAIM),	\
 	gfpflag_string(__GFP_KSWAPD_RECLAIM),	\
-	gfpflag_string(__GFP_ZEROTAGS)
+	gfpflag_string(__GFP_ZEROTAGS),		\
+	gfpflag_string(__GFP_RELIABLE)
 
 #ifdef CONFIG_KASAN_HW_TAGS
 #define __def_gfpflag_names_kasan ,			\

mm/Kconfig
@@ -1348,6 +1348,23 @@ config CLEAR_FREELIST_PAGE
 	  To enable this feature, kernel parameter "clear_freelist" also
 	  needs to be added.
 
+config MEMORY_RELIABLE
+	bool "Support for memory reliable"
+	depends on ARM64
+	default n
+	help
+	  Memory reliable is based on mirror memory. It has the following
+	  additional features:
+	  a) normal user tasks never alloc memory from mirrored region;
+	  b) special user tasks will allocate memory from mirrored region
+	  by default; c) upper limit of mirrored region allcated for user
+	  tasks, shmem and pagecache.
+	  Special user tasks and shmem/pagecache can fallback to
+	  non-mirrored region if you enable reliable fallback mechanism.
+
+	  To enable this function, mirrored memory is needed and
+	  "kernelcore=reliable" need to be added in kernel parameters.
+
 source "mm/damon/Kconfig"
 
 endmenu

mm/Makefile
@@ -144,3 +144,4 @@ obj-$(CONFIG_SHARE_POOL) += share_pool.o
 obj-$(CONFIG_MEMCG_MEMFS_INFO) += memcg_memfs_info.o
 obj-$(CONFIG_PAGE_CACHE_LIMIT) += page_cache_limit.o
 obj-$(CONFIG_CLEAR_FREELIST_PAGE) += clear_freelist_page.o
+obj-$(CONFIG_MEMORY_RELIABLE) += mem_reliable.o

OERV-RVCI/rvck-olk

commit ce7b69c31c4050bbdafe79b184a41b8323cafd73

mm: mem_reliable: prevent excessive direct reclaim loops
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/5441

------------------------------------------

In our testing, under memory pressure, it becomes difficult totrigger an
OOM(out-of-memory) kill when regular memory allocation competes
concurrently with driver GFP_ATOMIC allocations.
This occurs because GFP_ATOMIC allocations can succeed from as low as 3/8
of the min watermark. In such a situation, even if direct reclaim can free
a small number of pages, regular memory requests may still fail because the
current watermark remains too high for them.

In the following scenario:

restart:
    __alloc_pages_direct_reclaim   // reclaims a small number of pages
    should_reclaim_retry           // watermark too high for user
                                   // resets no_progress_loops to zero and
                                   // goes to restart
    goto restart;

In our production environment, we encountered a case where memory
allocation performed __alloc_pages_direct_reclaim 257 times without
either succeeding or triggering OOM, causing service freezes.

To avoid meaningless continuous direct reclaim loops, skip watermark
check for non mirrored zone if memory reliable enabled.

Fixes: e0fb8bd67d0a ("mm: mem_reliable: Introduce memory reliable")
Signed-off-by: Wupeng Ma <mawupeng1@huawei.com>

mm/page_alloc.c
@@ -4188,6 +4188,9 @@ should_reclaim_retry(gfp_t gfp_mask, unsigned order,
 		unsigned long min_wmark = min_wmark_pages(zone);
 		bool wmark;
 
+		if (skip_non_mirrored_zone(gfp_mask, z))
+			continue;
+
 		available = reclaimable = zone_reclaimable_pages(zone);
 		available += zone_page_state_snapshot(zone, NR_FREE_PAGES);
 