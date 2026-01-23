openeuler-mirror/kernel

commit ad540a13eb8292fc92651b3bf281088a502a85f9

virtcca feature: fix msi iova map
virtcca inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/IAQON6

--------------------------------

VirtCCA Coda Feature:
Fix msi iova map

Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>
Signed-off-by: Junbin Li <lijunbin4@huawei.com>
---

arch/arm64/include/asm/virtcca_coda.h
@@ -11,11 +11,26 @@
 #include "../../../drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h"
 #include "../../../drivers/iommu/arm/arm-smmu-v3/arm-s-smmu-v3.h"
 
-#ifdef CONFIG_HISI_VIRTCCA_HOST
-int virtcca_attach_secure_dev(struct iommu_domain *domain, struct iommu_group *group);
-int virtcca_smmu_secure_dev_operator(struct iommu_domain *domain, struct device *dev);
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 
-struct iommu_domain *virtcca_iommu_group_get_domain(struct iommu_group *iommu_group);
+#define MAX_CC_DEV_NUM_ORDER    8
+#define MASK_DEV_FUNCTION       0xfff8
+#define MASK_DEV_BUS            0xff
+
+#define DEV_BUS_NUM             0x8
+#define DEV_FUNCTION_NUM        0x3
+
+#define STE_ENTRY_SIZE          0x40
+
+#define SMMU_DOMAIN_IS_SAME     0x2
+
+int virtcca_attach_secure_dev(struct iommu_domain *domain, struct iommu_group *group);
+
+u64 virtcca_get_iommu_device_msi_addr(struct iommu_group *iommu_group);
+int virtcca_iommu_group_set_dev_msi_addr(struct iommu_group *iommu_group, unsigned long *iova);
+int virtcca_map_msi_address(struct kvm *kvm, struct arm_smmu_domain *smmu_domain, phys_addr_t pa,
+	unsigned long map_size);
+
 int virtcca_iommu_map(struct iommu_domain *domain, unsigned long iova,
 	phys_addr_t paddr, size_t size, int prot);
 size_t virtcca_iommu_unmap(struct iommu_domain *domain,
@@ -28,12 +43,12 @@ size_t virtcca_unmap_pages(void *ops, unsigned long iova,
 
 void virtcca_pci_read_msi_msg(struct pci_dev *dev, struct msi_msg *msg,
 	void __iomem *base);
-int virtcca_pci_write_msg_msi(struct msi_desc *desc, struct msi_msg *msg);
+bool virtcca_pci_write_msg_msi(struct msi_desc *desc, struct msi_msg *msg);
 void virtcca_msix_prepare_msi_desc(struct pci_dev *dev,
 	struct msi_desc *desc, void __iomem *addr);
-int virtcca_pci_msix_write_vector_ctrl(struct msi_desc *desc, u32 ctrl);
-int virtcca_pci_msix_mask(struct msi_desc *desc);
-int msix_mask_all_cc(struct pci_dev *dev, void __iomem *base, int tsize, u64 dev_num);
+bool virtcca_pci_msix_write_vector_ctrl(struct msi_desc *desc, u32 ctrl);
+bool virtcca_pci_msix_mask(struct msi_desc *desc);
+int virtcca_msix_mask_all_cc(struct pci_dev *dev, void __iomem *base, int tsize, u64 dev_num);
 
 int virtcca_pci_generic_config_read(void __iomem *addr, unsigned char bus_num,
 	unsigned int devfn, int size, u32 *val);
@@ -47,7 +62,7 @@ u64 virtcca_pci_io_read(struct vfio_pci_core_device *vdev,
 	u64 size, void __iomem *io);
 
 bool virtcca_iommu_domain_get_kvm(struct iommu_domain *domain, struct kvm **kvm);
-bool virtcca_check_kvm_is_cvm(void *iommu, struct kvm **kvm);
+bool virtcca_check_is_cvm_or_not(void *iommu, struct kvm **kvm);
 int virtcca_vfio_iommu_map(void *iommu, dma_addr_t iova,
 	unsigned long pfn, long npage, int prot);
 int cvm_vfio_add_kvm_to_smmu_domain(struct file *filp, void *kv);
@@ -56,6 +71,23 @@ void kvm_get_arm_smmu_domain(struct kvm *kvm, struct list_head *smmu_domain_grou
 struct arm_lpae_io_pgtable *virtcca_io_pgtable_get_data(void *ops);
 struct io_pgtable_cfg *virtcca_io_pgtable_get_cfg(struct arm_lpae_io_pgtable *data);
 struct iommu_group *cvm_vfio_file_iommu_group(struct file *file);
-struct kvm *virtcca_smmu_domain_get_kvm(struct arm_lpae_io_pgtable *data);
+void *virtcca_io_pgtable_get_smmu_domain(struct arm_lpae_io_pgtable *data);
+
+struct iommu_group *virtcca_vfio_file_iommu_group(struct file *file);
+
+bool is_cc_vmid(u32 vmid);
+/* Has the root bus device number switched to secure */
+bool is_cc_dev(u32 sid);
+
+u64 get_g_cc_dev_msi_addr(u32 sid);
+
+void set_g_cc_dev_msi_addr(u32 sid, u64 msi_addr);
+
+void g_cc_dev_table_init(void);
+
+u32 virtcca_tmi_dev_attach(struct arm_smmu_domain *arm_smmu_domain, struct kvm *kvm);
+
+struct iommu_dma_msi_page *virtcca_iommu_dma_get_msi_page(struct device *dev,
+	phys_addr_t msi_addr, struct iommu_domain *domain);
 #endif
 #endif

drivers/coda/coda_pci.c
@@ -1,0 +1,214 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2024. Huawei Technologies Co., Ltd. All rights reserved.
+ */
+#include <linux/kvm_host.h>
+#include <asm/virtcca_coda.h>
+
+#include "../drivers/pci/msi/msi.h"
+
+/**
+ * virtcca_pci_read_msi_msg - secure dev read msi msg
+ * @dev: Pointer to the pci_dev data structure of MSI-X device function
+ * @msg: Msg information
+ * @base: Msi base address
+ *
+ **/
+void virtcca_pci_read_msi_msg(struct pci_dev *dev, struct msi_msg *msg,
+	void __iomem *base)
+{
+	u64 pbase = mmio_va_to_pa(base);
+
+	msg->address_lo = tmi_mmio_read(pbase + PCI_MSIX_ENTRY_LOWER_ADDR,
+		CVM_RW_32_BIT, pci_dev_id(dev));
+	msg->address_hi = tmi_mmio_read(pbase + PCI_MSIX_ENTRY_UPPER_ADDR,
+		CVM_RW_32_BIT, pci_dev_id(dev));
+	msg->data = tmi_mmio_read(pbase + PCI_MSIX_ENTRY_DATA, CVM_RW_32_BIT, pci_dev_id(dev));
+}
+
+/**
+ * virtcca_pci_write_msi_msg - secure dev write msi msg
+ * @desc: MSI-X description
+ * @msg: Msg information
+ *
+ **/
+bool virtcca_pci_write_msg_msi(struct msi_desc *desc, struct msi_msg *msg)
+{
+	if (!is_virtcca_cvm_enable())
+		return false;
+
+	void __iomem *base = pci_msix_desc_addr(desc);
+	u32 ctrl = desc->pci.msix_ctrl;
+	bool unmasked = !(ctrl & PCI_MSIX_ENTRY_CTRL_MASKBIT);
+	u64 pbase = mmio_va_to_pa(base);
+	struct pci_dev *pdev = (desc->dev != NULL &&
+		dev_is_pci(desc->dev)) ? to_pci_dev(desc->dev) : NULL;
+
+	if (!is_cc_dev(pci_dev_id(pdev)))
+		return false;
+
+	u64 addr = (u64)msg->address_lo | ((u64)msg->address_hi << 32);
+
+	if (addr) {
+		/* Get the offset of the its register of a specific device */
+		u64 offset = addr - CVM_MSI_ORIG_IOVA;
+
+		addr = get_g_cc_dev_msi_addr(pci_dev_id(pdev));
+		addr += offset;
+		if (!addr)
+			return true;
+	}
+	tmi_mmio_write(pbase + PCI_MSIX_ENTRY_LOWER_ADDR,
+		lower_32_bits(addr), CVM_RW_32_BIT, pci_dev_id(pdev));
+	tmi_mmio_write(pbase + PCI_MSIX_ENTRY_UPPER_ADDR,
+		upper_32_bits(addr), CVM_RW_32_BIT, pci_dev_id(pdev));
+	tmi_mmio_write(pbase + PCI_MSIX_ENTRY_DATA,
+		msg->data, CVM_RW_32_BIT, pci_dev_id(pdev));
+
+	if (unmasked)
+		pci_msix_write_vector_ctrl(desc, ctrl);
+	tmi_mmio_read(mmio_va_to_pa((void *)pbase + PCI_MSIX_ENTRY_DATA),
+		CVM_RW_32_BIT, pci_dev_id(pdev));
+
+	return true;
+}
+
+void virtcca_msix_prepare_msi_desc(struct pci_dev *dev,
+	struct msi_desc *desc, void __iomem *addr)
+{
+	desc->pci.msix_ctrl = tmi_mmio_read(mmio_va_to_pa(addr + PCI_MSIX_ENTRY_VECTOR_CTRL),
+		CVM_RW_32_BIT, pci_dev_id(dev));
+}
+
+/*
+ * If it is a safety device, write vector ctrl need
+ * use tmi interface
+ */
+bool virtcca_pci_msix_write_vector_ctrl(struct msi_desc *desc, u32 ctrl)
+{
+	if (!is_virtcca_cvm_enable())
+		return false;
+
+	void __iomem *desc_addr = pci_msix_desc_addr(desc);
+	struct pci_dev *pdev = (desc->dev != NULL &&
+		dev_is_pci(desc->dev)) ? to_pci_dev(desc->dev) : NULL;
+
+	if (pdev == NULL || !is_cc_dev(pci_dev_id(pdev)))
+		return false;
+
+	if (desc->pci.msi_attrib.can_mask)
+		tmi_mmio_write(mmio_va_to_pa(desc_addr + PCI_MSIX_ENTRY_VECTOR_CTRL),
+			ctrl, CVM_RW_32_BIT, pci_dev_id(pdev));
+	return true;
+}
+
+/*
+ * If it is a safety device, read msix need
+ * use tmi interface
+ */
+bool virtcca_pci_msix_mask(struct msi_desc *desc)
+{
+	if (!is_virtcca_cvm_enable())
+		return false;
+
+	struct pci_dev *pdev = (desc->dev != NULL &&
+		dev_is_pci(desc->dev)) ? to_pci_dev(desc->dev) : NULL;
+
+	if (pdev == NULL || !is_cc_dev(pci_dev_id(pdev)))
+		return false;
+
+	/* Flush write to device */
+	tmi_mmio_read(mmio_va_to_pa(desc->pci.mask_base), CVM_RW_32_BIT, pci_dev_id(pdev));
+	return true;
+}
+
+/**
+ * virtcca_msix_mask_all_cc - mask all secure dev msix c
+ * @dev: Pointer to the pci_dev data structure of MSI-X device function
+ * @base: Io address
+ * @tsize: Number of entry
+ * @dev_num: Dev number
+ *
+ * Returns:
+ * %0 if msix mask all cc device success
+ **/
+int virtcca_msix_mask_all_cc(struct pci_dev *dev, void __iomem *base, int tsize, u64 dev_num)
+{
+	int i;
+	u16 rw_ctrl;
+	u32 ctrl = PCI_MSIX_ENTRY_CTRL_MASKBIT;
+	u64 pbase = mmio_va_to_pa(base);
+
+	if (pci_msi_ignore_mask)
+		goto out;
+
+	for (i = 0; i < tsize; i++, base += PCI_MSIX_ENTRY_SIZE) {
+		tmi_mmio_write(pbase + PCI_MSIX_ENTRY_VECTOR_CTRL,
+			ctrl, CVM_RW_32_BIT, dev_num);
+	}
+
+out:
+	pci_read_config_word(dev, dev->msix_cap + PCI_MSIX_FLAGS, &rw_ctrl);
+	rw_ctrl &= ~PCI_MSIX_FLAGS_MASKALL;
+	rw_ctrl |= 0;
+	pci_write_config_word(dev, dev->msix_cap + PCI_MSIX_FLAGS, rw_ctrl);
+
+	pcibios_free_irq(dev);
+	return 0;
+}
+
+/* If device is secure dev, read config need transfer to tmm module */
+int virtcca_pci_generic_config_read(void __iomem *addr, unsigned char bus_num,
+	unsigned int devfn, int size, u32 *val)
+{
+	u32 cvm_bit = size == 1 ? CVM_RW_8_BIT : size == 2 ? CVM_RW_16_BIT : CVM_RW_32_BIT;
+
+	*val = tmi_mmio_read(mmio_va_to_pa(addr), cvm_bit, PCI_DEVID(bus_num, devfn));
+	return 0;
+}
+
+/* If device is secure dev, write config need transfer to tmm module */
+int virtcca_pci_generic_config_write(void __iomem *addr, unsigned char bus_num,
+	unsigned int devfn, int size, u32 val)
+{
+	u32 cvm_bit = size == 1 ? CVM_RW_8_BIT : size == 2 ? CVM_RW_16_BIT : CVM_RW_32_BIT;
+
+	tmi_mmio_write(mmio_va_to_pa(addr), val, cvm_bit, PCI_DEVID(bus_num, devfn));
+	return 0;
+}
+
+/* Judge startup virtcca_cvm_host is enable and device is secure or not */
+bool is_virtcca_pci_io_rw(struct vfio_pci_core_device *vdev)
+{
+	if (!is_virtcca_cvm_enable())
+		return false;
+
+	struct pci_dev *pdev = vdev->pdev;
+	bool cc_dev = pdev == NULL ? false : is_cc_dev(pci_dev_id(pdev));
+
+	if (cc_dev)
+		return true;
+
+	return false;
+}
+EXPORT_SYMBOL_GPL(is_virtcca_pci_io_rw);
+
+/* Transfer to tmm write io value */
+void virtcca_pci_io_write(struct vfio_pci_core_device *vdev, u64 val,
+	u64 size, void __iomem *io)
+{
+	struct pci_dev *pdev = vdev->pdev;
+
+	WARN_ON(tmi_mmio_write(mmio_va_to_pa(io), val, size, pci_dev_id(pdev)));
+}
+EXPORT_SYMBOL_GPL(virtcca_pci_io_write);
+
+/* Transfer to tmm read io value */
+u64 virtcca_pci_io_read(struct vfio_pci_core_device *vdev,
+	u64 size, void __iomem *io)
+{
+	struct pci_dev *pdev = vdev->pdev;
+
+	return tmi_mmio_read(mmio_va_to_pa(io), size, pci_dev_id(pdev));
+}
+EXPORT_SYMBOL_GPL(virtcca_pci_io_read);

drivers/pci/msi/msi.c
@@ -13,7 +13,7 @@
 #include "../pci.h"
 #include "msi.h"
 
-#ifdef CONFIG_HISI_VIRTCCA_HOST
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 #include <asm/virtcca_cvm_host.h>
 #endif
 
@@ -163,7 +163,7 @@ void __pci_read_msi_msg(struct msi_desc *entry, struct msi_msg *msg)
 		if (WARN_ON_ONCE(entry->pci.msi_attrib.is_virtual))
 			return;
 
-#ifdef CONFIG_HISI_VIRTCCA_HOST
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 		if (is_virtcca_cvm_enable() && dev != NULL && is_cc_dev(pci_dev_id(dev)))
 			return virtcca_pci_read_msi_msg(dev, msg, base);
 #endif
@@ -229,8 +229,8 @@ static inline void pci_write_msg_msix(struct msi_desc *desc, struct msi_msg *msg
 	if (unmasked)
 		pci_msix_write_vector_ctrl(desc, ctrl | PCI_MSIX_ENTRY_CTRL_MASKBIT);
 
-#ifdef CONFIG_HISI_VIRTCCA_HOST
-	if (virtcca_pci_write_msg_msi(desc, msg))
+#ifdef CONFIG_HISI_VIRTCCA_CODA
+	if (is_virtcca_cvm_enable() && virtcca_pci_write_msg_msi(desc, msg))
 		return;
 #endif
 	writel(msg->address_lo, base + PCI_MSIX_ENTRY_LOWER_ADDR);
@@ -651,7 +651,7 @@ void msix_prepare_msi_desc(struct pci_dev *dev, struct msi_desc *desc)
 	if (desc->pci.msi_attrib.can_mask) {
 		void __iomem *addr = pci_msix_desc_addr(desc);
 
-#ifdef CONFIG_HISI_VIRTCCA_HOST
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 		if (is_virtcca_cvm_enable() && is_cc_dev(pci_dev_id(dev)))
 			return virtcca_msix_prepare_msi_desc(dev, desc, addr);
 #endif
@@ -792,9 +792,9 @@ static int msix_capability_init(struct pci_dev *dev, struct msix_entry *entries,
 	 * which takes the MSI-X mask bits into account even
 	 * when MSI-X is disabled, which prevents MSI delivery.
 	 */
-#ifdef CONFIG_HISI_VIRTCCA_HOST
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 	if (is_virtcca_cvm_enable() && is_cc_dev(pci_dev_id(dev)))
-		return msix_mask_all_cc(dev, dev->msix_base, tsize, pci_dev_id(dev));
+		return virtcca_msix_mask_all_cc(dev, dev->msix_base, tsize, pci_dev_id(dev));
 #endif
 	msix_mask_all(dev->msix_base, tsize);
 	pci_msix_clear_and_set_ctrl(dev, PCI_MSIX_FLAGS_MASKALL, 0);

arch/arm64/configs/openeuler_defconfig
@@ -760,6 +760,7 @@ CONFIG_KVM_HISI_VIRT=y
 CONFIG_VIRTUALIZATION=y
 CONFIG_KVM=y
 CONFIG_HISI_VIRTCCA_HOST=y
+CONFIG_HISI_VIRTCCA_CODA=y
 # CONFIG_NVHE_EL2_DEBUG is not set
 CONFIG_KVM_ARM_MULTI_LPI_TRANSLATE_CACHE=y
 CONFIG_ARCH_VCPU_STAT=y

arch/arm64/include/asm/kvm_tmi.h
@@ -83,7 +83,6 @@ struct tmi_smmu_cfg_params {
 #define TMI_SMMU_CMD_QUEUE  1
 #define TMI_SMMU_EVT_QUEUE  2
 struct tmi_smmu_queue_params {
-	uint64_t ns_src;     /* non-secure queue source address */
 	uint64_t smmu_base_addr;       /* smmu base address */
 	uint64_t size;       /* queue size */
 	uint64_t smmu_id;    /* smmu id */

arch/arm64/include/asm/kvm_tmm.h
@@ -10,16 +10,19 @@
 /*
  * There is a conflict with the internal iova of CVM,
  * so it is necessary to offset the msi iova.
+ * According to qemu file(hw/arm/virt.c), 0x0a001000 - 0x0b000000
+ * iova is not being used, so it is used as the iova range for msi
+ * mapping.
  */
-#define CVM_MSI_ORIG_IOVA 0x8000000
-#define CVM_MSI_IOVA_OFFSET (-0x1000000)
+#define CVM_MSI_ORIG_IOVA	0x8000000
+#define CVM_MSI_MIN_IOVA	0x0a001000
+#define CVM_MSI_MAX_IOVA	0x0b000000
+#define CVM_MSI_IOVA_OFFSET	0x1000
 
 #define CVM_RW_8_BIT	0x8
 #define CVM_RW_16_BIT	0x10
 #define CVM_RW_32_BIT	0x20
 #define CVM_RW_64_BIT	0x40
-
-#define BUS_NUM_SHIFT	0x8
 
 enum virtcca_cvm_state {
 	CVM_STATE_NONE = 1,
@@ -102,11 +105,8 @@ void kvm_free_rd(struct kvm *kvm);
 int cvm_psci_complete(struct kvm_vcpu *calling, struct kvm_vcpu *target);
 
 void kvm_cvm_unmap_destroy_range(struct kvm *kvm);
-
 int kvm_cvm_map_range(struct kvm *kvm);
-int cvm_arm_smmu_domain_set_kvm(void *group);
-int kvm_cvm_map_unmap_ipa_range(struct kvm *kvm, phys_addr_t ipa_base, phys_addr_t pa,
-	unsigned long map_size, uint32_t is_map);
+int virtcca_cvm_arm_smmu_domain_set_kvm(void *group);
 int cvm_map_unmap_ipa_range(struct kvm *kvm, phys_addr_t ipa_base, phys_addr_t pa,
 	unsigned long map_size, uint32_t is_map);
 int kvm_cvm_map_ipa_mmio(struct kvm *kvm, phys_addr_t ipa_base,
@@ -127,6 +127,11 @@ bool is_virtcca_iova_need_vfio_dma(struct kvm *kvm, uint64_t iova);
 #define CVM_TTT_LEVEL_SHIFT(l)	\
 	((CVM_PAGE_SHIFT - 3) * (4 - (l)) + 3)
 #define CVM_L2_BLOCK_SIZE	BIT(CVM_TTT_LEVEL_SHIFT(2))
+
+#define TMM_GRANULE_SIZE2		12
+#define TMM_TTT_WIDTH			19
+#define TMM_GRANULE_SIZE		(1UL << TMM_GRANULE_SIZE2)
+#define tmm_granule_size(level)	(TMM_GRANULE_SIZE << ((3 - level)) * TMM_TTT_WIDTH)
 
 static inline unsigned long cvm_ttt_level_mapsize(int level)
 {

arch/arm64/kernel/Makefile
@@ -82,7 +82,7 @@ obj-$(CONFIG_ARM64_ILP32)		+= vdso-ilp32/
 obj-$(CONFIG_UNWIND_PATCH_PAC_INTO_SCS)	+= patch-scs.o
 obj-$(CONFIG_IPI_AS_NMI)		+= ipi_nmi.o
 obj-$(CONFIG_HISI_VIRTCCA_GUEST)	+= virtcca_cvm_guest.o virtcca_cvm_tsi.o
-obj-$(CONFIG_HISI_VIRTCCA_HOST)		+= virtcca_cvm_host.o virtcca_coda.o
+obj-$(CONFIG_HISI_VIRTCCA_HOST)		+= virtcca_cvm_host.o
 CFLAGS_patch-scs.o			+= -mbranch-protection=none
 
 # Force dependency (vdso*-wrap.S includes vdso.so through incbin)

arch/arm64/kvm/mmu.c
@@ -19,7 +19,7 @@
 #include <asm/kvm_asm.h>
 #include <asm/kvm_emulate.h>
 #include <asm/virt.h>
-#ifdef CONFIG_HISI_VIRTCCA_HOST
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 #include <asm/kvm_tmi.h>
 #endif
 #include "trace.h"
@@ -873,7 +873,7 @@ int kvm_init_stage2_mmu(struct kvm *kvm, struct kvm_s2_mmu *mmu, unsigned long t
 	u64 mmfr0, mmfr1;
 	u32 phys_shift;
 
-#ifdef CONFIG_HISI_VIRTCCA_HOST
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 	if ((type & ~KVM_VM_TYPE_ARM_IPA_SIZE_MASK) && (!kvm_is_virtcca_cvm(kvm)))
 #else
 	if (type & ~KVM_VM_TYPE_ARM_IPA_SIZE_MASK)
@@ -1420,7 +1420,7 @@ static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 
 	fault_granule = 1UL << ARM64_HW_PGTABLE_LEVEL_SHIFT(fault_level);
 	write_fault = kvm_is_write_fault(vcpu);
-#ifdef CONFIG_HISI_VIRTCCA_HOST
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 	if (vcpu_is_tec(vcpu)) {
 		write_fault = true;
 		prot = KVM_PGTABLE_PROT_R | KVM_PGTABLE_PROT_W;
@@ -1608,7 +1608,7 @@ static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 					     KVM_PGTABLE_WALK_HANDLE_FAULT |
 					     KVM_PGTABLE_WALK_SHARED);
 
-#ifdef CONFIG_HISI_VIRTCCA_HOST
+#ifdef CONFIG_HISI_VIRTCCA_CODA
 	ret = kvm_cvm_map_ipa(kvm, fault_ipa, pfn, vma_pagesize, prot, ret);
 #endif
 

OERV-RVCI/rvck-olk

commit bc97b6378e5500bfc3cd50216e1f79cba9e59e6d

driver/coda: VirtCCA supports MSI device DA
community inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8419

------------------------

VirtCCA remapped the GICD address, so when writing MSI messages, we
modify the message address to make interrupt notification work.

Fixes: ad540a13eb82 ("virtcca feature: fix msi iova map")
Signed-off-by: yxk <yangxiangkai@huawei.com>

arch/arm64/include/asm/virtcca_coda.h
@@ -54,6 +54,7 @@ size_t virtcca_unmap_pages(void *ops, unsigned long iova,
 
 void virtcca_pci_read_msi_msg(struct pci_dev *dev, struct msi_msg *msg,
 	void __iomem *base);
+bool virtcca_pci_write_msg_msix(struct msi_desc *desc, struct msi_msg *msg);
 bool virtcca_pci_write_msg_msi(struct msi_desc *desc, struct msi_msg *msg);
 void virtcca_msix_prepare_msi_desc(struct pci_dev *dev,
 	struct msi_desc *desc, void __iomem *addr);

drivers/coda/coda_pci.c
@@ -28,29 +28,29 @@ void virtcca_pci_read_msi_msg(struct pci_dev *dev, struct msi_msg *msg,
 }
 
 /**
- * virtcca_pci_write_msi_msg - The secure device triggers an interrupt by writing
+ * virtcca_pci_write_msg_msix - The secure device triggers an interrupt by writing
  * to a specific memory address via TMM
  * @desc: MSI-X description
  * @msg: Msg information
  *
  **/
-bool virtcca_pci_write_msg_msi(struct msi_desc *desc, struct msi_msg *msg)
+bool virtcca_pci_write_msg_msix(struct msi_desc *desc, struct msi_msg *msg)
 {
-	if (!is_virtcca_cvm_enable())
+	struct pci_dev *pdev;
+	void __iomem *base;
+	u32 ctrl;
+	u64 pbase;
+	bool unmasked;
+	u64 addr;
+
+	if (!is_virtcca_cvm_enable() || !desc->dev || !dev_is_pci(desc->dev))
 		return false;
 
-	void __iomem *base = pci_msix_desc_addr(desc);
-	u32 ctrl = desc->pci.msix_ctrl;
-	bool unmasked = !(ctrl & PCI_MSIX_ENTRY_CTRL_MASKBIT);
-	u64 pbase = mmio_va_to_pa(base);
-	struct pci_dev *pdev = (desc->dev != NULL &&
-		dev_is_pci(desc->dev)) ? to_pci_dev(desc->dev) : NULL;
-
+	pdev = to_pci_dev(desc->dev);
 	if (!is_cc_dev(pci_dev_id(pdev)))
 		return false;
 
-	u64 addr = (u64)msg->address_lo | ((u64)msg->address_hi << 32);
-
+	addr = (u64)msg->address_lo | ((u64)msg->address_hi << 32);
 	/*
 	 * In the SR-IOV scenario, secure devices can be used on the host driver side. Therefore,
 	 * only the secure devices assigned to the CVM need to have their MSI addresses modified
@@ -64,6 +64,12 @@ bool virtcca_pci_write_msg_msi(struct msi_desc *desc, struct msi_msg *msg)
 		if (!addr)
 			return true;
 	}
+
+	base = pci_msix_desc_addr(desc);
+	ctrl = desc->pci.msix_ctrl;
+	unmasked = !(ctrl & PCI_MSIX_ENTRY_CTRL_MASKBIT);
+	pbase = mmio_va_to_pa(base);
+
 	tmi_mmio_write(pbase + PCI_MSIX_ENTRY_LOWER_ADDR,
 		lower_32_bits(addr), CVM_RW_32_BIT, pci_dev_id(pdev));
 	tmi_mmio_write(pbase + PCI_MSIX_ENTRY_UPPER_ADDR,
@@ -76,6 +82,49 @@ bool virtcca_pci_write_msg_msi(struct msi_desc *desc, struct msi_msg *msg)
 	tmi_mmio_read(pbase + PCI_MSIX_ENTRY_DATA,
 		CVM_RW_32_BIT, pci_dev_id(pdev));
 
+	return true;
+}
+
+bool virtcca_pci_write_msg_msi(struct msi_desc *desc, struct msi_msg *msg)
+{
+	int pos;
+	u16 msgctl;
+	struct pci_dev *dev;
+	u64 addr;
+
+	if (!is_virtcca_cvm_enable() || !desc->dev || !dev_is_pci(desc->dev))
+		return false;
+
+	dev = to_pci_dev(desc->dev);
+	if (!is_cc_dev(pci_dev_id(dev)))
+		return false;
+
+	pos = dev->msi_cap;
+	pci_read_config_word(dev, pos + PCI_MSI_FLAGS, &msgctl);
+	msgctl &= ~PCI_MSI_FLAGS_QSIZE;
+	msgctl |= desc->pci.msi_attrib.multiple << 4;
+	pci_write_config_word(dev, pos + PCI_MSI_FLAGS, msgctl);
+
+	addr = (u64)msg->address_lo | ((u64)msg->address_hi << 32);
+	if (addr && get_g_coda_dev_vm_type(pci_dev_id(dev)) == CC_DEV_CVM_TYPE) {
+		/* Get the offset of the its register of a specific device */
+		u64 offset = addr - CVM_MSI_ORIG_IOVA;
+
+		addr = get_g_cc_dev_msi_addr(pci_dev_id(dev));
+		addr += offset;
+		if (!addr)
+			return true;
+	}
+
+	pci_write_config_dword(dev, pos + PCI_MSI_ADDRESS_LO, lower_32_bits(addr));
+	if (desc->pci.msi_attrib.is_64) {
+		pci_write_config_dword(dev, pos + PCI_MSI_ADDRESS_HI, upper_32_bits(addr));
+		pci_write_config_word(dev, pos + PCI_MSI_DATA_64, msg->data);
+	} else {
+		pci_write_config_word(dev, pos + PCI_MSI_DATA_32, msg->data);
+	}
+	/* Ensure that the writes are visible in the device */
+	pci_read_config_word(dev, pos + PCI_MSI_FLAGS, &msgctl);
 	return true;
 }
 

drivers/pci/msi/msi.c
@@ -194,6 +194,11 @@ static inline void pci_write_msg_msi(struct pci_dev *dev, struct msi_desc *desc,
 	int pos = dev->msi_cap;
 	u16 msgctl;
 
+#ifdef CONFIG_HISI_VIRTCCA_CODA
+	if (virtcca_pci_write_msg_msi(desc, msg))
+		return;
+#endif
+
 	pci_read_config_word(dev, pos + PCI_MSI_FLAGS, &msgctl);
 	msgctl &= ~PCI_MSI_FLAGS_QSIZE;
 	msgctl |= desc->pci.msi_attrib.multiple << 4;
@@ -230,7 +235,7 @@ static inline void pci_write_msg_msix(struct msi_desc *desc, struct msi_msg *msg
 		pci_msix_write_vector_ctrl(desc, ctrl | PCI_MSIX_ENTRY_CTRL_MASKBIT);
 
 #ifdef CONFIG_HISI_VIRTCCA_CODA
-	if (is_virtcca_cvm_enable() && virtcca_pci_write_msg_msi(desc, msg))
+	if (virtcca_pci_write_msg_msix(desc, msg))
 		return;
 #endif
 	writel(msg->address_lo, base + PCI_MSIX_ENTRY_LOWER_ADDR);