openeuler-mirror/kernel

commit 70769fe636ef30b9740a194b160af25b11c4c974

sched/fair: Hoist idle_stamp up from idle_balance
maillist inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8PIYZ
CVE: NA

Reference: https://lore.kernel.org/lkml/1541767840-93588-6-git-send-email-steven.sistare@oracle.com/

---------------------------

Move the update of idle_stamp from idle_balance to the call site in
pick_next_task_fair, to prepare for a future patch that adds work to
pick_next_task_fair which must be included in the idle_stamp interval.
No functional change.

Signed-off-by: Steve Sistare <steven.sistare@oracle.com>
Signed-off-by: Cheng Yu <serein.chengyu@huawei.com>

kernel/sched/fair.c
@@ -5081,6 +5081,16 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 	rq->misfit_task_load = max_t(unsigned long, task_h_load(p), 1);
 }
 
+static inline void rq_idle_stamp_update(struct rq *rq)
+{
+	rq->idle_stamp = rq_clock(rq);
+}
+
+static inline void rq_idle_stamp_clear(struct rq *rq)
+{
+	rq->idle_stamp = 0;
+}
+
 static void overload_clear(struct rq *rq)
 {
 	struct sparsemask *overload_cpus;
@@ -5132,6 +5142,8 @@ static inline int newidle_balance(struct rq *rq, struct rq_flags *rf)
 	return 0;
 }
 
+static inline void rq_idle_stamp_update(struct rq *rq) {}
+static inline void rq_idle_stamp_clear(struct rq *rq) {}
 static inline void overload_clear(struct rq *rq) {}
 static inline void overload_set(struct rq *rq) {}
 
@@ -8948,7 +8960,17 @@ idle:
 	if (!rf)
 		return NULL;
 
+	/*
+	 * We must set idle_stamp _before_ calling idle_balance(), such that we
+	 * measure the duration of idle_balance() as idle time.
+	 */
+	rq_idle_stamp_update(rq);
+
 	new_tasks = newidle_balance(rq, rf);
+
+	if (new_tasks)
+		rq_idle_stamp_clear(rq);
+
 
 	/*
 	 * Because newidle_balance() releases (and re-acquires) rq->lock, it is
@@ -12771,12 +12793,6 @@ static int newidle_balance(struct rq *this_rq, struct rq_flags *rf)
 		return 0;
 
 	/*
-	 * We must set idle_stamp _before_ calling idle_balance(), such that we
-	 * measure the duration of idle_balance() as idle time.
-	 */
-	this_rq->idle_stamp = rq_clock(this_rq);
-
-	/*
 	 * Do not pull tasks towards !active CPUs...
 	 */
 	if (!cpu_active(this_cpu))
@@ -12865,9 +12881,7 @@ out:
 	if (time_after(this_rq->next_balance, next_balance))
 		this_rq->next_balance = next_balance;
 
-	if (pulled_task)
-		this_rq->idle_stamp = 0;
-	else
+	if (!pulled_task)
 		nohz_newidle_balance(this_rq);
 
 	rq_repin_lock(this_rq, rf);

OERV-RVCI/rvck-olk

commit 784489294d1012c03014af48d0e62f892d90f2be

sched/fair: Track idle balance interval with idle_stamp in balance_fair
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8425

--------------------------------

Fix commit 70769fe636ef ("sched/fair: Hoist idle_stamp up from
idle_balance")forgot to add the complete idle_stamp start/end
tracking interval around sched_balance_newidle() in balance_fair.

Fixes: 70769fe636ef ("sched/fair: Hoist idle_stamp up from idle_balance")
Signed-off-by: Chen Jinghuang <chenjinghuang2@huawei.com>

kernel/sched/fair.c
@@ -9467,10 +9467,19 @@ static void task_dead_fair(struct task_struct *p)
 static int
 balance_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 {
+	int new_tasks;
+
 	if (rq->nr_running)
 		return 1;
 
-	return newidle_balance(rq, rf) != 0;
+	rq_idle_stamp_update(rq);
+
+	new_tasks = newidle_balance(rq, rf);
+
+	if (new_tasks)
+		rq_idle_stamp_clear(rq);
+
+	return new_tasks != 0;
 }
 #endif /* CONFIG_SMP */
 