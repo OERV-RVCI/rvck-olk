OERV-RVCI/rvck-olk

commit 9e891675aab8c568e64b1bb102893b61a0a56733

ext4: don't write back data before punch hole in nojournal mode
stable inclusion
from stable-v6.6.93
commit 73e7c65b212746749d6094fa52c8f4e389dd9884
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8365

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=73e7c65b212746749d6094fa52c8f4e389dd9884

--------------------------------

[ Upstream commit 43d0105e2c7523cc6b14cad65e2044e829c0a07a ]

There is no need to write back all data before punching a hole in
non-journaled mode since it will be dropped soon after removing space.
Therefore, the call to filemap_write_and_wait_range() can be eliminated.
Besides, similar to ext4_zero_range(), we must address the case of
partially punched folios when block size < page size. It is essential to
remove writable userspace mappings to ensure that the folio can be
faulted again during subsequent mmap write access.

In journaled mode, we need to write dirty pages out before discarding
page cache in case of crash before committing the freeing data
transaction, which could expose old, stale data, even if synchronization
has been performed.

Signed-off-by: Zhang Yi <yi.zhang@huawei.com>
Reviewed-by: Jan Kara <jack@suse.cz>
Reviewed-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
Link: https://patch.msgid.link/20241220011637.1157197-4-yi.zhang@huaweicloud.com
Signed-off-by: Theodore Ts'o <tytso@mit.edu>
Signed-off-by: Sasha Levin <sashal@kernel.org>
(cherry picked from commit 73e7c65b212746749d6094fa52c8f4e389dd9884)
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

fs/ext4/inode.c
@@ -4572,17 +4572,6 @@ int ext4_punch_hole(struct file *file, loff_t offset, loff_t length)
 
 	trace_ext4_punch_hole(inode, offset, length, 0);
 
-	/*
-	 * Write out all dirty pages to avoid race conditions
-	 * Then release them.
-	 */
-	if (mapping_tagged(mapping, PAGECACHE_TAG_DIRTY)) {
-		ret = filemap_write_and_wait_range(mapping, offset,
-						   offset + length - 1);
-		if (ret)
-			return ret;
-	}
-
 	inode_lock(inode);
 
 	/* No need to punch hole beyond i_size */
@@ -4644,8 +4633,11 @@ int ext4_punch_hole(struct file *file, loff_t offset, loff_t length)
 		ret = ext4_update_disksize_before_punch(inode, offset, length);
 		if (ret)
 			goto out_dio;
-		truncate_pagecache_range(inode, first_block_offset,
-					 last_block_offset);
+
+		ret = ext4_truncate_page_cache_block_range(inode,
+				first_block_offset, last_block_offset + 1);
+		if (ret)
+			goto out_dio;
 	}
 
 	if (ext4_test_inode_flag(inode, EXT4_INODE_EXTENTS))

commit d3cdf06df33c57036b7970203b672732d8028f39

ext4: fix stale data issue on the iomap path
Offering: HULK
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8430

--------------------------------

There is a stale data issue caused by merging 9e891675aab8. After
the patch was merged, data writeback before punch hole cancellation
was canceled. This led to some regions of unwritten extents not
being zeroed, ultimately exposing stale data.

Fixes: 9e891675aab8 ("ext4: don't write back data before punch hole in nojournal mode")
Signed-off-by: Yongjian Sun <sunyongjian1@huawei.com>

fs/ext4/inode.c
@@ -4696,12 +4696,11 @@ int ext4_punch_hole(struct file *file, loff_t offset, loff_t length)
 		ret = ext4_update_disksize_before_punch(inode, offset, length);
 		if (ret)
 			goto out_dio;
-
-		ret = ext4_truncate_page_cache_block_range(inode,
-				first_block_offset, last_block_offset + 1);
-		if (ret)
-			goto out_dio;
 	}
+
+	ret = ext4_truncate_page_cache_block_range(inode, offset, offset + length);
+	if (ret)
+		goto out_dio;
 
 	if (ext4_test_inode_flag(inode, EXT4_INODE_EXTENTS))
 		credits = ext4_writepage_trans_blocks(inode);