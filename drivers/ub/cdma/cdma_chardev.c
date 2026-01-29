OERV-RVCI/rvck-olk

commit 35203448b9d150f435ec8bfa8072b5dc748127a0

ub: cdma: support reporting asynchronous events
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID459U
CVE: NA

-----------------------------------------------------------

This patch implements the handling and reporting of
asynchronous events in the CDMA driver. The implementation
includes writing the corresponding asynchronous events to
the device's asynchronous event queue when exceptions occur
in jfs or jfc.

Signed-off-by: Zhipeng Lu <luzhipeng8@h-partners.com>
Signed-off-by: Jingjing Ku <kujingjing@huawei.com>

drivers/ub/cdma/cdma_context.h
@@ -19,6 +19,7 @@ struct cdma_context {
 	spinlock_t lock;
 	int handle;
 	u32 tid;
+	void *jfae;
 	bool is_kernel;
 	atomic_t ref_cnt;
 	struct list_head queue_list;

drivers/ub/cdma/cdma_event.c
@@ -1,0 +1,431 @@
+// SPDX-License-Identifier: GPL-2.0+
+/* Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved. */
+
+#define pr_fmt(fmt) "CDMA: " fmt
+#define dev_fmt pr_fmt
+
+#include <linux/file.h>
+#include <linux/anon_inodes.h>
+#include <linux/rcupdate.h>
+#include <linux/ioctl.h>
+#include <linux/wait.h>
+#include <linux/poll.h>
+#include <uapi/ub/cdma/cdma_abi.h>
+#include "cdma_uobj.h"
+#include "cdma_event.h"
+
+static __poll_t cdma_jfe_poll(struct cdma_jfe *jfe, struct file *filp,
+			      struct poll_table_struct *wait)
+{
+	__poll_t flag = 0;
+
+	poll_wait(filp, &jfe->poll_wait, wait);
+
+	spin_lock_irq(&jfe->lock);
+	if (!list_empty(&jfe->event_list))
+		flag = EPOLLIN | EPOLLRDNORM;
+
+	spin_unlock_irq(&jfe->lock);
+
+	return flag;
+}
+
+static u32 cdma_read_jfe_event(struct cdma_jfe *jfe, u32 max_event_cnt,
+			       struct list_head *event_list)
+{
+	struct cdma_jfe_event *event;
+	struct list_head *next;
+	struct list_head *p;
+	u32 cnt = 0;
+
+	if (!max_event_cnt)
+		return 0;
+
+	spin_lock_irq(&jfe->lock);
+
+	list_for_each_safe(p, next, &jfe->event_list) {
+		event = list_entry(p, struct cdma_jfe_event, node);
+		if (event->counter) {
+			++(*event->counter);
+			list_del(&event->obj_node);
+		}
+		list_del(p);
+		if (jfe->event_list_count > 0)
+			jfe->event_list_count--;
+		list_add_tail(p, event_list);
+		cnt++;
+		if (cnt == max_event_cnt)
+			break;
+	}
+	spin_unlock_irq(&jfe->lock);
+
+	return cnt;
+}
+
+static int cdma_wait_event(struct cdma_jfe *jfe, bool nonblock,
+			   u32 max_event_cnt, u32 *event_cnt,
+			   struct list_head *event_list)
+{
+	int ret;
+
+	*event_cnt = 0;
+	spin_lock_irq(&jfe->lock);
+	while (list_empty(&jfe->event_list)) {
+		spin_unlock_irq(&jfe->lock);
+		if (nonblock)
+			return -EAGAIN;
+
+		ret = wait_event_interruptible(jfe->poll_wait,
+					       !list_empty(&jfe->event_list));
+		if (ret)
+			return ret;
+
+		spin_lock_irq(&jfe->lock);
+		if (list_empty(&jfe->event_list)) {
+			spin_unlock_irq(&jfe->lock);
+			return -EIO;
+		}
+	}
+	spin_unlock_irq(&jfe->lock);
+	*event_cnt = cdma_read_jfe_event(jfe, max_event_cnt, event_list);
+
+	return 0;
+}
+
+static void cdma_write_event(struct cdma_jfe *jfe, u64 event_data,
+			     u32 event_type, struct list_head *obj_event_list,
+			     u32 *counter)
+{
+	struct cdma_jfe_event *event;
+	unsigned long flags;
+
+	event = kzalloc(sizeof(*event), GFP_ATOMIC);
+	if (event == NULL)
+		return;
+
+	spin_lock_irqsave(&jfe->lock, flags);
+	INIT_LIST_HEAD(&event->obj_node);
+	event->event_type = event_type;
+	event->event_data = event_data;
+	event->counter = counter;
+	list_add_tail(&event->node, &jfe->event_list);
+	if (obj_event_list)
+		list_add_tail(&event->obj_node, obj_event_list);
+	if (jfe->async_queue)
+		kill_fasync(&jfe->async_queue, SIGIO, POLL_IN);
+	jfe->event_list_count++;
+	spin_unlock_irqrestore(&jfe->lock, flags);
+	wake_up_interruptible(&jfe->poll_wait);
+}
+
+static void cdma_init_jfe(struct cdma_jfe *jfe)
+{
+	spin_lock_init(&jfe->lock);
+	INIT_LIST_HEAD(&jfe->event_list);
+	init_waitqueue_head(&jfe->poll_wait);
+	jfe->async_queue = NULL;
+	jfe->event_list_count = 0;
+}
+
+static void cdma_uninit_jfe(struct cdma_jfe *jfe)
+{
+	struct cdma_jfe_event *event;
+	struct list_head *p, *next;
+
+	spin_lock_irq(&jfe->lock);
+	list_for_each_safe(p, next, &jfe->event_list) {
+		event = list_entry(p, struct cdma_jfe_event, node);
+		if (event->counter)
+			list_del(&event->obj_node);
+		kfree(event);
+	}
+	spin_unlock_irq(&jfe->lock);
+}
+
+static void cdma_write_async_event(struct cdma_context *ctx, u64 event_data,
+				   u32 type, struct list_head *obj_event_list,
+				   u32 *counter)
+{
+	struct cdma_jfae *jfae;
+
+	rcu_read_lock();
+	jfae = (struct cdma_jfae *)(rcu_dereference(ctx->jfae));
+	if (!jfae)
+		goto err_free_rcu;
+
+	if (jfae->jfe.event_list_count >= MAX_EVENT_LIST_SIZE) {
+		pr_debug("event list overflow, and this write will be discarded.\n");
+		goto err_free_rcu;
+	}
+
+	cdma_write_event(&jfae->jfe, event_data, type, obj_event_list, counter);
+
+err_free_rcu:
+	rcu_read_unlock();
+}
+
+void cdma_jfs_async_event_cb(struct cdma_event *event, struct cdma_context *ctx)
+{
+	struct cdma_jfs_event *jfs_event;
+
+	jfs_event = &event->element.jfs->jfs_event;
+	cdma_write_async_event(ctx, event->element.jfs->cfg.queue_id,
+			       event->event_type, &jfs_event->async_event_list,
+			       &jfs_event->async_events_reported);
+}
+
+void cdma_jfc_async_event_cb(struct cdma_event *event, struct cdma_context *ctx)
+{
+	struct cdma_jfc_event *jfc_event;
+
+	jfc_event = &event->element.jfc->jfc_event;
+	cdma_write_async_event(ctx, event->element.jfc->jfc_cfg.queue_id,
+			       event->event_type, &jfc_event->async_event_list,
+			       &jfc_event->async_events_reported);
+}
+
+static inline void cdma_set_async_event(struct cdma_cmd_async_event *async_event,
+					const struct cdma_jfe_event *event)
+{
+	async_event->event_data = event->event_data;
+	async_event->event_type = event->event_type;
+}
+
+static int cdma_get_async_event(struct cdma_jfae *jfae, struct file *filp,
+				unsigned long arg)
+{
+	struct cdma_cmd_async_event async_event = { 0 };
+	struct cdma_jfe_event *event;
+	struct list_head event_list;
+	u32 event_cnt;
+	int ret;
+
+	if (!arg) {
+		pr_err("invalid jfae arg.\n");
+		return -EINVAL;
+	}
+
+	INIT_LIST_HEAD(&event_list);
+	ret = cdma_wait_event(&jfae->jfe, filp->f_flags & O_NONBLOCK, 1,
+			      &event_cnt, &event_list);
+	if (ret < 0) {
+		pr_err("wait event failed, ret = %d.\n", ret);
+		return ret;
+	}
+	event = list_first_entry(&event_list, struct cdma_jfe_event, node);
+	if (event == NULL)
+		return -EIO;
+
+	cdma_set_async_event(&async_event, event);
+	list_del(&event->node);
+	kfree(event);
+
+	if (event_cnt > 0) {
+		ret = (int)copy_to_user((void *)arg, &async_event,
+					sizeof(async_event));
+		if (ret) {
+			pr_err("dev copy to user failed, ret = %d\n", ret);
+			return -EFAULT;
+		}
+	}
+
+	return 0;
+}
+
+static __poll_t cdma_jfae_poll(struct file *filp, struct poll_table_struct *wait)
+{
+	struct cdma_jfae *jfae = (struct cdma_jfae *)filp->private_data;
+
+	if (!jfae || !jfae->cfile || !jfae->cfile->cdev)
+		return POLLERR;
+
+	return cdma_jfe_poll(&jfae->jfe, filp, wait);
+}
+
+static long cdma_jfae_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	struct cdma_jfae *jfae = (struct cdma_jfae *)filp->private_data;
+	unsigned int nr;
+	int ret;
+
+	if (!jfae)
+		return -EINVAL;
+
+	nr = (unsigned int)_IOC_NR(cmd);
+
+	switch (nr) {
+	case JFAE_CMD_GET_ASYNC_EVENT:
+		ret = cdma_get_async_event(jfae, filp, arg);
+		break;
+	default:
+		dev_err(jfae->cfile->cdev->dev, "nr = %u.\n", nr);
+		ret = -ENOIOCTLCMD;
+		break;
+	}
+
+	return (long)ret;
+}
+
+static int cdma_delete_jfae(struct inode *inode, struct file *filp)
+{
+	struct cdma_file *cfile;
+	struct cdma_jfae *jfae;
+
+	if (!filp || !filp->private_data)
+		return 0;
+
+	jfae = (struct cdma_jfae *)filp->private_data;
+	cfile = jfae->cfile;
+	if (!cfile)
+		return 0;
+
+	if (!mutex_trylock(&cfile->ctx_mutex))
+		return -ENOLCK;
+	jfae->ctx->jfae = NULL;
+	cdma_uninit_jfe(&jfae->jfe);
+	kfree(jfae);
+	filp->private_data = NULL;
+	mutex_unlock(&cfile->ctx_mutex);
+	cdma_close_uobj_fd(cfile);
+
+	pr_debug("jfae is release.\n");
+	return 0;
+}
+
+static int cdma_jfae_fasync(int fd, struct file *filp, int on)
+{
+	struct cdma_jfae *jfae = (struct cdma_jfae *)filp->private_data;
+	int ret;
+
+	if (!jfae)
+		return -EINVAL;
+
+	spin_lock_irq(&jfae->jfe.lock);
+	ret = fasync_helper(fd, filp, on, &jfae->jfe.async_queue);
+	spin_unlock_irq(&jfae->jfe.lock);
+
+	return ret;
+}
+
+const struct file_operations cdma_jfae_fops = {
+	.owner = THIS_MODULE,
+	.poll = cdma_jfae_poll,
+	.unlocked_ioctl = cdma_jfae_ioctl,
+	.release = cdma_delete_jfae,
+	.fasync = cdma_jfae_fasync,
+};
+
+struct cdma_jfae *cdma_alloc_jfae(struct cdma_file *cfile)
+{
+	struct cdma_jfae *jfae;
+	struct file *file;
+	int fd;
+
+	if (!cfile)
+		return NULL;
+
+	fd = get_unused_fd_flags(O_RDWR | O_CLOEXEC);
+	if (fd < 0)
+		return NULL;
+
+	jfae = kzalloc(sizeof(*jfae), GFP_KERNEL);
+	if (!jfae)
+		goto err_put_unused_fd;
+
+	file = anon_inode_getfile("[jfae]", &cdma_jfae_fops, jfae,
+				  O_RDWR | O_CLOEXEC);
+	if (IS_ERR(file))
+		goto err_free_jfae;
+
+	cdma_init_jfe(&jfae->jfe);
+	jfae->fd = fd;
+	jfae->file = file;
+	jfae->cfile = cfile;
+	fd_install(fd, file);
+
+	return jfae;
+
+err_free_jfae:
+	kfree(jfae);
+err_put_unused_fd:
+	put_unused_fd(fd);
+
+	return NULL;
+}
+
+void cdma_free_jfae(struct cdma_jfae *jfae)
+{
+	if (!jfae)
+		return;
+
+	fput(jfae->file);
+	put_unused_fd(jfae->fd);
+}
+
+int cdma_get_jfae(struct cdma_context *ctx)
+{
+	struct cdma_jfae *jfae;
+	struct file *file;
+
+	if (!ctx)
+		return -EINVAL;
+
+	jfae = (struct cdma_jfae *)ctx->jfae;
+	if (!jfae)
+		return -EINVAL;
+
+	file = fget(jfae->fd);
+	if (!file)
+		return -ENOENT;
+
+	if (file->private_data != jfae) {
+		fput(file);
+		return -EBADF;
+	}
+
+	return 0;
+}
+
+void cdma_init_jfc_event(struct cdma_jfc_event *event, struct cdma_base_jfc *jfc)
+{
+	event->async_events_reported = 0;
+	INIT_LIST_HEAD(&event->async_event_list);
+	event->jfc = jfc;
+}
+
+void cdma_release_async_event(struct cdma_context *ctx, struct list_head *event_list)
+{
+	struct cdma_jfe_event *event, *tmp;
+	struct cdma_jfae *jfae;
+	struct cdma_jfe *jfe;
+
+	if (!ctx || !ctx->jfae)
+		return;
+
+	jfae = (struct cdma_jfae *)ctx->jfae;
+	jfe = &jfae->jfe;
+	spin_lock_irq(&jfe->lock);
+	list_for_each_entry_safe(event, tmp, event_list, obj_node) {
+		list_del(&event->node);
+		kfree(event);
+	}
+	spin_unlock_irq(&jfe->lock);
+	fput(jfae->file);
+}
+
+void cdma_put_jfae(struct cdma_context *ctx)
+{
+	struct cdma_jfae *jfae;
+
+	if (!ctx)
+		return;
+
+	jfae = (struct cdma_jfae *)ctx->jfae;
+	if (!jfae)
+		return;
+
+	if (!jfae->file)
+		return;
+
+	fput(jfae->file);
+}

drivers/ub/cdma/cdma_ioctl.c
@@ -12,6 +12,7 @@
 #include "cdma_tp.h"
 #include "cdma_jfs.h"
 #include "cdma_queue.h"
+#include "cdma_event.h"
 #include "cdma_jfc.h"
 #include "cdma_uobj.h"
 #include "cdma_ioctl.h"
@@ -65,6 +66,7 @@ static int cdma_create_ucontext(struct cdma_ioctl_hdr *hdr,
 	struct cdma_create_context_args args = { 0 };
 	struct cdma_dev *cdev = cfile->cdev;
 	struct cdma_context *ctx;
+	struct cdma_jfae *jfae;
 	int ret;
 
 	if (cfile->uctx) {
@@ -87,22 +89,34 @@ static int cdma_create_ucontext(struct cdma_ioctl_hdr *hdr,
 	if (IS_ERR(ctx))
 		return PTR_ERR(ctx);
 
+	ctx->jfae = cdma_alloc_jfae(cfile);
+	if (!ctx->jfae) {
+		dev_err(cdev->dev, "create jfae failed.\n");
+		ret = -EFAULT;
+		goto free_context;
+	}
+
+	jfae = (struct cdma_jfae *)ctx->jfae;
+	jfae->ctx = ctx;
 	args.out.cqe_size = cdev->caps.cqe_size;
 	args.out.dwqe_enable =
 		!!(cdev->caps.feature & CDMA_CAP_FEATURE_DIRECT_WQE);
+	args.out.async_fd = jfae->fd;
 	cfile->uctx = ctx;
 
 	ret = (int)copy_to_user((void *)hdr->args_addr, &args,
 				(u32)sizeof(args));
 	if (ret) {
 		dev_err(cdev->dev, "copy ctx to user failed, ret = %d.\n", ret);
-		goto free_context;
+		goto free_jfae;
 	}
 
 	return ret;
 
-free_context:
+free_jfae:
 	cfile->uctx = NULL;
+	cdma_free_jfae((struct cdma_jfae *)ctx->jfae);
+free_context:
 	cdma_free_context(cdev, ctx);
 
 	return ret;
@@ -270,6 +284,7 @@ static int cdma_cmd_create_jfs(struct cdma_ioctl_hdr *hdr,
 {
 	struct cdma_cmd_create_jfs_args arg = { 0 };
 	struct cdma_dev *cdev = cfile->cdev;
+	struct cdma_jfs_event *jfs_event;
 	struct cdma_jfs_cfg cfg = { 0 };
 	struct cdma_udata udata = { 0 };
 	struct cdma_base_jfs *jfs;
@@ -316,6 +331,9 @@ static int cdma_cmd_create_jfs(struct cdma_ioctl_hdr *hdr,
 	}
 
 	uobj->object = jfs;
+	jfs_event = &jfs->jfs_event;
+	jfs_event->async_events_reported = 0;
+	INIT_LIST_HEAD(&jfs_event->async_event_list);
 
 	arg.out.id = jfs->id;
 	arg.out.handle = uobj->id;
@@ -497,6 +515,7 @@ static int cdma_cmd_create_jfc(struct cdma_ioctl_hdr *hdr,
 {
 	struct cdma_cmd_create_jfc_args arg = { 0 };
 	struct cdma_dev *cdev = cfile->cdev;
+	struct cdma_jfc_event *jfc_event;
 	struct cdma_jfc_cfg cfg = { 0 };
 	struct cdma_udata udata = { 0 };
 	struct cdma_base_jfc *jfc;
@@ -541,7 +560,9 @@ static int cdma_cmd_create_jfc(struct cdma_ioctl_hdr *hdr,
 		goto err_create_jfc;
 	}
 
+	jfc_event = &jfc->jfc_event;
 	uobj->object = jfc;
+	cdma_init_jfc_event(jfc_event, jfc);
 
 	arg.out.id = jfc->id;
 	arg.out.depth = jfc->jfc_cfg.depth;

drivers/ub/cdma/cdma_jfc.c
@@ -8,6 +8,7 @@
 #include "cdma_context.h"
 #include "cdma_mbox.h"
 #include "cdma_common.h"
+#include "cdma_event.h"
 #include "cdma_db.h"
 #include "cdma_jfc.h"
 
@@ -271,6 +272,12 @@ static int cdma_destroy_and_flush_jfc(struct cdma_dev *cdev, u32 jfcn)
 	return -ETIMEDOUT;
 }
 
+static void cdma_release_jfc_event(struct cdma_jfc *jfc)
+{
+	cdma_release_async_event(jfc->base.ctx,
+		&jfc->base.jfc_event.async_event_list);
+}
+
 static int cdma_post_create_jfc_mbox(struct cdma_dev *cdev, struct cdma_jfc *jfc)
 {
 	struct ubase_mbx_attr attr = { 0 };
@@ -323,10 +330,19 @@ struct cdma_base_jfc *cdma_create_jfc(struct cdma_dev *cdev,
 	if (ret)
 		goto err_get_jfc_buf;
 
+	if (udata) {
+		ret = cdma_get_jfae(jfc->base.ctx);
+		if (ret)
+			goto err_get_jfae;
+	}
+
 	ret = cdma_post_create_jfc_mbox(cdev, jfc);
 	if (ret)
 		goto err_alloc_cqc;
 
+	refcount_set(&jfc->event_refcount, 1);
+	init_completion(&jfc->event_comp);
+	jfc->base.jfae_handler = cdma_jfc_async_event_cb;
 	jfc->base.dev = cdev;
 
 	dev_dbg(cdev->dev, "create jfc id = %u, queue id = %u.\n",
@@ -335,6 +351,9 @@ struct cdma_base_jfc *cdma_create_jfc(struct cdma_dev *cdev,
 	return &jfc->base;
 
 err_alloc_cqc:
+	if (udata)
+		cdma_put_jfae(jfc->base.ctx);
+err_get_jfae:
 	cdma_free_jfc_buf(cdev, jfc);
 err_get_jfc_buf:
 	cdma_jfc_id_free(cdev, jfc->jfcn);
@@ -348,6 +367,7 @@ err_get_cmd:
 int cdma_delete_jfc(struct cdma_dev *cdev, u32 jfcn,
 		    struct cdma_cmd_delete_jfc_args *arg)
 {
+	struct cdma_jfc_event *jfc_event;
 	struct cdma_jfc *jfc;
 	int ret;
 
@@ -373,11 +393,20 @@ int cdma_delete_jfc(struct cdma_dev *cdev, u32 jfcn,
 	if (ret)
 		dev_err(cdev->dev, "jfc delete failed, jfcn = %u.\n", jfcn);
 
+	if (refcount_dec_and_test(&jfc->event_refcount))
+		complete(&jfc->event_comp);
+	wait_for_completion(&jfc->event_comp);
+
 	cdma_free_jfc_buf(cdev, jfc);
 	cdma_jfc_id_free(cdev, jfc->jfcn);
+	if (arg) {
+		jfc_event = &jfc->base.jfc_event;
+		arg->out.async_events_reported = jfc_event->async_events_reported;
+	}
 
 	pr_debug("Leave %s, jfcn: %u.\n", __func__, jfc->jfcn);
 
+	cdma_release_jfc_event(jfc);
 	kfree(jfc);
 
 	return 0;

drivers/ub/cdma/cdma_jfs.c
@@ -12,6 +12,7 @@
 #include "cdma_cmd.h"
 #include "cdma_common.h"
 #include "cdma_mbox.h"
+#include "cdma_event.h"
 #include "cdma_context.h"
 #include "cdma_jfs.h"
 
@@ -296,13 +297,21 @@ struct cdma_base_jfs *cdma_create_jfs(struct cdma_dev *cdev,
 	if (ret)
 		goto err_get_jfs_buf;
 
+	if (udata) {
+		ret = cdma_get_jfae(jfs->base_jfs.ctx);
+		if (ret)
+			goto err_get_jfae;
+	}
+
 	ret = cdma_create_hw_jfs_ctx(cdev, jfs, cfg);
 	if (ret)
 		goto err_create_hw_jfsc;
 
 	cdma_set_query_flush_time(&jfs->sq, cfg->err_timeout);
-
+	refcount_set(&jfs->ae_ref_cnt, 1);
+	init_completion(&jfs->ae_comp);
 	jfs->sq.state = CDMA_JETTY_READY;
+	jfs->base_jfs.jfae_handler = cdma_jfs_async_event_cb;
 	jfs->base_jfs.dev = cdev;
 
 	dev_dbg(cdev->dev,
@@ -312,6 +321,9 @@ struct cdma_base_jfs *cdma_create_jfs(struct cdma_dev *cdev,
 	return &jfs->base_jfs;
 
 err_create_hw_jfsc:
+	if (udata)
+		cdma_put_jfae(jfs->base_jfs.ctx);
+err_get_jfae:
 	cdma_free_sq_buf(cdev, &jfs->sq);
 err_get_jfs_buf:
 	cdma_free_jfs_id(cdev, jfs->sq.id);
@@ -499,6 +511,12 @@ static int cdma_modify_and_destroy_jfs(struct cdma_dev *cdev,
 	return ret;
 }
 
+static inline void cdma_release_jfs_event(struct cdma_jfs *jfs)
+{
+	cdma_release_async_event(jfs->base_jfs.ctx,
+		&jfs->base_jfs.jfs_event.async_event_list);
+}
+
 int cdma_delete_jfs(struct cdma_dev *cdev, u32 jfs_id)
 {
 	struct cdma_jfs *jfs;
@@ -524,11 +542,17 @@ int cdma_delete_jfs(struct cdma_dev *cdev, u32 jfs_id)
 	if (ret)
 		dev_err(cdev->dev, "jfs delete failed, id = %u.\n", jfs->id);
 
+	if (refcount_dec_and_test(&jfs->ae_ref_cnt))
+		complete(&jfs->ae_comp);
+	wait_for_completion(&jfs->ae_comp);
+
 	cdma_free_sq_buf(cdev, &jfs->sq);
 
 	cdma_free_jfs_id(cdev, jfs_id);
 
 	pr_debug("Leave %s, jfsn: %u.\n", __func__, jfs_id);
+
+	cdma_release_jfs_event(jfs);
 
 	kfree(jfs);
 

drivers/ub/cdma/cdma_main.c
@@ -10,6 +10,7 @@
 #include "cdma_dev.h"
 #include "cdma_chardev.h"
 #include <ub/ubase/ubase_comm_dev.h>
+#include "cdma_eq.h"
 #include "cdma_cmd.h"
 
 /* Enabling jfc_arm_mode will cause jfc to report cqe; otherwise, it will not. */
@@ -24,9 +25,29 @@ MODULE_PARM_DESC(cqe_mode, "Set cqe reporting mode, default: 1 (0:BY_COUNT, 1:BY
 
 struct class *cdma_cdev_class;
 
+static int cdma_register_event(struct auxiliary_device *adev)
+{
+	int ret;
+
+	ret = cdma_reg_ae_event(adev);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+static inline void cdma_unregister_event(struct auxiliary_device *adev)
+{
+	cdma_unreg_ae_event(adev);
+}
+
 static int cdma_init_dev_info(struct auxiliary_device *auxdev, struct cdma_dev *cdev)
 {
 	int ret;
+
+	ret = cdma_register_event(auxdev);
+	if (ret)
+		return ret;
 
 	/* query eu failure does not affect driver loading, as eu can be updated. */
 	ret = cdma_ctrlq_query_eu(cdev);
@@ -77,6 +98,7 @@ static void cdma_uninit_dev(struct auxiliary_device *auxdev)
 		return;
 	}
 
+	cdma_unregister_event(auxdev);
 	cdma_destroy_chardev(cdev);
 	cdma_destroy_dev(cdev);
 }

drivers/ub/cdma/Makefile
@@ -2,6 +2,6 @@
 
 cdma-$(CONFIG_UB_CDMA) := cdma_main.o cdma_dev.o cdma_chardev.o cdma_cmd.o cdma_tid.o cdma_ioctl.o \
                           cdma_api.o cdma_context.o cdma_queue.o cdma_uobj.o cdma_jfc.o cdma_common.o \
-                          cdma_db.o cdma_mbox.o cdma_tp.o cdma_jfs.o
+                          cdma_db.o cdma_mbox.o cdma_tp.o cdma_jfs.o cdma_eq.o cdma_event.o
 
 obj-m += cdma.o

drivers/ub/cdma/cdma.h
@@ -8,7 +8,7 @@
 #include <linux/types.h>
 #include <linux/cdev.h>
 #include <ub/ubus/ubus.h>
-
+#include <ub/ubase/ubase_comm_eq.h>
 #include <ub/cdma/cdma_api.h>
 
 extern u32 jfc_arm_mode;
@@ -187,9 +187,26 @@ struct cdma_dev {
 	struct cdma_table ctp_table;
 	struct cdma_table jfs_table;
 	struct cdma_table jfc_table;
+	struct ubase_event_nb *ae_event_addr[UBASE_EVENT_TYPE_MAX];
 	struct mutex file_mutex;
 	struct list_head file_list;
 	struct page *arm_db_page;
 };
+
+struct cdma_jfs_event {
+	struct list_head async_event_list;
+	u32 async_events_reported;
+};
+
+struct cdma_jfc_event {
+	struct cdma_base_jfc *jfc;
+	struct list_head async_event_list;
+	u32 async_events_reported;
+};
+
+static inline struct cdma_dev *get_cdma_dev(struct auxiliary_device *adev)
+{
+	return (struct cdma_dev *)dev_get_drvdata(&adev->dev);
+}
 
 #endif /* _CDMA_H_ */

commit ae7797e10b1793dd9b84e48a63913eb6e9eefe53

ub: cdma: cdma uaf bugfix
driver inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8339
CVE: NA

-----------------------------------------------------------

cdma uaf bugfix

Fixes: 35203448b9d1 ("ub: cdma: support reporting asynchronous events")

Signed-off-by: Zhipeng Lu <luzhipeng8@h-partners.com>

drivers/ub/cdma/cdma_chardev.c
@@ -386,7 +386,7 @@ int cdma_create_chardev(struct cdma_dev *cdev)
 	}
 
 	chardev->dev = device_create(cdma_cdev_class, NULL, chardev->devno,
-				     NULL, chardev->name);
+				     NULL, "%s", chardev->name);
 	if (IS_ERR(chardev->dev)) {
 		ret = PTR_ERR(chardev->dev);
 		dev_err(cdev->dev, "create device failed, ret = %d\n", ret);

drivers/ub/cdma/cdma_context.c
@@ -133,6 +133,9 @@ void cdma_free_context(struct cdma_dev *cdev, struct cdma_context *ctx)
 	if (!cdev || !ctx)
 		return;
 
+	if (ctx->jfae)
+		ctx->jfae->ctx = NULL;
+
 	cdma_ctx_free_tid(cdev, ctx);
 	cdma_ctx_handle_free(cdev, ctx);
 	mutex_destroy(&ctx->pgdir_mutex);

drivers/ub/cdma/cdma_context.h
@@ -9,6 +9,7 @@
 #include <linux/mutex.h>
 #include <linux/xarray.h>
 #include <ub/cdma/cdma_api.h>
+#include "cdma_event.h"
 
 struct cdma_context {
 	struct dma_context base_ctx;
@@ -19,7 +20,7 @@ struct cdma_context {
 	spinlock_t lock;
 	int handle;
 	u32 tid;
-	void *jfae;
+	struct cdma_jfae *jfae;
 	bool is_kernel;
 	atomic_t ref_cnt;
 	struct list_head queue_list;

drivers/ub/cdma/cdma_debugfs.c
@@ -25,8 +25,6 @@ static void cdma_get_ctx_info(struct cdma_dev *cdev,
 			      enum cdma_dbg_ctx_type ctx_type,
 			      struct cdma_ctx_info *ctx_info)
 {
-	struct auxiliary_device *adev = cdev->adev;
-
 #define CDMA_DBG_CTX_SIZE_256 256
 #define UBASE_CTX_SIZE_128 128
 	switch (ctx_type) {
@@ -43,8 +41,6 @@ static void cdma_get_ctx_info(struct cdma_dev *cdev,
 		ctx_info->ctx_name = "sq_jfc";
 		break;
 	default:
-		dev_err(&adev->dev, "get ctx info failed, ctx_type = %d.\n",
-			ctx_type);
 		break;
 	}
 }
@@ -231,7 +227,7 @@ static int cdma_dbg_dump_ctx(struct seq_file *s, enum cdma_dbg_ctx_type ctx_type
 	return 0;
 }
 
-int cdma_dbg_dump_jfs_ctx(struct seq_file *s, void *data)
+static int cdma_dbg_dump_jfs_ctx(struct seq_file *s, void *data)
 {
 	if (!s || !s->private)
 		return -EINVAL;
@@ -239,7 +235,7 @@ int cdma_dbg_dump_jfs_ctx(struct seq_file *s, void *data)
 	return cdma_dbg_dump_ctx(s, CDMA_DBG_JFS_CTX);
 }
 
-int cdma_dbg_dump_sq_jfc_ctx(struct seq_file *s, void *data)
+static int cdma_dbg_dump_sq_jfc_ctx(struct seq_file *s, void *data)
 {
 	if (!s || !s->private)
 		return -EINVAL;

drivers/ub/cdma/cdma_event.c
@@ -467,7 +467,7 @@ static void cdma_write_async_event(struct cdma_context *ctx, u64 event_data,
 	struct cdma_jfae *jfae;
 
 	rcu_read_lock();
-	jfae = (struct cdma_jfae *)(rcu_dereference(ctx->jfae));
+	jfae = rcu_dereference(ctx->jfae);
 	if (!jfae)
 		goto err_free_rcu;
 
@@ -720,7 +720,7 @@ int cdma_get_jfae(struct cdma_context *ctx)
 	if (!ctx)
 		return -EINVAL;
 
-	jfae = (struct cdma_jfae *)ctx->jfae;
+	jfae = ctx->jfae;
 	if (!jfae)
 		return -EINVAL;
 
@@ -772,7 +772,7 @@ void cdma_release_async_event(struct cdma_context *ctx, struct list_head *event_
 	if (!ctx || !ctx->jfae)
 		return;
 
-	jfae = (struct cdma_jfae *)ctx->jfae;
+	jfae = ctx->jfae;
 	jfe = &jfae->jfe;
 	spin_lock_irq(&jfe->lock);
 	list_for_each_entry_safe(event, tmp, event_list, obj_node) {
@@ -790,7 +790,7 @@ void cdma_put_jfae(struct cdma_context *ctx)
 	if (!ctx)
 		return;
 
-	jfae = (struct cdma_jfae *)ctx->jfae;
+	jfae = ctx->jfae;
 	if (!jfae)
 		return;
 

drivers/ub/cdma/cdma_ioctl.c
@@ -97,7 +97,7 @@ static int cdma_create_ucontext(struct cdma_ioctl_hdr *hdr,
 		goto free_context;
 	}
 
-	jfae = (struct cdma_jfae *)ctx->jfae;
+	jfae = ctx->jfae;
 	jfae->ctx = ctx;
 	args.out.cqe_size = cdev->caps.cqe_size;
 	args.out.dwqe_enable =
@@ -116,7 +116,7 @@ static int cdma_create_ucontext(struct cdma_ioctl_hdr *hdr,
 
 free_jfae:
 	cfile->uctx = NULL;
-	cdma_free_jfae((struct cdma_jfae *)ctx->jfae);
+	cdma_free_jfae(ctx->jfae);
 free_context:
 	cdma_free_context(cdev, ctx);
 
@@ -127,7 +127,6 @@ static int cdma_delete_ucontext(struct cdma_ioctl_hdr *hdr,
 				struct cdma_file *cfile)
 {
 	struct cdma_dev *cdev = cfile->cdev;
-	struct cdma_jfae *jfae;
 
 	if (!cfile->uctx) {
 		dev_err(cdev->dev, "cdma context has not been created.\n");
@@ -140,10 +139,6 @@ static int cdma_delete_ucontext(struct cdma_ioctl_hdr *hdr,
 			cfile->uctx->handle);
 		return -EBUSY;
 	}
-
-	jfae = cfile->uctx->jfae;
-	if (jfae)
-		jfae->ctx = NULL;
 
 	cdma_free_context(cdev, cfile->uctx);
 	cfile->uctx = NULL;

drivers/ub/cdma/cdma_jfc.c
@@ -520,7 +520,7 @@ struct cdma_base_jfc *cdma_create_jfc(struct cdma_dev *cdev,
 	jfc->base.jfce_handler = cdma_jfc_comp_event_cb;
 	jfc->base.dev = cdev;
 
-	dev_dbg(cdev->dev, "create jfc id = %u, queue id = %u.\n",
+	dev_info(cdev->dev, "create jfc, id = %u, queue id = %u.\n",
 		jfc->jfcn, cfg->queue_id);
 
 	return &jfc->base;
@@ -580,7 +580,7 @@ int cdma_delete_jfc(struct cdma_dev *cdev, u32 jfcn,
 		arg->out.async_events_reported = jfc_event->async_events_reported;
 	}
 
-	pr_debug("Leave %s, jfcn: %u.\n", __func__, jfc->jfcn);
+	dev_info(cdev->dev, "delete jfc, id = %u.\n", jfc->jfcn);
 
 	cdma_release_jfc_event(jfc);
 	kfree(jfc);

drivers/ub/cdma/cdma_jfs.c
@@ -314,8 +314,9 @@ struct cdma_base_jfs *cdma_create_jfs(struct cdma_dev *cdev,
 	jfs->base_jfs.jfae_handler = cdma_jfs_async_event_cb;
 	jfs->base_jfs.dev = cdev;
 
-	dev_dbg(cdev->dev,
-		"create jfs id = %u, queue id = %u, depth = %u, priority = %u, jfc id = %u.\n",
+	dev_info(
+		cdev->dev,
+		"create jfs, id = %u, queue id = %u, depth = %u, priority = %u, jfc id = %u.\n",
 		jfs->id, jfs->queue_id, cfg->depth, cfg->priority, cfg->jfc_id);
 
 	return &jfs->base_jfs;
@@ -432,8 +433,8 @@ static bool cdma_query_jfs_fd(struct cdma_dev *cdev,
 	return false;
 }
 
-int cdma_modify_jfs_precondition(struct cdma_dev *cdev,
-				 struct cdma_jetty_queue *sq)
+static int cdma_modify_jfs_precondition(struct cdma_dev *cdev,
+					struct cdma_jetty_queue *sq)
 {
 	struct cdma_jfs_ctx ctx = { 0 };
 	u16 rcv_send_diff = 0;
@@ -558,7 +559,7 @@ int cdma_delete_jfs(struct cdma_dev *cdev, u32 jfs_id)
 
 	cdma_free_jfs_id(cdev, jfs_id);
 
-	pr_debug("Leave %s, jfsn: %u.\n", __func__, jfs_id);
+	dev_info(cdev->dev, "delete jfs, id = %u.\n", jfs_id);
 
 	cdma_release_jfs_event(jfs);
 

drivers/ub/cdma/cdma_main.c
@@ -110,7 +110,7 @@ static void cdma_free_cfile_uobj(struct cdma_dev *cdev)
 		cdma_cleanup_context_uobj(cfile, CDMA_REMOVE_DRIVER_REMOVE);
 		cfile->cdev = NULL;
 		if (cfile->uctx) {
-			jfae = (struct cdma_jfae *)cfile->uctx->jfae;
+			jfae = cfile->uctx->jfae;
 			if (jfae)
 				wake_up_interruptible(&jfae->jfe.poll_wait);
 			cdma_cleanup_context_res(cfile->uctx);