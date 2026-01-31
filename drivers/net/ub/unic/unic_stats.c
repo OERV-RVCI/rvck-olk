OERV-RVCI/rvck-olk

commit d8164d3745d4497c9fe901371f64a07b9c409537

net: unic: add io basic Rx/Tx functionality for unic
drivers inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID41MP
CVE: NA

-------------------------------------------

This patch provides queue management and basic tx/rx functionality;
however, this functionality depends on IP configuration management,
which will be provided in subsequent patches.

Signed-off-by: Haiqing Fang <fanghaiqing@huawei.com>
Signed-off-by: Haibin Lu <luhaibin10@hisilicon.com>
Signed-off-by: Yixi Shen <shenyixi@huawei.com>
Signed-off-by: Jianqiang Li <lijianqiang16@huawei.com>
Signed-off-by: Junxin Chen <chenjunxin1@huawei.com>
Signed-off-by: Xiongchuan Zhou <zhouxiongchuan@h-partners.com>
Signed-off-by: Fengyan Mu <mufengyan@huawei.com>

drivers/net/ub/unic/unic_tx.h
@@ -1,0 +1,305 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved.
+ *
+ */
+
+#ifndef __UNIC_TX_H__
+#define __UNIC_TX_H__
+
+#include <linux/netdevice.h>
+
+struct unic_dev;
+
+enum unic_jfs_ctx_mode {
+	UNIC_JFS,
+	UNIC_JETTY,
+};
+
+#define UNIC_RAW_TYPE			0
+#define UNIC_RAW_SQEBB_SIZE		64
+
+#define UNIC_SQE_VA0_OFFSET		12
+#define UNIC_SQE_VA0_VALID_BIT		GENMASK(19, 0)
+#define UNIC_SQE_VA1_OFFSET		32
+#define UNIC_SQE_VA1_VALID_BIT		GENMASK(31, 0)
+#define UNIC_SQE_TOKEN_ID_L_MASK	GENMASK(11, 0)
+#define UNIC_SQE_TOKEN_ID_H_OFFSET	12U
+#define UNIC_SQE_TOKEN_ID_H_MASK	GENMASK(7, 0)
+#define UNIC_SQE_SL_BIT			GENMASK(3, 0)
+
+#define UNIC_JFS_DB_BASE_OFFSET		0x1080
+#define UNIC_JFS_DB_4K_OFFSET		0x1000
+#define UNIC_JFS_DB_64K_OFFSET		0x10000
+
+#define UNIC_PAGE_SIZE_4K		4096
+#define UNIC_AVAIL_SGMT_OST_INIT	512
+
+#define unic_sq_stats_inc(sq, cnt)	do {		\
+		typeof(sq) _sq = (sq);			\
+		u64_stats_update_begin(&(_sq)->syncp);	\
+		((_sq)->stats.cnt)++;			\
+		u64_stats_update_end(&(_sq)->syncp);	\
+	} while (0)
+
+union l3_hdr_info {
+	struct iphdr *v4;
+	struct ipv6hdr *v6;
+	u8 *hdr;
+};
+
+union l4_hdr_info {
+	struct tcphdr *tcp;
+	struct udphdr *udp;
+	u8 *hdr;
+};
+
+enum unic_pkt_l4_type {
+	UNIC_L4_TYPE_UNKNOWN,
+	UNIC_L4_TYPE_TCP,
+	UNIC_L4_TYPE_UDP,
+	UNIC_L4_TYPE_RESERVE
+};
+
+enum unic_pkt_tun_type {
+	UNIC_TUN_NONE,
+	UNIC_TUN_UDP,
+	UNIC_TUN_NVGRE,
+	UNIC_TUN_OTHER
+};
+
+enum unic_jfs_ta_timeout {
+	UNIC_TIMEOUT_128MS,
+	UNIC_TIMEOUT_1S,
+	UNIC_TIMEOUT_8S,
+	UNIC_TIMEOUT_64S
+};
+
+enum unic_jfs_state {
+	UNIC_JFS_STATE_RESET,
+	UNIC_JFS_STATE_READY,
+	UNIC_JFS_STATE_ERROR,
+	UNIC_JFS_STATE_SUSPEND,
+	UNIC_JFS_STATE_RESERVED
+};
+
+struct unic_sq_stats {
+	u64 pad_err;
+	u64 bytes;
+	u64 packets;
+	u64 map_err;
+	u64 busy;
+	u64 more;
+	u64 restart_queue;
+	u64 over_max_sge_num;
+	u64 csum_err;
+	u64 ci_mismatch;
+	u64 vlan_err;
+	u64 fd_cnt;
+	u64 drop_cnt;
+	u64 cfg5_drop_cnt;
+};
+
+struct unic_jfs_ctx {
+	/* DW0 */
+	u32 ta_timeout : 2;
+	u32 rnr_retry_num : 3;
+	u32 type : 3;
+	u32 sqe_bb_shift : 4;
+	u32 sl : 4;
+	u32 state : 3;
+	u32 jfs_mode : 1;
+	u32 sqe_token_id_l : 12;
+	/* DW1 */
+	u32 sqe_token_id_h : 8;
+	u32 err_mode : 1;
+	u32 rsv0 : 1;
+	u32 cmp_odr : 1;
+	u32 rsv1 : 1;
+	u32 sqe_base_addr_l : 20;
+	/* DW2 */
+	u32 sqe_base_addr_h;
+	/* DW3 */
+	u32 rsv2;
+	/* DW4 */
+	u32 tx_jfcn : 20;
+	u32 jfrn : 12;
+	/* DW5 */
+	u32 jfrn1 : 8;
+	u32 rsv3 : 4;
+	u32 rx_jfcn : 20;
+	/* DW6 */
+	u32 seid_idx : 10;
+	u32 pi_type : 1;
+	u32 rsv4 : 21;
+	/* DW7 */
+	u32 user_data_l;
+	/* DW8 */
+	u32 user_data_h;
+	/* DW9 */
+	u32 sqe_position : 1;
+	u32 sqe_pld_position : 1;
+	u32 sqe_pld_tokenid : 20;
+	u32 rsv5 : 10;
+	/* DW10 */
+	u32 tpn : 24;
+	u32 rsv6 : 8;
+	/* DW11 */
+	u32 rmt_eid : 20;
+	u32 rsv7 : 12;
+	/* DW12 */
+	u32 rmt_tokenid : 20;
+	u32 rsv8 : 12;
+	/* DW13 - DW15 */
+	u32 rsv8_1[3];
+	/* DW16 */
+	u32 next_send_ssn : 16;
+	u32 src_order_wqe : 16;
+	/* DW17 */
+	u32 src_order_ssn : 16;
+	u32 src_order_sgme_cnt : 16;
+	/* DW18 */
+	u32 src_order_sgme_send_cnt : 16;
+	u32 CI : 16;
+	/* DW19 */
+	u32 wqe_sgmt_send_cnt : 20;
+	u32 src_order_wqebb_num : 4;
+	u32 src_order_wqe_vld : 1;
+	u32 no_wqe_send_cnt : 4;
+	u32 so_lp_vld : 1;
+	u32 fence_lp_vld : 1;
+	u32 strong_fence_lp_vld : 1;
+	/* DW20 */
+	u32 PI : 16;
+	u32 sq_db_doing : 1;
+	u32 ost_rce_credit : 15;
+	/* DW21 */
+	u32 sq_db_retrying : 1;
+	u32 wmtp_rsv0 : 31;
+	/* DW22 */
+	u32 wait_ack_timeout : 1;
+	u32 wait_rnr_timeout : 1;
+	u32 cqe_ie : 1;
+	u32 cqe_sz : 1;
+	u32 wml_rsv0 : 28;
+	/* DW23 */
+	u32 wml_rsv1 : 32;
+	/* DW24 */
+	u32 next_rcv_ssn : 16;
+	u32 next_cpl_bb_idx : 16;
+	/* DW25 */
+	u32 next_cpl_sgmt_num : 20;
+	u32 we_rsv0 : 12;
+	/* DW26 */
+	u32 next_cpl_bb_num : 4;
+	u32 next_cpl_cqe_en : 1;
+	u32 next_cpl_info_vld : 1;
+	u32 rpting_cqe : 1;
+	u32 not_rpt_cqe : 1;
+	u32 flush_ssn : 16;
+	u32 flush_ssn_vld : 1;
+	u32 flush_vld : 1;
+	u32 flush_cqe_done : 1;
+	u32 we_rsv1 : 5;
+	/* DW27 */
+	u32 rcved_cont_ssn_num : 20;
+	u32 we_rsv2 : 12;
+	/* DW28 */
+	u32 sq_timer;
+	/* DW29 */
+	u32 rnr_cnt : 3;
+	u32 abt_ssn : 16;
+	u32 abt_ssn_vld : 1;
+	u32 taack_timeout_flag : 1;
+	u32 we_rsv3 : 9;
+	u32 err_type_l : 2;
+	/* DW30 */
+	u32 err_type_h : 7;
+	u32 sq_flush_ssn : 16;
+	u32 we_rsv4 : 9;
+	/* DW31 */
+	u32 avail_sgmt_ost : 10;
+	u32 read_op_cnt : 10;
+	u32 we_rsv5 : 12;
+};
+
+struct unic_sqe_ctrl_section {
+	__le16 sqe_bd_idx;
+	__le16 l3_type : 2;
+	__le16 l4_type : 2;
+	__le16 l3_csum : 1;
+	__le16 l4_csum : 1;
+	__le16 ol3_type : 2;
+	__le16 ol4_csum : 1;
+	__le16 tun_type : 4;
+	__le16 tsyn : 1;
+	__le16 tso : 1;
+	__le16 owner : 1;
+
+	u8 l2hdr_len;
+	u8 l3hdr_len;
+	u8 l4hdr_len;
+	u8 sge_num : 5;
+	u8 rsv1 : 2;
+	u8 dip_type : 1;
+
+	u8 ol2hdr_len;
+	u8 ol3hdr_len;
+	u8 ol4hdr_len;
+	u8 rsv2;
+
+	__le32 mss : 14;
+	__le32 rsv3 : 18;
+
+	__le32 dip0;
+	__le32 dip1;
+	__le32 dip2;
+	__le32 dip3;
+};
+
+struct unic_sqe_sge_section {
+	__le32 length;
+	__le32 token_id : 20;
+	__le32 rsv : 12;
+	__le64 address;
+};
+
+struct unic_sqebb {
+	u64 data[8];
+};
+
+struct unic_jfs_db {
+	__le16 pi;
+	__le16 rsv;
+};
+
+struct unic_sq {
+	void __iomem		*db_addr;
+	struct device		*parent_dev;
+	struct unic_sq_stats	stats;
+	struct u64_stats_sync	syncp;
+	struct unic_jfs_ctx	jfs_ctx;
+	struct unic_sqebb	*sqebb;
+	dma_addr_t		sqebb_dma_addr;
+	struct sk_buff		**skbs;
+	struct unic_cq		*cq;
+	u16			pi; /* the start of next to product */
+	u16			ci; /* the start of next to consume */
+	u16			last_pi;
+	u16			start_pi;
+	bool			check_ci_late;
+	u16			queue_index;
+	struct net_device	*netdev;
+};
+
+void unic_poll_tx(struct unic_sq *sq, int budget);
+int unic_create_sq(struct unic_dev *unic_dev, u32 idx);
+void unic_destroy_sq(struct unic_dev *unic_dev, u32 num);
+
+netdev_tx_t unic_start_xmit(struct sk_buff *skb, struct net_device *netdev);
+void unic_clear_sq(struct unic_sq *sq);
+void unic_reset_tx_queue(struct net_device *netdev);
+void unic_dump_sq_stats(struct net_device *netdev, u32 queue_idx);
+void unic_mask_key_words(void *sqebb);
+
+#endif

drivers/net/ub/unic/Makefile
@@ -8,5 +8,5 @@ ccflags-y += -I$(srctree)/drivers/net/ub/unic/debugfs
 
 obj-$(CONFIG_UB_UNIC) += unic.o
 unic-objs = unic_main.o unic_ethtool.o unic_hw.o unic_guid.o unic_netdev.o unic_dev.o unic_qos_hw.o unic_event.o unic_crq.o
-unic-objs += debugfs/unic_debugfs.o
+unic-objs += debugfs/unic_debugfs.o unic_rx.o unic_tx.o unic_txrx.o
 unic-$(CONFIG_UB_UNIC_DCB) += unic_dcbnl.o

drivers/net/ub/unic/debugfs/unic_debugfs.c
@@ -81,6 +81,49 @@ static int unic_dbg_dump_caps_info(struct seq_file *s, void *data)
 	return 0;
 }
 
+static void unic_dump_page_pool_info(struct page_pool *page_pool,
+				     struct seq_file *s, u32 index)
+{
+#define UNIC_SIZE_1K	1024
+
+	seq_printf(s, "%-10u", index);
+	seq_printf(s, "%-14u", READ_ONCE(page_pool->pages_state_hold_cnt));
+	seq_printf(s, "%-14d", atomic_read(&page_pool->pages_state_release_cnt));
+	seq_printf(s, "%-21u", page_pool->p.pool_size);
+	seq_printf(s, "%-7u", page_pool->p.order);
+	seq_printf(s, "%-9d", page_pool->p.nid);
+	seq_printf(s, "%uK\n", page_pool->p.max_len / UNIC_SIZE_1K);
+}
+
+static int unic_dbg_dump_page_pool_info(struct seq_file *s, void *data)
+{
+	struct unic_dev *unic_dev = dev_get_drvdata(s->private);
+	struct unic_rq *rq;
+	int ret = 0;
+	u32 i;
+
+	seq_puts(s, "QUEUE_ID  ALLOCATE_CNT  FREE_CNT      POOL_SIZE(PAGE_NUM)  ");
+	seq_puts(s, "ORDER  NUMA_ID  MAX_LEN\n");
+
+	if (!mutex_trylock(&unic_dev->channels.mutex))
+		return -EBUSY;
+
+	if (__unic_resetting(unic_dev) ||
+	    !unic_dev->channels.c) {
+		ret = -EBUSY;
+		goto out;
+	}
+
+	for (i = 0; i < unic_dev->channels.num; i++) {
+		rq = unic_dev->channels.c[i].rq;
+		unic_dump_page_pool_info(rq->page_pool, s, i);
+	}
+
+out:
+	mutex_unlock(&unic_dev->channels.mutex);
+	return ret;
+}
+
 static bool unic_dbg_dentry_support(struct device *dev, u32 property)
 {
 	struct unic_dev *unic_dev = dev_get_drvdata(dev);
@@ -112,7 +155,14 @@ static struct ubase_dbg_cmd_info unic_dbg_cmd[] = {
 		.support = unic_dbg_dentry_support,
 		.init = ubase_dbg_seq_file_init,
 		.read_func = unic_dbg_dump_caps_info,
-	},
+	}, {
+		.name = "page_pool_info",
+		.dentry_index = UNIC_DBG_DENTRY_ROOT,
+		.property = UBASE_SUP_UNIC | UBASE_SUP_UBL,
+		.support = unic_dbg_dentry_support,
+		.init = ubase_dbg_seq_file_init,
+		.read_func = unic_dbg_dump_page_pool_info,
+	}
 };
 
 int unic_dbg_init(struct auxiliary_device *adev)

drivers/net/ub/unic/unic_dev.c
@@ -61,6 +61,13 @@ u32 unic_channels_max_num(struct auxiliary_device *adev)
 	return min(min(jfs_num, jfr_num), jfc_num >> 1);
 }
 
+u32 unic_get_max_rss_size(struct unic_dev *unic_dev)
+{
+	u8 vl_num = unic_dev->channels.rss_vl_num;
+
+	return unic_channels_max_num(unic_dev->comdev.adev) / vl_num;
+}
+
 void unic_update_queue_info(struct unic_dev *unic_dev)
 {
 	struct auxiliary_device *adev = unic_dev->comdev.adev;
@@ -283,6 +290,196 @@ static void unic_uninit_channels_attr(struct unic_dev *unic_dev)
 	struct unic_channels *channels = &unic_dev->channels;
 
 	mutex_destroy(&channels->mutex);
+}
+
+int unic_init_tx(struct unic_dev *unic_dev, u32 num)
+{
+	struct unic_channel *c;
+	u32 i, j;
+	int ret;
+
+	for (i = 0; i < num; i++) {
+		ret = unic_create_cq(unic_dev, i, UNIC_CQ_SQ);
+		if (ret) {
+			dev_err(unic_dev->comdev.adev->dev.parent,
+				"failed to init tx cq(%u), ret=%d.\n", i, ret);
+			goto err_create_cq;
+		}
+	}
+
+	for (j = 0; j < num; j++) {
+		ret = unic_create_sq(unic_dev, j);
+		if (ret) {
+			dev_err(unic_dev->comdev.adev->dev.parent,
+				"failed to init tx sq(%u), ret=%d.\n", j, ret);
+			goto err_create_sq;
+		}
+		c = &unic_dev->channels.c[j];
+		c->sq->cq = c->sq_cq;
+	}
+
+	set_bit(UNIC_TX_INITED, &unic_dev->channels.state);
+
+	return 0;
+
+err_create_sq:
+	unic_destroy_sq(unic_dev, j);
+err_create_cq:
+	unic_destroy_cq(unic_dev, i, UNIC_CQ_SQ);
+	return ret;
+}
+
+int unic_init_rx(struct unic_dev *unic_dev, u32 num)
+{
+	struct unic_channel *c;
+	u32 i, j;
+	int ret;
+
+	for (i = 0; i < num; i++) {
+		ret = unic_create_cq(unic_dev, i, UNIC_CQ_RQ);
+		if (ret) {
+			dev_err(unic_dev->comdev.adev->dev.parent,
+				"failed to init rx cq(%u), ret=%d.\n", i, ret);
+				goto err_create_cq;
+		}
+	}
+
+	for (j = 0; j < num; j++) {
+		ret = unic_create_rq(unic_dev, j);
+		if (ret) {
+			dev_err(unic_dev->comdev.adev->dev.parent,
+				"failed to init rx rq(%u), ret=%d.\n", j, ret);
+			goto err_create_rq;
+		}
+		c = &unic_dev->channels.c[j];
+		c->rq->cq = c->rq_cq;
+	}
+
+	set_bit(UNIC_RX_INITED, &unic_dev->channels.state);
+
+	return 0;
+
+err_create_rq:
+	unic_destroy_rq(unic_dev, j);
+err_create_cq:
+	unic_destroy_cq(unic_dev, i, UNIC_CQ_RQ);
+	return ret;
+}
+
+static int unic_init_jetty(struct unic_dev *unic_dev, u32 num)
+{
+	int ret;
+
+	ret = unic_init_rx(unic_dev, num);
+	if (ret)
+		return ret;
+
+	ret = unic_init_tx(unic_dev, num);
+	if (ret)
+		unic_destroy_rx(unic_dev, num);
+
+	return ret;
+}
+
+void unic_destroy_tx(struct unic_dev *unic_dev, u32 num)
+{
+	if (!test_and_clear_bit(UNIC_TX_INITED, &unic_dev->channels.state))
+		return;
+
+	unic_destroy_sq(unic_dev, num);
+	unic_destroy_cq(unic_dev, num, UNIC_CQ_SQ);
+}
+
+void unic_destroy_rx(struct unic_dev *unic_dev, u32 num)
+{
+	if (!test_and_clear_bit(UNIC_RX_INITED, &unic_dev->channels.state))
+		return;
+
+	unic_destroy_rq(unic_dev, num);
+	unic_destroy_cq(unic_dev, num, UNIC_CQ_RQ);
+}
+
+static void unic_destroy_jetty(struct unic_dev *unic_dev, u32 num)
+{
+	unic_destroy_tx(unic_dev, num);
+	unic_destroy_rx(unic_dev, num);
+}
+
+static int __unic_init_channels(struct unic_dev *unic_dev, u32 channels_num)
+{
+	struct auxiliary_device *adev = unic_dev->comdev.adev;
+	struct unic_channels *channels = &unic_dev->channels;
+	int ret;
+	u32 i;
+
+	channels->num = channels_num;
+	channels->c = kcalloc(channels_num, sizeof(*channels->c), GFP_KERNEL);
+	if (ZERO_OR_NULL_PTR(channels->c))
+		return -ENOMEM;
+
+	ret = unic_init_jetty(unic_dev, channels->num);
+	if (ret) {
+		dev_err(adev->dev.parent,
+			"failed to init jetty, ret = %d.\n", ret);
+		goto err_init_jetty;
+	}
+
+	for (i = 0; i < channels->num; i++)
+		netif_napi_add(unic_dev->comdev.netdev, &channels->c[i].napi,
+			       unic_napi_poll);
+
+	return 0;
+
+err_init_jetty:
+	kfree(channels->c);
+	channels->c = NULL;
+
+	return ret;
+}
+
+int unic_init_channels(struct unic_dev *unic_dev, u32 channels_num)
+{
+	struct unic_channels *channels = &unic_dev->channels;
+	int ret;
+
+	mutex_lock(&channels->mutex);
+
+	ret = __unic_init_channels(unic_dev, channels_num);
+	if (!ret)
+		clear_bit(UNIC_STATE_CHANNEL_INVALID, &unic_dev->state);
+
+	mutex_unlock(&channels->mutex);
+	return ret;
+}
+
+static void __unic_uninit_channels(struct unic_dev *unic_dev)
+{
+	struct unic_channels *channels = &unic_dev->channels;
+	u32 i;
+
+	if (!channels->c)
+		return;
+
+	for (i = 0; i < channels->num; i++)
+		netif_napi_del(&channels->c[i].napi);
+
+	unic_destroy_jetty(unic_dev, channels->num);
+
+	kfree(channels->c);
+
+	channels->c = NULL;
+}
+
+void unic_uninit_channels(struct unic_dev *unic_dev)
+{
+	struct unic_channels *channels = &unic_dev->channels;
+
+	mutex_lock(&channels->mutex);
+
+	set_bit(UNIC_STATE_CHANNEL_INVALID, &unic_dev->state);
+	__unic_uninit_channels(unic_dev);
+
+	mutex_unlock(&channels->mutex);
 }
 
 static void unic_set_netdev_attr(struct net_device *netdev)
@@ -544,9 +741,18 @@ static int unic_init_netdev_priv(struct net_device *netdev,
 	if (ret)
 		goto err_uninit_vport;
 
+	ret = unic_init_channels(priv, priv->channels.num);
+	if (ret) {
+		dev_err(adev->dev.parent,
+			"failed to init channels, ret = %d.\n", ret);
+		goto err_uninit_channels_attr;
+	}
+
 	set_bit(UNIC_STATE_DOWN, &priv->state);
 	return 0;
 
+err_uninit_channels_attr:
+	unic_uninit_channels_attr(priv);
 err_uninit_vport:
 	unic_uninit_vport(priv);
 destroy_lock:
@@ -559,6 +765,7 @@ static void unic_uninit_netdev_priv(struct net_device *netdev)
 {
 	struct unic_dev *priv = netdev_priv(netdev);
 
+	unic_uninit_channels(priv);
 	unic_uninit_channels_attr(priv);
 	unic_uninit_vport(priv);
 	mutex_destroy(&priv->act_info.mutex);

drivers/net/ub/unic/unic_dev.h
@@ -17,6 +17,9 @@
 #include "unic.h"
 #include "unic_cmd.h"
 #include "unic_ethtool.h"
+#include "unic_rx.h"
+#include "unic_tx.h"
+#include "unic_txrx.h"
 
 #define UNIC_MOD_VERSION	"1.0"
 #define UNIC_AE_LEVEL_NUM	1
@@ -30,6 +33,9 @@ enum unic_dev_state {
 	UNIC_STATE_DISABLED,
 	UNIC_STATE_LINK_UPDATING,
 	UNIC_STATE_TESTING,
+	UNIC_STATE_FEC_STATS_UPDATING,
+	UNIC_STATE_MAC_STATS_UPDATING,
+	UNIC_STATE_CHANNEL_INVALID,
 	UNIC_STATE_DEACTIVATE,
 };
 
@@ -39,6 +45,23 @@ enum unic_vport_state {
 	UNIC_VPORT_STATE_IP_TBL_CHANGE,
 	UNIC_VPORT_STATE_IP_QUERYING,
 };
+
+enum unic_channel_state {
+	UNIC_TX_CHANGED,
+	UNIC_RX_CHANGED,
+	UNIC_TX_INITED,
+	UNIC_RX_INITED,
+};
+
+#define UNIC_CQE_PERIOD_0	0
+#define UNIC_CQE_PERIOD_4	4
+#define UNIC_CQE_PERIOD_16	16
+#define UNIC_CQE_PERIOD_64	64
+#define UNIC_CQE_PERIOD_256	256
+#define UNIC_CQE_PERIOD_1024	1024
+#define UNIC_CQE_PERIOD_4096	4096
+#define UNIC_CQE_PERIOD_16384	16384
+#define UNIC_CQE_PERIOD_ERR	16385
 
 #define UNIC_DEFAULT_CHANNEL_NUM	1
 
@@ -199,10 +222,16 @@ struct unic_dev {
 int unic_dev_init(struct auxiliary_device *adev);
 void unic_dev_uninit(struct auxiliary_device *adev);
 u32 unic_channels_max_num(struct auxiliary_device *adev);
+int unic_init_channels(struct unic_dev *unic_dev, u32 channels_num);
+void unic_uninit_channels(struct unic_dev *unic_dev);
 void unic_start_period_task(struct net_device *netdev);
 void unic_remove_period_task(struct unic_dev *unic_dev);
 int unic_init_wq(void);
 void unic_destroy_wq(void);
+int unic_init_rx(struct unic_dev *unic_dev, u32 num);
+int unic_init_tx(struct unic_dev *unic_dev, u32 num);
+void unic_destroy_rx(struct unic_dev *unic_dev, u32 num);
+void unic_destroy_tx(struct unic_dev *unic_dev, u32 num);
 int unic_set_vl_map(struct unic_dev *unic_dev, u8 *dscp_prio, u8 *prio_vl,
 		    u8 map_type);
 int unic_dbg_log(void);
@@ -265,6 +294,26 @@ static inline bool unic_initing(struct net_device *netdev)
 static inline bool unic_is_initing_or_resetting(struct unic_dev *unic_dev)
 {
 	return __unic_resetting(unic_dev) || __unic_initing(unic_dev);
+}
+
+static inline u32 unic_get_sq_cqe_mask(struct unic_dev *unic_dev)
+{
+	return unic_dev->channels.sq_cqe_depth - 1;
+}
+
+static inline u32 unic_get_rq_cqe_mask(struct unic_dev *unic_dev)
+{
+	return unic_dev->channels.rq_cqe_depth - 1;
+}
+
+static inline bool unic_tx_changed(struct unic_dev *unic_dev)
+{
+	return test_bit(UNIC_TX_CHANGED, &unic_dev->channels.state);
+}
+
+static inline bool unic_rx_changed(struct unic_dev *unic_dev)
+{
+	return test_bit(UNIC_RX_CHANGED, &unic_dev->channels.state);
 }
 
 static inline u32 unic_cmd_timeout(struct unic_dev *unic_dev)

drivers/net/ub/unic/unic_event.c
@@ -13,6 +13,7 @@
 #include <ub/ubase/ubase_comm_eq.h>
 #include <ub/ubase/ubase_comm_hw.h>
 #include <ub/ubase/ubase_comm_qos.h>
+#include <ub/ubase/ubase_comm_ctrlq.h>
 
 #include "unic_cmd.h"
 #include "unic_crq.h"
@@ -22,6 +23,33 @@
 #include "unic_netdev.h"
 #include "unic_qos_hw.h"
 #include "unic_event.h"
+
+int unic_comp_handler(struct notifier_block *nb, unsigned long jfcn, void *data)
+{
+	struct auxiliary_device *adev = (struct auxiliary_device *)data;
+	struct unic_dev *unic_dev = dev_get_drvdata(&adev->dev);
+	struct unic_channels *channels = &unic_dev->channels;
+	struct unic_cq *cq;
+	u32 index;
+
+	if (test_bit(UNIC_STATE_CHANNEL_INVALID, &unic_dev->state))
+		return -EBUSY;
+
+	index = jfcn < channels->num ? jfcn : jfcn - channels->num;
+	if (index >= channels->num)
+		return -EINVAL;
+
+	if (jfcn > channels->num)
+		cq = channels->c[index].rq->cq;
+	else
+		cq = channels->c[index].sq->cq;
+
+	cq->event_cnt++;
+
+	napi_schedule(&channels->c[index].napi);
+
+	return 0;
+}
 
 static struct ubase_crq_event_nb unic_crq_events[] = {
 	{

drivers/net/ub/unic/unic_event.h
@@ -11,5 +11,6 @@
 
 int unic_register_event(struct auxiliary_device *adev);
 void unic_unregister_event(struct auxiliary_device *adev);
+int unic_comp_handler(struct notifier_block *nb, unsigned long jfcn, void *data);
 
 #endif

commit 203343d839832f5d5cfa7d64b16fd85ac8787daa

net: unic: fix poll tx read old skb cause a UAF bug
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8458
CVE: NA

---------------------------------------------------------------

This patch fixed an AUF bug in poll tx thread when reclaiming skb
because of missing a memory barrier.

The bug scenario is as follows:
In the xmit thread, UNIC saves the skb into the sq->skbs[], then
update sq->pi and then write the doorbell to trigger the hardware
to send this packet. After the hardware complete transmission, it
report a interrupt and trigger the napi poll thread running. In the
poll thread, UNIC may get an old skb from the sq->skbs[] because
of missing a memory barrier.

The main modification is:
Atfer consumed the skb, the poll thread will clear it in the
sq->skb[], and add a dma_rmb() barrier before polling to ensure that
poll thread can get the lastest sq->pi and skb.

To enhance reliability and locatability, some additional exception
conditions and statistics have also been added for interception.

Fixes: d8164d3745d4 ("net: unic: add io basic Rx/Tx functionality for unic")
Signed-off-by: Chuan Wu <wuchuan4@huawei.com>

drivers/net/ub/unic/unic_stats.c
@@ -93,7 +93,10 @@ static const struct unic_stats_desc unic_sq_stats_str[] = {
 	{"vlan_err", UNIC_SQ_STATS_FIELD_OFF(vlan_err)},
 	{"fd_cnt", UNIC_SQ_STATS_FIELD_OFF(fd_cnt)},
 	{"drop_cnt", UNIC_SQ_STATS_FIELD_OFF(drop_cnt)},
-	{"cfg5_drop_cnt", UNIC_SQ_STATS_FIELD_OFF(cfg5_drop_cnt)}
+	{"cfg5_drop_cnt", UNIC_SQ_STATS_FIELD_OFF(cfg5_drop_cnt)},
+	{"polled_old_pi", UNIC_SQ_STATS_FIELD_OFF(polled_old_pi)},
+	{"polled_skb_null", UNIC_SQ_STATS_FIELD_OFF(polled_skb_null)},
+	{"pi_ci_over_depth", UNIC_SQ_STATS_FIELD_OFF(pi_ci_over_depth)}
 };
 
 static const struct unic_stats_desc unic_rq_stats_str[] = {

drivers/net/ub/unic/unic_tx.c
@@ -86,7 +86,7 @@ static inline u16 unic_get_spare_page_num(struct unic_tx_buff *tx_buff)
 	return tx_buff->num - (pi - ci);
 }
 
-static bool unic_check_hw_ci_valid(u16 hw_ci, u16 sq_ci, struct unic_sq *sq)
+static bool unic_check_hw_ci_valid(struct unic_sq *sq, u16 hw_ci, u16 sq_ci)
 {
 	u16 sqebb_mask = unic_get_sqe_mask(sq);
 	struct unic_sqe_ctrl_section *ctrl;
@@ -100,6 +100,27 @@ static bool unic_check_hw_ci_valid(u16 hw_ci, u16 sq_ci, struct unic_sq *sq)
 		return false;
 	}
 
+	return true;
+}
+
+static bool unic_check_sq_ci_valid(struct unic_sq *sq, u32 sq_pi, u32 sq_ci)
+{
+	struct net_device *netdev = sq->netdev;
+	struct unic_dev *unic_dev = netdev_priv(netdev);
+	u16 sqebb_depth = unic_dev->channels.sqebb_depth;
+
+	if (unlikely(sq_pi < sq_ci))
+		sq_pi += UNIC_SQEBB_POINT_REVERSE;
+
+	if (unlikely(sq_pi == sq_ci)) {
+		unic_sq_stats_inc(sq, polled_old_pi);
+		return false;
+	}
+
+	if (unlikely(sq_pi - sq_ci > sqebb_depth)) {
+		unic_sq_stats_inc(sq, pi_ci_over_depth);
+		return false;
+	}
 	return true;
 }
 
@@ -137,20 +158,20 @@ static void unic_flush_unused_sqe(struct unic_sq *sq, u16 sqebb_mask,
 	}
 }
 
-static bool unic_check_hw_ci_late(struct unic_sq *sq, u16 sq_ci)
+static bool unic_check_hw_ci_late(struct unic_sq *sq, u32 sq_pi, u32 sq_ci)
 {
 	u32 effect_num, actual_num;
 
 	if (likely(!sq->check_ci_late))
 		return false;
 
-	effect_num = sq->pi < sq->start_pi ?
-		     (u32)sq->pi + UNIC_SQEBB_POINT_REVERSE - (u32)sq->start_pi :
-		     (u32)sq->pi - (u32)sq->start_pi;
+	effect_num = sq_pi < sq->start_pi ?
+		     sq_pi + UNIC_SQEBB_POINT_REVERSE - sq->start_pi :
+		     sq_pi - sq->start_pi;
 
-	actual_num = sq->pi < sq_ci ?
-		    (u32)sq->pi + UNIC_SQEBB_POINT_REVERSE - (u32)sq_ci :
-		    (u32)sq->pi - (u32)sq_ci;
+	actual_num = sq_pi < sq_ci ?
+		     sq_pi + UNIC_SQEBB_POINT_REVERSE - sq_ci :
+		     sq_pi - sq_ci;
 
 	if (unlikely(actual_num > effect_num)) {
 		unic_sq_stats_inc(sq, drop_cnt);
@@ -170,15 +191,20 @@ static bool unic_reclaim_sq_space(struct unic_sq *sq, int budget, u64 *bytes,
 	u16 sqebb_mask = unic_get_sqe_mask(sq);
 	union unic_cqe *cqe = sq->cq->cqe;
 	struct unic_cq *cq = sq->cq;
-	u32 cq_mask, cq_ci = cq->ci;
+	u32 cq_mask, last_cq_ci = cq->ci;
+	u16 sq_pi, sq_ci = sq->ci;
 	bool reclaimed = false;
 	struct sk_buff *skb;
-	u16 sq_ci = sq->ci;
 
 	cq_mask = unic_get_sq_cqe_mask(unic_dev);
 	cqe = &cq->cqe[cq->ci & cq_mask];
 	while (unic_cqe_owner_is_soft(jfc_shift, cq->ci, cqe->tx.owner)) {
-		trace_unic_tx_cqe(netdev, cq, sq->pi, sq_ci, cq_mask);
+		/* ensure that this polling thread can read the newest sq->pi
+		 * and sq->skbs[index] which match the cqe.
+		 */
+		dma_rmb();
+		sq_pi = sq->pi;
+		trace_unic_tx_cqe(netdev, cq, sq_pi, sq_ci, cq_mask);
 		if (unlikely(cqe->tx.fd)) {
 			cq->ci++;
 			unic_sq_stats_inc(sq, fd_cnt);
@@ -187,17 +213,23 @@ static bool unic_reclaim_sq_space(struct unic_sq *sq, int budget, u64 *bytes,
 			break;
 		}
 
-		if (unlikely(!unic_check_hw_ci_valid(cqe->tx.raw_ci, sq_ci, sq)))
+		if (unlikely(!unic_check_hw_ci_valid(sq, cqe->tx.raw_ci, sq_ci) ||
+			     !unic_check_sq_ci_valid(sq, sq_pi, sq_ci)))
 			break;
 
 		skb = sq->skbs[sq_ci & sqebb_mask];
+		if (unlikely(!skb)) {
+			unic_sq_stats_inc(sq, polled_skb_null);
+			break;
+		}
 
-		if (!clear && likely(!unic_check_hw_ci_late(sq, sq_ci))) {
+		if (!clear && likely(!unic_check_hw_ci_late(sq, sq_pi, sq_ci))) {
 			*bytes += skb_headlen(skb);
 			(*packets)++;
 		}
 
 		napi_consume_skb(skb, budget);
+		sq->skbs[sq_ci & sqebb_mask] = NULL;
 		unic_reclaim_single_sqe_space(sq, sqebb_mask, &sq_ci);
 
 		reclaimed = true;
@@ -205,7 +237,7 @@ static bool unic_reclaim_sq_space(struct unic_sq *sq, int budget, u64 *bytes,
 		cqe = &cq->cqe[cq->ci & cq_mask];
 	}
 
-	unic_cq_doorbell(cq, cq_ci);
+	unic_cq_doorbell(cq, last_cq_ci);
 	sq->ci = sq_ci;
 	return reclaimed;
 }
@@ -1292,13 +1324,17 @@ void unic_dump_sq_stats(struct net_device *netdev, u32 queue_idx)
 		  "ci_mismatch:      %llu\n"
 		  "fd_cnt:           %llu\n"
 		  "drop_cnt:         %llu\n"
-		  "cfg5_drop_cnt:    %llu\n",
+		  "cfg5_drop_cnt:    %llu\n"
+		  "polled_old_pi:    %llu\n"
+		  "polled_skb_null   %llu\n"
+		  "pi_ci_over_depth: %llu\n",
 		  queue_idx, queue->state, sq->pi, sq->ci,
 		  sq_stats->pad_err, sq_stats->bytes, sq_stats->packets,
 		  sq_stats->busy, sq_stats->more, sq_stats->restart_queue,
 		  sq_stats->over_max_sge_num, sq_stats->csum_err,
 		  sq_stats->ci_mismatch, sq_stats->fd_cnt, sq_stats->drop_cnt,
-		  sq_stats->cfg5_drop_cnt);
+		  sq_stats->cfg5_drop_cnt, sq_stats->polled_old_pi,
+		  sq_stats->polled_skb_null, sq_stats->pi_ci_over_depth);
 }
 
 void unic_mask_key_words(void *sqebb)

drivers/net/ub/unic/unic_tx.h
@@ -97,6 +97,9 @@ struct unic_sq_stats {
 	u64 fd_cnt;
 	u64 drop_cnt;
 	u64 cfg5_drop_cnt;
+	u64 polled_old_pi;
+	u64 polled_skb_null;
+	u64 pi_ci_over_depth;
 };
 
 struct unic_jfs_ctx {