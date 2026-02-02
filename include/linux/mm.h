openeuler-mirror/kernel

commit c333c44449530463d06fb3feb9e50959aed06061

mm: convert mm's rss stats to use atomic mode
maillist inclusion
category: performance
bugzilla: https://gitee.com/openeuler/kernel/issues/I9IA1I
CVE: NA

Reference: https://lore.kernel.org/all/20240418142008.2775308-1-zhangpeng362@huawei.com/

--------------------------------

Since commit f1a7941243c1 ("mm: convert mm's rss stats into
percpu_counter"), the rss_stats have converted into percpu_counter,
which convert the error margin from (nr_threads * 64) to approximately
(nr_cpus ^ 2). However, the new percpu allocation in mm_init() causes a
performance regression on fork/exec/shell. Even after commit 14ef95be6f55
("kernel/fork: group allocation/free of per-cpu counters for mm struct"),
the performance of fork/exec/shell is still poor compared to previous
kernel versions.

To mitigate performance regression, we delay the allocation of percpu
memory for rss_stats. Therefore, we convert mm's rss stats to use
percpu_counter atomic mode. For single-thread processes, rss_stat is in
atomic mode, which reduces the memory consumption and performance
regression caused by using percpu. For multiple-thread processes,
rss_stat is switched to the percpu mode to reduce the error margin.
We convert rss_stats from atomic mode to percpu mode only when the
second thread is created.

After lmbench test, we can get 2% ~ 4% performance improvement
for lmbench fork_proc/exec_proc/shell_proc and 6.7% performance
improvement for lmbench page_fault (before batch mode[1]).

The test results are as follows:

             base           base+revert        base+this patch

fork_proc    416.3ms        400.0ms  (3.9%)    398.6ms  (4.2%)
exec_proc    2095.9ms       2061.1ms (1.7%)    2047.7ms (2.3%)
shell_proc   3028.2ms       2954.7ms (2.4%)    2961.2ms (2.2%)
page_fault   0.3603ms       0.3358ms (6.8%)    0.3361ms (6.7%)

[1] https://lore.kernel.org/all/20240412064751.119015-1-wangkefeng.wang@huawei.com/

Suggested-by: Jan Kara <jack@suse.cz>
Signed-off-by: ZhangPeng <zhangpeng362@huawei.com>

include/linux/mm.h
@@ -2581,30 +2581,66 @@ static inline bool get_user_page_fast_only(unsigned long addr,
  */
 static inline unsigned long get_mm_counter(struct mm_struct *mm, int member)
 {
-	return percpu_counter_read_positive(&mm->rss_stat[member]);
+	struct percpu_counter *fbc = &mm->rss_stat[member];
+
+	if (percpu_counter_initialized(fbc))
+		return percpu_counter_read_positive(fbc);
+
+	return percpu_counter_atomic_read(fbc);
 }
 
 void mm_trace_rss_stat(struct mm_struct *mm, int member);
 
 static inline void add_mm_counter(struct mm_struct *mm, int member, long value)
 {
-	percpu_counter_add(&mm->rss_stat[member], value);
+	struct percpu_counter *fbc = &mm->rss_stat[member];
+
+	if (percpu_counter_initialized(fbc))
+		percpu_counter_add(fbc, value);
+	else
+		percpu_counter_atomic_add(fbc, value);
 
 	mm_trace_rss_stat(mm, member);
 }
 
 static inline void inc_mm_counter(struct mm_struct *mm, int member)
 {
-	percpu_counter_inc(&mm->rss_stat[member]);
-
-	mm_trace_rss_stat(mm, member);
+	add_mm_counter(mm, member, 1);
 }
 
 static inline void dec_mm_counter(struct mm_struct *mm, int member)
 {
-	percpu_counter_dec(&mm->rss_stat[member]);
+	add_mm_counter(mm, member, -1);
+}
 
-	mm_trace_rss_stat(mm, member);
+static inline s64 mm_counter_sum(struct mm_struct *mm, int member)
+{
+	struct percpu_counter *fbc = &mm->rss_stat[member];
+
+	if (percpu_counter_initialized(fbc))
+		return percpu_counter_sum(fbc);
+
+	return percpu_counter_atomic_read(fbc);
+}
+
+static inline s64 mm_counter_sum_positive(struct mm_struct *mm, int member)
+{
+	struct percpu_counter *fbc = &mm->rss_stat[member];
+
+	if (percpu_counter_initialized(fbc))
+		return percpu_counter_sum_positive(fbc);
+
+	return percpu_counter_atomic_read(fbc);
+}
+
+static inline int mm_counter_switch_to_pcpu(struct mm_struct *mm)
+{
+	return percpu_counter_switch_to_pcpu_many(mm->rss_stat, NR_MM_COUNTERS);
+}
+
+static inline void mm_counter_destroy(struct mm_struct *mm)
+{
+	percpu_counter_destroy_many(mm->rss_stat, NR_MM_COUNTERS);
 }
 
 /* Optimized variant when folio is already known not to be anon */

kernel/fork.c
@@ -853,7 +853,7 @@ static void check_mm(struct mm_struct *mm)
 			 "Please make sure 'struct resident_page_types[]' is updated as well");
 
 	for (i = 0; i < NR_MM_COUNTERS; i++) {
-		long x = percpu_counter_sum(&mm->rss_stat[i]);
+		long x = mm_counter_sum(mm, i);
 
 		if (unlikely(x))
 			pr_alert("BUG: Bad rss-counter state mm:%p type:%s val:%ld\n",
@@ -954,7 +954,7 @@ void __mmdrop(struct mm_struct *mm)
 	put_user_ns(mm->user_ns);
 	mm_pasid_drop(mm);
 	mm_destroy_cid(mm);
-	percpu_counter_destroy_many(mm->rss_stat, NR_MM_COUNTERS);
+	mm_counter_destroy(mm);
 
 	free_mm(mm);
 }
@@ -1357,17 +1357,11 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 	if (mm_alloc_cid(mm))
 		goto fail_cid;
 
-	if (percpu_counter_init_many(mm->rss_stat, 0, GFP_KERNEL_ACCOUNT,
-				     NR_MM_COUNTERS))
-		goto fail_pcpu;
-
 	sp_init_mm(mm);
 	mm->user_ns = get_user_ns(user_ns);
 	lru_gen_init_mm(mm);
 	return mm;
 
-fail_pcpu:
-	mm_destroy_cid(mm);
 fail_cid:
 	destroy_context(mm);
 fail_nocontext:
@@ -1782,6 +1776,16 @@ static int copy_mm(unsigned long clone_flags, struct task_struct *tsk)
 	oldmm = current->mm;
 	if (!oldmm)
 		return 0;
+
+	/*
+	 * For single-thread processes, rss_stat is in atomic mode, which
+	 * reduces the memory consumption and performance regression caused by
+	 * using percpu. For multiple-thread processes, rss_stat is switched to
+	 * the percpu mode to reduce the error margin.
+	 */
+	if (clone_flags & CLONE_THREAD)
+		if (mm_counter_switch_to_pcpu(oldmm))
+			return -ENOMEM;
 
 	if (clone_flags & CLONE_VM) {
 		mmget(oldmm);

include/trace/events/kmem.h
@@ -361,8 +361,8 @@ TRACE_EVENT(rss_stat,
 		__entry->mm_id = mm_ptr_to_hash(mm);
 		__entry->curr = !!(current->mm == mm);
 		__entry->member = member;
-		__entry->size = (percpu_counter_sum_positive(&mm->rss_stat[member])
-							    << PAGE_SHIFT);
+		__entry->size = (mm_counter_sum_positive(mm, member)
+							<< PAGE_SHIFT);
 	),
 
 	TP_printk("mm_id=%u curr=%d type=%s size=%ldB",

OERV-RVCI/rvck-olk

commit 46213033b8041754dea7ff02024462f02f96ebdd

mm: add config option for atomic mode in mm counter
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8456

------------------------

During the conversion of struct percpu_counters from atomic mode
to percpu mode, the entire percpu counter init process must be an
atomic operation. It needs to ensure that mm counter can only use
atomic mode when in a non-preemptible kernel.

Add MM_COUNTER_ATOMIC configuration option to enable atomic mode
for mm counter, now the atomic mode requires PREEMPT_NONE and
disables PREEMPT_DYNAMIC. Additionally, add a cmdline option
'rss_atomic_disable' to actively disable atomic mode.

Fixes: c333c4444953 ("mm: convert mm's rss stats to use atomic mode")
Signed-off-by: Quanmin Yan <yanquanmin1@huawei.com>
Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>

arch/arm64/configs/openeuler_defconfig
@@ -1278,6 +1278,7 @@ CONFIG_DAMON_LRU_SORT=y
 # end of Data Access Monitoring
 
 CONFIG_THP_CONTROL=y
+CONFIG_MM_COUNTER_ATOMIC=y
 # end of Memory Management options
 
 CONFIG_NET=y

arch/x86/configs/openeuler_defconfig
@@ -1240,6 +1240,7 @@ CONFIG_DAMON_LRU_SORT=y
 # end of Data Access Monitoring
 
 # CONFIG_THP_CONTROL is not set
+CONFIG_MM_COUNTER_ATOMIC=y
 # end of Memory Management options
 
 CONFIG_NET=y

include/linux/mm.h
@@ -2643,11 +2643,35 @@ static inline bool get_user_page_fast_only(unsigned long addr,
 /*
  * per-process(per-mm_struct) statistics.
  */
+#ifdef CONFIG_MM_COUNTER_ATOMIC
+extern bool mm_counter_atomic;
+
+static inline bool mm_counter_is_atomic(void)
+{
+	return mm_counter_atomic;
+}
+
+static inline bool mm_counter_is_pcpu(struct percpu_counter *fbc)
+{
+	return !mm_counter_is_atomic() || percpu_counter_initialized(fbc);
+}
+#else
+static inline bool mm_counter_is_atomic(void)
+{
+	return false;
+}
+
+static inline bool mm_counter_is_pcpu(struct percpu_counter *fbc)
+{
+	return true;
+}
+#endif
+
 static inline unsigned long get_mm_counter(struct mm_struct *mm, int member)
 {
 	struct percpu_counter *fbc = &mm->rss_stat[member];
 
-	if (percpu_counter_initialized(fbc))
+	if (mm_counter_is_pcpu(fbc))
 		return percpu_counter_read_positive(fbc);
 
 	return percpu_counter_atomic_read(fbc);
@@ -2659,7 +2683,7 @@ static inline void add_mm_counter(struct mm_struct *mm, int member, long value)
 {
 	struct percpu_counter *fbc = &mm->rss_stat[member];
 
-	if (percpu_counter_initialized(fbc))
+	if (mm_counter_is_pcpu(fbc))
 		percpu_counter_add(fbc, value);
 	else
 		percpu_counter_atomic_add(fbc, value);
@@ -2681,7 +2705,7 @@ static inline s64 mm_counter_sum(struct mm_struct *mm, int member)
 {
 	struct percpu_counter *fbc = &mm->rss_stat[member];
 
-	if (percpu_counter_initialized(fbc))
+	if (mm_counter_is_pcpu(fbc))
 		return percpu_counter_sum(fbc);
 
 	return percpu_counter_atomic_read(fbc);
@@ -2691,15 +2715,27 @@ static inline s64 mm_counter_sum_positive(struct mm_struct *mm, int member)
 {
 	struct percpu_counter *fbc = &mm->rss_stat[member];
 
-	if (percpu_counter_initialized(fbc))
+	if (mm_counter_is_pcpu(fbc))
 		return percpu_counter_sum_positive(fbc);
 
 	return percpu_counter_atomic_read(fbc);
 }
 
-static inline int mm_counter_switch_to_pcpu(struct mm_struct *mm)
+static inline int mm_counter_try_switch_to_pcpu(struct mm_struct *mm)
 {
+	if (!mm_counter_is_atomic())
+		return 0;
+
 	return percpu_counter_switch_to_pcpu_many(mm->rss_stat, NR_MM_COUNTERS);
+}
+
+static inline int mm_counter_init(struct mm_struct *mm)
+{
+	if (mm_counter_is_atomic())
+		return 0;
+
+	return percpu_counter_init_many(mm->rss_stat, 0, GFP_KERNEL_ACCOUNT,
+					NR_MM_COUNTERS);
 }
 
 static inline void mm_counter_destroy(struct mm_struct *mm)

kernel/fork.c
@@ -1343,6 +1343,18 @@ static void mm_init_uprobes_state(struct mm_struct *mm)
 #endif
 }
 
+#ifdef CONFIG_MM_COUNTER_ATOMIC
+bool mm_counter_atomic __ro_after_init = true;
+
+static int __init disable_rss_atomic_mode(char *str)
+{
+	mm_counter_atomic = false;
+
+	return 0;
+}
+__setup("rss_atomic_disable", disable_rss_atomic_mode);
+#endif /* CONFIG_MM_COUNTER_ATOMIC */
+
 static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 	struct user_namespace *user_ns)
 {
@@ -1398,11 +1410,16 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 	if (mm_alloc_cid(mm))
 		goto fail_cid;
 
+	if (mm_counter_init(mm))
+		goto fail_pcpu;
+
 	sp_init_mm(mm);
 	mm->user_ns = get_user_ns(user_ns);
 	lru_gen_init_mm(mm);
 	return mm;
 
+fail_pcpu:
+	mm_destroy_cid(mm);
 fail_cid:
 	destroy_context(mm);
 fail_nocontext:
@@ -1824,14 +1841,13 @@ static int copy_mm(unsigned long clone_flags, struct task_struct *tsk)
 		return 0;
 
 	/*
-	 * For single-thread processes, rss_stat is in atomic mode, which
+	 * For single-thread processes, mm counter is using atomic mode, which
 	 * reduces the memory consumption and performance regression caused by
-	 * using percpu. For multiple-thread processes, rss_stat is switched to
-	 * the percpu mode to reduce the error margin.
+	 * using percpu mode. For multiple-thread processes, will try to switch
+	 * to the percpu mode, but still using atomic mode once some error occurs.
 	 */
 	if (clone_flags & CLONE_THREAD)
-		if (mm_counter_switch_to_pcpu(oldmm))
-			return -ENOMEM;
+		mm_counter_try_switch_to_pcpu(oldmm);
 
 	if (clone_flags & CLONE_VM) {
 		mmget(oldmm);

mm/Kconfig
@@ -1537,4 +1537,16 @@ config THP_CONTROL
 	help
 	  This provides interface to control thp policy.
 
+config MM_COUNTER_ATOMIC
+	bool "use atomic for mm counters when single-thread process"
+	depends on PREEMPT_NONE && !PREEMPT_DYNAMIC
+	depends on SMP
+	default n
+	help
+	  For single-thread processes, mm counter uses atomic counter
+	  operations, which reduces the memory consumption and performance
+	  regression caused by using percpu counter. For multiple-thread
+	  processes, atomic counter is switched to percpu counter mode to
+	  reduce the error margin.
+
 endmenu