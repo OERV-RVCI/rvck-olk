OERV-RVCI/rvck-olk

commit a1ad9b2ed459a3e3a4a2de50e03f53036fc7ec96

interference: Add mutex interference track support
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ICAOAT

--------------------------------

Add mutex interference track support

Signed-off-by: Pu Lehui <pulehui@huawei.com>

include/linux/cgroup.h
@@ -863,6 +863,7 @@ void cgroup_move_task_to_root(struct task_struct *tsk);
 
 enum ifs_types {
 	IFS_SPINLOCK,
+	IFS_MUTEX,
 	IFS_SMT,
 	IFS_RUNDELAY,
 	IFS_WAKELAT,

kernel/locking/mutex.c
@@ -24,6 +24,7 @@
 #include <linux/sched/rt.h>
 #include <linux/sched/wake_q.h>
 #include <linux/sched/debug.h>
+#include <linux/cgroup.h>
 #include <linux/export.h>
 #include <linux/spinlock.h>
 #include <linux/interrupt.h>
@@ -572,6 +573,7 @@ __mutex_lock_common(struct mutex *lock, unsigned int state, unsigned int subclas
 {
 	struct mutex_waiter waiter;
 	struct ww_mutex *ww;
+	u64 ifs_clock;
 	int ret;
 
 	if (!use_ww_ctx)
@@ -647,6 +649,7 @@ __mutex_lock_common(struct mutex *lock, unsigned int state, unsigned int subclas
 
 	set_current_state(state);
 	trace_contention_begin(lock, LCB_F_MUTEX);
+	cgroup_ifs_enter_lock(&ifs_clock);
 	for (;;) {
 		bool first;
 
@@ -719,6 +722,7 @@ acquired:
 skip_wait:
 	/* got the lock - cleanup and rejoice! */
 	lock_acquired(&lock->dep_map, ip);
+	cgroup_ifs_leave_lock(ifs_clock, IFS_MUTEX);
 	trace_contention_end(lock, 0);
 
 	if (ww_ctx)
@@ -732,6 +736,7 @@ err:
 	__set_current_state(TASK_RUNNING);
 	__mutex_remove_waiter(lock, &waiter);
 err_early_kill:
+	cgroup_ifs_leave_lock(ifs_clock, IFS_MUTEX);
 	trace_contention_end(lock, ret);
 	raw_spin_unlock(&lock->wait_lock);
 	debug_mutex_free_waiter(&waiter);

kernel/cgroup/ifs.c
@@ -201,6 +201,9 @@ static const char *ifs_type_name(int type)
 	case IFS_SPINLOCK:
 		name = "spinlock";
 		break;
+	case IFS_MUTEX:
+		name = "mutex";
+		break;
 	case IFS_SMT:
 		name = "smt";
 		break;

commit 60fb74c8564334b664cace1bcd7bb4f8e1ed7101

interference: Fix uninitialized ifs_clock compiling issue
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/7429

--------------------------------

When compiling with clang, it will occur the warning:

     623 |         if (__mutex_trylock(lock)) {
         |             ^~~~~~~~~~~~~~~~~~~~~
   kernel/locking/mutex.c:725:24: note: uninitialized use occurs here
     725 |         cgroup_ifs_leave_lock(ifs_clock, IFS_MUTEX);
         |                               ^~~~~~~~~
   kernel/locking/mutex.c:623:2: note: remove the 'if' if its condition is always false
     623 |         if (__mutex_trylock(lock)) {
         |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~
     624 |                 if (ww_ctx)
         |                 ~~~~~~~~~~~
     625 |                         __ww_mutex_check_waiters(lock, ww_ctx);
         |                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     626 |
     627 |                 goto skip_wait;
         |                 ~~~~~~~~~~~~~~~
     628 |         }
         |         ~
   kernel/locking/mutex.c:576:15: note: initialize the variable 'ifs_clock' to silence this warning
     576 |         u64 ifs_clock;
         |                      ^
         |                       = 0
   2 warnings generated.

Let's fix it by zero initialize ifs_clock.

Fixes: a1ad9b2ed459 ("interference: Add mutex interference track support")
Signed-off-by: Pu Lehui <pulehui@huawei.com>

include/linux/cgroup.h
@@ -1000,6 +1000,9 @@ static inline void cgroup_ifs_leave_lock(u64 clock, enum ifs_types t)
 	if (!cgroup_ifs_enabled())
 		return;
 
+	if (unlikely(!clock))
+		return;
+
 	ifs = current_ifs();
 	if (ifs) {
 		ifsc = this_cpu_ptr(ifs->pcpu);

kernel/locking/mutex.c
@@ -573,7 +573,7 @@ __mutex_lock_common(struct mutex *lock, unsigned int state, unsigned int subclas
 {
 	struct mutex_waiter waiter;
 	struct ww_mutex *ww;
-	u64 ifs_clock;
+	u64 ifs_clock = 0;
 	int ret;
 
 	if (!use_ww_ctx)