OERV-RVCI/rvck-olk

commit baa290f9d3551a1bd56283294ea83793fe2c1be4

pps: fix poll support
stable inclusion
from stable-v6.6.102
commit 7ed42b79118d56028621f5b6298dc84065745ae1
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8365

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=7ed42b79118d56028621f5b6298dc84065745ae1

--------------------------------

[ Upstream commit 12c409aa1ec2592280a2ddcc66ff8f3c7f7bb171 ]

Because pps_cdev_poll() returns unconditionally EPOLLIN,
a user space program that calls select/poll get always an immediate data
ready-to-read response. As a result the intended use to wait until next
data becomes ready does not work.

User space snippet:

    struct pollfd pollfd = {
      .fd = open("/dev/pps0", O_RDONLY),
      .events = POLLIN|POLLERR,
      .revents = 0 };
    while(1) {
      poll(&pollfd, 1, 2000/*ms*/); // returns immediate, but should wait
      if(revents & EPOLLIN) { // always true
        struct pps_fdata fdata;
        memset(&fdata, 0, sizeof(memdata));
        ioctl(PPS_FETCH, &fdata); // currently fetches data at max speed
      }
    }

Lets remember the last fetch event counter and compare this value
in pps_cdev_poll() with most recent event counter
and return 0 if they are equal.

Signed-off-by: Denis OSTERLAND-HEIM <denis.osterland@diehl.com>
Co-developed-by: Rodolfo Giometti <giometti@enneenne.com>
Signed-off-by: Rodolfo Giometti <giometti@enneenne.com>
Fixes: eae9d2ba0cfc ("LinuxPPS: core support")
Link: https://lore.kernel.org/all/f6bed779-6d59-4f0f-8a59-b6312bd83b4e@enneenne.com/
Acked-by: Rodolfo Giometti <giometti@enneenne.com>
Link: https://lore.kernel.org/r/c3c50ad1eb19ef553eca8a57c17f4c006413ab70.camel@gmail.com
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
Signed-off-by: Sasha Levin <sashal@kernel.org>
(cherry picked from commit 7ed42b79118d56028621f5b6298dc84065745ae1)
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

include/linux/pps_kernel.h
@@ -52,6 +52,7 @@ struct pps_device {
 	int current_mode;			/* PPS mode at event time */
 
 	unsigned int last_ev;			/* last PPS event id */
+	unsigned int last_fetched_ev;		/* last fetched PPS event id */
 	wait_queue_head_t queue;		/* PPS event queue */
 
 	unsigned int id;			/* PPS source unique ID */

drivers/pps/pps.c
@@ -41,6 +41,9 @@ static __poll_t pps_cdev_poll(struct file *file, poll_table *wait)
 
 	poll_wait(file, &pps->queue, wait);
 
+	if (pps->last_fetched_ev == pps->last_ev)
+		return 0;
+
 	return EPOLLIN | EPOLLRDNORM;
 }
 
@@ -186,8 +189,10 @@ static long pps_cdev_ioctl(struct file *file,
 		if (err)
 			return err;
 
-		/* Return the fetched timestamp */
+		/* Return the fetched timestamp and save last fetched event  */
 		spin_lock_irq(&pps->lock);
+
+		pps->last_fetched_ev = pps->last_ev;
 
 		fdata.info.assert_sequence = pps->assert_sequence;
 		fdata.info.clear_sequence = pps->clear_sequence;
@@ -272,8 +277,10 @@ static long pps_cdev_compat_ioctl(struct file *file,
 		if (err)
 			return err;
 
-		/* Return the fetched timestamp */
+		/* Return the fetched timestamp and save last fetched event  */
 		spin_lock_irq(&pps->lock);
+
+		pps->last_fetched_ev = pps->last_ev;
 
 		compat.info.assert_sequence = pps->assert_sequence;
 		compat.info.clear_sequence = pps->clear_sequence;

commit 538516da0260809993b2588aa06ac3bf94632694

pps: Fix kabi breakage of struct pps_device for last_fetched_ev
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8365

--------------------------------

Fix kabi breakage in struct pps_device for last_fetched_ev.
and this kabi broken is ptp_clock_register<-struct ptp_clock<-.

Log:
struct pps_device {
        struct pps_source_info     info;                 /*     0    96 */

        /* XXX last struct has 1 hole */

        /* --- cacheline 1 boundary (64 bytes) was 32 bytes ago --- */
        struct pps_kparams         params;               /*    96    40 */
        /* --- cacheline 2 boundary (128 bytes) was 8 bytes ago --- */
        __u32                      assert_sequence;      /*   136     4 */
        __u32                      clear_sequence;       /*   140     4 */
        struct pps_ktime           assert_tu;            /*   144    16 */
        struct pps_ktime           clear_tu;             /*   160    16 */
        int                        current_mode;         /*   176     4 */
        unsigned int               last_ev;              /*   180     4 */
        wait_queue_head_t          queue;                /*   184    24 */
        /* --- cacheline 3 boundary (192 bytes) was 16 bytes ago --- */
        unsigned int               id;                   /*   208     4 */

        /* XXX 4 bytes hole, try to pack */
<- Fill in it.

Fixes: baa290f9d355 ("pps: fix poll support")
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

include/linux/pps_kernel.h
@@ -52,10 +52,10 @@ struct pps_device {
 	int current_mode;			/* PPS mode at event time */
 
 	unsigned int last_ev;			/* last PPS event id */
-	unsigned int last_fetched_ev;		/* last fetched PPS event id */
 	wait_queue_head_t queue;		/* PPS event queue */
 
 	unsigned int id;			/* PPS source unique ID */
+	KABI_FILL_HOLE(unsigned int last_fetched_ev)    /* last fetched PPS event id */
 	void const *lookup_cookie;		/* For pps_lookup_dev() only */
 	struct cdev cdev;
 	struct device *dev;