OERV-RVCI/rvck-olk

commit 31981b6fdbd5dc6a723a4233f9a13555f1b2e6ba

sched/core: Remove ifdeffery for saved_state
stable inclusion
from stable-v6.6.102
commit 8afa818c77330fcb269c77540b268f199d84ee8e
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8365

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=8afa818c77330fcb269c77540b268f199d84ee8e

--------------------------------

commit fbaa6a181a4b1886cbf4214abdf9a2df68471510 upstream.

In preparation for freezer to also use saved_state, remove the
CONFIG_PREEMPT_RT compilation guard around saved_state.

On the arm64 platform I tested which did not have CONFIG_PREEMPT_RT,
there was no statistically significant deviation by applying this patch.

Test methodology:

perf bench sched message -g 40 -l 40

Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>
Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
Signed-off-by: Ingo Molnar <mingo@kernel.org>
Signed-off-by: Chen Ridong <chenridong@huawei.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
(cherry picked from commit 8afa818c77330fcb269c77540b268f199d84ee8e)
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

include/linux/sched.h
@@ -798,10 +798,8 @@ struct task_struct {
 #endif
 	unsigned int			__state;
 
-#ifdef CONFIG_PREEMPT_RT
 	/* saved state for "spinlock sleepers" */
 	unsigned int			saved_state;
-#endif
 
 	/*
 	 * This begins the randomizable portion of task_struct. Only

kernel/sched/core.c
@@ -2249,17 +2249,15 @@ int __task_state_match(struct task_struct *p, unsigned int state)
 	if (READ_ONCE(p->__state) & state)
 		return 1;
 
-#ifdef CONFIG_PREEMPT_RT
 	if (READ_ONCE(p->saved_state) & state)
 		return -1;
-#endif
+
 	return 0;
 }
 
 static __always_inline
 int task_state_match(struct task_struct *p, unsigned int state)
 {
-#ifdef CONFIG_PREEMPT_RT
 	int match;
 
 	/*
@@ -2271,9 +2269,6 @@ int task_state_match(struct task_struct *p, unsigned int state)
 	raw_spin_unlock_irq(&p->pi_lock);
 
 	return match;
-#else
-	return __task_state_match(p, state);
-#endif
 }
 
 /*
@@ -4094,7 +4089,6 @@ bool ttwu_state_match(struct task_struct *p, unsigned int state, int *success)
 
 	*success = !!(match = __task_state_match(p, state));
 
-#ifdef CONFIG_PREEMPT_RT
 	/*
 	 * Saved state preserves the task state across blocking on
 	 * an RT lock.  If the state matches, set p::saved_state to
@@ -4110,7 +4104,7 @@ bool ttwu_state_match(struct task_struct *p, unsigned int state, int *success)
 	 */
 	if (match < 0)
 		p->saved_state = TASK_RUNNING;
-#endif
+
 	return match > 0;
 }
 

commit f1b45e14fb61f425f143968f2ad78991f54d98a4

sched: Fix kabi breakage of struct task_struct for saved_state
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8365

--------------------------------

Fix kabi breakage in struct task_struct for saved_state.

Fixes: 31981b6fdbd5 ("sched/core: Remove ifdeffery for saved_state")
Signed-off-by: Wentao Guan <guanwentao@uniontech.com>

include/linux/sched.h
@@ -798,9 +798,6 @@ struct task_struct {
 #endif
 	unsigned int			__state;
 
-	/* saved state for "spinlock sleepers" */
-	unsigned int			saved_state;
-
 	/*
 	 * This begins the randomizable portion of task_struct. Only
 	 * scheduling-critical items should be added above here.
@@ -1627,8 +1624,10 @@ struct task_struct {
 #else
 	KABI_RESERVE(1)
 #endif
+
 	KABI_RESERVE(2)
-	KABI_RESERVE(3)
+	/* saved state for "spinlock sleepers" */
+	KABI_USE2(3, unsigned kabi_reserved_int, unsigned int saved_state)
 	KABI_RESERVE(4)
 	KABI_RESERVE(5)
 	KABI_RESERVE(6)