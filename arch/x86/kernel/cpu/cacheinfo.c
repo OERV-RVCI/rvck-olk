openeuler-mirror/kernel

commit 5cdbea24bbe11e376be69a9088c8cfb484c5bc92

x86/cpu: Get LLC ID for Hygon family 18h model 5h
hygon inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8U3G1
CVE: NA

---------------------------

Add support to calculate LLC ID from the number of threads sharing
the cache for Hygon family 18h model 5h processor.

Signed-off-by: Pu Wen <puwen@hygon.cn>

arch/x86/kernel/cpu/cacheinfo.c
@@ -708,11 +708,30 @@ void cacheinfo_hygon_init_llc_id(struct cpuinfo_x86 *c, int cpu)
 	if (!cpuid_edx(0x80000006))
 		return;
 
-	/*
-	 * LLC is at the core complex level.
-	 * Core complex ID is ApicId[3] for these processors.
-	 */
-	per_cpu(cpu_llc_id, cpu) = c->apicid >> 3;
+	if (c->x86_model < 0x5) {
+		/*
+		 * LLC is at the core complex level.
+		 * Core complex ID is ApicId[3] for these processors.
+		 */
+		per_cpu(cpu_llc_id, cpu) = c->apicid >> 3;
+	} else {
+		/*
+		 * LLC ID is calculated from the number of threads
+		 * sharing the cache.
+		 */
+		u32 eax, ebx, ecx, edx, num_sharing_cache = 0;
+		u32 llc_index = find_num_cache_leaves(c) - 1;
+
+		cpuid_count(0x8000001d, llc_index, &eax, &ebx, &ecx, &edx);
+		if (eax)
+			num_sharing_cache = ((eax >> 14) & 0xfff) + 1;
+
+		if (num_sharing_cache) {
+			int bits = get_count_order(num_sharing_cache);
+
+			per_cpu(cpu_llc_id, cpu) = c->apicid >> bits;
+		}
+	}
 }
 
 void init_amd_cacheinfo(struct cpuinfo_x86 *c)

OERV-RVCI/rvck-olk

commit f60f1d088ff1c4991b3f98b0e55a896621bebacd

x86/cpu: Get CPU LLC ID from CPUID 0x8000001d in guest for Hygon processors
hygon inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8428
CVE: N/A

Reference: https://gitee.com/anolis/cloud-kernel/commit/182442452f386d5615bf5e9ae8383ad88d0f59c7

------------------------------------------

ANBZ: #27296

In virtualization environments, LLC ID should be obtained directly
from CPUID 0x8000001D instead of deriving it from fixed APIC ID
bit offsets. This approach ensures accurate LLC ID in virtual
machines.

Hygon-SIG: commit none hygon anolis: x86/cpu: Get CPU LLC ID from
CPUID 0x8000001d in guest for Hygon processors

Hygon-SIG: commit 182442452f38 anolis anolis: x86/cpu: Get CPU LLC ID
 from CPUID 0x8000001d in guest for Hygon processors
Backport to fix CPU LLC ID in guest for Hygon processor.

Fixes: 5cdbea24bbe1 ("x86/cpu: Get LLC ID for Hygon family 18h model 5h")
Fixes: f374fe74a80e ("x86/cpu: Get LLC ID for Hygon family 18h model 10h")
Signed-off-by: Wenhui Fan <fanwh@hygon.cn>
Cc: hygon-arch@list.openanolis.cn
Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/6060
[ Aichun Shi: amend commit log ]
Signed-off-by: Aichun Shi <shiaichun@hygon.cn>

arch/x86/kernel/cpu/cacheinfo.c
@@ -708,8 +708,9 @@ void cacheinfo_hygon_init_llc_id(struct cpuinfo_x86 *c, int cpu)
 	if (!cpuid_edx(0x80000006))
 		return;
 
-	if (c->x86_model < 0x5 ||
-	   (c->x86_model >= 0x10 && c->x86_model <= 0x1f)) {
+	if ((c->x86_model < 0x5 ||
+	     (c->x86_model >= 0x10 && c->x86_model <= 0x1f)) &&
+	    !boot_cpu_has(X86_FEATURE_HYPERVISOR)) {
 		/*
 		 * LLC is at the core complex level.
 		 * Core complex ID is ApicId[3] for these processors.