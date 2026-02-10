OERV-RVCI/rvck-olk

commit f1edc5dcf26f382f2b30bd35a504caf5035a9dc8

drivers/ub/sentry: add sentry_remote_reporter module
drivers inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/ID45WQ?from=project-issue

--------------------------------

1. report panic event to main node by URMA or UVB
2. report reboot event to main node by URMA or UVB

Signed-off-by: guodashun <guodashun1@huawei.com>
Signed-off-by: shixuantong <shixuantong1@huawei.com>

drivers/ub/sentry/smh_common_type.h
@@ -46,7 +46,11 @@ enum {
 enum sentry_msg_helper_msg_type {
     SMH_MESSAGE_POWER_OFF,
     SMH_MESSAGE_OOM,
+    SMH_MESSAGE_PANIC,
+    SMH_MESSAGE_KERNEL_REBOOT,
     SMH_MESSAGE_UB_MEM_ERR,
+    SMH_MESSAGE_PANIC_ACK,
+    SMH_MESSAGE_KERNEL_REBOOT_ACK,
     SMH_MESSAGE_UNKNOWN,
 };
 
@@ -65,6 +69,10 @@ struct sentry_msg_helper_msg {
 			int reason;
 		} oom_info;
 		struct {
+			uint32_t cna;
+			char eid[EID_MAX_LEN];
+		} remote_info;
+		struct {
 			uint64_t pa;
 			int mem_type;
 			int fault_with_kill;
@@ -74,10 +82,98 @@ struct sentry_msg_helper_msg {
     unsigned long res;
 };
 
+// urma communication interface
+extern int urma_send(const char *buf, size_t len, const char *dst_eid, int die_index);
+extern int urma_recv(char **buf_arr, size_t len);
+
+// UVB communication interface
+extern int uvb_send(const char *str, uint32_t dst_cna, bool is_sync);
+
 extern uint32_t g_local_cna;
 #define UVB_SENDER_ID_SYSSENTRY_INDEX (g_local_cna)
 #define UVB_SENDER_ID_SYSSENTRY (UBIOS_USER_ID_RICH_OS | UVB_SENDER_ID_SYSSENTRY_INDEX)
 #define UVB_RECEIVER_ID_SYSSENTRY(cna) (UBIOS_USER_ID_UB_DEVICE | (cna))
+
+/*
+ * str format type_cna_eid or type_cna_eid_res. type_cna_eid_res is ack msg.
+ * */
+static inline int convert_str_to_smh_msg(const char *str,
+	struct sentry_msg_helper_msg *smh_msg,
+	uint32_t *random_id)
+{
+    int n;
+    int ret = 0;
+    char input_copy[URMA_SEND_DATA_MAX_LEN];
+
+    n = sscanf(str, "%d_%s", (int *)&smh_msg->type, input_copy);
+    if (n != 2) {
+		pr_warn("Invalid msg str format and parse type failed! str is [%s].\n", str);
+		return -EINVAL;
+    }
+
+	switch (smh_msg->type) {
+	case SMH_MESSAGE_PANIC:
+	case SMH_MESSAGE_KERNEL_REBOOT:
+	    // eid length is EID_MAX_LEN - 1
+		if (!(sscanf(input_copy, "%u_%39[^_]_%llu_%u%n",
+			 &smh_msg->helper_msg_info.remote_info.cna,
+			 smh_msg->helper_msg_info.remote_info.eid,
+			 &smh_msg->timeout_time,
+			 random_id,
+			 &n) == 4) || strlen(input_copy) != n) {
+			pr_warn("Invalid msg str format and parse cna/eid failed! str is [%s].\n", str);
+			ret = -1;
+		}
+		break;
+	case SMH_MESSAGE_PANIC_ACK:
+	case SMH_MESSAGE_KERNEL_REBOOT_ACK:
+		if (!(sscanf(input_copy, "%u_%39[^_]_%lu%n",
+			 &smh_msg->helper_msg_info.remote_info.cna,
+			 smh_msg->helper_msg_info.remote_info.eid,
+			 &smh_msg->res,
+			 &n) == 3) || strlen(input_copy) != n) {
+			pr_warn("Invalid msg str format and parse cna/eid failed! str is [%s].\n", str);
+			ret = -1;
+		}
+		break;
+	default:
+	    pr_warn("Invalid event type!\n");
+	    ret = -1;
+    }
+    return ret;
+}
+
+static inline void free_char_array(char **array_ptr, int array_len)
+{
+    if (array_ptr) {
+		for (int i = 0; i < array_len; i++) {
+			if (array_ptr[i]) {
+				kfree(array_ptr[i]);
+				array_ptr[i] = NULL;
+			}
+		}
+		kfree(array_ptr);
+		array_ptr = NULL;
+    }
+}
+
+/*
+ * Return 1 when buf is valid ipv4 format, return 0 when buf is invalid ipv4 format
+ * or any error occurs.
+ *
+*/
+static inline int is_valid_ipv4(const char *buf)
+{
+    int ret;
+    __be32 addr;
+
+    if (buf == NULL) {
+	return 0;
+    }
+
+    ret = in4_pton(buf, strnlen(buf, EID_MAX_LEN), (u8 *)&addr, '\0', NULL);
+    return ret;
+}
 
 static inline int sentry_create_proc_file(const char *name, struct proc_dir_entry *parent,
 					  const struct proc_ops *proc_ops)

arch/arm64/configs/openeuler_defconfig
@@ -8390,6 +8390,7 @@ CONFIG_FWCTL_UB=m
 
 # UB sentry
 CONFIG_UB_SENTRY=m
+CONFIG_UB_SENTRY_REMOTE=m
 # end of UB sentry
 
 #

drivers/ub/sentry/Makefile
@@ -6,5 +6,7 @@ obj-$(CONFIG_UB_SENTRY) += sentry_msg_helper.o
 obj-$(CONFIG_UB_SENTRY) += sentry_reporter.o
 obj-$(CONFIG_UB_SENTRY_REMOTE) += sentry_urma_comm.o
 obj-$(CONFIG_UB_SENTRY_REMOTE) += sentry_uvb_comm.o
+obj-$(CONFIG_UB_SENTRY_REMOTE) += sentry_remote_reporter.o
 
 sentry_msg_helper-y := smh_core.o smh_message.o
+sentry_remote_reporter-y := sentry_remote_server.o sentry_remote_client.o

drivers/ub/sentry/sentry_remote_reporter.h
@@ -1,0 +1,73 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Description: Header File for sentry module
+ * Author: sxt1001
+ * Create: 2025-03-18
+ */
+
+#ifndef SENTRY_REMOTE_REPORTER_H
+#define SENTRY_REMOTE_REPORTER_H
+
+#include <ub/urma/ubcore_api.h>
+#include <ub/urma/ubcore_uapi.h>
+#include <linux/spinlock.h>
+#include <linux/atomic.h>
+#include <linux/types.h>
+
+#include "smh_common_type.h"
+
+extern void set_urma_panic_mode(bool is_panic);
+
+// sentry uvb global variable
+extern uint32_t g_server_cna_array[MAX_NODE_NUM];
+extern int g_server_cna_valid_num;
+extern int cis_ubios_remote_msg_cb(struct cis_message *cis_msg);
+
+// sentry urma global variable and functions
+extern bool g_is_created_ubcore_resource;
+extern int str_to_eid(const char *buf, union ubcore_eid *eid);
+extern int match_index_by_remote_ub_eid(union ubcore_eid remote_id, int *node_index, int *die_index);
+extern int sentry_create_urma_resource(union ubcore_eid eid[], int eid_num);
+extern int process_multi_eid_string(char *eid_buf, char eid_array[][EID_MAX_LEN],
+    union ubcore_eid eid_tmp[], const char *sepstr, int eid_max_num);
+
+enum SENTRY_REMOTE_COMM_TYPE {
+    COMM_TYPE_URMA,
+    COMM_TYPE_UVB,
+    COMM_TYPE_UNKNOWN
+};
+
+struct child_thread_process_data {
+    struct sentry_msg_helper_msg *msg;
+    enum SENTRY_REMOTE_COMM_TYPE comm_type;
+    uint32_t random_id;
+};
+
+struct node_msg_info {
+	uint32_t random_id;
+	uint64_t start_send_time;
+	uint64_t msgid;
+};
+
+struct sentry_remote_context {
+	struct node_msg_info node_msg_info_list[MAX_NODE_NUM];
+	struct sentry_msg_helper_msg remote_event_ack_msg_buf;
+	atomic_t remote_event_ack_received;
+	atomic_t remote_event_ack_done;
+	struct task_struct *urma_receiver_thread;
+};
+
+extern spinlock_t sentry_buf_lock;
+extern struct sentry_remote_context sentry_remote_ctx;
+
+int sentry_panic_reporter_init(void);
+void sentry_panic_reporter_exit(void);
+
+int send_msg_to_userspace_and_ack(struct sentry_msg_helper_msg *msg, enum SENTRY_REMOTE_COMM_TYPE comm_type,
+    uint32_t random_id, enum sentry_msg_helper_msg_type ack_type);
+
+void write_ack_msg_buf(const struct sentry_msg_helper_msg *msg, enum SENTRY_REMOTE_COMM_TYPE comm_type);
+int create_kthread_to_process_msg(const char *event_msg, enum SENTRY_REMOTE_COMM_TYPE comm_type);
+enum sentry_msg_helper_msg_type get_ack_type(enum sentry_msg_helper_msg_type event_type);
+#endif

drivers/ub/sentry/sentry_remote_server.c
@@ -1,0 +1,445 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ *
+ * Description: Server module, used for reporting panic or reboot msg to the
+ *              userspace and forward ack msg to the client
+ * Author: sxt1001
+ * Create: 2025-03-18
+ */
+
+#include <acpi/button.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/mm.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/delay.h>
+#include <linux/kallsyms.h>
+#include <linux/string.h>
+
+#include "smh_message.h"
+#include "sentry_remote_reporter.h"
+
+#undef pr_fmt
+#define pr_fmt(fmt) "[sentry][remote server]: " fmt
+
+struct sentry_remote_context sentry_remote_ctx;
+DEFINE_SPINLOCK(sentry_buf_lock);
+
+static DEFINE_MUTEX(sentry_msg_info_mutex);
+
+/**
+ * send_msg_to_userspace - Send message to userspace with proper tracking
+ * @msg: Message to send
+ * @comm_type: Communication type (URMA or UVB)
+ * @random_id: Random identifier for message tracking
+ *
+ * Return: 0 on success, negative error code on failure
+ *
+ * This function sends a message to userspace and tracks it using node message
+ * info for acknowledgment handling.
+ */
+int send_msg_to_userspace(struct sentry_msg_helper_msg *msg,
+			  enum SENTRY_REMOTE_COMM_TYPE comm_type, uint32_t random_id)
+{
+	int ret;
+	int node_idx = -1;
+	int die_index = -1;
+	union ubcore_eid dst_ubcore_eid;
+
+	pr_info("send %s message to userspace\n",
+		comm_type == COMM_TYPE_URMA ? "urma" : "uvb");
+
+	if (comm_type == COMM_TYPE_URMA) {
+		if (str_to_eid(msg->helper_msg_info.remote_info.eid, &dst_ubcore_eid) < 0) {
+			pr_err("send_msg_to_userspace: invalid dst eid [%s]\n",
+			       msg->helper_msg_info.remote_info.eid);
+			return -EINVAL;
+		}
+		match_index_by_remote_ub_eid(dst_ubcore_eid, &node_idx, &die_index);
+	} else if (comm_type == COMM_TYPE_UVB) {
+		int i;
+
+		for (i = 0; i < g_server_cna_valid_num; i++) {
+			if (msg->helper_msg_info.remote_info.cna == g_server_cna_array[i]) {
+				node_idx = i;
+				break;
+			}
+		}
+	}
+
+	if (node_idx < 0) {
+		pr_err("Invalid cna: %u or eid: %s of msg, stop to send to userspace\n",
+		       msg->helper_msg_info.remote_info.cna,
+		       msg->helper_msg_info.remote_info.eid);
+		return -EINVAL;
+	}
+
+	mutex_lock(&sentry_msg_info_mutex);
+	if (sentry_remote_ctx.node_msg_info_list[node_idx].random_id != random_id) {
+		pr_info("Get new message from cna: %u, eid: %s\n",
+			msg->helper_msg_info.remote_info.cna,
+			msg->helper_msg_info.remote_info.eid);
+		sentry_remote_ctx.node_msg_info_list[node_idx].start_send_time = ktime_get_ns();
+		sentry_remote_ctx.node_msg_info_list[node_idx].msgid = smh_get_new_msg_id();
+		sentry_remote_ctx.node_msg_info_list[node_idx].random_id = random_id;
+	}
+	msg->start_send_time = sentry_remote_ctx.node_msg_info_list[node_idx].start_send_time;
+	msg->msgid = sentry_remote_ctx.node_msg_info_list[node_idx].msgid;
+	mutex_unlock(&sentry_msg_info_mutex);
+
+	ret = smh_message_send(msg, true);
+	return ret;
+}
+
+/**
+ * send_msg_to_userspace_and_ack - Send message to userspace and wait for acknowledgment
+ * @msg: Message to send
+ * @comm_type: Communication type (URMA or UVB)
+ * @random_id: Random identifier for message tracking
+ * @ack_type: Type of acknowledgment expected
+ *
+ * Return: 0 on success, negative error code on failure
+ *
+ * This function sends a message to userspace, waits for acknowledgment, and
+ * sends acknowledgment back to the remote node.
+ */
+int send_msg_to_userspace_and_ack(struct sentry_msg_helper_msg *msg,
+				  enum SENTRY_REMOTE_COMM_TYPE comm_type,
+				  uint32_t random_id, enum sentry_msg_helper_msg_type ack_type)
+{
+	int ret;
+	int times = msg->timeout_time / MILLISECONDS_OF_EACH_MDELAY;
+	int i, j;
+
+	ret = send_msg_to_userspace(msg, comm_type, random_id);
+	if (ret) {
+		pr_err("Failed to send remote message to userspace\n");
+		return ret;
+	}
+
+	/* Wait for acknowledgment from userspace */
+	for (i = 0; i < times; i++) {
+		uint64_t cur_time = ktime_get_ns();
+
+		ret = smh_message_get_ack(msg);
+		if (!ret) {
+			int sleep_time = MILLISECONDS_OF_EACH_MDELAY -
+					(int)((ktime_get_ns() - cur_time) / NSEC_PER_MSEC);
+			if (sleep_time > 0)
+				msleep_interruptible(sleep_time);
+			continue;
+		}
+
+		/* Get acknowledgment success, send acknowledgment message */
+		char send_ack[URMA_SEND_DATA_MAX_LEN];
+
+		ret = snprintf(send_ack, URMA_SEND_DATA_MAX_LEN, "%d_%u_%s_%lu",
+			       ack_type,
+			       msg->helper_msg_info.remote_info.cna,
+			       msg->helper_msg_info.remote_info.eid,
+			       msg->res);
+		if ((size_t)ret >= URMA_SEND_DATA_MAX_LEN) {
+			pr_err("msg str size exceeds the max value\n");
+			return -EINVAL;
+		}
+
+		pr_info("Start to send %s ack msg to %u\n",
+			comm_type == COMM_TYPE_URMA ? "urma" : "uvb",
+			msg->helper_msg_info.remote_info.cna);
+
+		if (comm_type == COMM_TYPE_URMA) {
+			/* Retry URMA acknowledgment sending */
+			for (j = 0; j < URMA_ACK_RETRY_NUM; j++) {
+				ret = urma_send(send_ack, sizeof(send_ack),
+						msg->helper_msg_info.remote_info.eid, -1);
+				if (ret == COMM_PARM_NOT_SET)
+					break;
+				msleep_interruptible(MILLISECONDS_OF_EACH_MDELAY);
+			}
+		} else {
+			/* UVB is a reliable protocol, no need to resend */
+			ret = uvb_send(send_ack, msg->helper_msg_info.remote_info.cna, false);
+		}
+
+		if (ret <= 0) {
+			pr_warn("Failed to send %s ack message to client (cna:%u, eid:%s)\n",
+				comm_type == COMM_TYPE_URMA ? "urma" : "uvb",
+				msg->helper_msg_info.remote_info.cna,
+				msg->helper_msg_info.remote_info.eid);
+			return -EFAULT;
+		}
+		return 0;
+	}
+
+	return -ETIMEDOUT;
+}
+
+/**
+ * get_ack_type - Get acknowledgment type for given event type
+ * @event_type: Event type to get acknowledgment for
+ *
+ * Return: Corresponding acknowledgment type
+ */
+enum sentry_msg_helper_msg_type get_ack_type(enum sentry_msg_helper_msg_type event_type)
+{
+	enum sentry_msg_helper_msg_type ack_type;
+
+	switch (event_type) {
+	case SMH_MESSAGE_PANIC:
+		ack_type = SMH_MESSAGE_PANIC_ACK;
+		break;
+	case SMH_MESSAGE_KERNEL_REBOOT:
+		ack_type = SMH_MESSAGE_KERNEL_REBOOT_ACK;
+		break;
+	default:
+		pr_warn("Invalid event type!\n");
+		ack_type = SMH_MESSAGE_UNKNOWN;
+	}
+
+	return ack_type;
+}
+
+/**
+ * process_remote_event_msg - Process remote event message in kernel thread context
+ * @data: Pointer to child_thread_process_data structure containing message info
+ *
+ * Return: 0 on success, negative error code on failure
+ */
+static int process_remote_event_msg(void *data)
+{
+	int ret;
+	enum sentry_msg_helper_msg_type ack_type;
+	struct child_thread_process_data *child_data = data;
+
+	try_module_get(THIS_MODULE);
+
+	ack_type = get_ack_type(child_data->msg->type);
+	if (ack_type == SMH_MESSAGE_UNKNOWN) {
+		ret = -EINVAL;
+		goto cleanup_child;
+	}
+
+	ret = send_msg_to_userspace_and_ack(child_data->msg, child_data->comm_type,
+					    child_data->random_id, ack_type);
+
+cleanup_child:
+	kfree(child_data->msg);
+	kfree(child_data);
+	module_put(THIS_MODULE);
+	return ret;
+}
+
+/**
+ * write_ack_msg_buf - Write acknowledgment message to shared buffer
+ * @msg: Acknowledgment message
+ * @comm_type: Communication type (URMA or UVB)
+ *
+ * This function writes an acknowledgment message to a shared buffer for
+ * inter-process communication, ensuring thread-safe access.
+ */
+void write_ack_msg_buf(const struct sentry_msg_helper_msg *msg,
+		       enum SENTRY_REMOTE_COMM_TYPE comm_type)
+{
+	if (atomic_inc_return(&sentry_remote_ctx.remote_event_ack_received) == 1) {
+		pr_info("Receive ack message from %s: [%d_%u_%s_%lu]. Start to update buf\n",
+			comm_type == COMM_TYPE_URMA ? "URMA" : "UVB",
+			msg->type,
+			msg->helper_msg_info.remote_info.cna,
+			msg->helper_msg_info.remote_info.eid,
+			msg->res);
+
+		spin_lock(&sentry_buf_lock);
+		memcpy(&sentry_remote_ctx.remote_event_ack_msg_buf, msg,
+		       sizeof(sentry_remote_ctx.remote_event_ack_msg_buf));
+		spin_unlock(&sentry_buf_lock);
+		atomic_set(&sentry_remote_ctx.remote_event_ack_done, 1);
+	}
+}
+
+/**
+ * create_kthread_to_process_msg - Create kernel thread to process incoming message
+ * @event_msg: Raw event message string
+ * @comm_type: Communication type (URMA or UVB)
+ *
+ * Return: 0 on success, negative error code on failure
+ *
+ * This function creates a kernel thread to process incoming remote messages,
+ * handling both panic/reboot events and acknowledgment messages.
+ */
+int create_kthread_to_process_msg(const char *event_msg,
+				  enum SENTRY_REMOTE_COMM_TYPE comm_type)
+{
+	int ret;
+	struct sentry_msg_helper_msg msg;
+	uint32_t random_id;
+	struct child_thread_process_data *child_data;
+	struct task_struct *child_thread;
+
+	ret = convert_str_to_smh_msg(event_msg, &msg, &random_id);
+	if (ret) {
+		pr_err("convert %s data to smh msg failed. msg [%s]\n",
+		       comm_type == COMM_TYPE_URMA ? "urma" : "uvb", event_msg);
+		return -EINVAL;
+	}
+
+	if (msg.type != SMH_MESSAGE_PANIC && msg.type != SMH_MESSAGE_KERNEL_REBOOT) {
+		/* Write acknowledgment message to shared memory */
+		write_ack_msg_buf(&msg, comm_type);
+		return 0;
+	}
+
+	child_data = kzalloc(sizeof(*child_data), GFP_KERNEL);
+	if (!child_data) {
+		pr_err("Failed to allocate memory for child_data\n");
+		return -ENOMEM;
+	}
+
+	child_data->msg = kzalloc(sizeof(*child_data->msg), GFP_KERNEL);
+	if (!child_data->msg) {
+		kfree(child_data);
+		pr_err("Failed to allocate memory for child_data->msg\n");
+		return -ENOMEM;
+	}
+
+	/* Update child thread data */
+	memcpy(child_data->msg, &msg, sizeof(*child_data->msg));
+	child_data->random_id = random_id;
+	child_data->comm_type = comm_type;
+
+	child_thread = kthread_run(process_remote_event_msg, child_data,
+				   "sentry_msg_thread_%s_%u",
+				   comm_type == COMM_TYPE_URMA ? "urma" : "uvb",
+				   random_id);
+	if (IS_ERR(child_thread)) {
+		kfree(child_data->msg);
+		kfree(child_data);
+		pr_err("Failed to create child thread\n");
+		return PTR_ERR(child_thread);
+	}
+
+	return 0;
+}
+
+/**
+ * process_urma_data - Process URMA data in kernel thread
+ * @data: Thread data (unused)
+ *
+ * Return: 0 on success, negative error code on failure
+ *
+ * This function runs in a kernel thread to receive and process URMA messages,
+ * creating separate threads for message processing.
+ */
+static int process_urma_data(void *data)
+{
+	int ret = 0;
+	int recv_msg_nodes = 0;
+	char **msg_str;
+	int i;
+
+	msg_str = kcalloc(MAX_NODE_NUM * MAX_DIE_NUM, sizeof(*msg_str), GFP_KERNEL);
+	if (!msg_str) {
+		pr_err("Failed to allocate memory for msg_str!\n");
+		return -ENOMEM;
+	}
+
+	for (i = 0; i < MAX_NODE_NUM * MAX_DIE_NUM; i++) {
+		msg_str[i] = kzalloc(URMA_SEND_DATA_MAX_LEN, GFP_KERNEL);
+		if (!msg_str[i]) {
+			pr_err("Failed to allocate memory for msg_str[%d]!\n", i);
+			ret = -ENOMEM;
+			goto free_msg;
+		}
+	}
+
+	while (!kthread_should_stop()) {
+		/* Listen for URMA messages */
+		recv_msg_nodes = urma_recv(msg_str, URMA_SEND_DATA_MAX_LEN);
+		if (recv_msg_nodes <= 0) {
+			/*
+			 * Prevent processes from entering the D state if reboot event
+			 * occurs on the current node
+			 */
+			msleep_interruptible(MILLISECONDS_OF_EACH_MDELAY);
+			continue;
+		}
+
+		pr_info("urma messages are received, the number of nodes that are successfully received is %d\n",
+			recv_msg_nodes);
+
+		for (i = 0; i < recv_msg_nodes; i++) {
+			if (strcmp(HEARTBEAT, msg_str[i]) == 0 ||
+			    strcmp(HEARTBEAT_ACK, msg_str[i]) == 0)
+				continue;
+
+			ret = create_kthread_to_process_msg(msg_str[i], COMM_TYPE_URMA);
+			if (ret == -ENOMEM)
+				goto free_msg;
+		}
+
+		/*
+		 * Prevent processes from entering the D state if reboot event
+		 * occurs on the current node
+		 */
+		msleep_interruptible(MILLISECONDS_OF_EACH_MDELAY);
+	}
+
+free_msg:
+	free_char_array(msg_str, MAX_NODE_NUM);
+
+	pr_info("Urma receiver thread stopped!\n");
+	return ret;
+}
+
+/**
+ * cis_ubios_remote_msg_cb - UVB remote message callback
+ * @cis_msg: CIS message from UVB
+ *
+ * Return: 0 on success, negative error code on failure
+ *
+ * This function serves as the callback for UVB remote messages,
+ * processing incoming messages through the appropriate mechanism.
+ */
+int cis_ubios_remote_msg_cb(struct cis_message *cis_msg)
+{
+	int ret;
+
+	pr_info("uvb get msg: [%s]\n", (char *)cis_msg->input);
+	ret = create_kthread_to_process_msg((char *)cis_msg->input, COMM_TYPE_UVB);
+	return ret;
+}
+
+/**
+ * sentry_panic_reporter_init - Initialize sentry panic reporter module
+ *
+ * Return: 0 on success, negative error code on failure
+ */
+int sentry_panic_reporter_init(void)
+{
+	atomic_set(&sentry_remote_ctx.remote_event_ack_received, 0);
+	atomic_set(&sentry_remote_ctx.remote_event_ack_done, 0);
+
+	sentry_remote_ctx.urma_receiver_thread = kthread_run(process_urma_data, NULL, "sentry_urma_kthread");
+	if (IS_ERR(sentry_remote_ctx.urma_receiver_thread)) {
+		pr_err("Failed to create kernel urma receiver thread.\n");
+		return PTR_ERR(sentry_remote_ctx.urma_receiver_thread);
+	}
+
+	pr_info("Create kernel urma receiver thread success.\n");
+	return 0;
+}
+
+/**
+ * sentry_panic_reporter_exit - Cleanup sentry panic reporter module
+ */
+void sentry_panic_reporter_exit(void)
+{
+	if (sentry_remote_ctx.urma_receiver_thread) {
+		kthread_stop(sentry_remote_ctx.urma_receiver_thread);
+		sentry_remote_ctx.urma_receiver_thread = NULL;
+		pr_info("Kernel urma receiver thread stopped\n");
+	}
+}

commit e2e062ae72ea88db5db1ee2dd830e93e30330bf9

drivers/ub/sentry: fix potential buffer overflow in convert_str_to_smh_msg()
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8483

--------------------------------

The UVB and URMA communication is insecure. Attackers may send malicious msg to
sentry.

Fixes: f1edc5dcf26f (drivers/ub/sentry: add sentry_remote_reporter module)
Signed-off-by: shixuantong <shixuantong1@huawei.com>
Signed-off-by: guodashun <guodashun1@huawei.com>

drivers/ub/sentry/smh_common_type.h
@@ -15,6 +15,7 @@
 #include <linux/ktime.h>
 #include <linux/inet.h>
 #include <linux/proc_fs.h>
+#include <linux/string.h>
 
 #define SMH_TYPE ('}')
 #define MAX_DIE_NUM 2
@@ -103,7 +104,12 @@ static inline int convert_str_to_smh_msg(const char *str,
 {
     int n;
     int ret = 0;
-    char input_copy[URMA_SEND_DATA_MAX_LEN];
+    char input_copy[URMA_SEND_DATA_MAX_LEN + 1] = {0};
+
+    if (!str || strnlen(str, URMA_SEND_DATA_MAX_LEN + 1) > URMA_SEND_DATA_MAX_LEN) {
+		pr_err("%s: Invalid string, failed to convert it to smh_msg.\n", __func__);
+		return -EINVAL;
+    }
 
     n = sscanf(str, "%d_%s", (int *)&smh_msg->type, input_copy);
     if (n != 2) {