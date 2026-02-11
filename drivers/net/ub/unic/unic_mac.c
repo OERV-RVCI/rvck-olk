OERV-RVCI/rvck-olk

commit ebdbff9694c44e432a83a5646f4bd21f3a629c62

net: unic: Add support for MAC statistics
drivers inclusion
category: Feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID41MP
CVE: NA

------------------------------------------------------------------

This patch adds support for MAC statistics in the UNIC driver.
It introduces new functions to get and display MAC statistics,
as well as integrate these statistics with the existing ethtool
operations. The patch also ensures that the new statistics are
properly counted and displayed.

Key changes include:
1. Adding new functions to get and display MAC statistics.
2. Integrating the new statistics with the ethtool operations.
3. Ensuring proper counting and display of the new statistics.

This enhancement provides a comprehensive view of MAC statistics,
allowing for better monitoring and troubleshooting of network
performance in UNIC driver.

Signed-off-by: Yaoyao Tu <tuyaoyao@huawei.com>
Signed-off-by: Fengyan Mu <mufengyan@huawei.com>

drivers/net/ub/unic/unic_netdev.c
@@ -208,6 +208,7 @@ void unic_link_status_change(struct net_device *netdev, bool linkup)
 			netif_tx_wake_all_queues(netdev);
 			netif_carrier_on(netdev);
 			unic_clear_fec_stats(unic_dev);
+			ubase_clear_eth_port_stats(unic_dev->comdev.adev);
 		}
 	} else {
 		netif_carrier_off(netdev);
@@ -333,6 +334,7 @@ int unic_net_open_no_link_change(struct net_device *netdev)
 		netif_tx_wake_all_queues(netdev);
 		netif_carrier_on(netdev);
 		unic_clear_fec_stats(unic_dev);
+		ubase_clear_eth_port_stats(unic_dev->comdev.adev);
 	}
 
 	return 0;

drivers/ub/ubase/ubase_dev.c
@@ -300,6 +300,21 @@ static void ubase_uninit_aux_devices(struct ubase_dev *udev)
 	mutex_destroy(&udev->priv.uadev_lock);
 }
 
+static void ubase_update_stats_for_all(struct ubase_dev *udev)
+{
+	int ret;
+
+	if (ubase_dev_unic_supported(udev) &&
+	    ubase_dev_eth_mac_supported(udev) &&
+	    ubase_dev_mac_stats_supported(udev)) {
+		ret = ubase_update_eth_stats_trylock(udev);
+		if (ret)
+			ubase_err(udev,
+				  "failed to update stats for eth, ret = %d.\n",
+				  ret);
+	}
+}
+
 static void ubase_cancel_period_service_task(struct ubase_dev *udev)
 {
 	if (udev->period_service_task.service_task.work.func)
@@ -322,7 +337,6 @@ static int ubase_enable_period_service_task(struct ubase_dev *udev)
 static void ubase_period_service_task(struct work_struct *work)
 {
 #define UBASE_STATS_TIMER_INTERVAL		(300000 / (UBASE_PERIOD_100MS))
-#define UBASE_QUERY_SL_TIMER_INTERVAL		(1000 / (UBASE_PERIOD_100MS))
 
 	struct ubase_delay_work *ubase_work =
 		container_of(work, struct ubase_delay_work, service_task.work);
@@ -333,6 +347,10 @@ static void ubase_period_service_task(struct work_struct *work)
 		ubase_enable_period_service_task(udev);
 		return;
 	}
+
+	if (test_bit(UBASE_STATE_INITED_B, &udev->state_bits) &&
+	    !(udev->serv_proc_cnt % UBASE_STATS_TIMER_INTERVAL))
+		ubase_update_stats_for_all(udev);
 
 	udev->serv_proc_cnt++;
 	ubase_enable_period_service_task(udev);
@@ -1329,6 +1347,15 @@ struct ubase_adev_qos *ubase_get_adev_qos(struct auxiliary_device *adev)
 	return &udev->qos;
 }
 EXPORT_SYMBOL(ubase_get_adev_qos);
+
+bool ubase_adev_mac_stats_supported(struct auxiliary_device *adev)
+{
+	if (!adev)
+		return false;
+
+	return ubase_dev_mac_stats_supported(__ubase_get_udev_by_adev(adev));
+}
+EXPORT_SYMBOL(ubase_adev_mac_stats_supported);
 
 static void ubase_activate_notify(struct ubase_dev *udev,
 				  struct auxiliary_device *adev, bool activate)

drivers/net/ub/unic/debugfs/unic_debugfs.c
@@ -99,16 +99,21 @@ static const struct unic_dbg_cap_bit_info {
 	bool (*get_bit)(struct unic_dev *dev);
 } unic_cap_bits[] = {
 	{"\tsupport_ubl: %u\n", &unic_dev_ubl_supported},
+	{"\tsupport_pfc: %u\n", &unic_dev_pfc_supported},
 	{"\tsupport_ets: %u\n", &unic_dev_ets_supported},
 	{"\tsupport_fec: %u\n", &unic_dev_fec_supported},
+	{"\tsupport_pause: %u\n", &unic_dev_pause_supported},
+	{"\tsupport_eth: %u\n", &unic_dev_eth_supported},
 	{"\tsupport_tc_speed_limit: %u\n", &unic_dev_tc_speed_limit_supported},
 	{"\tsupport_tx_csum_offload: %u\n", &unic_dev_tx_csum_offload_supported},
 	{"\tsupport_rx_csum_offload: %u\n", &unic_dev_rx_csum_offload_supported},
 	{"\tsupport_app_lb: %u\n", &unic_dev_app_lb_supported},
+	{"\tsupport_external_lb: %u\n", &unic_dev_external_lb_supported},
 	{"\tsupport_serial_serdes_lb: %u\n", &unic_dev_serial_serdes_lb_supported},
-	{"\tsupport_parallel_serdes_lb: %u\n", unic_dev_parallel_serdes_lb_supported},
+	{"\tsupport_parallel_serdes_lb: %u\n", &unic_dev_parallel_serdes_lb_supported},
 	{"\tsupport_fec_stats: %u\n", &unic_dev_fec_stats_supported},
 	{"\tsupport_cfg_mac: %u\n", &unic_dev_cfg_mac_supported},
+	{"\tsupport_cfg_vlan_filter: %u\n", &unic_dev_cfg_vlan_filter_supported},
 };
 
 static void unic_dbg_dump_caps_bits(struct unic_dev *unic_dev,

drivers/net/ub/unic/unic_dev.h
@@ -331,6 +331,11 @@ static inline bool unic_dev_pause_supported(struct unic_dev *unic_dev)
 	return unic_get_cap_bit(unic_dev, UNIC_SUPPORT_PAUSE_B);
 }
 
+static inline bool unic_dev_eth_supported(struct unic_dev *unic_dev)
+{
+	return unic_get_cap_bit(unic_dev, UNIC_SUPPORT_ETH_B);
+}
+
 static inline bool unic_dev_serial_serdes_lb_supported(struct unic_dev *unic_dev)
 {
 	return unic_get_cap_bit(unic_dev, UNIC_SUPPORT_SERIAL_SERDES_LB_B);

drivers/net/ub/unic/unic_stats.c
@@ -6,6 +6,7 @@
 
 #include <linux/phy.h>
 #include <ub/ubase/ubase_comm_cmd.h>
+#include <ub/ubase/ubase_comm_dev.h>
 #include <ub/ubase/ubase_comm_stats.h>
 
 #include "unic.h"
@@ -451,6 +452,36 @@ static u64 *unic_get_queues_stats(struct unic_dev *unic_dev,
 	return data;
 }
 
+static void unic_get_mac_stats(struct unic_dev *unic_dev, u64 *data)
+{
+	struct auxiliary_device *adev = unic_dev->comdev.adev;
+	struct ubase_caps *caps = ubase_get_dev_caps(adev);
+	const struct unic_mac_stats_desc *stats_desc;
+	struct ubase_eth_mac_stats mac_stats = {0};
+	u32 stats_num = caps->mac_stats_num;
+	u32 i, stats_desc_num;
+	u8 *stats;
+	int ret;
+
+	if (unic_dev_ubl_supported(unic_dev))
+		return;
+
+	stats_desc = unic_eth_stats_str;
+	stats_desc_num = ARRAY_SIZE(unic_eth_stats_str);
+	ret = ubase_get_eth_port_stats(adev, &mac_stats);
+	if (ret)
+		return;
+
+	stats = (u8 *)&mac_stats;
+	for (i = 0; i < stats_desc_num; i++) {
+		if (stats_desc[i].stats_num > stats_num)
+			continue;
+
+		*data = UNIC_STATS_READ(stats, stats_desc[i].offset);
+		data++;
+	}
+}
+
 void unic_get_stats(struct net_device *netdev,
 		    struct ethtool_stats *stats, u64 *data)
 {
@@ -470,6 +501,7 @@ void unic_get_stats(struct net_device *netdev,
 	p = unic_get_queues_stats(unic_dev, unic_rq_stats_str,
 				  ARRAY_SIZE(unic_rq_stats_str),
 				  UNIC_QUEUE_TYPE_RQ, p);
+	unic_get_mac_stats(unic_dev, p);
 }
 
 static u8 *unic_get_strings(u8 *data, const char *prefix, u32 num,
@@ -510,18 +542,72 @@ static u8 *unic_get_queues_strings(struct unic_dev *unic_dev, u8 *data)
 	return data;
 }
 
+static void
+unic_get_mac_strings(struct unic_dev *unic_dev, u8 *data,
+		     const struct unic_mac_stats_desc *strs, u32 size)
+{
+	struct auxiliary_device *adev = unic_dev->comdev.adev;
+	struct ubase_caps *caps = ubase_get_dev_caps(adev);
+	u32 stats_num = caps->mac_stats_num;
+	u32 i;
+
+	if (!ubase_adev_mac_stats_supported(adev))
+		return;
+
+	for (i = 0; i < size; i++) {
+		if (strs[i].stats_num > stats_num)
+			continue;
+
+		(void)snprintf(data, ETH_GSTRING_LEN, "%s", strs[i].desc);
+		data += ETH_GSTRING_LEN;
+	}
+}
+
 void unic_get_stats_strings(struct net_device *netdev, u32 stringset, u8 *data)
 {
 	struct unic_dev *unic_dev = netdev_priv(netdev);
+	char unic_test_strs[][ETH_GSTRING_LEN] = {
+		"App      Loopback test ",
+		"Serdes   serial Loopback test",
+		"Serdes   parallel Loopback test",
+		"External Loopback test",
+	};
 	u8 *p = data;
 
 	switch (stringset) {
 	case ETH_SS_STATS:
 		p = unic_get_queues_strings(unic_dev, p);
+		if (unic_dev_ubl_supported(unic_dev))
+			break;
+
+		unic_get_mac_strings(unic_dev, p, unic_eth_stats_str,
+				     ARRAY_SIZE(unic_eth_stats_str));
+		break;
+	case ETH_SS_TEST:
+		memcpy(data, unic_test_strs, sizeof(unic_test_strs));
 		break;
 	default:
 		break;
 	}
+}
+
+static int unic_get_mac_count(struct unic_dev *unic_dev,
+			      const struct unic_mac_stats_desc strs[], u32 size)
+{
+	struct auxiliary_device *adev = unic_dev->comdev.adev;
+	struct ubase_caps *caps = ubase_get_dev_caps(adev);
+	u32 stats_num = caps->mac_stats_num;
+	int count = 0;
+	u32 i;
+
+	if (!ubase_adev_mac_stats_supported(adev))
+		return 0;
+
+	for (i = 0; i < size; i++)
+		if (strs[i].stats_num <= stats_num)
+			count++;
+
+	return count;
 }
 
 int unic_get_sset_count(struct net_device *netdev, int stringset)
@@ -534,6 +620,12 @@ int unic_get_sset_count(struct net_device *netdev, int stringset)
 	case ETH_SS_STATS:
 		count = ARRAY_SIZE(unic_sq_stats_str) * channel_num;
 		count += ARRAY_SIZE(unic_rq_stats_str) * channel_num;
+		if (unic_dev_ubl_supported(unic_dev))
+			break;
+
+		count += unic_get_mac_count(unic_dev, unic_eth_stats_str,
+					    ARRAY_SIZE(unic_eth_stats_str));
+		break;
 	case ETH_SS_TEST:
 		count = unic_get_selftest_count(unic_dev);
 		break;
@@ -557,6 +649,32 @@ static void unic_get_fec_stats_total(struct unic_dev *unic_dev, u8 stats_flags,
 		fec_stats->corrected_bits.total = total->corr_bits;
 }
 
+static void unic_get_fec_stats_lanes(struct unic_dev *unic_dev, u8 stats_flags,
+				     struct ethtool_fec_stats *fec_stats)
+{
+	u8 lane_num = unic_dev->stats.fec_stats.lane_num;
+	u8 i;
+
+	if (lane_num == 0 || lane_num > UNIC_FEC_STATS_MAX_LANE) {
+		unic_err(unic_dev,
+			 "fec stats lane number is invalid, lane_num = %u.\n",
+			 lane_num);
+		return;
+	}
+
+	for (i = 0; i < lane_num; i++) {
+		if (stats_flags & UNIC_FEC_CORR_BLOCKS)
+			fec_stats->corrected_blocks.lanes[i] =
+				unic_dev->stats.fec_stats.lane[i].corr_blocks;
+		if (stats_flags & UNIC_FEC_UNCORR_BLOCKS)
+			fec_stats->uncorrectable_blocks.lanes[i] =
+				unic_dev->stats.fec_stats.lane[i].uncorr_blocks;
+		if (stats_flags & UNIC_FEC_CORR_BITS)
+			fec_stats->corrected_bits.lanes[i] =
+				unic_dev->stats.fec_stats.lane[i].corr_bits;
+	}
+}
+
 static void unic_get_ubl_fec_stats(struct unic_dev *unic_dev,
 				   struct ethtool_fec_stats *fec_stats)
 {
@@ -567,6 +685,26 @@ static void unic_get_ubl_fec_stats(struct unic_dev *unic_dev,
 	case ETHTOOL_FEC_RS:
 		stats_flags = UNIC_FEC_UNCORR_BLOCKS | UNIC_FEC_CORR_BITS;
 		unic_get_fec_stats_total(unic_dev, stats_flags, fec_stats);
+		break;
+	default:
+		unic_err(unic_dev,
+			 "fec stats is not supported in mode(0x%x).\n",
+			 fec_mode);
+		break;
+	}
+}
+
+static void unic_get_eth_fec_stats(struct unic_dev *unic_dev,
+				   struct ethtool_fec_stats *fec_stats)
+{
+	u32 fec_mode = unic_dev->hw.mac.fec_mode;
+	u8 stats_flags = 0;
+
+	switch (fec_mode) {
+	case ETHTOOL_FEC_RS:
+		stats_flags = UNIC_FEC_CORR_BLOCKS | UNIC_FEC_UNCORR_BLOCKS;
+		unic_get_fec_stats_total(unic_dev, stats_flags, fec_stats);
+		unic_get_fec_stats_lanes(unic_dev, UNIC_FEC_CORR_BITS, fec_stats);
 		break;
 	default:
 		unic_err(unic_dev,
@@ -590,4 +728,6 @@ void unic_get_fec_stats(struct net_device *ndev,
 
 	if (unic_dev_ubl_supported(unic_dev))
 		unic_get_ubl_fec_stats(unic_dev, fec_stats);
+	else
+		unic_get_eth_fec_stats(unic_dev, fec_stats);
 }

drivers/ub/ubase/ubase_stats.c
@@ -52,6 +52,33 @@ out_send_cmd_fail:
 }
 
 /**
+ * ubase_clear_eth_port_stats() - clear eth port stats
+ * @adev: auxiliary device
+ *
+ * The function is used to clear eth port stats.
+ *
+ * Context: Process context. Takes and releases <lock>, BH-safe. Sleep.
+ * Return: 0 on success, negative error code otherwise
+ */
+void ubase_clear_eth_port_stats(struct auxiliary_device *adev)
+{
+	struct ubase_eth_mac_stats *eth_stats;
+	struct ubase_dev *udev;
+
+	if (!adev)
+		return;
+
+	udev = __ubase_get_udev_by_adev(adev);
+	eth_stats = &udev->stats.eth_stats;
+	if (ubase_dev_eth_mac_supported(udev)) {
+		mutex_lock(&udev->stats.stats_lock);
+		memset(eth_stats, 0, sizeof(*eth_stats));
+		mutex_unlock(&udev->stats.stats_lock);
+	}
+}
+EXPORT_SYMBOL(ubase_clear_eth_port_stats);
+
+/**
  * ubase_get_ub_port_stats() - get ub port stats
  * @adev: auxiliary device
  * @port_id: port id
@@ -132,4 +159,20 @@ void ubase_update_activate_stats(struct ubase_dev *udev, bool activate,
 	record->stats[idx].result = result;
 
 	mutex_unlock(&record->lock);
+}
+
+int ubase_update_eth_stats_trylock(struct ubase_dev *udev)
+{
+	struct ubase_eth_mac_stats *eth_stats = &udev->stats.eth_stats;
+	u32 stats_num = sizeof(*eth_stats) / sizeof(u64);
+	int ret;
+
+	if (!mutex_trylock(&udev->stats.stats_lock))
+		return 0;
+
+	ret = ubase_update_mac_stats(udev, udev->caps.dev_caps.io_port_logic_id,
+				     (u64 *)eth_stats, stats_num, true);
+	mutex_unlock(&udev->stats.stats_lock);
+
+	return ret;
 }

drivers/ub/ubase/ubase_stats.h
@@ -17,6 +17,7 @@ struct ubase_query_mac_stats_cmd {
 
 int __ubase_get_eth_port_stats(struct ubase_dev *udev,
 			       struct ubase_eth_mac_stats *data);
+int ubase_update_eth_stats_trylock(struct ubase_dev *udev);
 void ubase_update_activate_stats(struct ubase_dev *udev, bool activate,
 				 int result);
 

include/ub/ubase/ubase_comm_dev.h
@@ -356,6 +356,7 @@ struct ubase_bus_eid {
 bool ubase_adev_ubl_supported(struct auxiliary_device *adev);
 bool ubase_adev_ctrlq_supported(struct auxiliary_device *adev);
 bool ubase_adev_eth_mac_supported(struct auxiliary_device *adev);
+bool ubase_adev_mac_stats_supported(struct auxiliary_device *aux_dev);
 bool ubase_adev_prealloc_supported(struct auxiliary_device *aux_dev);
 struct ubase_resource_space *ubase_get_io_base(struct auxiliary_device *adev);
 struct ubase_resource_space *ubase_get_mem_base(struct auxiliary_device *adev);

commit 7d4cc817f0d13ee593139161484742153daf302d

ub: ubase: Fix MAC is_pfc sync, optimize list ops and code style
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8514
CVE: NA

----------------------------------------------------------

Fix issue where is_pfc flag does not take effect when
modifying an already configured MAC address.
When modifying a MAC address that has already
been configured, the unic_cfg_mac_address() interface
sets the is_pfc flag, but this flag is not forwarded to the
IMP by the periodic task via the command queue (cmdq).
To resolve this, we now proactively send a cmdq command
to delete the MAC in such scenarios, ensuring that the is_pfc
flag takes effect when modifying an existing unicast MAC.

Change list addition to head insertion to prevent entry
overflow and ensure MAC synchronization to hardware
UMV table. Replaced list_add_tail with list_add so that
each time a MAC is modified, it is inserted at the head
of the list. This prevents the situation where, when the
list is full, MAC entries cannot be added to the hardware
UMV table by the periodic task due to overflow.

Improve linked list traversal for better code standards.
Updated the linked list node traversal logic by
using list_for_each_entry() instead of list_for_each_entry_safe().
This change maintains correct functionality while aligning
with common coding practices, improving readability
and standardization.

Enhance code readability and maintainability with style
adjustments and comments. Adjusted coding style in
several sections to improve overall readability.
Added clear comments in complex logic areas such as
unic_sync_mac_list() to aid future developers in
understanding the execution flow.

Currently the log prints in __ubase_event_register and
__ubase_event_unregister are too similar to distinguish easily,
so the logging for these two interfaces has been modified to
differentiate them clearly. The redundant ubase_dbg log in
ubase_aeq_int_handler has been removed, as the aeq events
are already recorded by trace. Additionally, trace logging for
event_cause has been added after it is obtained in the
ubase_reg_event_handler interface, to facilitate issue diagnosis
in misc interrupt scenarios.

Fixes: ebdbff9694c4 ("net: unic: Add support for MAC statistics")

drivers/net/ub/unic/unic_mac.c
@@ -14,61 +14,6 @@
 #include "unic_cmd.h"
 #include "unic_mac.h"
 
-int unic_cfg_mac_address(struct unic_dev *unic_dev, u8 *mac_addr)
-{
-	struct unic_comm_addr_node *new_node, *old_node;
-	struct unic_vport *vport = &unic_dev->vport;
-	u8 *old_mac = unic_dev->hw.mac.mac_addr;
-	u8 unic_addr[UNIC_ADDR_LEN] = {0};
-	struct list_head *list;
-
-	list = &vport->addr_tbl.uc_mac_list;
-	spin_lock_bh(&vport->addr_tbl.mac_list_lock);
-	new_node = unic_comm_find_addr_node(list, mac_addr,
-					    UNIC_COMM_ADDR_NO_MASK);
-	if (new_node) {
-		if (new_node->state == UNIC_COMM_ADDR_TO_DEL)
-			new_node->state = UNIC_COMM_ADDR_ACTIVE;
-
-		/* make sure the new addr is in the list head, avoid dev
-		 * addr may be not re-added into mac table for the umv space
-		 * limitation after reset.
-		 */
-		new_node->is_pfc = 1;
-		list_move(&new_node->node, list);
-	} else {
-		new_node = kzalloc(sizeof(*new_node), GFP_ATOMIC);
-		if (!new_node) {
-			spin_unlock_bh(&vport->addr_tbl.mac_list_lock);
-			return -ENOMEM;
-		}
-
-		new_node->state = UNIC_COMM_ADDR_TO_ADD;
-		new_node->is_pfc = 1;
-		ether_addr_copy(new_node->mac_addr, mac_addr);
-		list_add_tail(&new_node->node, list);
-	}
-
-	ether_addr_copy(unic_addr, old_mac);
-	old_node = unic_comm_find_addr_node(list, unic_addr,
-					    UNIC_COMM_ADDR_NO_MASK);
-	if (old_node) {
-		if (old_node->state == UNIC_COMM_ADDR_TO_ADD) {
-			list_del(&old_node->node);
-			kfree(old_node);
-		} else {
-			old_node->state = UNIC_COMM_ADDR_TO_DEL;
-			old_node->is_pfc = 0;
-		}
-	}
-
-	set_bit(UNIC_VPORT_STATE_MAC_TBL_CHANGE, &vport->state);
-	ether_addr_copy(unic_dev->hw.mac.mac_addr, mac_addr);
-	spin_unlock_bh(&vport->addr_tbl.mac_list_lock);
-
-	return 0;
-}
-
 static int unic_add_mac_addr_common(struct unic_vport *vport, u8 *mac_addr,
 				    enum unic_mac_addr_type mac_type,
 				    u8 is_pfc)
@@ -145,19 +90,92 @@ static int unic_del_mac_addr_common(struct unic_vport *vport, u8 *mac_addr,
 	return ret;
 }
 
+int unic_cfg_mac_address(struct unic_dev *unic_dev, u8 *mac_addr)
+{
+	struct unic_comm_addr_node *new_node, *old_node;
+	struct unic_vport *vport = &unic_dev->vport;
+	u8 *old_mac = unic_dev->hw.mac.mac_addr;
+	u8 unic_addr[UNIC_ADDR_LEN] = {0};
+	struct list_head *list;
+	int ret;
+
+	list = &vport->addr_tbl.uc_mac_list;
+	spin_lock_bh(&vport->addr_tbl.mac_list_lock);
+	new_node = unic_comm_find_addr_node(list, mac_addr,
+					    UNIC_COMM_ADDR_NO_MASK);
+	if (new_node) {
+		if (new_node->state != UNIC_COMM_ADDR_TO_ADD) {
+			ret = unic_del_mac_addr_common(vport, mac_addr,
+						       UNIC_MAC_ADDR_UC, 0);
+			if (ret) {
+				spin_unlock_bh(&vport->addr_tbl.mac_list_lock);
+				return ret;
+			}
+		}
+
+		/* make sure the new addr is in the list head, avoid dev
+		 * addr may be not re-added into mac table for the umv space
+		 * limitation after reset.
+		 */
+		new_node->is_pfc = 1;
+		new_node->state = UNIC_COMM_ADDR_TO_ADD;
+		list_move(&new_node->node, list);
+	} else {
+		new_node = kzalloc(sizeof(*new_node), GFP_ATOMIC);
+		if (!new_node) {
+			spin_unlock_bh(&vport->addr_tbl.mac_list_lock);
+			return -ENOMEM;
+		}
+
+		new_node->state = UNIC_COMM_ADDR_TO_ADD;
+		new_node->is_pfc = 1;
+		ether_addr_copy(new_node->mac_addr, mac_addr);
+		list_add(&new_node->node, list);
+	}
+
+	ether_addr_copy(unic_addr, old_mac);
+	old_node = unic_comm_find_addr_node(list, unic_addr,
+					    UNIC_COMM_ADDR_NO_MASK);
+	if (old_node) {
+		if (old_node->state == UNIC_COMM_ADDR_TO_ADD) {
+			list_del(&old_node->node);
+			kfree(old_node);
+		} else {
+			old_node->state = UNIC_COMM_ADDR_TO_DEL;
+			old_node->is_pfc = 0;
+		}
+	}
+
+	set_bit(UNIC_VPORT_STATE_MAC_TBL_CHANGE, &vport->state);
+	ether_addr_copy(unic_dev->hw.mac.mac_addr, mac_addr);
+	spin_unlock_bh(&vport->addr_tbl.mac_list_lock);
+
+	return 0;
+}
+
 static void unic_sync_mac_list(struct unic_vport *vport, struct list_head *list,
 			       enum unic_mac_addr_type mac_type)
 {
-	struct unic_comm_addr_node *mac_node, *tmp;
+	struct unic_comm_addr_node *mac_node;
 	int ret;
 
-	list_for_each_entry_safe(mac_node, tmp, list, node) {
+	list_for_each_entry(mac_node, list, node) {
 		ret = unic_add_mac_addr_common(vport, mac_node->mac_addr, mac_type,
 					       mac_node->is_pfc);
 		if (!ret) {
 			mac_node->state = UNIC_COMM_ADDR_ACTIVE;
 		} else {
 			set_bit(UNIC_VPORT_STATE_MAC_TBL_CHANGE, &vport->state);
+
+			/* UC MAC addition: -EEXIST indicates the address already
+			 * exists in hardware. This entry cannot be added, but we
+			 * can continue to try adding the next UC MAC in the list
+			 * without breaking the loop.
+			 * MC MAC addition: -ENOSPC indicates the hardware table
+			 * is full for new entries. However, we can still continue
+			 * to process MC MACs that may already exist in hardware
+			 * without breaking.
+			 */
 			if ((mac_type == UNIC_MAC_ADDR_UC && ret != -EEXIST) ||
 			    (mac_type == UNIC_MAC_ADDR_MC && ret != -ENOSPC))
 				break;
@@ -377,11 +395,9 @@ int unic_init_mac_addr(struct unic_dev *unic_dev)
 		unic_comm_format_mac_addr(format_mac, unic_addr);
 		dev_warn(unic_dev->comdev.adev->dev.parent,
 			 "using random MAC address %s.\n", format_mac);
-	} else if (!ether_addr_equal(netdev->dev_addr, unic_addr)) {
+	} else {
 		dev_addr_set(netdev, unic_addr);
 		ether_addr_copy(netdev->perm_addr, unic_addr);
-	} else {
-		return 0;
 	}
 
 	if (!unic_dev_cfg_mac_supported(unic_dev)) {

drivers/net/ub/unic/unic_netdev.c
@@ -548,8 +548,8 @@ static int unic_set_mac_address(struct net_device *netdev, void *addr)
 		return -EADDRNOTAVAIL;
 	}
 
-	unic_comm_format_mac_addr(format_mac, mac_addr->sa_data);
 	if (ether_addr_equal(netdev->dev_addr, mac_addr->sa_data)) {
+		unic_comm_format_mac_addr(format_mac, mac_addr->sa_data);
 		unic_info(unic_dev, "already using mac(%s).\n", format_mac);
 		return 0;
 	}

drivers/ub/ubase/ubase_dev.c
@@ -320,6 +320,16 @@ static void ubase_update_stats_for_all(struct ubase_dev *udev)
 	}
 }
 
+static void ubase_report_rate_limited_log_cnt(struct ubase_dev *udev)
+{
+	if (udev->log_rs.aeq_event_type_exceed_max_cnt) {
+		ubase_warn(udev,
+			   "rate limited log: aeq_event_type_exceed_max_cnt = %llu.\n",
+			   udev->log_rs.aeq_event_type_exceed_max_cnt);
+		udev->log_rs.aeq_event_type_exceed_max_cnt = 0;
+	}
+}
+
 static void ubase_cancel_period_service_task(struct ubase_dev *udev)
 {
 	if (udev->period_service_task.service_task.work.func)
@@ -342,6 +352,7 @@ static int ubase_enable_period_service_task(struct ubase_dev *udev)
 static void ubase_period_service_task(struct work_struct *work)
 {
 #define UBASE_STATS_TIMER_INTERVAL		(300000 / (UBASE_PERIOD_100MS))
+#define UBASE_RL_LOG_TIMER_INTERVAL		(180000 / (UBASE_PERIOD_100MS))
 #define UBASE_CTRLQ_TIMER_INTERVAL		(3000 / (UBASE_PERIOD_100MS))
 
 	struct ubase_delay_work *ubase_work =
@@ -361,6 +372,10 @@ static void ubase_period_service_task(struct work_struct *work)
 	if (test_bit(UBASE_STATE_INITED_B, &udev->state_bits) &&
 	    !(udev->serv_proc_cnt % UBASE_CTRLQ_TIMER_INTERVAL))
 		ubase_ctrlq_clean_service_task(udev);
+
+	if (test_bit(UBASE_STATE_INITED_B, &udev->state_bits) &&
+	    !(udev->serv_proc_cnt % UBASE_RL_LOG_TIMER_INTERVAL))
+		ubase_report_rate_limited_log_cnt(udev);
 
 	udev->serv_proc_cnt++;
 	ubase_enable_period_service_task(udev);

drivers/ub/ubase/ubase_dev.h
@@ -353,6 +353,7 @@ struct ubase_prealloc_mem_info {
 struct ubase_log_rs {
 	struct ratelimit_state rs;
 	u16 ctrlq_other_seq_invalid_log_cnt;
+	u64 aeq_event_type_exceed_max_cnt;
 };
 
 enum ubase_node_type {

drivers/ub/ubase/ubase_eq.c
@@ -231,6 +231,7 @@ static int ubase_reg_event_handler(struct ubase_dev *udev)
 	ubase_enable_misc_vector(udev, false);
 
 	event_cause = ubase_check_event_cause(udev);
+	trace_ubase_misc_event_cause(udev->dev, event_cause);
 	if (test_bit(UBASE_ASYNC_EVENT_CRQ_B, &event_cause))
 		ubase_crq_task_schedule(udev);
 
@@ -352,8 +353,8 @@ static void ubase_aeq_event_handler(struct ubase_dev *udev,
 	u8 idx;
 
 	if (event_type >= UBASE_EVENT_TYPE_MAX) {
-		ubase_err(udev, "event type wrong, event_type = %u.\n",
-			  event_type);
+		ubase_err_rl(udev, udev->log_rs.aeq_event_type_exceed_max_cnt,
+			     "event type wrong, event_type = %u.\n", event_type);
 		return;
 	}
 
@@ -458,8 +459,6 @@ static irqreturn_t ubase_misc_int_handler(int irq, void *data)
 static irqreturn_t ubase_aeq_int_handler(int irq, void *data)
 {
 	struct ubase_dev *udev = (struct ubase_dev *)data;
-
-	ubase_dbg(udev, "ubase enter aeq handler.\n");
 
 	return IRQ_RETVAL(ubase_async_event_handler(udev));
 }
@@ -1010,13 +1009,14 @@ static int __ubase_event_register(struct ubase_dev *udev,
 	int ret;
 
 	if (cb->drv_type >= UBASE_DRV_MAX) {
-		ubase_err(udev, "unsupported drv_type(%u).\n", cb->drv_type);
+		ubase_err(udev, "register unsupported drv_type(%u).\n",
+			  cb->drv_type);
 		return -EINVAL;
 	}
 
 	if (cb->event_type >= UBASE_EVENT_TYPE_MAX) {
-		ubase_err(udev,
-			  "unsupported event type(%u).\n", cb->event_type);
+		ubase_err(udev, "register unsupported event type(%u).\n",
+			  cb->event_type);
 		return -EINVAL;
 	}
 
@@ -1060,13 +1060,14 @@ static void __ubase_event_unregister(struct ubase_dev *udev,
 	int ret;
 
 	if (cb->drv_type >= UBASE_DRV_MAX) {
-		ubase_err(udev, "unsupported drv_type(%u).\n", cb->drv_type);
+		ubase_err(udev, "unregister unsupported drv_type(%u).\n",
+			  cb->drv_type);
 		return;
 	}
 
 	if (cb->event_type >= UBASE_EVENT_TYPE_MAX) {
-		ubase_err(udev,
-			  "unsupported event type(%u).\n", cb->event_type);
+		ubase_err(udev, "unregister unsupported event type(%u).\n",
+			  cb->event_type);
 		return;
 	}
 

drivers/ub/ubase/ubase_trace.h
@@ -246,6 +246,27 @@ DEFINE_EVENT(ubase_ctrlq_ue_msg_template, ubase_send_ue_req,
 	TP_PROTO(const struct device *dev, u16 bus_ue_id, const void *buf, u16 len),
 	TP_ARGS(dev, bus_ue_id, buf, len));
 
+TRACE_EVENT(ubase_misc_event_cause,
+	TP_PROTO(struct device *dev, unsigned long event_cause),
+	TP_ARGS(dev, event_cause),
+
+	TP_STRUCT__entry(
+		__field(unsigned long, event_cause)
+		__dynamic_array(char, devname, TRACE_DEV_NAME_MAX_LEN)
+	),
+
+	TP_fast_assign(
+		__entry->event_cause = event_cause;
+		if (dev) {
+			(void)snprintf(__get_str(devname), TRACE_DEV_NAME_MAX_LEN,
+				       "%s %s", dev_driver_string(dev),
+				       dev_name(dev));
+		}
+	),
+
+	TP_printk("%s event_cause: 0x%lx", __get_str(devname), __entry->event_cause)
+);
+
 #endif /* __UBASE_TRACE_H__ */
 
 /* This must be outside ifdef __UBASE_TRACE_H__ */