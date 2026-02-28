f4d82ce1c3cc07e37c17d4decc10c418df1798cb
Liu Jian <liujian56@huawei.com>
2026-02-28 09:48:13 +0000

    cgroup: make cgroup_bpf_prog_attach work when cgroup2 is not mounted
    hulk inclusion
    category: featrue
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8480
    
    --------------------------------
    
    BPF_PROG_TYPE_CGROUP* bpf programs is associated with cgroup2. If cgroup2
    is not mounted, the bpf program is associated with cgrp_dfl_root.cgrp
    by default.
    
    Then we can use it like below:
    bpftool cgroup attach /sys/fs/cgroup/cpu sock_ops pinned /sys/fs/bpf/xxx
    
    Signed-off-by: Liu Jian <liujian56@huawei.com>
    Signed-off-by: Pu Lehui <pulehui@huawei.com>

--- kernel/bpf/cgroup.c
+++ kernel/bpf/cgroup.c
@@ -1157,7 +1157,7 @@ int cgroup_bpf_prog_attach(const union bpf_attr *attr,
 	struct cgroup *cgrp;
 	int ret;
 
-	cgrp = cgroup_get_from_fd(attr->target_fd);
+	cgrp = cgroup_get_from_fd_v2(attr->target_fd);
 	if (IS_ERR(cgrp))
 		return PTR_ERR(cgrp);
 
@@ -1185,7 +1185,7 @@ int cgroup_bpf_prog_detach(const union bpf_attr *attr, enum bpf_prog_type ptype)
 	struct cgroup *cgrp;
 	int ret;
 
-	cgrp = cgroup_get_from_fd(attr->target_fd);
+	cgrp = cgroup_get_from_fd_v2(attr->target_fd);
 	if (IS_ERR(cgrp))
 		return PTR_ERR(cgrp);
 
@@ -1304,7 +1304,7 @@ int cgroup_bpf_link_attach(const union bpf_attr *attr, struct bpf_prog *prog)
 	if (attr->link_create.flags)
 		return -EINVAL;
 
-	cgrp = cgroup_get_from_fd(attr->link_create.target_fd);
+	cgrp = cgroup_get_from_fd_v2(attr->link_create.target_fd);
 	if (IS_ERR(cgrp))
 		return PTR_ERR(cgrp);
 
@@ -1344,7 +1344,7 @@ int cgroup_bpf_prog_query(const union bpf_attr *attr,
 	struct cgroup *cgrp;
 	int ret;
 
-	cgrp = cgroup_get_from_fd(attr->query.target_fd);
+	cgrp = cgroup_get_from_fd_v2(attr->query.target_fd);
 	if (IS_ERR(cgrp))
 		return PTR_ERR(cgrp);
 
