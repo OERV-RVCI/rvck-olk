f41361faa11184f8f9e96a55181b6673a9375767
Wang Tao <wangtao554@huawei.com>
2026-02-28 07:04:29 +0000

    Revert "uacce: fix for cdev memory leak"
    hulk inclusion
    category: bugfix
    bugzilla: https://atomgit.com/src-openeuler/kernel/issues/13629
    CVE: CVE-2026-23096
    
    --------------------------------
    
    This reverts commit 3cf71ab1a46d0f9801b298231018a1667c935982.
    
    Revert driver patch and use upstream commit d9031575a2f8
    ("uacce: fix cdev handling in the cleanup path").
    
    Fixes: 3cf71ab1a46d ("uacce: fix for cdev memory leak")
    Signed-off-by: Wang Tao <wangtao554@huawei.com>

--- drivers/misc/uacce/uacce.c
+++ drivers/misc/uacce/uacce.c
@@ -966,8 +966,6 @@ EXPORT_SYMBOL_GPL(uacce_alloc);
  */
 int uacce_register(struct uacce_device *uacce)
 {
-	int ret;
-
 	if (!uacce)
 		return -ENODEV;
 
@@ -978,14 +976,7 @@ int uacce_register(struct uacce_device *uacce)
 	uacce->cdev->ops = &uacce_fops;
 	uacce->cdev->owner = THIS_MODULE;
 
-	ret = cdev_device_add(uacce->cdev, &uacce->dev);
-	if (ret) {
-		cdev_del(uacce->cdev);
-		uacce->cdev = NULL;
-		return ret;
-	}
-
-	return 0;
+	return cdev_device_add(uacce->cdev, &uacce->dev);
 }
 EXPORT_SYMBOL_GPL(uacce_register);
 
