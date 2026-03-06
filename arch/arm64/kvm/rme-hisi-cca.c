OERV-RVCI/rvck-olk

commit ee3114c1161a4b087c419561ecd8b801c1bd10bc

rme: isolate page when do delegation to avoid migration
kunpeng inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

Add normal pages and hugetlb pages isolate method to avoid page migration.

Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Yiwei Zhuang <zhuangyiwei@huawei.com>

arch/arm64/kvm/rme-hisi-cca.c
@@ -29,6 +29,19 @@ enum HISI_CCA_GRANULE_TYPES {
 	HISI_CCA_GRANULE_TYPES_NUM
 };
 
+static inline int _rmi_cca_hisi_delegate_range(unsigned long start_addr,
+					      unsigned long size)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_HISI_EXT, CCA_HISI_DELEGATE_RANGE,
+		start_addr, size
+	};
+
+	arm_smccc_1_2_smc(&regs, &regs);
+
+	return regs.a0;
+}
+
 static int get_start_level(struct realm *realm)
 {
 	return 4 - ((realm->ia_bits - 8) / (RMM_PAGE_SHIFT - 3));
@@ -360,11 +373,8 @@ int realm_hisi_cca_populate_region(struct kvm *kvm, phys_addr_t ipa_base,
 		}
 	}
 
-	if (ret == 0)
-		goto out_free;
 out_pin:
 	unpin_user_pages(pages, nr_pinned);
-out_free:
 	kfree(pages);
 	if (tmp_pages)
 		__free_pages(tmp_pages, order);
@@ -440,11 +450,8 @@ static int hisi_cca_map_range(struct kvm *kvm, unsigned long ipa_base,
 	}
 	*ipa_top = ipa_base + map_size;
 
-	if (ret == 0)
-		goto out_free;
 out_pin:
 	unpin_user_pages(pages, nr_pinned);
-out_free:
 	kfree(pages);
 out_srcu:
 	srcu_read_unlock(&kvm->srcu, idx);
@@ -501,23 +508,17 @@ static int hisi_cca_destroy_data(struct realm *realm, unsigned long ipa,
 	if (WARN_ON(ret))
 		return -ENXIO;
 
-	if (granule_type == HISI_CCA_GRANULE_NORMAL)
+	if (granule_type == HISI_CCA_GRANULE_NORMAL) {
 		ret = rmi_cca_hisi_undelegate_range(pa, size);
-	else
-		goto out_unpin;
-
-	/*
-	 * If the undelegate fails then something has gone seriously
-	 * wrong: take an extra reference to just leak pages.
-	 */
-	if (WARN_ON(ret)) {
-		for (offset = 0; offset < size; offset += PAGE_SIZE)
-			get_page(phys_to_page(pa + offset));
+		/*
+		 * If the undelegate fails then something has gone seriously
+		 * wrong: take an extra reference to just leak pages.
+		 */
+		if (WARN_ON(ret)) {
+			for (offset = 0; offset < size; offset += PAGE_SIZE)
+				get_page(phys_to_page(pa + offset));
+		}
 	}
-
-out_unpin:
-	for (offset = 0; offset < size; offset += PAGE_SIZE)
-		unpin_user_page(phys_to_page(pa + offset));
 
 	return 0;
 }
@@ -604,4 +605,25 @@ int realm_hisi_cca_set_ipa_state(struct kvm_vcpu *vcpu, unsigned long start,
 	*top_ipa = ipa;
 
 	return ret;
+}
+
+int rmi_cca_hisi_delegate_range(unsigned long start_addr, unsigned long size)
+{
+	unsigned long end_page_off = size - PAGE_SIZE;
+	struct folio *folio = page_folio(phys_to_page(start_addr));
+	struct folio *end_folio = page_folio(phys_to_page(start_addr + end_page_off));
+
+	if (!folio_test_large(folio) || folio != end_folio) {
+		pr_err("delegate range addr check fail\n");
+		return -EINVAL;
+	}
+
+	if (folio_test_hugetlb(folio)) {
+		if (rme_isolate_hugetlb(folio))
+			folio_put(folio);
+	} else if (folio_test_lru(folio)) {
+		if (folio_isolate_lru(folio))
+			folio_put(folio);
+	}
+	return _rmi_cca_hisi_delegate_range(start_addr, size);
 }

arch/arm64/include/asm/kvm_rme_hisi_cca.h
@@ -10,18 +10,7 @@
 #include <asm/kvm_host.h>
 #include <linux/kvm_host.h>
 
-static inline int rmi_cca_hisi_delegate_range(unsigned long start_addr,
-					      unsigned long size)
-{
-	struct arm_smccc_1_2_regs regs = {
-		SMC_RMI_HISI_EXT, CCA_HISI_DELEGATE_RANGE,
-		start_addr, size
-	};
-
-	arm_smccc_1_2_smc(&regs, &regs);
-
-	return regs.a0;
-}
+int rmi_cca_hisi_delegate_range(unsigned long start_addr, unsigned long size);
 
 static inline int rmi_cca_hisi_undelegate_range(unsigned long start_addr,
 						unsigned long size)

arch/arm64/include/asm/rmi_cmds.h
@@ -17,6 +17,12 @@ struct rtt_entry {
 	int ripas;
 };
 
+#ifdef CONFIG_HISI_CCA
+bool folio_isolate_lru(struct folio *folio);
+
+bool rme_isolate_hugetlb(struct folio *folio);
+#endif
+
 /**
  * rmi_data_create() - Create a data granule
  * @rd: PA of the RD
@@ -107,22 +113,7 @@ static inline int rmi_features(unsigned long index, unsigned long *out)
 	return res.a0;
 }
 
-/**
- * rmi_granule_delegate() - Delegate a granule
- * @phys: PA of the granule
- *
- * Delegate a granule for use by the realm world.
- *
- * Return: RMI return code
- */
-static inline int rmi_granule_delegate(unsigned long phys)
-{
-	struct arm_smccc_res res;
-
-	arm_smccc_1_1_invoke(SMC_RMI_GRANULE_DELEGATE, phys, &res);
-
-	return res.a0;
-}
+int rmi_granule_delegate(unsigned long phys);
 
 /**
  * rmi_granule_undelegate() - Undelegate a granule

arch/arm64/kvm/rme.c
@@ -40,6 +40,56 @@ static bool rme_has_feature(unsigned long feature)
 	return !!u64_get_bits(rmm_feat_reg0, feature);
 }
 
+/**
+ * rmi_granule_delegate() - Delegate a granule
+ * @phys: PA of the granule
+ *
+ * Delegate a granule for use by the realm world.
+ *
+ * Return: RMI return code
+ */
+static int _rmi_granule_delegate(unsigned long phys)
+{
+	struct arm_smccc_res res;
+
+	arm_smccc_1_1_invoke(SMC_RMI_GRANULE_DELEGATE, phys, &res);
+
+	return res.a0;
+}
+
+int rmi_granule_delegate(unsigned long phys)
+{
+	struct folio *folio = page_folio(phys_to_page(phys));
+
+	if (folio_test_hugetlb(folio)) {
+		if (rme_isolate_hugetlb(folio))
+			folio_put(folio);
+	} else if (folio_test_lru(folio)) {
+		if (folio_isolate_lru(folio))
+			folio_put(folio);
+	}
+	return _rmi_granule_delegate(phys);
+}
+
+#ifdef CONFIG_HISI_CCA
+bool rme_isolate_hugetlb(struct folio *folio)
+{
+	bool ret = true;
+
+	spin_lock_irq(&hugetlb_lock);
+	if (!folio_test_hugetlb(folio) ||
+		!folio_test_hugetlb_migratable(folio) ||
+		!folio_try_get(folio)) {
+		ret = false;
+		goto unlock;
+	}
+	folio_clear_hugetlb_migratable(folio);
+unlock:
+	spin_unlock_irq(&hugetlb_lock);
+	return ret;
+}
+#endif
+
 bool kvm_rme_supports_sve(void)
 {
 	return rme_has_feature(RMI_FEATURE_REGISTER_0_SVE_EN);

commit eec708f70838ac02f91e30768a777b1956a2192f

rme: Imporve range delegate
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

Add support for multiple pages delegate by hisi delegate range method.

Fixes: ee3114c1161a ("rme: isolate page when do delegation to avoid migration")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>

arch/arm64/kvm/rme-hisi-cca.c
@@ -612,13 +612,11 @@ int rmi_cca_hisi_delegate_range(unsigned long start_addr, unsigned long size)
 	unsigned long start = start_addr;
 	unsigned long end = start_addr + size;
 	struct folio *folio;
-	unsigned long nr_pages;
 
 	while (start < end) {
 		folio = page_folio(phys_to_page(start));
-		if (!folio || __pfn_to_phys(folio_pfn(folio)) != start)
+		if (!folio)
 			return -EINVAL;
-		nr_pages = folio_nr_pages(folio);
 
 		if (folio_test_hugetlb(folio)) {
 			if (rme_isolate_hugetlb(folio))
@@ -627,7 +625,7 @@ int rmi_cca_hisi_delegate_range(unsigned long start_addr, unsigned long size)
 			if (folio_isolate_lru(folio))
 				folio_put(folio);
 		}
-		start += nr_pages * PAGE_SIZE;
+		start = (folio_pfn(folio) + folio_nr_pages(folio)) * PAGE_SIZE;
 	}
 	return _rmi_cca_hisi_delegate_range(start_addr, size);
 }