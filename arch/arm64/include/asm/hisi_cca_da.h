OERV-RVCI/rvck-olk

commit c9eb3f90cea7ae310b4f4ea887e746a71cfa5193

hisi-cca-da: Support root dev delegate and dev init
kunpeng inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8414
CVE: NA

--------------------------------------------------------------------

Add hisi-cca-da file and support root dev delegate and dev init.
Host collects the devices info under the root port, or KAE VFs under
the PF to request rmm to delegate the root dev. Once the root dev is
delegated, devices under it are all realm and is not allowed to switch
back to non-secure. Rmi_dev_init provides available host memory for
rmm use to record device information.

Signed-off-by: hejiajing <hejiajing2@h-partners.com>
Signed-off-by: zhangwei <zhangwei375@huawei.com>
Signed-off-by: xuce <xuce10@h-partners.com>

arch/arm64/include/asm/hisi_cca_da.h
@@ -1,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __ASM_HISI_CCA_DA_H
+#define __ASM_HISI_CCA_DA_H
+
+#include <asm/kvm_host.h>
+
+#define MAX_DEV_PER_PORT 256
+struct rmi_dev_delegate_params {
+	uint16_t root_bdf; /* BDF of the PCIe root port or KAE PF device */
+	uint16_t num_dev; /* number of attachable devices */
+	uint32_t rsvd; /* padding for 64-bit alignment */
+	uint16_t devs[MAX_DEV_PER_PORT]; /* BDF of each attachable device */
+};
+
+bool is_support_rme(void);
+
+#endif

arch/arm64/include/asm/rmi_cmds.h
@@ -496,4 +496,52 @@ static inline int rmi_rtt_unmap_unprotected(unsigned long rd,
 	return res.a0;
 }
 
+#ifdef CONFIG_HISI_CCADA_HOST
+/**
+ * rmi_dev_init() - Initilaize the dev info
+ * @dev_bdf: Bdf of the pci dev
+ * @pa: Pa of the delegated page
+ *
+ * Initilaize the dev info with the delegated memory.
+ *
+ * Return: RMI return code
+ */
+static inline int rmi_dev_init(unsigned long dev_bdf, unsigned long pa)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_HISI_EXT, CCADA_DEV_INIT,
+		dev_bdf, pa
+	};
+
+	arm_smccc_1_2_smc(&regs, &regs);
+
+	return regs.a0;
+}
+
+/**
+ * rmi_root_dev_delegate() - Delegate the root dev
+ * @params_addr: PA of the struct rmi_dev_delegate_params
+ * @out_dev_bdf: dev bdf returned from rmm
+ *
+ * Request the rmm to delegate root dev and protect the device under it.
+ *
+ * Return: RMI return code
+ */
+static inline int rmi_root_dev_delegate(unsigned long params_addr,
+					unsigned long *out_dev_bdf)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_HISI_EXT, CCADA_ROOT_DEV_DELEGATE,
+		params_addr
+	};
+
+	arm_smccc_1_2_smc(&regs, &regs);
+
+	if (regs.a0 == RMI_ERROR_DEV_INFO)
+		*out_dev_bdf = regs.a1;
+
+	return regs.a0;
+}
+#endif
+
 #endif /* __ASM_RMI_CMDS_H */

arch/arm64/kvm/hisi-cca-da.c
@@ -1,0 +1,380 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2026. All rights reserved.
+ */
+#include <linux/hashtable.h>
+#include <linux/pci.h>
+#include <asm/rmi_cmds.h>
+#include <asm/kvm_emulate.h>
+#include <asm/hisi_cca_da.h>
+
+#define MAX_REALM_DEV_NUM_ORDER		8
+#define PCI_DEVICE_ID_HUAWEI_ZIP_PF	0xa250
+#define PCI_DEVICE_ID_HUAWEI_SEC_PF	0xa255
+#define PCI_DEVICE_ID_HUAWEI_HPRE_PF	0xa258
+
+struct dev_hash_entry {
+	u16 root_bdf; /* The bdf of the root device: pcie root port, or acc PF */
+	bool delegated; /* True if device is delegated to Realm */
+	u32 assigned_cnt; /* The count of devs assigned in VMs */
+	struct hlist_node node; /* hash table */
+};
+
+static DEFINE_HASHTABLE(g_root_dev_htable, MAX_REALM_DEV_NUM_ORDER);
+static DEFINE_SPINLOCK(g_dev_htable_lock);
+
+bool is_support_rme(void)
+{
+	return static_branch_unlikely(&kvm_rme_is_available);
+}
+EXPORT_SYMBOL_GPL(is_support_rme);
+
+/**
+ * get_child_devices_rec - Traverse pcie topology to find child devices
+ * If dev is a bridge, get it's children
+ * If dev is a regular device, get itself
+ * @dev: Device for which to get child devices
+ * @devs: All child devices under input dev
+ * @max_devs: Max num of devs
+ * @ndev: Num of child devices
+ */
+static int get_child_devices_rec(struct pci_dev *dev, uint16_t *devs,
+				  int max_devs, int *ndev)
+{
+	struct pci_bus *bus = dev->subordinate;
+
+	if (bus) { /* dev is a bridge */
+		struct pci_dev *child;
+		int ret = 0;
+
+		list_for_each_entry(child, &bus->devices, bus_list) {
+			ret = get_child_devices_rec(child, devs, max_devs, ndev);
+			if (ret < 0)
+				return ret;
+		}
+	} else { /* dev is a regular device */
+		uint16_t bdf = pci_dev_id(dev);
+		int i;
+		/* check if bdf is already in devs */
+		for (i = 0; i < *ndev; i++) {
+			if (devs[i] == bdf)
+				return *ndev;
+		}
+		/* check overflow */
+		if (*ndev >= max_devs)
+			return -EINVAL;
+
+		devs[*ndev] = bdf;
+		*ndev = *ndev + 1;
+	}
+
+	return *ndev;
+}
+
+static bool is_hisi_acc_dev(struct pci_dev *pdev)
+{
+	switch (pdev->vendor) {
+	case PCI_VENDOR_ID_HUAWEI:
+		switch (pdev->device) {
+		case PCI_DEVICE_ID_HUAWEI_ZIP_PF:
+		case PCI_DEVICE_ID_HUAWEI_SEC_PF:
+		case PCI_DEVICE_ID_HUAWEI_HPRE_PF:
+		case PCI_DEVICE_ID_HUAWEI_ZIP_VF:
+		case PCI_DEVICE_ID_HUAWEI_SEC_VF:
+		case PCI_DEVICE_ID_HUAWEI_HPRE_VF:
+			return true;
+		default:
+			return false;
+		}
+	default:
+		return false;
+	}
+}
+
+static int get_vf_devices(struct pci_dev *pf_dev, uint16_t *devs, int max_devs)
+{
+	struct pci_dev *vf_dev;
+	unsigned short vf_device_id;
+	int ndev = 0;
+
+	/* Add PF device */
+	devs[ndev] = pci_dev_id(pf_dev);
+	ndev++;
+
+	/* Get VF device id */
+	if (pf_dev->device == PCI_DEVICE_ID_HUAWEI_ZIP_PF)
+		vf_device_id = PCI_DEVICE_ID_HUAWEI_ZIP_VF;
+	else if (pf_dev->device == PCI_DEVICE_ID_HUAWEI_SEC_PF)
+		vf_device_id = PCI_DEVICE_ID_HUAWEI_SEC_VF;
+	else
+		vf_device_id = PCI_DEVICE_ID_HUAWEI_HPRE_VF;
+
+	/* Loop through all the VFs to find enabled VFs */
+	vf_dev = pci_get_device(pf_dev->vendor, vf_device_id, NULL);
+	while (vf_dev) {
+		if (vf_dev->is_virtfn && (vf_dev->physfn == pf_dev)) {
+			if (ndev >= max_devs)
+				return -EINVAL;
+
+			devs[ndev] = pci_dev_id(vf_dev);
+			ndev++;
+		}
+		vf_dev = pci_get_device(pf_dev->vendor, vf_device_id, vf_dev);
+	}
+	return ndev;
+}
+
+/**
+ * rme_get_all_dev_info - Retrieve all devices under the root port
+ * @rp_dev: root port device
+ * @params: Assign device parameters
+ *
+ * Returns:
+ * %0 if get all devices under the root port successful
+ * %-EINVAL if the total number of devices under the root port exceeds the maximum
+ */
+static int rme_get_all_dev_info(struct pci_dev *rp_dev,
+				struct rmi_dev_delegate_params *params)
+{
+	int ret;
+
+	params->root_bdf = pci_dev_id(rp_dev);
+	if (is_hisi_acc_dev(rp_dev)) {
+		/* Only search if we are a PF */
+		if (!rp_dev->is_physfn) {
+			pci_err(rp_dev, "Root dev is not a PF\n");
+			return -EINVAL;
+		}
+
+		ret = get_vf_devices(rp_dev, params->devs, MAX_DEV_PER_PORT);
+	} else {
+		int ndev = 0;
+
+		ret = get_child_devices_rec(rp_dev, params->devs,
+					    MAX_DEV_PER_PORT, &ndev);
+	}
+
+	if (ret < 0) {
+		pci_err(rp_dev, "Dev nums overflow\n");
+		return -EINVAL;
+	}
+	params->num_dev = (uint16_t)ret;
+	return 0;
+}
+
+static struct pci_dev *rme_get_root_dev(struct pci_dev *pdev)
+{
+	if (is_hisi_acc_dev(pdev))
+		return pci_physfn(pdev);
+
+	return pcie_find_root_port(pdev);
+}
+
+static inline bool is_root_dev_delegated(u16 root_bdf)
+{
+	struct dev_hash_entry *entry;
+
+	hash_for_each_possible(g_root_dev_htable, entry, node, root_bdf) {
+		if (entry->root_bdf == root_bdf && entry->delegated)
+			return true;
+	}
+
+	return false;
+}
+
+static inline bool is_dev_delegated(struct pci_dev *pdev)
+{
+	struct pci_dev *root_dev;
+	unsigned long flags;
+	bool is_delegated;
+
+	root_dev = rme_get_root_dev(pdev);
+	if (!root_dev)
+		return false;
+
+	spin_lock_irqsave(&g_dev_htable_lock, flags);
+	is_delegated = is_root_dev_delegated(pci_dev_id(root_dev));
+	spin_unlock_irqrestore(&g_dev_htable_lock, flags);
+
+	return is_delegated;
+}
+
+static inline struct dev_hash_entry *find_root_dev_entry(u16 root_bdf)
+{
+	struct dev_hash_entry *entry;
+
+	hash_for_each_possible(g_root_dev_htable, entry, node, root_bdf) {
+		if (entry->root_bdf == root_bdf)
+			return entry;
+	}
+
+	return NULL;
+}
+
+static inline struct dev_hash_entry *add_root_dev_entry(u16 root_bdf)
+{
+	struct dev_hash_entry *entry;
+
+	entry = kzalloc(sizeof(*entry), GFP_ATOMIC);
+	if (!entry)
+		return NULL;
+
+	entry->root_bdf = root_bdf;
+	entry->delegated = false;
+	entry->assigned_cnt = 0;
+
+	hash_add(g_root_dev_htable, &entry->node, root_bdf);
+
+	return entry;
+}
+
+static int rme_root_dev_delegate(phys_addr_t params_addr)
+{
+	unsigned long out_dev_bdf = ~0;
+	unsigned long last_dev_bdf = ~0;
+	phys_addr_t dev_info_phys;
+	void *dev_info;
+	int ret;
+
+retry:
+	ret = rmi_root_dev_delegate(params_addr, &out_dev_bdf);
+	if (ret == RMI_ERROR_DEV_INFO) {
+		if (out_dev_bdf == last_dev_bdf)
+			return -ENXIO;
+
+		dev_info = (void *)get_zeroed_page(GFP_ATOMIC);
+		if (!dev_info) {
+			pr_err("Failed to allocate page for dev %#lx\n",
+			       out_dev_bdf);
+			return -ENOMEM;
+		}
+
+		dev_info_phys = virt_to_phys(dev_info);
+		if (rmi_granule_delegate(dev_info_phys)) {
+			free_page((unsigned long)dev_info);
+			return -ENXIO;
+		}
+
+		ret = rmi_dev_init(out_dev_bdf, dev_info_phys);
+		if (ret && ret != RMI_ERROR_DEV_EXISTS) {
+			if (WARN_ON(rmi_granule_undelegate(dev_info_phys))) {
+				/* leak the page */
+				return -ENXIO;
+			}
+			free_page((unsigned long)dev_info);
+			return -ENXIO;
+		}
+		last_dev_bdf = out_dev_bdf;
+		goto retry;
+	} else if (ret) {
+		return -ENXIO;
+	}
+
+	return 0;
+}
+
+static int delegate_root_dev(struct pci_dev *root_dev)
+{
+	struct rmi_dev_delegate_params *params = NULL;
+	int ret;
+
+	params = (struct rmi_dev_delegate_params *)get_zeroed_page(GFP_ATOMIC);
+	if (!params)
+		return -ENOMEM;
+
+	ret = rme_get_all_dev_info(root_dev, params);
+	if (ret) {
+		free_page((unsigned long)params);
+		return ret;
+	}
+
+	ret = rme_root_dev_delegate(virt_to_phys(params));
+	if (ret)
+		pci_err(root_dev, "Failed to delegate\n");
+
+	free_page((unsigned long)params);
+	return ret;
+}
+
+/* Delegate root dev and devs under it to realm, undelegation is not supported */
+static int dev_assign_to_realm(struct dev_hash_entry *entry, struct pci_dev *root_dev)
+{
+	int ret = 0;
+
+	if (!entry->delegated && entry->assigned_cnt != 0) {
+		pci_err(root_dev, "Dev is in normal vm use\n");
+		return -EBUSY;
+	}
+
+	if (entry->delegated) {
+		entry->assigned_cnt++;
+		return 0;
+	}
+
+	ret = delegate_root_dev(root_dev);
+	if (ret)
+		return ret;
+
+	entry->delegated = true;
+	entry->assigned_cnt++;
+	return 0;
+}
+
+static int dev_assign_to_ns(struct dev_hash_entry *entry, struct pci_dev *root_dev)
+{
+	if (entry->delegated) {
+		pci_err(root_dev, "Dev is delegated to realm\n");
+		return -EINVAL;
+	}
+
+	entry->assigned_cnt++;
+	return 0;
+}
+
+static int rme_dev_assign(struct pci_dev *root_dev, struct kvm *kvm)
+{
+	struct dev_hash_entry *entry;
+	int ret;
+
+	spin_lock(&g_dev_htable_lock);
+	entry = find_root_dev_entry(pci_dev_id(root_dev));
+	if (!entry) {
+		entry = add_root_dev_entry(pci_dev_id(root_dev));
+		if (!entry) {
+			spin_unlock(&g_dev_htable_lock);
+			return -ENOMEM;
+		}
+	}
+
+	ret = kvm_is_realm(kvm) ? dev_assign_to_realm(entry, root_dev) :
+				  dev_assign_to_ns(entry, root_dev);
+	spin_unlock(&g_dev_htable_lock);
+	return ret;
+}
+
+static void rme_dev_unassign(struct pci_dev *pdev)
+{
+	struct dev_hash_entry *entry;
+	struct pci_dev *root_dev;
+
+	root_dev = rme_get_root_dev(pdev);
+	if (!root_dev)
+		return;
+
+	spin_lock(&g_dev_htable_lock);
+	entry = find_root_dev_entry(pci_dev_id(root_dev));
+	if (!entry) {
+		spin_unlock(&g_dev_htable_lock);
+		return;
+	}
+
+	if (entry->assigned_cnt != 0)
+		entry->assigned_cnt--;
+
+	if (!entry->delegated && entry->assigned_cnt == 0) {
+		hash_del(&entry->node);
+		kfree(entry);
+	}
+
+	spin_unlock(&g_dev_htable_lock);
+}

arch/arm64/kvm/rme.c
@@ -71,6 +71,7 @@ int rmi_granule_delegate(unsigned long phys)
 	}
 	return _rmi_granule_delegate(phys);
 }
+EXPORT_SYMBOL_GPL(rmi_granule_delegate);
 
 #ifdef CONFIG_HISI_CCA
 bool rme_isolate_hugetlb(struct folio *folio)

arch/arm64/include/asm/rmi_smc.h
@@ -97,6 +97,11 @@ enum hisi_ext_cmd {
 #define RMI_ERROR_REC		3
 #define RMI_ERROR_RTT		4
 
+#ifdef CONFIG_HISI_CCADA_HOST
+#define RMI_ERROR_DEV_INFO	9
+#define RMI_ERROR_DEV_EXISTS	11
+#endif
+
 enum rmi_ripas {
 	RMI_EMPTY = 0,
 	RMI_RAM = 1,

arch/arm64/kvm/Makefile
@@ -29,6 +29,7 @@ kvm-$(CONFIG_HISI_VIRTCCA_HOST) += tmi.o
 kvm-$(CONFIG_HISI_VIRTCCA_HOST) += virtcca_cvm.o
 kvm-$(CONFIG_HISI_VIRTCCA_HOST) += virtcca_cvm_exit.o
 kvm-$(CONFIG_HISI_CCA) += rme-hisi-cca.o
+kvm-$(CONFIG_HISI_CCADA_HOST) += hisi-cca-da.o
 obj-$(CONFIG_KVM_HISI_VIRT) += hisilicon/
 
 always-y := hyp_constants.h hyp-constants.s

commit 11a27efee0a38b33d359705cba2e184862afb696

hisi-cca-da: Improve hisi cca dev validate and delegate
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

Modify hisi cca device validate and delegate process.

Fixes: c9eb3f90cea7 ("hisi-cca-da: Support root dev delegate and dev init")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>

arch/arm64/include/asm/hisi_cca_da.h
@@ -9,7 +9,7 @@
 struct rmi_dev_delegate_params {
 	uint16_t root_bdf; /* BDF of the PCIe root port or KAE PF device */
 	uint16_t num_dev; /* number of attachable devices */
-	uint32_t rsvd; /* padding for 64-bit alignment */
+	uint32_t rsvd;
 	uint16_t devs[MAX_DEV_PER_PORT]; /* BDF of each attachable device */
 };
 
@@ -37,8 +37,7 @@ int realm_attach_devs(struct realm *realm);
 void realm_destroy_dev_list(struct realm *realm);
 int kvm_rme_assign_device(struct pci_dev *pdev, struct kvm *kvm);
 void kvm_rme_unassign_device(struct pci_dev *pdev, struct kvm *kvm);
-int kvm_arm_vcpu_rme_dev_validate(struct kvm_vcpu *vcpu,
-				  struct kvm_arm_rme_dev_validate *args);
+void kvm_complete_dev_op(struct kvm_vcpu *vcpu);
 
 void hisi_pcipc_ns_add(const struct pci_device_id *id_table);
 void hisi_pcipc_ns_remove(const struct pci_device_id *id_table);

arch/arm64/include/asm/rmi_cmds.h
@@ -534,11 +534,11 @@ static inline int rmi_rtt_unmap_unprotected(unsigned long rd,
 
 #ifdef CONFIG_HISI_CCADA_HOST
 /**
- * rmi_dev_init() - Initilaize the dev info
+ * rmi_dev_init() - Initialize the dev info
  * @dev_bdf: Bdf of the pci dev
  * @pa: Pa of the delegated page
  *
- * Initilaize the dev info with the delegated memory.
+ * Initialize the dev info with the delegated memory.
  *
  * Return: RMI return code
  */

arch/arm64/kvm/arm.c
@@ -1975,17 +1975,6 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 			return -EFAULT;
 		return kvm_arm_vcpu_rmm_psci_complete(vcpu, &req);
 	}
-#ifdef CONFIG_HISI_CCADA_HOST
-	case KVM_ARM_VCPU_RME_DEV_VALIDATE: {
-		struct kvm_arm_rme_dev_validate args;
-
-		if (!_vcpu_is_rec(vcpu))
-			return -EPERM;
-		if (copy_from_user(&args, argp, sizeof(args)))
-			return -EFAULT;
-		return kvm_arm_vcpu_rme_dev_validate(vcpu, &args);
-	}
-#endif
 	default:
 		r = -EINVAL;
 	}

arch/arm64/kvm/hisi-cca-da.c
@@ -503,8 +503,6 @@ static int rme_root_dev_delegate(phys_addr_t params_addr)
 retry:
 	ret = rmi_root_dev_delegate(params_addr, &out_dev_bdf);
 	if (ret == RMI_ERROR_DEV_INFO) {
-		if (out_dev_bdf == last_dev_bdf)
-			return -ENXIO;
 
 		dev_info = (void *)get_zeroed_page(GFP_ATOMIC);
 		if (!dev_info) {
@@ -518,7 +516,6 @@ retry:
 			free_page((unsigned long)dev_info);
 			return -ENXIO;
 		}
-
 		ret = rmi_dev_init(out_dev_bdf, dev_info_phys);
 		if (ret && ret != RMI_ERROR_DEV_EXISTS) {
 			if (WARN_ON(rmi_granule_undelegate(dev_info_phys))) {
@@ -837,19 +834,14 @@ err_detach:
 	return ret;
 }
 
-int kvm_arm_vcpu_rme_dev_validate(struct kvm_vcpu *vcpu,
-				  struct kvm_arm_rme_dev_validate *args)
+void kvm_complete_dev_op(struct kvm_vcpu *vcpu)
 {
-	if (!vcpu || !args || !_vcpu_is_rec(vcpu))
-		return -EINVAL;
+	struct kvm_run *run = vcpu->run;
+	struct realm_rec *rec = vcpu->arch.rec;
 
-	/* Record host pdev bdf in rec_enter, complete dev validate in rmm */
-	vcpu->arch.rec->run->enter.vfio_dev = args->vfio_dev;
-	vcpu->arch.rec->run->enter.dev_bdf = args->dev_bdf;
-
-	return 0;
+	rec->run->enter.dev_bdf = run->rme_dev.dev_bdf;
+	rec->run->enter.vfio_dev = run->rme_dev.vfio_dev;
 }
-
 
 #define MMIO_REG_32_BIT 32
 static u32 rme_mmio_read32(unsigned long addr, struct pci_dev *pdev)

arch/arm64/kvm/rme-exit.c
@@ -182,6 +182,7 @@ static int rec_exit_dev(struct kvm_vcpu *vcpu)
 	/* Return to qemu to find the host dev by guest dev bdf */
 	vcpu->run->exit_reason = KVM_EXIT_ARM_RME_DEV;
 	vcpu->run->rme_dev.guest_dev_bdf = rec->run->exit.guest_dev_bdf;
+	vcpu->run->rme_dev.vfio_dev = false;
 
 	return 0;
 }

arch/arm64/kvm/rme.c
@@ -1806,6 +1806,9 @@ int _kvm_rec_pre_enter(struct kvm_vcpu *vcpu)
 	case RMI_EXIT_RIPAS_CHANGE:
 		kvm_complete_ripas_change(vcpu);
 		break;
+	case RMI_EXIT_DEV_VALIDATE:
+	case RMI_EXIT_DEV_START:
+		kvm_complete_dev_op(vcpu);
 	}
 
 	return 1;

include/uapi/linux/kvm.h
@@ -267,7 +267,7 @@ struct kvm_xen_exit {
 #define KVM_EXIT_NOTIFY           37
 #define KVM_EXIT_LOONGARCH_IOCSR  38
 #ifndef __GENKSYMS__
-#define KVM_EXIT_ARM_RME_DEV      39
+#define KVM_EXIT_ARM_RME_DEV      50
 #endif
 
 /* For KVM_EXIT_INTERNAL_ERROR */
@@ -526,6 +526,8 @@ struct kvm_run {
 		/* KVM_EXIT_ARM_RME_DEV */
 		struct {
 			__u16 guest_dev_bdf;
+			__u16 dev_bdf;
+			__u32 vfio_dev;
 		} rme_dev;
 #endif
 		/* Fix the size of the union. */
@@ -2498,15 +2500,5 @@ struct kvm_arm_rmm_psci_complete {
 };
 
 #define KVM_ARM_VCPU_RMM_PSCI_COMPLETE	_IOW(KVMIO, 0xd6, struct kvm_arm_rmm_psci_complete)
-
-/* Available with KVM_CAP_ARM_RME, only for VMs with KVM_VM_TYPE_ARM_REALM  */
-struct kvm_arm_rme_dev_validate {
-	__u16 dev_bdf; /* pci dev bdf in the host */
-	__u16 guest_dev_bdf; /* pci dev bdf in the realm */
-	__u32 vfio_dev; /* Whether the host dev bdf refers to a VFIO dev */
-	__u32 reserved[4];
-};
-
-#define KVM_ARM_VCPU_RME_DEV_VALIDATE	_IOW(KVMIO, 0xd4, struct kvm_arm_rme_dev_validate)
 
 #endif /* __LINUX_KVM_H */