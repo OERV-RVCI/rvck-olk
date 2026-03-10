OERV-RVCI/rvck-olk

commit 03cc569bff0c8e3667ff330ddafe9dbb7fd5b2dc

hisi-cca-da: Set ripas of the bar region as RIPAS_DEV
kunpeng inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8414
CVE: NA

--------------------------------------------------------------------

For realm dev, we set the ripas of bar region as RIPAS_DEV, realm
will create realm mapping for stage 1, stage 2 mapping will be processed
in the host. However, msix region should remains RIPAS_EMPTY.

Signed-off-by: hejiajing <hejiajing2@h-partners.com>
Signed-off-by: zhangwei <zhangwei375@huawei.com>
Signed-off-by: xuce <xuce10@h-partners.com>

arch/arm64/kernel/realm_guest.c
@@ -5,6 +5,9 @@
 #include <linux/io.h>
 #include <linux/swiotlb.h>
 #include <asm/rsi.h>
+#ifdef CONFIG_HISI_CCADA_GUEST
+#include <linux/pci.h>
+#endif
 #include <asm/realm_guest.h>
 
 static struct device realm_alloc_device;
@@ -51,3 +54,64 @@ bool realm_free_swiotlb_shared_pages(void *addr, unsigned int order)
 	return swiotlb_free(&realm_alloc_device, (struct page *)addr,
 			    (1UL << order) * PAGE_SIZE);
 }
+
+#ifdef CONFIG_HISI_CCADA_GUEST
+#define msix_table_size(flags)	((flags & PCI_MSIX_FLAGS_QSIZE) + 1)
+static int set_msix_region_shared(struct pci_dev *pdev, int bar)
+{
+	resource_size_t start;
+	resource_size_t end;
+	u32 table_offset;
+	u16 control;
+	u8 msix_bir;
+	u8 tsize;
+
+	pci_read_config_dword(pdev, pdev->msix_cap + PCI_MSIX_TABLE, &table_offset);
+	msix_bir = (u8)(table_offset & PCI_MSIX_TABLE_BIR);
+	if (msix_bir != bar)
+		return 0;
+
+	table_offset &= PCI_MSIX_TABLE_OFFSET;
+	start = pci_resource_start(pdev, msix_bir) + table_offset;
+
+	pci_read_config_word(pdev, pdev->msix_cap + PCI_MSIX_FLAGS, &control);
+	tsize = msix_table_size(control);
+	end = ALIGN(start + tsize * PCI_MSIX_ENTRY_SIZE, PAGE_SIZE);
+
+	return rsi_set_memory_range_shared(start, end);
+}
+
+int ccada_init_mem_region(struct pci_dev *pdev, int bar)
+{
+	int ret;
+
+	if (!is_realm_world())
+		return 0;
+
+	if (!pdev || bar < 0 || bar >= PCI_STD_NUM_BARS)
+		return -EINVAL;
+
+	if (pci_resource_len(pdev, bar) == 0)
+		return 0;
+
+	if (!rsi_is_realm_dev(pci_dev_id(pdev)))
+		return 0;
+
+	ret = rsi_set_mmio_range_protected(pci_resource_start(pdev, bar),
+					   pci_resource_len(pdev, bar));
+	if (ret) {
+		pci_err(pdev, "Failed to set bar[%d] to protected\n", bar);
+		return ret;
+	}
+
+	if (pdev->msix_cap) {
+		ret = set_msix_region_shared(pdev, bar);
+		if (ret)
+			pci_err(pdev, "Failed to set msi-x to shared\n");
+		return ret;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(ccada_init_mem_region);
+#endif

arch/arm64/include/asm/realm_guest.h
@@ -13,4 +13,8 @@ struct page *realm_alloc_swiotlb_shared_pages(gfp_t gfp, unsigned int order);
 
 bool realm_free_swiotlb_shared_pages(void *addr, unsigned int order);
 
+#ifdef CONFIG_HISI_CCADA_GUEST
+int ccada_init_mem_region(struct pci_dev *pdev, int bar);
+#endif
+
 #endif /* __REALM_GUEST_H */

arch/arm64/include/asm/rsi.h
@@ -77,5 +77,11 @@ static inline bool rsi_is_realm_dev(u16 bdf)
 
 	return is_realm_dev;
 }
+
+static inline int rsi_set_mmio_range_protected(phys_addr_t addr, size_t len)
+{
+	return rsi_set_memory_range(addr, addr + len, RSI_RIPAS_DEV,
+				    RSI_CHANGE_DESTROYED);
+}
 #endif /* CONFIG_HISI_CCADA_GUEST */
 #endif /* __ASM_RSI_H_ */

drivers/pci/pci.c
@@ -35,6 +35,11 @@
 #ifdef CONFIG_MACH_LOONGSON64
 #include <linux/suspend.h>
 #endif
+#ifdef CONFIG_HISI_CCADA_GUEST
+#ifndef __GENKSYMS__
+#include <asm/realm_guest.h>
+#endif
+#endif
 #include "pci.h"
 
 DEFINE_MUTEX(pci_slot_mutex);
@@ -4091,6 +4096,10 @@ static int __pci_request_region(struct pci_dev *pdev, int bar,
 					pci_resource_len(pdev, bar), res_name,
 					exclusive))
 			goto err_out;
+#ifdef CONFIG_HISI_CCADA_GUEST
+		if (ccada_init_mem_region(pdev, bar))
+			goto err_out;
+#endif
 	}
 
 	dr = find_pci_dr(pdev);

commit d1d9e42c0b79695ba0a7b5193672b4ae5de9bf38

hisi-cca-da: Fix msi region shared problem
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

Modify tsize type to avoid data overflow.

Fixes: 03cc569bff0c ("hisi-cca-da: Set ripas of the bar region as RIPAS_DEV")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>

arch/arm64/kernel/realm_guest.c
@@ -64,7 +64,7 @@ static int set_msix_region_shared(struct pci_dev *pdev, int bar)
 	u32 table_offset;
 	u16 control;
 	u8 msix_bir;
-	u8 tsize;
+	size_t tsize;
 
 	pci_read_config_dword(pdev, pdev->msix_cap + PCI_MSIX_TABLE, &table_offset);
 	msix_bir = (u8)(table_offset & PCI_MSIX_TABLE_BIR);
@@ -73,11 +73,18 @@ static int set_msix_region_shared(struct pci_dev *pdev, int bar)
 
 	table_offset &= PCI_MSIX_TABLE_OFFSET;
 	start = pci_resource_start(pdev, msix_bir) + table_offset;
+	if (start <= table_offset) {
+		pci_err(pdev, "pci dev msi start overflow.\n");
+		return -EINVAL;
+	}
 
 	pci_read_config_word(pdev, pdev->msix_cap + PCI_MSIX_FLAGS, &control);
 	tsize = msix_table_size(control);
 	end = ALIGN(start + tsize * PCI_MSIX_ENTRY_SIZE, PAGE_SIZE);
-
+	if (end <= start) {
+		pci_err(pdev, "pci dev msi end overflow.\n");
+		return -EINVAL;
+	}
 	return rsi_set_memory_range_shared(start, end);
 }
 