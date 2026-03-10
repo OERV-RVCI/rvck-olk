OERV-RVCI/rvck-olk

commit 97de9d3b126b454edf04f554490f3b6073069d7f

hisi-cca-da: Add realm iommu support for vfio-iommu-type1
kunpeng inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8414
CVE: NA

--------------------------------------------------------------------

Add Realm Management Extension (RME) support to the vfio-iommu-type1
implementation, with underlying hooks into the ARM SMMUv3 driver to
enable realm mode for IOMMU domains when the HISI_CCADA_HOST config
is enabled.

KABI: Extend struct iommu_domain_ops with enable_rme() via KABI_USE(2)
under CONFIG_HISI_CCADA_HOST to maintain compatibility.

Signed-off-by: zhangwei <zhangwei375@huawei.com>
Signed-off-by: hejiajing <hejiajing2@h-partners.com>
Signed-off-by: xuce <xuce10@h-partners.com>

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3924,6 +3924,19 @@ static int arm_smmu_clear_dirty_log(struct iommu_domain *domain,
 }
 #endif
 
+#ifdef CONFIG_HISI_CCADA_HOST
+static int arm_smmu_enable_rme(struct iommu_domain *domain)
+{
+	struct arm_smmu_domain *smmu_domain = to_smmu_domain(domain);
+
+	mutex_lock(&smmu_domain->init_mutex);
+	smmu_domain->realm = true;
+	mutex_unlock(&smmu_domain->init_mutex);
+
+	return 0;
+}
+#endif
+
 static int arm_smmu_of_xlate(struct device *dev, struct of_phandle_args *args)
 {
 	return iommu_fwspec_add_ids(dev, args->args, 1);
@@ -4170,6 +4183,9 @@ static struct iommu_ops arm_smmu_ops = {
 		.iotlb_sync_map         = arm_smmu_iotlb_sync_map,
 #endif
 		.iova_to_phys		= arm_smmu_iova_to_phys,
+#ifdef CONFIG_HISI_CCADA_HOST
+		.enable_rme		= arm_smmu_enable_rme,
+#endif
 #ifdef CONFIG_ARM_SMMU_V3_HTTU
 		.support_dirty_log	= arm_smmu_support_dirty_log,
 		.switch_dirty_log	= arm_smmu_switch_dirty_log,

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -899,6 +899,9 @@ struct arm_smmu_domain {
 	struct list_head		node;
 	struct kvm			*kvm;
 #endif
+#ifdef CONFIG_HISI_CCADA_HOST
+	bool				realm;
+#endif
 };
 
 struct arm_smmu_nested_domain {

drivers/iommu/iommu.c
@@ -2786,6 +2786,18 @@ static int __init iommu_init(void)
 }
 core_initcall(iommu_init);
 
+#ifdef CONFIG_HISI_CCADA_HOST
+int iommu_enable_rme(struct iommu_domain *domain)
+{
+	if (domain->type != IOMMU_DOMAIN_UNMANAGED)
+		return -EINVAL;
+	if (!domain->ops->enable_rme)
+		return -EINVAL;
+	return domain->ops->enable_rme(domain);
+}
+EXPORT_SYMBOL_GPL(iommu_enable_rme);
+#endif
+
 int iommu_set_pgtable_quirks(struct iommu_domain *domain,
 		unsigned long quirk)
 {

drivers/vfio/vfio_iommu_type1.c
@@ -85,6 +85,9 @@ struct vfio_iommu {
 #ifdef CONFIG_HISI_VIRTCCA_CODA
 	bool            secure;	/* Whether the vfio iommu is secure or not */
 #endif
+#ifdef CONFIG_HISI_CCADA_HOST
+	bool			realm; /* Whether the vfio iommu is realm or not */
+#endif
 };
 
 struct vfio_domain {
@@ -2644,6 +2647,13 @@ static int vfio_iommu_type1_attach_group(void *iommu_data,
 	if (is_virtcca_cvm_enable() && iommu->secure)
 		domain->domain->secure = true;
 #endif
+#ifdef CONFIG_HISI_CCADA_HOST
+	if (iommu->realm) {
+		ret = iommu_enable_rme(domain->domain);
+		if (ret)
+			goto out_free_domain;
+	}
+#endif
 
 	ret = iommu_attach_group(domain->domain, group->iommu_group);
 	if (ret)
@@ -3020,6 +3030,12 @@ static void *vfio_iommu_type1_open(unsigned long arg)
 		iommu->secure = true;
 		break;
 #endif
+#ifdef CONFIG_HISI_CCADA_HOST
+	case VFIO_TYPE1_IOMMU_RME:
+		iommu->v2 = true;
+		iommu->realm = true;
+		break;
+#endif
 	default:
 		kfree(iommu);
 		return ERR_PTR(-EINVAL);
@@ -3113,6 +3129,9 @@ static int vfio_iommu_type1_check_extension(struct vfio_iommu *iommu,
 	case VFIO_TYPE1v2_IOMMU:
 #ifdef CONFIG_HISI_VIRTCCA_CODA
 	case VFIO_TYPE1v2_S_IOMMU:
+#endif
+#ifdef CONFIG_HISI_CCADA_HOST
+	case VFIO_TYPE1_IOMMU_RME:
 #endif
 	case VFIO_UNMAP_ALL:
 		return 1;

include/linux/iommu.h
@@ -832,6 +832,7 @@ struct iommu_ops {
  * @free: Release the domain after use.
  * @get_msi_mapping_domain: Return the related iommu_domain that should hold the
  *                          MSI cookie and accept mapping(s).
+ * @enable_rme: Enable realm management extension for the related iommu_domain
  */
 struct iommu_domain_ops {
 	int (*attach_dev)(struct iommu_domain *domain, struct device *dev);
@@ -880,7 +881,11 @@ struct iommu_domain_ops {
 
 	KABI_USE(1, struct iommu_domain *
 			(*get_msi_mapping_domain)(struct iommu_domain *domain))
+#ifdef CONFIG_HISI_CCADA_HOST
+	KABI_USE(2, int (*enable_rme)(struct iommu_domain *domain))
+#else
 	KABI_RESERVE(2)
+#endif
 	KABI_RESERVE(3)
 	KABI_RESERVE(4)
 	KABI_RESERVE(5)
@@ -1084,6 +1089,9 @@ extern int iommu_group_id(struct iommu_group *group);
 struct kset *iommu_get_group_kset(void);
 extern struct iommu_domain *iommu_group_default_domain(struct iommu_group *);
 
+#ifdef CONFIG_HISI_CCADA_HOST
+int iommu_enable_rme(struct iommu_domain *domain);
+#endif
 int iommu_set_pgtable_quirks(struct iommu_domain *domain,
 		unsigned long quirks);
 

include/uapi/linux/vfio.h
@@ -26,6 +26,7 @@
 #define VFIO_SPAPR_TCE_IOMMU		2
 #define VFIO_TYPE1v2_IOMMU		3
 #define VFIO_TYPE1v2_S_IOMMU		12	/* Virtcca feature: secure iommu */
+#define VFIO_TYPE1_IOMMU_RME		13	/* Realm Management Extension */
 /*
  * IOMMU enforces DMA cache coherence (ex. PCIe NoSnoop stripping).  This
  * capability is subject to change as groups are added or removed.

commit 9c2b20f961e2dc67a9efb73060215d73cebe0ffc

hisi-cca-da: Fix smmu enable rme problem
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

Enable smmu rme ability in  arm_smmu_attach_dev.

Fixes: 97de9d3b126b ("hisi-cca-da: Add realm iommu support for vfio-iommu-type1")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3140,7 +3140,12 @@ static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev)
 	mutex_lock(&smmu_domain->init_mutex);
 
 	if (!smmu_domain->smmu) {
-		ret = arm_smmu_domain_finalise(smmu_domain, smmu, 0);
+		if (smmu->realm.enabled) {
+			ret = arm_smmu_domain_finalise(smmu_domain, smmu, 0);
+		} else {
+			pr_err("realm smmu has not enabled.\n");
+			ret = -EOPNOTSUPP;
+		}
 	} else if (smmu_domain->smmu != smmu)
 		ret = -EINVAL;
 
@@ -3985,7 +3990,7 @@ static int arm_smmu_enable_rme(struct iommu_domain *domain)
 	int ret = 0;
 
 	mutex_lock(&smmu_domain->init_mutex);
-	if (arm_smmu_support_rme(smmu))
+	if (!smmu || arm_smmu_support_rme(smmu))
 		smmu_domain->realm = true;
 	else
 		ret = -EOPNOTSUPP;