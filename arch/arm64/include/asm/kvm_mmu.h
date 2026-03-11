OERV-RVCI/rvck-olk

commit 23e4b444a30ff28d68459718bf03c3a57100480a

hisi-cca-da: Delay realm memory release to kvm_destroy_realm
kunpeng inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8414
CVE: NA

--------------------------------------------------------------------

Defer Realm stage2 memory unmap, RTT destruction and granule undelegation
from kvm_free_stage2_pgd() to _kvm_destroy_realm() for HISI_CCA_DA, add
proper page ref management for delegated granules, and export
unmap_stage2_range() for Realm teardown.

Signed-off-by: zhangwei <zhangwei375@huawei.com>
Signed-off-by: hejiajing <hejiajing2@h-partners.com>
Signed-off-by: xuce <xuce10@h-partners.com>

arch/arm64/include/asm/kvm_mmu.h
@@ -173,6 +173,8 @@ void __init free_hyp_pgds(void);
 
 void stage2_unmap_vm(struct kvm *kvm);
 int kvm_init_stage2_mmu(struct kvm *kvm, struct kvm_s2_mmu *mmu, unsigned long type);
+void unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 size,
+			bool may_block);
 void kvm_uninit_stage2_mmu(struct kvm *kvm);
 void kvm_free_stage2_pgd(struct kvm_s2_mmu *mmu);
 int kvm_phys_addr_ioremap(struct kvm *kvm, phys_addr_t guest_ipa,

arch/arm64/kvm/mmu.c
@@ -336,8 +336,8 @@ static void __unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 
 					   may_block));
 }
 
-static void unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 size,
-			       bool may_block)
+void unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 size,
+			bool may_block)
 {
 	__unmap_stage2_range(mmu, start, size, may_block, false);
 }
@@ -1045,11 +1045,13 @@ void kvm_free_stage2_pgd(struct kvm_s2_mmu *mmu)
 	if (_kvm_is_realm(kvm) &&
 	    (kvm_realm_state(kvm) != REALM_STATE_DEAD &&
 	     kvm_realm_state(kvm) != REALM_STATE_NONE)) {
-		struct realm *realm = &kvm->arch.realm;
-
-		unmap_stage2_range(mmu, 0, BIT(realm->ia_bits - 1), true);
+#ifdef CONFIG_HISI_CCADA_HOST
+		write_unlock(&kvm->mmu_lock);
+#else
+		unmap_stage2_range(mmu, 0, BIT(kvm->arch.realm.ia_bits - 1), true);
 		write_unlock(&kvm->mmu_lock);
 		kvm_realm_destroy_rtts(kvm, pgt->ia_bits);
+#endif
 
 		/*
 		 * The physical PGD pages are delegated to the RMM, so cannot

arch/arm64/kvm/rme.c
@@ -317,6 +317,7 @@ retry:
 	if (WARN_ON(ret))
 		return -ENXIO;
 
+	put_page(phys_to_page(rtt_addr));
 	*out_rtt = rtt_addr;
 
 	return 0;
@@ -909,6 +910,8 @@ static int realm_create_protected_data_granule(struct realm *realm,
 	if (rmi_granule_delegate(dst_phys))
 		return -ENXIO;
 
+	get_page(phys_to_page(dst_phys));
+
 	ret = rmi_data_create(rd, dst_phys, ipa, src_phys, flags);
 	if (RMI_RETURN_STATUS(ret) == RMI_ERROR_RTT) {
 		/* Create missing RTTs and retry */
@@ -937,7 +940,6 @@ int realm_create_protected_data_page(struct realm *realm,
 {
 	unsigned long rd = virt_to_phys(realm->rd);
 	phys_addr_t dst_phys, src_phys;
-	bool undelegate_failed = false;
 	int ret, offset;
 
 	dst_phys = page_to_phys(dst_page);
@@ -964,8 +966,8 @@ int realm_create_protected_data_page(struct realm *realm,
 err:
 	if (ret == -EIO) {
 		/* current offset needs undelegating */
-		if (WARN_ON(rmi_granule_undelegate(dst_phys)))
-			undelegate_failed = true;
+		if (!WARN_ON(rmi_granule_undelegate(dst_phys)))
+			put_page(dst_page);
 	}
 	while (offset > 0) {
 		ipa -= RMM_PAGE_SIZE;
@@ -974,16 +976,8 @@ err:
 
 		rmi_data_destroy(rd, ipa, NULL, NULL);
 
-		if (WARN_ON(rmi_granule_undelegate(dst_phys)))
-			undelegate_failed = true;
-	}
-
-	if (undelegate_failed) {
-		/*
-		 * A granule could not be undelegated,
-		 * so the page has to be leaked
-		 */
-		get_page(dst_page);
+		if (!WARN_ON(rmi_granule_undelegate(dst_phys)))
+			put_page(dst_page);
 	}
 
 	return -ENXIO;
@@ -1144,6 +1138,7 @@ int realm_map_protected(struct realm *realm,
 			return 0;
 		}
 
+		get_page(phys_to_page(phys));
 		ret = rmi_data_create_unknown(rd, phys, ipa);
 
 		if (RMI_RETURN_STATUS(ret) == RMI_ERROR_RTT) {
@@ -1177,10 +1172,8 @@ int realm_map_protected(struct realm *realm,
 	return 0;
 
 err_undelegate:
-	if (WARN_ON(rmi_granule_undelegate(phys))) {
-		/* Page can't be returned to NS world so is lost */
-		get_page(phys_to_page(phys));
-	}
+	if (!WARN_ON(rmi_granule_undelegate(phys)))
+		put_page(phys_to_page(phys));
 err:
 	while (size > 0) {
 		unsigned long data, top;
@@ -1191,10 +1184,8 @@ err:
 
 		WARN_ON(rmi_data_destroy(rd, ipa, &data, &top));
 
-		if (WARN_ON(rmi_granule_undelegate(phys))) {
-			/* Page can't be returned to NS world so is lost */
-			get_page(phys_to_page(phys));
-		}
+		if (!WARN_ON(rmi_granule_undelegate(phys)))
+			put_page(phys_to_page(phys));
 	}
 	return -ENXIO;
 }
@@ -1714,6 +1705,7 @@ int _kvm_realm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap)
 
 void _kvm_destroy_realm(struct kvm *kvm)
 {
+	struct kvm_s2_mmu *mmu = &kvm->arch.mmu;
 	struct realm *realm = &kvm->arch.realm;
 	size_t pgd_size = kvm_pgtable_stage2_pgd_size(kvm->arch.vtcr);
 	int i;
@@ -1727,6 +1719,11 @@ void _kvm_destroy_realm(struct kvm *kvm)
 		return;
 
 #ifdef CONFIG_HISI_CCADA_HOST
+	write_lock(&kvm->mmu_lock);
+	unmap_stage2_range(mmu, 0, BIT(realm->ia_bits - 1), true);
+	write_unlock(&kvm->mmu_lock);
+	kvm_realm_destroy_rtts(kvm, mmu->pgt->ia_bits);
+
 	/* undelegate and detach dev from realm */
 	realm_destroy_dev_list(realm);
 #endif

commit d464f7c0c20a2eca9681f17b58cc8da358bcdb94

hisi-cca-da: Destroy mmu stage2 pagetable after closing vfio devices
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

To avoid SMMU pagefault when shutdown realm virtual machine, we destroy
mmu stage2 pagetable after closing vfio devices.

Fixes: 23e4b444a30f ("hisi-cca-da: Delay realm memory release to kvm_destroy_realm")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>
Signed-off-by: Yiwei Zhuang <zhuangyiwei@huawei.com>

arch/arm64/include/asm/kvm_mmu.h
@@ -171,6 +171,8 @@ int create_hyp_exec_mappings(phys_addr_t phys_addr, size_t size,
 int create_hyp_stack(phys_addr_t phys_addr, unsigned long *haddr);
 void __init free_hyp_pgds(void);
 
+void unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 size,
+			bool may_block);
 void stage2_unmap_vm(struct kvm *kvm);
 int kvm_init_stage2_mmu(struct kvm *kvm, struct kvm_s2_mmu *mmu, unsigned long type);
 void kvm_uninit_stage2_mmu(struct kvm *kvm);

arch/arm64/include/asm/kvm_rme.h
@@ -45,6 +45,15 @@ enum realm_state {
 	REALM_STATE_DEAD
 };
 
+#define REALM_HUGETLB_FOLIO_NUM 509
+
+/* Collect realm mapped folios */
+struct realm_hugetlb_folios {
+	struct list_head page_node;
+	unsigned long folio_num;
+	unsigned long folio_addr[REALM_HUGETLB_FOLIO_NUM];
+};
+
 /**
  * struct realm - Additional per VM data for a Realm
  *
@@ -64,14 +73,17 @@ struct realm {
 	unsigned long num_aux;
 	unsigned int vmid;
 	unsigned int ia_bits;
-#ifndef __GENKSYMS__
+
 #ifdef CONFIG_HISI_CCA
-	bool hisi_cca_enable;
+	KABI_EXTEND(bool hisi_cca_enable)
 #endif
 #ifdef CONFIG_HISI_CCADA_HOST
-	struct list_head rdev_list;
+	KABI_EXTEND(struct list_head rdev_list)
 #endif
-#endif
+	KABI_EXTEND(spinlock_t realm_lock)
+	KABI_EXTEND(struct list_head page_list)
+	KABI_EXTEND(struct list_head hugetlb_page_list)
+	KABI_EXTEND(struct realm_hugetlb_folios *cur_rhf)
 };
 
 /**
@@ -157,6 +169,8 @@ static inline bool kvm_realm_is_private_address(struct realm *realm,
 {
 	return !(addr & BIT(realm->ia_bits - 1));
 }
+
+int realm_add_hugetlb_folios(struct realm *realm, struct folio *folio);
 
 #ifdef CONFIG_HISI_CCA
 static inline bool kvm_realm_supports_hisi_cca(struct realm *realm)

arch/arm64/include/asm/kvm_rme_hisi_cca.h
@@ -10,7 +10,7 @@
 #include <asm/kvm_host.h>
 #include <linux/kvm_host.h>
 
-int rmi_cca_hisi_delegate_range(unsigned long start_addr, unsigned long size);
+int rmi_cca_hisi_delegate_range_get(unsigned long start_addr, unsigned long size, void *realm_p);
 
 static inline int rmi_cca_hisi_undelegate_range(unsigned long start_addr,
 						unsigned long size)

arch/arm64/include/asm/rmi_cmds.h
@@ -115,6 +115,8 @@ static inline int rmi_features(unsigned long index, unsigned long *out)
 
 int rmi_granule_delegate(unsigned long phys);
 
+int rmi_granule_delegate_get(unsigned long phys, void *realm_p);
+
 /**
  * rmi_granule_undelegate() - Undelegate a granule
  * @phys: PA of the granule

arch/arm64/kvm/mmu.c
@@ -336,7 +336,7 @@ static void __unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 
 					   may_block));
 }
 
-static void unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 size,
+void unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 size,
 			       bool may_block)
 {
 	__unmap_stage2_range(mmu, start, size, may_block, false);
@@ -1045,11 +1045,13 @@ void kvm_free_stage2_pgd(struct kvm_s2_mmu *mmu)
 	if (_kvm_is_realm(kvm) &&
 	    (kvm_realm_state(kvm) != REALM_STATE_DEAD &&
 	     kvm_realm_state(kvm) != REALM_STATE_NONE)) {
-		struct realm *realm = &kvm->arch.realm;
-
-		unmap_stage2_range(mmu, 0, BIT(realm->ia_bits - 1), true);
+#ifdef CONFIG_HISI_CCADA_HOST
+		write_unlock(&kvm->mmu_lock);
+#else
+		unmap_stage2_range(mmu, 0, BIT(kvm->arch.realm.ia_bits - 1), true);
 		write_unlock(&kvm->mmu_lock);
 		kvm_realm_destroy_rtts(kvm, pgt->ia_bits);
+#endif
 
 		/*
 		 * The physical PGD pages are delegated to the RMM, so cannot

arch/arm64/kvm/rme-hisi-cca.c
@@ -116,7 +116,7 @@ static int hisi_cca_create_data_page_unknown(struct realm *realm,
 	int ret, offset;
 
 	for (offset = 0; offset < PAGE_SIZE; offset += RMM_PAGE_SIZE) {
-		if (rmi_granule_delegate(phys)) {
+		if (rmi_granule_delegate_get(phys, realm)) {
 			/*
 			 * It's likely we raced with another VCPU on the same
 			 * fault. Assume the other VCPU has handled the fault
@@ -188,7 +188,7 @@ static int hisi_cca_create_data_block(struct realm *realm, unsigned long ipa,
 	dst_phys = page_to_phys(dst_pages[0]);
 	tmp_phys = page_to_phys(tmp_block);
 
-	if (rmi_cca_hisi_delegate_range(dst_phys, RMM_L2_BLOCK_SIZE) != RMI_SUCCESS)
+	if (rmi_cca_hisi_delegate_range_get(dst_phys, RMM_L2_BLOCK_SIZE, realm) != RMI_SUCCESS)
 		return -ENXIO;
 
 	ret = rmi_cca_hisi_block_create(virt_to_phys(realm->rd), dst_phys, ipa,
@@ -233,7 +233,7 @@ static int hisi_cca_create_data_block_unknown(struct realm *realm,
 
 	dst_phys = page_to_phys(dst_pages[0]);
 
-	if (rmi_cca_hisi_delegate_range(dst_phys, map_size)) {
+	if (rmi_cca_hisi_delegate_range_get(dst_phys, map_size, realm)) {
 		/* Race with another thread. */
 		return 0;
 	}
@@ -607,11 +607,12 @@ int realm_hisi_cca_set_ipa_state(struct kvm_vcpu *vcpu, unsigned long start,
 	return ret;
 }
 
-int rmi_cca_hisi_delegate_range(unsigned long start_addr, unsigned long size)
+int rmi_cca_hisi_delegate_range_get(unsigned long start_addr, unsigned long size, void *realm_p)
 {
 	unsigned long start = start_addr;
 	unsigned long end = start_addr + size;
 	struct folio *folio;
+	struct realm *realm = (struct realm *)realm_p;
 
 	while (start < end) {
 		folio = page_folio(phys_to_page(start));
@@ -619,11 +620,28 @@ int rmi_cca_hisi_delegate_range(unsigned long start_addr, unsigned long size)
 			return -EINVAL;
 
 		if (folio_test_hugetlb(folio)) {
-			if (rme_isolate_hugetlb(folio))
+			if (rme_isolate_hugetlb(folio)) {
 				folio_put(folio);
+				if (realm) {
+					int ret;
+
+					ret = realm_add_hugetlb_folios(realm, folio);
+					if (ret)
+						return ret;
+				}
+			}
 		} else if (folio_test_lru(folio)) {
-			if (folio_isolate_lru(folio))
+			if (folio_isolate_lru(folio)) {
 				folio_put(folio);
+				if (realm) {
+					unsigned long flags;
+
+					spin_lock_irqsave(&realm->realm_lock, flags);
+					folio_get(folio);
+					list_add(&folio->lru, &realm->page_list);
+					spin_unlock_irqrestore(&realm->realm_lock, flags);
+				}
+			}
 		}
 		start = (folio_pfn(folio) + folio_nr_pages(folio)) * PAGE_SIZE;
 	}

arch/arm64/kvm/rme.c
@@ -908,7 +908,7 @@ static int realm_create_protected_data_granule(struct realm *realm,
 	phys_addr_t rd = virt_to_phys(realm->rd);
 	int ret;
 
-	if (rmi_granule_delegate(dst_phys))
+	if (rmi_granule_delegate_get(dst_phys, realm))
 		return -ENXIO;
 
 	ret = rmi_data_create(rd, dst_phys, ipa, src_phys, flags);
@@ -1137,7 +1137,7 @@ int realm_map_protected(struct realm *realm,
 	}
 
 	for (size = 0; size < map_size; size += RMM_PAGE_SIZE) {
-		if (rmi_granule_delegate(phys)) {
+		if (rmi_granule_delegate_get(phys, realm)) {
 			/*
 			 * It's likely we raced with another VCPU on the same
 			 * fault. Assume the other VCPU has handled the fault
@@ -1583,7 +1583,9 @@ static int kvm_create_realm(struct kvm *kvm)
 	}
 
 	WRITE_ONCE(realm->state, REALM_STATE_NEW);
-
+	INIT_LIST_HEAD(&realm->page_list);
+	INIT_LIST_HEAD(&realm->hugetlb_page_list);
+	realm->cur_rhf = NULL;
 	/* The realm is up, free the parameters.  */
 	free_page((unsigned long)realm->params);
 	realm->params = NULL;
@@ -1714,8 +1716,108 @@ int _kvm_realm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap)
 	return r;
 }
 
+int realm_add_hugetlb_folios(struct realm *realm, struct folio *folio)
+{
+	int ret = 0;
+	unsigned long flags;
+	struct realm_hugetlb_folios *rhf;
+	struct realm_hugetlb_folios *free_rhf = NULL;
+
+	spin_lock_irqsave(&realm->realm_lock, flags);
+	rhf = realm->cur_rhf;
+	if (!rhf || rhf->folio_num >= REALM_HUGETLB_FOLIO_NUM) {
+		realm->cur_rhf = NULL;
+		spin_unlock_irqrestore(&realm->realm_lock, flags);
+		free_rhf = (struct realm_hugetlb_folios *)get_zeroed_page(GFP_KERNEL);
+		if (!free_rhf)
+			return -ENOMEM;
+		pr_info("Alloc new hugetlb_folio page.\n");
+		INIT_LIST_HEAD(&free_rhf->page_node);
+
+		spin_lock_irqsave(&realm->realm_lock, flags);
+		if (!realm->cur_rhf) {
+			realm->cur_rhf = free_rhf;
+			list_add(&free_rhf->page_node, &realm->hugetlb_page_list);
+			free_rhf = NULL;
+		}
+		rhf = realm->cur_rhf;
+		if (rhf->folio_num >= REALM_HUGETLB_FOLIO_NUM) {
+			ret = -ENOMEM;
+			goto out;
+		}
+	}
+	rhf->folio_addr[rhf->folio_num] = (unsigned long)folio;
+	rhf->folio_num++;
+
+out:
+	spin_unlock_irqrestore(&realm->realm_lock, flags);
+	if (free_rhf)
+		free_page((unsigned long)free_rhf);
+	return ret;
+}
+
+int rmi_granule_delegate_get(unsigned long phys, void *realm_p)
+{
+	int ret;
+	struct realm *realm = (struct realm *)realm_p;
+	struct folio *folio = page_folio(phys_to_page(phys));
+
+	if (folio_test_hugetlb(folio)) {
+		if (rme_isolate_hugetlb(folio)) {
+			folio_put(folio);
+			if (realm) {
+				ret = realm_add_hugetlb_folios(realm, folio);
+				if (ret)
+					return ret;
+			}
+		}
+	} else if (folio_test_lru(folio)) {
+		if (folio_isolate_lru(folio)) {
+			folio_put(folio);
+			if (realm) {
+				unsigned long flags;
+
+				spin_lock_irqsave(&realm->realm_lock, flags);
+				folio_get(folio);
+				list_add(&folio->lru, &realm->page_list);
+				spin_unlock_irqrestore(&realm->realm_lock, flags);
+			}
+		}
+	}
+	return _rmi_granule_delegate(phys);
+}
+
+static void realm_put_folios(struct realm *realm)
+{
+	unsigned long flags;
+	struct folio *folio, *next;
+	struct realm_hugetlb_folios *rhf, *node;
+	unsigned long i;
+	LIST_HEAD(free_list);
+
+	spin_lock_irqsave(&realm->realm_lock, flags);
+	list_for_each_entry_safe(folio, next, &realm->page_list, lru) {
+		list_del(&folio->lru);
+		folio_put(folio);
+	}
+
+	list_for_each_entry_safe(rhf, node, &realm->hugetlb_page_list, page_node) {
+		for (i = 0; i < rhf->folio_num; i++)
+			folio_put((struct folio *)rhf->folio_addr[i]);
+		list_move(&rhf->page_node, &free_list);
+	}
+	realm->cur_rhf = NULL;
+	spin_unlock_irqrestore(&realm->realm_lock, flags);
+
+	list_for_each_entry_safe(rhf, node, &free_list, page_node) {
+		list_del(&rhf->page_node);
+		free_page((unsigned long)rhf);
+	}
+}
+
 void _kvm_destroy_realm(struct kvm *kvm)
 {
+	struct kvm_s2_mmu *mmu = &kvm->arch.mmu;
 	struct realm *realm = &kvm->arch.realm;
 	size_t pgd_size = kvm_pgtable_stage2_pgd_size(kvm->arch.vtcr);
 	int i;
@@ -1729,9 +1831,14 @@ void _kvm_destroy_realm(struct kvm *kvm)
 		return;
 
 #ifdef CONFIG_HISI_CCADA_HOST
+	write_lock(&kvm->mmu_lock);
+	unmap_stage2_range(mmu, 0, BIT(realm->ia_bits - 1), true);
+	write_unlock(&kvm->mmu_lock);
+	kvm_realm_destroy_rtts(kvm, mmu->pgt->ia_bits);
 	/* undelegate and detach dev from realm */
 	realm_destroy_dev_list(realm);
 #endif
+	realm_put_folios(realm);
 
 	WRITE_ONCE(realm->state, REALM_STATE_DYING);
 