OERV-RVCI/rvck-olk

commit 92af6a401f0b589dc88c7d9bff8dd868445edb2a

hisi-cca-da: Support pcipc sriov capability
kunpeng inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8414
CVE: NA

--------------------------------------------------------------------

Add PCIe PCIPC SR-IOV support for HISI_CCA_DA, extending RMI SMMU APIs for
page table/STE operations, adding PCIPC NS device tracking via bitmap,
and implementing dedicated CCA_REALM_NS_S2 pgtable format with separate
map/unmap logic. Extend realm dev entries with NS VTTBR/PCIPC flags,
add L2 stream table init for PCIPC devices, and update SMMU domain/stage
handling to support PCIPC NS mode with proper STE writing and granule
delegation.

Signed-off-by: zhangwei <zhangwei375@huawei.com>
Signed-off-by: hejiajing <hejiajing2@h-partners.com>
Signed-off-by: xuce <xuce10@h-partners.com>

drivers/iommu/arm/arm-smmu-v3/arm-r-smmu-v3.h
@@ -74,7 +74,7 @@ void realm_smmu_domain_clear(struct arm_smmu_domain *smmu_domain);
 
 static inline void realm_smmu_set_stage(struct arm_smmu_domain *smmu_domain)
 {
-	if (smmu_domain->realm)
+	if (smmu_domain->realm || smmu_domain->pcipc_ns)
 		smmu_domain->stage = ARM_SMMU_DOMAIN_S2;
 }
 

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -39,6 +39,7 @@
 
 #ifdef CONFIG_HISI_CCADA_HOST
 #include "arm-r-smmu-v3.h"
+#include <asm/hisi_cca_da.h>
 #endif
 
 static bool disable_msipolling;
@@ -2623,6 +2624,12 @@ static struct iommu_domain *arm_smmu_domain_alloc_paging(struct device *dev)
 		struct arm_smmu_master *master = dev_iommu_priv_get(dev);
 		int ret;
 
+#ifdef CONFIG_HISI_CCADA_HOST
+		if (dev_is_pci(dev) && is_hisi_pcipc_ns(dev) &&
+		    arm_smmu_support_rme(master->smmu))
+			smmu_domain->pcipc_ns = true;
+#endif
+
 		ret = arm_smmu_domain_finalise(smmu_domain, master->smmu, 0);
 		if (ret) {
 			kfree(smmu_domain);
@@ -2754,6 +2761,8 @@ static int arm_smmu_domain_finalise(struct arm_smmu_domain *smmu_domain,
 #ifdef CONFIG_HISI_CCADA_HOST
 	if (smmu_domain->realm)
 		fmt = CCA_REALM_S2;
+	else if (smmu_domain->pcipc_ns)
+		fmt = CCA_REALM_NS_S2;
 #endif
 
 	if (smmu->features & ARM_SMMU_FEAT_HD)

arch/arm64/include/asm/hisi_cca_da.h
@@ -3,6 +3,7 @@
 #define __ASM_HISI_CCA_DA_H
 
 #include <asm/kvm_host.h>
+#include "../../../../drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h"
 
 #define MAX_DEV_PER_PORT 256
 struct rmi_dev_delegate_params {
@@ -21,8 +22,10 @@ struct rdev_node {
 struct realm_dev_entry {
 	struct device *dev;
 	u64 vttbr;
+	u64 ns_vttbr;
 	u64 msi_iova;
 	bool realm;
+	bool pcipc_ns;
 	struct hlist_node node;
 };
 
@@ -37,10 +40,19 @@ void kvm_rme_unassign_device(struct pci_dev *pdev, struct kvm *kvm);
 int kvm_arm_vcpu_rme_dev_validate(struct kvm_vcpu *vcpu,
 				  struct kvm_arm_rme_dev_validate *args);
 
+void hisi_pcipc_ns_add(const struct pci_device_id *id_table);
+void hisi_pcipc_ns_remove(const struct pci_device_id *id_table);
+bool is_hisi_pcipc_ns(struct device *dev);
+
 struct realm *rme_get_realm(u64 vttbr);
-void rme_add_dev_entry(struct device *dev, u64 vttbr, bool realm);
+void rme_add_dev_entry(struct device *dev, u64 vttbr, bool realm, u64 ns_vttbr,
+		       bool pcipc_ns);
+u64 rme_get_ns_vttbr(struct device *dev);
 void rme_update_msi_iova(u64 vttbr, u64 msi_iova);
 bool rme_is_realm_dev(struct device *dev);
+bool rme_is_pcipc_ns_dev(struct device *dev);
 void rme_remove_dev_entry(struct device *dev);
+
+int realm_smmu_init_l2_strtab(struct arm_smmu_device *smmu, u32 sid);
 
 #endif

arch/arm64/include/asm/rmi_cmds.h
@@ -1008,6 +1008,143 @@ static inline int rmi_smmu_send_cmdlist(unsigned long ioaddr,
 	return regs.a0;
 }
 
+/**
+ * rmi_smmu_map() - Create non-secure mapping from iova to pa for the realm smmu
+ * @pgd: Realm page table dentry
+ * @iova: Start iova
+ * @pa: Start pa
+ * @prot: Prot of the PTE(page table entry)
+ * @page_attr: Size and page count of mappings
+ * @map_cnt: Number of successful mappings
+ *
+ * Create multiple non-secure mapping from iova to pa in the realm smmu stage-2
+ * page table
+ *
+ * Return: RMI return code
+ */
+static inline int rmi_smmu_map(unsigned long pgd, unsigned long iova,
+			       unsigned long pa, unsigned long prot,
+			       unsigned long page_attr, unsigned long *map_cnt)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_HISI_EXT, CCADA_SMMU_MAP,
+		pgd, iova, pa, prot, page_attr
+	};
+
+	arm_smccc_1_2_smc(&regs, &regs);
+
+	*map_cnt = regs.a1;
+
+	return regs.a0;
+}
+
+/**
+ * rmi_smmu_page_table_create() - Create realm page-table for given iova and level
+ * @pgd: Realm page table dentry
+ * @iova: Given iova
+ * @tbl_entry: Relam page table entry's PA and attribute
+ * @tbl_size: Relam page table size
+ * @lvl: Create page table at given level
+ *
+ * Create realm page-table for given iova and level
+ *
+ * Return: RMI return code
+ */
+static inline int rmi_smmu_page_table_create(unsigned long pgd,
+					     unsigned long iova,
+					     unsigned long tbl_entry,
+					     unsigned long tbl_size,
+					     unsigned long lvl)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_HISI_EXT, CCADA_SMMU_PAGE_TABLE_CREATE,
+		pgd, iova, tbl_entry, tbl_size, lvl
+	};
+
+	arm_smccc_1_2_smc(&regs, &regs);
+
+	return regs.a0;
+}
+
+/**
+ * rmi_smmu_page_table_destroy() - Destroy realm page-table for given iova and level
+ * @pgd: Realm page table dentry
+ * @iova: Given iova
+ * @tbl_pa: PA of realm page table
+ * @tbl_size: Relam page table size
+ * @lvl: Create page table at given level
+ *
+ * Destroy realm page-table for given iova and level
+ *
+ * Return: RMI return code
+ */
+static inline int rmi_smmu_page_table_destroy(unsigned long pgd,
+					      unsigned long iova,
+					      unsigned long tbl_pa,
+					      unsigned long tbl_size,
+					      unsigned long lvl)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_HISI_EXT, CCADA_SMMU_PAGE_TABLE_DESTROY,
+		pgd, iova, tbl_pa, tbl_size, lvl
+	};
+
+	arm_smccc_1_2_smc(&regs, &regs);
+
+	return regs.a0;
+}
+
+/**
+ * rmi_smmu_read_pte() - Read realm PTE
+ * @pgd: Realm page table dentry
+ * @iova: Given iova
+ * @lvl: Level of PTE
+ * @pte: Value of PTE
+ *
+ * Read realm PTE(Page table entry) for given iova and level
+ *
+ * Return: RMI return code
+ */
+static inline int rmi_smmu_read_pte(unsigned long pgd, unsigned long iova,
+				    unsigned long lvl, unsigned long *pte)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_HISI_EXT, CCADA_SMMU_READ_PTE,
+		pgd, iova, lvl
+	};
+
+	arm_smccc_1_2_smc(&regs, &regs);
+
+	*pte = regs.a1;
+	return regs.a0;
+}
+
+/**
+ * rmi_smmu_ste_write() - Write realm smmu stream table entry
+ * @smmu_addr: PA of the realm smmu
+ * @sid: Steam id
+ * @ste_pa: PA of stream table entry
+ * @lvl_strtab: Level of stream table
+ *
+ * Write realm smmu stream table entry
+ *
+ * Return: RMI return code
+ */
+static inline int rmi_smmu_ste_write(unsigned long smmu_addr,
+				     unsigned long sid,
+				     unsigned long ste_pa,
+				     unsigned long lvl_strtab)
+{
+	struct arm_smccc_1_2_regs regs = {
+		SMC_RMI_HISI_EXT, CCADA_SMMU_STE_WRITE,
+		smmu_addr, sid, ste_pa, lvl_strtab
+	};
+
+	arm_smccc_1_2_smc(&regs, &regs);
+
+	return regs.a0;
+}
+
 #endif
 
 #endif /* __ASM_RMI_CMDS_H */

arch/arm64/kvm/hisi-cca-da.c
@@ -13,6 +13,7 @@
 #define PCI_DEVICE_ID_HUAWEI_ZIP_PF	0xa250
 #define PCI_DEVICE_ID_HUAWEI_SEC_PF	0xa255
 #define PCI_DEVICE_ID_HUAWEI_HPRE_PF	0xa258
+#define PCI_BDF_MASK			0xffff
 
 struct dev_hash_entry {
 	u16 root_bdf; /* The bdf of the root device: pcie root port, or acc PF */
@@ -27,11 +28,77 @@ static DEFINE_SPINLOCK(g_dev_htable_lock);
 static DEFINE_HASHTABLE(g_realm_dev_htable, MAX_REALM_DEV_NUM_ORDER);
 static DEFINE_SPINLOCK(g_realm_dev_lock);
 
+static DECLARE_BITMAP(g_pcipc_ns, (PCI_BDF_MASK + 1));
+static DEFINE_SPINLOCK(g_pcipc_ns_lock);
+
 bool is_support_rme(void)
 {
 	return static_branch_unlikely(&kvm_rme_is_available);
 }
 EXPORT_SYMBOL_GPL(is_support_rme);
+
+void hisi_pcipc_ns_remove(const struct pci_device_id *id_table)
+{
+	const struct pci_device_id *ent;
+	struct pci_dev *pdev = NULL;
+	uint16_t bdf;
+
+	if (!is_support_rme())
+		return;
+
+	spin_lock(&g_pcipc_ns_lock);
+	for_each_pci_dev(pdev) {
+		ent = pci_match_id(id_table, pdev);
+		if (!ent)
+			continue;
+		bdf = pci_dev_id(pdev);
+		bitmap_clear(g_pcipc_ns, bdf, 1);
+	}
+	spin_unlock(&g_pcipc_ns_lock);
+}
+EXPORT_SYMBOL_GPL(hisi_pcipc_ns_remove);
+
+/*
+ * hisi_pcipc_ns_add should be called before pci_register_driver
+ */
+void hisi_pcipc_ns_add(const struct pci_device_id *id_table)
+{
+	const struct pci_device_id *ent;
+	struct pci_dev *pdev = NULL;
+	uint16_t bdf;
+
+	if (!is_support_rme())
+		return;
+
+	spin_lock(&g_pcipc_ns_lock);
+	for_each_pci_dev(pdev) {
+		ent = pci_match_id(id_table, pdev);
+		if (!ent)
+			continue;
+
+		bdf = pci_dev_id(pdev);
+		bitmap_set(g_pcipc_ns, bdf, 1);
+	}
+	spin_unlock(&g_pcipc_ns_lock);
+}
+EXPORT_SYMBOL_GPL(hisi_pcipc_ns_add);
+
+bool is_hisi_pcipc_ns(struct device *dev)
+{
+	bool is_pcipc_ns = false;
+
+	if (!is_support_rme() || !dev)
+		return false;
+
+	if (dev_is_pci(dev)) {
+		spin_lock(&g_pcipc_ns_lock);
+		is_pcipc_ns = bitmap_read(g_pcipc_ns, pci_dev_id(to_pci_dev(dev)), 1);
+		spin_unlock(&g_pcipc_ns_lock);
+	}
+
+	return is_pcipc_ns;
+}
+EXPORT_SYMBOL_GPL(is_hisi_pcipc_ns);
 
 struct realm *rme_get_realm(u64 vttbr)
 {
@@ -69,21 +136,26 @@ struct realm *rme_get_realm(u64 vttbr)
 }
 
 static void rme_dev_entry_set(struct realm_dev_entry *dev_entry,
-			      struct device *dev, u64 vttbr, bool realm)
+			      struct device *dev, u64 vttbr, bool realm,
+			      u64 ns_vttbr, bool pcipc_ns)
 {
 	dev_entry->dev = dev;
 	dev_entry->vttbr = vttbr;
 	dev_entry->realm = realm;
+	dev_entry->ns_vttbr = ns_vttbr;
+	dev_entry->pcipc_ns = pcipc_ns;
 }
 
-void rme_add_dev_entry(struct device *dev, u64 vttbr, bool realm)
+void rme_add_dev_entry(struct device *dev, u64 vttbr, bool realm, u64 ns_vttbr,
+		       bool pcipc_ns)
 {
 	struct realm_dev_entry *dev_entry;
 
 	spin_lock(&g_realm_dev_lock);
 	hash_for_each_possible(g_realm_dev_htable, dev_entry, node, (u64)dev) {
 		if (dev_entry->dev == dev) {
-			rme_dev_entry_set(dev_entry, dev, vttbr, realm);
+			rme_dev_entry_set(dev_entry, dev, vttbr, realm, ns_vttbr,
+					  pcipc_ns);
 			spin_unlock(&g_realm_dev_lock);
 			return;
 		}
@@ -94,10 +166,11 @@ void rme_add_dev_entry(struct device *dev, u64 vttbr, bool realm)
 		pr_err("Alloc realm_dev_entry failed\n");
 		return;
 	}
-	rme_dev_entry_set(dev_entry, dev, vttbr, realm);
+	rme_dev_entry_set(dev_entry, dev, vttbr, realm, ns_vttbr, pcipc_ns);
 	hash_add(g_realm_dev_htable, &dev_entry->node, (u64)dev);
 	spin_unlock(&g_realm_dev_lock);
 }
+EXPORT_SYMBOL_GPL(rme_add_dev_entry);
 
 void rme_update_msi_iova(u64 vttbr, u64 msi_iova)
 {
@@ -133,10 +206,25 @@ u64 rme_get_msi_iova(struct device *dev)
 	return rme_get_dev_entry(dev).msi_iova;
 }
 
+u64 rme_get_ns_vttbr(struct device *dev)
+{
+	return rme_get_dev_entry(dev).ns_vttbr;
+}
+EXPORT_SYMBOL_GPL(rme_get_ns_vttbr);
+
 bool rme_is_realm_dev(struct device *dev)
 {
 	return rme_get_dev_entry(dev).realm;
 }
+EXPORT_SYMBOL_GPL(rme_is_realm_dev);
+
+bool rme_is_pcipc_ns_dev(struct device *dev)
+{
+	struct realm_dev_entry dev_entry = rme_get_dev_entry(dev);
+
+	return !dev_entry.realm && dev_entry.pcipc_ns;
+}
+EXPORT_SYMBOL_GPL(rme_is_pcipc_ns_dev);
 
 void rme_remove_dev_entry(struct device *dev)
 {
@@ -153,6 +241,54 @@ void rme_remove_dev_entry(struct device *dev)
 	}
 	spin_unlock(&g_realm_dev_lock);
 }
+EXPORT_SYMBOL_GPL(rme_remove_dev_entry);
+
+int realm_smmu_init_l2_strtab(struct arm_smmu_device *smmu, u32 sid)
+{
+	int ret;
+	size_t size;
+	struct arm_smmu_ste *l2ptr;
+	struct realm_smmu_strtab_cfg *cfg = &smmu->realm.strtab_cfg;
+	struct arm_smmu_strtab_l1_desc *desc = &cfg->l1_desc[sid >> STRTAB_SPLIT];
+
+	if (desc->l2ptr)
+		return 0;
+
+	size = (1 << STRTAB_SPLIT) * sizeof(struct arm_smmu_ste);
+	desc->span = STRTAB_SPLIT + 1;
+
+	l2ptr = dma_alloc_coherent(smmu->dev, size, &desc->l2ptr_dma, GFP_KERNEL);
+	if (!l2ptr)
+		return -ENOMEM;
+
+	ret = granule_delegate_range(desc->l2ptr_dma, size);
+	if (ret) {
+		dev_err(smmu->dev,
+			"failed to delegate realm l2 stream table for SID %u\n",
+			sid);
+		goto out_free;
+	}
+
+	ret = realm_config_strtab_l2(SMMU_STRTAB_L2_INIT, smmu->realm.ioaddr,
+				     cfg->strtab_dma, sid, desc->l2ptr_dma,
+				     desc->span);
+	if (ret) {
+		dev_err(smmu->dev,
+			"failed to init realm l2 stream table for SID %u\n",
+			sid);
+		goto out_undelegate;
+	}
+	desc->l2ptr = l2ptr;
+	return 0;
+
+out_undelegate:
+	if (WARN_ON(granule_undelegate_range(desc->l2ptr_dma, size)))
+		return ret;
+out_free:
+	dma_free_coherent(smmu->dev, size, l2ptr, desc->l2ptr_dma);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(realm_smmu_init_l2_strtab);
 
 /**
  * get_child_devices_rec - Traverse pcie topology to find child devices

drivers/iommu/arm/arm-smmu-v3/arm-r-smmu-v3.c
@@ -70,15 +70,57 @@ static int realm_smmu_write_reg_sync(struct arm_smmu_device *smmu, u32 val,
 					    ARM_SMMU_POLL_TIMEOUT_US);
 }
 
+void realm_smmu_write_ste(struct arm_smmu_master *master, u32 sid,
+			  const struct arm_smmu_ste *target)
+{
+	u64 ns_vttbr;
+	bool lvl_strtab;
+	struct arm_smmu_ste *rste;
+	struct arm_smmu_device *smmu = master->smmu;
+
+	if (!arm_smmu_support_rme(smmu))
+		return;
+
+	if (!rme_is_pcipc_ns_dev(master->dev))
+		return;
+
+	lvl_strtab = !!(smmu->features & ARM_SMMU_FEAT_2_LVL_STRTAB);
+	if (lvl_strtab) {
+		if (realm_smmu_init_l2_strtab(smmu, sid))
+			return;
+	}
+
+	rste = (struct arm_smmu_ste *)get_zeroed_page(GFP_KERNEL);
+	if (!rste) {
+		dev_err(smmu->dev, "failed to allocate realm ste page\n");
+		return;
+	}
+
+	memcpy(rste, target, sizeof(struct arm_smmu_ste));
+
+	if (rste->data[3] & STRTAB_STE_3_S2TTB_MASK) {
+		ns_vttbr = rme_get_ns_vttbr(master->dev);
+		rste->data[3] =  ns_vttbr & STRTAB_STE_3_S2TTB_MASK;
+	}
+
+	if (rmi_smmu_ste_write(smmu->realm.ioaddr, sid, virt_to_phys(rste),
+			       lvl_strtab))
+		dev_err(smmu->dev, "failed to write realm ste\n");
+
+	free_page((unsigned long)rste);
+}
+
 static bool is_realm_dev_attach(struct arm_smmu_domain *smmu_domain)
 {
-	return smmu_domain->realm;
+	return smmu_domain->realm || smmu_domain->pcipc_ns;
 }
 
 static bool is_realm_dev_detach(struct arm_smmu_domain *smmu_domain,
 				struct arm_smmu_master *master)
 {
 	if (!smmu_domain->realm && rme_is_realm_dev(master->dev))
+		return true;
+	else if (!smmu_domain->pcipc_ns && rme_is_pcipc_ns_dev(master->dev))
 		return true;
 	else
 		return false;
@@ -89,7 +131,7 @@ void realm_smmu_attach_dev(struct arm_smmu_domain *smmu_domain,
 {
 	int i, j;
 	bool attach;
-	u64 vttbr;
+	u64 vttbr, ns_vttbr;
 	struct arm_smmu_device *smmu = master->smmu;
 	struct realm_smmu_device *realm = &smmu->realm;
 	const struct io_pgtable_cfg *pgtbl_cfg =
@@ -109,10 +151,12 @@ void realm_smmu_attach_dev(struct arm_smmu_domain *smmu_domain,
 	else
 		return;
 
-	vttbr = pgtbl_cfg->arm_lpae_s2_cfg.vttbr;
+	vttbr = pgtbl_cfg->realm_s2_cfg.vttbr;
+	ns_vttbr = pgtbl_cfg->realm_s2_cfg.ns_vttbr;
 
 	if (attach)
-		rme_add_dev_entry(dev, vttbr, smmu_domain->realm);
+		rme_add_dev_entry(dev, vttbr, smmu_domain->realm, ns_vttbr,
+				  smmu_domain->pcipc_ns);
 	else
 		rme_remove_dev_entry(dev);
 

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -951,6 +951,7 @@ struct arm_smmu_domain {
 #endif
 #ifdef CONFIG_HISI_CCADA_HOST
 	bool				realm;
+	bool				pcipc_ns;
 #endif
 };
 

drivers/iommu/io-pgtable-realm.c
@@ -449,6 +449,134 @@ static size_t realm_host_unmap(struct realm_io_pgtable *data,
 	return realm_host_unmap(data, gather, iova, size, pgcount, lvl + 1, ptep);
 }
 
+static void *__realm_alloc_pages(size_t size, gfp_t gfp,
+				 struct io_pgtable_cfg *cfg)
+{
+	struct device *dev = cfg->iommu_dev;
+	int order = get_order(size);
+	struct page *p;
+	void *pages;
+
+	p = alloc_pages_node(dev_to_node(dev), gfp | __GFP_ZERO, order);
+	if (unlikely(!p))
+		return NULL;
+
+	pages = page_address(p);
+	if (granule_delegate_range(virt_to_phys(pages), size)) {
+		free_pages((unsigned long)pages, order);
+		return NULL;
+	}
+
+	return pages;
+}
+
+static void __realm_free_pages(void *pages, size_t size,
+			       struct io_pgtable_cfg *cfg)
+{
+	/* If the undelegate fails then leak the pages */
+	if (WARN_ON(granule_undelegate_range(virt_to_phys(pages), size)))
+		return;
+
+	free_pages((unsigned long)pages, get_order(size));
+}
+
+static int realm_ns_map(struct realm_io_pgtable *data, unsigned long iova,
+			phys_addr_t paddr, size_t size, size_t pgcount,
+			realm_iopte prot, int lvl, realm_iopte *pgd,
+			gfp_t gfp, size_t *mapped)
+{
+	int ret = 0;
+	realm_iopte *cptep;
+	phys_addr_t phys, tbl_entry;
+	size_t page_attr, block_size;
+	size_t tblsz = REALM_GRANULE(data);
+	unsigned long pgdp = virt_to_phys(pgd), map_cnt;
+
+	if (size > SZ_1G || pgcount > U32_MAX)
+		return -EINVAL;
+
+	page_attr = size | (pgcount << 32);
+
+	lvl = rmi_smmu_map(pgdp, iova, paddr, prot, page_attr, &map_cnt);
+	if (lvl == REALM_MAX_LEVELS)
+		*mapped += map_cnt * size;
+	if (lvl < 0)
+		return -EINVAL;
+
+	for (; lvl < REALM_MAX_LEVELS; lvl++) {
+		block_size = REALM_BLOCK_SIZE(lvl, data);
+
+		/* If we can install a leaf entry at this level, then do so */
+		if (size == block_size) {
+			lvl = rmi_smmu_map(pgdp, iova, paddr, prot, page_attr, &map_cnt);
+			if (lvl == REALM_MAX_LEVELS)
+				*mapped += map_cnt * size;
+			break;
+		}
+
+		/* We can't allocate tables at the final level */
+		if (WARN_ON(lvl >= REALM_MAX_LEVELS - 1))
+			return -EINVAL;
+
+		cptep = (realm_iopte *)__realm_alloc_pages(tblsz, GFP_KERNEL,
+							   &data->iop.cfg);
+		if (!cptep)
+			return -ENOMEM;
+
+		phys = virt_to_phys(cptep);
+		tbl_entry = phys | REALM_PTE_TYPE_TABLE;
+		ret = rmi_smmu_page_table_create(pgdp, iova, tbl_entry, tblsz,
+						 lvl);
+		if (ret) {
+			__realm_free_pages(cptep, tblsz, &data->iop.cfg);
+			break;
+		}
+	}
+
+	return ret;
+}
+
+static void realm_ns_free_pgtable(struct realm_io_pgtable *data, int lvl,
+				  void *va, unsigned long iova)
+{
+	unsigned long i, entry_num, pte, table_size, next, pa;
+
+	if (lvl == data->start_level)
+		table_size = REALM_PGD_SIZE(data);
+	else
+		table_size = REALM_GRANULE(data);
+
+	entry_num = table_size / sizeof(realm_iopte);
+	for (i = 0; i < entry_num; i++) {
+		next = iova + (i << REALM_LVL_SHIFT(lvl, data));
+		rmi_smmu_read_pte(virt_to_phys(data->ns_pgd), next, lvl, &pte);
+		if (!pte || iopte_leaf(pte, lvl, data->iop.fmt))
+			continue;
+
+		pa = pte & REALM_PTE_ADDR_MASK;
+		realm_ns_free_pgtable(data, lvl + 1, __va(pa), next);
+	}
+	(void)rmi_smmu_page_table_destroy(virt_to_phys(data->ns_pgd), iova,
+					  virt_to_phys(va), table_size, lvl);
+	__realm_free_pages(va, table_size, &data->iop.cfg);
+}
+
+static size_t realm_ns_unmap(struct realm_io_pgtable *data,
+			     struct iommu_iotlb_gather *gather,
+			     unsigned long iova, size_t size, size_t pgcount,
+			     int lvl, realm_iopte *pgd)
+{
+	unsigned long map_cnt;
+
+	if (size > SZ_1G || pgcount > U32_MAX)
+		return -EINVAL;
+
+	(void)rmi_smmu_map(virt_to_phys(pgd), iova, 0, 0, size | (pgcount << 32),
+			   &map_cnt);
+
+	return map_cnt * size;
+}
+
 static realm_iopte realm_prot_to_pte(struct realm_io_pgtable *data, int prot)
 {
 	realm_iopte pte;
@@ -499,15 +627,28 @@ static int realm_map_pages(struct io_pgtable_ops *ops, unsigned long iova,
 
 	prot = realm_prot_to_pte(data, iommu_prot);
 
-	ret = realm_mmio_map(data, iova, paddr, pgsize, pgcount, prot, lvl,
-			     (realm_iopte *)data->pgd, gfp);
-	if (ret)
-		return ret;
-
 	ret = realm_host_map(data, iova, paddr, pgsize, pgcount, prot,
 				lvl, (realm_iopte *)data->pgd, gfp, mapped);
 	if (ret)
 		return ret;
+
+	if (!data->ns) {
+		ret = realm_mmio_map(data, iova, paddr, pgsize, pgcount, prot, lvl,
+				(realm_iopte *)data->pgd, gfp);
+		if (ret)
+			return ret;
+	} else {
+		size_t r_mapped = 0;
+
+		ret = realm_ns_map(data, iova, paddr, pgsize, pgcount,
+				   prot | REALM_PTE_NS, lvl,
+				   (realm_iopte *)data->ns_pgd, gfp, &r_mapped);
+		if (ret)
+			return ret;
+
+		if (r_mapped != *mapped)
+			return -EPERM;
+	}
 
 	/*
 	 * Synchronise all PTE updates for the new mapping before there's
@@ -603,7 +744,7 @@ static size_t realm_unmap_pages(struct io_pgtable_ops *ops, unsigned long iova,
 	struct io_pgtable_cfg *cfg = &data->iop.cfg;
 	long iaext = (s64)iova >> cfg->ias;
 	int sl = data->start_level;
-	size_t unmap_size;
+	size_t unmapped, r_unmapped;
 	int ret;
 
 	if (WARN_ON(!pgsize || (pgsize & cfg->pgsize_bitmap) != pgsize || !pgcount))
@@ -612,14 +753,22 @@ static size_t realm_unmap_pages(struct io_pgtable_ops *ops, unsigned long iova,
 	if (WARN_ON(iaext))
 		return 0;
 
-	ret = realm_mmio_unmap(ops, data, gather, iova, pgsize, pgcount, sl,
-			      (realm_iopte *)data->pgd);
-	if (ret)
-		return 0;
-
-	unmap_size = realm_host_unmap(data, gather, iova, pgsize, pgcount, sl,
+	unmapped = realm_host_unmap(data, gather, iova, pgsize, pgcount, sl,
 				      (realm_iopte *)data->pgd);
-	return unmap_size;
+
+	if (!data->ns) {
+		ret = realm_mmio_unmap(ops, data, gather, iova, pgsize, pgcount, sl,
+				(realm_iopte *)data->pgd);
+		if (ret)
+			return 0;
+	} else {
+		r_unmapped = realm_ns_unmap(data, gather, iova, pgsize, pgcount,
+					    sl, (realm_iopte *)data->ns_pgd);
+		if (unmapped != r_unmapped)
+			return 0;
+	}
+
+	return unmapped;
 }
 
 static struct realm_io_pgtable *realm_alloc_pgtable(struct io_pgtable_cfg *cfg,
@@ -724,6 +873,19 @@ static struct io_pgtable *__realm_alloc_pgtable_s2(struct io_pgtable_cfg *cfg,
 	vtcr->tsz = 64ULL - cfg->ias;
 	vtcr->sl = ~sl & REALM_VTCR_SL0_MASK;
 
+	if (data->ns) {
+		data->ns_pgd = __realm_alloc_pages(REALM_PGD_SIZE(data), GFP_KERNEL, cfg);
+		if (!data->ns_pgd)
+			goto out_free_data;
+
+		if (rmi_smmu_page_table_create(virt_to_phys(data->ns_pgd), 0,
+					       virt_to_phys(data->ns_pgd),
+					       REALM_PGD_SIZE(data), 0)) {
+			goto out_free_realm_page;
+		}
+		cfg->realm_s2_cfg.ns_vttbr = virt_to_phys(data->ns_pgd);
+	}
+
 	/* Ensure the empty pgd is visible before any actual TTBR write */
 	wmb();
 
@@ -736,6 +898,8 @@ static struct io_pgtable *__realm_alloc_pgtable_s2(struct io_pgtable_cfg *cfg,
 	cfg->arm_lpae_s2_cfg.vttbr = virt_to_phys(data->pgd);
 	return &data->iop;
 
+out_free_realm_page:
+	__realm_free_pages(data->ns_pgd, REALM_PGD_SIZE(data), cfg);
 out_free_data:
 	kfree(data);
 	return NULL;
@@ -744,6 +908,9 @@ out_free_data:
 static void realm_free_pgtable(struct io_pgtable *iop)
 {
 	struct realm_io_pgtable *data = io_pgtable_to_data(iop);
+
+	if (data->ns)
+		realm_ns_free_pgtable(data, data->start_level, data->ns_pgd, 0UL);
 
 	realm_host_free_pgtable(data, data->start_level, data->pgd);
 
@@ -756,7 +923,18 @@ realm_alloc_pgtable_s2(struct io_pgtable_cfg *cfg, void *cookie)
 	return __realm_alloc_pgtable_s2(cfg, cookie, false);
 }
 
+static struct io_pgtable *
+realm_ns_alloc_pgtable_s2(struct io_pgtable_cfg *cfg, void *cookie)
+{
+	return __realm_alloc_pgtable_s2(cfg, cookie, true);
+}
+
 struct io_pgtable_init_fns io_pgtable_realm_s2_init_fns = {
 	.alloc	= realm_alloc_pgtable_s2,
+	.free	= realm_free_pgtable,
+};
+
+struct io_pgtable_init_fns io_pgtable_realm_ns_s2_init_fns = {
+	.alloc	= realm_ns_alloc_pgtable_s2,
 	.free	= realm_free_pgtable,
 };

commit 991f505e0e556c775ea18b8ddbd9a5f3bfb35ee8

hisi-cca-da: Write NS STE for PF drivers.
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

In SR-IOV scenarios, to make REE side PF drivers work, write NS STE
for them.

Fixes: 92af6a401f0b ("hisi-cca-da: Support pcipc sriov capability")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>

drivers/iommu/arm/arm-smmu-v3/arm-r-smmu-v3.h
@@ -5,7 +5,7 @@
 
 #ifndef _ARM_R_SMMU_V3_H
 #define _ARM_R_SMMU_V3_H
-
+#ifdef CONFIG_HISI_CCADA_HOST
 #include <linux/iopoll.h>
 
 #define SMMU_R_IDR0			0
@@ -80,4 +80,7 @@ static inline void realm_smmu_set_stage(struct arm_smmu_domain *smmu_domain)
 		smmu_domain->stage = ARM_SMMU_DOMAIN_S2;
 }
 
+void realm_smmu_write_ste(struct arm_smmu_master *master, u32 sid,
+			  const struct arm_smmu_ste *target);
+#endif /* CONFIG_HISI_CCADA_HOST */
 #endif /* _ARM_R_SMMU_V3_H */

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -1752,6 +1752,10 @@ static void arm_smmu_write_ste(struct arm_smmu_master *master, u32 sid,
 		.sid = sid,
 	};
 
+#ifdef CONFIG_HISI_CCADA_HOST
+	realm_smmu_write_ste(master, sid, target);
+#endif
+
 	arm_smmu_write_entry(&ste_writer.writer, ste->data, target->data);
 
 	/* It's likely that we'll want to use the new STE soon */