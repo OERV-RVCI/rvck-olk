OERV-RVCI/rvck-olk

commit 11a27efee0a38b33d359705cba2e184862afb696

hisi-cca-da: Improve hisi cca dev validate and delegate
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

Modify hisi cca device validate and delegate process.

Fixes: c9eb3f90cea7 ("hisi-cca-da: Support root dev delegate and dev init")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>

arch/arm64/kvm/rme.c
@@ -1806,6 +1806,9 @@ int _kvm_rec_pre_enter(struct kvm_vcpu *vcpu)
 	case RMI_EXIT_RIPAS_CHANGE:
 		kvm_complete_ripas_change(vcpu);
 		break;
+	case RMI_EXIT_DEV_VALIDATE:
+	case RMI_EXIT_DEV_START:
+		kvm_complete_dev_op(vcpu);
 	}
 
 	return 1;

arch/arm64/include/asm/hisi_cca_da.h
@@ -9,7 +9,7 @@
 struct rmi_dev_delegate_params {
 	uint16_t root_bdf; /* BDF of the PCIe root port or KAE PF device */
 	uint16_t num_dev; /* number of attachable devices */
-	uint32_t rsvd; /* padding for 64-bit alignment */
+	uint32_t rsvd;
 	uint16_t devs[MAX_DEV_PER_PORT]; /* BDF of each attachable device */
 };
 
@@ -37,8 +37,7 @@ int realm_attach_devs(struct realm *realm);
 void realm_destroy_dev_list(struct realm *realm);
 int kvm_rme_assign_device(struct pci_dev *pdev, struct kvm *kvm);
 void kvm_rme_unassign_device(struct pci_dev *pdev, struct kvm *kvm);
-int kvm_arm_vcpu_rme_dev_validate(struct kvm_vcpu *vcpu,
-				  struct kvm_arm_rme_dev_validate *args);
+void kvm_complete_dev_op(struct kvm_vcpu *vcpu);
 
 void hisi_pcipc_ns_add(const struct pci_device_id *id_table);
 void hisi_pcipc_ns_remove(const struct pci_device_id *id_table);

arch/arm64/include/asm/rmi_cmds.h
@@ -534,11 +534,11 @@ static inline int rmi_rtt_unmap_unprotected(unsigned long rd,
 
 #ifdef CONFIG_HISI_CCADA_HOST
 /**
- * rmi_dev_init() - Initilaize the dev info
+ * rmi_dev_init() - Initialize the dev info
  * @dev_bdf: Bdf of the pci dev
  * @pa: Pa of the delegated page
  *
- * Initilaize the dev info with the delegated memory.
+ * Initialize the dev info with the delegated memory.
  *
  * Return: RMI return code
  */

arch/arm64/kvm/arm.c
@@ -1975,17 +1975,6 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 			return -EFAULT;
 		return kvm_arm_vcpu_rmm_psci_complete(vcpu, &req);
 	}
-#ifdef CONFIG_HISI_CCADA_HOST
-	case KVM_ARM_VCPU_RME_DEV_VALIDATE: {
-		struct kvm_arm_rme_dev_validate args;
-
-		if (!_vcpu_is_rec(vcpu))
-			return -EPERM;
-		if (copy_from_user(&args, argp, sizeof(args)))
-			return -EFAULT;
-		return kvm_arm_vcpu_rme_dev_validate(vcpu, &args);
-	}
-#endif
 	default:
 		r = -EINVAL;
 	}

arch/arm64/kvm/hisi-cca-da.c
@@ -503,8 +503,6 @@ static int rme_root_dev_delegate(phys_addr_t params_addr)
 retry:
 	ret = rmi_root_dev_delegate(params_addr, &out_dev_bdf);
 	if (ret == RMI_ERROR_DEV_INFO) {
-		if (out_dev_bdf == last_dev_bdf)
-			return -ENXIO;
 
 		dev_info = (void *)get_zeroed_page(GFP_ATOMIC);
 		if (!dev_info) {
@@ -518,7 +516,6 @@ retry:
 			free_page((unsigned long)dev_info);
 			return -ENXIO;
 		}
-
 		ret = rmi_dev_init(out_dev_bdf, dev_info_phys);
 		if (ret && ret != RMI_ERROR_DEV_EXISTS) {
 			if (WARN_ON(rmi_granule_undelegate(dev_info_phys))) {
@@ -837,19 +834,14 @@ err_detach:
 	return ret;
 }
 
-int kvm_arm_vcpu_rme_dev_validate(struct kvm_vcpu *vcpu,
-				  struct kvm_arm_rme_dev_validate *args)
+void kvm_complete_dev_op(struct kvm_vcpu *vcpu)
 {
-	if (!vcpu || !args || !_vcpu_is_rec(vcpu))
-		return -EINVAL;
+	struct kvm_run *run = vcpu->run;
+	struct realm_rec *rec = vcpu->arch.rec;
 
-	/* Record host pdev bdf in rec_enter, complete dev validate in rmm */
-	vcpu->arch.rec->run->enter.vfio_dev = args->vfio_dev;
-	vcpu->arch.rec->run->enter.dev_bdf = args->dev_bdf;
-
-	return 0;
+	rec->run->enter.dev_bdf = run->rme_dev.dev_bdf;
+	rec->run->enter.vfio_dev = run->rme_dev.vfio_dev;
 }
-
 
 #define MMIO_REG_32_BIT 32
 static u32 rme_mmio_read32(unsigned long addr, struct pci_dev *pdev)

arch/arm64/kvm/rme-exit.c
@@ -182,6 +182,7 @@ static int rec_exit_dev(struct kvm_vcpu *vcpu)
 	/* Return to qemu to find the host dev by guest dev bdf */
 	vcpu->run->exit_reason = KVM_EXIT_ARM_RME_DEV;
 	vcpu->run->rme_dev.guest_dev_bdf = rec->run->exit.guest_dev_bdf;
+	vcpu->run->rme_dev.vfio_dev = false;
 
 	return 0;
 }

include/uapi/linux/kvm.h
@@ -267,7 +267,7 @@ struct kvm_xen_exit {
 #define KVM_EXIT_NOTIFY           37
 #define KVM_EXIT_LOONGARCH_IOCSR  38
 #ifndef __GENKSYMS__
-#define KVM_EXIT_ARM_RME_DEV      39
+#define KVM_EXIT_ARM_RME_DEV      50
 #endif
 
 /* For KVM_EXIT_INTERNAL_ERROR */
@@ -526,6 +526,8 @@ struct kvm_run {
 		/* KVM_EXIT_ARM_RME_DEV */
 		struct {
 			__u16 guest_dev_bdf;
+			__u16 dev_bdf;
+			__u32 vfio_dev;
 		} rme_dev;
 #endif
 		/* Fix the size of the union. */
@@ -2498,15 +2500,5 @@ struct kvm_arm_rmm_psci_complete {
 };
 
 #define KVM_ARM_VCPU_RMM_PSCI_COMPLETE	_IOW(KVMIO, 0xd6, struct kvm_arm_rmm_psci_complete)
-
-/* Available with KVM_CAP_ARM_RME, only for VMs with KVM_VM_TYPE_ARM_REALM  */
-struct kvm_arm_rme_dev_validate {
-	__u16 dev_bdf; /* pci dev bdf in the host */
-	__u16 guest_dev_bdf; /* pci dev bdf in the realm */
-	__u32 vfio_dev; /* Whether the host dev bdf refers to a VFIO dev */
-	__u32 reserved[4];
-};
-
-#define KVM_ARM_VCPU_RME_DEV_VALIDATE	_IOW(KVMIO, 0xd4, struct kvm_arm_rme_dev_validate)
 
 #endif /* __LINUX_KVM_H */

commit 03f4fe25e4bf0ed015fa80e3d2a35fbf54ff7a7d

hisi-cca-da: Revert smmu realm enabled condition for arm_smmu_domain_finalise.
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

1. Fix compile problem without enable CONFIG_HISI_CCA.
2. Revert smmu realm enabled condition for arm_smmu_domain_finalise.

Fixes: 11a27efee0a3 ("hisi-cca-da: Improve hisi cca dev validate and delegate")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>

arch/arm64/kvm/rme.c
@@ -1764,6 +1764,7 @@ int rmi_granule_delegate_get(unsigned long phys, void *realm_p)
 	struct realm *realm = (struct realm *)realm_p;
 	struct folio *folio = page_folio(phys_to_page(phys));
 
+#ifdef CONFIG_HISI_CCA
 	if (folio_test_hugetlb(folio)) {
 		if (rme_isolate_hugetlb(folio)) {
 			folio_put(folio);
@@ -1786,6 +1787,7 @@ int rmi_granule_delegate_get(unsigned long phys, void *realm_p)
 			}
 		}
 	}
+#endif
 	return _rmi_granule_delegate(phys);
 }
 
@@ -1819,7 +1821,6 @@ static void realm_put_folios(struct realm *realm)
 
 void _kvm_destroy_realm(struct kvm *kvm)
 {
-	struct kvm_s2_mmu *mmu = &kvm->arch.mmu;
 	struct realm *realm = &kvm->arch.realm;
 	size_t pgd_size = kvm_pgtable_stage2_pgd_size(kvm->arch.vtcr);
 	int i;
@@ -1834,9 +1835,9 @@ void _kvm_destroy_realm(struct kvm *kvm)
 
 #ifdef CONFIG_HISI_CCADA_HOST
 	write_lock(&kvm->mmu_lock);
-	unmap_stage2_range(mmu, 0, BIT(realm->ia_bits - 1), true);
+	unmap_stage2_range(&kvm->arch.mmu, 0, BIT(realm->ia_bits - 1), true);
 	write_unlock(&kvm->mmu_lock);
-	kvm_realm_destroy_rtts(kvm, mmu->pgt->ia_bits);
+	kvm_realm_destroy_rtts(kvm, kvm->arch.mmu.pgt->ia_bits);
 	/* undelegate and detach dev from realm */
 	realm_destroy_dev_list(realm);
 #endif
@@ -1920,9 +1921,11 @@ int _kvm_rec_pre_enter(struct kvm_vcpu *vcpu)
 	case RMI_EXIT_RIPAS_CHANGE:
 		kvm_complete_ripas_change(vcpu);
 		break;
+#ifdef CONFIG_HISI_CCADA_HOST
 	case RMI_EXIT_DEV_VALIDATE:
 	case RMI_EXIT_DEV_START:
 		kvm_complete_dev_op(vcpu);
+#endif
 	}
 
 	return 1;

drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3143,14 +3143,9 @@ static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev)
 
 	mutex_lock(&smmu_domain->init_mutex);
 
-	if (!smmu_domain->smmu) {
-		if (smmu->realm.enabled) {
-			ret = arm_smmu_domain_finalise(smmu_domain, smmu, 0);
-		} else {
-			pr_err("realm smmu has not enabled.\n");
-			ret = -EOPNOTSUPP;
-		}
-	} else if (smmu_domain->smmu != smmu)
+	if (!smmu_domain->smmu)
+		ret = arm_smmu_domain_finalise(smmu_domain, smmu, 0);
+	else if (smmu_domain->smmu != smmu)
 		ret = -EINVAL;
 
 	mutex_unlock(&smmu_domain->init_mutex);