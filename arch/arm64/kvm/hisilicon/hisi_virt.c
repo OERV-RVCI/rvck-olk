openeuler-mirror/kernel

commit e85b97c7e2b40e2457665368b4b9779556efd4c5

KVM: arm64: Probe and configure DVMBM capability on HiSi CPUs
virt inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8TN8N
CVE: NA

----------------------------------------------------

DVMBM is an virtualization extension since HIP09, which allows TLBI
executed at NS EL1 to be broadcast in a configurable range of physical
CPUs (even with HCR_EL2.FB set). It will bring TLBI broadcast optimization.

Introduce the method to detect and enable this feature. Also add a kernel
command parameter "kvm-arm.dvmbm_enabled" (=0 on default) so that users can
{en,dis}able DVMBM on need. The parameter description is added under
Documentation/.

Signed-off-by: Quan Zhou <zhouquan65@huawei.com>
Signed-off-by: lishusen <lishusen2@huawei.com>

arch/arm64/kvm/hisilicon/hisi_virt.c
@@ -11,6 +11,8 @@
 
 static enum hisi_cpu_type cpu_type = UNKNOWN_HI_TYPE;
 
+static bool dvmbm_enabled;
+
 static const char * const hisi_cpu_type_str[] = {
 	"Hisi1612",
 	"Hisi1616",
@@ -123,4 +125,51 @@ bool hisi_ncsnp_supported(void)
 		supported = true;
 
 	return supported;
+}
+
+static int __init early_dvmbm_enable(char *buf)
+{
+	return strtobool(buf, &dvmbm_enabled);
+}
+early_param("kvm-arm.dvmbm_enabled", early_dvmbm_enable);
+
+static void hardware_enable_dvmbm(void *data)
+{
+	u64 val;
+
+	val  = read_sysreg_s(SYS_LSUDVM_CTRL_EL2);
+	val |= LSUDVM_CTLR_EL2_MASK;
+	write_sysreg_s(val, SYS_LSUDVM_CTRL_EL2);
+}
+
+static void hardware_disable_dvmbm(void *data)
+{
+	u64 val;
+
+	val  = read_sysreg_s(SYS_LSUDVM_CTRL_EL2);
+	val &= ~LSUDVM_CTLR_EL2_MASK;
+	write_sysreg_s(val, SYS_LSUDVM_CTRL_EL2);
+}
+
+bool hisi_dvmbm_supported(void)
+{
+	if (cpu_type != HI_IP09)
+		return false;
+
+	/* Determine whether DVMBM is supported by the hardware */
+	if (!(read_sysreg(aidr_el1) & AIDR_EL1_DVMBM_MASK))
+		return false;
+
+	/* User provided kernel command-line parameter */
+	if (!dvmbm_enabled || !is_kernel_in_hyp_mode()) {
+		on_each_cpu(hardware_disable_dvmbm, NULL, 1);
+		return false;
+	}
+
+	/*
+	 * Enable TLBI Broadcast optimization by setting
+	 * LSUDVM_CTRL_EL2's bit[0].
+	 */
+	on_each_cpu(hardware_enable_dvmbm, NULL, 1);
+	return true;
 }

arch/arm64/include/asm/kvm_host.h
@@ -1155,5 +1155,6 @@ void kvm_arm_vcpu_power_off(struct kvm_vcpu *vcpu);
 bool kvm_arm_vcpu_stopped(struct kvm_vcpu *vcpu);
 
 extern bool kvm_ncsnp_support;
+extern bool kvm_dvmbm_support;
 
 #endif /* __ARM64_KVM_HOST_H__ */

arch/arm64/kvm/arm.c
@@ -63,6 +63,9 @@ static bool vgic_present, kvm_arm_initialised;
 /* Capability of non-cacheable snooping */
 bool kvm_ncsnp_support;
 
+/* Capability of DVMBM */
+bool kvm_dvmbm_support;
+
 static DEFINE_PER_CPU(unsigned char, kvm_hyp_initialized);
 DEFINE_STATIC_KEY_FALSE(userspace_irqchip_in_use);
 
@@ -2425,8 +2428,10 @@ static __init int kvm_arm_init(void)
 #ifdef CONFIG_KVM_HISI_VIRT
 	probe_hisi_cpu_type();
 	kvm_ncsnp_support = hisi_ncsnp_supported();
+	kvm_dvmbm_support = hisi_dvmbm_supported();
 #endif
 	kvm_info("KVM ncsnp %s\n", kvm_ncsnp_support ? "enabled" : "disabled");
+	kvm_info("KVM dvmbm %s\n", kvm_dvmbm_support ? "enabled" : "disabled");
 
 	in_hyp_mode = is_kernel_in_hyp_mode();
 

arch/arm64/kvm/hisilicon/hisi_virt.h
@@ -14,7 +14,13 @@ enum hisi_cpu_type {
 	UNKNOWN_HI_TYPE
 };
 
+/* HIP09 */
+#define AIDR_EL1_DVMBM_MASK	GENMASK_ULL(13, 12)
+#define SYS_LSUDVM_CTRL_EL2	sys_reg(3, 4, 15, 7, 4)
+#define LSUDVM_CTLR_EL2_MASK	BIT_ULL(0)
+
 void probe_hisi_cpu_type(void);
 bool hisi_ncsnp_supported(void);
+bool hisi_dvmbm_supported(void);
 
 #endif /* __HISI_VIRT_H__ */

OERV-RVCI/rvck-olk

commit 7c56727fd614fac5a3dceda041e20a73eb1c1d18

KVM: arm64: Fix CCA guest panic when dvmbm is enabled
virt inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8610

--------------------------------------------------------------------

Enabling TLBI optimization via kvm-arm.dvmbm_enabled=1 permanently sets the
SYS_LSUDVM_CTRL_EL2 control bit. Since CCA doesn't yet support this feature,
CCA VMs will read an erroneous bitmap from SYS_LSUDVMBM_EL2 when performing
TLB flushes, causing a panic.

To avoid this issue, this patch dynamically sets/clears the bit in
vcpu_load/vcpu_put, limiting the optimization to normal VMs only.

Although the default value of SYS_LSUDVM_CTRL_EL2 is 0, we still reset it to
0 in kvm_arm_init for safety.

This patch can also solve another issue:
When LPI is enabled, a CPU may enter power-down mode. During this transition,
the dvmbm ctrl register is reset to 0 after the cpu powers down, which can cause
functional breakdown.

Kernel ensures that no threads are running on the pCPU before entering the
power-down mode. Therefore, vCPU must have been put before this pCPU powers
down. When the vCPU runs again, vcpu_load will rewrite the control register.
This guarantees that even if the register is reset to 0 during power-down mode,
it will no longer affect the TLBI broadcast optimization.

Fixes: e85b97c7e2b4 ("KVM: arm64: Probe and configure DVMBM capability on HiSi CPUs")
Signed-off-by: Tian Zheng <zhengtian10@huawei.com>
Reviewed-by: Yanan Wang <wangyanan55@huawei.com>

arch/arm64/kvm/hisilicon/hisi_virt.c
@@ -144,7 +144,7 @@ static int __init early_dvmbm_enable(char *buf)
 }
 early_param("kvm-arm.dvmbm_enabled", early_dvmbm_enable);
 
-static void hardware_enable_dvmbm(void *data)
+static void hardware_enable_dvmbm(void)
 {
 	u64 val;
 
@@ -245,18 +245,10 @@ bool hisi_dvmbm_supported(void)
 	if (!(read_sysreg(aidr_el1) & AIDR_EL1_DVMBM_MASK))
 		return false;
 
-	/* User provided kernel command-line parameter */
-	if (!dvmbm_enabled) {
-		on_each_cpu(hardware_disable_dvmbm, NULL, 1);
-		return false;
-	}
+	/* reset */
+	on_each_cpu(hardware_disable_dvmbm, NULL, 1);
 
-	/*
-	 * Enable TLBI Broadcast optimization by setting
-	 * LSUDVM_CTRL_EL2's bit[0].
-	 */
-	on_each_cpu(hardware_enable_dvmbm, NULL, 1);
-	return true;
+	return dvmbm_enabled;
 }
 
 int kvm_sched_affinity_vcpu_init(struct kvm_vcpu *vcpu)
@@ -555,6 +547,7 @@ void kvm_tlbi_dvmbm_vcpu_load(struct kvm_vcpu *vcpu)
 	if (!kvm_dvmbm_support)
 		return;
 
+	hardware_enable_dvmbm();
 	cpumask_copy(vcpu->arch.sched_cpus, current->cpus_ptr);
 
 	if (likely(cpumask_equal(vcpu->arch.sched_cpus,
@@ -605,6 +598,7 @@ void kvm_tlbi_dvmbm_vcpu_put(struct kvm_vcpu *vcpu)
 		return;
 
 	cpumask_copy(vcpu->arch.pre_sched_cpus, vcpu->arch.sched_cpus);
+	hardware_disable_dvmbm(NULL);
 }
 
 void kvm_get_pg_cfg(void)