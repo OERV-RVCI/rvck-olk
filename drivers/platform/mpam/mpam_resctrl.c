openeuler-mirror/kernel

commit 9119da1439390ea4f29a0635652248aa3c2040f0

arm_mpam: resctrl: Allow resctrl to allocate monitors
maillist inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8T2RT

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git/log/?h=mpam/snapshot/v6.7-rc2

---------------------------

When resctrl wants to read a domain's 'QOS_L3_OCCUP', it needs
to allocate a monitor on the corresponding resource. Monitors are
allocated by class instead of component.

Add helpers to do this.

The MBM events depend on having their monitors allocated at init
time so that they can be left running. The value USE_RMID_IDX
is out of range for a monitor, and is used to indicate this
behaviour.

resctrl_arch_mon_ctx_alloc() is implemented to have a no_wait
version and a waitqueue for callers that sleep. The no_wait
version will later become an interface for the resctrl_pmu
to use.

Signed-off-by: James Morse <james.morse@arm.com>
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -22,6 +22,8 @@
 
 u64 mpam_resctrl_default_group;
 
+DECLARE_WAIT_QUEUE_HEAD(resctrl_mon_ctx_waiters);
+
 /*
  * The classes we've picked to map to resctrl resources.
  * Class pointer may be NULL.
@@ -38,6 +40,10 @@ static struct mpam_class *mbm_total_class;
  * This applies globally to all traffic the CPU generates.
  */
 static bool cdp_enabled;
+
+/* A dummy mon context to use when the monitors were allocated up front */
+u32 __mon_is_rmid_idx = USE_RMID_IDX;
+void *mon_is_rmid_idx = &__mon_is_rmid_idx;
 
 bool resctrl_arch_alloc_capable(void)
 {
@@ -230,6 +236,72 @@ struct rdt_resource *resctrl_arch_get_resource(enum resctrl_res_level l)
 		return NULL;
 
 	return &mpam_resctrl_exports[l].resctrl_res;
+}
+
+static void *resctrl_arch_mon_ctx_alloc_no_wait(struct rdt_resource *r,
+						int evtid)
+{
+	struct mpam_resctrl_res *res;
+	u32 *ret = kmalloc(sizeof(*ret), GFP_KERNEL);
+
+	if (!ret)
+		return ERR_PTR(-ENOMEM);
+
+	switch (evtid) {
+	case QOS_L3_OCCUP_EVENT_ID:
+		res = container_of(r, struct mpam_resctrl_res, resctrl_res);
+
+		*ret = mpam_alloc_csu_mon(res->class);
+		return ret;
+	case QOS_L3_MBM_LOCAL_EVENT_ID:
+	case QOS_L3_MBM_TOTAL_EVENT_ID:
+		return mon_is_rmid_idx;
+	}
+
+	return ERR_PTR(-EOPNOTSUPP);
+}
+
+void *resctrl_arch_mon_ctx_alloc(struct rdt_resource *r, int evtid)
+{
+	DEFINE_WAIT(wait);
+	void *ret;
+
+	might_sleep();
+
+	do {
+		prepare_to_wait(&resctrl_mon_ctx_waiters, &wait,
+				TASK_INTERRUPTIBLE);
+		ret = resctrl_arch_mon_ctx_alloc_no_wait(r, evtid);
+		if (PTR_ERR(ret) == -ENOSPC)
+			schedule();
+	} while (PTR_ERR(ret) == -ENOSPC && !signal_pending(current));
+	finish_wait(&resctrl_mon_ctx_waiters, &wait);
+
+	return ret;
+}
+
+void resctrl_arch_mon_ctx_free(struct rdt_resource *r, int evtid,
+			       void *arch_mon_ctx)
+{
+	struct mpam_resctrl_res *res;
+	u32 mon = *(u32 *)arch_mon_ctx;
+
+	if (mon == USE_RMID_IDX)
+		return;
+	kfree(arch_mon_ctx);
+	arch_mon_ctx = NULL;
+
+	res = container_of(r, struct mpam_resctrl_res, resctrl_res);
+
+	switch (evtid) {
+	case QOS_L3_OCCUP_EVENT_ID:
+		mpam_free_csu_mon(res->class, mon);
+		wake_up(&resctrl_mon_ctx_waiters);
+		return;
+	case QOS_L3_MBM_TOTAL_EVENT_ID:
+	case QOS_L3_MBM_LOCAL_EVENT_ID:
+		return;
+	}
 }
 
 static bool cache_has_usable_cpor(struct mpam_class *class)

drivers/platform/mpam/mpam_internal.h
@@ -17,6 +17,9 @@
 
 DECLARE_STATIC_KEY_FALSE(mpam_enabled);
 
+/* Value to indicate the allocated monitor is derived from the RMID index. */
+#define USE_RMID_IDX	(U16_MAX + 1)
+
 static inline bool mpam_is_enabled(void)
 {
 	return static_branch_likely(&mpam_enabled);

include/linux/arm_mpam.h
@@ -72,4 +72,8 @@ u32 resctrl_arch_rmid_idx_encode(u32 closid, u32 rmid);
 void resctrl_arch_rmid_idx_decode(u32 idx, u32 *closid, u32 *rmid);
 u32 resctrl_arch_system_num_rmid_idx(void);
 
+struct rdt_resource;
+void *resctrl_arch_mon_ctx_alloc(struct rdt_resource *r, int evtid);
+void resctrl_arch_mon_ctx_free(struct rdt_resource *r, int evtid, void *ctx);
+
 #endif /* __LINUX_ARM_MPAM_H */

OERV-RVCI/rvck-olk

commit 4dd41e259489354d33c3a57a8d24e8a91074d9f2

arm64/mpam: Fix warning of schedule state in resctrl_arch_mon_ctx_alloc()
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8686

------------------

resctrl_arch_mon_ctx_alloc() triggers a scheduling state warning when
calling kmalloc() while the task is in a non-running state:

do not call blocking ops when !TASK_RUNNING; state=1 set at prepare_to_wait
WARNING: CPU: 78 PID: 12592 at kernel/sched/core.c:10584 _might_sleep

Call trace:
 _might_sleep+0x1cc/0xf8
 _kmalloc_cache_alloc_node+0x3a0/0x4e0
 kmalloc_trace+0x44/0x120
 resctrl_arch_mon_ctx_alloc+0xf0/0x178
 mon_event_read+0xa0/0x308
 mkdir_mondata_subdir+0x24c/0x330
 mkdir_mondata_all+0x17c/0x248
 rdt_get_tree+0x2e8/0x680
 vfs_get_tree+0x5c/0x190
 do_new_mount+0x27c/0x408
 path_mount+0x2ac/0x448
 arm64_sys_mount+0x1d4/0x228

Ensure the allocation is performed in TASK_RUNNING state to avoid
schedule state corrupted.

Fixes: 9119da143939 ("arm_mpam: resctrl: Allow resctrl to allocate monitors")
Signed-off-by: Zeng Heng <zengheng4@huawei.com>

drivers/platform/mpam/mpam_resctrl.c
@@ -313,34 +313,15 @@ struct rdt_resource *resctrl_arch_get_resource(enum resctrl_res_level l)
 	return &mpam_resctrl_exports[l].resctrl_res;
 }
 
-static void *resctrl_arch_mon_ctx_alloc_no_wait(struct rdt_resource *r,
-						int evtid)
+void *resctrl_arch_mon_ctx_alloc(struct rdt_resource *r, int evtid)
 {
-	u32 *ret = kmalloc(sizeof(*ret), GFP_KERNEL);
+	u32 *ret;
 
+	ret = kmalloc(sizeof(*ret), GFP_KERNEL);
 	if (!ret)
 		return ERR_PTR(-ENOMEM);
 
 	*ret = __mon_is_rmid_idx;
-	return ret;
-}
-
-void *resctrl_arch_mon_ctx_alloc(struct rdt_resource *r, int evtid)
-{
-	DEFINE_WAIT(wait);
-	void *ret;
-
-	might_sleep();
-
-	do {
-		prepare_to_wait(&resctrl_mon_ctx_waiters, &wait,
-				TASK_INTERRUPTIBLE);
-		ret = resctrl_arch_mon_ctx_alloc_no_wait(r, evtid);
-		if (PTR_ERR(ret) == -ENOSPC)
-			schedule();
-	} while (PTR_ERR(ret) == -ENOSPC && !signal_pending(current));
-	finish_wait(&resctrl_mon_ctx_waiters, &wait);
-
 	return ret;
 }
 