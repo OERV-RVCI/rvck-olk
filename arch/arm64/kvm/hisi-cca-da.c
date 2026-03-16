OERV-RVCI/rvck-olk

commit f026465c5504fe63e1315d090bc6f47661423f70

hisi-cca-da: Transfer PCIe ECAM access requests to RMM for realm device.
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

1. Remove duplicate definition for msix_table_size.
2. Remove unused header.
3. Add header file to preserve KABI.
4. Transfer PCIe ECAM access requests to RMM for realm device.

Fixes: 92af6a401f0b ("hisi-cca-da: Support pcipc sriov capability")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>
Signed-off-by: Jiajing He <hejiajing2@h-partners.com>

arch/arm64/kvm/hisi-cca-da.c
@@ -36,6 +36,9 @@ static DEFINE_SPINLOCK(g_realm_dev_lock);
 static DECLARE_BITMAP(g_pcipc_ns, (PCI_BDF_MASK + 1));
 static DEFINE_SPINLOCK(g_pcipc_ns_lock);
 
+static DECLARE_BITMAP(g_dev_protected, (PCI_BDF_MASK + 1));
+static DEFINE_SPINLOCK(g_dev_protected_lock);
+
 bool is_support_rme(void)
 {
 	return static_branch_unlikely(&kvm_rme_is_available);
@@ -424,6 +427,9 @@ static int rme_get_all_dev_info(struct pci_dev *rp_dev,
 	} else {
 		int ndev = 0;
 
+		params->devs[ndev] = pci_dev_id(rp_dev);
+		ndev++;
+
 		ret = get_child_devices_rec(rp_dev, params->devs,
 					    MAX_DEV_PER_PORT, &ndev,
 					    pdevs);
@@ -608,6 +614,11 @@ static int delegate_root_dev(struct pci_dev *root_dev)
 		free_page((unsigned long)params);
 		return ret;
 	}
+
+	spin_lock(&g_dev_protected_lock);
+	for (int i = 0; i < params->num_dev; i++)
+		bitmap_set(g_dev_protected, params->devs[i], 1);
+	spin_unlock(&g_dev_protected_lock);
 
 	ret = rme_root_dev_delegate(virt_to_phys(params));
 	if (ret)
@@ -1010,7 +1021,6 @@ static u64 rme_mmio_read64(unsigned long addr, struct pci_dev *pdev)
 	return value;
 }
 
-
 static int rme_mmio_write64(unsigned long addr, unsigned long value,
 			     struct pci_dev *pdev)
 {
@@ -1366,3 +1376,40 @@ bool is_realm_device(struct device *dev, struct device_driver *drv)
 	return false;
 }
 EXPORT_SYMBOL_GPL(is_realm_device);
+
+bool is_dev_ecam_protected(u16 dev_bdf)
+{
+	bool is_dev_ecam_protected = false;
+
+	spin_lock(&g_dev_protected_lock);
+	is_dev_ecam_protected = bitmap_read(g_dev_protected, dev_bdf, 1);
+	spin_unlock(&g_dev_protected_lock);
+
+	return is_dev_ecam_protected;
+}
+
+/* If device is realm dev, read config need transfer to rmm */
+int ccada_pci_generic_config_read(void __iomem *addr, unsigned char bus_num,
+				   unsigned int devfn, u32 size, u32 *val)
+{
+	u16 dev_bdf = PCI_DEVID(bus_num, devfn);
+	u64 bits = MMIO_RW_32BITS;
+
+	if (MMIO_RW_8BITS * size <= MMIO_RW_16BITS)
+		bits = MMIO_RW_8BITS * size;
+
+	return rmi_dev_mmio_read(rme_mmio_va_to_pa(addr), bits, (unsigned long *)val, dev_bdf);
+}
+
+/* If device is realm dev, write config need transfer to rmm */
+int ccada_pci_generic_config_write(void __iomem *addr, unsigned char bus_num,
+				    unsigned int devfn, u32 size, u32 val)
+{
+	u16 dev_bdf = PCI_DEVID(bus_num, devfn);
+	u64 bits = MMIO_RW_32BITS;
+
+	if (MMIO_RW_8BITS * size <= MMIO_RW_16BITS)
+		bits = MMIO_RW_8BITS * size;
+
+	return rmi_dev_mmio_write(rme_mmio_va_to_pa(addr), bits, val, dev_bdf);
+}

drivers/pci/access.c
@@ -4,6 +4,7 @@
 #include <linux/slab.h>
 #include <linux/ioport.h>
 #include <linux/wait.h>
+#include <linux/cca_cvm_domain.h>
 
 #ifdef CONFIG_HISI_VIRTCCA_CODA
 #ifndef __GENKSYMS__
@@ -102,6 +103,11 @@ int pci_generic_config_read(struct pci_bus *bus, unsigned int devfn,
 		return virtcca_pci_generic_config_read(addr, bus->number, devfn, size, val);
 #endif
 
+#ifdef CONFIG_HISI_CCADA_HOST
+	if (is_support_rme() && is_dev_ecam_protected(devfn))
+		return ccada_pci_generic_config_read(addr, bus->number, devfn, size, val);
+#endif
+
 	if (size == 1)
 		*val = readb(addr);
 	else if (size == 2)
@@ -127,6 +133,11 @@ int pci_generic_config_write(struct pci_bus *bus, unsigned int devfn,
 	    (is_cc_dev(PCI_DEVID(bus->number, devfn)) ||
 	     is_cc_root_bd((PCI_DEVID(bus->number, devfn)))))
 		return virtcca_pci_generic_config_write(addr, bus->number, devfn, size, val);
+#endif
+
+#ifdef CONFIG_HISI_CCADA_HOST
+	if (is_support_rme() && is_dev_ecam_protected(devfn))
+		return ccada_pci_generic_config_write(addr, bus->number, devfn, size, val);
 #endif
 
 	if (size == 1)

arch/arm64/include/asm/hisi_cca_da.h
@@ -2,6 +2,7 @@
 #ifndef __ASM_HISI_CCA_DA_H
 #define __ASM_HISI_CCA_DA_H
 
+#include <linux/cca_cvm_domain.h>
 #include <asm/kvm_host.h>
 #include "../../../../drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h"
 
@@ -30,8 +31,9 @@ struct realm_dev_entry {
 };
 
 #define REALM_MSI_ORIG_IOVA		0x8000000
-
-bool is_support_rme(void);
+#define MMIO_RW_8BITS			0x8
+#define MMIO_RW_16BITS			0x10
+#define MMIO_RW_32BITS			0x20
 
 int realm_attach_devs(struct realm *realm);
 void realm_destroy_dev_list(struct realm *realm);
@@ -77,5 +79,4 @@ void __rme_raw_writel_hook(u32 val, void __iomem *addr, struct pci_dev *pdev);
 int __rme_iowrite64_copy_hook(void __iomem *to, const void *from,
 	size_t count, struct pci_dev *pdev);
 
-bool is_realm_device(struct device *dev, struct device_driver *drv);
 #endif

arch/arm64/kernel/realm_guest.c
@@ -56,7 +56,6 @@ bool realm_free_swiotlb_shared_pages(void *addr, unsigned int order)
 }
 
 #ifdef CONFIG_HISI_CCADA_GUEST
-#define msix_table_size(flags)	((flags & PCI_MSIX_FLAGS_QSIZE) + 1)
 static int set_msix_region_shared(struct pci_dev *pdev, int bar)
 {
 	resource_size_t start;
@@ -79,7 +78,7 @@ static int set_msix_region_shared(struct pci_dev *pdev, int bar)
 	}
 
 	pci_read_config_word(pdev, pdev->msix_cap + PCI_MSIX_FLAGS, &control);
-	tsize = msix_table_size(control);
+	tsize = (control & PCI_MSIX_FLAGS_QSIZE) + 1;
 	end = ALIGN(start + tsize * PCI_MSIX_ENTRY_SIZE, PAGE_SIZE);
 	if (end <= start) {
 		pci_err(pdev, "pci dev msi end overflow.\n");

arch/arm64/kvm/arm.c
@@ -46,9 +46,6 @@
 #include <kvm/arm_hypercalls.h>
 #include <kvm/arm_pmu.h>
 #include <kvm/arm_psci.h>
-#ifdef CONFIG_HISI_CCADA_HOST
-#include <asm/hisi_cca_da.h>
-#endif
 
 static enum kvm_mode kvm_mode = KVM_MODE_DEFAULT;
 

arch/arm64/kvm/mmu.c
@@ -337,7 +337,7 @@ static void __unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 
 }
 
 void unmap_stage2_range(struct kvm_s2_mmu *mmu, phys_addr_t start, u64 size,
-			       bool may_block)
+			bool may_block)
 {
 	__unmap_stage2_range(mmu, start, size, may_block, false);
 }

arch/arm64/kvm/rme.c
@@ -434,8 +434,10 @@ static int hisi_cca_realm_unmap_private(struct realm *realm,
 	ret = rmi_cca_hisi_data_destroy_level(rd, ipa, &rtt_addr, next_addr, &map_level);
 	if (RMI_RETURN_STATUS(ret) == RMI_ERROR_RTT)
 		return 0;
-	else if (WARN_ON(ret))
+	else if (ret) {
+		pr_warn("CCA data destroy with level fail:%d.\n", ret);
 		return -ENXIO;
+	}
 
 	if (map_level > RMM_RTT_MAX_LEVEL)
 		return -ENXIO;

drivers/base/dd.c
@@ -29,11 +29,7 @@
 #include <linux/pinctrl/devinfo.h>
 #include <linux/slab.h>
 #include <linux/virtcca_cvm_domain.h>
-#ifndef __GENKSYMS__
-#ifdef CONFIG_HISI_CCADA_HOST
-#include <asm/hisi_cca_da.h>
-#endif
-#endif
+#include <linux/cca_cvm_domain.h>
 
 #include "base.h"
 #include "power/power.h"
@@ -616,10 +612,8 @@ static int really_probe(struct device *dev, struct device_driver *drv)
 	if (is_virtcca_secure_vf(dev, drv))
 		return 0;
 
-#ifdef CONFIG_HISI_CCADA_HOST
 	if (is_realm_device(dev, drv))
 		return 0;
-#endif
 
 	if (defer_all_probes) {
 		/*

commit f5a42efac109b3efeecbbcde54b1cfa6dd06de89

hisi-cca-da: Fix potential data overflow.
kunpeng inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8502
CVE: NA

--------------------------------------------------------------------

1.Fix potential data overflow issue in pci_generic_config_read.
2.Enforce CCA and virtCCA isolation.
3.Fix incorrect conditions for config read/write transferring.

Fixes: f026465c5504 ("hisi-cca-da: Transfer PCIe ECAM access requests to RMM for realm device.")
Signed-off-by: Jingxian He <hejingxian@huawei.com>
Signed-off-by: Xiangkai Yang <yangxiangkai@huawei.com>

arch/arm64/kvm/hisi-cca-da.c
@@ -41,7 +41,7 @@ static DEFINE_SPINLOCK(g_dev_protected_lock);
 
 bool is_support_rme(void)
 {
-	return static_branch_unlikely(&kvm_rme_is_available);
+	return static_branch_unlikely(&kvm_rme_is_available) && (kvm_get_cvm_type() == ARMCCA_CVM);
 }
 EXPORT_SYMBOL_GPL(is_support_rme);
 
@@ -1392,13 +1392,19 @@ bool is_dev_ecam_protected(u16 dev_bdf)
 int ccada_pci_generic_config_read(void __iomem *addr, unsigned char bus_num,
 				   unsigned int devfn, u32 size, u32 *val)
 {
+	int ret;
 	u16 dev_bdf = PCI_DEVID(bus_num, devfn);
 	u64 bits = MMIO_RW_32BITS;
+	unsigned long ret_val;
 
 	if (MMIO_RW_8BITS * size <= MMIO_RW_16BITS)
 		bits = MMIO_RW_8BITS * size;
 
-	return rmi_dev_mmio_read(rme_mmio_va_to_pa(addr), bits, (unsigned long *)val, dev_bdf);
+	ret = rmi_dev_mmio_read(rme_mmio_va_to_pa(addr), bits, &ret_val, dev_bdf);
+	if (ret)
+		return ret;
+	*val = (u32)ret_val;
+	return ret;
 }
 
 /* If device is realm dev, write config need transfer to rmm */

drivers/pci/access.c
@@ -104,7 +104,7 @@ int pci_generic_config_read(struct pci_bus *bus, unsigned int devfn,
 #endif
 
 #ifdef CONFIG_HISI_CCADA_HOST
-	if (is_support_rme() && is_dev_ecam_protected(devfn))
+	if (is_support_rme() && is_dev_ecam_protected(PCI_DEVID(bus->number, devfn)))
 		return ccada_pci_generic_config_read(addr, bus->number, devfn, size, val);
 #endif
 
@@ -136,7 +136,7 @@ int pci_generic_config_write(struct pci_bus *bus, unsigned int devfn,
 #endif
 
 #ifdef CONFIG_HISI_CCADA_HOST
-	if (is_support_rme() && is_dev_ecam_protected(devfn))
+	if (is_support_rme() && is_dev_ecam_protected(PCI_DEVID(bus->number, devfn)))
 		return ccada_pci_generic_config_write(addr, bus->number, devfn, size, val);
 #endif
 