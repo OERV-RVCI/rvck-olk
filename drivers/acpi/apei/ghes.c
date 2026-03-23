OERV-RVCI/rvck-olk

commit a81f1aa5c8ea118127614142b13a67095d947a89

mm: memory-failure: use kill_accessing_process() in ghes
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID3KH8

----------------------------------------

Use kill_accessing_process() instead of force_sig() to pass addr
to userspace.

Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>

drivers/acpi/apei/ghes.c
@@ -489,8 +489,7 @@ static void memory_failure_cb(struct callback_head *twork)
 	if (!ret || ret == -EHWPOISON || ret == -EOPNOTSUPP)
 		return;
 
-	pr_err("Sending SIGBUS to current task due to memory error not recovered");
-	force_sig(SIGBUS);
+	kill_accessing_process(twcb->pfn, twcb->flags, true);
 }
 
 static bool ghes_do_memory_failure(u64 physical_addr, int flags)

include/linux/mm.h
@@ -3982,6 +3982,7 @@ extern void shake_page(struct page *p);
 extern atomic_long_t num_poisoned_pages __read_mostly;
 extern int soft_offline_page(unsigned long pfn, int flags);
 extern int soft_online_page(unsigned long pfn);
+int kill_accessing_process(unsigned long pfn, int flags, bool force_kill);
 #ifdef CONFIG_MEMORY_FAILURE
 /*
  * Sysfs entries for memory failure handling statistics.

mm/memory-failure.c
@@ -856,9 +856,9 @@ static const struct mm_walk_ops hwpoison_walk_ops = {
  * is proper in most cases, but it could be wrong when the application
  * process has multiple entries mapping the error page.
  */
-static int kill_accessing_process(unsigned long pfn, int flags)
+int kill_accessing_process(unsigned long pfn, int flags, bool force_kill)
 {
-	int ret;
+	int ret, ret_kill = -EINVAL;
 	struct task_struct *p = current;
 	struct hwpoison_walk priv = {
 		.pfn = pfn,
@@ -878,7 +878,14 @@ static int kill_accessing_process(unsigned long pfn, int flags)
          * SIGBUS is needed.
         */
 	if (ret == 1 && priv.tk.addr)
-		kill_proc(&priv.tk, pfn, flags);
+		ret_kill = kill_proc(&priv.tk, pfn, flags);
+
+	if (force_kill && (ret_kill < 0)) {
+		pr_err("%#lx: Sending force SIGBUS to %s:%d due to hardware memory corruption\n",
+				pfn, p->comm, task_pid_nr(p));
+		force_sig(SIGBUS);
+	}
+
 	mmap_read_unlock(p->mm);
 
 	return ret > 0 ? -EHWPOISON : 0;
@@ -2094,7 +2101,7 @@ retry:
 		pr_err("%#lx: already hardware poisoned\n", pfn);
 		if (flags & MF_ACTION_REQUIRED) {
 			folio = page_folio(p);
-			res = kill_accessing_process(folio_pfn(folio), flags);
+			res = kill_accessing_process(folio_pfn(folio), flags, false);
 			action_result(pfn, MF_MSG_ALREADY_POISONED, MF_FAILED);
 		}
 		return res;
@@ -2290,7 +2297,7 @@ try_again:
 		pr_err("%#lx: already hardware poisoned\n", pfn);
 		res = -EHWPOISON;
 		if (flags & MF_ACTION_REQUIRED)
-			res = kill_accessing_process(pfn, flags);
+			res = kill_accessing_process(pfn, flags, false);
 		if (flags & MF_COUNT_INCREASED)
 			put_page(p);
 		action_result(pfn, MF_MSG_ALREADY_POISONED, MF_FAILED);

commit c0d602f00936aa59f0ddc7078acff85963d0d688

mm: memory_failure: fix UAF in memory_failure_cb
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8787

------------------------------------------

The twcb pointer is accessed after gen_pool_free() in memory_failure_cb,
which leads to a use-after-free vulnerability. Fix this by moving
gen_pool_free() to the end of the function after all twcb accesses.

Fixes: a81f1aa5c8ea ("mm: memory-failure: use kill_accessing_process() in ghes")
Signed-off-by: Wupeng Ma <mawupeng1@huawei.com>

drivers/acpi/apei/ghes.c
@@ -484,12 +484,14 @@ static void memory_failure_cb(struct callback_head *twork)
 		container_of(twork, struct sync_task_work, twork);
 
 	ret = memory_failure(twcb->pfn, twcb->flags);
-	gen_pool_free(ghes_estatus_pool, (unsigned long)twcb, sizeof(*twcb));
 
-	if (!ret || ret == -EHWPOISON || ret == -EOPNOTSUPP)
+	if (!ret || ret == -EHWPOISON || ret == -EOPNOTSUPP) {
+		gen_pool_free(ghes_estatus_pool, (unsigned long)twcb, sizeof(*twcb));
 		return;
+	}
 
 	kill_accessing_process(twcb->pfn, twcb->flags, true);
+	gen_pool_free(ghes_estatus_pool, (unsigned long)twcb, sizeof(*twcb));
 }
 
 static bool ghes_do_memory_failure(u64 physical_addr, int flags)