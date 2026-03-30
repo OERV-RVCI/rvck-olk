196327144a5590cab35bda1c0574ad530954e1a5
Zhang Yi <yi.zhang@huawei.com>
2026-03-30 17:56:18 +0800

    ext4: move ordered data handling out of ext4_block_do_zero_range()
    hulk inclusion
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/8477
    
    --------------------------------
    
    Remove the handle parameter from ext4_block_do_zero_range() and move the
    ordered data handling to ext4_block_zero_eof().
    
    This is necessary for truncate up and append writes across a range
    extending beyond EOF. The ordered data must be committed before updating
    i_disksize to prevent exposing stale on-disk data from concurrent
    post-EOF mmap writes during previous folio writeback or in case of
    system crash during append writes.
    
    This is unnecessary for partial block hole punching because the entire
    punch operation does not provide atomicity guarantees and can already
    expose intermediate results in case of crash.
    
    Since ordered data handling is no longer performed inside
    ext4_zero_partial_blocks(), ext4_punch_hole() no longer needs to attach
    jinode.
    
    This is prepared for the conversion to the iomap infrastructure, which
    does not use ordered data mode while zeroing post-EOF partial blocks.
    
    Fixes: 5721968224e0 ("ext4: implement zero_range iomap path")
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>
    Signed-off-by: Yongjian Sun <sunyongjian1@huawei.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -4313,12 +4313,12 @@ unlock:
 	return err ? ERR_PTR(err) : NULL;
 }
 
-static int ext4_block_do_zero_range(handle_t *handle, struct inode *inode,
-				    loff_t from, loff_t length, bool *did_zero)
+static int ext4_block_do_zero_range(struct inode *inode, loff_t from,
+				    loff_t length, bool *did_zero,
+				    bool *zero_written)
 {
 	struct buffer_head *bh;
 	struct folio *folio;
-	int err = 0;
 
 	bh = ext4_block_get_zero_range(inode, from, length);
 	if (IS_ERR_OR_NULL(bh))
@@ -4329,19 +4329,14 @@ static int ext4_block_do_zero_range(handle_t *handle, struct inode *inode,
 	BUFFER_TRACE(bh, "zeroed end of block");
 
 	mark_buffer_dirty(bh);
-	/*
-	 * Only the written block requires ordered data to prevent exposing
-	 * stale data.
-	 */
-	if (ext4_should_order_data(inode) &&
-	    !buffer_unwritten(bh) && !buffer_delay(bh))
-		err = ext4_jbd2_inode_add_write(handle, inode, from, length);
-	if (!err && did_zero)
+	if (did_zero)
 		*did_zero = true;
+	if (zero_written && !buffer_unwritten(bh) && !buffer_delay(bh))
+		*zero_written = true;
 
 	folio_unlock(folio);
 	folio_put(folio);
-	return err;
+	return 0;
 }
 
 static int ext4_block_journalled_zero_range(handle_t *handle,
@@ -4392,7 +4387,8 @@ static int ext4_iomap_zero_range(struct inode *inode, loff_t from,
  * that corresponds to 'from'
  */
 static int ext4_block_zero_range(handle_t *handle, struct inode *inode,
-				 loff_t from, loff_t length, bool *did_zero)
+				 loff_t from, loff_t length, bool *did_zero,
+				 bool *zero_written)
 {
 	unsigned offset = from & (PAGE_SIZE-1);
 	unsigned blocksize = inode->i_sb->s_blocksize;
@@ -4414,7 +4410,8 @@ static int ext4_block_zero_range(handle_t *handle, struct inode *inode,
 	} else if (ext4_test_inode_state(inode, EXT4_STATE_BUFFERED_IOMAP)) {
 		return ext4_iomap_zero_range(inode, from, length, did_zero);
 	}
-	return ext4_block_do_zero_range(handle, inode, from, length, did_zero);
+	return ext4_block_do_zero_range(inode, from, length, did_zero,
+					zero_written);
 }
 
 /*
@@ -4431,6 +4428,7 @@ int ext4_block_zero_eof(handle_t *handle, struct inode *inode,
 	unsigned int offset;
 	loff_t length = end - from;
 	bool did_zero = false;
+	bool zero_written = false;
 	int err;
 
 	offset = from & (blocksize - 1);
@@ -4443,9 +4441,22 @@ int ext4_block_zero_eof(handle_t *handle, struct inode *inode,
 	if (length > blocksize - offset)
 		length = blocksize - offset;
 
-	err = ext4_block_zero_range(handle, inode, from, length, &did_zero);
+	err = ext4_block_zero_range(handle, inode, from, length,
+				    &did_zero, &zero_written);
 	if (err)
 		return err;
+	/*
+	 * It's necessary to order zeroed data before update i_disksize when
+	 * truncating up or performing an append write, because there might be
+	 * exposing stale on-disk data which may caused by concurrent post-EOF
+	 * mmap write during folio writeback.
+	 */
+	if (ext4_should_order_data(inode) &&
+	    did_zero && zero_written && !IS_DAX(inode)) {
+		err = ext4_jbd2_inode_add_write(handle, inode, from, length);
+		if (err)
+			return err;
+	}
 
 	return did_zero ? length : 0;
 }
@@ -4469,13 +4480,13 @@ int ext4_zero_partial_blocks(handle_t *handle, struct inode *inode,
 	if (start == end &&
 	    (partial_start || (partial_end != sb->s_blocksize - 1))) {
 		err = ext4_block_zero_range(handle, inode, lstart,
-					    length, NULL);
+					    length, NULL, NULL);
 		return err;
 	}
 	/* Handle partial zero out on the start of the range */
 	if (partial_start) {
 		err = ext4_block_zero_range(handle, inode, lstart,
-					    sb->s_blocksize, NULL);
+					    sb->s_blocksize, NULL, NULL);
 		if (err)
 			return err;
 	}
@@ -4483,7 +4494,7 @@ int ext4_zero_partial_blocks(handle_t *handle, struct inode *inode,
 	if (partial_end != sb->s_blocksize - 1)
 		err = ext4_block_zero_range(handle, inode,
 					    byte_end - partial_end,
-					    partial_end + 1, NULL);
+					    partial_end + 1, NULL, NULL);
 	return err;
 }
 
@@ -4674,18 +4685,6 @@ int ext4_punch_hole(struct file *file, loff_t offset, loff_t length)
 	max_length = sbi->s_bitmap_maxbytes - inode->i_sb->s_blocksize;
 	if (offset + length > max_length)
 		length = max_length - offset;
-
-	if (offset & (sb->s_blocksize - 1) ||
-	    (offset + length) & (sb->s_blocksize - 1)) {
-		/*
-		 * Attach jinode to inode for jbd2 if we do any zeroing of
-		 * partial block
-		 */
-		ret = ext4_inode_attach_jinode(inode);
-		if (ret < 0)
-			goto out_mutex;
-
-	}
 
 	/* Wait all existing dio workers, newcomers will block on i_rwsem */
 	inode_dio_wait(inode);
