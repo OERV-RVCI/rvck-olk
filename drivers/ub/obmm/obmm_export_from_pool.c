OERV-RVCI/rvck-olk

commit ac654ff4a9ebb99ff7666090c1e5c49c138b7d32

obmm: Add sysfs interface for configuration and monitoring
euleros inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/ID45O2
CVE: NA

------------------------------------------------------------------

Add sysfs information interfaces for each obmm_shmdev device to provide
detailed memory properties. The information is categorized into three
types based on their usage scenarios:

* Common information: Properties shared by both providers and users,
  located in the root sysfs path, such as memory length and type.
* Provider information: Provider-specific properties, located in the
  'export_info' subdirectory, like memory distribution across local NUMA
  nodes.
* User information: User-specific properties, located in the
  'import_info' subdirectory, such as physical addresses after remote
  mapping.

Some properties are only available when specific conditions are met. For
example, import_info/numa_id is only present when imported in NUMA mode.

Signed-off-by: Li Ruilin <liruilin4@huawei.com>
Signed-off-by: Gao Chao <gaochao24@huawei.com>

drivers/ub/obmm/obmm_sysfs_ub.c
@@ -1,0 +1,275 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+ * Description：OBMM Framework's implementations.
+ */
+
+#include <linux/pgtable.h>
+#include <linux/elf.h>
+
+#include "obmm_sysfs.h"
+#include "obmm_preimport.h"
+#include "obmm_import.h"
+
+static ssize_t size_show(struct device *dev, struct device_attribute *attr __always_unused,
+			 char *buf)
+{
+	struct obmm_region *region;
+
+	region = container_of(dev, struct obmm_region, device);
+	return sysfs_emit(buf, "0x%llx\n", region->mem_size);
+}
+static DEVICE_ATTR_ADMIN_RO(size);
+
+static const char *get_type_str(const struct obmm_region *region)
+{
+	return region->type == OBMM_EXPORT_REGION ? "export" : "import";
+}
+
+/* show some attribute of a region as string */
+#define REGION_ATTR_SHOW(tag)                                                               \
+	static ssize_t tag##_show(struct device *dev,                                       \
+				  struct device_attribute *attr __always_unused, char *buf) \
+	{                                                                                   \
+		struct obmm_region *region;                                                 \
+		region = container_of(dev, struct obmm_region, device);                     \
+		return sysfs_emit(buf, "%s\n", get_##tag##_str(region));                    \
+	}                                                                                   \
+	static DEVICE_ATTR_ADMIN_RO(tag)
+
+REGION_ATTR_SHOW(type);
+
+static ssize_t priv_len_show(struct device *dev, struct device_attribute *attr __always_unused,
+			     char *buf)
+{
+	struct obmm_region *region;
+
+	region = container_of(dev, struct obmm_region, device);
+	return sysfs_emit(buf, "%u\n", region->priv_len);
+}
+static DEVICE_ATTR_ADMIN_RO(priv_len);
+
+/* binary attribute of the sysfs entry for priv data */
+static ssize_t priv_read(struct file *filp __always_unused, struct kobject *kobj,
+			 struct bin_attribute *bin_attr __always_unused, char *buf, loff_t off,
+			 size_t count)
+{
+	struct device *dev;
+	struct obmm_region *region;
+
+	dev = kobj_to_dev(kobj);
+	region = container_of(dev, struct obmm_region, device);
+
+	if (off + count > OBMM_MAX_PRIV_LEN)
+		count = OBMM_MAX_PRIV_LEN - off;
+	memcpy(buf, region->priv + off, count);
+
+	return count;
+}
+
+static struct bin_attribute bin_attr_priv __ro_after_init = {
+	.attr = {
+		.name = "priv",
+		.mode = 0400,
+	},
+	.read = priv_read,
+	.size = OBMM_MAX_PRIV_LEN,
+};
+
+/* show some attribute of a region as string */
+#define REGION_FLAG_SHOW(flag)                                                               \
+	static ssize_t flag##_show(struct device *dev,                                       \
+				   struct device_attribute *attr __always_unused, char *buf) \
+	{                                                                                    \
+		struct obmm_region *region;                                                  \
+		region = container_of(dev, struct obmm_region, device);                      \
+		return sysfs_emit(buf, "%d\n", region_##flag(region));                       \
+	}                                                                                    \
+	static DEVICE_ATTR_ADMIN_RO(flag)
+
+REGION_FLAG_SHOW(allow_mmap);
+REGION_FLAG_SHOW(memory_from_user);
+REGION_FLAG_SHOW(preimport);
+
+/* for export region only */
+static ssize_t node_mem_size_show(struct device *dev, struct device_attribute *attr __always_unused,
+				  char *buf)
+{
+	unsigned int i;
+	ssize_t count;
+	struct obmm_region *reg;
+	struct obmm_export_region *e_reg;
+
+	reg = container_of(dev, struct obmm_region, device);
+	e_reg = container_of(reg, struct obmm_export_region, region);
+
+	count = sysfs_emit(buf, "%#llx", e_reg->node_mem_size[0]);
+	for (i = 1; i < e_reg->node_count; i++)
+		count += sysfs_emit_at(buf, count, ",%#llx", e_reg->node_mem_size[i]);
+	count += sysfs_emit_at(buf, count, "\n");
+	return count;
+}
+static DEVICE_ATTR_ADMIN_RO(node_mem_size);
+
+static ssize_t deid_show(struct device *dev, struct device_attribute *attr __always_unused,
+			 char *buf)
+{
+	struct obmm_import_region *i_reg;
+	struct obmm_export_region *e_reg;
+	struct obmm_region *reg;
+
+	reg = container_of(dev, struct obmm_region, device);
+	if (reg->type == OBMM_EXPORT_REGION) {
+		e_reg = container_of(reg, struct obmm_export_region, region);
+		return sysfs_emit(buf, EID_FMT64 "\n", EID_ARGS64_H(e_reg->deid),
+				  EID_ARGS64_L(e_reg->deid));
+	}
+	i_reg = container_of(reg, struct obmm_import_region, region);
+	return sysfs_emit(buf, EID_FMT64 "\n", EID_ARGS64_H(i_reg->deid),
+			  EID_ARGS64_L(i_reg->deid));
+}
+static DEVICE_ATTR_ADMIN_RO(deid);
+
+static ssize_t seid_show(struct device *dev, struct device_attribute *attr __always_unused,
+			 char *buf)
+{
+	struct obmm_import_region *i_reg;
+	struct obmm_region *reg;
+
+	reg = container_of(dev, struct obmm_region, device);
+	i_reg = container_of(reg, struct obmm_import_region, region);
+
+	return sysfs_emit(buf, EID_FMT64 "\n", EID_ARGS64_H(i_reg->seid),
+			  EID_ARGS64_L(i_reg->seid));
+}
+static DEVICE_ATTR_ADMIN_RO(seid);
+
+#define COMMON_FIELD_SHOW(field, fmt)                                                         \
+	static ssize_t field##_show(struct device *dev,                                       \
+				    struct device_attribute *attr __always_unused, char *buf) \
+	{                                                                                     \
+		struct obmm_region *reg;                                                      \
+		struct obmm_export_region *e_reg;                                             \
+		reg = container_of(dev, struct obmm_region, device);                          \
+		e_reg = container_of(reg, struct obmm_export_region, region);                 \
+		return sysfs_emit(buf, fmt, e_reg->field);                                    \
+	}                                                                                     \
+	static DEVICE_ATTR_ADMIN_RO(field)
+
+COMMON_FIELD_SHOW(tokenid, "0x%x\n");
+COMMON_FIELD_SHOW(uba, "0x%llx\n");
+
+#define IREG_FIELD_SHOW(field, fmt)                                                           \
+	static ssize_t field##_show(struct device *dev,                                       \
+				    struct device_attribute *attr __always_unused, char *buf) \
+	{                                                                                     \
+		struct obmm_region *reg;                                                      \
+		struct obmm_import_region *i_reg;                                             \
+		reg = container_of(dev, struct obmm_region, device);                          \
+		i_reg = container_of(reg, struct obmm_import_region, region);                 \
+		return sysfs_emit(buf, fmt, i_reg->field);                                    \
+	}                                                                                     \
+	static DEVICE_ATTR_ADMIN_RO(field)
+
+IREG_FIELD_SHOW(pa, "0x%llx\n");
+IREG_FIELD_SHOW(numa_id, "%d\n");
+IREG_FIELD_SHOW(dcna, "0x%x\n");
+IREG_FIELD_SHOW(scna, "0x%x\n");
+
+static struct attribute *root_attrs[] __ro_after_init = {
+	&dev_attr_size.attr,
+	&dev_attr_type.attr,
+	&dev_attr_priv_len.attr,
+	&dev_attr_allow_mmap.attr,
+	NULL,
+};
+
+static struct bin_attribute *root_bin_attrs[] __ro_after_init = {
+	&bin_attr_priv,
+	NULL,
+};
+
+static struct attribute *import_numa_attrs[] __ro_after_init = {
+	&dev_attr_numa_id.attr,
+	&dev_attr_pa.attr,
+	&dev_attr_dcna.attr,
+	&dev_attr_scna.attr,
+	&dev_attr_preimport.attr,
+	&dev_attr_seid.attr,
+	&dev_attr_deid.attr,
+	NULL,
+};
+static struct attribute *import_mmap_attrs[] __ro_after_init = {
+	&dev_attr_pa.attr,
+	&dev_attr_dcna.attr,
+	&dev_attr_scna.attr,
+	&dev_attr_seid.attr,
+	&dev_attr_deid.attr,
+	NULL,
+};
+
+static struct attribute *export_attrs[] __ro_after_init = {
+	&dev_attr_node_mem_size.attr,
+	&dev_attr_uba.attr,
+	&dev_attr_tokenid.attr,
+	&dev_attr_memory_from_user.attr,
+	&dev_attr_deid.attr,
+	NULL,
+};
+
+static struct attribute_group root_attrs_group __ro_after_init = {
+	.name = NULL,
+	.attrs = root_attrs,
+	.bin_attrs = root_bin_attrs,
+};
+
+#define SYSFS_NUMA_REMOTE 1U
+
+static unsigned int get_import_region_sysfs_index(const struct obmm_region *region)
+{
+	unsigned int index = 0;
+
+	if (region_numa_remote(region))
+		index |= SYSFS_NUMA_REMOTE;
+
+	return index;
+}
+
+static const struct attribute_group import_attrs_groups[] = {
+	[0] = {
+		.name = "import_info",
+		.attrs = import_mmap_attrs,
+	},
+	[SYSFS_NUMA_REMOTE] = {
+		.name = "import_info",
+		.attrs = import_numa_attrs,
+	},
+};
+
+static const struct attribute_group export_attrs_group = {
+	.name = "export_info",
+	.attrs = export_attrs,
+};
+
+static const struct attribute_group *obmm_import_attrs_groups_list[][3] = {
+	{ &root_attrs_group, &import_attrs_groups[0], NULL },
+	{ &root_attrs_group, &import_attrs_groups[1], NULL },
+	{ &root_attrs_group, &import_attrs_groups[2], NULL },
+	{ &root_attrs_group, &import_attrs_groups[3], NULL },
+};
+
+static const struct attribute_group *obmm_export_attrs_groups[] = {
+	&root_attrs_group,
+	&export_attrs_group,
+	NULL,
+};
+
+const struct attribute_group **obmm_region_get_attr_groups(const struct obmm_region *region)
+{
+	unsigned int index;
+
+	if (region->type == OBMM_EXPORT_REGION)
+		return obmm_export_attrs_groups;
+	index = get_import_region_sysfs_index(region);
+	return obmm_import_attrs_groups_list[index];
+}

drivers/ub/obmm/Makefile
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-2.0+
 
 obmm-y := obmm_core.o \
-	obmm_shm_dev.o obmm_cache.o obmm_import.o \
+	obmm_shm_dev.o obmm_sysfs_ub.o obmm_cache.o obmm_import.o \
 	obmm_export_from_user.o obmm_ownership.o conti_mem_allocator.o \
 	obmm_export.o obmm_export_from_pool.o obmm_preimport.o \
 	ubmempool_allocator.o obmm_lowmem.o obmm_export_region_ops.o \

drivers/ub/obmm/obmm_core.c
@@ -29,6 +29,7 @@
 #include "obmm_lowmem.h"
 #include "obmm_preimport.h"
 #include "obmm_addr_check.h"
+#include "obmm_sysfs.h"
 #include "obmm_export.h"
 #include "obmm_core.h"
 

drivers/ub/obmm/obmm_shm_dev.c
@@ -10,6 +10,7 @@
 #include <linux/mm.h>
 
 #include "obmm_cache.h"
+#include "obmm_sysfs.h"
 #include "obmm_export_region_ops.h"
 #include "obmm_import.h"
 #include "obmm_ownership.h"
@@ -897,6 +898,7 @@ int obmm_shm_dev_add(struct obmm_region *reg)
 	reg->cdevice.owner = THIS_MODULE;
 	reg->device.devt = devt;
 	reg->device.release = obmm_shm_dev_release;
+	reg->device.groups = obmm_region_get_attr_groups(reg);
 	reg->device.parent = obmm_shm_rootdev;
 	device_initialize(&reg->device);
 

drivers/ub/obmm/obmm_sysfs.h
@@ -1,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+ */
+
+#ifndef OBMM_SYSFS_H
+#define OBMM_SYSFS_H
+
+#include "obmm_core.h"
+
+const struct attribute_group **obmm_region_get_attr_groups(const struct obmm_region *);
+
+#endif

commit 53f22d6b0cc1564bfdc031fdeda448a9e53b4c1b

ub/obmm: minor bugfixes
euleros inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8539
CVE: NA

-------------------------------------

Fix following problems discovered by code view:
- When ioremap_cache fails, the function jumps to
  out_recover_kernel_pagetable but doesn't set ret to an error code,
  still returning 0 (success) when it should indicate failure.
  Fix: Add ret = -ENOMEM; before the goto statement when ioremap_cache
  fails.
- Returns -ENOMEM when conditions are not met, but for notifier_block
  callbacks, it should return specific notifier values like NOTIFY_DONE
  or NOTIFY_OK instead of negative values.
  Fix: Change return value from -ENOMEM to NOTIFY_DONE.
- The overflow check condition is logically flawed. When addition
  overflows, the result wraps around, potentially making the condition
  false even though an overflow occurred.
  Fix: Repair the check.
- The parm parameter is dereferenced directly without a NULL check.
  Fix: Return NOTIFY_DONE if data is NULL.
- The import_attrs_groups array only has 2 elements (indices 0 and 1),
  but obmm_import_attrs_groups_list attempts to access indices 2 and 3,
  causing uninitialized memory access.
  Fix: Remove entries with indices 2 and 3 from
  obmm_import_attrs_groups_list.

Fixes: ac654ff4a9eb ("obmm: Add sysfs interface for configuration and monitoring")
Fixes: f6bc214461e9 ("obmm: Add low memory management support")
Fixes: 131c090885fb ("obmm: Add continuous memory allocator for OBMM framework")
Signed-off-by: Li Ruilin <liruilin4@huawei.com>

drivers/ub/obmm/obmm_export_from_pool.c
@@ -221,7 +221,7 @@ static int calculate_export_region_size(unsigned long *total_size,
 			return -ENODEV;
 		}
 		if (cmd_export->size[i] != 0) {
-			if (*total_size > *total_size + cmd_export->size[i]) {
+			if (*total_size > ULONG_MAX - cmd_export->size[i]) {
 				pr_err("Memory size overflowed!\n");
 				return -EOVERFLOW;
 			}

drivers/ub/obmm/obmm_lowmem.c
@@ -26,13 +26,16 @@ static int obmm_lowmem_notify_handler(struct notifier_block *nb __always_unused,
 	bool is_huge = false;
 	int i;
 
+	if (!data)
+		return NOTIFY_DONE;
+
 	pr_debug_ratelimited("got lowmem message. pid=%d sync=%d reason=%u\n", current->pid,
 			     data->sync, data->reason);
 
 	if (data->reason != RR_DIRECT_RECLAIM &&
 	    data->reason != RR_KSWAPD &&
 	    data->reason != RR_HUGEPAGE_RECLAIM)
-		return -ENOMEM;
+		return NOTIFY_DONE;
 
 	if (data->reason == RR_HUGEPAGE_RECLAIM)
 		is_huge = true;
@@ -42,7 +45,7 @@ static int obmm_lowmem_notify_handler(struct notifier_block *nb __always_unused,
 		data->nr_freed += ubmempool_contract(data->nid[i], is_huge) >> PAGE_SHIFT;
 	}
 
-	return 0;
+	return NOTIFY_OK;
 }
 
 int lowmem_notify_init(void)

drivers/ub/obmm/obmm_sysfs_ub.c
@@ -254,8 +254,6 @@ static const struct attribute_group export_attrs_group = {
 static const struct attribute_group *obmm_import_attrs_groups_list[][3] = {
 	{ &root_attrs_group, &import_attrs_groups[0], NULL },
 	{ &root_attrs_group, &import_attrs_groups[1], NULL },
-	{ &root_attrs_group, &import_attrs_groups[2], NULL },
-	{ &root_attrs_group, &import_attrs_groups[3], NULL },
 };
 
 static const struct attribute_group *obmm_export_attrs_groups[] = {

drivers/ub/obmm/ubmempool_allocator.c
@@ -105,6 +105,7 @@ static int clear_block(struct conti_mem_allocator *a __always_unused, struct mem
 	va = ioremap_cache(pa, size);
 	if (!va) {
 		pr_err_ratelimited("%s: failed to run ioremap.\n", __func__);
+		ret = -ENOMEM;
 		goto out_recover_kernel_pagetable;
 	}
 	memset(va, 0, size);