OERV-RVCI/rvck-olk

commit e7a6bffde0fe08cd6a2752fd372ef84ebc0e7cda

netfilter: nf_tables: unconditionally bump set->nelems before insertion
mainline inclusion
from mainline-v7.0-rc3
commit def602e498a4f951da95c95b1b8ce8ae68aa733a
category: bugfix
bugzilla: https://atomgit.com/src-openeuler/kernel/issues/13901
CVE: CVE-2026-23272

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=def602e498a4f951da95c95b1b8ce8ae68aa733a

--------------------------------

In case that the set is full, a new element gets published then removed
without waiting for the RCU grace period, while RCU reader can be
walking over it already.

To address this issue, add the element transaction even if set is full,
but toggle the set_full flag to report -ENFILE so the abort path safely
unwinds the set to its previous state.

As for element updates, decrement set->nelems to restore it.

A simpler fix is to call synchronize_rcu() in the error path.
However, with a large batch adding elements to already maxed-out set,
this could cause noticeable slowdown of such batches.

Fixes: 35d0ac9070ef ("netfilter: nf_tables: fix set->nelems counting with no NLM_F_EXCL")
Reported-by: Inseo An <y0un9sa@gmail.com>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
Signed-off-by: Florian Westphal <fw@strlen.de>
Conflicts:
     net/netfilter/nf_tables_api.c
[ Context conflict ]
Signed-off-by: Dong Chenchen <dongchenchen2@huawei.com>

net/netfilter/nf_tables_api.c
@@ -6811,6 +6811,7 @@ static int nft_add_set_elem(struct nft_ctx *ctx, struct nft_set *set,
 	enum nft_registers dreg;
 	struct nft_trans *trans;
 	u64 timeout;
+	bool set_full = false;
 	u64 expiration;
 	int err, i;
 	u8 ulen;
@@ -7102,10 +7103,18 @@ static int nft_add_set_elem(struct nft_ctx *ctx, struct nft_set *set,
 	if (err < 0)
 		goto err_elem_free;
 
+	if (!(flags & NFT_SET_ELEM_CATCHALL)) {
+		unsigned int max = nft_set_maxsize(set), nelems;
+
+		nelems = atomic_inc_return(&set->nelems);
+		if (nelems > max)
+			set_full = true;
+	}
+
 	trans = nft_trans_elem_alloc(ctx, NFT_MSG_NEWSETELEM, set);
 	if (trans == NULL) {
 		err = -ENOMEM;
-		goto err_elem_free;
+		goto err_set_size;
 	}
 
 	ext->genmask = nft_genmask_cur(ctx->net);
@@ -7137,23 +7146,14 @@ static int nft_add_set_elem(struct nft_ctx *ctx, struct nft_set *set,
 		goto err_element_clash;
 	}
 
-	if (!(flags & NFT_SET_ELEM_CATCHALL)) {
-		unsigned int max = nft_set_maxsize(set);
-
-		if (!atomic_add_unless(&set->nelems, 1, max)) {
-			err = -ENFILE;
-			goto err_set_full;
-		}
-	}
-
-	nft_trans_elem(trans) = elem;
 	nft_trans_commit_list_add_tail(ctx->net, trans);
-	return 0;
+	return set_full ? -ENFILE : 0;
 
-err_set_full:
-	nft_setelem_remove(ctx->net, set, &elem);
 err_element_clash:
 	kfree(trans);
+err_set_size:
+	if (!(flags & NFT_SET_ELEM_CATCHALL))
+		atomic_dec(&set->nelems);
 err_elem_free:
 	nf_tables_set_elem_destroy(ctx, set, elem.priv);
 err_parse_data:

commit ce07ff4681bc4d83315681ae78e0b7ef1a5bd315

netfilter: nf_tables: Fix null ptr dereference of nft_setelem_remove
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/src-openeuler/kernel/issues/14141
CVE: CVE-2026-23272

--------------------------------

The initialization process of elem is missing in nft_add_set_elem(),
which lead to null-ptr-deref of nft_setelem_remove as below.

Unable to handle kernel NULL pointer dereference at virtual address 0000000000000014
Call trace:
 nft_setelem_remove+0x28/0xe0 [nf_tables]
 __nf_tables_abort+0x5f8/0xbe8 [nf_tables]
 nf_tables_abort+0x64/0x1c8 [nf_tables]
 nfnetlink_rcv_batch+0x2d8/0x850 [nfnetlink]
 nfnetlink_rcv+0x168/0x1a8 [nfnetlink]
 netlink_unicast_kernel+0x7c/0x160
 netlink_unicast+0x1ac/0x250
 netlink_sendmsg+0x21c/0x458
 __sock_sendmsg+0x4c/0xa8
 ____sys_sendmsg+0x280/0x300
 ___sys_sendmsg+0x8c/0xf8
 __sys_sendmsg+0x74/0xe0
 __arm64_sys_sendmsg+0x2c/0x40
 invoke_syscall+0x50/0x128
 el0_svc_common.constprop.0+0xc8/0xf0
 do_el0_svc+0x48/0x78
 el0_slow_syscall+0x44/0x1b8
 el0t_64_sync_handler+0x100/0x130
 el0t_64_sync+0x188/0x190

Initialize elem to fix it.

Fixes: e7a6bffde0fe ("netfilter: nf_tables: unconditionally bump set->nelems before insertion")
Signed-off-by: Dong Chenchen <dongchenchen2@huawei.com>

net/netfilter/nf_tables_api.c
@@ -7144,6 +7144,7 @@ static int nft_add_set_elem(struct nft_ctx *ctx, struct nft_set *set,
 		goto err_element_clash;
 	}
 
+	nft_trans_elem(trans) = elem;
 	nft_trans_commit_list_add_tail(ctx->net, trans);
 	return set_full ? -ENFILE : 0;
 