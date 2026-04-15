OERV-RVCI/rvck-olk

commit 24c71ca3842ca57c683837dd4fc18ae0637dde9d

ub: cdma: support the probe and remove processes
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID459U
CVE: NA

-----------------------------------------------------------

This patch implements the probe and remove processes for
auxiliary bus driver devices in the CDMA driver. The
implementation includes device creation and initialization,
adding the device to the device list, querying the device's
eid and upi, and the open, close, and ioctl processes for
the device file.

Signed-off-by: Zhipeng Lu <luzhipeng8@h-partners.com>
Signed-off-by: Hongwu Wang <wanghongwu9@huawei.com>

drivers/ub/cdma/cdma_chardev.c
@@ -1,0 +1,188 @@
+// SPDX-License-Identifier: GPL-2.0+
+/* Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved. */
+
+#define pr_fmt(fmt) "CDMA: " fmt
+#define dev_fmt pr_fmt
+
+#include <linux/fs.h>
+#include <ub/ubus/ubus.h>
+#include "cdma_ioctl.h"
+#include "cdma_chardev.h"
+#include "cdma_types.h"
+#include "cdma.h"
+
+#define CDMA_DEVICE_NAME "cdma/dev"
+
+struct cdma_num_manager {
+	struct idr idr;
+	spinlock_t lock;
+};
+
+static struct cdma_num_manager cdma_num_mg = {
+	.idr = IDR_INIT(cdma_num_mg.idr),
+	.lock = __SPIN_LOCK_UNLOCKED(cdma_num_mg.lock),
+};
+
+static void cdma_num_free(struct cdma_dev *cdev)
+{
+	spin_lock(&cdma_num_mg.lock);
+	idr_remove(&cdma_num_mg.idr, cdev->chardev.dev_num);
+	spin_unlock(&cdma_num_mg.lock);
+}
+
+static int cdma_num_alloc(struct cdma_dev *cdev)
+{
+#define CDMA_START 0
+#define CDMA_END 0xffff
+	int id;
+
+	idr_preload(GFP_KERNEL);
+	spin_lock(&cdma_num_mg.lock);
+	id = idr_alloc(&cdma_num_mg.idr, cdev, CDMA_START, CDMA_END, GFP_NOWAIT);
+	spin_unlock(&cdma_num_mg.lock);
+	idr_preload_end();
+
+	return id;
+}
+
+static long cdma_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+#define CDMA_MAX_CMD_SIZE 8192
+	struct cdma_file *cfile = (struct cdma_file *)file->private_data;
+	struct cdma_ioctl_hdr hdr = { 0 };
+	int ret;
+
+	if (cmd == CDMA_SYNC) {
+		ret = copy_from_user(&hdr, (void *)arg, sizeof(hdr));
+		if (ret || hdr.args_len > CDMA_MAX_CMD_SIZE) {
+			pr_err("copy user ret = %d, input parameter len = %u.\n",
+				ret, hdr.args_len);
+			return -EINVAL;
+		}
+		ret = cdma_cmd_parse(cfile, &hdr);
+		return ret;
+	}
+
+	pr_err("invalid ioctl command, command = %u.\n", cmd);
+	return -ENOIOCTLCMD;
+}
+
+static int cdma_open(struct inode *inode, struct file *file)
+{
+	struct cdma_chardev *chardev;
+	struct cdma_file *cfile;
+	struct cdma_dev *cdev;
+
+	chardev = container_of(inode->i_cdev, struct cdma_chardev, cdev);
+	cdev = container_of(chardev, struct cdma_dev, chardev);
+
+	cfile = kzalloc(sizeof(struct cdma_file), GFP_KERNEL);
+	if (!cfile)
+		return -ENOMEM;
+
+	mutex_lock(&cdev->file_mutex);
+	cfile->cdev = cdev;
+	kref_init(&cfile->ref);
+	file->private_data = cfile;
+	list_add_tail(&cfile->list, &cdev->file_list);
+	nonseekable_open(inode, file);
+	mutex_unlock(&cdev->file_mutex);
+
+	return 0;
+}
+
+static int cdma_close(struct inode *inode, struct file *file)
+{
+	struct cdma_file *cfile = (struct cdma_file *)file->private_data;
+	struct cdma_dev *cdev;
+
+	cdev = cfile->cdev;
+
+	mutex_lock(&cdev->file_mutex);
+	list_del(&cfile->list);
+	mutex_unlock(&cdev->file_mutex);
+
+	kref_put(&cfile->ref, cdma_release_file);
+	pr_debug("cdma close success.\n");
+
+	return 0;
+}
+
+static const struct file_operations cdma_ops = {
+	.owner = THIS_MODULE,
+	.unlocked_ioctl = cdma_ioctl,
+	.open = cdma_open,
+	.release = cdma_close,
+};
+
+void cdma_destroy_chardev(struct cdma_dev *cdev)
+{
+	struct cdma_chardev *chardev = &cdev->chardev;
+
+	if (!chardev->dev)
+		return;
+
+	device_destroy(cdma_cdev_class, chardev->devno);
+	cdev_del(&chardev->cdev);
+	unregister_chrdev_region(chardev->devno, CDMA_MAX_DEVICES);
+	cdma_num_free(cdev);
+}
+
+int cdma_create_chardev(struct cdma_dev *cdev)
+{
+	struct cdma_chardev *chardev = &cdev->chardev;
+	int ret;
+
+	chardev->dev_num = cdma_num_alloc(cdev);
+	if (chardev->dev_num < 0) {
+		dev_err(cdev->dev, "alloc dev_num failed, ret = %d\n", chardev->dev_num);
+		return -ENOMEM;
+	}
+
+	ret = snprintf(chardev->name, sizeof(chardev->name),
+		       "%s.%d", CDMA_DEVICE_NAME, chardev->dev_num);
+	if (ret < 0) {
+		dev_err(cdev->dev, "sprintf failed in create cdma chardev\n");
+		goto num_free;
+	}
+
+	ret = alloc_chrdev_region(&chardev->devno, 0, CDMA_MAX_DEVICES,
+				  chardev->name);
+	if (ret) {
+		dev_err(cdev->dev, "alloc chrdev region failed, ret = %d\n", ret);
+		goto num_free;
+	}
+
+	cdev_init(&chardev->cdev, &cdma_ops);
+	ret = cdev_add(&chardev->cdev, chardev->devno, CDMA_MAX_DEVICES);
+	if (ret) {
+		dev_err(cdev->dev, "cdev add failed, ret = %d\n", ret);
+		goto chrdev_unregister;
+	}
+
+	chardev->dev = device_create(cdma_cdev_class, NULL, chardev->devno,
+				     NULL, chardev->name);
+	if (IS_ERR(chardev->dev)) {
+		ret = PTR_ERR(chardev->dev);
+		dev_err(cdev->dev, "create device failed, ret = %d\n", ret);
+		goto cdev_delete;
+	}
+
+	dev_dbg(cdev->dev, "create chardev: %s succeeded\n", chardev->name);
+	return 0;
+
+cdev_delete:
+	cdev_del(&chardev->cdev);
+chrdev_unregister:
+	unregister_chrdev_region(chardev->devno, CDMA_MAX_DEVICES);
+num_free:
+	cdma_num_free(cdev);
+	return ret;
+}
+
+void cdma_release_file(struct kref *ref)
+{
+	struct cdma_file *cfile = container_of(ref, struct cdma_file, ref);
+
+	kfree(cfile);
+}

drivers/ub/cdma/cdma_cmd.c
@@ -1,0 +1,190 @@
+// SPDX-License-Identifier: GPL-2.0+
+/* Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved. */
+
+#define dev_fmt(fmt) "CDMA: " fmt
+
+#include <linux/slab.h>
+#include <linux/dmapool.h>
+
+#include "cdma.h"
+#include <ub/ubase/ubase_comm_dev.h>
+#include <ub/ubase/ubase_comm_cmd.h>
+#include <ub/ubase/ubase_comm_ctrlq.h>
+#include "cdma_cmd.h"
+
+static int cdma_cmd_query_fw_resource(struct cdma_dev *cdev, struct cdma_ue_info *out_addr)
+{
+#define CDMA_QUERY_UE_RES 0x0004
+	struct ubase_cmd_buf out = { 0 };
+	struct ubase_cmd_buf in = { 0 };
+
+	ubase_fill_inout_buf(&in, CDMA_QUERY_UE_RES, true, 0, NULL);
+	ubase_fill_inout_buf(&out, CDMA_QUERY_UE_RES, true,
+			     sizeof(*out_addr), out_addr);
+
+	return ubase_cmd_send_inout(cdev->adev, &in, &out);
+}
+
+static int cdma_query_caps_from_firmware(struct cdma_dev *cdev)
+{
+	struct cdma_caps *caps = &cdev->caps;
+	struct cdma_ue_info cmd = { 0 };
+	int ret;
+
+	ret = cdma_cmd_query_fw_resource(cdev, &cmd);
+	if (ret)
+		return dev_err_probe(cdev->dev, ret, "query fw resource failed\n");
+
+	caps->jfs_sge = cmd.jfs_sge;
+	caps->trans_mode = cmd.trans_mode;
+	caps->seid.max_cnt = cmd.seid_upi_tbl_num;
+	caps->feature = cmd.cap_info;
+	caps->ue_cnt = cmd.ue_cnt;
+	caps->ue_id = cmd.ue_id;
+
+	dev_dbg(cdev->dev, "jfs_sge = 0x%x, trans_mode = 0x%x, seid.max_cnt = 0x%x\n",
+		 caps->jfs_sge, caps->trans_mode, caps->seid.max_cnt);
+	dev_dbg(cdev->dev, "feature = 0x%x, ue_cnt = 0x%x, ue_id = 0x%x\n",
+		 caps->feature, caps->ue_cnt, caps->ue_id);
+
+	return 0;
+}
+
+static int cdma_set_caps_from_adev_caps(struct cdma_dev *cdev)
+{
+#define MAX_WQEBB_IN_SQE 4
+	struct cdma_caps *caps = &cdev->caps;
+	struct ubase_adev_caps *adev_caps;
+
+	adev_caps = ubase_get_cdma_caps(cdev->adev);
+	if (!adev_caps) {
+		dev_err(cdev->dev, "get cdma adev caps failed\n");
+		return -EINVAL;
+	}
+
+	caps->jfs.max_cnt = adev_caps->jfs.max_cnt;
+	caps->jfs.depth = adev_caps->jfs.depth / MAX_WQEBB_IN_SQE;
+	caps->jfs.start_idx = adev_caps->jfs.start_idx;
+	caps->jfc.max_cnt = adev_caps->jfc.max_cnt;
+	caps->jfc.depth = adev_caps->jfc.depth;
+	caps->jfc.start_idx = adev_caps->jfc.start_idx;
+	caps->cqe_size = adev_caps->cqe_size;
+
+	return 0;
+}
+
+static int cdma_set_caps_from_ubase_caps(struct cdma_dev *cdev)
+{
+	struct cdma_caps *caps = &cdev->caps;
+	struct ubase_caps *ubase_caps;
+
+	ubase_caps = ubase_get_dev_caps(cdev->adev);
+	if (!ubase_caps) {
+		dev_err(cdev->dev, "get cdma ubase caps failed\n");
+		return -EINVAL;
+	}
+
+	caps->comp_vector_cnt = ubase_caps->num_ceq_vectors;
+	caps->public_jetty_cnt = ubase_caps->public_jetty_cnt;
+	cdev->eid = ubase_caps->eid;
+	cdev->upi = ubase_caps->upi;
+
+	return 0;
+}
+
+int cdma_init_dev_caps(struct cdma_dev *cdev)
+{
+	struct cdma_caps *caps = &cdev->caps;
+	int ret;
+	u8 i;
+
+	ret = cdma_query_caps_from_firmware(cdev);
+	if (ret)
+		return ret;
+
+	ret = cdma_set_caps_from_adev_caps(cdev);
+	if (ret)
+		return ret;
+
+	ret = cdma_set_caps_from_ubase_caps(cdev);
+	if (ret)
+		return ret;
+
+	caps->queue.max_cnt = min(caps->jfs.max_cnt, caps->jfc.max_cnt);
+	caps->queue.start_idx = 0;
+	caps->jfce.max_cnt = caps->jfc.max_cnt;
+	caps->jfce.start_idx = 0;
+
+	dev_info(cdev->dev, "query cdev eid = 0x%x, cdev upi = 0x%x\n", cdev->eid,
+		 cdev->upi);
+	dev_info(cdev->dev, "queue:max_cnt = 0x%x, start_idx = 0x%x\n",
+		 caps->queue.max_cnt, caps->queue.start_idx);
+	dev_info(cdev->dev, "jfs:max_cnt = 0x%x, depth = 0x%x, start_idx = 0x%x\n",
+		 caps->jfs.max_cnt, caps->jfs.depth, caps->jfs.start_idx);
+	dev_info(cdev->dev, "jfce:max_cnt = 0x%x, depth = 0x%x, start_idx = 0x%x\n",
+		 caps->jfce.max_cnt, caps->jfce.depth, caps->jfce.start_idx);
+	dev_info(cdev->dev, "jfc:max_cnt = 0x%x, depth = 0x%x, start_idx = 0x%x\n",
+		 caps->jfc.max_cnt, caps->jfc.depth, caps->jfc.start_idx);
+	dev_info(cdev->dev, "comp_vector_cnt = 0x%x, public_jetty_cnt = 0x%x\n",
+		 caps->comp_vector_cnt, caps->public_jetty_cnt);
+	dev_info(cdev->dev, "sl_num = 0x%x\n", cdev->sl_num);
+	for (i = 0; i < cdev->sl_num; i++)
+		dev_info(cdev->dev, "sl[%u] = 0x%x\n", i, cdev->sl[i]);
+
+	return 0;
+}
+
+int cdma_ctrlq_query_eu(struct cdma_dev *cdev)
+{
+#define CDMA_CTRLQ_QUERY_SEID_UPI 0x1
+#define CDMA_CTRLQ_CMD_SEID_UPI 0xB5
+	struct cdma_device_attr *attr = &cdev->base.attr;
+	struct eu_query_out out_query = { 0 };
+	struct eu_query_in in_query = { 0 };
+	struct ubase_ctrlq_msg msg = { 0 };
+	struct eu_info *eus = attr->eus;
+	int ret;
+	u8 i;
+
+	in_query.cmd = CDMA_CTRLQ_CMD_SEID_UPI;
+
+	msg = (struct ubase_ctrlq_msg) {
+		.service_ver = UBASE_CTRLQ_SER_VER_01,
+		.service_type = UBASE_CTRLQ_SER_TYPE_DEV_REGISTER,
+		.opcode = CDMA_CTRLQ_QUERY_SEID_UPI,
+		.need_resp = 1,
+		.is_resp = 0,
+		.resv = 0,
+		.resp_seq = 0,
+		.in_size = sizeof(in_query),
+		.in = &in_query,
+		.out_size = sizeof(out_query),
+		.out = &out_query,
+	};
+
+	ret = ubase_ctrlq_send_msg(cdev->adev, &msg);
+	if (ret) {
+		dev_err(cdev->dev,
+			"query seid upi from ctrl cpu failed, ret = %d.\n", ret);
+		return ret;
+	}
+
+	if (!out_query.seid_num || out_query.seid_num > CDMA_MAX_EU_NUM) {
+		dev_err(cdev->dev,
+			"query seid upi num is invalid, num = %u.\n",
+			out_query.seid_num);
+		return -EINVAL;
+	}
+
+	mutex_lock(&cdev->eu_mutex);
+	memcpy(eus, out_query.eus, sizeof(struct eu_info) * out_query.seid_num);
+	attr->eu_num = out_query.seid_num;
+
+	for (i = 0; i < attr->eu_num; i++)
+		dev_dbg(cdev->dev,
+			"cdma init eus[%u], upi = 0x%x, eid = 0x%x, eid_idx = 0x%x.\n",
+			i, eus[i].upi, eus[i].eid.dw0, eus[i].eid_idx);
+	mutex_unlock(&cdev->eu_mutex);
+
+	return 0;
+}

drivers/ub/cdma/cdma_ioctl.c
@@ -1,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0+
+/* Copyright (c) 2025 HiSilicon Technologies Co., Ltd. All rights reserved. */
+
+#define dev_fmt(fmt) "CDMA: " fmt
+
+#include <ub/ubus/ubus.h>
+
+#include <uapi/ub/cdma/cdma_abi.h>
+#include "cdma.h"
+#include "cdma_types.h"
+#include "cdma_ioctl.h"
+
+typedef int (*cdma_cmd_handler)(struct cdma_ioctl_hdr *hdr,
+				struct cdma_file *cfile);
+
+static cdma_cmd_handler g_cdma_cmd_handler[CDMA_CMD_MAX] = {
+};
+
+int cdma_cmd_parse(struct cdma_file *cfile, struct cdma_ioctl_hdr *hdr)
+{
+	struct cdma_dev *cdev = cfile->cdev;
+
+	if (hdr->command >= CDMA_CMD_MAX || !g_cdma_cmd_handler[hdr->command]) {
+		dev_err(cdev->dev,
+			"invalid cdma user command or no handler, command = %u\n",
+			hdr->command);
+		return -EINVAL;
+	}
+
+	return g_cdma_cmd_handler[hdr->command](hdr, cfile);
+}

arch/arm64/configs/openeuler_defconfig
@@ -8349,6 +8349,9 @@ CONFIG_UB_UNIC=m
 CONFIG_UB_UNIC_UBL=y
 CONFIG_UB_UNIC_DCB=y
 
+# UB CDMA driver
+CONFIG_UB_CDMA=m
+
 # end of unified bus
 
 # UMMU

drivers/ub/Kconfig
@@ -16,6 +16,7 @@ if UB
 source "drivers/ub/ubus/Kconfig"
 source "drivers/ub/ubfi/Kconfig"
 source "drivers/ub/ubase/Kconfig"
+source "drivers/ub/cdma/Kconfig"
 
 config UB_URMA
 	tristate "Unified Bus (UB) urma support"

drivers/ub/Makefile
@@ -3,4 +3,6 @@
 obj-y += ubus/
 obj-y += ubfi/
 obj-$(CONFIG_UB_UBASE) += ubase/
+obj-$(CONFIG_UB_CDMA) += cdma/
 obj-$(CONFIG_UB_URMA) += urma/
+

drivers/ub/cdma/Kconfig
@@ -1,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0+
+menuconfig UB_CDMA
+	default n
+	tristate "cdma driver"
+	depends on UB_UBASE && UB_UMMU_CORE
+	help
+	   This option enables support for CDMA drivers. The CDMA driver facilitates
+	   the creation and destruction of CDMA devices, as well as the creation of
+	   resources within CDMA devices to perform DMA read/write tasks and retrieve
+	   the completion status of executed tasks.

drivers/ub/cdma/Makefile
@@ -1,0 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0+
+
+cdma-$(CONFIG_UB_CDMA) := cdma_main.o cdma_dev.o cdma_chardev.o cdma_cmd.o cdma_tid.o cdma_ioctl.o
+
+obj-m += cdma.o

commit 792b9535641fd255e84f16d0b8cace7193b9949d

ub: cdma: cdma code format issue bugfix
driver inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8339
CVE: NA

-----------------------------------------------------------

cdma code format issue bugfix
The size check logic in cdma_mmap has been modified because
currently, there are only usage scenarios where it equals
the page size.
The locking mechanism in cdma_umap_priv_init ensures the
security of the process vma pointer at the code level.
The return value of cdma_create_ucontext was modified
because the return value of copy_from_user cannot be
directly used as the return value.

Fixes: 24c71ca3842c ("ub: cdma: support the probe and remove processes")

Signed-off-by: Zhipeng Lu <luzhipeng8@h-partners.com>

drivers/ub/cdma/cdma_chardev.c
@@ -174,6 +174,7 @@ static int cdma_remap_pfn_range(struct cdma_file *cfile, struct vm_area_struct *
 static int cdma_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	struct cdma_file *cfile = (struct cdma_file *)file->private_data;
+	unsigned long size = vma->vm_end - vma->vm_start;
 	struct cdma_umap_priv *priv;
 	int ret;
 
@@ -182,12 +183,12 @@ static int cdma_mmap(struct file *file, struct vm_area_struct *vma)
 		return -ENODEV;
 	}
 
-	if (((vma->vm_end - vma->vm_start) % PAGE_SIZE) != 0) {
-		pr_err("mmap failed, expect vm area size to be an integer multiple of page size.\n");
+	if (size != PAGE_SIZE) {
+		pr_err("expect size 0x%lx, but got 0x%lx\n", PAGE_SIZE, size);
 		return -EINVAL;
 	}
 
-	priv = kzalloc(sizeof(struct cdma_umap_priv), GFP_KERNEL);
+	priv = kzalloc(sizeof(*priv), GFP_KERNEL);
 	if (!priv)
 		return -ENOMEM;
 

drivers/ub/cdma/cdma_cmd.c
@@ -12,7 +12,8 @@
 #include <ub/ubase/ubase_comm_ctrlq.h>
 #include "cdma_cmd.h"
 
-static int cdma_cmd_query_fw_resource(struct cdma_dev *cdev, struct cdma_ue_info *out_addr)
+static int cdma_cmd_query_fw_resource(struct cdma_dev *cdev,
+				      struct cdma_ue_info *out_addr)
 {
 #define CDMA_QUERY_UE_RES 0x0004
 	struct ubase_cmd_buf out = { 0 };

drivers/ub/cdma/cdma_debugfs.c
@@ -18,6 +18,7 @@
 
 #define BUF_10_BASE 10
 #define BUF_SIZE 8
+#define CDMA_S_IRUSR 0400
 
 /* ctx debugfs start */
 static void cdma_get_ctx_info(struct cdma_dev *cdev,
@@ -718,7 +719,7 @@ static int cdma_dbg_create_cfg_file(struct cdma_dev *cdev,
 			if (!cdma_dbg_cfg[j].dentry_valid[i])
 				continue;
 			debugfs_file = debugfs_create_file(
-				cdma_dbg_cfg[j].name, 0400, cur_dir,
+				cdma_dbg_cfg[j].name, CDMA_S_IRUSR, cur_dir,
 				&cdev->cdbgfs.cfg, &cdma_dbg_cfg[j].file_ops);
 			if (!debugfs_file)
 				return -ENOMEM;

drivers/ub/cdma/cdma_event.c
@@ -540,7 +540,7 @@ static int cdma_get_async_event(struct cdma_jfae *jfae, struct file *filp,
 	} else {
 		INIT_LIST_HEAD(&event_list);
 		ret = cdma_wait_event(&jfae->jfe, filp->f_flags & O_NONBLOCK, 1,
-				&event_cnt, &event_list);
+				      &event_cnt, &event_list);
 		if (ret < 0) {
 			pr_err("wait event failed, ret = %d.\n", ret);
 			return ret;

drivers/ub/cdma/cdma_ioctl.c
@@ -84,7 +84,7 @@ static int cdma_create_ucontext(struct cdma_ioctl_hdr *hdr,
 				  (u32)sizeof(args));
 	if (ret) {
 		dev_err(cdev->dev, "get user data failed, ret = %d.\n", ret);
-		return ret;
+		return -EFAULT;
 	}
 
 	ctx = cdma_alloc_context(cdev, false);

drivers/ub/cdma/cdma_mmap.c
@@ -10,7 +10,7 @@
 void cdma_umap_priv_init(struct cdma_umap_priv *priv,
 			struct vm_area_struct *vma)
 {
-	struct cdma_file *cfile = (struct cdma_file *)vma->vm_file->private_data;
+	struct cdma_file *cfile = vma->vm_file->private_data;
 
 	priv->vma = vma;
 	vma->vm_private_data = priv;
@@ -45,6 +45,7 @@ void cdma_unmap_vma_pages(struct cdma_file *cfile)
 		if (!mm)
 			return;
 
+		mmap_read_lock(mm);
 		mutex_lock(&cfile->umap_mutex);
 		list_for_each_entry_safe(priv, next_priv, &cfile->umaps_list, node) {
 			vma = priv->vma;
@@ -54,6 +55,7 @@ void cdma_unmap_vma_pages(struct cdma_file *cfile)
 			zap_vma_ptes(vma, vma->vm_start, vma->vm_end - vma->vm_start);
 		}
 		mutex_unlock(&cfile->umap_mutex);
+		mmap_read_unlock(mm);
 
 		mmput(mm);
 	}