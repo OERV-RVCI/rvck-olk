OERV-RVCI/rvck-olk

commit 6ce46d339a414f4a9b1ef87be83a83332d4bc0fb

drivers/ub/sentry: add oom rate limit
drivers inclusion
category: feature
bugzilla: https://atomgit.com/openeuler/kernel/issues/8797

--------------------------------

different flow control policies are provided based on oom event types.

before getting ack msg, sentry need sleep for a while.

Signed-off-by: shixuantong <shixuantong1@huawei.com>
Signed-off-by: luckky <guodashun1@huawei.com>

drivers/ub/sentry/sentry_local_reporter.c
@@ -17,16 +17,18 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/types.h>
-#include <linux/ratelimit.h>
 
 #include "smh_message.h"
+#include "oom_rate_limit.h"
 
 #define REBOOT_RESULT_SUCCESS	0
 #define MAX_TIMEOUT		3600000
 #define FD_MODE			0
 #define NUMA_MODE		1
 
-static DEFINE_RATELIMIT_STATE(oom_log_rs, HZ, 5);
+#define WAIT_TIME_TO_GET_ACK 300   // sleep time before get ack msg
+
+DEFINE_RATELIMIT_STATE(oom_log_rs, HZ, 5);
 
 static unsigned int reboot_timeout_ms = 30000;
 static unsigned int oom_timeout_ms = 30000;
@@ -88,6 +90,8 @@ static int smh_message_retry_send(struct sentry_msg_helper_msg *msg, bool ack)
 		if (!ack)
 			return ret;
 
+		msleep_interruptible(WAIT_TIME_TO_GET_ACK);
+
 		ret = smh_message_get_ack(msg);
 		if (ret)
 			return 0;
@@ -148,8 +152,8 @@ static int lowmem_notifier_callback(struct notifier_block *nb,
 {
 	struct reclaim_notify_data *data = parm;
 	struct sentry_msg_helper_msg msg = {0};
-	int ret;
-	int i;
+	int ret, i;
+	bool should_report = false;
 
 	if (!g_oom_enable)
 		return NOTIFY_OK;
@@ -167,9 +171,20 @@ static int lowmem_notifier_callback(struct notifier_block *nb,
 
 	msg.type = SMH_MESSAGE_OOM;
 	msg.helper_msg_info.oom_info.nr_nid = data->nr_nid > OOM_EVENT_MAX_NUMA_NODES ?
-					      OOM_EVENT_MAX_NUMA_NODES : data->nr_nid;
+					 OOM_EVENT_MAX_NUMA_NODES : data->nr_nid;
 	for (i = 0; i < msg.helper_msg_info.oom_info.nr_nid; i++)
 		msg.helper_msg_info.oom_info.nid[i] = data->nid[i];
+
+	for (i = 0; i < msg.helper_msg_info.oom_info.nr_nid; i++) {
+		ret = oom_rate_limit_check(data->reason, data->nid[i]);
+		if (ret == 0) {
+			should_report = true;
+			break;
+		}
+	}
+
+	if (!should_report)
+		return NOTIFY_OK;
 
 	msg.helper_msg_info.oom_info.sync = data->sync;
 	msg.helper_msg_info.oom_info.timeout = oom_timeout_ms;
@@ -455,6 +470,93 @@ static const struct proc_ops proc_ub_mem_fault_with_kill_file_operations = {
 };
 
 /**
+ * proc_oom_rate_limit_write - Write handler for proc interface to configure OOM rate limit
+ * @file: Proc file structure (unused)
+ * @ubuf: User-space buffer containing configuration string
+ * @cnt: Number of bytes to write
+ * @ppos: File position (unused)
+ *
+ * This function handles write operations to the proc file for OOM rate limit configuration.
+ * It copies the user buffer, removes trailing newline if present, and passes the configuration
+ * string to the rate limit configuration handler.
+ *
+ * Configuration string format: "event1:limit1,event2:limit2,..."
+ * Example: "RR_KSWAPD:10,RR_DIRECT_RECLAIM:5"
+ *
+ * Return: Number of bytes written on success, negative error code on failure
+ */
+static ssize_t proc_oom_rate_limit_write(struct file *file,
+					 const char __user *ubuf,
+					 size_t cnt, loff_t *ppos)
+{
+	char *buf;
+	int ret;
+
+	buf = kzalloc(cnt + 1, GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	if (copy_from_user(buf, ubuf, cnt)) {
+		kfree(buf);
+		return -EFAULT;
+	}
+
+	if (cnt > 0 && buf[cnt - 1] == '\n')
+		buf[cnt - 1] = '\0';
+
+	ret = oom_rate_limit_config_write(buf, cnt);
+	kfree(buf);
+
+	if (ret)
+		return ret;
+
+	return cnt;
+}
+
+/**
+ * proc_oom_rate_limit_show - Read handler for proc interface to show OOM rate limit config
+ * @file: Proc file structure (unused)
+ * @buf: User-space buffer to receive configuration data
+ * @count: Maximum number of bytes to read
+ * @ppos: File position (updated by simple_read_from_buffer)
+ *
+ * This function handles read operations from the proc file for OOM rate limit configuration.
+ * It retrieves the current configuration, formats it as a string, and copies it to user space.
+ *
+ * Output format: "event1:limit1,event2:limit2,..." followed by newline
+ * Example: "RR_KSWAPD:10,RR_DIRECT_RECLAIM:5\n"
+ *
+ * Return: Number of bytes read on success, negative error code on failure
+ */
+static ssize_t proc_oom_rate_limit_show(struct file *file,
+					char __user *buf,
+					size_t count, loff_t *ppos)
+{
+	char *kbuf;
+	int ret;
+
+	kbuf = kzalloc(OOM_RATE_LIMIT_RAW_STR_MAX_LEN, GFP_KERNEL);
+	if (!kbuf)
+		return -ENOMEM;
+
+	ret = oom_rate_limit_config_read(kbuf, OOM_RATE_LIMIT_RAW_STR_MAX_LEN);
+	if (ret < 0) {
+		kfree(kbuf);
+		return ret;
+	}
+
+	ret = simple_read_from_buffer(buf, count, ppos, kbuf, ret);
+	kfree(kbuf);
+
+	return ret;
+}
+
+static const struct proc_ops proc_oom_rate_limit_file_operations = {
+	.proc_read	= proc_oom_rate_limit_show,
+	.proc_write	= proc_oom_rate_limit_write,
+};
+
+/**
  * ub_mem_ras_handler - UB memory RAS error handler
  * @phys_addr: Physical address of the error
  * @err_type: Error type
@@ -524,6 +626,8 @@ static int __init sentry_reporter_init(void)
 	if (ret)
 		return ret;
 
+	oom_rate_limit_init();
+
 	g_sentry_reporter_proc_dir = proc_mkdir_mode("sentry_reporter",
 						     PROC_DIR_PERMISSION, NULL);
 	if (!g_sentry_reporter_proc_dir) {
@@ -543,6 +647,9 @@ static int __init sentry_reporter_init(void)
 	ret |= sentry_create_proc_file("ub_mem_fault",
 				      g_sentry_reporter_proc_dir,
 				      &proc_ub_mem_fault_enable_file_operations);
+	ret |= sentry_create_proc_file("oom_rate_limit",
+				      g_sentry_reporter_proc_dir,
+				      &proc_oom_rate_limit_file_operations);
 	if (ret < 0)
 		goto remove_proc_dir;
 

drivers/ub/sentry/Makefile
@@ -9,4 +9,5 @@ obj-$(CONFIG_UB_SENTRY_REMOTE) += sentry_uvb_comm.o
 obj-$(CONFIG_UB_SENTRY_REMOTE) += sentry_remote_reporter.o
 
 sentry_msg_helper-y := smh_core.o smh_message.o
+sentry_reporter-y := sentry_local_reporter.o oom_rate_limit.o
 sentry_remote_reporter-y := sentry_remote_server.o sentry_remote_client.o

drivers/ub/sentry/oom_rate_limit.c
@@ -1,0 +1,251 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+ *
+ * Description: OOM Event Rate Limit Implementation
+ * Author: Luckky
+ * Create: 2026-03-20
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/jiffies.h>
+
+#include "oom_rate_limit.h"
+#include "smh_common_type.h"
+
+#undef pr_fmt
+#define pr_fmt(fmt) "[sentry][oom_limit]: " fmt
+
+static struct event_rate_limit_config g_event_config[MAX_EVENT_TYPES];
+static struct event_rate_limit_state g_event_state[MAX_EVENT_TYPES][OOM_EVENT_MAX_NUMA_NODES];
+
+static int g_config_event_num;
+
+const char *event_type_names[] = {
+	"RR_KSWAPD",
+	"RR_DIRECT_RECLAIM",
+	"RR_HUGEPAGE_RECLAIM"
+};
+
+
+/**
+ * parse_config_string - Parse configuration string and fill config structure
+ * @buf: Input buffer containing configuration information
+ * @len: Length of the input buffer
+ * @config: Output parameter, stores parsed configuration
+ *
+ * Configuration string format: "event_name1:limit1,event_name2:limit2,..."
+ * Example: "RR_KSWAPD:10,RR_DIRECT_RECLAIM:5"
+ *
+ * Return: Number of parsed events on success, negative error code on failure
+ */
+static int parse_config_string(const char *buf, size_t len,
+				   struct event_rate_limit_config *config)
+{
+	char *copy_str;
+	char *token;
+	char *event_name;
+	char *limit_str;
+	int limit;
+	int event_idx;
+	int ret = 0;
+	int config_event_limit_num = 0;
+
+	copy_str = kzalloc(len + 1, GFP_KERNEL);
+	if (!copy_str)
+		return -ENOMEM;
+
+	memcpy(copy_str, buf, len);
+	copy_str[len] = '\0';
+
+	token = strsep(&copy_str, ",");
+	while (token) {
+		event_name = strsep(&token, ":");
+		if (!event_name || !token) {
+			pr_err("parse oom event name failed\n");
+			ret = -EINVAL;
+			goto out;
+		}
+
+		limit_str = strsep(&token, ":");
+		if (!limit_str) {
+			pr_err("parse oom event limit value failed\n");
+			ret = -EINVAL;
+			goto out;
+		}
+
+		if (kstrtoint(limit_str, 10, &limit)) {
+			pr_err("covert limit value failed\n");
+			ret = -EINVAL;
+			goto out;
+		}
+		if (limit < 0) {
+			pr_err("invalid limit config (%s), it can't be an negative number\n",
+				limit_str);
+			ret = -EINVAL;
+			goto out;
+		}
+		for (event_idx = 0; event_idx < MAX_EVENT_TYPES; event_idx++) {
+			if (strcmp(event_name, event_type_names[event_idx]) == 0) {
+				config[event_idx].limit = limit;
+				config_event_limit_num++;
+				break;
+			}
+		}
+
+		token = strsep(&copy_str, ",");
+	}
+	ret = config_event_limit_num;
+
+out:
+	kfree(copy_str);
+	return ret;
+}
+
+/**
+ * oom_rate_limit_init - Initialize OOM rate limit module
+ *
+ * Initializes configuration array, and state array.
+ * Configuration array limit is initialized to -1 indicating not configured (unlimited).
+ * State array timestamps and counts are initialized to 0.
+ *
+ * Return: Always returns 0 (success)
+ */
+int oom_rate_limit_init(void)
+{
+	int i, j;
+
+	for (i = 0; i < MAX_EVENT_TYPES; i++)
+		g_event_config[i].limit = -1;
+
+	for (i = 0; i < MAX_EVENT_TYPES; i++) {
+		for (j = 0; j < OOM_EVENT_MAX_NUMA_NODES; j++) {
+			g_event_state[i][j].last_update_time = 0;
+			g_event_state[i][j].count = 0;
+		}
+	}
+
+	return 0;
+}
+
+/**
+ * oom_rate_limit_check - Check whether specified event should be reported
+ * @reason: Event reason type (reclaim type)
+ * @nid: NUMA node ID
+ *
+ * Checks whether the specified event should be reported based on the configured
+ * rate limiting policy.
+ * Rate limit semantics:
+ * - limit = 0: Reporting completely disabled
+ * - limit = -1: Unlimited, always report
+ * - limit = n: Report at most n times per second (counted independently per NUMA node)
+ *
+ * The global variables g_event_config and g_event_state ara not locked. It is acceptable
+ * that the number of reported oom events msg exceeds the configured limit. Locking these
+ * variables will affect the performance.
+ *
+ * Return: 0 if should report, -EINVAL if shouldn't report
+ */
+int oom_rate_limit_check(enum reclaim_reason reason, int nid)
+{
+	unsigned long current_time;
+	unsigned long time_diff;
+	int should_report = 0;
+
+	if (nid < 0 || nid >= OOM_EVENT_MAX_NUMA_NODES) {
+		pr_err("invalid params\n");
+		return -EINVAL;
+	}
+
+	if (g_event_config[reason].limit == 0) {
+		if (__ratelimit(&oom_log_rs))
+			pr_warn("sentry not report %s type oom event msg\n",
+					event_type_names[reason]);
+		return -EINVAL;
+	}
+
+	if (g_event_config[reason].limit == -1)
+		return 0;
+
+	current_time = jiffies;
+	time_diff = current_time - g_event_state[reason][nid].last_update_time;
+
+	if (time_diff >= HZ * RATE_LIMIT_WINDOW_SEC) {
+		g_event_state[reason][nid].count = 0;
+		g_event_state[reason][nid].last_update_time = current_time;
+	}
+
+	if (g_event_state[reason][nid].count < g_event_config[reason].limit) {
+		g_event_state[reason][nid].count++;
+		should_report = 1;
+	}
+
+	return should_report ? 0 : -EINVAL;
+}
+
+/**
+ * oom_rate_limit_config_write - Write rate limit configuration
+ * @buf: Buffer containing configuration information
+ * @len: Buffer length
+ *
+ * Parses user-provided configuration string and updates global rate limit configuration.
+ * Configuration string format: "event1:limit1,event2:limit2,..."
+ *
+ * Return: 0 on success, negative error code on failure
+ */
+int oom_rate_limit_config_write(const char *buf, size_t len)
+{
+	struct event_rate_limit_config new_config[MAX_EVENT_TYPES];
+	int i;
+	int ret;
+
+	for (i = 0; i < MAX_EVENT_TYPES; i++)
+		new_config[i].limit = -1;
+
+	ret = parse_config_string(buf, len, new_config);
+	if (ret < 0) {
+		pr_err("parse limit config failed\n");
+		return ret;
+	}
+	g_config_event_num = ret;
+
+	for (i = 0; i < MAX_EVENT_TYPES; i++) {
+		g_event_config[i].limit = new_config[i].limit;
+		pr_info("config oom event (%s type) limit to %d\n",
+			event_type_names[i], new_config[i].limit);
+	}
+	return 0;
+}
+
+/**
+ * oom_rate_limit_config_read - Read current rate limit configuration
+ * @buf: Output buffer
+ * @len: Buffer length
+ *
+ * Formats the current active rate limit configuration as a string and writes it
+ * to the output buffer.
+ * Output format matches the write format: "event1:limit1,event2:limit2,..."
+ *
+ * Return: Number of bytes written on success, negative error code on failure
+ */
+int oom_rate_limit_config_read(char *buf, size_t len)
+{
+	int pos = 0;
+	int i;
+	int ret;
+
+	for (i = 0; i < MAX_EVENT_TYPES; i++) {
+		if (g_event_config[i].limit != -1) {
+			ret = snprintf(buf + pos, len - pos, "%s:%d%s",
+					   event_type_names[i],
+					   g_event_config[i].limit,
+					   (i < g_config_event_num - 1) ? "," : "\n");
+			if (ret < 0 || ret >= len - pos)
+				return -ENOSPC;
+			pos += ret;
+		}
+	}
+	return pos;
+}

drivers/ub/sentry/oom_rate_limit.h
@@ -1,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+ * Description: OOM Event Rate Limit Header File
+ * Author: Luckky
+ * Create: 2026-03-20
+ */
+
+#ifndef OOM_RATE_LIMIT_H
+#define OOM_RATE_LIMIT_H
+
+#include <linux/types.h>
+#include <linux/spinlock.h>
+#include <linux/mm.h>
+#include <linux/ratelimit.h>
+
+#define MAX_EVENT_TYPES 3
+#define RATE_LIMIT_WINDOW_SEC 1
+#define OOM_RATE_LIMIT_RAW_STR_MAX_LEN 256
+
+extern struct ratelimit_state oom_log_rs;
+
+struct event_rate_limit_config {
+	int limit;
+};
+
+struct event_rate_limit_state {
+	unsigned long last_update_time;
+	int count;
+};
+
+int oom_rate_limit_init(void);
+
+int oom_rate_limit_check(enum reclaim_reason reason, int nid);
+
+int oom_rate_limit_config_write(const char *buf, size_t len);
+int oom_rate_limit_config_read(char *buf, size_t len);
+
+#endif

commit 51c66e79f28e07693a5cfb11371719b5424af831

drivers/ub/sentry: fix input length validation in proc_oom_rate_limit_write
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8483

--------------------------------

Add input length validation to prevent integer overflow in memory
allocation. The kzalloc(cnt + 1) could overflow if cnt is very large
(size_t), leading to small buffer allocation and potential buffer
overflow when copy_from_user writes cnt bytes.

Security Impact:
- Without this check, a malicious user could pass SIZE_MAX or large
  value causing cnt+1 to wrap to 0 or small value
- kzalloc would allocate tiny buffer but copy_from_user writes full
  cnt bytes, causing kernel memory corruption

Fix: Add bounds check before memory allocation, limit input to
OOM_RATE_LIMIT_RAW_STR_MAX_LEN (256) which is sufficient for config.

Fixes: 6ce46d339a41 ("drivers/ub/sentry: add oom rate limit")
Signed-off-by: shixuantong <shixuantong1@huawei.com>
Signed-off-by: guodashun <guodashun1@huawei.com>

drivers/ub/sentry/sentry_local_reporter.c
@@ -495,6 +495,12 @@ static ssize_t proc_oom_rate_limit_write(struct file *file,
 	char *buf;
 	int ret;
 
+	if (cnt == 0 || cnt > OOM_RATE_LIMIT_RAW_STR_MAX_LEN) {
+		pr_err("invalid input length for oom_rate_limit, max %d\n",
+		       OOM_RATE_LIMIT_RAW_STR_MAX_LEN);
+		return -EINVAL;
+	}
+
 	buf = kzalloc(cnt + 1, GFP_KERNEL);
 	if (!buf)
 		return -ENOMEM;