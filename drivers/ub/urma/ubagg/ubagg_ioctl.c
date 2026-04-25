OERV-RVCI/rvck-olk

commit 858ace3f811c79005bf67859d459db626844f787

urma: fix ubcore_get_device_by_eid uaf.
urma inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8921
CVE: NA

----------------------------------------------

The function ubcore_get_device_by_eid retrieves a device from
g_device_list without reference count protection. This could lead to
use-after-free (UAF) vulnerabilities in concurrent deletion scenarios
where the device might be freed while still being accessed, this commit
fixed it.

Fixes: 1fe6d02bf7ba ("urma: add ubagg ioctl interface and topology management")

Signed-off-by: Luo Zihang <luozihang2@huawei.com>

drivers/ub/urma/ubagg/ubagg_ioctl.c
@@ -1049,6 +1049,17 @@ set_ubagg_device_attr_by_ubcore_cap(struct ubcore_device *dev,
 	dev->attr.dev_cap = *dev_cap;
 }
 
+static void ubagg_put_ubcore_device(struct ubcore_device *dev)
+{
+	if (IS_ERR_OR_NULL(dev)) {
+		ubagg_log_err("Invalid parameter\n");
+		return;
+	}
+
+	if (atomic_dec_and_test(&dev->use_cnt))
+		complete(&dev->comp);
+}
+
 static int init_ubagg_dev(struct ubagg_device *ubagg_dev,
 			  struct ubagg_add_dev_by_uvs *arg)
 {
@@ -1083,6 +1094,7 @@ static int init_ubagg_dev(struct ubagg_device *ubagg_dev,
 
 		(void)memcpy(ubagg_dev->slave_dev_name[slave_dev_idx],
 			     dev->dev_name, UBAGG_MAX_DEV_NAME_LEN);
+		ubagg_put_ubcore_device(dev);
 		slave_dev_idx++;
 	}
 
@@ -1111,6 +1123,7 @@ static int init_ubagg_dev(struct ubagg_device *ubagg_dev,
 
 			(void)memcpy(ubagg_dev->slave_dev_name[slave_dev_idx],
 				     dev->dev_name, UBAGG_MAX_DEV_NAME_LEN);
+			ubagg_put_ubcore_device(dev);
 			slave_dev_idx++;
 		}
 	}
@@ -1725,6 +1738,7 @@ static int ubagg_get_dev_name(struct ubagg_get_dev_name_arg *arg)
 
 	(void)strscpy(arg->out.dev_name, dev->dev_name, UBAGG_MAX_DEV_NAME_LEN);
 
+	ubagg_put_ubcore_device(dev);
 	return 0;
 }
 

drivers/ub/urma/ubcore/ubcore_device.c
@@ -2469,6 +2469,7 @@ struct ubcore_device *ubcore_get_device_by_eid(union ubcore_eid *eid,
 				   sizeof(union ubcore_eid)) == 0 &&
 			    dev->transport_type == type) {
 				target = dev;
+				ubcore_get_device(target);
 				break;
 			}
 		}

drivers/ub/urma/ubcore/ubcore_uvs_cmd.c
@@ -214,6 +214,7 @@ static int ubcore_create_jetty_rsrc(struct ubcore_topo_map *topo_map)
 						&eid_info.eid_index);
 			if (ret != 0) {
 				ubcore_log_err("Failed to get eid index\n");
+				ubcore_put_device(dev);
 				return ret;
 			}
 
@@ -221,6 +222,7 @@ static int ubcore_create_jetty_rsrc(struct ubcore_topo_map *topo_map)
 						UBCORE_MGMT_EVENT_EID_ADD);
 			if (ret != 0) {
 				ubcore_log_err("Failed to call cm eid ops\n");
+				ubcore_put_device(dev);
 				return ret;
 			}
 
@@ -231,6 +233,7 @@ static int ubcore_create_jetty_rsrc(struct ubcore_topo_map *topo_map)
 				EID_RAW_ARGS(
 				cur_node_info->agg_devs[dev_idx].ues[die_idx].primary_eid),
 				eid_info.eid_index);
+			ubcore_put_device(dev);
 		}
 	}
 

commit 2b95c4269bb717d800a13cf4d1fa0fd7b8b97fcf

urma: fix ubcore_get_device_by_eid without put.
urma inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8921
CVE: NA

----------------------------------------------

The function ubcore_get_device_by_eid calls without put, causes device
can't free, this commit fixed it, add put device after
ubcore_get_device_by_eid.

Fixes: 858ace3f811c ("urma: fix ubcore_get_device_by_eid uaf.")

Signed-off-by: Luo Zihang <luozihang2@huawei.com>

drivers/ub/urma/ubagg/ubagg_ioctl.c
@@ -222,6 +222,7 @@ static int get_physical_device(struct ubagg_device *ubagg_dev,
 				continue;
 			}
 		}
+		ubagg_put_ubcore_device(dev);
 	}
 	return 0;
 }
@@ -1131,7 +1132,7 @@ set_ubagg_device_attr_by_ubcore_cap(struct ubcore_device *dev,
 	dev->attr.dev_cap = *dev_cap;
 }
 
-static void ubagg_put_ubcore_device(struct ubcore_device *dev)
+void ubagg_put_ubcore_device(struct ubcore_device *dev)
 {
 	if (IS_ERR_OR_NULL(dev)) {
 		ubagg_log_err("Invalid parameter\n");

drivers/ub/urma/ubagg/ubagg_ioctl.h
@@ -202,6 +202,6 @@ struct ubagg_add_dev_by_uvs {
 };
 
 long ubagg_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
-
+void ubagg_put_ubcore_device(struct ubcore_device *dev);
 void ubagg_clear_dev_list(void);
 #endif // UBAGG_IOCTL_H

drivers/ub/urma/ubagg/ubagg_netlink.c
@@ -17,6 +17,7 @@
 #include <ub/urma/ubcore_uapi.h>
 
 #include "ubagg_log.h"
+#include "ubagg_ioctl.h"
 #include "ubagg_netlink.h"
 
 #define UBAGG_MAX_NL_MSG_BUF_LEN 2048
@@ -161,7 +162,7 @@ static int ubagg_nl_handle_user_msg(struct sk_buff *skb, struct genl_info *info)
 			       ", peer_eid:" EID_FMT ", payload_len:%u\n",
 			       EID_ARGS(local_eid), EID_ARGS(peer_eid),
 			       payload_len);
-
+	ubagg_put_ubcore_device(dev);
 	return ret;
 }
 