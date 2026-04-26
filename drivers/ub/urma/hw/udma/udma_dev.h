OERV-RVCI/rvck-olk

commit 05c3cda9c33a875b228a3f70b9f18f4346b68461

ub: udma: Support loading and unloading driver.
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID457F
CVE: NA

--------------------------------------------------------------

This patch adds the function of loading and unloading the driver.
In driver loading process, udma queries the required hardware
information from ubase and stores it in the internal structure of the
driver. At the same time, udma also provides ubcore with ops for
querying device status and create rc table. This patch also adds the
function of mailbox and software table for udma driver.

Signed-off-by: Wei Qin <qinwei61@huawei.com>
Signed-off-by: zhaoweibo <zhaoweibo3@huawei.com>

drivers/ub/urma/hw/udma/udma_dev.h
@@ -1,0 +1,147 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/* Copyright(c) 2025 HiSilicon Technologies CO., Ltd. All rights reserved. */
+
+#ifndef __UDMA_DEV_H__
+#define __UDMA_DEV_H__
+
+#include <ub/urma/ubcore_api.h>
+#include <ub/ubase/ubase_comm_dev.h>
+#include <ub/ubase/ubase_comm_eq.h>
+#include "udma_def.h"
+
+#define UBCORE_MAX_DEV_NAME 64
+
+#define MAX_JETTY_IN_JETTY_GRP 32
+
+#define MAX_WQEBB_IN_SQE 4
+
+#define UDMA_HW_PAGE_SHIFT 12
+#define UDMA_HW_PAGE_SIZE (1 << UDMA_HW_PAGE_SHIFT)
+
+#define UDMA_DEV_UE_NUM 47
+
+#define UDMA_MAX_SL_NUM 16
+#define UDMA_DEFAULT_SL_NUM 0
+
+#define UDMA_CQE_SIZE 64
+
+#define UDMA_MAX_GRANT_SIZE 0xFFFFFFFFF000
+
+enum udma_status {
+	UDMA_NORMAL,
+	UDMA_SUSPEND,
+};
+
+struct udma_ida {
+	struct ida	ida;
+	uint32_t	min; /* Lowest ID to allocate. */
+	uint32_t	max; /* Highest ID to allocate. */
+	uint32_t	next; /* Next ID to allocate. */
+	spinlock_t	lock;
+};
+
+struct udma_group_bitmap {
+	uint32_t min;
+	uint32_t max;
+	uint32_t grp_next;
+	uint32_t n_bits;
+	uint32_t *bit;
+	uint32_t bitmap_cnt;
+	spinlock_t lock;
+};
+
+struct udma_group_table {
+	struct xarray xa;
+	struct udma_group_bitmap bitmap_table;
+};
+
+struct udma_table {
+	struct xarray xa;
+	struct udma_ida ida_table;
+};
+
+struct udma_mailbox_cmd {
+	struct dma_pool *pool;
+	struct semaphore poll_sem;
+	struct rw_semaphore udma_mb_rwsem;
+};
+
+struct udma_dev {
+	struct ubase_adev_com comdev;
+	struct ubcore_device ub_dev;
+	struct device *dev;
+	struct udma_caps caps;
+	uint16_t adev_id;
+	uint32_t chip_id;
+	uint32_t die_id;
+	uint32_t port_id;
+	uint32_t port_logic_id;
+	bool is_ue;
+	char dev_name[UBCORE_MAX_DEV_NAME];
+	struct udma_mailbox_cmd mb_cmd;
+	struct udma_table jfr_table;
+	struct udma_group_table jetty_table;
+	struct udma_table jfc_table;
+	struct udma_table jetty_grp_table;
+	struct udma_ida rsvd_jetty_ida_table;
+	struct udma_table rc_table;
+	struct xarray crq_nb_table;
+	struct xarray npu_nb_table;
+	struct mutex npu_nb_mutex;
+	struct xarray tpn_ue_idx_table;
+	resource_size_t db_base;
+	void __iomem *k_db_base;
+	struct xarray ksva_table;
+	struct mutex ksva_mutex;
+	struct xarray eid_table;
+	struct mutex eid_mutex;
+	uint32_t tid;
+	struct iommu_sva *ksva;
+	uint32_t status;
+	uint32_t ue_num;
+	uint32_t ue_id;
+	struct page *db_page;
+	u8 udma_tp_sl_num;
+	u8 udma_ctp_sl_num;
+	u8 unic_sl_num;
+	u8 udma_total_sl_num;
+	u8 udma_tp_resp_vl_off;
+	u8 udma_tp_sl[UDMA_MAX_SL_NUM];
+	u8 udma_ctp_sl[UDMA_MAX_SL_NUM];
+	u8 unic_sl[UDMA_MAX_SL_NUM];
+	u8 udma_sl[UDMA_MAX_SL_NUM];
+	int disable_ue_rx_count;
+	struct mutex disable_ue_rx_mutex;
+};
+
+#define UDMA_ERR_MSG_LEN	128
+struct udma_func_map {
+	char err_msg[UDMA_ERR_MSG_LEN];
+	int (*init_func)(struct udma_dev *udma_dev);
+	void (*uninit_func)(struct udma_dev *udma_dev);
+};
+
+static inline struct udma_dev *get_udma_dev(struct auxiliary_device *adev)
+{
+	return (struct udma_dev *)dev_get_drvdata(&adev->dev);
+}
+
+static inline struct udma_dev *to_udma_dev(struct ubcore_device *ub_device)
+{
+	return container_of(ub_device, struct udma_dev, ub_dev);
+}
+
+static inline void udma_id_free(struct udma_ida *ida_table, int idx)
+{
+	ida_free(&ida_table->ida, idx);
+}
+
+void udma_destroy_tables(struct udma_dev *udma_dev);
+int udma_init_tables(struct udma_dev *udma_dev);
+int udma_probe(struct auxiliary_device *adev, const struct auxiliary_device_id *id);
+void udma_remove(struct auxiliary_device *adev);
+void udma_reset_init(struct auxiliary_device *adev);
+void udma_reset_uninit(struct auxiliary_device *adev);
+void udma_reset_down(struct auxiliary_device *adev);
+
+#endif /* __UDMA_DEV_H__ */

arch/arm64/configs/openeuler_defconfig
@@ -8353,6 +8353,9 @@ CONFIG_UB_UNIC_DCB=y
 # UB CDMA driver
 CONFIG_UB_CDMA=m
 
+# UB UDMA driver
+CONFIG_UB_UDMA=m
+
 # end of unified bus
 
 # UMMU

drivers/ub/Kconfig
@@ -17,6 +17,7 @@ source "drivers/ub/ubus/Kconfig"
 source "drivers/ub/ubfi/Kconfig"
 source "drivers/ub/ubase/Kconfig"
 source "drivers/ub/cdma/Kconfig"
+source "drivers/ub/urma/hw/udma/Kconfig"
 
 config UB_URMA
 	tristate "Unified Bus (UB) urma support"

drivers/ub/Makefile
@@ -5,4 +5,5 @@ obj-y += ubfi/
 obj-$(CONFIG_UB_UBASE) += ubase/
 obj-$(CONFIG_UB_CDMA) += cdma/
 obj-$(CONFIG_UB_URMA) += urma/
+obj-$(CONFIG_UB_UDMA) += urma/hw/udma/
 

drivers/ub/urma/hw/udma/Kconfig
@@ -1,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0+
+# Copyright(c) 2025 HiSilicon Technologies CO., Ltd. All rights reserved.
+
+menuconfig UB_UDMA
+	default n
+	tristate "UB UDMA Driver"
+	depends on UB_UBASE && UB_URMA && UB_UMMU_CORE
+	help
+	  UDMA driver support for Hisilicon UBUS engine
+	  in Hisilicon SoC. To compile this driver,
+	  choose Y here: if UB_UDMA is m, this module
+	  will be called udma.

drivers/ub/urma/hw/udma/Makefile
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0+
+
+udma-$(CONFIG_UB_UDMA) := udma_main.o udma_cmd.o udma_common.o \
+	     udma_rct.o
+
+obj-m := udma.o

drivers/ub/urma/hw/udma/udma_cmd.c
@@ -1,0 +1,208 @@
+// SPDX-License-Identifier: GPL-2.0+
+/* Copyright(c) 2025 HiSilicon Technologies CO., Ltd. All rights reserved. */
+
+#define dev_fmt(fmt) "UDMA: " fmt
+
+#include <linux/slab.h>
+#include <linux/dmapool.h>
+#include <ub/ubase/ubase_comm_dev.h>
+#include "udma_cmd.h"
+
+bool debug_switch = true;
+
+int udma_cmd_init(struct udma_dev *udma_dev)
+{
+	sema_init(&udma_dev->mb_cmd.poll_sem, 1);
+	udma_dev->mb_cmd.pool = dma_pool_create("udma_cmd", udma_dev->dev,
+						UDMA_MAILBOX_SIZE,
+						UDMA_MAILBOX_SIZE, 0);
+	if (!udma_dev->mb_cmd.pool) {
+		dev_err(udma_dev->dev, "failed to dma_pool_create.\n");
+		return -ENOMEM;
+	}
+
+	init_rwsem(&udma_dev->mb_cmd.udma_mb_rwsem);
+
+	return 0;
+}
+
+void udma_cmd_cleanup(struct udma_dev *udma_dev)
+{
+	down_write(&udma_dev->mb_cmd.udma_mb_rwsem);
+	dma_pool_destroy(udma_dev->mb_cmd.pool);
+	up_write(&udma_dev->mb_cmd.udma_mb_rwsem);
+}
+
+struct ubase_cmd_mailbox *udma_alloc_cmd_mailbox(struct udma_dev *dev)
+{
+	struct ubase_cmd_mailbox *mailbox;
+
+	mailbox = kzalloc(sizeof(*mailbox), GFP_KERNEL);
+	if (!mailbox)
+		goto failed_alloc_mailbox;
+
+	down_read(&dev->mb_cmd.udma_mb_rwsem);
+	mailbox->buf = dma_pool_zalloc(dev->mb_cmd.pool, GFP_KERNEL,
+				       &mailbox->dma);
+	if (!mailbox->buf) {
+		dev_err(dev->dev, "failed to alloc buffer of mailbox.\n");
+		goto failed_alloc_mailbox_buf;
+	}
+
+	return mailbox;
+
+failed_alloc_mailbox_buf:
+	up_read(&dev->mb_cmd.udma_mb_rwsem);
+	kfree(mailbox);
+failed_alloc_mailbox:
+	return NULL;
+}
+
+void udma_free_cmd_mailbox(struct udma_dev *dev,
+			   struct ubase_cmd_mailbox *mailbox)
+{
+	if (!mailbox) {
+		dev_err(dev->dev, "Invalid mailbox.\n");
+		return;
+	}
+
+	dma_pool_free(dev->mb_cmd.pool, mailbox->buf, mailbox->dma);
+	up_read(&dev->mb_cmd.udma_mb_rwsem);
+	kfree(mailbox);
+}
+
+static bool udma_op_ignore_eagain(uint8_t op, void *buf)
+{
+	struct udma_mbx_op_match matches[] = {
+		{ UDMA_CMD_CREATE_JFS_CONTEXT, false },
+		{ UDMA_CMD_MODIFY_JFS_CONTEXT, true },
+		{ UDMA_CMD_DESTROY_JFS_CONTEXT, true },
+		{ UDMA_CMD_QUERY_JFS_CONTEXT, true },
+		{ UDMA_CMD_CREATE_JFC_CONTEXT, false },
+		{ UDMA_CMD_MODIFY_JFC_CONTEXT, true },
+		{ UDMA_CMD_DESTROY_JFC_CONTEXT, true },
+		{ UDMA_CMD_QUERY_JFC_CONTEXT, true },
+		{ UDMA_CMD_CREATE_JFR_CONTEXT, false },
+		{ UDMA_CMD_MODIFY_JFR_CONTEXT, true },
+		{ UDMA_CMD_DESTROY_JFR_CONTEXT, true },
+		{ UDMA_CMD_QUERY_JFR_CONTEXT, true },
+		{ UDMA_CMD_QUERY_TP_CONTEXT, true },
+		{ UDMA_CMD_CREATE_JETTY_GROUP_CONTEXT, false },
+		{ UDMA_CMD_MODIFY_JETTY_GROUP_CONTEXT, true },
+		{ UDMA_CMD_DESTROY_JETTY_GROUP_CONTEXT, true },
+		{ UDMA_CMD_QUERY_JETTY_GROUP_CONTEXT, true },
+		{ UDMA_CMD_CREATE_RC_CONTEXT, false },
+		{ UDMA_CMD_MODIFY_RC_CONTEXT, true },
+		{ UDMA_CMD_DESTROY_RC_CONTEXT, true },
+		{ UDMA_CMD_QUERY_RC_CONTEXT, true },
+		{ UDMA_CMD_READ_SEID_UPI, true },
+	};
+	uint32_t i;
+
+	for (i = 0; i < ARRAY_SIZE(matches); i++) {
+		if (op == matches[i].op)
+			return matches[i].ignore_ret;
+	}
+
+	return false;
+}
+
+int udma_post_mbox(struct udma_dev *dev, struct ubase_cmd_mailbox *mailbox,
+		   struct ubase_mbx_attr *attr)
+{
+	int ret;
+
+	if (debug_switch)
+		dev_info_ratelimited(dev->dev,
+				     "Send cmd mailbox, data: %08x %04x%04x.\n",
+				     attr->tag, attr->op, attr->mbx_ue_id);
+
+	ret = ubase_hw_upgrade_ctx_ex(dev->comdev.adev, attr, mailbox);
+
+	return (ret == -EAGAIN &&
+		udma_op_ignore_eagain(attr->op, mailbox->buf)) ? 0 : ret;
+}
+
+int udma_config_ctx_buf_to_hw(struct udma_dev *udma_dev,
+			      struct udma_buf *ctx_buf,
+			      struct ubase_mbx_attr *attr)
+{
+	struct ubase_cmd_mailbox mailbox;
+	int ret;
+
+	mailbox.dma = ctx_buf->addr;
+	ret = udma_post_mbox(udma_dev, &mailbox, attr);
+	if (ret)
+		dev_err(udma_dev->dev,
+			"failed to config ctx_buf to hw, ret = %d.\n", ret);
+
+	return ret;
+}
+
+int udma_cmd_query_hw_resource(struct udma_dev *udma_dev, void *out_addr)
+{
+	struct ubase_cmd_buf out = {};
+	struct ubase_cmd_buf in = {};
+
+	udma_fill_buf(&in, UDMA_CMD_QUERY_UE_RES, true, 0, NULL);
+	udma_fill_buf(&out, UDMA_CMD_QUERY_UE_RES, true,
+		      sizeof(struct udma_cmd_ue_resource), out_addr);
+
+	return ubase_cmd_send_inout(udma_dev->comdev.adev, &in, &out);
+}
+
+int post_mailbox_update_ctx(struct udma_dev *udma_dev, void *ctx, uint32_t size,
+			    struct ubase_mbx_attr *attr)
+{
+	struct ubase_cmd_mailbox *mailbox;
+	int ret;
+
+	mailbox = udma_alloc_cmd_mailbox(udma_dev);
+	if (!mailbox) {
+		dev_err(udma_dev->dev,
+			"failed to alloc mailbox for opcode 0x%x.\n", attr->op);
+		return -ENOMEM;
+	}
+
+	if (ctx)
+		memcpy(mailbox->buf, ctx, size);
+
+	ret = udma_post_mbox(udma_dev, mailbox, attr);
+	if (ret)
+		dev_err(udma_dev->dev,
+			"failed to post mailbox, opcode = 0x%x, ret = %d.\n", attr->op,
+			ret);
+
+	udma_free_cmd_mailbox(udma_dev, mailbox);
+
+	return ret;
+}
+
+struct ubase_cmd_mailbox *udma_mailbox_query_ctx(struct udma_dev *udma_dev,
+						struct ubase_mbx_attr *attr)
+{
+	struct ubase_cmd_mailbox *mailbox;
+	int ret;
+
+	mailbox = udma_alloc_cmd_mailbox(udma_dev);
+	if (!mailbox) {
+		dev_err(udma_dev->dev,
+			"failed to alloc mailbox query ctx, opcode = %u, id = %u.\n",
+			attr->op, attr->tag);
+		return NULL;
+	}
+
+	ret = udma_post_mbox(udma_dev, mailbox, attr);
+	if (ret) {
+		dev_err(udma_dev->dev,
+			"failed to post mailbox query ctx, opcode = %u, id = %u, ret = %d.\n",
+			attr->op, attr->tag, ret);
+		udma_free_cmd_mailbox(udma_dev, mailbox);
+		return NULL;
+	}
+
+	return mailbox;
+}
+
+module_param(debug_switch, bool, 0444);
+MODULE_PARM_DESC(debug_switch, "set debug print ON, default: true");

commit 4c4ea949ad36619a4451f52c92c5948aad378f50

udma：fix ub udma set dev name style
udma inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9034
CVE: NA

--------------------------

The name of the UDMA device is optimized.
The device name change caused by restart is fixed.

Fixes: 05c3cda9c33a ("ub: udma: Support loading and unloading driver.")
Signed-off-by: zhawenrui <zhawenrui@huawei.com>

Signed-off-by: suiyi_z <1520835527@qq.com>

drivers/ub/urma/hw/udma/udma_dev.h
@@ -11,6 +11,7 @@
 #include <uapi/ub/urma/udma/udma_abi.h>
 #include <ub/urma/udma/udma_ctl.h>
 
+extern bool dev_name_style;
 extern bool dfx_switch;
 extern bool cqe_mode;
 extern uint32_t batch_flush_query_freq;

drivers/ub/urma/hw/udma/udma_main.c
@@ -34,6 +34,7 @@
 
 #define UDMA_DRV_VER "1.0"
 
+bool dev_name_style;
 bool cqe_mode = true;
 uint32_t batch_flush_query_freq = 10;
 uint32_t batch_flush_query_timeout = 64000;
@@ -503,7 +504,12 @@ static int udma_set_ubcore_dev(struct udma_dev *udma_dev)
 	ub_dev->dma_dev = ub_dev->dev.parent;
 	ub_dev->attr.dev_cap.feature.value = udma_dev->caps.feature;
 
-	scnprintf(udma_dev->dev_name, UBCORE_MAX_DEV_NAME, "udma%hu", udma_dev->adev_id);
+	if (dev_name_style) {
+		scnprintf(udma_dev->dev_name, UBCORE_MAX_DEV_NAME,
+			"udmac%ud%ue%u", udma_dev->chip_id, udma_dev->die_id, udma_dev->ue_id);
+	} else {
+		scnprintf(udma_dev->dev_name, UBCORE_MAX_DEV_NAME, "udma%hu", udma_dev->adev_id);
+	}
 	strscpy(ub_dev->dev_name, udma_dev->dev_name, UBCORE_MAX_DEV_NAME);
 	scnprintf(ub_dev->ops->driver_name, UBCORE_MAX_DRIVER_NAME, "udma");
 
@@ -1430,6 +1436,9 @@ module_init(udma_init);
 module_exit(udma_exit);
 MODULE_VERSION(UDMA_DRV_VER);
 MODULE_LICENSE("GPL");
+
+module_param(dev_name_style, bool, 0644);
+MODULE_PARM_DESC(dev_name_style, "Set device name style, default: 0(0:disable, 1:enable)");
 
 module_param(cqe_mode, bool, 0444);
 MODULE_PARM_DESC(cqe_mode, "Set cqe reporting mode, default: 1 (0:BY_COUNT, 1:BY_CI_PI_GAP)");