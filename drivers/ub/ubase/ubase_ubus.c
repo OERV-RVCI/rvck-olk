OERV-RVCI/rvck-olk

commit f44f2fb07ce0fd5bc5dae9a9cc625707cac67eca

ub: ubase: optimized reboot time required
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8801
CVE: NA

---------------------------------------------------------------------

This patch used to optimized reboot time required.The code in the current
library exceeds the preset restart time of 3s during the shutdown process.
Therefore, the following two points are optimized:
1. During the shutdown process, the UE state machine does not need to
send an activation message and directly returns a failure message to the
Ubus. In this way, the UE state machine remains in the disabled state
and no subsequent disabling message is sent.
2. Set the AE semaphore to enable the AE polling thread to exit
immediately. After the preceding two optimizations, the reboot time is
optimized about 2-3 seconds.

Fixes: 727362c0978c ("ub: ubase: Support for ELR and entity reset.")
Signed-off-by: Xiaobo Zhang <zhangxiaobo21@huawei.com>

drivers/ub/ubase/ubase_ubus.c
@@ -293,6 +293,8 @@ static void ubase_ubus_shutdown(struct ub_entity *ue)
 	ubase_info(udev, "ubase shutdown start.\n");
 
 	__ubase_ubus_remove(ue);
+
+	ubase_info(udev, "ubase shutdown end.\n");
 }
 
 int ubase_ubus_irq_vectors_alloc(struct device *dev)

drivers/ub/ubase/ubase_dev.c
@@ -1656,27 +1656,30 @@ void ubase_activate_unregister(struct auxiliary_device *adev)
 }
 EXPORT_SYMBOL(ubase_activate_unregister);
 
-static int ubase_wait_activate_done(struct ubase_dev *udev, u16 bus_ue_id)
+static bool ubase_fast_shutdown(struct ubase_dev *udev,
+				struct ubase_act_info *info)
+{
+	return ((ubase_shutting_down(udev) || info->shutdown) &&
+		 ubase_is_ctrl_node(udev));
+}
+
+static int ubase_wait_activate_done(struct ubase_dev *udev, u16 bus_ue_id,
+				    struct ubase_act_info *info)
 {
 #define UBASE_ACTIVE_DEV_TIMEOUT_SHUTDOWN 1000
 #define UBASE_ACTIVE_DEV_TIMEOUT 10000
 
-	struct ub_entity *ue = container_of(udev->dev, struct ub_entity, dev);
-	struct ubase_act_info *info;
+	bool shutdown = ubase_fast_shutdown(udev, info);
 	u32 timeout;
 
-	info = (ue->entity_idx == bus_ue_id) ? &udev->act_ctx.self :
-		&udev->act_ctx.other;
-
-	timeout = ((ubase_shutting_down(udev) || info->shutdown) &&
-		   ubase_is_ctrl_node(udev)) ?
-		   UBASE_ACTIVE_DEV_TIMEOUT_SHUTDOWN : UBASE_ACTIVE_DEV_TIMEOUT;
+	timeout = shutdown ? UBASE_ACTIVE_DEV_TIMEOUT_SHUTDOWN :
+			     UBASE_ACTIVE_DEV_TIMEOUT;
 	if (!wait_for_completion_timeout(&info->activate_done,
 					 msecs_to_jiffies(timeout))) {
 		ubase_err(udev,
 			  "wait activate dev resp timeout(%u ms), bus_ue_id = %u, msn = %u.\n",
 			  timeout, bus_ue_id, info->wait_msn);
-		return -ETIMEDOUT;
+		return shutdown ? 0 : -ETIMEDOUT;
 	}
 
 	return info->result;
@@ -1712,10 +1715,24 @@ static void ubase_alloc_msn(struct ubase_dev *udev, u16 *msn)
 static int ubase_send_activate_dev_req(struct ubase_dev *udev, bool activate,
 				       u16 bus_ue_id)
 {
+	struct ub_entity *ue = container_of(udev->dev, struct ub_entity, dev);
 	struct ubase_activate_req req = {0};
+	struct ubase_act_info *info;
 	struct ubase_cmd_buf in;
 	u16 msn;
 	int ret;
+
+	info = (ue->entity_idx == bus_ue_id) ? &udev->act_ctx.self :
+	       &udev->act_ctx.other;
+
+	/* During the shutdown process, the activation message does not need to
+	 * be sent, and a failure message is directly returned to the ubus.
+	 * In this way, the UE state machine can remain in the disabled state,
+	 * and subsequent disable messages will not be sent. This speeds up the
+	 * shutdown process.
+	 */
+	if (ubase_fast_shutdown(udev, info) && activate)
+		return -EPERM;
 
 	req.activate = activate ? 1 : 0;
 	req.bus_ue_id = cpu_to_le16(bus_ue_id);
@@ -1734,7 +1751,7 @@ static int ubase_send_activate_dev_req(struct ubase_dev *udev, bool activate,
 		return ret;
 	}
 
-	return ubase_wait_activate_done(udev, bus_ue_id);
+	return ubase_wait_activate_done(udev, bus_ue_id, info);
 }
 
 int ubase_activate_handler(struct ubase_dev *udev, u32 bus_ue_id)

drivers/ub/ubase/ubase_eq.c
@@ -752,6 +752,9 @@ static void ubase_free_aeq_irq(struct ubase_dev *udev)
 	if (!aeq->ae_task)
 		return;
 
+	if (ubase_shutting_down(udev))
+		complete(&aeq->poll);
+
 	ret = kthread_stop(aeq->ae_task);
 	if (ret)
 		ubase_err(udev, "failed to stop ae task thread, ret = %d\n",

commit 84a615419b9c96eeafa24c79c2e750069f50ae57

ub: ubase: Rectify the problem that the shutdown process ends and logs are printed
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9028
CVE: NA

---------------------------------------------------------------------

Fix the use-after-free error. In the current 'ubase_ubus_shutdown'
function, the 'udev' pointer used by the second 'ubase_info' function
has already been released. In the environment, the system does not
break down after the reboot command is executed for multiple times.
This is because the dev device pointer pointed to by udev is not
released, allowing the last log entry to be printed. Therefore, the
dev_info interface is used to print the last log, do not use the udev
pointer.

Fixes: f44f2fb07ce0 ("ub: ubase: optimized reboot time required")
Signed-off-by: Xiaobo Zhang <zhangxiaobo21@huawei.com>

drivers/ub/ubase/ubase_ubus.c
@@ -295,7 +295,7 @@ static void ubase_ubus_shutdown(struct ub_entity *ue)
 
 	__ubase_ubus_remove(ue);
 
-	ubase_info(udev, "ubase shutdown end.\n");
+	dev_info(&ue->dev, "ubase shutdown end.\n");
 }
 
 int ubase_ubus_irq_vectors_alloc(struct device *dev)