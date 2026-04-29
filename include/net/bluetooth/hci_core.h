OERV-RVCI/rvck-olk

commit ec03e0b8aa8171483b621a63db9637197eaa70a6

Bluetooth: hci_sync: fix double free in 'hci_discovery_filter_clear()'
stable inclusion
from stable-v6.6.117
commit 7ce9bb0b95fc280e9212b8922590c492ca1d9c39
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8763

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=7ce9bb0b95fc280e9212b8922590c492ca1d9c39

--------------------------------

[ Upstream commit 2935e556850e9c94d7a00adf14d3cd7fe406ac03 ]

Function 'hci_discovery_filter_clear()' frees 'uuids' array and then
sets it to NULL. There is a tiny chance of the following race:

'hci_cmd_sync_work()'

 'update_passive_scan_sync()'

   'hci_update_passive_scan_sync()'

     'hci_discovery_filter_clear()'
       kfree(uuids);

       <-------------------------preempted-------------------------------->
                                           'start_service_discovery()'

                                             'hci_discovery_filter_clear()'
                                               kfree(uuids); // DOUBLE FREE

       <-------------------------preempted-------------------------------->

      uuids = NULL;

To fix it let's add locking around 'kfree()' call and NULL pointer
assignment. Otherwise the following backtrace fires:

[ ] ------------[ cut here ]------------
[ ] kernel BUG at mm/slub.c:547!
[ ] Internal error: Oops - BUG: 00000000f2000800 [#1] PREEMPT SMP
[ ] CPU: 3 UID: 0 PID: 246 Comm: bluetoothd Tainted: G O 6.12.19-kernel #1
[ ] Tainted: [O]=OOT_MODULE
[ ] pstate: 60400005 (nZCv daif +PAN -UAO -TCO -DIT -SSBS BTYPE=--)
[ ] pc : __slab_free+0xf8/0x348
[ ] lr : __slab_free+0x48/0x348
...
[ ] Call trace:
[ ]  __slab_free+0xf8/0x348
[ ]  kfree+0x164/0x27c
[ ]  start_service_discovery+0x1d0/0x2c0
[ ]  hci_sock_sendmsg+0x518/0x924
[ ]  __sock_sendmsg+0x54/0x60
[ ]  sock_write_iter+0x98/0xf8
[ ]  do_iter_readv_writev+0xe4/0x1c8
[ ]  vfs_writev+0x128/0x2b0
[ ]  do_writev+0xfc/0x118
[ ]  __arm64_sys_writev+0x20/0x2c
[ ]  invoke_syscall+0x68/0xf0

include/net/bluetooth/hci_core.h
@@ -899,6 +899,8 @@ static inline void hci_discovery_filter_clear(struct hci_dev *hdev)
 	spin_lock(&hdev->discovery.lock);
 	kfree(hdev->discovery.uuids);
 	hdev->discovery.uuids = NULL;
+	spin_unlock(&hdev->discovery.lock);
+
 	hdev->discovery.scan_start = 0;
 	hdev->discovery.scan_duration = 0;
 	spin_unlock(&hdev->discovery.lock);

commit 25413d915636ac626429a31e60c7921f9aada56f

Bluetooth: hci_sync: fix double unlock in hci_discovery_filter_clear()
hulk inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9070

--------------------------------

hci_discovery_filter_clear() acquires hdev->discovery.lock only once,
but due to the patch being applied repeatedly, spin_unlock() is called
twice. This leads to an unmatched unlock and may cause preempt count
underflow, resulting in scheduling warnings or other unexpected behavior.
Delete the extra spin_unlock() at the end of the function.

Fixes: ec03e0b8aa81 ("Bluetooth: hci_sync: fix double free in 'hci_discovery_filter_clear()'")
Fixes: 5b70b93fc768 ("Bluetooth: hci_sync: fix double free in 'hci_discovery_filter_clear()'")
Signed-off-by: Fanhua Li <lifanhua5@huawei.com>

include/net/bluetooth/hci_core.h
@@ -903,7 +903,6 @@ static inline void hci_discovery_filter_clear(struct hci_dev *hdev)
 
 	hdev->discovery.scan_start = 0;
 	hdev->discovery.scan_duration = 0;
-	spin_unlock(&hdev->discovery.lock);
 }
 
 bool hci_discovery_active(struct hci_dev *hdev);